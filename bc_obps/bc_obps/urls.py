2949cbb4f6dcc2d1b56a4aa3b072f9fb9a159cc4
SeSo <sobhani.sepehr@gmail.com>
2023-08-30 10:32:01 -0700

    chore: run black to format the code

--- bc_obps/bc_obps/urls.py
+++ bc_obps/bc_obps/urls.py
@@ -20,9 +20,11 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
+
 @api.get("/add")
 def add(request, a: int, b: int):
-    return {"result": a+b}
+    return {"result": a + b}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
