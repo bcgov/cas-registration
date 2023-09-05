366f40b11f60d7490da627d8966ecb9ca627e2f1
Brianna Cerkiewicz <brianna@button.is>
2023-09-05 13:53:06 -0700

    chore: move models to correct directory

--- bc_obps/registration/models.py
+++ bc_obps/registration/models.py
@@ -1,8 +1,28 @@
-from django.db import models, uuid
+import uuid
+from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.gis.db import models
+
+# this may be helpful: https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.FileField.upload_to
+class Document(models.Model):
+    file = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_comment="")
+    document_type = models.CharField(max_length=1000, db_comment="Type of document, e.g., boundary map")
+    description = models.CharField(max_length=1000, db_comment="")
+    file_name = models.CharField(max_length=1000, db_comment="")
+    file_type = models.CharField(max_length=1000, db_comment="")
+    file_size = models.CharField(max_length=1000, db_comment="")
+    class Meta:
+        db_table_comment = "Documents"
+
+class NaicsCode(models.Model):
+    naics_code = models.CharField(max_length=1000, db_comment="")
+    ciip_sector = models.CharField(max_length=1000, db_comment="")
+    naics_description = models.CharField(max_length=1000, db_comment="")
+    class Meta:
+        db_table_comment = "Naics codes"
 
 class UserAndContactCommonInfo(models.Model):
-    first_name = models.CharField(max_length=1000, db_comment="", db_comment="")
+    first_name = models.CharField(max_length=1000, db_comment="")
     last_name = models.CharField(max_length=1000, db_comment="")
     mailing_address = models.CharField(max_length=1000, db_comment="")
     email = models.EmailField(max_length=254, db_comment="")
@@ -12,8 +32,8 @@ class UserAndContactCommonInfo(models.Model):
         db_table_comment = "An abstract base class (used for putting common information into a number of other models) containing fields for users and contacts"
 
 class User(UserAndContactCommonInfo):
-    user_guid = models.CharField(max_length=1000, db_comment="")
-    business_guid uuid = models.CharField(max_length=1000, db_comment="")
+    user_guid = models.UUIDField(default=uuid.uuid4, db_comment="")
+    business_guid  = models.UUIDField(default=uuid.uuid4, db_comment="")
     position_title = models.CharField(max_length=1000, db_comment="")
     documents  = models.ManyToManyField(Document)
     class Meta:
@@ -21,33 +41,33 @@ class User(UserAndContactCommonInfo):
 
 class Contact(UserAndContactCommonInfo):
     is_operational_representative = models.BooleanField(db_comment="")
-    verified_at = models.DateTimeField(db_comment="")(db_comment="")
+    verified_at = models.DateTimeField(db_comment="")
     verified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_comment="")
     class Meta:
         db_table_comment = "Contacts (people who don't use the app, e.g. authorized signing officers)"
         indexes = [
-            models.Index(fields=["verified_by"], name="verified_by_idx"),
+            models.Index(fields=["verified_by"], name="contact_verified_by_idx"),
             ]
 
 class Operator (models.Model):
-    class Statuses(models.TextChoices, db_comment=""):
-        PENDING = 'pending', 'Pending'
-        APPROVED = 'approved', 'Approved'
-        DENIED = 'rejected', 'Rejected'
+    # class Statuses(models.TextChoices):
+    #     PENDING = 'pending', 'Pending'
+    #     APPROVED = 'approved', 'Approved'
+    #     DENIED = 'rejected', 'Rejected'
     legal_name = models.CharField(max_length=1000, db_comment="")  
     trade_name = models.CharField(max_length=1000, db_comment="")
     business_number = models.CharField(max_length=1000, db_comment="")
     business_structure = models.CharField(max_length=1000, db_comment="") 
     mailing_address = models.CharField(max_length=1000, db_comment="") 
     bceid = models.CharField(max_length=1000, db_comment="") 
-    parent_operator = models.ForeignKey('self', on_delete=models.CASCADE,db_comment="")
+    parent_operator = models.ForeignKey('self', on_delete=models.CASCADE, related_name='parent', db_comment="")
     relationship_with_parent_operator = models.CharField(max_length=1000, db_comment="") 
-    compliance_obligee = models.ForeignKey('self', on_delete=models.CASCADE,db_comment="")
+    compliance_obligee = models.ForeignKey('self', on_delete=models.CASCADE, related_name='obligee', db_comment="")
     date_aso_became_responsible_for_operator = models.DateTimeField(db_comment="")
     # status = models.CharField(max_length=50, choices=Statuses.choices, default=Statuses.PENDING, db_comment="")
     documents  = models.ManyToManyField(Document)
     contacts  = models.ManyToManyField(Contact)
-    operators = models.ManyToManyField(User, through="UserOperators")
+    operators = models.ManyToManyField(User, through="UserOperator")
     class Meta:
         db_table_comment = "Operators (also called organizations)"
         # don't need indexes if we end up using `unique`
@@ -57,28 +77,27 @@ class Operator (models.Model):
 
 class UserOperator(models.Model):
     
-    # class Roles(models.TextChoices, db_comment=""):
+    # class Roles(models.TextChoices):
     #     ADMIN = 'admin', 'Admin'
     #     SUPERADMIN = 'superadmin', 'SuperAdmin'
     
-    class Statuses(models.TextChoices, db_comment=""):
+    class Statuses(models.TextChoices):
         PENDING = 'pending', 'Pending'
         APPROVED = 'approved', 'Approved'
         REJECTED = 'rejected', 'Rejected'
 
     users = models.ForeignKey(User, on_delete=models.CASCADE)
-    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
-    # role = models.CharField(max_length=100, choices=Roles.choices, db_comment="")
+    operators = models.ForeignKey(Operator, on_delete=models.CASCADE)
     role = models.CharField(max_length=1000, db_comment="")
     status = models.CharField(max_length=1000, choices=Statuses.choices, default=Statuses.PENDING, db_comment="")
     user_is_aso = models.BooleanField
     user_is_third_party = models.BooleanField
-    proof_of_authority = models.ForeignKey(Documents, on_delete=models.DO_NOTHING, db_comment="")
+    proof_of_authority = models.ForeignKey(Document, on_delete=models.DO_NOTHING, db_comment="")
     class Meta:
         db_table_comment = "Through table to connect Users and Operators and track access requests"
         indexes = [
-            models.Index(fields=["user_id"], name="user_id_idx"),
-            models.Index(fields=["operator_id"], name="operator_id_idx")]
+            models.Index(fields=["users"], name="user_operator_user_id_idx"),
+            models.Index(fields=["operators"], name="user_operator_operator_id_idx")]
     
 class Operation(models.Model):
     operator_id = models.ForeignKey(Operator, on_delete=models.CASCADE, db_comment="")
@@ -92,16 +111,14 @@ class Operation(models.Model):
     ghfrp_id = models.CharField(max_length=1000, db_comment="")
     bcghrp_id = models.CharField(max_length=1000, db_comment="")
     petrinex_id = models.CharField(max_length=1000, db_comment="")
-    # brianna install postgis
-    location = models.PointField(geography=True, default=Point(0.0, 0.0, db_comment=""), db_comment="")
+    location = models.PointField(geography=True, db_comment="")
     legal_land_description = models.CharField(max_length=1000, db_comment="")
     nearest_municipality = models.CharField(max_length=1000, db_comment="")
-    boundary_map = models.ForeignKey(Documents, on_delete=models.DO_NOTHING, db_comment="")
-    operator_percent_of_ownership = models.DecimalField(db_comment="")
+    operator_percent_of_ownership = models.DecimalField(decimal_places=10, max_digits=10,db_comment="")
     registered_for_obps = models.BooleanField
     verified_at = models.DateTimeField(db_comment="")
     verified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_comment="")
-    estimated_emissions = models.DecimalField(db_comment="")
+    estimated_emissions = models.DecimalField(decimal_places=10, max_digits=10,db_comment="")
     documents  = models.ManyToManyField(Document)
     contacts  = models.ManyToManyField(Contact)
     class Meta:
@@ -109,24 +126,9 @@ class Operation(models.Model):
         indexes = [
             models.Index(fields=["operator_id"], name="operator_id_idx"),
             models.Index(fields=["naics_code"], name="naics_code_idx"),
-            models.Index(fields=["boundary_map"], name="boundary_map_idx"),
-            models.Index(fields=["verified_by"], name="verified_by_idx"),
+            models.Index(fields=["verified_by"], name="operation_verified_by_idx"),
             ]
 
 
-class NacisCode(models.Model):
-        naics_code = models.CharField(max_length=1000, db_comment="")
-        ciip_sector = models.CharField(max_length=1000, db_comment="")
-        naics_description = models.CharField(max_length=1000, db_comment="")
-    class Meta:
-        db_table_comment = "Naics codes"
-        # this may be helpful: https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.FileField.upload_to
 
-class Document(models.Model):
-        file = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_comment="")
-        description = models.CharField(max_length=1000, db_comment="")
-        file_name = models.CharField(max_length=1000, db_comment="")
-        file_type = models.CharField(max_length=1000, db_comment="")
-        file_size = models.CharField(max_length=1000, db_comment="")
-        class Meta:
-            db_table_comment = "Documents"
+
