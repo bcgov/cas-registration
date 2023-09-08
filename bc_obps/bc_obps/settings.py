8a533f0d4945d0e4d324547f970c1be3edf0b254
Brianna Cerkiewicz <brianna@button.is>
2023-09-08 08:55:56 -0700

    chore: apply suggestions from code review

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 # Application definition
 
 INSTALLED_APPS = [
-    'registration.apps.RegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration.apps.RegistrationConfig',
 ]
 
 MIDDLEWARE = [
--- bc_obps/registration/models.py
+++ bc_obps/registration/models.py
@@ -31,28 +31,35 @@ class User(UserAndContactCommonInfo):
     user_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, db_comment="")
     business_guid  = models.UUIDField(default=uuid.uuid4, db_comment="")
     position_title = models.CharField(max_length=1000, db_comment="")
-    documents  = models.ManyToManyField(Document,blank=True, null=True)
+    documents  = models.ManyToManyField(Document,blank=True, null=True, related_name="user_documents")
     class Meta:
         db_table_comment = "App users"
+        constraints = [
+            models.UniqueConstraint(fields=['user_guid', 'business_guid'], name='uuid_user_and_business_constraint')
+        ]
 
 class Contact(UserAndContactCommonInfo):
     is_operational_representative = models.BooleanField(db_comment="")
     verified_at = models.DateTimeField(db_comment="",blank=True, null=True)
-    verified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_comment="",blank=True, null=True)
+    verified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_comment="",blank=True, null=True, related_name='contacts')
     class Meta:
         db_table_comment = "Contacts (people who don't use the app, e.g. authorized signing officers)"
         indexes = [
             models.Index(fields=["verified_by"], name="contact_verified_by_idx"),
+            ]
+        constraints = [
+            models.UniqueConstraint(fields=['email'], name='contact_email_constraint')
             ]
 
 class Operator (models.Model):
     # class Statuses(models.TextChoices):
     #     PENDING = 'pending', 'Pending'
     #     APPROVED = 'approved', 'Approved'
-    #     DENIED = 'rejected', 'Rejected'
+    #     REJECTED = 'rejected', 'Rejected'
     legal_name = models.CharField(max_length=1000, db_comment="")  
     trade_name = models.CharField(max_length=1000, db_comment="")
-    business_number = models.CharField(max_length=1000, db_comment="")
+    cra_business_number = models.CharField(max_length=1000, db_comment="")
+    bc_corporate_registry_number = models.CharField(max_length=1000, db_comment="")
     business_structure = models.CharField(max_length=1000, db_comment="") 
     mailing_address = models.CharField(max_length=1000, db_comment="") 
     bceid = models.CharField(max_length=1000, db_comment="") 
@@ -61,9 +68,9 @@ class Operator (models.Model):
     compliance_obligee = models.ForeignKey('self', on_delete=models.CASCADE, related_name='obligee', db_comment="")
     date_aso_became_responsible_for_operator = models.DateTimeField(db_comment="")
     # status = models.CharField(max_length=50, choices=Statuses.choices, default=Statuses.PENDING, db_comment="")
-    documents  = models.ManyToManyField(Document,blank=True, null=True)
-    contacts  = models.ManyToManyField(Contact)
-    operators = models.ManyToManyField(User, through="UserOperator")
+    documents  = models.ManyToManyField(Document,blank=True, null=True, related_name='operator_documents')
+    contacts  = models.ManyToManyField(Contact, related_name='operator_contacts')
+    operators = models.ManyToManyField(User, through="UserOperator", related_name='operator_users')
     class Meta:
         db_table_comment = "Operators (also called organizations)"
         # don't need indexes if we end up using `unique`
@@ -82,13 +89,15 @@ class UserOperator(models.Model):
         APPROVED = 'approved', 'Approved'
         REJECTED = 'rejected', 'Rejected'
 
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
-    operators = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_operators')
+    operators = models.ForeignKey(Operator, on_delete=models.CASCADE, db_comment='',related_name='user_operators')
     role = models.CharField(max_length=1000, db_comment="")
     status = models.CharField(max_length=1000, choices=Statuses.choices, default=Statuses.PENDING, db_comment="")
-    user_is_aso = models.BooleanField
-    user_is_third_party = models.BooleanField
-    proof_of_authority = models.ForeignKey(Document, on_delete=models.DO_NOTHING, db_comment="")
+    user_is_aso = models.BooleanField(db_comment="")
+    aso_is_owner_or_operator = models.BooleanField(db_comment="")
+    user_is_third_party = models.BooleanField(db_comment="")
+    proof_of_authority = models.ForeignKey(Document, on_delete=models.DO_NOTHING, db_comment="", related_name='user_operators_proof_of_authority')
+    signed_statuatory_declaration = models.ForeignKey(Document, on_delete=models.DO_NOTHING, db_comment="", related_name='user_operators_signed_statuatory_declaration')
     class Meta:
         db_table_comment = "Through table to connect Users and Operators and track access requests"
         indexes = [
@@ -96,12 +105,11 @@ class UserOperator(models.Model):
             models.Index(fields=["operators"], name="user_operator_operator_id_idx")]
     
 class Operation(models.Model):
-    operator_id = models.ForeignKey(Operator, on_delete=models.CASCADE, db_comment="")
+    operator_id = models.ForeignKey(Operator, on_delete=models.CASCADE, db_comment="", related_name='operations')
     name = models.CharField(max_length=1000, db_comment="")
     operation_type = models.CharField(max_length=1000, db_comment="")
-    naics_code = models.ForeignKey(NaicsCode, on_delete=models.CASCADE, db_comment="")
-    first_year_of_commercial_operation = models.IntegerField(db_comment="")
-    description_of_operation_criteria = models.CharField(max_length=1000, db_comment="")
+    naics_code = models.ForeignKey(NaicsCode, on_delete=models.CASCADE, db_comment="", related_name='operations')
+    eligible_commercial_product_name = models.CharField(max_length=1000, db_comment="")
     permit_id = models.CharField(max_length=1000, db_comment="")
     npr_id = models.CharField(max_length=1000, db_comment="",blank=True, null=True)
     ghfrp_id = models.CharField(max_length=1000, db_comment="",blank=True, null=True)
@@ -111,12 +119,12 @@ class Operation(models.Model):
     legal_land_description = models.CharField(max_length=1000, db_comment="")
     nearest_municipality = models.CharField(max_length=1000, db_comment="")
     operator_percent_of_ownership = models.DecimalField(decimal_places=10, max_digits=10,db_comment="")
-    registered_for_obps = models.BooleanField
+    registered_for_obps = models.BooleanField(db_comment="")
     verified_at = models.DateTimeField(db_comment="",blank=True, null=True)
-    verified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_comment="",blank=True, null=True)
+    verified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_comment="",blank=True, null=True, related_name='operations')
     estimated_emissions = models.DecimalField(decimal_places=10, max_digits=10,db_comment="")
-    documents  = models.ManyToManyField(Document,blank=True, null=True)
-    contacts  = models.ManyToManyField(Contact)
+    documents  = models.ManyToManyField(Document,blank=True, null=True, related_name='operation_documents')
+    contacts  = models.ManyToManyField(Contact, related_name='operation_contacts')
     class Meta:
         db_table_comment = "Operations (also called facilities)"
         indexes = [
