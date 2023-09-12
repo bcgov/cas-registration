184448a7b04951ca0dc350a0921eeffb20a1466e
Brianna Cerkiewicz <brianna@button.is>
2023-09-12 13:21:51 -0700

    test: create smoke test

--- client/e2e/example.spec.ts
+++ client/e2e/example.spec.ts
@@ -1,18 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("backend is running", async ({ page }) => {
+  await page.goto("http://127.0.0.1:8000/api/docs");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.getByText(/Ninja/i)).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("frontend is running", async ({ page }) => {
+  await page.goto("http://localhost:3000");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByText(/CAS registration app/i)).toBeVisible();
 });
