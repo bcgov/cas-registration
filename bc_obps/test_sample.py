0348079f588035a16429a4a5c177131efad8a465
SeSo <sobhani.sepehr@gmail.com>
2023-09-14 15:38:31 -0700

    chore: disable pylint rules for now

--- bc_obps/test_sample.py
+++ bc_obps/test_sample.py
@@ -1,3 +1,9 @@
+# Missing module docstring (missing-module-docstring)
+# Missing function or method docstring (missing-function-docstring)
+# Argument name "x" doesn't conform to snake_case naming style (invalid-name)
+
+
+# pylint: skip-file
 def func(x):
     return x + 1
 
