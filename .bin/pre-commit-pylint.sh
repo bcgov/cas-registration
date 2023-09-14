0abb2b66cd1d2b8dc4b0dad6de46c7fad0155230
SeSo <sobhani.sepehr@gmail.com>
2023-09-14 16:14:53 -0700

    chore: debug pylint

--- .bin/pre-commit-pylint.sh
+++ .bin/pre-commit-pylint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd ../bc_obps
 
-# Activate the Poetry virtual environment
-poetry shell
+# Install and activate the Poetry virtual environment
+make install_poetry
 
 # Run pylint on the bc_obps folder
 poetry run pylint --rcfile=.pylintrc *.py
