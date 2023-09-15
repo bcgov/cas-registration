07cca58003628f0e9b1f1a279a1ab5eb1c286e7e
SeSo <sobhani.sepehr@gmail.com>
2023-09-15 14:17:49 -0700

    chore: debug CI

--- .bin/pre-commit-pylint.sh
+++ .bin/pre-commit-pylint.sh
@@ -3,14 +3,17 @@ set -euxo pipefail
 
 pushd bc_obps || exit 1
 
-# # Get the Poetry virtual environment path and save it in a variable
+# Get the Poetry virtual environment path and save it in a variable
 # VENV_PATH=$(poetry env info --path)
 curl -sSL https://install.python-poetry.org | python3 -
 which poetry
 
 # Activate the virtual environment
+# Creating virtualenv registration-ucQVSwfn-py3.10 in /home/runner/.cache/pypoetry/virtualenvs
 # source /home/runner/.cache/pypoetry/virtualenvs/bc-obps-3Z4QX3ZS-py3.8/bin/activate
 poetry install
+
+poetry run pylint --version
 
 # Run pylint on the bc_obps folder
 poetry run pylint --rcfile=.pylintrc *.py
