d770371e649083a7829ccee064dc0f9e70490996
SeSo <sobhani.sepehr@gmail.com>
2023-09-15 13:31:01 -0700

    chore: debug CI

--- .bin/pre-commit-pylint.sh
+++ .bin/pre-commit-pylint.sh
@@ -4,14 +4,15 @@ set -euxo pipefail
 pushd bc_obps || exit 1
 
 pwd
-# export VIRTUAL_ENV_DISABLE_PROMPT=1
-# poetry shell
+
+echo $PATH
 
 # # Get the Poetry virtual environment path and save it in a variable
 # VENV_PATH=$(poetry env info --path)
+poetry --version
 
 # # Activate the virtual environment
 # source "$VENV_PATH/bin/activate"
 
 # Run pylint on the bc_obps folder
-poetry run pylint --rcfile=.pylintrc *.py
+# poetry run pylint --rcfile=.pylintrc *.py
