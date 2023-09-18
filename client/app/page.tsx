9a4987ff58ac77249cde0c1305e47dc2b5298e11
shon-button <shon@button.is>
2023-09-18 10:10:28 -0400

    ✨ Add Next.js 13 page and API routing, layout, styles, components, and Redux for state management
    - Added client/app folder for page routing
    - Added client/app/api folder for API routing (e.g., /api/hello)
    - Introduced client/app/layout.tsx for the application-level layout
    - Included client/app/components as an example
    - Included client/app/styles as an example
    - Created the client/redux folder for Redux and Redux Toolkit files
    - Added application state demo pages:
      - Setting count state: client/app/counter/page.tsx (http://localhost:3000/)
      - Displaying count state: client/app/display/page.tsx (http://localhost:3000/)

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+/* Components */
+import { Counter } from "./components/Counter/Counter";
+
+export default function Page() {
+  return (
+    <>
+      <h1>👉 Demo SSR Page with a...</h1>
+      <Counter />
+    </>
+  );
+}
