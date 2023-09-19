5138075a15b0148f6bf1f89d8f291358a58820aa
Brianna Cerkiewicz <brianna@button.is>
2023-09-19 13:27:07 -0700

    chore: create operation endpoint

--- bc_obps/registration/api.py
+++ bc_obps/registration/api.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from datetime import date
 from typing import List
-from ninja import NinjaAPI, Schema
+from ninja import Router
 from django.shortcuts import get_object_or_404
 from .models import Operation
 from ninja.orm import create_schema
 
-api = NinjaAPI()
+router = Router()
 
 
 # Operation schemas and endpoints
@@ -16,25 +16,25 @@ api = NinjaAPI()
 OperationSchema = create_schema(Operation)
 
 
-@api.get("/operations", response=List[OperationSchema])
+@router.get("/operations", response=List[OperationSchema])
 def list_operations(request):
     qs = Operation.objects.all()
     return qs
 
 
-@api.get("/operations/{operation_id}", response=OperationSchema)
+@router.get("/operations/{operation_id}", response=OperationSchema)
 def get_operation(request, operation_id: int):
     operation = get_object_or_404(operation, id=operation_id)
     return operation
 
 
-@api.post("/operations")
+@router.post("/operations")
 def create_operation(request, payload: OperationSchema):
     operation = Operation.objects.create(**payload.dict())
     return {"id": operation.id}
 
 
-@api.put("/operations/{operation_id}")
+@router.put("/operations/{operation_id}")
 def update_operation(request, operation_id: int, payload: OperationSchema):
     operation = get_object_or_404(operation, id=operation_id)
     for attr, value in payload.dict().items():
