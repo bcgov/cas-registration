002327c0521ba927ba79290f3d6ad092be245ab0
Brianna Cerkiewicz <brianna@button.is>
2023-09-19 13:14:06 -0700

    chore: create operation endpoint

--- bc_obps/bc_obps/urls.py
+++ bc_obps/bc_obps/urls.py
@@ -15,18 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from ninja import NinjaAPI
-
-api = NinjaAPI()
-
-
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
+from django.urls import path, include
+from .api import api
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/", api.urls),
+    path("registration", include("registration.urls")),
 ]
