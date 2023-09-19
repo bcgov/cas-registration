9ac91bbddee7922591ab05d8356f8ea5d7a355d2
Brianna Cerkiewicz <brianna@button.is>
2023-09-19 13:27:07 -0700

    chore: create operation endpoint

--- bc_obps/bc_obps/urls.py
+++ bc_obps/bc_obps/urls.py
@@ -19,8 +19,4 @@ from django.urls import path, include
 from .api import api
 
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", api.urls),
-    path("registration", include("registration.urls")),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", api.urls)]
