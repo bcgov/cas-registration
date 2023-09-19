96f3bc8dd5933bb59818f279c349fa411c7366f9
Brianna Cerkiewicz <brianna@button.is>
2023-09-19 13:27:07 -0700

    chore: create operation endpoint

--- bc_obps/bc_obps/api.py
+++ bc_obps/bc_obps/api.py
@@ -1,6 +1,9 @@
 from ninja import NinjaAPI
+from registration.api import router as registration_router
 
 api = NinjaAPI()
+
+api.add_router("/registration/", registration_router)
 
 
 @api.get("/add")
