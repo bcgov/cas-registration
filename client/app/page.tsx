194ecdb694b22149848519af09f8489cce79e1b8
Brianna Cerkiewicz <brianna@button.is>
2023-09-20 08:25:08 -0700

    chore: add link to operations page on homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,15 @@
 import Login from "@/components/auth/Login";
+/* Components */
+import Link from "next/link";
 
 export default async function Page() {
   return (
     <>
       <Login />
+      <h1>Links to app pages</h1>
+      <ul>
+        <Link href="/operations">Operations</Link>
+      </ul>
     </>
   );
 }
