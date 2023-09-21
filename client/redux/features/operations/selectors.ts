7bafa12c63886ece6f253824bd77fa8a8e597be1
Brianna Cerkiewicz <brianna@button.is>
2023-09-21 09:07:55 -0700

    chore: learning redux

--- client/redux/features/operations/selectors.ts
+++ client/redux/features/operations/selectors.ts
@@ -1,7 +1,10 @@
 import type { ReduxState } from "@/redux";
 import type { RootState } from "@/redux/index";
 
-// 🎯 Selector fucntion, allowing us to retrieve a value from the Redux state.
+// 🎯 Selector function, allowing us to retrieve a value from the Redux state.
 // Selectors can also be defined inline where they're used, instead of within the slice file.
 // For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectOperations = (state: RootState) => state.operations.value;
+export const selectOperations = (state: ReduxState) => {
+  console.log("state", state);
+  return state.operations;
+};
