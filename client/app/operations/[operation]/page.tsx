aced6adb6b770374b51903dde477d81936feb850
Brianna Cerkiewicz <brianna@button.is>
2023-09-22 15:28:59 -0700

    chore: set up edit operations

--- client/app/operations/[operation]/page.tsx
+++ client/app/operations/[operation]/page.tsx
@@ -1,29 +1,40 @@
 "use client";
 
 import { operationSchema, operationUiSchema } from "@/jsonSchema/operations";
-import { useGetOperationQuery } from "@/redux";
-import Form from "@rjsf/core";
+import { useGetOperationQuery, useEditOperationMutation } from "@/redux";
+import { Form } from "@rjsf/mui";
+
 import validator from "@rjsf/validator-ajv8";
 
 export default function Page({ params }: { params: { operation: number } }) {
-  console.log("params.operation", params.operation);
-  const { data, isLoading, error } = useGetOperationQuery(params.operation);
+  const {
+    data: operation,
+    isLoading: isLoadingOperation,
+    error,
+  } = useGetOperationQuery(params.operation);
+  const [updateOperation, { isLoading: isLoadingUpdate }] =
+    useEditOperationMutation();
 
-  if (!data) {
+  const operationUpdateHandler = async (data) => {
+    await updateOperation(data.formData);
+  };
+  if (!operation) {
     return (
       <section>
         <h2>Operation not found!</h2>
       </section>
     );
   }
+  console.log("operation", operation);
 
   return (
     <Form
       schema={operationSchema}
       validator={validator}
       onSubmit={operationUpdateHandler}
-      disabled={isLoading}
+      disabled={isLoadingOperation}
       uiSchema={operationUiSchema}
+      formData={operation}
     ></Form>
   );
 }
--- client/jsonSchema/operations.ts
+++ client/jsonSchema/operations.ts
@@ -4,10 +4,10 @@ export const operationSchema: RJSFSchema = {
   type: "object",
   required: [
     "id",
-    "operator_id_id",
+    "operator",
     "name",
     "operation_type",
-    "naics_code_id",
+    "naics_code",
     "eligible_commercial_product_name",
     "permit_id",
     "npr_id",
@@ -24,10 +24,10 @@ export const operationSchema: RJSFSchema = {
   ],
   properties: {
     id: { type: "number", title: "id" },
-    operator_id_id: { type: "number", title: "operator_id_id" },
+    operator: { type: "number", title: "operator_id" },
     name: { type: "string", title: "name" },
     operation_type: { type: "string", title: "operation_type" },
-    naics_code_id: { type: "number", title: "naics_code_id" },
+    naics_code: { type: "string", title: "naics_code" },
     eligible_commercial_product_name: {
       type: "string",
       title: "eligible_commercial_product_name",
@@ -37,25 +37,26 @@ export const operationSchema: RJSFSchema = {
     ghfrp_id: { type: "string", title: "ghfrp_id" },
     bcghrp_id: { type: "string", title: "bcghrp_id" },
     petrinex_id: { type: "string", title: "petrinex_id" },
-    latitude: { type: "number", title: "latitude" },
-    longitude: { type: "number", title: "longitude" },
+    // these come out of django as string, will need some transformation probably
+    latitude: { type: "string", title: "latitude" },
+    longitude: { type: "string", title: "longitude" },
     legal_land_description: { type: "string", title: "legal_land_description" },
     nearest_municipality: { type: "string", title: "nearest_municipality" },
     operator_percent_of_ownership: {
-      type: "number",
+      type: "string",
       title: "operator_percent_of_ownership",
     },
     registered_for_obps: { type: "boolean", title: "registered_for_obps" },
     // verified_at: { type: "string", title: "verified_at", format: "date" },
     // verified_by_id: { type: "string", title: "verified_by_id" },
-    estimated_emissions: { type: "number", title: "estimated_emissions" },
-    documents: { type: "string", title: "documents" },
+    estimated_emissions: { type: "string", title: "estimated_emissions" },
+    // documents: { type: "string", title: "documents" },
     // contacts: { type: "string", title: "contacts" },
   },
 };
 
 export const operationUiSchema = {
-  // operator_id_id: {
+  // operator_id: {
   //   "ui:widget": "hidden",
   // },
 };
