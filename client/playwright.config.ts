956e9c3bde02d33fc0096de3c9f73eafb1c503c0
SeSo <sobhani.sepehr@gmail.com>
2023-09-21 17:01:33 -0700

    chore: add both backend and frontend servers to playwright

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -69,9 +69,15 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: [
+    {
+      command: "yarn dev",
+      url: "http://localhost:3000",
+    },
+    {
+      command: "make run",
+      cwd: "../bc_obps",
+      url: "http://127.0.0.1:8000/api/docs",
+    },
+  ],
 });
