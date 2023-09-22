b7f7483039bde367a76da4581d8eaed4f8ffc101
Brianna Cerkiewicz <brianna@button.is>
2023-09-22 15:21:26 -0700

    chore: set up edit operations

--- bc_obps/registration/api.py
+++ bc_obps/registration/api.py
@@ -4,7 +4,7 @@ from datetime import date
 from typing import List
 from ninja import Router
 from django.shortcuts import get_object_or_404
-from .models import Operation
+from .models import Operation, Operator, NaicsCode
 from ninja.orm import create_schema
 
 router = Router()
@@ -14,6 +14,8 @@ router = Router()
 
 # brianna-this is inadvisable, put desired fields on it later
 OperationSchema = create_schema(Operation)
+
+# brianna this is how to add contacts: https://django-ninja.rest-framework.com/guides/response/
 
 
 @router.get("/operations", response=List[OperationSchema])
@@ -37,7 +39,26 @@ def create_operation(request, payload: OperationSchema):
 @router.put("/operations/{operation_id}")
 def update_operation(request, operation_id: int, payload: OperationSchema):
     operation = get_object_or_404(Operation, id=operation_id)
+    breakpoint()
+    if "operator" in payload.dict():
+        operator = payload.dict()["operator"]
+        op = get_object_or_404(Operator, id=operator)
+        # Assign the Operator instance to the operation
+        operation.operator = op
+    if "naics_code" in payload.dict():
+        naics_code = payload.dict()["naics_code"]
+        nc = get_object_or_404(NaicsCode, id=naics_code)
+        # Assign the naics_code instance to the operation
+        operation.naics_code = nc
+    # Update other attributes as needed
     for attr, value in payload.dict().items():
-        setattr(operation, attr, value)
+        # brianna will need to fix this for docs and contacts
+        if (
+            attr != "operator"
+            and attr != "naics_code"
+            and attr != "documents"
+            and attr != "contacts"
+        ):
+            setattr(operation, attr, value)
     operation.save()
     return {"success": True}
