0fcba720aa3ae2af90c13e0695ddd38d1c7f9bcd
Brianna Cerkiewicz <brianna@button.is>
2023-09-22 08:49:37 -0700

    chore: remove unused redux code

--- client/redux/features/operations/fetchOperations.ts
+++
@@ -1,9 +0,0 @@
-export const retrieveOperations = async (): Promise<{ data: any }> => {
-  const response = await fetch("http://localhost:8000/api/operations", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-  const result = await response.json();
-
-  return result;
-};
