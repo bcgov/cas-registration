eed7b2497238b3463495bf8cc2c16c2bedf1750e
Brianna Cerkiewicz <brianna@button.is>
2023-09-22 15:46:33 -0700

    chore: set up edit operations

--- client/jsonSchema/operations.ts
+++ client/jsonSchema/operations.ts
@@ -10,10 +10,7 @@ export const operationSchema: RJSFSchema = {
     "naics_code",
     "eligible_commercial_product_name",
     "permit_id",
-    "npr_id",
-    "ghfrp_id",
-    "bcghrp_id",
-    "petrinex_id",
+
     "latitude",
     "longitude",
     "legal_land_description",
@@ -23,11 +20,11 @@ export const operationSchema: RJSFSchema = {
     "estimated_emissions",
   ],
   properties: {
-    id: { type: "number", title: "id" },
-    operator: { type: "number", title: "operator_id" },
+    // id: { type: "number", title: "id" },
+    // operator: { type: "number", title: "operator_id" },
     name: { type: "string", title: "name" },
     operation_type: { type: "string", title: "operation_type" },
-    naics_code: { type: "string", title: "naics_code" },
+    naics_code: { type: "number", title: "naics_code" },
     eligible_commercial_product_name: {
       type: "string",
       title: "eligible_commercial_product_name",
@@ -38,25 +35,29 @@ export const operationSchema: RJSFSchema = {
     bcghrp_id: { type: "string", title: "bcghrp_id" },
     petrinex_id: { type: "string", title: "petrinex_id" },
     // these come out of django as string, will need some transformation probably
-    latitude: { type: "string", title: "latitude" },
-    longitude: { type: "string", title: "longitude" },
+    latitude: { type: "number", title: "latitude" },
+    longitude: { type: "number", title: "longitude" },
     legal_land_description: { type: "string", title: "legal_land_description" },
     nearest_municipality: { type: "string", title: "nearest_municipality" },
     operator_percent_of_ownership: {
-      type: "string",
+      type: "number",
       title: "operator_percent_of_ownership",
     },
-    registered_for_obps: { type: "boolean", title: "registered_for_obps" },
-    // verified_at: { type: "string", title: "verified_at", format: "date" },
-    // verified_by_id: { type: "string", title: "verified_by_id" },
-    estimated_emissions: { type: "string", title: "estimated_emissions" },
+    // registered_for_obps: { type: "boolean", title: "registered_for_obps" },
+    estimated_emissions: { type: "number", title: "estimated_emissions" },
     // documents: { type: "string", title: "documents" },
     // contacts: { type: "string", title: "contacts" },
   },
 };
 
 export const operationUiSchema = {
-  // operator_id: {
-  //   "ui:widget": "hidden",
-  // },
+  id: {
+    "ui:widget": "hidden",
+  },
+  operator: {
+    "ui:widget": "hidden",
+  },
+  registered_for_obps: {
+    "ui:widget": "hidden",
+  },
 };
