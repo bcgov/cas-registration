dc8c8a0347dedd57f199358de99f3d6d5835a25d
Brianna Cerkiewicz <brianna@button.is>
2023-09-25 14:29:47 -0700

    chore: get everything working with RTK

--- client/app/operations/create/page.tsx
+++ client/app/operations/create/page.tsx
@@ -4,24 +4,53 @@ import { useAddNewOperationMutation } from "@/redux";
 import validator from "@rjsf/validator-ajv8";
 import Form from "@rjsf/mui";
 import { operationSchema, operationUiSchema } from "@/jsonSchema/operations";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Page() {
   const [addNewOperation, { isLoading }] = useAddNewOperationMutation();
 
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   const operationSubmitHandler = async (data) => {
+    const {
+      naics_code,
+      latitude,
+      longitude,
+      operator_percent_of_ownership,
+      estimated_emissions,
+      operator,
+    } = data.formData;
     if (!isLoading) {
       try {
-        const updatedFormData = {
+        const tranformedFormData = {
           ...data.formData,
+          registered_for_obps: false,
+          operator_id: operator,
+          naics_code_id: naics_code.toString(),
+          latitude: latitude.toString(),
+          longitude: longitude.toString(),
+          operator_percent_of_ownership:
+            operator_percent_of_ownership.toString(),
+          estimated_emissions: estimated_emissions.toString(),
+          documents: [],
+          contacts: [],
         };
-        await addNewOperation(updatedFormData).unwrap();
+        await addNewOperation(tranformedFormData).then(() =>
+          setShowSuccessMessage(true)
+        );
       } catch (err) {
         console.error("Failed to save the operation: ", err);
       }
     }
   };
 
-  return (
+  return showSuccessMessage ? (
+    <>
+      <div>You have successfully submitted the operation form</div>
+      <Link href="/operations">Return to operations list</Link>
+    </>
+  ) : (
     <Form
       schema={operationSchema}
       validator={validator}
