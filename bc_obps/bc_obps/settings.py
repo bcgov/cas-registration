f6e19b4761756ca0710db29e8bf207a8a1a3b058
SeSo <sobhani.sepehr@gmail.com>
2023-09-26 14:21:17 -0700

    chore: bring back changes due to debugging

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
