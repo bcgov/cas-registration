18b698cdfe5401a206c94f643f67237319f67860
Brianna Cerkiewicz <brianna@button.is>
2023-09-26 08:34:15 -0700

    chore: experiment with client components

--- client/app/operations/create/page.tsx
+++ client/app/operations/create/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import {
   djangoApiSlice,
   useAddNewOperationMutation,
@@ -13,61 +12,8 @@ import Link from "next/link";
 import { createOperationSchema } from "../[operation]/page";
 import { createSelector } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
+import OperationsForm from "@/app/components/Form/operationsForm";
 
 export default function Page() {
-  const [addNewOperation, { isLoading }] = useAddNewOperationMutation();
-
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-
-  const { data: naics_codes, isLoading: isLoadingNaicsCodes } =
-    useGetNaicsCodesQuery(null);
-  const localSchema = createOperationSchema(operationSchema, naics_codes);
-
-  const operationSubmitHandler = async (data) => {
-    const {
-      naics_code,
-      latitude,
-      longitude,
-      operator_percent_of_ownership,
-      estimated_emissions,
-      operator,
-    } = data.formData;
-    if (!isLoading) {
-      try {
-        const tranformedFormData = {
-          ...data.formData,
-          registered_for_obps: false,
-          operator_id: operator,
-          naics_code_id: naics_code.toString(),
-          latitude: latitude.toString(),
-          longitude: longitude.toString(),
-          operator_percent_of_ownership:
-            operator_percent_of_ownership.toString(),
-          estimated_emissions: estimated_emissions.toString(),
-          documents: [],
-          contacts: [],
-        };
-        await addNewOperation(tranformedFormData).then(() =>
-          setShowSuccessMessage(true)
-        );
-      } catch (err) {
-        console.error("Failed to save the operation: ", err);
-      }
-    }
-  };
-
-  return showSuccessMessage ? (
-    <>
-      <div>You have successfully submitted the operation form</div>
-      <Link href="/operations">Return to operations list</Link>
-    </>
-  ) : (
-    <Form
-      schema={localSchema}
-      validator={validator}
-      onSubmit={operationSubmitHandler}
-      disabled={isLoading}
-      uiSchema={operationUiSchema}
-    ></Form>
-  );
+  return <OperationsForm />;
 }
--- client/app/operations/page.tsx
+++ client/app/operations/page.tsx
@@ -1,10 +1,14 @@
-"use client";
-
 import { GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import DataGrid from "../components/DataGrid/DataGrid";
 import Link from "next/link";
 import { Button } from "@mui/material";
-import { useGetOperationsQuery } from "@/redux/index";
+import { Suspense } from "react";
+
+export async function getOperations() {
+  return (
+    await fetch("http://localhost:8000/api/registration/operations")
+  ).json();
+}
 
 export const columns: GridColDef[] = [
   { field: "operation_id", headerName: "Operation ID", width: 150 },
@@ -18,34 +22,25 @@ export const columns: GridColDef[] = [
     headerName: "Action",
     sortable: false,
     width: 200,
-    renderCell: (params) => {
-      return (
-        <Link href={`operations/${params.row.id}`}>
-          <Button variant="contained">
-            {params.row.status === "Not Registered"
-              ? "Start Registration"
-              : "View Details"}
-          </Button>
-        </Link>
-      );
-    },
+    // brianna figure out
+    // renderCell: (params) => {
+    //   return (
+    //     <Link href={`operations/${params.row.id}`}>
+    //       <Button variant="contained">
+    //         {params.row.status === "Not Registered"
+    //           ? "Start Registration"
+    //           : "View Details"}
+    //       </Button>
+    //     </Link>
+    //   );
+    // },
   },
 ];
 
-export default function Page() {
-  const { data, isLoading, error } = useGetOperationsQuery(null);
+export default async function Page() {
+  const operations = await getOperations();
 
-  // Check if data is currently loading
-  if (isLoading) {
-    return <div>🚀 Loading data... </div>;
-  }
-
-  // Check if an error occurred
-  if (error) {
-    return <div>Something went wrong, Please retry after some time</div>;
-  }
-
-  if (!data) {
+  if (!operations) {
     return (
       <div>
         No operations data in database (did you forget to run `make
@@ -54,7 +49,7 @@ export default function Page() {
     );
   }
 
-  const rows: GridRowsProp = data.map(
+  const rows: GridRowsProp = operations.map(
     ({
       id,
       registration_year,
@@ -78,10 +73,12 @@ export default function Page() {
   return (
     <>
       <h1>Operations List</h1>
-      <Link href="/operations/create">
-        <Button variant="contained">Add Operation</Button>
-      </Link>
-      <DataGrid columns={columns} rows={rows} />
+      <Suspense fallback={<p>🚀 Loading data...</p>}>
+        <Link href="/operations/create">
+          <Button variant="contained">Add Operation</Button>
+        </Link>
+        <DataGrid columns={columns} rows={rows} />
+      </Suspense>
     </>
   );
 }
