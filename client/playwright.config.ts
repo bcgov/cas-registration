4427103ff392f1e45686eaa990501a950f53717a
SeSo <sobhani.sepehr@gmail.com>
2023-09-27 12:00:22 -0700

    docs: add e2e test docs

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: [
   //   {
-  //     command: "yarn start",
+  //     command: "yarn build && yarn start",
   //     url: "http://localhost:3000",
   //     reuseExistingServer: true,
   //   },
