33df4f37040bd316c35482235a0d400c094ed9f8
Brianna Cerkiewicz <brianna@button.is>
2023-09-27 14:28:36 -0700

    chore: remove redux files

--- client/redux/features/auth/selectors.ts
+++
@@ -1,5 +0,0 @@
-import type { ReduxState } from "@/redux";
-
-// 🎯 Selector function, allowing us to retrieve a value from the Redux state.
-export const selectUser = (state: ReduxState) => state.auth.user;
-export const selectUserToken = (state: ReduxState) => state.auth.token;
