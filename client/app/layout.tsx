8a2a9bcd7a4ae8a681b7441bfa5e5bf3afcc4a88
Brianna Cerkiewicz <brianna@button.is>
2023-09-28 11:46:29 -0700

    chore: post-rebase cleanup

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,3 @@
-import styles from "@/styles/layout.module.css";
 import "@/styles/globals.css";
 import ThemeRegistry from "@/theme/ThemeRegistry";
 
@@ -6,7 +5,6 @@ import ThemeRegistry from "@/theme/ThemeRegistry";
 It allows us to define a common structure and appearance for a group of pages, reducing redundancy and promoting code reusability*/
 export default function RootLayout(props: React.PropsWithChildren) {
   return (
-    // Wrap the content in the ReduxProvider to provide access to Redux state
     <html lang="en">
       <ThemeRegistry>
         <body>
