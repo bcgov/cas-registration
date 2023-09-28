d2be0878f74bac6e2d4b295331b0696f7cbc0e08
Dylan Leard <dylan@button.is>
2023-09-28 14:41:20 -0700

    chore: move middleware into middleware directory

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'registration.kubernetesMiddleware.KubernetesHealthCheckMiddleware',
+    'registration.middleware.kubernetes_middleware.KubernetesHealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
