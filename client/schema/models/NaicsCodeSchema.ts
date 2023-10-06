80f6a9bb5ca418ab1298ca53a99940b2b2579de9
SeSo <sobhani.sepehr@gmail.com>
2023-10-06 08:36:26 -0700

    chore: experiment with openapi-typescript-codegen

---
+++ client/schema/models/NaicsCodeSchema.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type NaicsCodeSchema = {
+  id: number;
+  naics_code: string;
+  ciip_sector: string;
+  naics_description: string;
+};
