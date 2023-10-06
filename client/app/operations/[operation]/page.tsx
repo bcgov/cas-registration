0ff437148eb34744f17ee6f492fff58646043318
shon-button <shon@button.is>
2023-10-06 16:41:21 -0400

    Remove async on operations page routes

--- client/app/operations/[operation]/page.tsx
+++ client/app/operations/[operation]/page.tsx
@@ -2,11 +2,7 @@ import { Suspense } from "react";
 import Operation from "@/components/routes/operations/form/Operation";
 import Loading from "@/components/loading/SkeletonForm";
 
-export default async function Page({
-  params,
-}: {
-  params: { operation: number };
-}) {
+export default function Page({ params }: { params: { operation: number } }) {
   return (
     <>
       <h1>Edit an Operation</h1>
