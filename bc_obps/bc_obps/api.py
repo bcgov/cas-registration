93de1c18e007c36a994900bc629f32563bb8641e
Brianna Cerkiewicz <brianna@button.is>
2023-10-11 09:10:09 -0700

    chore: add ninja api exception handler

--- bc_obps/bc_obps/api.py
+++ bc_obps/bc_obps/api.py
@@ -1,7 +1,15 @@
 from ninja import NinjaAPI
 from registration.api import router as registration_router
+from ninja.errors import ValidationError
 
 api = NinjaAPI()
+
+
+@api.exception_handler(ValidationError)
+def custom_validation_errors(request, exc):
+    print(exc.errors)  # <--------------------- !!!!
+    return api.create_response(request, {"detail": exc.errors}, status=422)
+
 
 api.add_router("/registration/", registration_router)
 
