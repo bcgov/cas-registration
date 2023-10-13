0c32dba507bd69d891ce415377c375010261303b
Brianna Cerkiewicz <brianna@button.is>
2023-10-13 09:01:19 -0700

    chore: create erc schema with pre_migrate instead of makefile

--- bc_obps/registration/apps.py
+++ bc_obps/registration/apps.py
@@ -1,6 +1,16 @@
+from django.db.models.signals import pre_migrate
 from django.apps import AppConfig
+from django.db import connection
+
+
+def create_erc_schema(sender, app_config, **kwargs):
+    cursor = connection.cursor()
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS erc")
 
 
 class RegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'registration'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "registration"
+
+    def ready(self):
+        pre_migrate.connect(create_erc_schema, sender=self)
