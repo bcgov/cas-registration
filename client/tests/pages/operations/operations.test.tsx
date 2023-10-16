768d04b50f6464b6cc67b82d6037688e5c541ade
Brianna Cerkiewicz <brianna@button.is>
2023-10-16 11:06:42 -0700

    test: operations test

---
+++ client/tests/pages/operations/operations.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Page from "@/app/operations/page";
+
+describe("operations page", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("renders the Operations List page", () => {
+    render(<Page />);
+
+    // Check if the "Add Operation" button is present and has the correct text
+    expect(
+      screen.getByRole("button", {
+        name: /add operation/i,
+      }),
+    ).toBeVisible();
+
+    // Check if the data grid is present
+    expect(screen.getByRole("grid")).toBeVisible();
+  });
+});
