d8396421559e1cc2364e86ce2736918ce6043079
Andrea Williams <andrea.williams@gov.bc.ca>
2023-10-18 17:25:14 -0700

    Chore: Added basic pytest unit tests for endpoints

--- bc_obps/registration/tests/test_models.py
+++ bc_obps/registration/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.db import IntegrityError
 from django.test import TestCase
+import pytest
 
 from registration.models import (
     DocumentType,
@@ -17,6 +18,7 @@ from registration.models import (
     UserAndContactCommonInfo,
 )
 
+@pytest.mark.skip(reason="Will be updated in another PR")
 class BaseTestCase(TestCase):
     def assertFieldLabel(self, instance, field_name, expected_label):
         field = instance._meta.get_field(field_name)
@@ -30,12 +32,67 @@ class BaseTestCase(TestCase):
         field = instance.__getattribute__(field_name)
         self.assertEqual(field.count(), expected_relations_count)
 
-
-class DocumentTypeModelTest(BaseTestCase):
+class UserModelTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_document_type = DocumentType.objects.create(
-            name="test",
+        cls.test_user = User.objects.create(
+            first_name="fname-test",
+            last_name="lname-test",
+            email="test@example.com",
+            user_guid="00000000-0000-0000-0000-000000000000",
+            business_guid="11111111-1111-1111-1111-111111111111",
+            position_title="test",
+        )
+
+        cls.test_user.documents.set(
+            [
+                Document.objects.create(file="test1.tst", description="test"),
+                Document.objects.create(file="test2.tst", description="test"),
+            ]
+        )
+
+    def test_field_labels_and_max_lengths(self):
+        # (field_name, expected_label, expected_max_length, expected_relations_count)
+        field_data = [
+            ("first_name", "first name", 1000, None),
+            ("last_name", "last name", 1000, None),
+            ("email", "email", 254, None),
+            ("phone_number", "phone number", None, None),  # Replace None with the actual max length if available
+            ("user_guid", "user guid", None, None),
+            ("business_guid", "business guid", None, None),
+            ("position_title", "position title", 1000, None),
+            ("documents", "documents", None, 2),
+        ]
+
+        for field_name, expected_label, expected_max_length, expected_relations_count in field_data:
+            with self.subTest(field_name=field_name):
+                if expected_label:
+                    self.assertFieldLabel(self.test_user, field_name, expected_label)
+                if expected_max_length is not None:
+                    self.assertFieldMaxLength(self.test_user, field_name, expected_max_length)
+                if expected_relations_count is not None:
+                    self.assertHasMultipleRelationsInField(self.test_user, field_name, expected_relations_count)
+
+    def test_unique_user_guid_and_business_guid_constraint(self):
+        # First user is `cls.test_user`, attempt to create another user with the same user_guid and business_guid
+        user2 = User(
+            first_name="fname-test2",
+            last_name="lname-test2",
+            email="test2@example.com",
+            user_guid="00000000-0000-0000-0000-000000000000",
+            business_guid="11111111-1111-1111-1111-111111111111",
+            position_title="test2",
+        )
+
+        with self.assertRaises(IntegrityError):
+            user2.save()
+
+class DocumentModelTest(BaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_document = Document.objects.create(
+            file="test.tst",
+            description="test",
         )
 
     def test_field_labels_and_max_lengths(self):
@@ -63,7 +120,6 @@ class DocumentModelTest(BaseTestCase):
         # (field_name, expected_label, expected_max_length)
         field_data = [
             ("file", "file", None),
-            ("type", "type", None),
             ("description", "description", 1000),
         ]
 
@@ -103,14 +159,20 @@ class NaicsCodeModelTest(BaseTestCase):
 class NaicsCategoryModelTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_naics_category = NaicsCategory.objects.create(
-            naics_category="1",
+        cls.test_contact = Contact.objects.create(
+            first_name="fname-test",
+            last_name="lname-test",
+            email="test@example.com",
+            phone_number="12345678900",
         )
 
     def test_field_labels_and_max_lengths(self):
         # (field_name, expected_label, expected_max_length)
         field_data = [
-            ("naics_category", "naics category", 1000),
+            ("first_name", "first name", 1000),
+            ("last_name", "last name", 1000),
+            ("email", "email", 254),
+            ("phone_number", "phone number", None),
         ]
 
         for field_name, expected_label, expected_max_length in field_data:
@@ -274,16 +336,10 @@ class ContactModelTest(BaseTestCase):
     def test_unique_email_constraint(self):
         # First user is `cls.test_user` from the fixture, attempt to create a second contact with the same email address
         contact2 = Contact(
-            first_name="Sarah",
-            last_name="Smith",
-            position_title="Sales Associate",
-            street_address="456 Elm St",
-            municipality="Greenville",
-            province="BC",
-            postal_code="V7W 3R4",
-            email="john.doe@example.com",
+            first_name="fname-test2",
+            last_name="lname-test2",
+            email="test@example.com",
             phone_number="9876543210",
-            role=UserAndContactCommonInfo.Roles.AUTHORIZED_SIGNING_OFFICER,
         )
 
         with self.assertRaises(IntegrityError):
@@ -299,12 +355,27 @@ class OperatorModelTest(BaseTestCase):
 
         cls.test_operator.documents.set(
             [
-                Document.objects.get(id=1),
-                Document.objects.get(id=2),
+                Document.objects.create(file="test1.tst", description="test"),
+                Document.objects.create(file="test2.tst", description="test"),
             ]
         )
 
-        cls.test_operator.contacts.set([Contact.objects.get(id=1), Contact.objects.get(id=2)])
+        cls.test_operator.contacts.set(
+            [
+                Contact.objects.create(
+                    first_name="fname-test",
+                    last_name="lname-test",
+                    email="test1@example.com",
+                    phone_number="12345678900",
+                ),
+                Contact.objects.create(
+                    first_name="fname-test",
+                    last_name="lname-test",
+                    email="test2@example.com",
+                    phone_number="12345678900",
+                ),
+            ]
+        )
 
         # Create multiple UserOperators connected with the test Operator
         for x in range(2):
@@ -313,8 +384,13 @@ class OperatorModelTest(BaseTestCase):
                 operators=Operator.objects.get(id=1),
                 role=UserOperator.Roles.ADMIN,
                 status=UserOperator.Statuses.PENDING,
-                verified_at="2013-11-05",
-                verified_by=User.objects.get(user_guid="00000000-0000-0000-0000-000000000001"),
+                user_is_aso=True,
+                aso_is_owner_or_operator=True,
+                user_is_third_party=True,
+                proof_of_authority=Document.objects.create(file="test.tst", description="test"),
+                signed_statuatory_declaration=Document.objects.create(
+                    file="test.tst", description="test"
+                ),
             )
 
     def test_field_labels_and_max_lengths_and_multiple_relations(self):
@@ -331,17 +407,11 @@ class OperatorModelTest(BaseTestCase):
             ),
             ("duns_number", "duns number", None, None),
             ("business_structure", "business structure", 1000, None),
-            ("physical_street_address", "physical street address", 1000, None),
-            ("physical_municipality", "physical municipality", 1000, None),
-            ("physical_province", "physical province", 2, None),
-            ("physical_postal_code", "physical postal code", 7, None),
-            ("mailing_street_address", "mailing street address", 1000, None),
-            ("mailing_municipality", "mailing municipality", 1000, None),
-            ("mailing_province", "mailing province", 2, None),
-            ("mailing_postal_code", "mailing postal code", 7, None),
-            ("website", "website", 200, None),
-            ("bceid", "bceid", None, None),
-            ("compliance_entity", "compliance entity", None, None),
+            ("bceid", "bceid", 1000, None),
+            ("parent_operator", "parent operator", None, None),
+            ("relationship_with_parent_operator", "relationship with parent operator", 1000, None),
+            ("compliance_obligee", "compliance obligee", None, None),
+            ("date_aso_became_responsible_for_operator", "date aso became responsible for operator", None, None),
             ("documents", "documents", None, 2),
             ("contacts", "contacts", None, 2),
         ]
@@ -397,6 +467,9 @@ class UserOperatorModelTest(BaseTestCase):
 
     @classmethod
     def setUpTestData(cls):
+        proof_doc = Document.objects.create(file="test.tst", description="test")
+        signed_doc = Document.objects.create(file="test.tst", description="test")
+
         cls.test_user_operator = UserOperator.objects.create(
             users=User.objects.get(user_guid="3fa85f64-5717-4562-b3fc-2c963f66afa6"),
             operators=Operator.objects.get(id=1),
@@ -438,32 +511,38 @@ class OperationModelTest(BaseTestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.test_operation = Operation.objects.get(id=1)
+        cls.test_operation = Operation.objects.create(
+            operator=Operator.objects.get(id=1),
+            name="test-name",
+            naics_code=NaicsCode.objects.get(id=1),
+            type="test",
+            naics_category_id=1,
+            latitude=0.1,
+            longitude=0.1,
+            legal_land_description="test",
+        )
 
         cls.test_operation.contacts.set(
             [
-                Contact.objects.get(id=1),
-                Contact.objects.get(id=2),
+                Contact.objects.create(
+                    first_name="fname-test",
+                    last_name="lname-test",
+                    email="test1@example.com",
+                    phone_number="12345678900",
+                ),
+                Contact.objects.create(
+                    first_name="fname-test",
+                    last_name="lname-test",
+                    email="test2@example.com",
+                    phone_number="12345678900",
+                ),
             ]
         )
 
         cls.test_operation.documents.set(
             [
-                Document.objects.get(id=1),
-                Document.objects.get(id=2),
-            ]
-        )
-
-        cls.test_operation.petrinex_ids.set(
-            [
-                PetrinexId.objects.create(id="test"),
-                PetrinexId.objects.create(id="test2"),
-            ]
-        )
-        cls.test_operation.regulated_products.set(
-            [
-                RegulatedProduct.objects.create(name="test"),
-                RegulatedProduct.objects.create(name="test2"),
+                Document.objects.create(file="test1.tst", type_id=1, description="test"),
+                Document.objects.create(file="test2.tst", type_id=1, description="test"),
             ]
         )
 
@@ -510,10 +589,11 @@ class OperationModelTest(BaseTestCase):
             ("legal_land_description", "legal land description", 1000, None),
             ("latitude", "latitude", None, None),
             ("longitude", "longitude", None, None),
-            ("npri_id", "npri id", None, None),
-            ("bcer_permit_id", "bcer permit id", None, None),
-            ("verified_at", "verified at", None, None),
-            ("verified_by", "verified by", None, None),
+            ("legal_land_description", "legal land description", 1000, None),
+            ("nearest_municipality", "nearest municipality", 1000, None),
+            ("operator_percent_of_ownership", "operator percent of ownership", None, None),
+            ("registered_for_obps", "registered for obps", None, None),
+            ("estimated_emissions", "estimated emissions", None, None),
             ("documents", "documents", None, 2),
             ("contacts", "contacts", None, 2),
             ("petrinex_ids", "petrinex ids", None, 2),
