1e92b6fd94f5db6ab4c3ae718dcfa54b1ad5b842
Brianna Cerkiewicz <brianna@button.is>
2023-10-20 08:57:22 -0700

    chore: cleanup

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME", "registration"),
         "USER": os.environ.get("DB_USER", "postgres"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
