a6b02f864591f2e821e23119f2456af5a4a01a4e
SeSo <sobhani.sepehr@gmail.com>
2023-10-20 16:45:07 -0700

    chore: update user operator schema to incorporate recent changes

--- client/app/utils/jsonSchema/userOperator.ts
+++ client/app/utils/jsonSchema/userOperator.ts
@@ -13,12 +13,8 @@ export const userOperatorSchema: RJSFSchema = {
     "phone_number",
     "province",
     "role",
-    "file",
+    // "file", temporary handling of many-to-many fields, will be addressed in #138
     "legal_name",
-    "trade_name",
-    "cra_business_number",
-    "bc_corporate_registry_number",
-    "duns_number",
     "business_structure",
     "physical_street_address",
     "physical_municipality",
@@ -28,10 +24,7 @@ export const userOperatorSchema: RJSFSchema = {
     "mailing_municipality",
     "mailing_province",
     "mailing_postal_code",
-    "website",
-    "bceid",
-    "Does the operator have a parent company?",
-    "Is operator a compliance entity?",
+    "operator_has_parent_company",
   ],
   properties: {
     step_1: {
@@ -51,14 +44,15 @@ export const userOperatorSchema: RJSFSchema = {
     province: { type: "string", title: "Province" },
     role: {
       title: "Are you a senior officer of the operator?",
-      enum: ["Yes", "No"],
-      default: "Yes",
+      type: "boolean",
+      default: true,
     },
-    file: {
-      title: "Signed Statutory Declaration",
-      type: "string",
-      format: "data-url",
-    },
+    // temporary handling of many-to-many fields, will be addressed in #138
+    // file: {
+    //   title: "Signed Statutory Declaration",
+    //   type: "string",
+    //   format: "data-url",
+    // },
     step_2: {
       //Not an actual field in the db - this is just to make the form look like the wireframes
       type: "object",
@@ -68,16 +62,14 @@ export const userOperatorSchema: RJSFSchema = {
     legal_name: { type: "string", title: "Legal Name" },
     trade_name: { type: "string", title: "Trade Name" },
     cra_business_number: {
-      type: "string",
+      type: "number",
       title: "CRA Business Number",
+      readOnly: true,
     },
     bc_corporate_registry_number: {
-      type: "string",
+      type: "number",
       title: "BC Corporate Registry Number",
-    },
-    duns_number: {
-      type: "string",
-      title: "Dun & Bradstreet D-U-N-S Number",
+      readOnly: true,
     },
     business_structure: {
       type: "string",
@@ -116,15 +108,10 @@ export const userOperatorSchema: RJSFSchema = {
       title: "MA Postal Code",
     },
     website: { type: "string", title: "Website" },
-    bceid: { type: "string", title: "BCeID" },
-    "Does the operator have a parent company?": {
-      enum: ["Yes", "No"],
-      default: "Yes",
-    },
-    "Is operator a compliance entity?": {
-      //FIXME: This question is not in the wireframes - waiting for designers to confirm
-      enum: ["Yes", "No"],
-      default: "Yes",
+    operator_has_parent_company: {
+      title: "Does the operator have a parent company?",
+      type: "boolean",
+      default: true,
     },
   },
   allOf: [
@@ -132,15 +119,14 @@ export const userOperatorSchema: RJSFSchema = {
       if: {
         properties: {
           role: {
-            const: "No",
+            const: false,
           },
         },
       },
       then: {
         type: "object",
         required: [
-          "Are you an operation representative of the operator?",
-          "Proof of authority of operation representative from a SO",
+          // "Proof of authority of operation representative from a SO", uncomment when we have document upload working
           "so_first_name",
           "so_last_name",
           "so_position_title",
@@ -159,14 +145,11 @@ export const userOperatorSchema: RJSFSchema = {
             type: "object",
             readOnly: true,
           },
-          "Are you an operation representative of the operator?": {
-            enum: ["Yes", "No"],
-            default: "Yes", // have to set a default value to fix rendering flow
-          },
-          "Proof of authority of operation representative from a SO": {
-            type: "string",
-            format: "data-url",
-          },
+          // temporary handling of many-to-many fields, will be addressed in #138
+          // "Proof of authority of operation representative from a SO": {
+          //   type: "string",
+          //   format: "data-url",
+          // },
           so_first_name: {
             type: "string",
             title: "SO First Name",
@@ -204,88 +187,13 @@ export const userOperatorSchema: RJSFSchema = {
             title: "SO Phone Number",
           },
         },
-        allOf: [
-          {
-            if: {
-              properties: {
-                "Are you an operation representative of the operator?": {
-                  const: "No",
-                },
-              },
-            },
-            then: {
-              type: "object",
-              required: [
-                "Proof of authority of operation representative from a OR",
-                "or_first_name",
-                "or_last_name",
-                "or_position_title",
-                "or_street_address",
-                "or_municipality",
-                "or_province",
-                "or_postal_code",
-                "or_email",
-                "or_phone_number",
-              ],
-              properties: {
-                not_operation_representative: {
-                  //Not an actual field in the db - this is just to make the form look like the wireframes
-                  title:
-                    "If user is not operation representative (OR) - Ignore this section if you selected 'Yes'",
-                  type: "object",
-                  readOnly: true,
-                },
-                "Proof of authority of operation representative from a OR": {
-                  type: "string",
-                  format: "data-url",
-                },
-                or_first_name: {
-                  type: "string",
-                  title: "OR First Name",
-                },
-                or_last_name: {
-                  type: "string",
-                  title: "OR Last Name",
-                },
-                or_position_title: {
-                  type: "string",
-                  title: "OR Position Title",
-                },
-                or_street_address: {
-                  type: "string",
-                  title: "OR Mailing Address",
-                },
-                or_municipality: {
-                  type: "string",
-                  title: "OR Municipality",
-                },
-                or_province: {
-                  type: "string",
-                  title: "OR Province",
-                },
-                or_postal_code: {
-                  type: "string",
-                  title: "OR Postal Code",
-                },
-                or_email: {
-                  type: "string",
-                  title: "OR Email Address",
-                },
-                or_phone_number: {
-                  type: "string",
-                  title: "OR Phone Number",
-                },
-              },
-            },
-          },
-        ],
       },
     },
     {
       if: {
         properties: {
-          "Does the operator have a parent company?": {
-            const: "No",
+          operator_has_parent_company: {
+            const: false,
           },
         },
       },
@@ -293,10 +201,8 @@ export const userOperatorSchema: RJSFSchema = {
         type: "object",
         required: [
           "pc_legal_name",
-          "pc_trade_name",
           "pc_cra_business_number",
           "pc_bc_corporate_registry_number",
-          "pc_duns_number",
           "pc_business_structure",
           "pc_physical_street_address",
           "pc_physical_municipality",
@@ -306,8 +212,6 @@ export const userOperatorSchema: RJSFSchema = {
           "pc_mailing_municipality",
           "pc_mailing_province",
           "pc_mailing_postal_code",
-          "pc_website",
-          "pc_percentage_owned_by_parent_company",
         ],
         properties: {
           has_parent_company: {
@@ -318,7 +222,6 @@ export const userOperatorSchema: RJSFSchema = {
             readOnly: true,
           },
           pc_legal_name: {
-            // FIXME: List of PC legal names?!
             type: "string",
             title: "PC Legal Name",
           },
@@ -327,16 +230,12 @@ export const userOperatorSchema: RJSFSchema = {
             title: "PC Trade Name",
           },
           pc_cra_business_number: {
-            type: "string",
+            type: "number",
             title: "PC CRA Business Number",
           },
           pc_bc_corporate_registry_number: {
-            type: "string",
+            type: "number",
             title: "PC BC Corporate Registry Number",
-          },
-          pc_duns_number: {
-            type: "string",
-            title: "PC Dun & Bradstreet D-U-N-S Number",
           },
           pc_business_structure: {
             type: "string",
@@ -375,177 +274,11 @@ export const userOperatorSchema: RJSFSchema = {
             title: "PC MA Postal Code",
           },
           pc_website: { type: "string", title: "PC Website" },
-          pc_percentage_owned_by_parent_company: {
+          percentage_owned_by_parent_company: {
             type: "number",
             title: "Percentage of ownership of operator",
           },
         },
-      },
-    },
-    {
-      if: {
-        properties: {
-          "Is operator a compliance entity?": {
-            const: "Yes",
-          },
-        },
-      },
-      then: {
-        properties: {
-          compliance_entity: {
-            title: "Legal Name of compliance entity",
-            type: "string", //FIXME: we need a list of compliance entities
-          },
-        },
-      },
-    },
-    {
-      if: {
-        properties: {
-          "Is operator a compliance entity?": {
-            const: "No",
-          },
-        },
-      },
-      then: {
-        type: "object",
-        required: [
-          "aso_first_name",
-          "aso_last_name",
-          "aso_position_title",
-          "aso_street_address",
-          "aso_municipality",
-          "aso_province",
-          "aso_postal_code",
-          "aso_email",
-          "aso_phone_number",
-          "Is ASO a senior officer of the compliance entity?",
-        ],
-        properties: {
-          not_compliance_entity: {
-            //Not an actual field in the db - this is just to make the form look like the wireframes
-            title:
-              "If operator is not compliance entity - Ignore this section if you selected 'Yes'",
-            type: "object",
-            readOnly: true,
-          },
-          aso_first_name: {
-            type: "string",
-            title: "ASO First Name",
-          },
-          aso_last_name: {
-            type: "string",
-            title: "ASO Last Name",
-          },
-          aso_position_title: {
-            type: "string",
-            title: "ASO Position Title",
-          },
-          aso_street_address: {
-            type: "string",
-            title: "ASO Mailing Address",
-          },
-          aso_municipality: {
-            type: "string",
-            title: "ASO Municipality",
-          },
-          aso_province: {
-            type: "string",
-            title: "ASO Province",
-          },
-          aso_postal_code: {
-            type: "string",
-            title: "ASO Postal Code",
-          },
-          aso_email: {
-            type: "string",
-            title: "ASO Email Address",
-          },
-          aso_phone_number: {
-            type: "string",
-            title: "ASO Phone Number",
-          },
-          "Is ASO a senior officer of the compliance entity?": {
-            type: "string",
-            enum: ["Yes", "No"],
-            default: "Yes",
-          },
-        },
-        allOf: [
-          {
-            if: {
-              properties: {
-                "Is ASO a senior officer of the compliance entity?": {
-                  const: "No",
-                },
-              },
-            },
-            then: {
-              type: "object",
-              required: [
-                "Proof of authority of operation representative from a ASO",
-                "soce_first_name",
-                "soce_last_name",
-                "soce_position_title",
-                "soce_street_address",
-                "soce_municipality",
-                "soce_province",
-                "soce_postal_code",
-                "soce_email",
-                "soce_phone_number",
-              ],
-              properties: {
-                is_not_senior_officer: {
-                  //Not an actual field in the db - this is just to make the form look like the wireframes
-                  title:
-                    "If ASO is not senior officer of the compliance entity (SOCE) - Ignore this section if you selected 'Yes'",
-                  type: "object",
-                  readOnly: true,
-                },
-                "Proof of authority of ASO from a SOCE": {
-                  type: "string",
-                  format: "data-url",
-                },
-                soce_first_name: {
-                  type: "string",
-                  title: "SOCE First Name",
-                },
-                soce_last_name: {
-                  type: "string",
-                  title: "SOCE Last Name",
-                },
-                soce_position_title: {
-                  type: "string",
-                  title: "SOCE Position Title",
-                },
-                soce_street_address: {
-                  type: "string",
-                  title: "SOCE Mailing Address",
-                },
-                soce_municipality: {
-                  type: "string",
-                  title: "SOCE Municipality",
-                },
-                soce_province: {
-                  type: "string",
-                  title: "SOCE Province",
-                },
-                soce_postal_code: {
-                  type: "string",
-                  title: "SOCE Postal Code",
-                },
-                soce_email: {
-                  type: "string",
-                  title: "SOCE Email Address",
-                },
-                soce_phone_number: {
-                  type: "string",
-                  title: "SOCE Phone Number",
-                },
-              },
-            },
-          },
-        ],
       },
     },
   ],
@@ -564,10 +297,9 @@ export const userOperatorUiSchema = {
     "phone_number",
     "province",
     "role",
-    "file",
+    // "file", temporary handling of many-to-many fields, will be addressed in #138
     "not_senior_officer",
-    "Are you an operation representative of the operator?",
-    "Proof of authority of operation representative from a SO",
+    // "Proof of authority of operation representative from a SO", temporary handling of many-to-many fields, will be addressed in #138
     "so_first_name",
     "so_last_name",
     "so_position_title",
@@ -577,23 +309,11 @@ export const userOperatorUiSchema = {
     "so_postal_code",
     "so_email",
     "so_phone_number",
-    "not_operation_representative",
-    "Proof of authority of operation representative from a OR",
-    "or_first_name",
-    "or_last_name",
-    "or_position_title",
-    "or_street_address",
-    "or_municipality",
-    "or_province",
-    "or_postal_code",
-    "or_email",
-    "or_phone_number",
     "step_2",
     "legal_name",
     "trade_name",
     "cra_business_number",
     "bc_corporate_registry_number",
-    "duns_number",
     "business_structure",
     "physical_street_address",
     "physical_municipality",
@@ -604,14 +324,12 @@ export const userOperatorUiSchema = {
     "mailing_province",
     "mailing_postal_code",
     "website",
-    "bceid",
-    "Does the operator have a parent company?",
+    "operator_has_parent_company",
     "has_parent_company",
     "pc_legal_name",
     "pc_trade_name",
     "pc_cra_business_number",
     "pc_bc_corporate_registry_number",
-    "pc_duns_number",
     "pc_business_structure",
     "pc_physical_street_address",
     "pc_physical_municipality",
@@ -622,83 +340,40 @@ export const userOperatorUiSchema = {
     "pc_mailing_province",
     "pc_mailing_postal_code",
     "pc_website",
-    "pc_percentage_owned_by_parent_company",
-    "Is operator a compliance entity?",
-    "compliance_entity",
-    "not_compliance_entity",
-    "aso_first_name",
-    "aso_last_name",
-    "aso_position_title",
-    "aso_street_address",
-    "aso_municipality",
-    "aso_province",
-    "aso_postal_code",
-    "aso_email",
-    "aso_phone_number",
-    "Is ASO a senior officer of the compliance entity?",
-    "is_not_senior_officer",
-    "Proof of authority of ASO from a SOCE",
-    "soce_first_name",
-    "soce_last_name",
-    "soce_position_title",
-    "soce_street_address",
-    "soce_municipality",
-    "soce_province",
-    "soce_postal_code",
-    "soce_email",
-    "soce_phone_number",
+    "percentage_owned_by_parent_company",
   ],
   step_1: {
     "ui:classNames":
       "bg-bc-gov-primary-brand-color-blue text-white no-underline text-start ps-4 rounded",
   },
+  email: {
+    "ui:widget": "EmailWidget",
+  },
   role: {
-    "ui:widget": "radio",
-    "ui:inline": "true",
-    "ui:classNames": "flex border rounded p-3",
+    "ui:widget": "SelectWidget",
   },
   not_senior_officer: {
     "ui:classNames":
       "text-bc-gov-primary-brand-color-blue no-underline text-start ps-4 text-xs",
   },
-  "Are you an operation representative of the operator?": {
-    "ui:widget": "radio",
-    "ui:inline": "true",
-    "ui:classNames": "flex border rounded p-3",
-  },
-  not_operation_representative: {
-    "ui:classNames":
-      "text-bc-gov-primary-brand-color-blue no-underline text-start ps-4 text-xs",
+  so_email: {
+    "ui:widget": "EmailWidget",
   },
   step_2: {
     "ui:classNames":
       "bg-bc-gov-primary-brand-color-blue text-white no-underline text-start ps-4 rounded",
   },
-  "Does the operator have a parent company?": {
-    "ui:widget": "radio",
-    "ui:inline": "true",
-    "ui:classNames": "flex border rounded p-3",
-  },
-  "Is operator a compliance entity?": {
-    "ui:widget": "radio",
-    "ui:inline": "true",
-    "ui:classNames": "flex border rounded p-3",
+  operator_has_parent_company: {
+    "ui:widget": "SelectWidget",
   },
   has_parent_company: {
     "ui:classNames":
       "text-bc-gov-primary-brand-color-blue no-underline text-start ps-4 text-xs",
   },
-  not_compliance_entity: {
-    "ui:classNames":
-      "text-bc-gov-primary-brand-color-blue no-underline text-start ps-4 text-xs",
+  website: {
+    "ui:widget": "URLWidget",
   },
-  "Is ASO a senior officer of the compliance entity?": {
-    "ui:widget": "radio",
-    "ui:inline": "true",
-    "ui:classNames": "flex border rounded p-3",
-  },
-  is_not_senior_officer: {
-    "ui:classNames":
-      "text-bc-gov-primary-brand-color-blue no-underline text-start ps-4 text-xs",
+  pc_website: {
+    "ui:widget": "URLWidget",
   },
 };
