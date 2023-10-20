fbd4075527c304b5cc01656a47532f8f88b6617d
SeSo <sobhani.sepehr@gmail.com>
2023-10-20 16:38:37 -0700

    feat: add API endpoints for prime admin to request access

--- bc_obps/registration/api.py
+++ bc_obps/registration/api.py
@@ -6,7 +6,9 @@ from bc_obps.registration.schema import (
     NaicsCodeSchema,
     OperationIn,
     OperationOut,
+    OperatorOut,
     RequestAccessOut,
+    SelectOperatorIn,
     UserOperatorIn,
     UserOperatorOut,
     Message,
@@ -15,6 +17,7 @@ from registration.utils import check_users_admin_request_eligibility, update_mod
 from ninja import ModelSchema, Router
 from ninja.responses import codes_4xx
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 from django.forms import model_to_dict
 from ninja import Field, Schema, ModelSchema
 from .models import Contact, Operation, Operator, NaicsCode, NaicsCategory, ParentChildOperator, User, UserOperator
@@ -116,21 +119,6 @@ def update_operation(request, operation_id: int, payload: OperationIn):
     operation.status = "Pending"
     operation.save()
     return {"name": operation.name}
-
-
-# OPERATOR
-class OperatorOut(ModelSchema):
-    """
-    Schema for the Operator model
-    """
-
-    class Config:
-        model = Operator
-        model_fields = '__all__'
-
-
-class SelectOperatorIn(Schema):
-    operator_id: int
 
 
 @router.get("/operators", response=List[OperatorOut])
@@ -384,41 +372,185 @@ def select_operator(request, operator_id: int):
     user: User = User.objects.first()  # FIXME: get the user from the request
     operator: Operator = get_object_or_404(Operator, id=operator_id)
 
-    # User already has an admin user for this operator
-    if UserOperator.objects.filter(
-        users=user, operators=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.APPROVED
-    ).exists():
-        raise HttpError(400, "You are already an admin for this Operator!")
+    # check if user is eligible to request access
+    check_users_admin_request_eligibility(user, operator)
 
-    # Operator already has an admin user
-    if UserOperator.objects.filter(
-        operators=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.APPROVED
-    ).exists():
-        raise HttpError(400, "This Operator already has an admin user!")
-
-    return {"operator_id": operator.id}
+    return 200, {"operator_id": operator.id}
 
 
-@router.post("/select-operator/request-access", response=SelectOperatorIn)
+@router.post("/select-operator/request-access", response={201: RequestAccessOut, codes_4xx: Message})
 def request_access(request, payload: SelectOperatorIn):
     user: User = User.objects.first()  # FIXME: get the user from the request
     payload_dict: dict = payload.dict()
     operator: Operator = get_object_or_404(Operator, id=payload_dict.get("operator_id"))
 
-    # User already has an admin user for this operator
-    if UserOperator.objects.filter(
-        users=user, operators=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.APPROVED
-    ).exists():
-        raise HttpError(400, "You are already an admin for this Operator")
+    # check if user is eligible to request access
+    check_users_admin_request_eligibility(user, operator)
 
-    # Operator already has an admin user
-    if UserOperator.objects.filter(
-        operators=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.APPROVED
-    ).exists():
-        raise HttpError(400, "This Operator already has an admin user")
-
-    # Making a draft UserOperator instance
-    UserOperator.objects.create(
-        users=user, operators=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.DRAFT
+    # Making a draft UserOperator instance if one doesn't exist
+    user_operator, _ = UserOperator.objects.get_or_create(
+        user=user, operator=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.DRAFT
     )
-    return {"operator_id": operator.id}
+    return 201, {"user_operator_id": user_operator.id}
+
+
+@router.get("/select-operator/request-access/user-operator/{int:user_operator_id}", response=UserOperatorOut)
+def get_user_operator(request, user_operator_id: int):
+    user_operator = get_object_or_404(UserOperator, id=user_operator_id)
+    user: User = user_operator.user
+    user_related_fields_dict = model_to_dict(
+        user,
+        fields=[
+            "first_name",
+            "last_name",
+            "position_title",
+            "street_address",
+            "municipality",
+            "province",
+            "postal_code",
+            "email",
+        ],
+    )
+    operator: Operator = user_operator.operator
+    operator_related_fields_dict = model_to_dict(
+        operator,
+        fields=[
+            "legal_name",
+            "trade_name",
+            "cra_business_number",
+            "bc_corporate_registry_number",
+            "business_structure",
+            "physical_street_address",
+            "physical_municipality",
+            "physical_province",
+            "physical_postal_code",
+            "mailing_street_address",
+            "mailing_municipality",
+            "mailing_province",
+            "mailing_postal_code",
+            "website",
+        ],
+    )
+
+    return {
+        **user_related_fields_dict,
+        "phone_number": user.phone_number.as_e164,  # PhoneNumberField returns a PhoneNumber object and we need a string
+        **operator_related_fields_dict,
+    }
+
+
+@router.put(
+    "/select-operator/request-access/user-operator/{int:user_operator_id}",
+    response={200: SelectOperatorIn, codes_4xx: Message},
+)
+def create_user_operator_request(request, user_operator_id: int, payload: UserOperatorIn):
+    try:
+        user_operator = get_object_or_404(UserOperator, id=user_operator_id)
+        user: User = user_operator.user
+        operator: Operator = user_operator.operator
+        payload_dict = payload.dict()
+
+        is_senior_officer: bool = payload_dict.get("role")
+        operator_has_parent_company: bool = payload_dict.get("operator_has_parent_company")
+
+        # fields to update on the User model
+        user_related_fields = [
+            "first_name",
+            "last_name",
+            "position_title",
+            "street_address",
+            "municipality",
+            "province",
+            "postal_code",
+            "email",
+            "phone_number",
+        ]
+
+        updated_user_instance: User = update_model_instance(user, user_related_fields, payload_dict)
+
+        if is_senior_officer:
+            updated_user_instance.role = User.Roles.SENIOR_OFFICER
+        else:
+            contact_fields_mapping = {
+                "so_first_name": "first_name",
+                "so_last_name": "last_name",
+                "so_position_title": "position_title",
+                "so_street_address": "street_address",
+                "so_municipality": "municipality",
+                "so_province": "province",
+                "so_postal_code": "postal_code",
+                "so_email": "email",
+                "so_phone_number": "phone_number",
+            }
+            contact_instance: Contact = Contact(role=Contact.Roles.SENIOR_OFFICER)
+            senior_officer_contact: Contact = update_model_instance(
+                contact_instance, contact_fields_mapping, payload_dict
+            )
+
+        # fields to update on the Operator model
+        operator_related_fields = [
+            "legal_name",
+            "trade_name",
+            "business_structure",
+            "physical_street_address",
+            "physical_municipality",
+            "physical_province",
+            "physical_postal_code",
+            "mailing_street_address",
+            "mailing_municipality",
+            "mailing_province",
+            "mailing_postal_code",
+            "website",
+        ]
+        updated_operator_instance: Operator = update_model_instance(operator, operator_related_fields, payload_dict)
+
+        if not operator_has_parent_company:
+            parent_operator_fields_mapping = {
+                "pc_legal_name": "legal_name",
+                "pc_trade_name": "trade_name",
+                "pc_cra_business_number": "cra_business_number",
+                "pc_bc_corporate_registry_number": "bc_corporate_registry_number",
+                "pc_business_structure": "business_structure",
+                "pc_physical_street_address": "physical_street_address",
+                "pc_physical_municipality": "physical_municipality",
+                "pc_physical_province": "physical_province",
+                "pc_physical_postal_code": "physical_postal_code",
+                "pc_mailing_street_address": "mailing_street_address",
+                "pc_mailing_municipality": "mailing_municipality",
+                "pc_mailing_province": "mailing_province",
+                "pc_mailing_postal_code": "mailing_postal_code",
+                "pc_website": "website",
+            }
+            parent_operator_instance: Operator = Operator()
+            parent_operator_instance: Operator = update_model_instance(
+                parent_operator_instance, parent_operator_fields_mapping, payload_dict
+            )
+
+            percentage_owned_by_parent_company: Optional[int] = payload_dict.get('percentage_owned_by_parent_company')
+            if percentage_owned_by_parent_company:
+                parent_child_operator_instance = ParentChildOperator(
+                    parent_operator=parent_operator_instance,
+                    child_operator=updated_operator_instance,
+                    percentage_owned_by_parent_company=percentage_owned_by_parent_company,
+                )
+
+        # saving the updated instances
+        updated_user_instance.save()
+        updated_operator_instance.save()
+
+        if not is_senior_officer:
+            senior_officer_contact.save()
+
+        if not operator_has_parent_company:
+            parent_operator_instance.save()
+            if percentage_owned_by_parent_company:
+                parent_child_operator_instance.save()
+
+    except ValidationError as e:
+        return 400, {"message": e.messages[0]}
+
+    # updating the status of the UserOperator instance
+    user_operator.status = UserOperator.Statuses.PENDING
+    user_operator.save(update_fields=["status"])
+
+    return 200, {"operator_id": operator.id}
