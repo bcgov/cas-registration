71217bda05061c7fe1d2eec392a498dc99f33965
Brianna Cerkiewicz <brianna@button.is>
2023-10-24 15:41:39 -0700

    chore: apply suggestions from code review

--- client/e2e/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const checkboxChecker = async (page: any, ariaLabel: string) => {
-  const checkbox = await page.$(`[aria-label="${ariaLabel}"]`);
-  if (checkbox) {
-    await checkbox.click();
-  } else {
-    console.error("Checkbox not found");
-  }
-};
