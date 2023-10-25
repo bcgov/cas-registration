e2250f1969b7f77e80b5d09a09242d7e740a433c
shon-button <shon@button.is>
2023-10-25 15:22:11 -0400

    test 8

--- client/e2e/operations.spec.ts
+++ client/e2e/operations.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from "@playwright/test";
+test.beforeEach(async ({ page }, testInfo) => {
+  // eslint-disable-next-line no-console
+  console.log(`Running ${testInfo.title}`);
+  // reset db
+  await page.goto("http://localhost:8000/api/registration/test-setup");
+  await expect(page.getByText(/Test setup failed/i)).not.toBeVisible();
+  await expect(
+    page.getByText(/This endpoint only exists in the development environment./i)
+  ).not.toBeVisible();
+  await expect(page.getByText(/Test setup complete/i)).toBeVisible();
 
-test("backend is running", async ({ page }) => {
-  await page.goto("http://0.0.0.0:8000/api/docs"); // NOSONAR
-
-  await expect(page.getByText(/Ninja/i)).toBeVisible();
+  // navigate to operations page
+  await page.goto("http://localhost:3000/dashboard/operations");
 });
 
 test("frontend is running", async ({ page }) => {
