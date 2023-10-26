1433fd09bcec4ddf2682fdb18457fe8b387285dc
shon-button <shon@button.is>
2023-10-26 08:38:49 -0400

    🐛 Fix: arrary dependancy and invalid token

--- client/app/components/layout/Footer.tsx
+++ client/app/components/layout/Footer.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 // 🏷 import {named} can be significantly slower than import default
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack/Stack";
 import footerLinks from "@/app/data/layout/footer.json";
 
 export default function Footer() {
@@ -17,31 +16,24 @@ export default function Footer() {
           mt: "auto", //in a flexbox item, this will push the footer down to the bottom
         }}
       >
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          justifyContent={{ xs: "center", md: "flex-start" }}
-          alignItems={{ xs: "center" }}
-        >
-          {footerLinks.map((link, index) => (
-            <Link
-              key={index}
-              href={link.href}
-              target={link.target}
-              style={{
-                fontWeight: 400,
-                fontSize: "16px",
-                lineHeight: "19.36px",
-                textDecoration: "none",
-                color: "white",
-                margin: "0 10px ",
-              }}
-              aria-label={link.label}
-            >
-              {link.name}
-            </Link>
-          ))}
-        </Stack>
+        {footerLinks.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            target={link.target}
+            style={{
+              fontWeight: 400,
+              fontSize: "16px",
+              lineHeight: "19.36px",
+              textDecoration: "none",
+              color: "white",
+              margin: "0 10px ",
+            }}
+            aria-label={link.label}
+          >
+            {link.name}
+          </Link>
+        ))}
       </Box>
     </>
   );
