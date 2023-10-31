6b07d5091f1a37f816595aa740ef041cd46a9f21
SeSo <sobhani.sepehr@gmail.com>
2023-10-31 13:42:12 -0700

    chore: bring back print statement

--- bc_obps/bc_obps/api.py
+++ bc_obps/bc_obps/api.py
@@ -8,6 +8,7 @@ api = NinjaAPI()
 # print errors for 422 status codes for faster debugging
 @api.exception_handler(ValidationError)
 def custom_validation_errors(request, exc):
+    print(exc.errors)  # This print statement is really helpful for debugging
     return api.create_response(request, {"detail": exc.errors}, status=422)
 
 
