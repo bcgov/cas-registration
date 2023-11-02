dd17482f9af24cf5b3f96597d1467ad386bff264
Josh Gamache <joshua@button.is>
2023-11-02 11:40:21 -0600

    chore: provide backups sa credentials in openshift secret

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -6,12 +6,21 @@ terraform {
       source = "hashicorp/google"
       version = "~> 5.2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
   }
 }
 
 provider "google" {
   project = var.project
   region = var.region
+}
+
+provider "kubernetes" {
+  host = var.kubernetes_host
+  token = var.kubernetes_token
 }
 
 resource "google_storage_bucket" "database_backups" {
@@ -22,7 +31,7 @@ resource "google_storage_bucket" "database_backups" {
 
 }
 
-resource "google_service_account" "account" {
+resource "google_service_account" "backups_sa" {
   account_id = "${var.openshift_nameplate}-${var.openshift_environment}-backups-sa"
   display_name = "${var.openshift_nameplate}-${var.openshift_environment}-backups-sa"
   depends_on = [ google_storage_bucket.database_backups ]
@@ -34,7 +43,27 @@ resource "google_storage_bucket_iam_binding" "database_binding" {
   members = [
     "projectEditor:ggl-cas-storage",
     "projectOwner:ggl-cas-storage",
-    "serviceAccount:${google_service_account.account.email}",
+    "serviceAccount:${google_service_account.backups_sa.email}",
   ]
-  depends_on = [ google_service_account.account ]
+  depends_on = [ google_service_account.backups_sa ]
+}
+
+resource "google_service_account_key" "backups_sa_key" {
+  service_account_id = google_service_account.backups_sa.name
+}
+
+resource "kubernetes_secret" "storage_secret" {
+  metadata {
+    # name = "key" gcp-d193ca-dev-obps-backups-sa-key
+    name = "gcp-${google_service_account.backups_sa.display_name}-key"
+    namespace = "${var.openshift_nameplate}-${var.openshift_environment}"
+    labels = {
+      created-by = "Terraform"
+    }
+  }
+
+  data = {
+    "bucket_name" = google_storage_bucket.database_backups.name
+    "credentials.json" = base64decode(google_service_account_key.backups_sa_key.private_key)
+  }
 }
