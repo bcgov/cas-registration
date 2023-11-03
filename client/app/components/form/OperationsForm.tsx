32ac6db56e1cd1cb9a0c337553b7d451e987b19c
marcellmueller <marcel@button.is>
2023-11-03 14:23:57 -0700

    refactor: add form base component and default rjsf theme

--- client/app/components/form/OperationsForm.tsx
+++ client/app/components/form/OperationsForm.tsx
@@ -28,12 +28,12 @@ export default function OperationsForm(props: Props) {
     ...props.formData,
 
     previous_year_attributable_emissions: Number(
-      props.formData?.previous_year_attributable_emissions,
+      props.formData?.previous_year_attributable_emissions
     ),
     swrs_facility_id: Number(props.formData?.swrs_facility_id),
     bcghg_id: Number(props.formData?.bcghg_id),
     operator_percent_of_ownership: Number(
-      props.formData?.operator_percent_of_ownership,
+      props.formData?.operator_percent_of_ownership
     ),
     "Did you submit a GHG emissions report for reporting year 2022?": props
       .formData?.previous_year_attributable_emissions
@@ -81,7 +81,7 @@ export default function OperationsForm(props: Props) {
             reporting_activities: [],
             operator_id: 1,
           },
-          props.formData ? "PUT" : "POST",
+          props.formData ? "PUT" : "POST"
         );
         if (response.error) {
           setError(response.error);
