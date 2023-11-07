49cf1f7ef1f8d90ff93596480997847cfc2a9e7d
shon-button <shon@button.is>
2023-11-07 17:31:30 -0500

    🚧 nextauth with keycloak provider

--- client/middlewares/withAuthorization.tsx
+++ client/middlewares/withAuthorization.tsx
@@ -6,23 +6,43 @@ import {
 } from "next/server";
 
 import { MiddlewareFactory } from "./types";
+import { getToken } from "next-auth/jwt";
 
 export const withAuthorization: MiddlewareFactory = (next: NextMiddleware) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
     // 👇️ vars for route management
     const { pathname } = request.nextUrl;
+    const isRouteAuth =
+      pathname.indexOf("/auth") > -1 || pathname.indexOf("/unauth") > -1;
 
-    /******************TODO***********************
-     *
-     * AUTHENTICATION
-     */
-
-    //...for now, route baseUrl to {onboarding)/home}
-    if (pathname.endsWith("/")) {
-      //route to (onboarding)\home
-      return NextResponse.redirect(new URL(`/home`, request.url));
+    // 👇️ check if authentication route
+    if (isRouteAuth === true) {
+      //👌 ok: route to next middleware
+      return next(request, _next);
     }
-    //👌 ok: route to next middleware
-    return next(request, _next);
+    // 👇️ check for auth token: /api/auth/session
+    const token = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    if (token) {
+      if (pathname.endsWith("/") || pathname.endsWith("/home")) {
+        //route to (authenticated)\dashboard
+        return NextResponse.redirect(new URL(`/dashboard`, request.url));
+      } else {
+        //👌 ok: route to next middleware
+        return next(request, _next);
+      }
+    } else {
+      //📛 route to (onboarding)\home
+      if (pathname.endsWith("/home")) {
+        //👌 ok: route to next middleware
+        return next(request, _next);
+      } else {
+        //route to (onboarding)\home
+        return NextResponse.redirect(new URL(`/home`, request.url));
+      }
+    }
   };
 };
