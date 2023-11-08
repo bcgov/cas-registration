11802189755d22f30222c80b8ff7eaf371781e2b
shon-button <shon@button.is>
2023-11-08 16:54:09 -0500

    📖 chore: developer-guide.md

--- client/app/error.tsx
+++ client/app/error.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+<<<<<<< HEAD
 export default async function RootError() {
   return <p>Error ocurred.</p>;
+=======
+export default async function Error() {
+  return (
+    <>
+      <p>Error ocurred.</p>
+    </>
+  );
+>>>>>>> 20d7217 (📖 chore: developer-guide.md)
 }
--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,5 +1,9 @@
 import Skeleton from "@mui/material/Skeleton";
 
+<<<<<<< HEAD
 export default function Loading() {
+=======
+export default function Variants() {
+>>>>>>> 20d7217 (📖 chore: developer-guide.md)
   return <Skeleton variant="circular" width={40} height={40} />;
 }
