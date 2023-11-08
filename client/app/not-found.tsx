ca59695ef1d535a444a5be6b4c4261c836bf17d7
shon-button <shon@button.is>
2023-11-08 16:54:09 -0500

    📖 chore: developer-guide.md

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,3 +1,11 @@
 export default async function NotFound() {
+<<<<<<< HEAD
   return <p>Page not found</p>;
+=======
+  return (
+    <>
+      <p>Page not found</p>
+    </>
+  );
+>>>>>>> 20d7217 (📖 chore: developer-guide.md)
 }
