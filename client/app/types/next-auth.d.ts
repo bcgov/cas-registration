c4891f68604db35e2316f83c424007f11840f83e
shon-button <shon@button.is>
2023-11-09 08:08:37 -0500

    🚧 nextauth refresh token

--- client/app/types/next-auth.d.ts
+++ client/app/types/next-auth.d.ts
@@ -2,27 +2,29 @@ import { DefaultSession } from "next-auth";
 
 /*
 📌 Module Augmentation
-next-auth comes with certain types/interfaces that are shared across submodules, e.g. Session and JWT
+next-auth comes with certain types/interfaces that are shared across submodules, e.g. JWT or Session
 Ideally, you should only need to create these types at a single place, and TS should pick them up in every location where they are referenced.
 Module Augmentation is exactly that-
 define your shared interfaces in a single place, and get type-safety across your application
 */
-declare module "next-auth" {
-  interface Session {
-    error?: string;
-    user: {
-      // 👇️ Module augmentation to add 'roles' definition to the Session object
-      role?: string;
-    } & DefaultSession["user"];
-  }
-}
 
 declare module "next-auth/jwt" {
-  // 👇️ Module augmentation to add 'role' definition to the JWT
   interface JWT {
+    accessToken?: string | undefined;
+    refreshToken?: string | undefined;
+    expires_at?: number | undefined;
     id_token: string | undefined;
     idir_user_guid: string | undefined;
     error?: string;
     role?: string;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    error?: string;
+    user: {
+      role?: string;
+    } & DefaultSession["user"];
   }
 }
