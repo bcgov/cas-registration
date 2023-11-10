b519fc9c46722ce530c778617b7764f8e689c13a
marcellmueller <marcel@button.is>
2023-11-10 11:42:29 -0800

    refactor: destructure props in operations form

--- client/app/components/form/OperationsForm.tsx
+++ client/app/components/form/OperationsForm.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import { useState } from "react";
+import { useParams } from "next/navigation";
 import {
   operationUiSchema,
-  operationsGroupSchema,
+  /*   operationsGroupSchema, */
 } from "@/app/utils/jsonSchema/operations";
 import FormBase from "@/app/components/form/FormBase";
 import { RJSFSchema } from "@rjsf/utils";
+import { Button } from "@mui/material";
 import Link from "next/link";
 import React from "react";
 import { Alert } from "@mui/material";
-import SubmitButton from "./SubmitButton";
 import { operationSubmitHandler } from "@/app/utils/actions";
 
 export interface OperationsFormData {
@@ -20,31 +22,36 @@ interface Props {
   formData?: OperationsFormData;
 }
 
-export default function OperationsForm(props: Props) {
-  const [operationName, setOperationName] = React.useState("");
-  const [error, setError] = React.useState(undefined);
+export default function OperationsForm({ formData, schema }: Props) {
+  const [operationName, setOperationName] = useState("");
+  const [error, setError] = useState(undefined);
+
+  const params = useParams();
+  const formSection = parseInt(params?.formSection as string) - 1;
+
   // need to convert some of the information received from django into types RJSF can read
   const existingFormData = {
-    ...props.formData,
+    ...formData,
 
     previous_year_attributable_emissions: Number(
-      props.formData?.previous_year_attributable_emissions
+      formData?.previous_year_attributable_emissions
     ),
-    swrs_facility_id: Number(props.formData?.swrs_facility_id),
-    bcghg_id: Number(props.formData?.bcghg_id),
+    swrs_facility_id: Number(formData?.swrs_facility_id),
+    bcghg_id: Number(formData?.bcghg_id),
     operator_percent_of_ownership: Number(
-      props.formData?.operator_percent_of_ownership
+      formData?.operator_percent_of_ownership
     ),
-    "Did you submit a GHG emissions report for reporting year 2022?": props
-      .formData?.previous_year_attributable_emissions
-      ? true
-      : false,
-    verified_at: props.formData?.verified_at?.toString(),
-    verified_by: props.formData?.verified_by?.toString(),
+    "Did you submit a GHG emissions report for reporting year 2022?":
+      formData?.previous_year_attributable_emissions ? true : false,
+    verified_at: formData?.verified_at?.toString(),
+    verified_by: formData?.verified_by?.toString(),
     //  temporary handling of required many-to-many fields, will be addressed in #138
     regulated_products: "",
     reporting_activities: "",
   };
+  // handle form state externally so we can save data for multiple pages of the form
+  const [formState, setFormState] = useState(existingFormData || {});
+  const formSectionList = Object.keys(schema.properties as any);
 
   return operationName ? (
     <>
@@ -63,16 +70,15 @@ export default function OperationsForm(props: Props) {
     <FormBase
       // Because this is an RJSF form, we can't use the Nextjs13.5 pattern of putting a function in the action prop and using the useFormState hook.
       readonly={
-        props.formData?.status === "Registered" ||
-        props.formData?.status === "Pending"
+        formData?.status === "Registered" || formData?.status === "Pending"
           ? true
           : false
       }
-      schema={props.schema}
+      schema={schema.properties[formSectionList[formSection]]}
       onSubmit={async (data: { formData?: any }) => {
         const response = await operationSubmitHandler(
           {
-            ...props.formData,
+            ...formData,
             ...data.formData,
             //  temporary handling of required many-to-many fields, will be addressed in #138
             documents: [],
@@ -81,7 +87,7 @@ export default function OperationsForm(props: Props) {
             reporting_activities: [],
             operator_id: 1,
           },
-          props.formData ? "PUT" : "POST"
+          formData ? "PUT" : "POST"
         );
         if (response.error) {
           setError(response.error);
@@ -90,13 +96,49 @@ export default function OperationsForm(props: Props) {
         setOperationName(response.name);
       }}
       uiSchema={operationUiSchema}
-      formData={existingFormData}
-      formContext={{
-        groupSchema: operationsGroupSchema,
+      // formContext={{
+      //   groupSchema: operationsGroupSchema,
+      // }}
+      formData={formState[formSectionList[formSection]]}
+      onChange={(e) => {
+        // merge page data with existing form data
+        setFormState({
+          ...formState,
+          ...e.formData,
+        });
       }}
     >
       {error && <Alert severity="error">{error}</Alert>}
-      <SubmitButton label="submit" />
+      {formSection !== 0 ? (
+        <Link href={`/dashboard/operations/create/${formSection}`}>
+          <Button
+            variant="contained"
+            type="button"
+            disabled={formSection === 0}
+          >
+            Previous
+          </Button>
+        </Link>
+      ) : (
+        <Button variant="contained" type="button" disabled>
+          Previous
+        </Button>
+      )}
+      {formSection !== formSectionList.length - 1 ? (
+        <Link href={`/dashboard/operations/create/${formSection + 2}`}>
+          <Button
+            variant="contained"
+            type="button"
+            disabled={formSection === formSectionList.length - 1}
+          >
+            Next
+          </Button>
+        </Link>
+      ) : (
+        <Button type="submit" variant="contained">
+          Submit
+        </Button>
+      )}
     </FormBase>
   );
 }
