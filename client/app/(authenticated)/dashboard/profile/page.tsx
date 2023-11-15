9eb2e1c7b100f351883e71399a88cc102091c710
shon-button <shon@button.is>
2023-11-15 16:16:50 -0500

    🏗️ feat: NextAuth with Keycloak for IDIR and BCeid auth

--- client/app/(authenticated)/dashboard/profile/page.tsx
+++ client/app/(authenticated)/dashboard/profile/page.tsx
@@ -2,15 +2,22 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Page() {
-  /*When calling from the server-side i.e. in Route Handlers, React Server Components, API routes getServerSession requires passing the same object you would pass to NextAuth*/
+  /* When calling from the server-side i.e., in Route Handlers, React Server Components, API routes,
+   * getServerSession requires passing the same object you would pass to NextAuth
+   */
   const session = await getServerSession(authOptions);
-  const name = session?.user?.name;
-  const role = session?.user?.role;
+
   return (
     <>
-      <h1>Hello</h1>
-      {name && <p>Name: {name}</p>}
-      {role && <p>Role: {role}</p>}
+      <h1>Server Page Session Information</h1>
+      {session && (
+        <>
+          <p>Name: {session?.user?.name}</p>
+          <p>Role: {session?.user?.role}</p>
+          <p>GUID: {session?.user?.user_guid}</p>
+          <p>IP: {session?.identity_provider}</p>
+        </>
+      )}
     </>
   );
 }
