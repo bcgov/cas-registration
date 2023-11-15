d7f85e26cf9acc0fec5e959b028072295050d4b6
shon-button <shon@button.is>
2023-11-15 10:41:51 -0500

    🌸 fix: prettier

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,17 +1,5 @@
 import Skeleton from "@mui/material/Skeleton";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default function Loading() {
-=======
-export default function Variants() {
->>>>>>> 20d7217 (📖 chore: developer-guide.md)
-=======
-export default function Loading() {
->>>>>>> 0fe7d50 (🦨 fix: code smell)
-=======
-export default function Loading() {
->>>>>>> a2ee6416c63b48533be481e5c6e4eb19aec5e811
   return <Skeleton variant="circular" width={40} height={40} />;
 }
