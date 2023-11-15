e5313d6380caf0d3343094d857a1ed5f72cacedc
SeSo <sobhani.sepehr@gmail.com>
2023-11-15 13:42:06 -0800

    chore: fix pre-commit issues

--- bc_obps/registration/api/__init__.py
+++ bc_obps/registration/api/__init__.py
@@ -1,2 +1,2 @@
-from . import (contact, naics, operation, operator, user_operator, user)
+from . import contact, naics, operation, operator, user_operator, user
 from .api_base import router  # django_ninja router object
