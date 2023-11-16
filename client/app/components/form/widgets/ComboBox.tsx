8bd8773adac8ae09f2d56d0bc599a3d3bcc053e2
marcellmueller <marcel@button.is>
2023-11-16 09:14:20 -0800

    chore: remove console.log

--- client/app/components/form/widgets/ComboBox.tsx
+++ client/app/components/form/widgets/ComboBox.tsx
@@ -10,7 +10,6 @@ const ComboBox: React.FC<WidgetProps> = (props) => {
   const handleChange = (e: React.ChangeEvent<{}>, option: any) => {
     onChange(option?.enum?.[0]);
   };
-  console.log("value", value, schema);
 
   const getSelected = useCallback(() => {
     if (!value || !schema?.anyOf) return null;
