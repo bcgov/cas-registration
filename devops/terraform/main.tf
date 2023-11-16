530e7b9200b9d3a5dce667da6fa80e7eb131bf78
Josh Gamache <joshua@button.is>
2023-11-16 14:24:24 -0700

    chore: add attachment storage to Terraform

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -68,3 +68,53 @@ resource "kubernetes_secret" "storage_secret" {
     "credentials.json" = base64decode(google_service_account_key.backups_sa_key.private_key)
   }
 }
+
+## ---------------------------------------------------------------------------------------------------------------------
+## STORAGE FOR ATTACHMENTS
+## Provisions storage buckets and service accounts for the buckets, then loads the secrets for the service accounts
+## into an OpenShift secret.
+## ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_storage_bucket" "reg_attachments" {
+  name = "${var.openshift_nameplate}-${var.openshift_environment}-reg-attachments"
+  location = var.region
+
+  public_access_prevention = "enforced"
+
+}
+
+resource "google_service_account" "reg_attachments_sa" {
+  account_id = "${var.openshift_nameplate}-${var.openshift_environment}-reg-attachments-sa"
+  display_name = "${var.openshift_nameplate}-${var.openshift_environment}-reg-attachments-sa"
+  depends_on = [ google_storage_bucket.reg_attachments ]
+}
+
+resource "google_storage_bucket_iam_binding" "reg_attachments_binding" {
+  bucket = google_storage_bucket.reg_attachments.name
+  role = "roles/storage.admin"
+  members = [
+    "projectEditor:ggl-cas-storage",
+    "projectOwner:ggl-cas-storage",
+    "serviceAccount:${google_service_account.reg_attachments_sa.email}",
+  ]
+  depends_on = [ google_service_account.reg_attachments_sa ]
+}
+
+resource "google_service_account_key" "reg_attachments_key" {
+  service_account_id = google_service_account.reg_attachments_sa.name
+}
+
+resource "kubernetes_secret" "reg_attachments_storage_secret" {
+  metadata {
+    name = "gcp-${google_service_account.reg_attachments_sa.display_name}-key"
+    namespace = "${var.openshift_nameplate}-${var.openshift_environment}"
+    labels = {
+      created-by = "Terraform"
+    }
+  }
+
+  data = {
+    "bucket_name" = google_storage_bucket.reg_attachments.name
+    "credentials.json" = base64decode(google_service_account_key.reg_attachments_key.private_key)
+  }
+}
