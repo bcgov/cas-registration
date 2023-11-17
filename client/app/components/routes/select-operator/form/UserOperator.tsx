88caf50051a6762c28e1112363a8f6b0fba5b1c5
SeSo <sobhani.sepehr@gmail.com>
2023-11-17 15:58:11 -0800

    chore: use user-operator form for editing and creating

--- client/app/components/routes/select-operator/form/UserOperator.tsx
+++ client/app/components/routes/select-operator/form/UserOperator.tsx
@@ -1,7 +1,12 @@
 import { actionHandler } from "@/app/utils/actions";
 import { userOperatorSchema } from "@/app/utils/jsonSchema/userOperator";
-import UserOperatorForm from "@/app/components/form/UserOperatorForm";
-import { UserOperatorFormData } from "@/app/components/form/formDataTypes";
+import UserOperatorForm, {
+  UserOperatorFormProps,
+} from "@/app/components/form/UserOperatorForm";
+import {
+  UserFormData,
+  UserOperatorFormData,
+} from "@/app/components/form/formDataTypes";
 
 export async function getUserOperatorFormData(id: number) {
   return actionHandler(
@@ -11,23 +16,36 @@ export async function getUserOperatorFormData(id: number) {
   );
 }
 
+export async function getUser() {
+  return fetchAPI(`registration/user`);
+}
+
 export default async function UserOperator({
   params,
 }: {
-  params: { id: number };
+  params?: { id: number };
 }) {
-  const formData: UserOperatorFormData | { error: string } =
-    await getUserOperatorFormData(params.id);
+  const userOperatorProps: UserOperatorFormProps = {
+    schema: userOperatorSchema,
+  };
 
-  if ("error" in formData) {
-    return <div>Server Error. Please try again later.</div>;
+  const serverError = <div>Server Error. Please try again later.</div>;
+
+  if (params) {
+    // if params exist, then we are editing an existing userOperator
+    const formData: UserOperatorFormData = await getUserOperatorFormData(
+      params.id,
+    );
+    userOperatorProps.formData = formData;
+    userOperatorProps.userOperatorId = params.id;
+  } else {
+    // if params do not exist, then we are creating a new userOperator
+    const formData: UserFormData = await getUser();
+    userOperatorProps.formData = formData;
   }
 
-  return (
-    <UserOperatorForm
-      schema={userOperatorSchema}
-      formData={formData}
-      userOperatorId={params.id}
-    />
-  );
+  if (userOperatorProps.formData && "error" in userOperatorProps.formData)
+    return serverError;
+
+  return <UserOperatorForm {...userOperatorProps} />;
 }
