c059cdc42f1b66c6f05040dcdef8539df8dfd372
shon-button <shon@button.is>
2023-11-21 15:56:17 -0500

    🌸 chore: prettier

--- client/middlewares/withAuthorization.tsx
+++ client/middlewares/withAuthorization.tsx
@@ -8,17 +8,28 @@ import {
 import { MiddlewareFactory } from "./types";
 import { getToken } from "next-auth/jwt";
 
+/*
+Access control logic is managed using Next.js middleware and NextAuth.js authentication JWT token.
+The middleware intercepts requests, and for restricted areas...
+Checks for a valid user session, and extracts user information from the JWT token.
+Based on JWT properties of identity_provider and role, the middleware dynamically rewrites the request URL
+to the appropriate folder structure.
+ */
+
+// Function to check if the path is in the unauthenticated allow list
 const isUnauthenticatedAllowListedPath = (pathname: string): boolean => {
   const authList = ["auth", "unauth"];
   return authList.some((path) => pathname.includes(path));
 };
 
+// Function to check if the path is in the authenticated allow list
 const isAuthenticatedAllowListedPath = (pathname: string): boolean => {
   const allowList = ["dashboard", "problem", "profile"];
   const lastSegment = pathname.split("/").pop();
   return allowList.includes(lastSegment || "");
 };
 
+// Function to check if the path requires authorization
 const isAuthorizationRequiredPath = (
   pathname: string,
   token: { identity_provider?: string; app_role?: string },
@@ -28,8 +39,8 @@ const isAuthorizationRequiredPath = (
   }
 
   const idp = token.identity_provider;
-  const app_role = token.app_role;
-  const authRoute = `${idp}/${app_role}`;
+  const appRole = token.app_role;
+  const authRoute = `${idp}/${appRole}`;
 
   return (
     !pathname.includes(authRoute) &&
@@ -39,6 +50,7 @@ const isAuthorizationRequiredPath = (
   );
 };
 
+// Middleware for authorization
 export const withAuthorization: MiddlewareFactory = (next: NextMiddleware) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
     const { pathname } = request.nextUrl;
@@ -47,24 +59,30 @@ export const withAuthorization: MiddlewareFactory = (next: NextMiddleware) => {
       secret: process.env.NEXTAUTH_SECRET,
     });
 
+    // Check if the path is in the unauthenticated allow list
     if (isUnauthenticatedAllowListedPath(pathname)) {
       return next(request, _next);
     }
 
+    // Check if the user is authenticated
     if (token) {
+      // Redirect root or home requests to the dashboard
       if (pathname.endsWith("/") || pathname.endsWith("/home")) {
         return NextResponse.redirect(new URL(`/dashboard`, request.url));
       }
 
+      // Check if the path is in the authenticated allow list
       if (isAuthenticatedAllowListedPath(pathname)) {
         return next(request, _next);
       }
 
+      // Check if the path requires authorization
       if (isAuthorizationRequiredPath(pathname, token)) {
         request.nextUrl.pathname = `${token.identity_provider}/${token.app_role}${pathname}`;
         return NextResponse.rewrite(request.nextUrl);
       }
 
+      // Routes with the folder structure break the breadcrumbs
       const pageSegment = pathname.replace(
         `/${token.identity_provider}/${token.app_role}`,
         "",
@@ -72,6 +90,7 @@ export const withAuthorization: MiddlewareFactory = (next: NextMiddleware) => {
 
       return NextResponse.redirect(new URL(`${pageSegment}`, request.url));
     } else {
+      // Handle unauthenticated requests
       if (pathname.endsWith("/home")) {
         return next(request, _next);
       } else {
