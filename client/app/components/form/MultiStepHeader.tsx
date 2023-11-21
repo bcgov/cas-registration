96591945953baea0ef5e49ac5edd3d4d636a6ca4
marcellmueller <marcel@button.is>
2023-11-20 16:06:13 -0800

    refactor: update multistep form base

--- client/app/components/form/MultiStepHeader.tsx
+++ client/app/components/form/MultiStepHeader.tsx
@@ -7,7 +7,7 @@ interface MultiStepHeaderProps {
 
 const MultiStepHeader = ({ step, steps }: MultiStepHeaderProps) => {
   return (
-    <div className="block sm:flex flex-row my-10 justify-between w-full">
+    <div className="block sm:flex flex-row mt-10 mb-6 justify-between w-full">
       {steps.map((s, i) => {
         const isLastStep = i === steps.length - 1;
         const bgColor =
@@ -20,7 +20,7 @@ const MultiStepHeader = ({ step, steps }: MultiStepHeaderProps) => {
             className={`mb-4 flex flex-row items-center ${
               isLastStep ? "grow-0" : "grow"
             }`}
-            key={step}
+            key={steps[i]}
           >
             <div
               key={s}
@@ -28,7 +28,7 @@ const MultiStepHeader = ({ step, steps }: MultiStepHeaderProps) => {
             >
               {i + 1}
             </div>
-            <div className="ml-4 h-min">{steps[i]}</div>
+            <div className={`ml-4 h-min `}>{steps[i]}</div>
             {!isLastStep && (
               <div className="hidden sm:block mx-4 grow">
                 <hr className="border-black" />
