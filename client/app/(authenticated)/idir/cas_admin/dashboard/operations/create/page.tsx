b4a2c33b1d96b2f4377e5d88d17907006f4f01c9
shon-button <shon@button.is>
2023-11-22 09:14:08 -0500

    🧹 cleanup: role routes and  spelling

--- client/app/(authenticated)/idir/cas_admin/dashboard/operations/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import OperationsCreatePage from "@/app/components/routes/operations/create/Page";
-
-export default async function Page() {
-  return <OperationsCreatePage />;
-}
--- client/app/(authenticated)/idir/cas_admin/dashboard/select-operator/(request-access)/received/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SelectOperatorRequestAccessReceivedPage from "@/app/components/routes/select-operator/request-access/received/Page";
-
-export default async function Page({
-  params,
-}: {
-  params: Readonly<{ id: number }>;
-}) {
-  return <SelectOperatorRequestAccessReceivedPage params={params} />;
-}
