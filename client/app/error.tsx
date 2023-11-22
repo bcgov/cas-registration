73830d67d4786c975c5d655711f637e3691fe2e7
marcellmueller <marcel@button.is>
2023-11-22 08:48:47 -0800

    refactor: move use effect inside error component

--- client/app/error.tsx
+++ client/app/error.tsx
@@ -1,6 +1,5 @@
 "use client"; // Error components must be Client Components
 
-import { useEffect } from "react";
 import ErrorComponent from "@/app/components/Error";
 
 export default function ErrorBoundary({
@@ -8,12 +7,7 @@ export default function ErrorBoundary({
   reset,
 }: {
   error: Error & { digest?: string };
-  reset: any;
+  reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
   return <ErrorComponent error={error} reset={reset} />;
 }
