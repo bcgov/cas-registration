2cb92064fee31a3ae15ad81deb6a94ea10a599c1
Dylan Leard <dylan@button.is>
2023-11-21 17:20:59 -0800

    chore: add generic actionHandler() function to actions.ts

--- client/app/utils/actions.ts
+++ client/app/utils/actions.ts
@@ -10,6 +10,8 @@ In a separate file (Client and Server Components), for reusability.
 "use server";
 import { revalidatePath } from "next/cache";
 import { OperationsFormData } from "@/app/components/form/OperationsForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export const operationSubmitHandler = async (
   formData: OperationsFormData,
@@ -75,3 +77,85 @@ export const createSubmitHandler = async (
     return { error: err.message };
   }
 };
+
+export const requestAccessHandler = async (
+  operatorId: number
+) => {
+  try {
+    const response: Response = await fetch(
+      process.env.API_URL + "registration/select-operator/request-access", {
+        method: "POST",
+        body: JSON.stringify({"operator_id": operatorId}),
+      },
+    );
+    if (!response.ok) {
+      throw new Error("Failed to save data.");
+    }
+    revalidatePath(`/dashboard/select-operator/confirm/${operatorId}`);
+    return await response.json();
+  } catch (err: any) {
+    return { error: err.message };
+  }
+};
+
+
+export async function actionHandler(
+  endpoint: string,
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH",
+  pathToRevalidate: string,
+  options: RequestInit = {},
+  latency: number = 0, // Default latency is 0 milliseconds,
+
+) {
+  try {
+    // Simulate latency
+    if (latency > 0) await new Promise((r) => setTimeout(r, latency));
+
+    const session = await getServerSession(authOptions);
+
+    const defaultOptions: RequestInit = {
+      cache: "no-store", // Default cache option
+      method,
+      headers: new Headers({
+        'Authorization': JSON.stringify({
+        'user_guid': session?.user?.user_guid
+        })
+      })
+    };
+
+    const mergedOptions: RequestInit = {
+      ...defaultOptions,
+      ...options, // Merge the provided options, allowing cache to be overridden
+    };
+
+    const response = await fetch(
+      `${process.env.API_URL}${endpoint}`,
+      mergedOptions,
+    );
+
+    if (!response.ok) {
+      // Handle HTTP errors, e.g., response.status is not in the 200-299 range
+      return { error: `HTTP error! Status: ${response.status}` };
+    }
+
+    const data = await response.json();
+    revalidatePath(pathToRevalidate);
+    return data;
+  } catch (error: unknown) {
+    // Handle any errors, including network issues
+    if (error instanceof Error) {
+      // eslint-disable-next-line no-console
+      console.error(`An error occurred while fetching ${endpoint}:`, error);
+      return {
+        // eslint-disable-next-line no-console
+        error: `An error occurred while fetching ${endpoint}: ${error.message}`,
+      };
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(`An unknown error occurred while fetching ${endpoint}`);
+      return {
+        error: `An unknown error occurred while fetching ${endpoint}`,
+      };
+    }
+  }
+}
