bf4d0709ea1d0895d420cc9d30fd3620cbbb33c2
marcellmueller <marcel@button.is>
2023-11-23 15:23:19 -0800

    feat: add rjsf phone widget

--- client/app/components/form/widgets/index.ts
+++ client/app/components/form/widgets/index.ts
@@ -1,6 +1,7 @@
 export { default as ComboBox } from "./ComboBox";
 export { default as EmailWidget } from "./EmailWidget";
 export { default as FileWidget } from "./FileWidget";
+export { default as PhoneWidget } from "./PhoneWidget";
 export { default as RadioWidget } from "./RadioWidget";
 export { default as SelectWidget } from "./SelectWidget";
 export { default as TextWidget } from "./TextWidget";
