27f7146f88da2e1e4841f17bfc364a14c1ee0bcb
SeSo <sobhani.sepehr@gmail.com>
2023-11-27 17:28:47 -0800

    chore: update pages with new params after rebase

--- client/app/(authenticated)/idir/cas_admin/dashboard/select-operator/(request-access)/user-operator/[id]/page.tsx
+++ client/app/(authenticated)/idir/cas_admin/dashboard/select-operator/(request-access)/user-operator/[id]/page.tsx
@@ -1,11 +1,5 @@
 import SelectOperatorRequestAccessReceivedUserOperatorPage from "@/app/components/routes/select-operator/request-access/user-operator/Page";
 
-export default async function Page({
-  params,
-}: {
-  params: Readonly<{ id: number }>;
-}) {
-  return (
-    <SelectOperatorRequestAccessReceivedUserOperatorPage params={params} />
-  );
+export default async function Page() {
+  return <SelectOperatorRequestAccessReceivedUserOperatorPage />;
 }
