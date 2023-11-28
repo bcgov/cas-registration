c808db086cda59cda40b3ca9ad6f44a7d8e156d7
Dylan Leard <dylan@button.is>
2023-11-28 11:32:35 -0800

    chore: update endpoints

--- bc_obps/registration/api/operator.py
+++ bc_obps/registration/api/operator.py
@@ -27,6 +27,37 @@ def get_operator(request, operator_id: int):
 ##### POST #####
 
 
+@router.post("/select-operator/request-admin-access", response={201: RequestAccessOut, codes_4xx: Message})
+def request_access(request, payload: SelectOperatorIn):
+    user: User = request.current_user
+    payload_dict: dict = payload.dict()
+    operator: Operator = get_object_or_404(Operator, id=payload_dict.get("operator_id"))
+
+    # check if user is eligible to request access
+    status, message = check_users_admin_request_eligibility(user, operator)
+    if status != 200:
+        return status, message
+
+    # Making a draft UserOperator instance if one doesn't exist
+    user_operator, _ = UserOperator.objects.get_or_create(
+        user=user, operator=operator, role=UserOperator.Roles.ADMIN, status=UserOperator.Statuses.DRAFT
+    )
+    return 201, {"user_operator_id": user_operator.id}
+
+@router.post("/select-operator/request-access", response={201: RequestAccessOut, codes_4xx: Message})
+def request_access(request, payload: SelectOperatorIn):
+    current_user_guid = json.loads(request.headers.get('Authorization'))["user_guid"]
+    user: User = get_object_or_404(User, user_guid=current_user_guid)
+    payload_dict: dict = payload.dict()
+    operator: Operator = get_object_or_404(Operator, id=payload_dict.get("operator_id"))
+
+    # Making a draft UserOperator instance if one doesn't exist
+    user_operator, _ = UserOperator.objects.get_or_create(
+        user=user, operator=operator, status=UserOperator.Statuses.DRAFT
+    )
+    return 201, {"user_operator_id": user_operator.id}
+
+
 ##### PUT #####
 
 
