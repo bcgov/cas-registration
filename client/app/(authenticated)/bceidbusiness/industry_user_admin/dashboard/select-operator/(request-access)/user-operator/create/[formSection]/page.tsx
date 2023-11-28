b0ad49555b86f0c9c68c9f40be0b69e30e9c7385
SeSo <sobhani.sepehr@gmail.com>
2023-11-28 09:40:20 -0800

    chore: relocate files and remove redundant ones after rebase

---
+++ client/app/(authenticated)/bceidbusiness/industry_user_admin/dashboard/select-operator/(request-access)/user-operator/create/[formSection]/page.tsx
@@ -0,0 +1,5 @@
+import SelectOperatorRequestAccessReceivedUserOperatorPage from "@/app/components/routes/select-operator/request-access/user-operator/Page";
+
+export default async function Page() {
+  return <SelectOperatorRequestAccessReceivedUserOperatorPage />;
+}
