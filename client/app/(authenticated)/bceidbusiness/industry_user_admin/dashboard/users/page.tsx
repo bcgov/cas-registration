1b928a4361a2d341ecf79e29c6f007b8cf4d0879
Josh Gamache <joshua@button.is>
2023-12-01 12:00:45 -0700

    feat: add data fetch and pre-formatted display to user's page

--- client/app/(authenticated)/bceidbusiness/industry_user_admin/dashboard/users/page.tsx
+++ client/app/(authenticated)/bceidbusiness/industry_user_admin/dashboard/users/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <h1>TO DO: Users Page</h1>;
+import { actionHandler } from "@/app/utils/actions";
+
+// 🛠️ Function to fetch userOperators
+async function getUserOperatorsForOperator() {
+  try {
+    return await actionHandler(
+      "registration/operators/2/user-operators",
+      "GET",
+      "/dashboard/users",
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default async function Page() {
+  const raw = await getUserOperatorsForOperator();
+
+  return (
+    <>
+      <h1>TO DO: Users Page</h1>
+      <pre>{JSON.stringify(raw, null, 2)}</pre>
+    </>
+  );
 }
