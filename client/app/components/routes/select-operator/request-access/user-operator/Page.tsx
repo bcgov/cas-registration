b26e8f4e1b716b6c1b4c70b3a3452b84e868ef7b
SeSo <sobhani.sepehr@gmail.com>
2023-12-06 10:41:57 -0800

    chore: refactor user-operator page to accept params

--- client/app/components/routes/select-operator/request-access/user-operator/Page.tsx
+++ client/app/components/routes/select-operator/request-access/user-operator/Page.tsx
@@ -2,10 +2,14 @@ import { Suspense } from "react";
 import Loading from "@/app/components/loading/SkeletonGrid";
 import UserOperator from "@/app/components/routes/select-operator/form/UserOperator";
 
-export default async function SelectOperatorRequestAccessReceivedUserOperatorPage() {
+export default async function SelectOperatorRequestAccessReceivedUserOperatorPage({
+  params,
+}: {
+  readonly params?: Readonly<{ id?: number }>;
+}) {
   return (
     <Suspense fallback={<Loading />}>
-      <UserOperator />
+      <UserOperator params={params} />
     </Suspense>
   );
 }
