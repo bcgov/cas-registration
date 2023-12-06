5afca491261df779b11aeac3587f2d805959204d
Josh Gamache <joshua@button.is>
2023-12-06 02:29:03 -0700

    feat: add action button to put status change to api

--- client/app/components/datagrid/ChangeUserOperatorStatusColumnCell.tsx
+++ client/app/components/datagrid/ChangeUserOperatorStatusColumnCell.tsx
@@ -2,7 +2,14 @@
 
 import { GridRenderCellParams } from "@mui/x-data-grid/models/params/gridCellParams";
 import Button from "@mui/material/Button";
+import { actionHandler } from "@/app/utils/actions";
+
 type UserOperatorStatus = "draft" | "pending" | "approved" | "rejected";
+
+interface UserOperatorStatusAction {
+  statusTo: UserOperatorStatus;
+  title: string;
+}
 
 interface ButtonRenderCellParams extends GridRenderCellParams {
   row: {
@@ -16,19 +23,45 @@ interface ButtonRenderCellParams extends GridRenderCellParams {
   };
 }
 
+const handleUpdateStatus = async (
+  userOperatorId: string,
+  statusUpdate: UserOperatorStatus,
+) => {
+  try {
+    return await actionHandler(
+      `registration/select-operator/user-operator/${userOperatorId}/update-status`,
+      "PUT",
+      "/dashboard/users",
+      {
+        body: JSON.stringify({
+          status: statusUpdate,
+        }),
+      },
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
 export async function ChangeUserOperatorStatusColumnCell(
   params: ButtonRenderCellParams,
 ) {
   const userOperatorStatus = params.row.status;
+  const userOperatorId = params.row.id;
 
-  const buttonsToShow = (status: UserOperatorStatus): string[] => {
+  const buttonsToShow = (
+    status: UserOperatorStatus,
+  ): UserOperatorStatusAction[] => {
     if (status.toLowerCase() === "pending") {
-      return ["Approve", "Deny"];
+      return [
+        { statusTo: "approved", title: "Approve" },
+        { statusTo: "rejected", title: "Deny" },
+      ];
     } else if (
       status.toLowerCase() === "approved" ||
       status.toLowerCase() === "rejected"
     ) {
-      return ["Undo"];
+      return [{ statusTo: "pending", title: "Undo" }];
     }
     return [];
   };
@@ -36,8 +69,14 @@ export async function ChangeUserOperatorStatusColumnCell(
   return (
     <>
       {buttonsToShow(userOperatorStatus).map((item, index) => (
-        <Button variant="outlined" key={index}>
-          {item}
+        <Button
+          variant="outlined"
+          key={index}
+          onClick={async () =>
+            handleUpdateStatus(userOperatorId, item.statusTo)
+          }
+        >
+          {item.title}
         </Button>
       ))}
     </>
