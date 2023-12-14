bcba2474054aaa01fac0be663c7268b94fce7fea
shon-button <shon@button.is>
2023-12-13 20:01:14 -0500

    fix: enums architecture

--- bc_obps/registration/api/user.py
+++ bc_obps/registration/api/user.py
@@ -6,7 +6,8 @@ from django.shortcuts import get_object_or_404
 from .api_base import router
 from django.forms import model_to_dict
 from ninja.responses import codes_4xx
-
+from registration.enums.enums import IdPs
+from registration.enums.enums import Roles
 
 ##### GET #####
 
@@ -44,7 +45,7 @@ def get_user_role(request, user_guid: str):
 def create_user_profile(request, identity_provider: str, payload: UserIn):
     try:
         # Determine the role based on the identity provider
-        role_mapping = {User.IdPs.IDIR: User.Roles.CAS_PENDING, User.IdPs.BCEIDBUSINESS: User.Roles.INDUSTRY_USER}
+        role_mapping = {IdPs.IDIR.value: Roles.CAS_PENDING.value, IdPs.BCEIDBUSINESS.value: User.Roles.INDUSTRY_USER.value}
         role = role_mapping.get(identity_provider, None)
 
         new_user = User.objects.create(
