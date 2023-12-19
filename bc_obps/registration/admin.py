30bfeb75e13143346c53d12e9f6b649699033bd1
SeSo <sobhani.sepehr@gmail.com>
2023-12-19 10:25:44 -0800

    chore: add `OperationAdmin` to admin.py

--- bc_obps/registration/admin.py
+++ bc_obps/registration/admin.py
@@ -21,8 +21,14 @@ admin.site.register(User)
 admin.site.register(Contact)
 admin.site.register(Operator)
 admin.site.register(UserOperator)
-admin.site.register(Operation)
 admin.site.register(ParentChildOperator)
 admin.site.register(RegulatedProduct)
 admin.site.register(ReportingActivity)
 admin.site.register(MultipleOperator)
+
+
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'operator', 'name', 'type', 'status', 'created_at', 'updated_at', 'archived_at')
+    search_fields = ('id', 'operator', 'name', 'operation_type', 'status', 'created_at', 'updated_at', 'archived_at')
+    ordering = ('id',)
