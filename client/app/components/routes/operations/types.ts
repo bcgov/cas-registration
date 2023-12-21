013a4972ba9f499f408078a27e244fa5c97c5c26
SeSo <sobhani.sepehr@gmail.com>
2023-12-20 16:42:22 -0800

    chore: add `Operation` interface

---
+++ client/app/components/routes/operations/types.ts
@@ -0,0 +1,30 @@
+export interface Operation {
+  id: number;
+  name: string;
+  type: string;
+  created_by: string;
+  created_at: string;
+  updated_by?: string;
+  updated_at?: string;
+  archived_by?: string;
+  archived_at?: string;
+  naics_code: number;
+  previous_year_attributable_emissions?: string;
+  swrs_facility_id?: string;
+  bcghg_id?: string;
+  opt_in?: boolean;
+  operator: number;
+  operation_has_multiple_operators: boolean;
+  verified_at?: string;
+  verified_by?: string;
+  application_lead?: any; // Change this once we have the Contact type
+  status: string;
+  bc_obps_regulated_operation?: number;
+  documents: Array<any>; // Change this once we have the Document type
+  regulated_products: Array<any>; // Change this once we have the RegulatedProduct type
+  reporting_activities: Array<any>; // Change this once we have the ReportingActivity type
+  operator_id: number;
+  naics_code_id: number;
+  is_application_lead_external?: boolean;
+  multiple_operators_array?: Array<any>; // Change this once we have the MultipleOperator type
+}
