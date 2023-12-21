7d1bcca5bd40c811035ebaa8f6bf807ee6a9ecc3
JoshLarouche <joshua.t.larouche@gmail.com>
2023-12-20 17:12:00 -0800

    Chore: addressed remaining PR comments

--- client/app/(authenticated)/idir/cas_admin/dashboard/operators/page.tsx
+++ client/app/(authenticated)/idir/cas_admin/dashboard/operators/page.tsx
@@ -1,15 +1,16 @@
 import { Suspense } from "react";
 import AccessRequests from "@/app/components/routes/access-requests/AccessRequests";
 import Loading from "@/app/components/loading/SkeletonGrid";
+import Note from "@/app/components/datagrid/Note";
 
 export default function Page() {
   return (
     <>
-      <p>
-        <b>Note:</b> Once “Approved”, the user will have access to their
-        operator dashboard with full admin permissions, and can grant access and
-        designate permissions to other Business BCeID holders there.
-      </p>
+      <Note
+        message="Once “Approved”, the user will have access to their
+      operator dashboard with full admin permissions, and can grant access and
+      designate permissions to other Business BCeID holders there."
+      />
       <Suspense fallback={<Loading />}>
         <AccessRequests />
       </Suspense>
