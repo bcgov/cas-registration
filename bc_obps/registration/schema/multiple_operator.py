54fef2cdf4a7bd28c8817032a11f8faedc397ad9
SeSo <sobhani.sepehr@gmail.com>
2023-12-29 13:58:54 -0800

    chore: refactor`MultipleOperatorOut` and remove unused schema

--- bc_obps/registration/schema/multiple_operator.py
+++ bc_obps/registration/schema/multiple_operator.py
@@ -1,32 +1,7 @@
 from typing import Optional
-from registration.schema.address import AddressSchema
-from ninja import Field, Schema, ModelSchema
+from ninja import Field, ModelSchema
 from registration.models import MultipleOperator
-
-### Multiple Operator schemas
-
-
-class MultipleOperatorIn(ModelSchema):
-    operation_id: int
-    mo_legal_name: str
-    mo_trade_name: str
-    mo_cra_business_number: int
-    mo_bc_corporate_registry_number: str = Field(regex=r"^[A-Za-z]{1,3}\d{7}$")
-    mo_business_structure: str
-    mo_website: Optional[str]
-    mo_physical_street_address: str
-    mo_physical_municipality: str
-    mo_physical_province: str
-    mo_physical_postal_code: str
-    mo_mailing_address_same_as_physical: Optional[bool] = False
-    mo_mailing_street_address: Optional[str]
-    mo_mailing_municipality: Optional[str]
-    mo_mailing_province: Optional[str]
-    mo_mailing_postal_code: Optional[str]
-
-    class Config:
-        model = MultipleOperator
-        model_fields = "__all__"
+from registration.utils import AUDIT_FIELDS
 
 
 class MultipleOperatorOut(ModelSchema):
@@ -42,8 +17,16 @@ class MultipleOperatorOut(ModelSchema):
     )
     mo_business_structure: str = Field(..., alias="business_structure")
     mo_website: Optional[str] = Field(None, alias="website")
-    mo_percentage_ownership: int = Field(None, alias="percentage_ownership")
+    mo_percentage_ownership: float = Field(None, alias="percentage_ownership")
+    mo_physical_street_address: str = Field(..., alias="physical_address.street_address")
+    mo_physical_municipality: str = Field(..., alias="physical_address.municipality")
+    mo_physical_province: str = Field(..., alias="physical_address.province")
+    mo_physical_postal_code: str = Field(..., alias="physical_address.postal_code")
     mo_mailing_address_same_as_physical: Optional[bool] = Field(False, alias="mailing_address_same_as_physical")
+    mo_mailing_street_address: Optional[str] = Field(None, alias="mailing_address.street_address")
+    mo_mailing_municipality: Optional[str] = Field(None, alias="mailing_address.municipality")
+    mo_mailing_province: Optional[str] = Field(None, alias="mailing_address.province")
+    mo_mailing_postal_code: Optional[str] = Field(None, alias="mailing_address.postal_code")
 
     @staticmethod
     def resolve_business_structure(mo: MultipleOperator):
@@ -51,4 +34,16 @@ class MultipleOperatorOut(ModelSchema):
 
     class Config:
         model = MultipleOperator
-        model_fields = "__all__"
+        model_exclude = [
+            *AUDIT_FIELDS,
+            # exclude the following fields since they are handled by the aliases above
+            "legal_name",
+            "trade_name",
+            "cra_business_number",
+            "bc_corporate_registry_number",
+            "business_structure",
+            "physical_address",
+            "mailing_address",
+            "percentage_ownership",
+            "website",
+        ]
