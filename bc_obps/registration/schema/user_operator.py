218b7d3a6814dd3d526607d8e946cedc56eb1236
SeSo <sobhani.sepehr@gmail.com>
2023-12-29 14:10:12 -0800

    chore: using resolvers for `UserOperatorOut` schema in

--- bc_obps/registration/schema/user_operator.py
+++ bc_obps/registration/schema/user_operator.py
@@ -1,10 +1,8 @@
 from typing import Optional
 import uuid
-from django.forms import UUIDField
 from ninja import ModelSchema, Schema, Field
 from registration.models import Contact, User, UserOperator
-from pydantic import Field
-from ninja import ModelSchema, Schema, Field
+from registration.utils import AUDIT_FIELDS
 
 
 class UserOperatorStatus(Schema):
@@ -23,7 +21,7 @@ class RequestAccessOut(Schema):
     user_operator_id: int
 
 
-class UserOperatorOut(Schema):
+class UserOperatorOut(ModelSchema):
     """
     Custom schema for the user operator form
     """
@@ -34,27 +32,81 @@ class UserOperatorOut(Schema):
     street_address: str
     municipality: str
     postal_code: str
-    email: str
-    phone_number: str
     province: str
-    legal_name: str
-    trade_name: Optional[str]
-    cra_business_number: Optional[int]
-    bc_corporate_registry_number: Optional[str] = Field(regex=r"^[A-Za-z]{1,3}\d{7}$")
-    business_structure: str
-    physical_street_address: str
-    physical_municipality: str
-    physical_province: str
-    physical_postal_code: str
-    # below are optional fields because we might use physical address as mailing address
-    mailing_street_address: Optional[str]
-    mailing_municipality: Optional[str]
-    mailing_province: Optional[str]
-    mailing_postal_code: Optional[str]
-    website: Optional[str]
-    status: str
-    user_operator_status: str
-    role: str
+    email: str = Field(..., alias="user.email")
+    phone_number: str
+    so_email: str
+    so_phone_number: str
+    legal_name: str = Field(..., alias="operator.legal_name")
+    trade_name: Optional[str] = Field("", alias="operator.trade_name")
+    cra_business_number: Optional[int] = Field(None, alias="operator.cra_business_number")
+    bc_corporate_registry_number: str = Field(
+        ..., regex=r"^[A-Za-z]{1,3}\d{7}$", alias="operator.bc_corporate_registry_number"
+    )
+    business_structure: str = Field(..., alias="operator.business_structure.name")
+    physical_street_address: str = Field(..., alias="operator.physical_address.street_address")
+    physical_municipality: str = Field(..., alias="operator.physical_address.municipality")
+    physical_province: str = Field(..., alias="operator.physical_address.province")
+    physical_postal_code: str = Field(..., alias="operator.physical_address.postal_code")
+    mailing_street_address: str = Field(..., alias="operator.mailing_address.street_address")
+    mailing_municipality: str = Field(..., alias="operator.mailing_address.municipality")
+    mailing_province: str = Field(..., alias="operator.mailing_address.province")
+    mailing_postal_code: str = Field(..., alias="operator.mailing_address.postal_code")
+    website: Optional[str] = Field("", alias="operator.website")
+    is_senior_officer: bool
+    mailing_address_same_as_physical: bool
+
+    @staticmethod
+    def resolve_is_senior_officer(obj: UserOperator):
+        return obj.user_is_senior_officer()
+
+    @staticmethod
+    def resolve_mailing_address_same_as_physical(obj: UserOperator):
+        return obj.operator.mailing_address.id == obj.operator.physical_address.id
+
+    @staticmethod
+    def resolve_first_name(obj: UserOperator):
+        return obj.get_senior_officer().first_name if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_last_name(obj: UserOperator):
+        return obj.get_senior_officer().last_name if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_position_title(obj: UserOperator):
+        return obj.get_senior_officer().position_title if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_street_address(obj: UserOperator):
+        return obj.get_senior_officer().address.street_address if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_municipality(obj: UserOperator):
+        return obj.get_senior_officer().address.municipality if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_postal_code(obj: UserOperator):
+        return obj.get_senior_officer().address.postal_code if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_province(obj: UserOperator):
+        return obj.get_senior_officer().address.province if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_so_email(obj: UserOperator):
+        return obj.get_senior_officer().email if obj.get_senior_officer() else ""
+
+    @staticmethod
+    def resolve_phone_number(obj: UserOperator):
+        return str(obj.user.phone_number) if obj.user.phone_number else ""
+
+    @staticmethod
+    def resolve_so_phone_number(obj: UserOperator):
+        return str(obj.get_senior_officer().phone_number) if obj.get_senior_officer() else ""
+
+    class Config:
+        model = UserOperator
+        model_fields = ["role", "status"]
 
 
 class UserOperatorOperatorIn(Schema):
