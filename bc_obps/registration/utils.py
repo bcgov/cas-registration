93877b218fe3c3bfa1a1ff1e1de28e6752faeba1
SeSo <sobhani.sepehr@gmail.com>
2023-12-29 13:22:22 -0800

    chore: audit fields constant to exclude from schemas

--- bc_obps/registration/utils.py
+++ bc_obps/registration/utils.py
@@ -15,6 +15,16 @@ from registration.models import (
 
 UNAUTHORIZED_MESSAGE = "Unauthorized."
 
+# Used to exclude audit fields from the schema
+AUDIT_FIELDS = [
+    "created_at",
+    "created_by",
+    "updated_at",
+    "updated_by",
+    "archived_at",
+    "archived_by",
+]
+
 
 def check_users_admin_request_eligibility(user: User, operator: Operator) -> Union[None, tuple[int, dict]]:
     """
