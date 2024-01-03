f82df9f77b315c7bc5d6bbab12a7819bfd9323c9
Brianna Cerkiewicz <brianna@button.is>
2024-01-03 08:27:47 -0800

    feat: cas users can approve/deny operator and users separately

--- client/app/components/form/formDataTypes.ts
+++ client/app/components/form/formDataTypes.ts
@@ -49,6 +49,9 @@ export interface UserOperatorFormData extends UserFormData {
   percentage_owned_by_parent_company?: number;
   // Not in form, but needed for API to create a contact based on the existing user-operator
   user_operator_id?: string;
+  user_operator_status?: string;
+  is_new: boolean;
+  operator_id: number;
 }
 export interface UserProfileFormData {
   first_name: string;
