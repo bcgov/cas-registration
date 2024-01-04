d7abbea2e10d40ca18fb8157b6757a87e3a534f6
Dylan Leard <dylan@button.is>
2024-01-04 09:44:06 -0800

    fix: use glob pattern to reach nested fixtures

--- bc_obps/registration/api/api_base.py
+++ bc_obps/registration/api/api_base.py
@@ -1,3 +1,6 @@
+from glob import glob
+import os
+from bc_obps.settings import BASE_DIR
 from ninja import Router
 from django.core.management import call_command
 from django.conf import settings
@@ -12,7 +15,7 @@ def setup(request):
     if settings.ENVIRONMENT == "develop":
         try:
             call_command('truncate_all_tables')
-            call_command('load_fixtures')
+            call_command('loaddata', glob(os.path.join(BASE_DIR,'registration/fixtures/mock/*.json')), glob(os.path.join(BASE_DIR,'registration/fixtures/real/*.json')))
             return HttpResponse("Test setup complete.", status=200)
         except Exception as e:
             return HttpResponse(f"Test setup failed. Reason:{e}", status=500)
