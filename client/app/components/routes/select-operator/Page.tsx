57eba87d771c822a5415f945580924e9e0bbc79a
marcellmueller <marcel@button.is>
2024-01-05 10:46:19 -0800

    fix: select operator pending status check

--- client/app/components/routes/select-operator/Page.tsx
+++ client/app/components/routes/select-operator/Page.tsx
@@ -24,7 +24,7 @@ export default async function MyOperatorPage() {
   const session = await getServerSession(authOptions);
   const userName = session?.user?.name?.split(" ")?.[0];
   const { status } = await getUserOperatorStatus();
-  if (status === Status.PENDING.toLowerCase()) {
+  if (status === Status.PENDING) {
     return <div>Your request is pending.</div>;
   }
   return (
