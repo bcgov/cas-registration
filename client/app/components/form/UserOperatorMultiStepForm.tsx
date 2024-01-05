3d2a52517cfc11273323249dc03e27b19c194404
marcellmueller <marcel@button.is>
2024-01-05 11:38:06 -0800

    fix: revalidation error

--- client/app/components/form/UserOperatorMultiStepForm.tsx
+++ client/app/components/form/UserOperatorMultiStepForm.tsx
@@ -61,7 +61,7 @@ export default function UserOperatorMultiStepForm({
     const response = await actionHandler(
       `${apiUrl}${!isCreate && !isFinalStep ? `/${userOperatorId}` : ""}`,
       isCreate || isFinalStep ? "POST" : "PUT",
-      `/dashboard/select-operator/user-operator/create/${params?.formSection}`,
+      "",
       {
         body: JSON.stringify(newFormData),
       }
