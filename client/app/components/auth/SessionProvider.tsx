5abce6b7b8150f9fe67b2c691b5c83539eb1cac4
shon-button <shon@button.is>
2024-01-05 15:57:14 -0500

    fix: useSession missing props

--- client/app/components/auth/SessionProvider.tsx
+++ client/app/components/auth/SessionProvider.tsx
@@ -2,8 +2,8 @@
 /*
 📚 Nextauth provider:
 Import NextAuth.js SessionProvider as a client component because it uses context
-Then export the client SessionProvider be used in server component app\layout.tsx
-This enables sharing session state as context throught the application
+Then export the client SessionProvider to be used in server component client\layout.tsx
+This enables sharing session state as context throughout the application
 */
 "use client";
 import { SessionProvider } from "next-auth/react";
