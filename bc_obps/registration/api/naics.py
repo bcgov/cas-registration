b4c6737e10e0b9c81600568f7eb1c9a13bd4c0c3
Brianna Cerkiewicz <brianna@button.is>
2024-01-08 11:49:59 -0800

    chore: spec work on putting file back into rjsf

--- bc_obps/registration/api/naics.py
+++ bc_obps/registration/api/naics.py
@@ -2,7 +2,7 @@ from registration.decorators import authorize
 from .api_base import router
 from django.core.files.base import ContentFile
 from typing import List
-from registration.models import NaicsCode, AppRole, Document,  DocumentType
+from registration.models import NaicsCode, AppRole, Document, DocumentType
 from registration.schema import (
     NaicsCodeSchema,
 )
@@ -19,36 +19,28 @@ from ninja import NinjaAPI, File
 from ninja.files import UploadedFile
 import re
 import requests
-class DocumentOut(ModelSchema):
 
+
+class DocumentOut(ModelSchema):
 
     """
     Schema for the Operator model
     """
+
     boundary_map: str = None
 
-    # static method, start with resolve, name of field
     @staticmethod
     def resolve_boundary_map(obj: Document):
-        # if obj.file.file:
-        #     breakpoint()
-        #     file_content = obj.file.file
-        #     encoded_content = base64.b64encode(file_content).decode("utf-8")
-
-        #     return "data:application/pdf;base64," + encoded_content
-        
 
         response = requests.get(obj.file.url)
         if response.status_code == 200:
             # The file content is in response.content
             file_content = response.content
             encoded_content = base64.b64encode(file_content).decode("utf-8")
-            # data:image/jpeg;name=IMG-20231228-WA0000.jpg;base64 data:application/pdf;base64
-            var = "data:application/pdf;name=" + obj.file.name.split("/")[-1] + ";base64," + encoded_content
-            print(var)
-            return var
-        return 'i dont work yet'
-        
+            # on
+            return "data:application/pdf;name=" + obj.file.name.split("/")[-1] + ";base64," + encoded_content
+        return None
+
     class Config:
         model = Document
         model_fields = '__all__'
@@ -71,26 +63,32 @@ class DocumentSchema(Schema):
 
 #     return 201, document
 
+
 @router.get("/handle-file", response=DocumentOut)
 def get_file(request):
     qs = Document.objects.last()
     return qs
 
+
 @router.post("/handle-file", response={201: DocumentOut, codes_4xx: Message})
 def create_document(request, payload: DocumentSchema):
 
-# for the real endpoint, we'll probably have to do some sort of loop because there can be multiple types of documents in each form
+    # for the real endpoint, we'll probably have to do some sort of loop because there can be multiple types of documents in each form
 
-    file_name =  re.search(r'name=([^;]+)', payload.boundary_map).group(1)
+
+    # we could do this in a schema resolver too
+    file_name = re.search(r'name=([^;]+)', payload.boundary_map).group(1)
     _, encoded_data = payload.boundary_map.split(',')
 
     # Decode the base64-encoded data
     file_data = base64.b64decode(encoded_data)
     file = ContentFile(file_data, file_name)
 
- 
-    document = Document.objects.create(file=file,  type=DocumentType.objects.get(name='boundary_map'), description='what was this meant to be? maybe it would make more sense to put the description in the DocumentType table?')
-
+    document = Document.objects.create(
+        file=file,
+        type=DocumentType.objects.get(name='boundary_map'),
+        description='what was this meant to be? maybe it would make more sense to put the description in the DocumentType table?',
+    )
 
     return 201, document
 
