477e9bfb24f51ac3b9dd9d39ed99f8b298b37259
marcellmueller <marcel@button.is>
2024-01-08 11:29:34 -0800

    feat: update status enums

--- bc_obps/registration/models.py
+++ bc_obps/registration/models.py
@@ -343,7 +343,7 @@ class Operator(TimeStampedModel):
         PENDING = "Pending"
         APPROVED = "Approved"
         REJECTED = "Rejected"
-        CHANGES = "Changes Requested"
+        CHANGES_REQUESTED = "Changes Requested"
 
     legal_name = models.CharField(max_length=1000, db_comment="The legal name of an operator")
     trade_name = models.CharField(max_length=1000, blank=True, db_comment="The trade name of an operator")
@@ -465,7 +465,7 @@ class UserOperator(TimeStampedModel):
         PENDING = "Pending"
         APPROVED = "Approved"
         REJECTED = "Rejected"
-        CHANGES = "Changes Requested"
+        CHANGES_REQUESTED = "Changes Requested"
 
     user = models.ForeignKey(
         User,
@@ -587,7 +587,7 @@ class Operation(OperationAndFacilityCommonInfo):
         PENDING = "Pending"
         APPROVED = "Approved"
         REJECTED = "Rejected"
-        CHANGES = "Changes Requested"
+        CHANGES_REQUESTED = "Changes Requested"
 
     operator = models.ForeignKey(
         Operator,
