309de80b942f4e8016feee3e574adea9609388e2
SeSo <sobhani.sepehr@gmail.com>
2024-01-08 15:49:22 -0800

    chore: show user operator contact form based on the param

--- client/app/components/routes/select-operator/request-access/user-operator/Page.tsx
+++ client/app/components/routes/select-operator/request-access/user-operator/Page.tsx
@@ -5,7 +5,7 @@ import UserOperator from "@/app/components/routes/select-operator/form/UserOpera
 export default async function SelectOperatorRequestAccessReceivedUserOperatorPage({
   params,
 }: {
-  readonly params?: Readonly<{ id?: number }>;
+  readonly params?: Readonly<{ id?: number | string }>;
 }) {
   return (
     <Suspense fallback={<Loading />}>
