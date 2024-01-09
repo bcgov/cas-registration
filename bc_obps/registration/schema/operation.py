6591f5b935f2307d63d7704b7b3b7f3d6bf41325
Brianna Cerkiewicz <brianna@button.is>
2024-01-09 11:51:40 -0800

    chore: create helper functions for file proccessing

--- bc_obps/registration/schema/operation.py
+++ bc_obps/registration/schema/operation.py
@@ -1,9 +1,50 @@
 from typing import List, Optional
 from ninja import Field, ModelSchema, Schema
-from registration.models import Operation
+from registration.models import Operation, Document
 from datetime import date
 from registration.utils import AUDIT_FIELDS
 from .contact import ContactSchema
+import requests
+import base64
+from django.core.files.base import ContentFile
+import re
+
+# Helpers, move somewhere more general later brianna
+
+
+def file_to_data_url(document: Document):
+    """
+    Transforms a Django FieldField record into a data url that RJSF can process.
+    """
+    timeout_seconds = 10
+    try:
+        response = requests.get(document.file.url, timeout=timeout_seconds)
+        if response.status_code == 200:
+            document_content = response.content
+            encoded_content = base64.b64encode(document_content).decode("utf-8")
+            # only pdf format is allowed
+            return "data:application/pdf;name=" + document.file.name.split("/")[-1] + ";base64," + encoded_content
+        else:
+            print(f"Request failed with status code {response.status_code}")
+    except requests.exceptions.Timeout:
+        # Handle the timeout exception
+        print(f"Request timed out after {timeout_seconds} seconds")
+
+    except requests.exceptions.RequestException as e:
+        # Handle other types of exceptions (e.g., connection error)
+        print(f"An error occurred: {e}")
+
+
+def data_url_to_file(data_url: str):
+    """
+    Transforms a data url into a ContentFile that Django can insert into the db and add to google cloud storage
+    """
+    file_name = re.search(r'name=([^;]+)', data_url).group(1)
+    _, encoded_data = data_url.split(',')
+
+    # Decode the base64-encoded data
+    file_data = base64.b64decode(encoded_data)
+    return ContentFile(file_data, file_name)
 
 
 #### Operation schemas
