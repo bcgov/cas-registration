5576a4ffb5817c048e82b9c8ca7666482453231e
SeSo <sobhani.sepehr@gmail.com>
2024-01-09 11:42:06 -0800

    chore: move initial data to a migration

--- bc_obps/registration/tests/utils/bakers.py
+++ bc_obps/registration/tests/utils/bakers.py
@@ -1,5 +1,4 @@
 import pytest
-from django.core.management import call_command
 from model_bakery import baker
 from registration.models import (
     MultipleOperator,
@@ -8,39 +7,17 @@ from registration.models import (
     Contact,
     Operation,
     Operator,
-    ReportingActivity,
     User,
     UserOperator,
-    RegulatedProduct,
 )
 import uuid
 
+
 # We can import any of the below fixtures into our tests and they will be available by using the fixture name like:
-# @pytest.mark.usefixtures('app_role_baker')
-@pytest.fixture
-def app_role_baker():
-    # Load the fixture data into the test database
-    call_command('loaddata', 'real/appRole.json')
-
-
+# @pytest.mark.usefixtures('user_baker')
 @pytest.fixture
 def user_baker():
     return baker.make(User)
-
-
-@pytest.fixture
-def activity_baker():
-    return baker.make(ReportingActivity)
-
-
-@pytest.fixture
-def product_baker():
-    return baker.make(RegulatedProduct)
-
-
-@pytest.fixture
-def naics_code_baker():
-    return baker.make(NaicsCode)
 
 
 @pytest.fixture
@@ -59,12 +36,12 @@ def operator_baker(user_baker):
 
 
 @pytest.fixture
-def operation_baker(user_baker, naics_code_baker, contact_baker, operator_baker):
+def operation_baker(user_baker, contact_baker, operator_baker):
     return baker.make(
         Operation,
         operator=operator_baker,
         application_lead=contact_baker,
-        naics_code=naics_code_baker,
+        naics_code=NaicsCode.objects.first(),
         bcghg_id=uuid.uuid4(),
     )
 
