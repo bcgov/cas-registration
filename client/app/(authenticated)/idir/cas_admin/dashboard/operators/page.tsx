8b6660e1b28cd739bacf84e2ca456a83690118cb
marcellmueller <marcel@button.is>
2024-01-16 09:27:00 -0800

    refactor: move idir route operations dashboard note component

--- client/app/(authenticated)/idir/cas_admin/dashboard/operators/page.tsx
+++ client/app/(authenticated)/idir/cas_admin/dashboard/operators/page.tsx
@@ -1,19 +1,11 @@
 import { Suspense } from "react";
 import AccessRequests from "@/app/components/routes/access-requests/AccessRequests";
 import Loading from "@/app/components/loading/SkeletonGrid";
-import Note from "@/app/components/datagrid/Note";
 
 export default function Page() {
   return (
-    <>
-      <Note
-        message="Once “Approved”, the user will have access to their
-      operator dashboard with full admin permissions, and can grant access and
-      designate permissions to other Business BCeID holders there."
-      />
-      <Suspense fallback={<Loading />}>
-        <AccessRequests />
-      </Suspense>
-    </>
+    <Suspense fallback={<Loading />}>
+      <AccessRequests />
+    </Suspense>
   );
 }
