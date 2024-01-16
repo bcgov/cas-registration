224c90dad0d87293cf53a8f070b23f4de0eefa85
marcellmueller <marcel@button.is>
2024-01-16 12:44:54 -0800

    feat: style mui datagrid header icon

--- client/app/components/datagrid/DataGrid.tsx
+++ client/app/components/datagrid/DataGrid.tsx
@@ -16,6 +16,40 @@ interface Props {
   cntxt?: string;
 }
 
+interface SortIconProps {
+  topFill?: string;
+  bottomFill?: string;
+}
+
+const SortIcon = ({ topFill, bottomFill }: SortIconProps) => {
+  return (
+    <svg
+      width="10"
+      height="15"
+      viewBox="0 0 10 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.32714 5.86914L0.672861 5.86914C0.0741596 5.86914 -0.225192 5.12087 0.198608 4.68235L4.52407 0.203175C4.78642 -0.0682907 5.21358 -0.0682907 5.47593 0.203175L9.80139 4.68235C10.2252 5.12087 9.92584 5.86914 9.32714 5.86914Z"
+        fill={topFill || "white"}
+      />
+      <path
+        d="M0.672861 9.13086H9.32714C9.92584 9.13086 10.2252 9.87913 9.80139 10.3176L5.47593 14.7968C5.21358 15.0683 4.78642 15.0683 4.52407 14.7968L0.198608 10.3176C-0.225193 9.87913 0.0741586 9.13086 0.672861 9.13086Z"
+        fill={bottomFill || "white"}
+      />
+    </svg>
+  );
+};
+
+const AscendingIcon = () => {
+  return <SortIcon topFill="grey" bottomFill="white" />;
+};
+
+const DescendingIcon = () => {
+  return <SortIcon topFill="white" bottomFill="grey" />;
+};
+
 const DataGrid: React.FC<Props> = ({ rows, columns, cntxt }) => {
   // 📚  Define a state variable to store columns
   const [customColumns, setCustomColumns] = useState<GridColDef[]>(columns);
@@ -80,12 +114,17 @@ const DataGrid: React.FC<Props> = ({ rows, columns, cntxt }) => {
       <MuiGrid
         rows={rows}
         columns={customColumns}
+        components={{
+          ColumnSortedAscendingIcon: AscendingIcon,
+          ColumnSortedDescendingIcon: DescendingIcon,
+          ColumnUnsortedIcon: SortIcon,
+        }}
         sx={{
           "& .MuiSvgIcon-root": {
             color: "white",
           },
           "& .MuiDataGrid-columnHeaderDraggableContainer": {
-            flex: 0,
+            minWidth: "100%",
           },
           "& .MuiDataGrid-columnHeader": {
             border: "1px white solid",
@@ -103,11 +142,19 @@ const DataGrid: React.FC<Props> = ({ rows, columns, cntxt }) => {
           "& .MuiDataGrid-columnHeader:last-child": {
             borderRight: "none",
           },
+          "& .MuiDataGrid-columnHeaderTitleContainer": {
+            justifyContent: "space-between",
+          },
+          "& .MuiDataGrid-columnHeaderTitleContainerContent": {
+            flex: 1,
+          },
           "& .MuiDataGrid-columnHeaderTitle": {
             fontWeight: "bold",
             whiteSpace: "pre-line",
             lineHeight: "normal",
             textAlign: "center",
+            width: "100%",
+            minWidth: "100%",
           },
           "& .MuiDataGrid-columnSeparator": {
             display: "none",
@@ -115,6 +162,17 @@ const DataGrid: React.FC<Props> = ({ rows, columns, cntxt }) => {
           "& .MuiDataGrid-cell": {
             justifyContent: "center",
             textAlign: "center",
+          },
+          ".MuiDataGrid-iconButtonContainer": {
+            visibility: "visible !important",
+            minWidth: "20px",
+          },
+          "& .MuiDataGrid-sortIcon": {
+            opacity: "inherit !important",
+          },
+          // We may want tor eenable this functionality though it's not in the design
+          ".MuiDataGrid-menuIcon": {
+            display: "none",
           },
         }}
         disableVirtualization
