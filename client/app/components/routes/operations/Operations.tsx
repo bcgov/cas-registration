ff37e9fa5524703935a52971e049b31bfc1777de
marcellmueller <marcel@button.is>
2024-01-18 15:02:34 -0800

    fix: move datagrid flex column

--- client/app/components/routes/operations/Operations.tsx
+++ client/app/components/routes/operations/Operations.tsx
@@ -103,8 +103,6 @@ export default async function Operations() {
       field: "submission_date",
       headerName: "Submission Date",
       width: isOperatorColumn ? 200 : 220,
-      // Set flex to 1 to make the column take up all the remaining width if user zooms out
-      flex: 1,
     },
     {
       field: "bc_obps_regulated_operation",
@@ -122,6 +120,8 @@ export default async function Operations() {
       headerName: "Action",
       sortable: false,
       width: 120,
+      // Set flex to 1 to make the column take up all the remaining width if user zooms out
+      flex: 1,
     },
   ];
 
