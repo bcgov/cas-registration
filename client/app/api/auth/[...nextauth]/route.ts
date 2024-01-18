40f6f67e9498520bb21f89926242020805718df3
shon-button <shon@button.is>
2024-01-18 10:25:28 -0500

    fix: allow custom redirect

--- client/app/api/auth/[...nextauth]/route.ts
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -70,7 +70,7 @@ export const authOptions: NextAuthOptions = {
           // 🚀 API call: Get user app_role by user_guid from user table
           const responseRole = await actionHandler(
             `registration/user-app-role/${token.user_guid}`,
-            "GET",
+            "GET"
           );
           if (responseRole?.role_name) {
             // user found in table, assign role to token (note: all industry users have the same app role of `industry_user`, and their permissions are further defined by their role in the UserOperator model)
@@ -81,7 +81,7 @@ export const authOptions: NextAuthOptions = {
                 // 🚀 API call: check if user is admin approved
                 const responseAdmin = await actionHandler(
                   `registration/is-approved-admin-user-operator/${token.user_guid}`,
-                  "GET",
+                  "GET"
                 );
                 if (responseAdmin?.approved) {
                   token.app_role = "industry_user_admin"; // note: industry_user_admin a front-end only role. In the db, all industry users have an industry_user app_role, and their permissions are further defined by UserOperator.role
@@ -182,6 +182,18 @@ export const authOptions: NextAuthOptions = {
           app_role: token.app_role,
         },
       };
+    },
+    // 👇️ called anytime the user is redirected to a callback URL (e.g. on signin or signout).
+    // by default only URLs on the same URL as the site are allowed, you can use the redirect callback to customise that behaviour.
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      // 🛸 Allow callbacks to identity server
+      else if (new URL(url).origin === process.env.SITEMINDER_AUTH_URL)
+        return url;
+      return baseUrl;
     },
   },
 };
