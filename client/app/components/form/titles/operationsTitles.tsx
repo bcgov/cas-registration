353cc425c9e09d42f6e35f363eed5301df218a26
marcellmueller <marcel@button.is>
2024-01-19 13:07:46 -0800

    feat: add statutory declarations disclaimer message

--- client/app/components/form/titles/operationsTitles.tsx
+++ client/app/components/form/titles/operationsTitles.tsx
@@ -1,3 +1,22 @@
+export const StatutoryDeclarationDisclaimerTitle = (
+  <>
+    <b>Please note:</b>
+    <br />
+    <ul className="mt-0">
+      <li>
+        Information in this application will be shared with the Minister of
+        Finance and the name of the Operator and BORO ID may be disclosed [for
+        the purposes of administering the Carbon Tax Act][fuel
+        suppliers][publicly].
+      </li>
+      <li>
+        The Operator is responsible for determining their eligibility for an
+        exemption under the Carbon Tax Act.
+      </li>
+    </ul>
+  </>
+);
+
 export const StatutoryDeclarationUploadFieldTitle = (
   // Uncomment and replace the URL with the correct one when the template is ready in #645
   // <>
