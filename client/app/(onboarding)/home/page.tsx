cb8b4c8d1a28ad03244ec00dfb73e0d68743b462
marcellmueller <marcel@button.is>
2024-01-22 15:33:11 -0800

    feat: redo home page content

--- client/app/(onboarding)/home/page.tsx
+++ client/app/(onboarding)/home/page.tsx
@@ -1,22 +1,11 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
 
 // 🏷 import {named} can be significantly slower than import default
-import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid/Grid";
 
 import events from "@/app/data/home/events.json";
-import features from "@/app/data/home/features.json";
-import needs from "@/app/data/home/needs.json";
+import { signIn } from "next-auth/react";
 /*
 📚
 In the app directory, nested folders are normally mapped to URL paths.
@@ -26,8 +15,47 @@ e.g. app\(onboarding)\home maps to route: http://localhost:3000/home
 */
 
 export default function Page() {
+  const fontSize = "text-2xl";
+  const headerStyle = "text-bc-bg-blue text-2xl";
+  const tableBorder = "border border-solid border-bc-bg-dark-grey";
+
+  const handleIdirLogin = () => {
+    signIn("keycloak", undefined, { kc_idp_hint: "idir" });
+  };
+
+  const handleBceidLogin = () => {
+    signIn("keycloak", undefined, { kc_idp_hint: "bceidbusiness" });
+  };
+
   return (
     <>
+      <section className={fontSize}>
+        <p>
+          Welcome to the B.C. Industrial Emissions Reporting System (BCIERS), a
+          web application for industrial operators to participate in the B.C.
+          Output-Based Pricing System (B.C. OBPS).
+        </p>
+
+        <p>
+          Operators who are eligible to participate in the B.C. OBPS will need
+          to use this web application to start the registration process.
+        </p>
+
+        <p>
+          Before getting started, take a moment to review the detailed{" "}
+          <a href="ADD LINK HERE" target="_blank" rel="noopener noreferrer">
+            guidance
+          </a>
+          .
+        </p>
+
+        <p>
+          BCIERS will enable operators to apply for a B.C. Regulated Operation
+          ID (BORO ID) for each industrial operation that is eligible to
+          participate in the B.C. OBPS. A BORO ID is needed to claim a carbon
+          tax exemption under the Carbon Tax Act starting April 1, 2024.
+        </p>
+      </section>
       {/*
       💻📲
       MUI responsive layout grid adapts to screen size and orientation, ensuring consistency across layouts.
@@ -37,210 +65,120 @@ export default function Page() {
       Using breakpoints for responsive design, grid items will display in two columns on laptop & desktop and a single column on mobile & tablet,
       Using the order prop for stacking order, grid item 1 and 2 will be reverse order on mobile & tablet,
        */}
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6} order={{ xs: 2, md: 1 }}>
-          <Typography
-            color="primary.light"
-            sx={{
-              fontWeight: 700,
-              fontSize: "24px",
-              lineHeight: "29.05px",
-            }}
-          >
-            What is the Output-Based Pricing System (OBPS)?
-          </Typography>
-          <Typography
-            component="div"
-            sx={{
-              marginTop: "20px",
-              fontWeight: 400,
-              fontSize: "16px",
-              lineHeight: "36px",
-            }}
-          >
-            The OBPS is designed to ensure there is a price incentive for
-            industrial emitters to reduce their greenhouse gas emissions and
-            spur innovation while maintaining competitiveness and protecting
-            against “carbon leakage” (i.e. the risk of industrial facilities
-            moving from one region to another to avoid paying a price on carbon
-            pollution). Please see additional information on our{" "}
-            <Link href="/program-website">program website.</Link>
-          </Typography>
-          <Typography
-            sx={{
-              marginTop: "10px",
-              fontWeight: 700,
-            }}
-          >
-            Key features of the BC OBPS include:
-          </Typography>
-          <List>
-            {features.map((feature, index) => (
-              <ListItem
-                key={index}
-                sx={{
-                  paddingLeft: "8px", // Adjust the left indentation here
-                }}
-              >
-                <ListItemText
-                  primary={`• ${feature}`} // Add a bullet (•) before each feature
-                  primaryTypographyProps={{
-                    variant: "body1", // Optional: Apply a specific typography style
-                    sx: {
-                      lineHeight: "1.2",
-                    },
-                  }}
-                />
-              </ListItem>
-            ))}
-          </List>
-
-          <Image
-            alt="chart for comparing industrial carbon pricing systems"
-            src="/img/registration/image5.svg"
-            height={389.49}
-            width={650}
-            style={{ marginTop: "25px" }}
-          />
-          <Image
-            alt="chart for compliance mechanisms in a B.C. output based pricing system"
-            src="/img/registration/image6.svg"
-            height={443.08}
-            width={650}
-            style={{ marginTop: "45px" }}
-          />
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          marginTop: "24px",
+          marginBottom: "48px",
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={6}
+          order={{ xs: 2, md: 1 }}
+          sx={{
+            fontSize: "24px",
+            marginRight: {
+              xs: "0px",
+              md: "40px",
+            },
+          }}
+        >
+          <h2 className={headerStyle}>
+            How to apply for a B.C. OBPS Regulated Operation ID
+          </h2>
+          <ol>
+            <li>Log in as an industrial operator with your Business BCeID.</li>
+            <li>Activate your industrial operator profile.</li>
+            <li>
+              Submit an application for each of your industrial operations.
+            </li>
+          </ol>
+          <p>
+            Upon review, each eligible industrial operation will receive a BORO
+            ID.
+          </p>
+          <p>
+            To check eligibility, and for further information about the B.C.
+            OBPS, please visit the <a>program website.</a>
+          </p>
+          <p>
+            Please visit the <a>carbon tax exemption</a> webpage to learn more
+            about claiming an exemption from the carbon tax.
+          </p>
         </Grid>
-        <Grid item xs={12} md={6} order={{ xs: 1, md: 2 }}>
-          <Typography
-            color="primary.light"
-            sx={{
-              fontWeight: 700,
-              fontSize: "24px",
-              lineHeight: "29.05px",
-            }}
+        <Grid item xs={12} md={5} order={{ xs: 1, md: 2 }}>
+          <section className="flex flex-col items-center">
+            <h2 className={`${headerStyle} w-full text-center`}>
+              Log in as Industrial Operator
+            </h2>
+            <Button
+              variant="contained"
+              className="w-full md:max-w-[70%] bg-bc-bg-blue"
+              onClick={handleBceidLogin}
+            >
+              Log in with Business BCeID
+            </Button>
+            <p>
+              Don’t have a Business BCeID?{" "}
+              <a href="ADD LINK HERE" target="_blank" rel="noopener noreferrer">
+                Create one here
+              </a>
+            </p>
+          </section>
+          <h2 className={headerStyle}>Key Dates</h2>
+          <table
+            className={`table-auto w-full text-lg border-collapse ${tableBorder}`}
           >
-            Key Dates
-          </Typography>
-          <TableContainer
-            sx={{
-              marginTop: "30px",
-            }}
-          >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell
-                    align="center"
-                    sx={{
-                      border: "1px solid black",
-                      fontWeight: 700,
-                      fontSize: "18px",
-                      lineHeight: "21.78px",
-                    }}
+            <colgroup>
+              <col className="w-[30%]" />
+              <col className="w-[70%]" />
+            </colgroup>
+            <thead>
+              <tr>
+                <th className={`p-4 ${tableBorder}`}>Date</th>
+                <th className={`p-4 ${tableBorder}`}>Event</th>
+              </tr>
+            </thead>
+            <tbody>
+              {events.map((row, index) => (
+                <tr key={index} className="[&>td]:p-4">
+                  <td
+                    className={`px-2 py-4 ${tableBorder} whitespace-pre-line`}
                   >
-                    Date
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    sx={{
-                      border: "1px solid black",
-                      fontWeight: 700,
-                      fontSize: "18px",
-                      lineHeight: "21.78px",
-                    }}
-                  >
-                    Event
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {events.map((row, index) => (
-                  <TableRow key={index}>
-                    <TableCell
-                      align="center"
-                      sx={{ border: "1px solid black" }}
-                    >
-                      {row.date}
-                    </TableCell>
-                    <TableCell
-                      align="center"
-                      sx={{ border: "1px solid black" }}
-                    >
-                      {row.event}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>{" "}
-          <Typography
-            color="primary.light"
-            sx={{
-              marginTop: "40px",
-              fontWeight: 700,
-              fontSize: "24px",
-              lineHeight: "29.05px",
-            }}
-          >
-            How to register BC OBPS
-          </Typography>
-          <Typography
-            variant="body1"
-            component="div"
-            sx={{
-              marginTop: "20px",
-              fontWeight: 400,
-              fontSize: "16px",
-              lineHeight: "36px",
-            }}
-          >
-            Please see the{" "}
-            <Link href="/completeguidance ">
-              complete guidance for registration
-            </Link>
-          </Typography>
-          <Typography
-            sx={{
-              marginTop: "10px",
-              fontWeight: 700,
-            }}
-          >
-            User will need:
-          </Typography>
-          <List>
-            {needs.map((need, index) => (
-              <ListItem
-                key={index}
-                sx={{
-                  paddingLeft: "8px", // Adjust the left indentation here
-                }}
-              >
-                <ListItemText
-                  primary={`• ${need}`} // Add a bullet (•) before each feature
-                  primaryTypographyProps={{
-                    variant: "body1", // Optional: Apply a specific typography style
-                    sx: {
-                      lineHeight: "1.2",
-                    },
-                  }}
-                />
-              </ListItem>
-            ))}
-          </List>
-          <Typography variant="body1" component="div">
-            If you have any questions, please email us at <br />
-            <Link href="mailto:GHGRegulator@gov.bc.ca">
-              <span
-                style={{
-                  color: "black",
-                  fontWeight: "bold",
-                  textDecoration: "none",
-                }}
+                    {row.date}
+                  </td>
+                  <td className={`px-2 py-4 ${tableBorder}`}>{row.event}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <section className="flex flex-col items-center bg-bc-bg-light-grey my-10 py-8">
+            <h2 className={headerStyle}>Contact us</h2>
+            <p className="text-center">
+              If you have any questions, please email us at <br />
+              <a
+                href="mailto:GHGRegulator@gov.bc.ca"
+                className="text-black font-bold no-underline"
               >
                 GHGRegulator@gov.bc.ca
-              </span>
-            </Link>
-          </Typography>
+              </a>
+            </p>
+          </section>
+          <section className="flex flex-col items-center">
+            <h2 className={`${headerStyle} w-full text-center`}>
+              Log in as Government Employee
+            </h2>
+            <Button
+              variant="outlined"
+              className="w-full md:max-w-[70%]"
+              onClick={handleIdirLogin}
+            >
+              Log in with IDIR
+            </Button>
+          </section>
         </Grid>
       </Grid>
     </>
