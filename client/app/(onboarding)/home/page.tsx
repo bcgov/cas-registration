00299296c9a6861e3b953381eb3b07f249a77514
marcellmueller <marcel@button.is>
2024-01-23 10:54:14 -0800

    fix: home page grid container takes up remaining width

--- client/app/(onboarding)/home/page.tsx
+++ client/app/(onboarding)/home/page.tsx
@@ -122,7 +122,18 @@ export default function Page() {
             tax.
           </p>
         </Grid>
-        <Grid item xs={12} md={5} order={{ xs: 2, md: 2 }}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          order={{ xs: 2, md: 2 }}
+          sx={{
+            paddingLeft: {
+              xs: "0px",
+              md: "40px",
+            },
+          }}
+        >
           <section className="flex flex-col items-center">
             <h2 className={`${headerStyle} w-full text-center`}>
               Log in as Industrial Operator
