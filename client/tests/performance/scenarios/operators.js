f9d3cdc40361ce196bc80db709c659d9573a2e1b
marcellmueller <marcel@button.is>
2024-01-25 14:40:10 -0800

    test: move k6 params to setup file

--- client/tests/performance/scenarios/operators.js
+++ client/tests/performance/scenarios/operators.js
@@ -1,29 +1,10 @@
+/* eslint-disable */
 import http from "k6/http";
 import { check } from "k6";
+import { industryUserParams, internalUserParams } from "../setup/params.js";
 
 const queries = () => {
   const HOST = __ENV.SERVER_HOST;
-  const INDUSTRY_USER_GUID = "279c80cf57814c28872740a133d17c0d";
-  const INTERNAL_USER_GUID = "4da70f32-65fd-4137-87c1-111f2daba3dd";
-
-  const industryUserParams = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: JSON.stringify({
-        user_guid: INDUSTRY_USER_GUID,
-      }),
-    },
-  };
-
-  const internalUserParams = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: JSON.stringify({
-        user_guid: INTERNAL_USER_GUID,
-      }),
-    },
-  };
-
   // GET routes
 
   // operators?cra_business_number GET route
