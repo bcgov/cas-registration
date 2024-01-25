977814396bc923b3cb71226b9d4848bdc3d56bc0
marcellmueller <marcel@button.is>
2024-01-25 13:00:51 -0800

    test: add separate server_host k6 env var

--- client/tests/performance/scenarios/queries.js
+++ client/tests/performance/scenarios/queries.js
@@ -6,7 +6,7 @@ import exec from "k6/execution";
 const queries = () => {
   const base_endpoint = "/api/registration";
   const endpoint =
-    __ENV.APP_HOST + base_endpoint + "/operators/legal-name?search_value=Op";
+    __ENV.SERVER_HOST + base_endpoint + "/operators/legal-name?search_value=Op";
   console.log("endpoint: ", endpoint);
 
   const params = {
