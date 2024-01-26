c3257337d7403f2cfa9f1c759da05eef96f4b8b5
shon-button <shon@button.is>
2024-01-26 14:02:11 -0500

    debug: simple test

--- client/e2e/pages/onboarding/home/page.spec.ts
+++ client/e2e/pages/onboarding/home/page.spec.ts
@@ -3,90 +3,12 @@
 
 import { test, expect } from "@playwright/test";
 import * as dotenv from "dotenv";
-import { navigateAndWaitForLoad } from "@/e2e/utils/helpers";
 // 👤 User Roles
 import { UserRole } from "@/e2e/utils/enums";
-// 🛸 Login Links
-import { LoginLink } from "@/e2e/utils/enums";
-
 dotenv.config({ path: "./e2e/.env.local" });
 
 // Set the test URL
 const url = process.env.BASEURL + "home";
-
-// 🛠️ Function: log in to Keycloak
-const login = async (
-  page: any,
-  userName: string,
-  password: string,
-  role: string
-) => {
-  try {
-    // eslint-disable-next-line no-console
-    console.log(`Navigating to: ${url}`);
-    // 🛸 Navigate to the home page
-    await navigateAndWaitForLoad(page, url);
-    expect(page.url().toLocaleLowerCase()).toContain("/home");
-    // eslint-disable-next-line no-console
-    console.log("Asserted url is correct");
-    // Determine the login button based on the user role
-    let loginButton = LoginLink.INDUSTRY_USER;
-    console.log("Clicking Login...");
-    // Click the login button with a longer timeout (e.g., 60 seconds)
-    await page
-      .getByRole("button", { name: loginButton }, { timeout: 60000 })
-      .click();
-    console.log("Completing login form...");
-    // 🕒 Wait for the user field to be present
-    const userField = await page.waitForSelector("#user");
-    // 🔍 Assert that the field is available
-    expect(userField).not.toBeNull();
-    // Fill the field
-    await userField.fill(userName);
-
-    // 🕒 Wait for the password field to be present
-    const pwField = await page.waitForSelector("#password");
-    // 🔍 Assert that the field is available
-    expect(pwField).not.toBeNull();
-    // Fill the field
-    await pwField.fill(password);
-
-    // 🕒Wait for the "Continue" button to be present
-    const continueButton = await page.waitForSelector(
-      'input[type="submit"][name="btnSubmit"].btn.btn-primary'
-    );
-    // 🔍 Assert that the button is available
-    expect(continueButton).not.toBeNull();
-    // eslint-disable-next-line no-console
-    console.log("Submiting login form...");
-    // Click the "Continue" button
-    await continueButton.click();
-
-    // eslint-disable-next-line no-console
-    console.log("Waiting for the profile link to be present...");
-    // 🕒 Wait for the home page profile navigation link to be present
-    const profileNavSelector = '[data-testid="nav-user-profile"]';
-    await page.waitForSelector(profileNavSelector);
-    // 🔍 Assert that the link is available
-    expect(profileNavSelector).not.toBeNull();
-
-    // eslint-disable-next-line no-console
-    console.log("Asserting authenticated url...");
-    // 🔍 Assert based on user role
-    switch (role) {
-      case UserRole.NEW_USER:
-        expect(page.url().toLocaleLowerCase()).toContain("/profile");
-        break;
-      default:
-        expect(page.url().toLocaleLowerCase()).toContain("/dashboard");
-        break;
-    }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(`Login failed for ${userName}:`, error);
-    throw error;
-  }
-};
 
 // 🏷 Annotate test suite as serial
 test.describe.configure({ mode: "serial" });
@@ -95,23 +17,14 @@ test.describe("Test Page - Home", () => {
   // ➰ Loop through the entries of UserRole enum
   for (const [role, value] of Object.entries(UserRole)) {
     test.describe(`Test User Role - ${value}`, () => {
-      // Set user and password based on the user role
-      let user = "";
-      let pw = "";
-      switch (value) {
-        case UserRole.INDUSTRY_USER_ADMIN:
-        case UserRole.INDUSTRY_USER:
-        case UserRole.NEW_USER:
-          user = process.env[`${role}_USERNAME`] || "";
-          pw = process.env[`${role}_PASSWORD`] || "";
-          break;
-      }
       test("Test Login", async ({ page }) => {
-        // 🚩 role is already logged in during setup\global.ts
-        //await loggedIn(page);
-        // test logout-login flow
-        // await logout(page);
-        await login(page, user, pw, value);
+        await page.goto(url);
+        await expect(page.url().toLocaleLowerCase()).toContain("/home");
+        // eslint-disable-next-line no-console
+        console.log("Asserted home url");
+        // eslint-disable-next-line no-console
+        console.log("Waiting for the Welcome text to be present...");
+        await expect(page.getByText("Welcome")).toBeVisible();
       });
     });
   }
