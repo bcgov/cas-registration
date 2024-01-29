2ce1e85eb5b1890f64d9a6ce8254bf38de034264
marcellmueller <marcel@button.is>
2024-01-29 08:19:40 -0800

    test: adjust perf test stages

--- client/tests/performance/script.js
+++ client/tests/performance/script.js
@@ -10,12 +10,12 @@ import user from "./scenarios/user.js";
 import user_operator from "./scenarios/user_operator.js";
 
 const stages = [
-  { duration: "5m", target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-  { duration: "10m", target: 60 }, // stay at 60 users for 10 minutes
-  { duration: "3m", target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-  { duration: "2m", target: 100 }, // stay at 100 users for short amount of time (peak hour)
-  { duration: "3m", target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-  { duration: "10m", target: 60 }, // continue at 60 for additional 10 minutes
+  { duration: "5m", target: 20 }, // simulate ramp-up of traffic from 1 to 20 users over 5 minutes.
+  { duration: "10m", target: 20 }, // stay at 20 users for 10 minutes
+  { duration: "3m", target: 40 }, // ramp-up to 40 users over 3 minutes (peak hour starts)
+  { duration: "2m", target: 40 }, // stay at 40 users for short amount of time (peak hour)
+  { duration: "3m", target: 15 }, // ramp-down to 20 users over 3 minutes (peak hour ends)
+  { duration: "10m", target: 15 }, // continue at 20 for additional 10 minutes
   { duration: "2m", target: 0 }, // ramp-down to 0 users
 ];
 
