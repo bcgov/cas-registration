8fae0e0dd97adeb4cf50830f6c98569e7760d9e1
shon-button <shon@button.is>
2024-01-29 11:50:40 -0500

    debug: rebuild

--- client/app/components/form/widgets/EmailWidget.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import TextWidget from "./TextWidget";
-import { WidgetProps } from "@rjsf/utils/lib/types";
-
-const EmailWidget: React.FC<WidgetProps> = (props) => {
-  return <TextWidget {...props} type="email" />;
-};
-export default EmailWidget;
--- client/app/components/routes/access-requests/types.ts
+++
@@ -1,8 +0,0 @@
-export interface UserOperator {
-  id: number;
-  status: string;
-  first_name: string;
-  last_name: string;
-  email: string;
-  legal_name: string;
-}
