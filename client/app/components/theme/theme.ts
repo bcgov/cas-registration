62dc78c3aaa985842077ee4f257a1089be2ecc9a
shon-button <shon@button.is>
2024-01-29 13:54:55 -0500

    debug: add layout theme

---
+++ client/app/components/theme/theme.ts
@@ -0,0 +1,115 @@
+import { createTheme } from "@mui/material/styles";
+import {
+  BC_GOV_PRIMARY_BRAND_COLOR_BLUE,
+  BC_GOV_BACKGROUND_COLOR_BLUE,
+  BC_GOV_LINKS_COLOR,
+  BC_GOV_YELLOW,
+  BC_GOV_TEXT,
+  LIGHT_GREY_BG_COLOR,
+  BC_GOV_SEMANTICS_RED,
+  BC_GOV_SEMANTICS_GREEN,
+  DARK_GREY_BG_COLOR,
+} from "@/app/styles/colors";
+import "@bcgov/bc-sans/css/BCSans.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "BCSans, sans-serif",
+    // Include "sans-serif" as a fallback font family.
+    allVariants: {
+      color: BC_GOV_TEXT,
+    },
+  },
+  // To modify each color directly, provide an object with one or more of the color tokens. Only the main token is required; light, dark, and contrastText are optional, and if not provided, then their values are calculated
+  palette: {
+    primary: {
+      main: BC_GOV_PRIMARY_BRAND_COLOR_BLUE,
+      light: BC_GOV_BACKGROUND_COLOR_BLUE,
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: DARK_GREY_BG_COLOR,
+      light: LIGHT_GREY_BG_COLOR,
+    },
+    error: {
+      main: BC_GOV_SEMANTICS_RED,
+    },
+    warning: {
+      main: BC_GOV_YELLOW,
+    },
+    success: {
+      main: BC_GOV_SEMANTICS_GREEN,
+    },
+    text: {
+      primary: BC_GOV_TEXT,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontSize: "1rem",
+          textTransform: "capitalize",
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          "& a": {
+            color: BC_GOV_LINKS_COLOR,
+          },
+        },
+      },
+    },
+
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            "&:hover fieldset": {
+              borderColor: BC_GOV_LINKS_COLOR,
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: BC_GOV_LINKS_COLOR,
+            },
+          },
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: BC_GOV_LINKS_COLOR,
+            borderWidth: "1px",
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: BC_GOV_LINKS_COLOR,
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: DARK_GREY_BG_COLOR,
+            },
+            "&:hover fieldset": {
+              borderColor: BC_GOV_LINKS_COLOR,
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: BC_GOV_LINKS_COLOR,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+export default theme;
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -6,6 +6,7 @@ You should not manually add <head> tags such as <title> and <meta> to root layou
 */
 
 import "@/app/styles/globals.css";
+import ThemeRegistry from "@/app/components/theme/ThemeRegistry";
 import type { Metadata, Viewport } from "next";
 // 🏷 import {named} can be significantly slower than import default
 import Box from "@mui/material/Box";
@@ -32,21 +33,30 @@ export default async function RootLayout({
         //👇️ Used to mark the root element where Next.js will mount the client-side React application
       }
       <body id="__next">
-        <Box
-          sx={{
-            display: "flex",
-            minHeight: "100vh",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "center",
-            margin: "0 auto",
-            maxWidth: "1536px",
-            padding: "0 16px",
-          }}
-        >
-          {/* Content goes here */}
-          {children}
-        </Box>
+        <ThemeRegistry>
+          {/*
+          MUI Box component is a versatile and essential building block in Material-UI v5.
+          It serves as a wrapper element that helps structure and organize the layout of your application.
+          One of the Box component's strengths is its ability to create responsive layouts easily...
+          utilizing Box component's sx prop to create a responsive layout...
+          You can use properties like display, flexDirection, alignItems, justifyContent and more to control the arrangement and alignment of elements within a flex container
+          */}
+          <Box
+            sx={{
+              display: "flex",
+              minHeight: "100vh",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+              margin: "0 auto",
+              maxWidth: "1536px",
+              padding: "0 16px",
+            }}
+          >
+            {/* Content goes here */}
+            {children}
+          </Box>
+        </ThemeRegistry>
       </body>
     </html>
   );
