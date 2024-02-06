886ebc525f9a62c25127768f366383df22f47d7c
shon-button <shon@button.is>
2024-02-06 07:43:44 -0500

    debug: increase e2e timeout

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -33,9 +33,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-  expect: {
-    timeout: 10 * 1000,
-  },
+  timeout: 60000,
   // 🚩 The globalSetup option in playwright.config.js allows you to specify a JavaScript file that will be executed ONCE before all test suites.
   globalSetup: require.resolve("e2e/setup/global.ts"),
 
