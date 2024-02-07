b5623b8616f4f0e7ea4671b4b3b4f47627dae674
shon-button <shon@button.is>
2024-02-07 08:57:34 -0500

    config: e2e

--- client/app/api/auth/[...nextauth]/route.ts
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -28,6 +28,8 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  //https://next-auth.js.org/configuration/options#secret
+  secret: `${process.env.NEXTAUTH_SECRET}`,
   //https://next-auth.js.org/configuration/pages
   pages: {
     error: "/auth/error", // Error code passed in query string as ?error=
@@ -76,7 +78,7 @@ export const authOptions: NextAuthOptions = {
           // 🚀 API call: Get user app_role by user_guid from user table
           const responseRole = await actionHandler(
             `registration/user-app-role/${token.user_guid}`,
-            "GET",
+            "GET"
           );
           if (responseRole?.role_name) {
             // user found in table, assign role to token (note: all industry users have the same app role of `industry_user`, and their permissions are further defined by their role in the UserOperator model)
@@ -87,7 +89,7 @@ export const authOptions: NextAuthOptions = {
                 // 🚀 API call: check if user is admin approved
                 const responseAdmin = await actionHandler(
                   `registration/is-approved-admin-user-operator/${token.user_guid}`,
-                  "GET",
+                  "GET"
                 );
                 if (responseAdmin?.approved) {
                   token.app_role = "industry_user_admin"; // note: industry_user_admin a front-end only role. In the db, all industry users have an industry_user app_role, and their permissions are further defined by UserOperator.role
--- client/e2e/pages/onboarding/home/page.spec.ts
+++ client/e2e/pages/onboarding/home/page.spec.ts
@@ -1,18 +1,24 @@
-// 🧪 Suite to test the Home page `http://localhost:3000/home`
-// 🔍 Asserts the user can login, logout, and login
+// 🧪 Suite to test the onboarding\Home page `http://localhost:3000/home`
+// 🔍 Asserts simple test = home page has welcome text
 
 import { test, expect } from "@playwright/test";
+// ⛏️ Helpers
+import { navigateAndWaitForLoad } from "@/e2e/utils/helpers";
+// ℹ️ Environment variables
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./e2e/.env.local" });
 
-// set the test url
-const url = "http://localhost:3000/home";
+// Set the test URL
+const url = process.env.E2E_BASEURL || "";
 
 // 🏷 Annotate test suite as serial
-test.describe.configure({ mode: "serial" });
-
-test.describe("Test Page - Home", () => {
-  test("Test Login", async ({ page }) => {
-    await page.goto(url);
+test.describe.serial("Test Page - Home", () => {
+  test("Test Welcome", async ({ page }) => {
+    // 🛸 Navigate to the home page
+    await navigateAndWaitForLoad(page, url);
+    // 🔍 Assert that home url
     await expect(page.url().toLocaleLowerCase()).toContain("/home");
+    // 🔍 Assert welcome text is visible
     await expect(page.getByText("Welcome")).toBeVisible();
   });
 });
