98803ef908f22b5371eefea83b8cd74e2d0817f3
shon-button <shon@button.is>
2024-02-09 18:31:09 -0500

    add: all role storage

---
+++ client/e2e/workflows/bceidbusiness/industry_user.ts
@@ -0,0 +1,29 @@
+// 🧪 Suite to test the bceidbusiness new user workflow using storageState
+// 🔍 Asserts new user is redirected to profile
+
+import { test } from "@playwright/test";
+// 🪄 Page Object Models
+import { DashboardPOM } from "@/e2e/poms/dashboard";
+import { HomePOM } from "@/e2e/poms/home";
+// ℹ️ Environment variables
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./e2e/.env.local" });
+
+// 🏷 Annotate test suite as serial
+test.describe.configure({ mode: "serial" });
+test.describe("Test Workflow industry_user", () => {
+  // 👤 run test as new user with no role
+  const storageState = process.env.E2E_INDUSTRY_USER_STORAGE;
+  // Note: specify storageState for each test file or test group, instead of setting it in the config. https://playwright.dev/docs/next/auth#reuse-signed-in-state
+  test.use({ storageState: storageState }); // this will error if no such file or directory
+  test("Test Redirect to Dashboard", async ({ page }) => {
+    // 🛸 Navigate to home page
+    const homePage = new HomePOM(page);
+    await homePage.route();
+    // 🔍 Assert user is logged in
+    await homePage.userIsLoggedIn();
+    // 🔍 Assert that the current URL ends with "/dashboard"
+    const dashboardPage = new DashboardPOM(page);
+    await dashboardPage.urlIsCorrect();
+  });
+});
