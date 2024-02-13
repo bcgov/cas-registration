e8998af75a368f7cf06535801376865968407d86
SeSo <sobhani.sepehr@gmail.com>
2024-02-13 14:38:16 -0800

    chore: url_name and optimize reporting activities endpoint

--- bc_obps/registration/api/reporting_activities.py
+++ bc_obps/registration/api/reporting_activities.py
@@ -5,15 +5,21 @@ from registration.models import AppRole, ReportingActivity, UserOperator
 from registration.schema import (
     ReportingActivitySchema,
 )
+from django.core.cache import cache
 
 ##### GET #####
 
 
-@router.get("/reporting_activities", response=List[ReportingActivitySchema])
+@router.get("/reporting_activities", response=List[ReportingActivitySchema], url_name="list_reporting_activities")
 @authorize(AppRole.get_all_authorized_app_roles(), UserOperator.get_all_industry_user_operator_roles())
 def list_reporting_activities(request):
-    qs = ReportingActivity.objects.all()
-    return qs
+    cached_data = cache.get("reporting_activities")
+    if cached_data:
+        return cached_data
+    else:
+        reporting_activities = ReportingActivity.objects.only(*ReportingActivitySchema.Config.model_fields)
+        cache.set("reporting_activities", reporting_activities, 60 * 60 * 24 * 1)
+        return reporting_activities
 
 
 ##### POST #####
