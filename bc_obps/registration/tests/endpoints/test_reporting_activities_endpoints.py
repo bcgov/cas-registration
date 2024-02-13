382c0aec10d4170e086038c9025ec5919940fc83
SeSo <sobhani.sepehr@gmail.com>
2024-02-13 15:47:13 -0800

    refactor: refactor reporting activities endpoint tests

--- bc_obps/registration/tests/endpoints/test_reporting_activities_endpoints.py
+++ bc_obps/registration/tests/endpoints/test_reporting_activities_endpoints.py
@@ -1,28 +1,17 @@
-import pytest
-from model_bakery import baker
 from registration.models import ReportingActivity
 from registration.tests.utils.helpers import CommonTestSetup, TestUtils
 
-pytestmark = pytest.mark.django_db
-
-base_endpoint = "/api/registration/"
-
-content_type_json = "application/json"
-
 
 class TestReportingActivitiesEndpoint(CommonTestSetup):
-    endpoint = base_endpoint + "reporting_activities"
+    endpoint = CommonTestSetup.base_endpoint + "reporting_activities"
 
-    def test_unauthorized_users_cannot_get(self):
+    def test_unauthorized_users_cannot_get_reporting_activities(self):
         response = TestUtils.mock_get_with_auth_role(self, "cas_pending")
         assert response.status_code == 401
 
-    def test_get_method_for_200_status(self):
+    def test_authorized_users_can_get_reporting_activities(self):
         roles = ["cas_analyst", "cas_admin", "industry_user"]
         for role in roles:
             response = TestUtils.mock_get_with_auth_role(self, role)
             assert response.status_code == 200
-
-    def test_get_method_with_mock_data(self):
-        response = TestUtils.mock_get_with_auth_role(self, "cas_admin")
-        assert response.status_code == 200
+            assert len(response.json()) == ReportingActivity.objects.count()
