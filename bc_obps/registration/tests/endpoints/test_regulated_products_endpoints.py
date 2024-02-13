5943e3d8a6769627634f0bcec7627aff643847fd
SeSo <sobhani.sepehr@gmail.com>
2024-02-13 15:45:21 -0800

    refactor: refactor regulated products endpoint tests

--- bc_obps/registration/tests/endpoints/test_regulated_products_endpoints.py
+++ bc_obps/registration/tests/endpoints/test_regulated_products_endpoints.py
@@ -1,27 +1,17 @@
-import pytest
 from registration.models import RegulatedProduct
 from registration.tests.utils.helpers import CommonTestSetup, TestUtils
 
-pytestmark = pytest.mark.django_db
-
-base_endpoint = "/api/registration/"
-
-content_type_json = "application/json"
-
 
 class TestRegulatedProductsEndpoint(CommonTestSetup):
-    endpoint = base_endpoint + "regulated_products"
+    endpoint = CommonTestSetup.base_endpoint + "regulated_products"
 
-    def test_unauthorized_users_cannot_get(self):
+    def test_unauthorized_users_cannot_get_regulated_products(self):
         response = TestUtils.mock_get_with_auth_role(self, "cas_pending")
         assert response.status_code == 401
 
-    def test_get_method_for_200_status(self):
+    def test_authorized_users_can_get_regulated_products(self):
         roles = ["cas_analyst", "cas_admin", "industry_user"]
         for role in roles:
             response = TestUtils.mock_get_with_auth_role(self, role)
             assert response.status_code == 200
-
-    def test_get_method_with_mock_data(self):
-        response = TestUtils.mock_get_with_auth_role(self, "industry_user")
-        assert response.status_code == 200
+            assert len(response.json()) == RegulatedProduct.objects.count()
