29b1b7ef8e9c0d3f189ea6a6c2303aaaac77c1ed
SeSo <sobhani.sepehr@gmail.com>
2024-02-13 14:46:35 -0800

    refactor: refactor naics code endpoint tests

--- bc_obps/registration/tests/endpoints/test_naicscode_endpoints.py
+++ bc_obps/registration/tests/endpoints/test_naicscode_endpoints.py
@@ -1,24 +1,18 @@
-import pytest
+from registration.models import NaicsCode
 from registration.tests.utils.helpers import CommonTestSetup, TestUtils
-
-pytestmark = pytest.mark.django_db
-
-base_endpoint = "/api/registration/"
 
 
 class TestNaicsCodeEndpoint(CommonTestSetup):
-    endpoint = base_endpoint + "naics_codes"
+    endpoint = CommonTestSetup.base_endpoint + "naics_codes"
 
-    def test_unauthorized_users_cannot_get(self):
+    # AUTHORIZATION
+    def test_unauthorized_users_cannot_get_naics_codes(self):
         response = TestUtils.mock_get_with_auth_role(self, "cas_pending")
         assert response.status_code == 401
 
-    def test_get_method_for_200_status(self):
+    def test_authorized_users_can_get_naics_codes(self):
         roles = ["cas_analyst", "cas_admin", "industry_user"]
         for role in roles:
             response = TestUtils.mock_get_with_auth_role(self, role)
             assert response.status_code == 200
-
-    def test_get_method_with_mock_data(self):
-        response = TestUtils.mock_get_with_auth_role(self, "cas_analyst")
-        assert response.status_code == 200
+            assert len(response.json()) == NaicsCode.objects.count()
