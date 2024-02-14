d04dddbefbe1120d499928103351d302939d32b2
marcellmueller <marcel@button.is>
2024-02-14 13:52:19 -0800

    fix: use permenant redirect function instead of redirect

--- client/app/components/routes/select-operator/Page.tsx
+++ client/app/components/routes/select-operator/Page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 import Loading from "@/app/components/loading/SkeletonField";
 import SelectOperator from "@/app/components/routes/select-operator/form/SelectOperator";
 import { BC_GOV_LINKS_COLOR } from "@/app/styles/colors";
@@ -30,17 +30,21 @@ export default async function MyOperatorPage() {
   const { status, id, operator } = userOperator;
   if (status === UserOperatorStatus.PENDING) {
     if (isNew) {
-      return redirect(
+      // Using permanentRedirect instead of redirect to avoid the double rendering bug
+      // https://github.com/vercel/next.js/issues/57257
+      return permanentRedirect(
         `/dashboard/select-operator/received/add-operator/${operator}`,
       );
     }
-    return redirect(
+    return permanentRedirect(
       `/dashboard/select-operator/received/request-access/${operator}`,
     );
   }
 
   if (status === UserOperatorStatus.APPROVED) {
-    return redirect(`/dashboard/select-operator/user-operator/${id}/1`);
+    return permanentRedirect(
+      `/dashboard/select-operator/user-operator/${id}/1`,
+    );
   }
 
   return (
