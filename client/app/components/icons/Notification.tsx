40045edab95651d5dcd3a503f2847933336daffd
marcellmueller <marcel@button.is>
2024-02-14 15:59:03 -0800

    feat: notification tile font is bold

--- client/app/components/icons/Notification.tsx
+++ client/app/components/icons/Notification.tsx
@@ -1,5 +1,5 @@
 const Notification = () => (
-  <div className="w-3 h-3 min-w-3 min-h-3 rounded-full bg-bc-error-red" />
+  <div className="w-2 h-2 min-w-2 min-h-2 rounded-full bg-bc-error-red" />
 );
 
 export default Notification;
