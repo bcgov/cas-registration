e3b0f8e214cbb6e5b9c4c31ad1ed1514539a15a5
Dylan Leard <dylan@button.is>
2024-02-15 15:44:48 -0800

    chore: make prettier happy

--- client/app/components/routes/select-operator/form/ConfirmSelectedOperator.tsx
+++ client/app/components/routes/select-operator/form/ConfirmSelectedOperator.tsx
@@ -29,7 +29,6 @@ export async function getOperatorAccessDeclined(id: number) {
   );
 }
 
-
 export default async function ConfirmSelectedOperator({
   params,
 }: Readonly<{
@@ -39,16 +38,19 @@ export default async function ConfirmSelectedOperator({
   const hasAdmin: boolean | { error: string } = await getOperatorHasAdmin(
     params.id,
   );
-  const accessDeclined: boolean | { error: string} = await getOperatorAccessDeclined(params.id);
+  const accessDeclined: boolean | { error: string } =
+    await getOperatorAccessDeclined(params.id);
 
   if (accessDeclined) {
     const declinedHasAdminJSX: JSX.Element = (
       <>
         <p>
-          Your access request was declined by an Administrator of <b>{operator.legal_name}</b>
+          Your access request was declined by an Administrator of{" "}
+          <b>{operator.legal_name}</b>
         </p>
         <p className="text-center">
-          If you believe this is an error and you should be granted access, please contact the administrator of <b>{operator.legal_name}</b>
+          If you believe this is an error and you should be granted access,
+          please contact the administrator of <b>{operator.legal_name}</b>
         </p>
       </>
     );
@@ -56,37 +58,38 @@ export default async function ConfirmSelectedOperator({
     const declinedNoAdminJSX: JSX.Element = (
       <>
         <p>
-          Your Administrator access request to be the Operation Representative of{" "}
-          <b>{operator.legal_name}</b> was declined.
+          Your Administrator access request to be the Operation Representative
+          of <b>{operator.legal_name}</b> was declined.
         </p>
         <p className="text-center">
-        If you believe this is an error and you should be granted access, please email us at <br />
-        <a
-          href="mailto:GHGRegulator@gov.bc.ca"
-          className="text-black font-bold no-underline"
-        >
-          GHGRegulator@gov.bc.ca
-        </a>
+          If you believe this is an error and you should be granted access,
+          please email us at <br />
+          <a
+            href="mailto:GHGRegulator@gov.bc.ca"
+            className="text-black font-bold no-underline"
+          >
+            GHGRegulator@gov.bc.ca
+          </a>
         </p>
       </>
     );
     return (
       <section className="text-center my-auto text-2xl flex flex-col gap-3">
-      <span>
-        <CancelIcon sx={{ color: "#FF0000", fontSize: 50 }} />
-      </span>
-      {hasAdmin ? declinedHasAdminJSX : declinedNoAdminJSX}
-      <span className="text-sm">
-              <Link
-                href="/dashboard/select-operator"
-                className="underline hover:no-underline text-sm"
-                style={{ color: BC_GOV_LINKS_COLOR }}
-              >
-                Select another operator
-              </Link>
-            </span>
-    </section>
-    )
+        <span>
+          <CancelIcon sx={{ color: "#FF0000", fontSize: 50 }} />
+        </span>
+        {hasAdmin ? declinedHasAdminJSX : declinedNoAdminJSX}
+        <span className="text-sm">
+          <Link
+            href="/dashboard/select-operator"
+            className="underline hover:no-underline text-sm"
+            style={{ color: BC_GOV_LINKS_COLOR }}
+          >
+            Select another operator
+          </Link>
+        </span>
+      </section>
+    );
   }
 
   if (
