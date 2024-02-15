cf9d144dc69a253c5021ddca24406fb44869b713
SeSo <sobhani.sepehr@gmail.com>
2024-02-15 08:10:02 -0800

    chore: add assertion to check the response structure

--- bc_obps/registration/tests/endpoints/test_business_structure_endpoints.py
+++ bc_obps/registration/tests/endpoints/test_business_structure_endpoints.py
@@ -16,3 +16,5 @@ class TestBusinessStructureEndpoint(CommonTestSetup):
             response = TestUtils.mock_get_with_auth_role(self, role)
             assert response.status_code == 200
             assert len(response.json()) == BusinessStructure.objects.count()
+            # Check that we only get the business structures name
+            assert list(response.json()[0].keys()) == ["name"]
