62cceef871ed94c7b08efdc50e1f16f8d649a02d
Dylan Leard <dylan@button.is>
2024-02-16 11:38:34 -0800

    chore: undo debugging change to middleware

--- bc_obps/registration/middleware/current_user_middleware.py
+++ bc_obps/registration/middleware/current_user_middleware.py
@@ -14,18 +14,17 @@ class CurrentUserMiddleware:
         if auth_header:
             try:
                 user_guid = json.loads(auth_header).get('user_guid')
-                user = get_object_or_404(User, user_guid=user_guid)
 
-                # # Try to get the user from cache
-                # cache_key = f"{USER_CACHE_PREFIX}{user_guid}"
-                # user = cache.get(cache_key)
+                # Try to get the user from cache
+                cache_key = f"{USER_CACHE_PREFIX}{user_guid}"
+                user = cache.get(cache_key)
 
-                # if not user:
-                #     # If user is not in cache, fetch from database
-                #     user = get_object_or_404(User, user_guid=user_guid)
+                if not user:
+                    # If user is not in cache, fetch from database
+                    user = get_object_or_404(User, user_guid=user_guid)
 
-                #     # Cache the user for 5 minutes
-                #     cache.set(cache_key, user, 300)
+                    # Cache the user for 5 minutes
+                    cache.set(cache_key, user, 300)
 
                 request.current_user = user  # Attach user to request for access in API endpoints
 
