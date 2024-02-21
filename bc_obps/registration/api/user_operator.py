88683e19afe0946a47b3927693492beef1f09755
Brianna Cerkiewicz <brianna@button.is>
2024-02-21 14:03:30 -0800

    chore: circular import

--- bc_obps/registration/api/user_operator.py
+++ bc_obps/registration/api/user_operator.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 from django.db import transaction
 from django.db import transaction
+# from registration.api.utils.save_operator import save_operator
 
 from registration.api.utils.handle_parent_operators import handle_parent_operators
 from registration.utils import (
@@ -9,6 +10,7 @@ from registration.utils import (
     update_model_instance,
     check_users_admin_request_eligibility,
     check_access_request_matches_business_guid,
+    save_operator
 )
 from django.core.exceptions import ValidationError
 import pytz
@@ -46,42 +48,6 @@ from datetime import datetime
 from django.forms import model_to_dict
 from registration.constants import PAGE_SIZE
 
-
-# Function to save operator data to reuse in POST/PUT methods
-def save_operator(payload: UserOperatorOperatorIn, operator_instance: Operator, user: User):
-    # rollback the transaction if any of the following fails (mostly to prevent orphaned addresses)
-    with transaction.atomic():
-        existing_physical_address = getattr(getattr(operator_instance, 'physical_address', None), 'id', None)
-        existing_mailing_address = existing_physical_address = getattr(getattr(operator_instance, 'mailing_address', None), 'id', None)
-    
-        physical_address, mailing_address = handle_operator_addresses(payload.dict(),existing_physical_address ,existing_mailing_address ).values()
-
-        operator_instance.physical_address = physical_address
-        operator_instance.mailing_address = mailing_address
-
-        # fields to update on the Operator model
-        operator_related_fields = [
-            "legal_name",
-            "trade_name",
-            "physical_address",
-            "mailing_address",
-            "website",
-            "business_structure",
-        ]
-        created_or_updated_operator_instance: Operator = update_model_instance(
-            operator_instance, operator_related_fields, payload.dict()
-        )
-        created_or_updated_operator_instance.save()
-        created_or_updated_operator_instance.set_create_or_update(user.pk)
-        handle_parent_operators(payload.parent_operators_array, created_or_updated_operator_instance, user)
-
-        # get an existing user_operator instance or create a new one with the default role
-        user_operator, created = UserOperator.objects.get_or_create(
-            user=user, operator=created_or_updated_operator_instance
-        )
-        if created:
-            user_operator.set_create_or_update(user.pk)
-        return 200, {"user_operator_id": user_operator.id, 'operator_id': user_operator.operator.id}
 
 
 ##### GET #####
