177dc210216408f21c6d5103a9e1a14590683bc7
shon-button <shon@button.is>
2024-02-23 14:30:00 -0500

    fix: profile all roles

--- client/app/api/auth/[...nextauth]/route.ts
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -78,7 +78,7 @@ export const authOptions: NextAuthOptions = {
           // 🚀 API call: Get user app_role by user_guid from user table
           const responseRole = await actionHandler(
             `registration/user-app-role/${token.user_guid}`,
-            "GET"
+            "GET",
           );
           if (responseRole?.role_name) {
             // user found in table, assign role to token (note: all industry users have the same app role of `industry_user`, and their permissions are further defined by their role in the UserOperator model)
@@ -89,7 +89,7 @@ export const authOptions: NextAuthOptions = {
                 // 🚀 API call: check if user is admin approved
                 const responseAdmin = await actionHandler(
                   `registration/is-approved-admin-user-operator/${token.user_guid}`,
-                  "GET"
+                  "GET",
                 );
                 if (responseAdmin?.approved) {
                   token.app_role = "industry_user_admin"; // note: industry_user_admin a front-end only role. In the db, all industry users have an industry_user app_role, and their permissions are further defined by UserOperator.role
@@ -144,13 +144,9 @@ export const authOptions: NextAuthOptions = {
               body: formData,
             });
             const refreshedToken = await response.json();
-
-            console.log(refreshedToken);
             if (!response.ok) throw refreshedToken;
             token = refreshedToken;
             token.access_token_expires_at = refreshedToken.expires_at;
-
-            console.log(token);
           } catch (error) {
             token.error = Errors.ACCESS_TOKEN;
           }
--- client/e2e/pages/authenticated/dashboard/profile/page.spec.ts
+++ client/e2e/pages/authenticated/dashboard/profile/page.spec.ts
@@ -6,58 +6,14 @@ import { DashboardPOM } from "@/e2e/poms/dashboard";
 import { ProfilePOM } from "@/e2e/poms/profile";
 // ☰ Enums
 import { UserRole } from "@/e2e/utils/enums";
-// 🥞 DB CRUD
-import { deleteUserRecord, upsertUserRecord } from "@/e2e/utils/queries";
 // ℹ️ Environment variables
 import * as dotenv from "dotenv";
 dotenv.config({ path: "./e2e/.env.local" });
 
-// 📚 Declare a beforeAll hook that is executed once per worker process before all tests.
-// 🥞 Set DB for profile update
-/*
-For industry_user:
-- create user
-For "new user":
--  delete record in the db so that on "new user" login the ID will have no app_role
-*/
-test.beforeAll(async () => {
-  try {
-    // 👤 upsert industry_user: bc-cas-dev-secondary
-    await upsertUserRecord(UserRole.INDUSTRY_USER);
-    // 👤 delete new user: bc-cas-dev-three
-    await deleteUserRecord(process.env.E2E_NEW_USER_GUID as string);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error("❌ Error in Db setup for profile roles:", error);
-    throw error;
-  }
-});
-// 📚 Declare an afterAll hook that is executed once per worker process after all tests.
-/*
-For "new user":
--  delete record created in "Test Profile for none\Test Update"
-*/
-test.afterAll(async () => {
-  try {
-    // Cleanup the environment for other tests
-    // 👤 delete new user: bc-cas-dev-three
-    await deleteUserRecord(process.env.E2E_NEW_USER_GUID as string);
-  } catch (error) {
-    throw error;
-  }
-});
 // 🏷 Annotate test suite as serial
 test.describe.configure({ mode: "serial" });
 // ➰ Loop through the entries of UserRole enum
 for (let [role, value] of Object.entries(UserRole)) {
-  // Ensures that certain roles are not tested in the current context
-  if (
-    value === UserRole.CAS_PENDING ||
-    value === UserRole.CAS_ANALYST ||
-    value === UserRole.INDUSTRY_USER_ADMIN
-  ) {
-    continue;
-  }
   role = "E2E_" + role;
   const storageState = process.env[role + "_STORAGE"] as string;
   test.describe(`Test Profile for ${value}`, () => {
