6ed3b2c30b28aa87dcc8f41f30c9a56eb4bb08b5
SeSo <sobhani.sepehr@gmail.com>
2024-02-27 15:32:34 -0800

    fix: add error message for statutory declaration field

--- client/app/utils/customTransformErrors.ts
+++ client/app/utils/customTransformErrors.ts
@@ -5,11 +5,15 @@ export const customTransformErrors = (
   customFormatsErrorMessages: { [key: string]: string },
 ) => {
   return errors.map((error) => {
-    if (
-      error?.property === ".cra_business_number" ||
-      ".po_cra_business_number"
-    ) {
-      error.message = customFormatsErrorMessages.cra_business_number;
+    if (error?.property) {
+      if (
+        [".cra_business_number", ".po_cra_business_number"].includes(
+          error.property,
+        )
+      )
+        error.message = customFormatsErrorMessages.cra_business_number;
+      if (error.property === ".statutory_declaration")
+        error.message = customFormatsErrorMessages.statutory_declaration;
     }
     if (error?.name === "required") {
       error.message = "Required field";
