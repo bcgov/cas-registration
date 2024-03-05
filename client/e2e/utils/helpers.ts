3ef8eaf57e7d5b303689eeb09b0f1eb087270d5b
SeSo <sobhani.sepehr@gmail.com>
2024-03-05 15:26:13 -0800

    test: add e2e test helper util

--- client/e2e/utils/helpers.ts
+++ client/e2e/utils/helpers.ts
@@ -52,13 +52,7 @@ export async function fieldsUpdate(page: Page) {
   }
 }
 
-export async function getAllFields(page: Page) {
-  const fields = await page.$$("fieldset#root");
+export async function getAllFormInputs(page: Page) {
+  const fields = await page.locator("input").all();
   return fields;
-}
-
-export async function getReadonlyFields(page: Page) {
-  const fieldset = await page.$("fieldset#root");
-  const readonlyFields = await fieldset?.$$("readonly");
-  return readonlyFields;
 }
