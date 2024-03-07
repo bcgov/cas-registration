aa02b87e1d4f4941bf00359110bac4b8332d7800
Ballard <ballard@button.is>
2024-03-07 17:53:46 -0500

    chore: add reporting app and model in django

---
+++ bc_obps/reporting/apps.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import pre_migrate
+from django.apps import AppConfig
+from django.db import connection
+
+
+def create_erc_schemas(sender, app_config, **kwargs):
+    cursor = connection.cursor()
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS erc")
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS erc_history")
+
+class ReportingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reporting'
+
+    def ready(self):
+        pre_migrate.connect(create_erc_schemas, sender=self)
