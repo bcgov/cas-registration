1cb1056f098c8a7391ecdd6de71cd8f9fb4988f5
Mike Vesprini <mike@button.is>
2024-03-08 13:01:35 -0800

    chore: add a sample shared error component

--- bciers/libs/shared/components/src/lib/error/Error.tsx
+++ bciers/libs/shared/components/src/lib/error/Error.tsx
@@ -1,4 +1,4 @@
-import styles from './Error.module.css';
+import styles from './error.module.css';
 
 /* eslint-disable-next-line */
 export interface ErrorProps {}
@@ -6,7 +6,7 @@ export interface ErrorProps {}
 export function Error(props: ErrorProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Error!</h1>
+      <h1>Sample shared error message</h1>
     </div>
   );
 }
