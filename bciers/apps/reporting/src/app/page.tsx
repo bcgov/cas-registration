c16bdb6c03b97c448d96530a492c8fb7076e6e1d
Mike Vesprini <mike@button.is>
2024-03-08 13:33:43 -0800

    chore: add sample component to page

--- bciers/apps/reporting/src/app/page.tsx
+++ bciers/apps/reporting/src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Error } from '@bciers/components';
+import { Sample } from '@bciers/components';
 export default function Index() {
   return (
     <div>
       Here is a sample shared component, to prove the work!
-      <Error />
+      <Sample />
     </div>
   );
 }
