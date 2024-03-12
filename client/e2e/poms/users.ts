a2341fc1e901247ef873da5e85c466c63889084a
marcellmueller <marcel@button.is>
2024-03-12 09:37:29 -0700

    test: add industry admin user approval test

--- client/e2e/poms/users.ts
+++ client/e2e/poms/users.ts
@@ -5,7 +5,7 @@
  */
 import { Page, expect } from "@playwright/test";
 // ☰ Enums
-import { AppRoute } from "@/e2e/utils/enums";
+import { AppRoute, UserOperatorStatus } from "@/e2e/utils/enums";
 // ℹ️ Environment variables
 import * as dotenv from "dotenv";
 dotenv.config({ path: "./e2e/.env.local" });
@@ -25,7 +25,68 @@ export class UsersPOM {
 
   async urlIsCorrect() {
     const path = this.url;
-    const currentUrl = await this.page.url();
+    const currentUrl = this.page.url();
     expect(currentUrl.toLowerCase()).toMatch(path.toLowerCase());
+  }
+
+  async userStatusIsUpdated(expectedStatus: string, rowIndex: number = 0) {
+    // Get all users from the table
+    const userRows = await this.page.$$(".MuiDataGrid-row");
+    const user = userRows[rowIndex];
+
+    const status = await user
+      .$$(".MuiDataGrid-cell")
+      .then((cells) => cells[5].textContent());
+
+    expect(status).toStrictEqual(expectedStatus);
+  }
+
+  async approveOrDeclineUser(
+    action: UserOperatorStatus.APPROVED | UserOperatorStatus.DECLINED,
+    rowIndex: number = 0,
+  ) {
+    const userRows = await this.page.$$(".MuiDataGrid-row");
+    const user = userRows[rowIndex];
+    const status = await user
+      .$$(".MuiDataGrid-cell")
+      .then((cells) => cells[5].textContent());
+
+    expect(status).toStrictEqual(UserOperatorStatus.PENDING);
+    const buttonText =
+      action === UserOperatorStatus.APPROVED ? "Approve" : "Decline";
+    const approveButton = await user
+      .$$(".MuiDataGrid-cell")
+      .then((cells) => cells[6].$(`button:has-text('${buttonText}')`));
+
+    await approveButton?.click();
+
+    await this.page.waitForTimeout(2000);
+
+    // Verify that the user status is updated to "Approved"
+    await this.userStatusIsUpdated(action, rowIndex);
+  }
+
+  async undoUserStatusChange(
+    statusToUndo: string = UserOperatorStatus.APPROVED ||
+      UserOperatorStatus.DECLINED,
+    rowIndex: number = 0,
+  ) {
+    const userRows = await this.page.$$(".MuiDataGrid-row");
+    const user = userRows[rowIndex];
+    const status = await user
+      .$$(".MuiDataGrid-cell")
+      .then((cells) => cells[5].textContent());
+
+    expect(status).toStrictEqual(statusToUndo);
+    const undoButton = await user
+      .$$(".MuiDataGrid-cell")
+      .then((cells) => cells[6].$("button:has-text('Undo')"));
+
+    await undoButton?.click();
+
+    await this.page.waitForTimeout(2000);
+
+    // Verify that the user status is updated to "Pending"
+    await this.userStatusIsUpdated(UserOperatorStatus.PENDING, rowIndex);
   }
 }
