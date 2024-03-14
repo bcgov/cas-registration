db1c42e36110f763a8867d84f3e0cf9ea4fcd615
Brianna Cerkiewicz <brianna@button.is>
2024-03-14 07:19:39 -0700

    chore: update test-setup endpoint and commands

--- bc_obps/registration/management/commands/load_fixtures.py
+++ bc_obps/registration/management/commands/load_fixtures.py
@@ -1,4 +1,3 @@
-import pathlib
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
@@ -6,6 +5,20 @@ from django.core.management import call_command
 class Command(BaseCommand):
     help = 'Load all fixtures in app directories'
 
-    def handle(self, *args, **kwargs):
-        cmd_args = list(pathlib.Path().glob('./registration/fixtures/*/*.json'))
-        call_command('loaddata', *cmd_args)
+    def handle(self, *args, **options):
+        fixture_dir = 'registration/fixtures/mock'
+        # Order of fixtures is important
+        fixtures = [
+            f'{fixture_dir}/address.json',
+            f'{fixture_dir}/user.json',
+            f'{fixture_dir}/contact.json',
+            f'{fixture_dir}/bc_obps_regulated_operation.json',
+            f'{fixture_dir}/operator.json',
+            f'{fixture_dir}/operation.json',
+            f'{fixture_dir}/document.json',
+            f'{fixture_dir}/user_operator.json',
+            f'{fixture_dir}/parent_operator.json',
+        ]
+        for fixture in fixtures:
+            self.stdout.write(self.style.SUCCESS(f"Loading: {fixture}"))
+            call_command('loaddata', fixture)
