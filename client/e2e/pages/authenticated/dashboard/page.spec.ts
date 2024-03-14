f2166b0232d0655cbababf19c4a7680197d37e11
marcellmueller <marcel@button.is>
2024-03-14 09:13:48 -0700

    test: set roles as happo screenshot variants

--- client/e2e/pages/authenticated/dashboard/page.spec.ts
+++ client/e2e/pages/authenticated/dashboard/page.spec.ts
@@ -41,7 +41,7 @@ test.beforeAll(async () => {
     await upsertUserOperatorRecord(
       process.env.E2E_INDUSTRY_USER_ADMIN_GUID as string,
       AppRole.ADMIN,
-      UserOperatorStatus.APPROVED,
+      UserOperatorStatus.APPROVED
     );
     // Scenario FrontEndRoles.INDUSTRY_USER where userOperatorStatus !== UserOperatorStatus.APPROVED
     // Shows "Select Operator\...1 pending action(s) required" bceidSelectOperatorTile
@@ -49,7 +49,7 @@ test.beforeAll(async () => {
     // Upsert a User record: bc-cas-dev-secondary
     await upsertUserRecord(UserRole.INDUSTRY_USER);
     await deleteUserOperatorRecord(
-      process.env.E2E_INDUSTRY_USER_GUID as string,
+      process.env.E2E_INDUSTRY_USER_GUID as string
     );
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -93,10 +93,9 @@ test.describe("Test Dashboard Page", () => {
 
             const pageContent = page.locator("html");
             await happoPlaywright.screenshot(dashboardPage.page, pageContent, {
-              component: `${role} Dashboard page`,
-              variant: "default",
+              component: "Dashboard page",
+              variant: role,
             });
-
             break;
         }
       });
