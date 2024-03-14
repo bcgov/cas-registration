8742f8a4e4165736bc0808ac29c6dbe5c75e38aa
SeSo <sobhani.sepehr@gmail.com>
2024-03-14 11:03:43 -0700

    chore: suppress all sonar cloud warning on a specific file temp

--- bc_obps/registration/management/commands/truncate_dev_data_tables.py
+++ bc_obps/registration/management/commands/truncate_dev_data_tables.py
@@ -1,3 +1,5 @@
+# @SuppressWarnings("all")
+# Suppressing all warnings in this file because it is a management command that is not intended to be used in production
 from django.core.management.base import BaseCommand
 from django.db import connection
 
@@ -20,14 +22,12 @@ class Command(BaseCommand):
             for row in cursor.fetchall():
                 table_name = row[0]
                 if table_name not in tables_with_production_data:
-                    cursor.execute('TRUNCATE TABLE erc."{}" RESTART IDENTITY CASCADE;'.format(table_name))  # NOSONAR
+                    cursor.execute('TRUNCATE TABLE erc."{}" RESTART IDENTITY CASCADE;'.format(table_name))
 
             # Truncate tables in 'erc_history' schema
             cursor.execute("SELECT tablename FROM pg_tables WHERE schemaname = 'erc_history';")
             for row in cursor.fetchall():
                 table_name = row[0]
-                cursor.execute(
-                    'TRUNCATE TABLE erc_history."{}" RESTART IDENTITY CASCADE;'.format(table_name)  # NOSONAR
-                )
+                cursor.execute('TRUNCATE TABLE erc_history."{}" RESTART IDENTITY CASCADE;'.format(table_name))
 
         self.stdout.write(self.style.SUCCESS('All tables have been truncated.'))
