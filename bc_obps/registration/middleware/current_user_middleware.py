a9e1a8295125b0c34607e854d14e55984ac8b345
SeSo <sobhani.sepehr@gmail.com>
2024-03-15 17:26:41 -0700

    chore: revert spec work
    This reverts commit 8216f56cca43c7f2f5d397f1453a5ae5a4e54cce.

--- bc_obps/registration/middleware/current_user_middleware.py
+++ bc_obps/registration/middleware/current_user_middleware.py
@@ -15,17 +15,17 @@ class CurrentUserMiddleware:
         if auth_header:
             try:
                 user_guid = UUID(json.loads(auth_header).get('user_guid'), version=4)
-                user = get_object_or_404(User, user_guid=user_guid)
-                # # Try to get the user from cache
-                # cache_key = f"{USER_CACHE_PREFIX}{user_guid}"
-                # user = cache.get(cache_key)
 
-                # if not user:
-                #     # If user is not in cache, fetch from database
-                #     user = get_object_or_404(User, user_guid=user_guid)
+                # Try to get the user from cache
+                cache_key = f"{USER_CACHE_PREFIX}{user_guid}"
+                user = cache.get(cache_key)
 
-                #     # Cache the user for 5 minutes
-                #     cache.set(cache_key, user, 300)
+                if not user:
+                    # If user is not in cache, fetch from database
+                    user = get_object_or_404(User, user_guid=user_guid)
+
+                    # Cache the user for 5 minutes
+                    cache.set(cache_key, user, 300)
 
                 request.current_user = user  # Attach user to request for access in API endpoints
 
