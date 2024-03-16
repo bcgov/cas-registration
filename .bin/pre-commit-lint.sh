ede8b3de31098b41de802d28e4344b7d3af1b10d
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-03-15 17:05:21 -0700

    chore: eslint doesnt look for extra config files

--- .bin/pre-commit-lint.sh
+++ .bin/pre-commit-lint.sh
@@ -6,4 +6,4 @@ pushd client || exit 1
 files=("$@")
 files=("${files[@]/#/../}") # add ../ to each element
 
-yarn run eslint -c ./.eslintrc.js "${files[@]}"
+yarn run eslint --no-eslintrc -c ./.eslintrc.js "${files[@]}"
