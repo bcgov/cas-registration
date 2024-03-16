30cfd7de8ad0a947534b2e1eee1c28dceccb8c94
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-03-15 17:05:37 -0700

    feat: main page with BC headers

--- bciers/apps/reporting/src/app/page.tsx
+++ bciers/apps/reporting/src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Sample } from '@bciers/components';
+import { Grid } from '@mui/material';
+import Main from 'registration/components/layout/Main';
+
 export default function Index() {
   return (
-    <div>
-      Here is a sample shared component, to prove the work!
-      <Sample />
-    </div>
+    <Main>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          marginTop: '24px',
+          marginBottom: '48px',
+        }}
+      >
+        Here is a sample shared component, to prove the work!
+        <Sample />
+      </Grid>
+    </Main>
   );
 }
