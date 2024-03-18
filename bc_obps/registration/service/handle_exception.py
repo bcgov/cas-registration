89e22e83354a763fd219ff40d389eb2a7c1913bf
Brianna Cerkiewicz <brianna@button.is>
2024-03-18 13:53:55 -0700

    chore: create handle_exception service

---
+++ bc_obps/registration/service/handle_exception.py
@@ -0,0 +1,16 @@
+from registration.utils import (
+    generate_useful_error,
+)
+from django.core.exceptions import ValidationError
+
+from django.http import Http404
+
+
+def handle_exception(error):
+    if isinstance(error, ValidationError):
+        return 400, {"message": generate_useful_error(error)}
+    elif isinstance(error, Http404):
+
+        return 404, {"message": "Not Found"}
+    else:
+        return 400, {"message": str(error)}
