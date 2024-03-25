5ae82a3f11ad8625ce16b60f5f4054518bc8cca5
Brianna Cerkiewicz <brianna@button.is>
2024-03-25 08:25:53 -0700

    chore: fix pylint

--- bc_obps/handle_exception.py
+++ bc_obps/handle_exception.py
@@ -1,15 +1,20 @@
+"""
+Module: handle_exception.py
+Description: This module handles http exceptions.
+"""
+from django.http import Http404
+from django.core.exceptions import ValidationError
 from registration.utils import (
     generate_useful_error,
 )
-from django.core.exceptions import ValidationError
-
-from django.http import Http404
 
 
 def handle_exception(error):
+    """
+    This function handles exceptions for BCEIRS. Returns a 4xx status.
+    """
     if isinstance(error, ValidationError):
         return 422, {"message": generate_useful_error(error)}
-    elif isinstance(error, Http404):
+    if isinstance(error, Http404):
         return 404, {"message": "Not Found"}
-    else:
-        return 400, {"message": str(error)}
+    return 400, {"message": str(error)}
