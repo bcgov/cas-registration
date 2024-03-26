6c1c116df4b9f4a592fd19b9355223a19dcb5544
marcellmueller <marcel@button.is>
2024-03-26 15:43:22 -0700

    test: experiment with vitest

---
+++ client/vitest.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import path from "path";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    include: ["tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["node_modules", "dist", "e2e"],
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+      app: path.resolve(__dirname, "./app"),
+    },
+    setupFiles: ["./tests/setup.ts"],
+  },
+});
