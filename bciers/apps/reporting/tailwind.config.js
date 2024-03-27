bc4cf11c6c129d0e1aaf22dfa98c229066ec7032
Josh Gamache <joshua@button.is>
2024-03-27 15:52:53 -0600

    feat: add tailwindcss config to monorepo

--- bciers/apps/reporting/tailwind.config.js
+++ bciers/apps/reporting/tailwind.config.js
@@ -3,15 +3,12 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
   content: [
     join(
       __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{js,jsx,ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
---
+++ bciers/libs/shared/styles/src/lib/colors.ts
@@ -0,0 +1,12 @@
+// BC GOV colors
+export const BC_GOV_LINKS_COLOR: string = "#1A5A96";
+export const BC_GOV_PRIMARY_BRAND_COLOR_BLUE: string = "#003366";
+export const BC_GOV_BACKGROUND_COLOR_BLUE: string = "#38598a";
+export const BC_GOV_TEXT: string = "#313132";
+// App specific colors
+export const DARK_GREY_BG_COLOR: string = "#E5E5E5";
+export const LIGHT_GREY_BG_COLOR: string = "#fafafc";
+export const BC_GOV_COMPONENTS_GREY: string = "#606060";
+export const BC_GOV_YELLOW: string = "#FCBA19";
+export const BC_GOV_SEMANTICS_RED: string = "#D8292F";
+export const BC_GOV_SEMANTICS_GREEN: string = "#2E8540";
