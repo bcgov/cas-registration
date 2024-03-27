0c0f2536faba14134c53d50e2545443cb1bde379
SeSo <sobhani.sepehr@gmail.com>
2024-03-27 16:36:06 -0700

    chore: remove redundant enum related to ticket 428

--- bc_obps/registration/enums/enums.py
+++ bc_obps/registration/enums/enums.py
@@ -1,13 +1,6 @@
 from enum import Enum
 
 
-class AppRoles(Enum):
-    CAS_ADMIN = "cas_admin"
-    CAS_ANALYST = "cas_analyst"
-    CAS_PENDING = "cas_pending"
-    INDUSTRY_USER = "industry_user"  # note: industry users all have the same app_role of "industry_user". Their permissions (admin vs. reporter vs. none) are further defined in the UserOperator table.
-
-
 class IdPs(Enum):
     IDIR = "idir"
     BCEIDBUSINESS = "bceidbusiness"
