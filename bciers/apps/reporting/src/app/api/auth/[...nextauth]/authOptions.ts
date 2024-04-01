46ea24b8bf1b75e8e30d53a3d8a1c88dfa4a7abb
Josh Gamache <joshua@button.is>
2024-04-01 14:26:06 -0600

    style: match prettier config with reporting

--- bciers/apps/reporting/src/app/api/auth/[...nextauth]/authOptions.ts
+++ bciers/apps/reporting/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from 'next-auth';
-import { authOptions as regAuthOptions } from 'registration/api/auth/[...nextauth]/route';
+import { NextAuthOptions } from "next-auth";
+import { authOptions as regAuthOptions } from "registration/api/auth/[...nextauth]/route";
 
 export const authOptions = regAuthOptions as NextAuthOptions;
--- bciers/libs/shared/styles/src/index.ts
+++ bciers/libs/shared/styles/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/colors';
+export * from "./lib/colors";
