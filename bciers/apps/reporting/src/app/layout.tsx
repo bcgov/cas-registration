64c3a73a8e203eb6e6841a14c321b6a30bdec07f
Josh Gamache <joshua@button.is>
2024-04-01 14:19:21 -0600

    chore: add theme provider to monorepo

--- bciers/apps/reporting/src/app/layout.tsx
+++ bciers/apps/reporting/src/app/layout.tsx
@@ -1,9 +1,17 @@
+/*
+A layout is UI that is shared between routes.
+The app directory must include a root app/layout.js.
+The root layout must define <html> and <body> tags.
+You should not manually add <head> tags such as <title> and <meta> to root layouts. Instead, you should use the Metadata API which automatically handles advanced requirements such as streaming and de-duplicating <head> elements.
+*/
+
 import './global.css';
 import {
   metadata as regMetadata,
   default as RegRootLayout,
   viewport as regViewport,
 } from 'registration/layout';
+import { ThemeRegistry } from '@bciers/components';
 
 export const metadata = regMetadata;
 export const viewport = regViewport;
@@ -15,7 +23,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body id="__next">{children}</body>
+      <body id="__next">
+        {
+          //👇️ provide MUI custom theme to the components within the layout
+        }
+        <ThemeRegistry>{children}</ThemeRegistry>
+      </body>
     </html>
   );
 }
