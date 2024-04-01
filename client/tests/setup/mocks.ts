8336bd0167869a79e4e1218c82b700e46f010686
marcellmueller <marcel@button.is>
2024-04-01 11:26:10 -0700

    chore: cleanup mocks setup

--- client/tests/setup/mocks.ts
+++ client/tests/setup/mocks.ts
@@ -1,22 +1,45 @@
+import { Query, Router, Session } from "@/tests/setup/types";
+
+// To return a different value for each test, you can import this file and use mockReturnValue
+// to set the return value for the function you want to mock. For example:
+// import mocks from "@/tests/setup/mocks";
+//
+// mocks.useRouter.mockReturnValue({
+//    query: {
+//      formSection: "3",
+//      operation: "test-operation-id",
+//    },
+//    replace: vi.fn(),
+//  });
+
 const mocks = {
-  useRouter: vi.fn(() => ({
-    query: {
-      formSection: "1",
-      operation: "create",
-    },
-    replace: vi.fn(),
-  })),
-  useParams: vi.fn(() => ({
-    formSection: "1",
-    operation: "create",
-  })),
-  useSession: vi.fn(() => ({
-    data: {
-      user: {
-        app_role: "cas_admin",
-      },
-    },
-  })),
+  useRouter: vi.fn(
+    () =>
+      ({
+        query: {
+          formSection: "1",
+          operation: "create",
+        },
+        replace: vi.fn(),
+      }) as Router,
+  ),
+  useParams: vi.fn(
+    () =>
+      ({
+        formSection: "1",
+        operation: "create",
+      }) as Query,
+  ),
+  useSession: vi.fn(
+    () =>
+      ({
+        data: {
+          user: {
+            app_role: "cas_admin",
+          },
+        },
+      }) as Session,
+  ),
   getServerSession: vi.fn(),
 };
 
