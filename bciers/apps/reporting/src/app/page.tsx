d04b210999b58a78ec19aa9db7b4c2f3423bddd8
Josh Gamache <joshua@button.is>
2024-04-02 00:19:49 -0600

    chore: add image assets as lib to nx

--- bciers/apps/reporting/src/app/page.tsx
+++ bciers/apps/reporting/src/app/page.tsx
@@ -1,22 +1,25 @@
-'use client';
+"use client";
 
-import { Button, Grid } from '@mui/material';
-import { signIn } from 'next-auth/react';
-import Main from 'registration/components/layout/Main';
+import { Button, Grid } from "@mui/material";
+import { signIn } from "next-auth/react";
+import { Main } from "@bciers/components/server";
+import Logo from "@bciers/img/src/lib/BCID_CleanBC_rev_tagline_colour.svg";
+import Image from "next/image";
 
 export default function Index() {
   const handleIdirLogin = () => {
-    signIn('keycloak', undefined, { kc_idp_hint: 'idir' });
+    signIn("keycloak", undefined, { kc_idp_hint: "idir" });
   };
 
   return (
     <Main>
+      <Image src={Logo} alt="testing" width="200" height="42.67" />
       <Grid
         container
         spacing={2}
         sx={{
-          marginTop: '24px',
-          marginBottom: '48px',
+          marginTop: "24px",
+          marginBottom: "48px",
         }}
       >
         <Button
