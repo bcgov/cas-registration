dcf26f3e26a3c44cacd028be02ac37a0515d9bb2
marcellmueller <marcel@button.is>
2024-04-02 13:58:55 -0700

    refactor: update mock exports

--- client/tests/setup/mocks.ts
+++ client/tests/setup/mocks.ts
@@ -1,8 +1,10 @@
 // To return a different value for each test, you can import this file and use mockReturnValue
-// to set the return value for the function you want to mock. For example:
-// import mocks from "@/tests/setup/mocks";
+// to set the return value for the function you want to mock using mockReturnValue
+// or mockReturnValueOnce. Example:
 //
-// mocks.useRouter.mockReturnValue({
+// import { useRouter } from "@/tests/setup/mocks";
+//
+// useRouter.mockReturnValue({
 //  query: {
 //      formSection: "3",
 //      operation: "test-operation-id",
@@ -10,12 +12,10 @@
 //    replace: vi.fn(),
 //  });
 
-const mocks = {
-  actionHandler: vi.fn(),
-  useRouter: vi.fn(),
-  useParams: vi.fn(),
-  useSession: vi.fn(),
-  getServerSession: vi.fn(),
-};
+const actionHandler = vi.fn();
+const useRouter = vi.fn();
+const useParams = vi.fn();
+const useSession = vi.fn();
+const getServerSession = vi.fn();
 
-export default mocks;
+export { actionHandler, getServerSession, useRouter, useParams, useSession };
