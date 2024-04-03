0ea43803c49a5ece55124961ed69c4df6e381ee5
Josh Gamache <joshua@button.is>
2024-04-03 12:02:08 -0600

    fix: update config to handle depricated key

--- bciers/libs/shared/components/vite.config.ts
+++ bciers/libs/shared/components/vite.config.ts
@@ -16,12 +16,8 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: {
-      dir: "../../../node_modules/.vitest",
-    },
     environment: "jsdom",
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-
     reporters: ["default"],
     coverage: {
       reportsDirectory: "../../../coverage/libs/shared/components",
