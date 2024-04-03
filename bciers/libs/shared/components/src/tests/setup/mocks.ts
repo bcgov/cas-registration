9eca05685c8cf44f1e63498870593deae8a14dfc
Josh Gamache <joshua@button.is>
2024-04-03 15:07:42 -0600

    chore: add global setup used in Reg

---
+++ bciers/libs/shared/components/src/tests/setup/mocks.ts
@@ -0,0 +1,21 @@
+// To return a different value for each test, you can import this file and use mockReturnValue
+// to set the return value for the function you want to mock using mockReturnValue
+// or mockReturnValueOnce. Example:
+//
+// import { useRouter } from "@/tests/setup/mocks";
+//
+// useRouter.mockReturnValue({
+//  query: {
+//      formSection: "3",
+//      operation: "test-operation-id",
+//    },
+//    replace: vi.fn(),
+//  });
+
+const actionHandler = vi.fn();
+const useRouter = vi.fn();
+const useParams = vi.fn();
+const useSession = vi.fn();
+const getServerSession = vi.fn();
+
+export { actionHandler, getServerSession, useRouter, useParams, useSession };
