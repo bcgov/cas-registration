25addb003d694b242e51c2a23595fb5ad76b8998
Andrea Williams <andrea.williams@gov.bc.ca>
2024-04-03 17:06:52 -0700

    Chore: replaced prints w logging in email_service

--- bc_obps/common_utils/email/email_service.py
+++ bc_obps/common_utils/email/email_service.py
@@ -1,10 +1,11 @@
-import json
 from datetime import datetime, timedelta
-from typing import List
 from uuid import UUID
+import logging
 
 import requests
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 class EmailService:
@@ -15,7 +16,7 @@ class EmailService:
         self.token_endpoint = settings.CHES_TOKEN_ENDPOINT
         self.token = None
         self.token_expiry = datetime.now()
-        print(f'Initializing EmailService for clientID {self.client_id} to connect to {self.api_url}')
+        logger.info(f'Initializing EmailService for clientID {self.client_id} to connect to {self.api_url}')
 
     def _get_token(self):
         if not self.token or self.token_expiry < datetime.now():
@@ -29,11 +30,10 @@ class EmailService:
                 self.token = response.json()["access_token"]
                 self.token_expiry = datetime.now() + timedelta(seconds=response.json()["expires_in"])
             else:
-                raise Exception("Failed to retrieve CHES access token")
+                logger.error("Failed to retrieve CHES access token")
 
     def _make_request(self, endpoint, method='GET', data: any = None):
         headers = {"Authorization": f'Bearer {self.token}'} if self.token else {}
-        print(f'Received request to {method} to {endpoint} with payload {data}')
         if method == 'GET':
             response = requests.get(self.api_url + endpoint, headers=headers, timeout=10)
         elif method == 'POST':
@@ -49,7 +49,8 @@ class EmailService:
             response = self._make_request("/health")
             return response.json()
         except Exception as exc:
-            raise Exception("Exception in /email/health_check ", str(exc))
+            logger.error(f'Exception in /email/health_check {str(exc)}')
+            raise
 
     def get_message_status(self, message_id: UUID):
         """
@@ -65,7 +66,8 @@ class EmailService:
             response = self._make_request(f'/status/{message_id}')
             return response.json()
         except Exception as exc:
-            raise Exception(f'Exception retrieving message status for {message_id} - ', str(exc))
+            logger.error(f'Exception retrieving message status for {message_id} - {str(exc)}')
+            raise
 
     def send_email(self, email_data: dict):
         self._get_token()
@@ -77,7 +79,8 @@ class EmailService:
             )
             return response.json()
         except Exception as exc:
-            raise Exception("Exception in send_email ", str(exc))
+            logger.error(f'Exception in send_email {str(exc)}')
+            raise
 
     def merge_template_and_send(self, email_template_data: dict):
         self._get_token()
@@ -85,4 +88,5 @@ class EmailService:
             response = self._make_request("/emailMerge", method='POST', data=email_template_data)
             return response.json()
         except Exception as exc:
-            raise Exception('Exception in merging template and sending! ', str(exc))
+            logger.error(f'Exception in merging template and sending! - {str(exc)}')
+            raise
