12f6ff752411e78abb034865c026403e0cf85b5e
SeSo <sobhani.sepehr@gmail.com>
2024-04-05 14:11:52 -0700

    feat: add common app files and configs

---
+++ bc_obps/common/apps.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import pre_migrate
+from django.apps import AppConfig
+from django.db import connection
+
+
+def create_common_schema(sender, app_config, **kwargs):
+    cursor = connection.cursor()
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS common")
+
+
+class CommonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'common'
+
+    def ready(self):
+        pre_migrate.connect(create_common_schema, sender=self)
