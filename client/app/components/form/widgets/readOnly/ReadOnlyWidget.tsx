0c70d131ff60183e2547c2a77c0404c36f899cc5
marcellmueller <marcel@button.is>
2024-04-08 12:55:41 -0700

    refactor: clean up read only widgets

--- client/app/components/form/widgets/readOnly/ReadOnlyWidget.tsx
+++ client/app/components/form/widgets/readOnly/ReadOnlyWidget.tsx
@@ -2,7 +2,7 @@ import { WidgetProps } from "@rjsf/utils/lib/types";
 
 const ReadOnlyWidget: React.FC<WidgetProps> = ({ id, value }) => {
   return (
-    <div id={id} className="w-full min-h-[50px] flex items-center">
+    <div id={id} className="read-only-widget">
       {value}
     </div>
   );
