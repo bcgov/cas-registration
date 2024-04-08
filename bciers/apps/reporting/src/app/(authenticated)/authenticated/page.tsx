3a952bf84103f611114bf9d98480061f846dee75
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-04-08 12:46:33 -0700

    chore: eslint

--- bciers/apps/reporting/src/app/(authenticated)/authenticated/page.tsx
+++ bciers/apps/reporting/src/app/(authenticated)/authenticated/page.tsx
@@ -1,21 +1,19 @@
-import { Grid } from "@mui/material";
-import Main from "registration/components/layout/Main";
+import { Grid } from '@mui/material';
+import Main from 'registration/components/layout/Main';
 
 export default function Index() {
- 
-    return (
-      <Main>
-        <Grid
-          container
-          spacing={2}
-          sx={{
-            marginTop: '24px',
-            marginBottom: '48px',
-          }}
-        >
-          You have been signed in!
-        </Grid>
-      </Main>
-    );
-  }
-  
+  return (
+    <Main>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          marginTop: '24px',
+          marginBottom: '48px',
+        }}
+      >
+        You have been signed in!
+      </Grid>
+    </Main>
+  );
+}
