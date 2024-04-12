105d5002aa910d7bf50948544ede911f4e9a2e4d
SeSo <sobhani.sepehr@gmail.com>
2024-04-12 11:45:04 -0700

    chore: add email notification data models

--- bc_obps/common/admin.py
+++ bc_obps/common/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from common.models import EmailNotificationTemplate, EmailNotification
 
-# Register your models here.
+
+@admin.register(EmailNotificationTemplate)
+class EmailNotificationTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'body')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(EmailNotification)
+class EmailNotificationAdmin(admin.ModelAdmin):
+    list_display = ('transaction_id', 'message_id', 'template')
+    search_fields = ('transaction_id',)
+    ordering = ('transaction_id',)
