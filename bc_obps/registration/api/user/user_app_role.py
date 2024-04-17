aa650bda6e7cc7f1f6f6afe095a310e4aa5978f6
Brianna Cerkiewicz <brianna@button.is>
2024-04-17 12:10:58 -0700

    chore: debug e2e

--- bc_obps/registration/api/user/user_app_role.py
+++ bc_obps/registration/api/user/user_app_role.py
@@ -1,3 +1,4 @@
+from registration.models import User
 from registration.decorators import handle_http_errors
 from service.data_access_service.user_service import UserDataAccessService
 from registration.schema import UserAppRoleOut, Message
@@ -10,4 +11,9 @@ from ninja.responses import codes_4xx
 )
 @handle_http_errors()
 def get_user_role(request, user_guid: str):
-    return UserDataAccessService.get_app_role(user_guid)
+    # return UserDataAccessService.get_app_role(user_guid)
+    try:
+        user = User.objects.only('app_role').select_related('app_role').get(user_guid=user_guid)
+    except User.DoesNotExist:
+        return 404, {"message": "No matching user found"}
+    return 200, user.app_role
