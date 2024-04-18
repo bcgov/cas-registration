7918a3b3f3b9ef882751e80a1fefc933bcc18603
SeSo <sobhani.sepehr@gmail.com>
2024-04-18 16:55:40 -0700

    chore: fix wording in an existing email template

--- bc_obps/common/tests/test_common_initial_data.py
+++ bc_obps/common/tests/test_common_initial_data.py
@@ -15,7 +15,7 @@ class TestInitialData(TestCase):
             <p style="text-align: center;">Province of British Columbia</p>
             <p style="text-align: center;">B.C. Industrial Emissions Reporting System (BCIERS)</p><br>
             <p>Dear {{ external_user_full_name }},</p><br>
-            <p>Your administrator access request to {{ operator_legal_name }} in BCIERS has been received by the Climate Action Secretariat.</p>
+            <p>Your administrator access request to {{ operator_legal_name }} in BCIERS has been received by the Climate Action Secretariat (CAS).</p>
             <p>CAS is reviewing your request. Please allow 3-5 business days to process this request. Once a decision is made, you may expect a notification email.</p><br>
             <p>If you have any questions, please contact our support team at <a href="mailto:GHGRegulator@gov.bc.ca">GHGRegulator@gov.bc.ca.</a></p><br>
             <p>Sent to: {{ external_user_email_address }}</p>
