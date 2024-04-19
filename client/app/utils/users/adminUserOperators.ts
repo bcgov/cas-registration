ff70e1668c7a22836aace496dd5222b4ce14cbcd
marcellmueller <marcel@button.is>
2024-04-19 08:19:39 -0700

    chore: remove unnecessary data transformation

--- client/app/utils/users/adminUserOperators.ts
+++ client/app/utils/users/adminUserOperators.ts
@@ -1,6 +1,5 @@
-import { Status, UserOperatorStatus } from "@/app/utils/enums";
+import { Status } from "@/app/utils/enums";
 import { actionHandler, getToken } from "@/app/utils/actions";
-import { OperatorStatus, UserOperatorRoles } from "@/app/utils/enums";
 
 export interface ExternalDashboardUsersTile {
   user: { [key: string]: any };
@@ -31,7 +30,7 @@ export async function getExternalDashboardUsersTileData(): Promise<
     return await actionHandler(
       `registration/user-operator/user-operator-list-from-user`,
       "GET",
-      "/dashboard/users",
+      "/dashboard/users"
     );
   } catch (error) {
     throw error;
@@ -40,49 +39,17 @@ export async function getExternalDashboardUsersTileData(): Promise<
 export async function processExternalDashboardUsersTileData() {
   const tileData = await getExternalDashboardUsersTileData();
 
-  // Ensure tileData is an array before using map
-  const transformedTileData = Array.isArray(tileData)
-    ? tileData.map((userOperator) => {
-        userOperator.status = userOperator.status
-          ? UserOperatorStatus[
-              userOperator.status.toUpperCase() as keyof typeof UserOperatorStatus
-            ]
-          : UserOperatorStatus.PENDING;
-
-        return userOperator;
-      })
-    : [];
-
   // 🤳Identify current admin user in the list
   const token = await getToken();
   const uid = token?.user_guid ?? "";
-  const selfIndex = transformedTileData.findIndex((userOperator) => {
+  const selfIndex = tileData.findIndex((userOperator) => {
     return userOperator.user.user_guid.replace(/-/g, "") === uid;
   });
 
   // Ensure selfIndex is within the valid range before modifying the array
-  if (selfIndex !== -1 && selfIndex < transformedTileData.length) {
-    transformedTileData[selfIndex].status = Status.MYSELF;
+  if (selfIndex !== -1 && selfIndex < tileData.length) {
+    tileData[selfIndex].status = Status.MYSELF;
   }
 
-  const rowData = transformedTileData.map((uOS) => {
-    const { id, role, status, user, operator } = uOS;
-
-    // If the user is pending, we want to default the access type dropdown to Reporter
-    const accessType =
-      role === UserOperatorRoles.PENDING ? UserOperatorRoles.REPORTER : role;
-
-    return {
-      id: id, // This unique ID is needed for DataGrid to work properly
-      name: `${user.first_name} ${user.last_name}`,
-      email: user.email,
-      business: operator.legal_name,
-      accessType: status === OperatorStatus.DECLINED ? "N/A" : accessType,
-      status: status,
-    };
-  });
-
-  return {
-    rows: rowData as UserOperatorDataGridRow[],
-  };
+  return tileData;
 }
