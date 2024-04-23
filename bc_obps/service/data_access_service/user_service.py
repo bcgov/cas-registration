f55ff4921e0124022e06d3a3cff1c7adceb1917c
Brianna Cerkiewicz <brianna@button.is>
2024-04-23 09:10:04 -0700

    chore: refactor user endpoints

--- bc_obps/service/data_access_service/user_service.py
+++ bc_obps/service/data_access_service/user_service.py
@@ -1,22 +1,20 @@
 from uuid import UUID
-from registration.models import UserOperator, User
-from registration.schema.user_operator import PendingUserOperatorOut
-
-NO_USER_OPERATOR_MESSAGE = "User is not associated with any operator"
+from registration.schema.user import UserIn, UserOut, UserUpdateIn
+from registration.models import AppRole, Operator, UserOperator, User
 
 
 class UserDataAccessService:
-    def get_user_by_guid(user_guid: str):
+    def get_by_guid(user_guid: UUID) -> User:
         return User.objects.get(user_guid=user_guid)
 
-    def get_user_business_guid(user_guid: str):
+    def get_user_business_guid(user_guid: UUID) -> UUID:
         return User.objects.get(user_guid=user_guid).business_guid
 
-    def get_operator_by_user(user_guid: str):
+    def get_operator_by_user(user_guid: UUID) -> Operator:
         user_operator = UserDataAccessService.get_user_operator_by_user(user_guid)
         return user_operator.operator
 
-    def get_user_operator_by_user(user_guid: str):
+    def get_user_operator_by_user(user_guid: UUID) -> UserOperator:
         user_operator = (
             UserOperator.objects.only("id", "status", "operator__id", "operator__is_new", "operator__status")
             .exclude(
@@ -33,8 +31,38 @@ class UserDataAccessService:
         ).exists()
         return {"approved": approved_user_operator}
 
-    def is_users_user_operator_declined(user_guid: str, operator_id: UUID):
+    def is_users_user_operator_declined(user_guid: UUID, operator_id: UUID) -> bool:
         is_declined = UserOperator.objects.filter(
             operator_id=operator_id, user_id=user_guid, status=UserOperator.Statuses.DECLINED
         ).exists()
         return is_declined
+
+    def get_app_role(user_guid: UUID) -> AppRole:
+        return User.objects.only('app_role').select_related('app_role').get(user_guid=user_guid)
+
+    def get_user_profile(user_guid: UUID):
+        return (
+            User.objects.only(*UserOut.Config.model_fields, "app_role")
+            .select_related('app_role')
+            .get(user_guid=user_guid)
+        )
+
+    def create_user(user_guid: UUID, role: AppRole, user_data: UserIn) -> User:
+        return User.objects.create(
+            user_guid=user_guid,
+            business_guid=user_data.business_guid,
+            bceid_business_name=user_data.bceid_business_name,
+            app_role=role,
+            first_name=user_data.first_name,
+            last_name=user_data.last_name,
+            email=user_data.email,
+            position_title=user_data.position_title,
+            phone_number=user_data.phone_number,
+        )
+
+    def update_user(user_guid: UUID, updated_data: UserUpdateIn) -> User:
+        user: User = UserDataAccessService.get_by_guid(user_guid)
+        for attr, value in updated_data.dict().items():
+            setattr(user, attr, value)
+        user.save()
+        return user
