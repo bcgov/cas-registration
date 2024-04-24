b8954fe75c7ba6f9e6501779bc67a91a8f44adae
marcellmueller <marcel@button.is>
2024-04-24 12:58:12 -0700

    chore: update and export endpoint allow list

--- client/tests/utils/getUUIDFromEndpoint.test.ts
+++ client/tests/utils/getUUIDFromEndpoint.test.ts
@@ -1,15 +1,18 @@
 import { describe, expect } from "vitest";
 
-import getUUIDFromEndpoint from "@/app/utils/getUUIDFromEndpoint";
+import getUUIDFromEndpoint, {
+  endpointAllowList,
+} from "@/app/utils/getUUIDFromEndpoint";
 
 describe("getUUIDFromEndpoint", () => {
-  it("should return the UUID from the endpoint", () => {
-    const endpoint =
-      "/registration/user/user-app-role/ba2ba62a121842e0942aab9e92ce8822";
-    const result = getUUIDFromEndpoint(endpoint);
-    expect(result).toBe("ba2ba62a121842e0942aab9e92ce8822");
-  });
-
+  it.each(endpointAllowList)(
+    "should return the UUID from the endpoint %s",
+    (endpoint) => {
+      const endpointWithUUID = `${endpoint}/ba2ba62a121842e0942aab9e92ce8822`;
+      const result = getUUIDFromEndpoint(endpointWithUUID);
+      expect(result).toBe("ba2ba62a121842e0942aab9e92ce8822");
+    },
+  );
   it("should return an empty string if the endpoint contains a malformed UUID", () => {
     const endpoint =
       "registration/user/user-app-role/ba2ba62a121842e0942aab9e92ce882";
