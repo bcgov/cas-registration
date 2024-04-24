695269fe379052111047e3d6b75fdbe24e53f9db
marcellmueller <marcel@button.is>
2024-04-24 11:13:40 -0700

    test: update get uuid from endpoint test

--- client/tests/utils/getUUIDFromEndpoint.test.ts
+++ client/tests/utils/getUUIDFromEndpoint.test.ts
@@ -4,20 +4,29 @@ import getUUIDFromEndpoint from "@/app/utils/getUUIDFromEndpoint";
 
 describe("getUUIDFromEndpoint", () => {
   it("should return the UUID from the endpoint", () => {
-    const endpoint = "/registration/endpoint/ba2ba62a121842e0942aab9e92ce8822";
+    const endpoint =
+      "/registration/user/user-app-role/ba2ba62a121842e0942aab9e92ce8822";
     const result = getUUIDFromEndpoint(endpoint);
     expect(result).toBe("ba2ba62a121842e0942aab9e92ce8822");
   });
 
   it("should return an empty string if the endpoint contains a malformed UUID", () => {
-    const endpoint = "/registration/endpoint/ba2ba62a121842e0942aab9e92ce882";
+    const endpoint =
+      "registration/user/user-app-role/ba2ba62a121842e0942aab9e92ce882";
     const result = getUUIDFromEndpoint(endpoint);
     expect(result).toBe(null);
   });
 
   it("should return an empty string if the endpoint does not contain a UUID", () => {
-    const endpoint = "/registration/endpoint";
+    const endpoint = "registration/user/user-app-role";
     const result = getUUIDFromEndpoint(endpoint);
     expect(result).toBe(null);
+  });
+
+  it("should throw an error if the endpoint is not allowed", () => {
+    const endpoint = "registration/user/not-allowed-endpoint";
+    expect(() => getUUIDFromEndpoint(endpoint)).toThrowError(
+      "Endpoint is not allowed",
+    );
   });
 });
