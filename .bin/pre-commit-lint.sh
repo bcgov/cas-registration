cc385dc18f8b2b18d5c30ea2bb137b03d3706072
Josh Gamache <joshua@button.is>
2024-04-24 15:38:43 -0600

    fix: pre-commit not working

--- .bin/pre-commit-lint.sh
+++ .bin/pre-commit-lint.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-pushd client || exit 1
+pushd bciers || exit 1
 files=("$@")
 files=("${files[@]/#/../}") # add ../ to each element
 
-yarn run eslint --no-eslintrc -c ./.eslintrc.js "${files[@]}"
+yarn run eslint --no-eslintrc -c ./.eslintrc.json "${files[@]}"
