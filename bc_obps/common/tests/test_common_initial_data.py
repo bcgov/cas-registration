0fe963717f62fe80235332fff6fc05c3a44e7450
Andrea Williams <andrea.williams@gov.bc.ca>
2024-04-29 11:33:08 -0700

    Chore: updated migration for boro id emails

--- bc_obps/common/tests/test_common_initial_data.py
+++ bc_obps/common/tests/test_common_initial_data.py
@@ -164,3 +164,28 @@ class TestInitialData(TestCase):
             <p>On behalf of the Climate Action Secretariat</p>
         ''',
         )
+
+    def test_boro_id_application_templates(self):
+        template = EmailNotificationTemplate.objects.get(name='BORO ID Application Confirmation')
+        self.assertEqual(
+            template.subject,
+            'BCIERS Receipt Acknowledgement - BORO ID application for {{ operation_name }} of {{ operator_legal_name }}',
+        )
+        self.assertEqual(
+            template.body,
+            '''
+            <p style="text-align: center;">Province of British Columbia</p>
+            <p style="text-align: center;">B.C. Industrial Emissions Reporting System (BCIERS)</p><br>
+            <p>Dear {{ external_user_full_name }},</p><br>
+            <p>Your application to obtain a BC OBPS Regulated Operation ID (BORO ID) for {{ operation_name }} of {{ operator_legal_name }} has been received by the Climate Action Secretariat (CAS) through BCIERS.</p>
+            <p>CAS is reviewing the application. Please allow 3-5 business days to process this request. Clarifications and changes may be requested as part of the reviewing process. Once a decision is made, you may expect a notification email.</p><br>
+            <p>If you have any questions, please contact our support team at <a href="mailto:GHGRegulator@gov.bc.ca">GHGRegulator@gov.bc.ca.</a></p><br>
+            <p>Sent to: {{ external_user_email_address }}</p>
+            <p>On behalf of the Climate Action Secretariat</p>
+        ''',
+        )
+        template = EmailNotificationTemplate.objects.get(name='BORO ID Application Approved')
+
+        template = EmailNotificationTemplate.objects.get(name='BORO ID Application Declined')
+
+        template = EmailNotificationTemplate.objects.get(name='BORO ID Application Changes Requested')
