f2ef6d2816aaeda267d83f83d3a3b9c490e10ea8
shon-button <shon@button.is>
2024-04-29 11:15:01 -0400

    chore: auth header consistency

--- bc_obps/registration/api/user_operator/is_approved_admin_user_operator.py
+++ bc_obps/registration/api/user_operator/is_approved_admin_user_operator.py
@@ -1,3 +1,4 @@
+import json
 from service.data_access_service.user_service import UserDataAccessService
 
 from registration.decorators import authorize, handle_http_errors
@@ -14,11 +15,13 @@ from service.error_service.custom_codes_4xx import custom_codes_4xx
 
 
 @router.get(
-    "/user-operator/is-approved-admin-user-operator/{user_guid}",
+    "/user-operator/is-approved-admin-user-operator",
     response={200: IsApprovedUserOperator, custom_codes_4xx: Message},
     url_name="is_approved_admin_user_operator",
 )
 @authorize(["industry_user"], UserOperator.get_all_industry_user_operator_roles())
 @handle_http_errors()
-def is_approved_admin_user_operator(request, user_guid):
-    return 200, UserDataAccessService.is_user_an_approved_admin_user_operator(user_guid)
+def is_approved_admin_user_operator(request):
+    return 200, UserDataAccessService.is_user_an_approved_admin_user_operator(
+        json.loads(request.headers.get('Authorization')).get('user_guid')
+    )
--- bc_obps/registration/tests/endpoints/user_operator/test_is_approved_admin_user_operator.py
+++ bc_obps/registration/tests/endpoints/user_operator/test_is_approved_admin_user_operator.py
@@ -1,3 +1,4 @@
+import json
 from model_bakery import baker
 from registration.models import (
     User,
@@ -21,13 +22,13 @@ class TestUserOperatorIsApprovedAdminUserOperatorEndpoint(CommonTestSetup):
         mock_user_operator.role = UserOperator.Roles.ADMIN
         mock_user_operator.status = UserOperator.Statuses.APPROVED
         mock_user_operator.save(update_fields=["user_id", "role", "status"])
+        self.user.user_guid = mock_user.user_guid
+        self.user.save()
+        self.auth_header_dumps = json.dumps({'user_guid': str(self.user.user_guid)})
         response = TestUtils.mock_get_with_auth_role(
             self,
             "industry_user",
-            custom_reverse_lazy(
-                "is_approved_admin_user_operator",
-                kwargs={"user_guid": mock_user.user_guid},
-            ),
+            custom_reverse_lazy("is_approved_admin_user_operator"),
         )
         assert response.status_code == 200
         assert response.json() == {"approved": True}
@@ -44,10 +45,7 @@ class TestUserOperatorIsApprovedAdminUserOperatorEndpoint(CommonTestSetup):
         response = TestUtils.mock_get_with_auth_role(
             self,
             "industry_user",
-            custom_reverse_lazy(
-                "is_approved_admin_user_operator",
-                kwargs={"user_guid": mock_user.user_guid},
-            ),
+            custom_reverse_lazy("is_approved_admin_user_operator"),
         )
         assert response.status_code == 200
         assert response.json() == {"approved": False}
