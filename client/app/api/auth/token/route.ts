adaeb6830287ca529e689b8bb8b0f38e0b02838a
shon-button <shon@button.is>
2024-05-01 14:24:33 -0400

    debug: nextauth_secret

--- client/app/api/auth/token/route.ts
+++ client/app/api/auth/token/route.ts
@@ -3,10 +3,10 @@ import { getToken } from "@auth/core/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  // @ts-expect-error
   const token = await getToken({
     req: request,
-    // @ts-expect-error
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: `${process.env.NEXTAUTH_SECRET}`,
     secureCookie: false,
   });
   return NextResponse.json(token, { status: 200 });
