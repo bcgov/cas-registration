c27f8d157d6f844b9befefce43e9fd34d5bd9e58
Dylan Leard <dylan@button.is>
2024-05-02 10:05:58 -0700

    chore: move packages

--- bciers/apps/registration/next.config.js
+++ bciers/apps/registration/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const { withSentryConfig } = require("@sentry/nextjs");
+const { composePlugins, withNx } = require("@nx/next");
+
+const nextConfigBase = require("../../next.config.base");
+
 const nextConfig = {
+  ...nextConfigBase,
   reactStrictMode: true,
   swcMinify: true,
   //use modularizeImports properties to optimize the imports in the application
@@ -17,24 +22,36 @@ const nextConfig = {
       bodySizeLimit: "20mb",
     },
   },
-};
-
-const sentryOptions = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-};
-
-module.exports = withSentryConfig(
-  nextConfig,
-  {
-    org: "government-of-british-columbia",
-    project: "registration-next-js",
-    // Set to false to create a sentry release on build with the sentry CLI
-    // This will upload sourcemaps to sentry.
-    dryRun: true,
-    silent: true, // Suppresses source map uploading logs during build
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
   },
-  sentryOptions,
-);
+};
+
+// const sentryOptions = {
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options
+//   // Hides source maps from generated client bundles
+//   hideSourceMaps: true,
+// };
+
+// module.exports = withSentryConfig(
+//   nextConfig,
+//   {
+//     org: "government-of-british-columbia",
+//     project: "registration-next-js",
+//     // Set to false to create a sentry release on build with the sentry CLI
+//     // This will upload sourcemaps to sentry.
+//     dryRun: true,
+//     silent: true, // Suppresses source map uploading logs during build
+//   },
+//   sentryOptions
+// );
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
