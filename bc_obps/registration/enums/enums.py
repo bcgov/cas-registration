866f78db425159d660a4dadb8da236c297bc209d
SeSo <sobhani.sepehr@gmail.com>
2024-05-03 17:49:51 -0700

    chore: remove non-enum classes and make them inline

--- bc_obps/registration/enums/enums.py
+++ bc_obps/registration/enums/enums.py
@@ -13,12 +13,6 @@ class BoroIdApplicationStates(Enum):
     CHANGES_REQUESTED = "Changes Requested"
 
 
-class BoroIdApplication:
-    @staticmethod
-    def generate_boro_id_application_template_name(application_state):
-        return f"BORO ID Application {application_state}"
-
-
 class AccessRequestStates(Enum):
     CONFIRMATION = "Confirmation"
     APPROVED = "Approved"
@@ -29,9 +23,3 @@ class AccessRequestTypes(Enum):
     ADMIN = "Admin"
     OPERATOR_WITH_ADMIN = "Operator With Admin"
     NEW_OPERATOR_AND_ADMIN = "New Operator And Admin"
-
-
-class OperatorAccessRequest:
-    @staticmethod
-    def generate_access_request_template_name(access_type, access_state):
-        return f"{access_type} Access Request {access_state}"
