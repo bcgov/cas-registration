b5abda2c37de4afff79b87d59b212befb52631e8
Josh Gamache <joshua@button.is>
2024-05-06 09:05:48 -0600

    fix: ignore lint error in rep placeholder page

--- bciers/apps/reporting/src/app/page.tsx
+++ bciers/apps/reporting/src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Button, Grid } from "@mui/material";
 import { signIn } from "next-auth/react";
 import { Main } from "@bciers/components/server";
+// eslint-disable-next-line import/extensions
 import Logo from "@bciers/img/src/lib/BCID_CleanBC_rev_tagline_colour.svg";
 import Image from "next/image";
 
