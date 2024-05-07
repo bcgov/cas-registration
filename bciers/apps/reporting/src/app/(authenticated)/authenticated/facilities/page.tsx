650222fb81d6d2c18b2025e7d751ae46a26f5b81
Ballard <ballard@button.is>
2024-05-07 11:57:32 -0400

    chore: split component onto new page, switch pattern for calling API

--- bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/page.tsx
+++ bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/page.tsx
@@ -1,45 +1,15 @@
-"use client"
+import { Suspense } from "react";
+import Loading from "@/app/components/loading/SkeletonForm";
+import Operators from './Operators';
 
-import React from 'react';
-import { actionHandler } from "@/app/utils/actions";
-
-const Page: React.FC = () => {
-  const fetchOperators = async () => {
-    try {
-      const operatorList = await actionHandler(
-        "registration/operations",
-        "GET",
-      );
-      return operatorList.data;
-    } catch (error) {
-      // Handle the error here or rethrow it to handle it at a higher level
-      throw error;
-    }
-  };
-
+export default async function OperatorsPage() {
   return (
-    <div>
-      <h1>Facilities and operators page</h1>
-      <h2>Operators:</h2>
-      <OperatorsDisplay fetchOperators={fetchOperators} />
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div>
+        <h1>Facilities and operators page</h1>
+        <h2>Operators:</h2>
+        <Operators />
+      </div>
+    </Suspense>
   );
-};
-
-const OperatorsDisplay: React.FC<{ fetchOperators: () => Promise<any> }> = ({ fetchOperators }) => {
-  const [operators, setOperators] = React.useState<any>(null);
-
-  React.useEffect(() => {
-    fetchOperators().then((operatorsData) => {
-      setOperators(operatorsData);
-    });
-  }, [fetchOperators]);
-
-  if (!operators) {
-    return <p>Loading...</p>;
-  }
-
-  return <p>{JSON.stringify(operators, null, 2)}</p>;
-};
-
-export default Page;
+}
