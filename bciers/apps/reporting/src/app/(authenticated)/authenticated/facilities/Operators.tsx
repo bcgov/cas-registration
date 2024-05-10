99d906a1ef76e7a9310f52f0f7ff7380c67d8f9d
SeSo <sobhani.sepehr@gmail.com>
2024-05-10 10:28:42 -0700

    chore: make prettier happy

--- bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/Operators.tsx
+++ bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/Operators.tsx
@@ -1,19 +1,16 @@
 import { actionHandler } from "@/app/utils/actions";
 
 async function getOperators() {
-    try {
-      return await actionHandler(
-        "registration/operations",
-        "GET",
-      );
-    } catch (error) {
-      // Handle the error here or rethrow it to handle it at a higher level
-      throw error;
-    }
+  try {
+    return await actionHandler("registration/operations", "GET");
+  } catch (error) {
+    // Handle the error here or rethrow it to handle it at a higher level
+    throw error;
   }
+}
 
 export default async function Operators() {
-    const operators = await getOperators();
+  const operators = await getOperators();
 
-    return <p>{JSON.stringify(operators.data, null, 2)}</p>;
+  return <p>{JSON.stringify(operators.data, null, 2)}</p>;
 }
