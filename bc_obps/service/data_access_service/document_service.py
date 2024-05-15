ba8148f1a4cd17c2dfaa17e2f161b6f68e813f00
Brianna Cerkiewicz <brianna@button.is>
2024-05-15 14:27:51 -0700

    chore: refactor update service to use create_or_update_statutory_declaration function

--- bc_obps/service/data_access_service/document_service.py
+++ bc_obps/service/data_access_service/document_service.py
@@ -3,11 +3,11 @@ from registration.models import Document, DocumentType
 
 class DocumentDataAccessService:
     @classmethod
-    def create_statutory_declaration(cls, user_guid, statutory_declaration, operation):
+    def create_statutory_declaration(cls, user_guid, statutory_declaration):
         document = Document.objects.create(
             file=statutory_declaration,
             type=DocumentType.objects.get(name="signed_statutory_declaration"),
             created_by_id=user_guid,
         )
-        operation.documents.set([document])
+
         return document
