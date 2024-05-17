686e3fbaf764d379f1e71369bd2df247413bb39a
Ballard <ballard@button.is>
2024-05-17 14:06:38 -0400

    chore: add SourceType model and data

--- bc_obps/reporting/models.py
+++ bc_obps/reporting/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from registration.models import BaseModel
+import uuid
 
 
 class Report(models.Model):
@@ -13,3 +15,13 @@ class Report(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+class SourceType(BaseModel):
+    """Reporting source type model"""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_comment="Unique ID for the source type")
+    name = models.CharField(max_length=1000, db_comment="The name of a source type")
+
+    class Meta:
+        db_table_comment = "Source types"
+        db_table = 'erc"."source_type'
