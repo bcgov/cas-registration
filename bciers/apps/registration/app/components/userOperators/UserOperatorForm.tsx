6e858e0cddcf75112a97abb89d00ff96b61c08e1
Brianna Cerkiewicz <brianna@button.is>
2024-05-27 13:55:50 -0700

    chore: apply suggestions from code review

--- bciers/apps/registration/app/components/userOperators/UserOperatorForm.tsx
+++ bciers/apps/registration/app/components/userOperators/UserOperatorForm.tsx
@@ -42,7 +42,6 @@ export default function UserOperatorForm({
       ...data.formData,
     } as UserOperatorFormData;
 
-    // brianna
     const endpoint = `registration/user-operators${
       !isCreate ? `/${userOperatorId}` : ""
     }`;
