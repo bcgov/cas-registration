338de2ff717db35df18e16394b50fd09ce630a1e
SeSo <sobhani.sepehr@gmail.com>
2024-05-28 16:07:36 -0700

    chore: add swagger configs

--- bc_obps/bc_obps/api.py
+++ bc_obps/bc_obps/api.py
@@ -1,8 +1,12 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Swagger
 from common.api import router as common_router
 from registration.api import router as registration_router
 
-api = NinjaAPI(title="BC OBPS API")
+# Docs: https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
+# Filtering is case sensitive matching the filter expression anywhere inside the tag.
+api = NinjaAPI(
+    title="BCIERS API", docs=Swagger(settings={"filter": True, "operationsSorter": "method", "tagsSorter": "alpha"})
+)
 
 api.add_router("/common/", common_router, tags=["V1"])
 api.add_router("/registration/", registration_router, tags=["V1"])
