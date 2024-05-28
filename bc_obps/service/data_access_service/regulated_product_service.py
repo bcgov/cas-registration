46ab7211940c7af92fbb8bbe185a7149f0537ae7
SeSo <sobhani.sepehr@gmail.com>
2024-05-28 16:01:52 -0700

    chore: remove redundant texts

--- bc_obps/service/data_access_service/regulated_product_service.py
+++ bc_obps/service/data_access_service/regulated_product_service.py
@@ -6,8 +6,6 @@ from registration.schema.v1 import (
 from django.core.cache import cache
 from django.db.models import QuerySet
 
-##### GET #####
-
 
 class RegulatedProductDataAccessService:
     @classmethod
@@ -19,12 +17,3 @@ class RegulatedProductDataAccessService:
             regulated_products = RegulatedProduct.objects.only(*RegulatedProductSchema.Config.model_fields)
             cache.set("regulated_products", regulated_products, 60 * 60 * 24 * 1)  # 1 day
             return regulated_products
-
-
-##### POST #####
-
-
-##### PUT #####
-
-
-##### DELETE #####
