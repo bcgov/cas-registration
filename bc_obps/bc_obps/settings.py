0c071c320b14d1875c0b49082f0d6d48e31b5c30
SeSo <sobhani.sepehr@gmail.com>
2024-05-29 15:20:16 -0700

    947 bump django and python (#1723)
    * chore: make erd diagram generator happy
    
    * chore: bump python and django
    
    * chore: remove silk profiler causing conflicts

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -101,7 +101,6 @@ if DEBUG:  # DEV only apps
     # SILK
     INSTALLED_APPS.append("silk")
     MIDDLEWARE.insert(0, "silk.middleware.SilkyMiddleware")
-    SILKY_PYTHON_PROFILER = True
 
 ROOT_URLCONF = "bc_obps.urls"
 
