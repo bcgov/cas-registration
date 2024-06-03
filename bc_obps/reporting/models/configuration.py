df52a66ccf2ae50bb49c600ee8c18556c23aae8d
Dylan Leard <dylan@button.is>
2024-06-03 13:59:50 -0700

    chore: add configuration model

---
+++ bc_obps/reporting/models/configuration.py
@@ -0,0 +1,14 @@
+from common.models import BaseModel
+from django.db import models
+
+
+class Configuration(BaseModel):
+    """Reporting configuration for a year/program state"""
+
+    slug = models.CharField(max_length=1000, db_comment="Unique identifier for a configuration", unique=True)
+    valid_from = models.DateField(blank=True, null=True, db_comment="Date from which the configuration is applicable")
+    valid_to = models.DateField(blank=True, null=True, db_comment="Date until which the configuration is applicable")
+
+    class Meta:
+        db_table_comment = "Table containing program configurations for a date range. Each record will define a time period for when configuration elements are valid. When a change to the configuration is made a new configuration record will be created. This enables historical accuracy when applying configurations from previous years."
+        db_table = 'erc"."configuration'
