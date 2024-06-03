ce3410759cb1f858b8aa5050efc7966b345c3ba5
Dylan Leard <dylan@button.is>
2024-06-03 15:53:06 -0700

    chore: add configuration_element model

--- bc_obps/reporting/models/gas_type.py
+++ bc_obps/reporting/models/gas_type.py
@@ -3,7 +3,7 @@ from django.db import models
 from simple_history.models import HistoricalRecords
 
 
-class ReportingGasType(BaseModel):
+class GasType(BaseModel):
     """Reporting gas type"""
 
     name = models.CharField(max_length=1000, db_comment="The name of a greenhouse gas type")
@@ -12,4 +12,4 @@ class ReportingGasType(BaseModel):
 
     class Meta:
         db_table_comment = "This table contains the list of gas types that can be reported as defined in GGERR (Greenhous Gas Emission Reporting Regulation)"
-        db_table = 'erc"."reporting_gas_type'
+        db_table = 'erc"."gas_type'
