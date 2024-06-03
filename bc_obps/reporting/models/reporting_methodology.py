4701dff3780c3b9e7a03b65cea8a263c19aec316
Dylan Leard <dylan@button.is>
2024-06-03 14:12:01 -0700

    chore: add reporting_methodology model

---
+++ bc_obps/reporting/models/reporting_methodology.py
@@ -0,0 +1,13 @@
+from common.models import BaseModel
+from django.db import models
+from simple_history.models import HistoricalRecords
+
+
+class ReportingMethodology(BaseModel):
+    """Reporting methodology"""
+
+    name = models.CharField(max_length=1000, db_comment="The name of a reporting methodology")
+
+    class Meta:
+        db_table_comment = "Table contains the set of reporting methodologies that can be applied to an emission as outlined in GGERR (Greenhous Gas Emission Reporting Regulation)"
+        db_table = 'erc"."reporting_methodology'
