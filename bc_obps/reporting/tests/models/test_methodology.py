c0c640e9733188e69215620c8318a9d3f68f7342
Dylan Leard <dylan@button.is>
2024-06-04 16:37:10 -0700

    test: add methodology model test

---
+++ bc_obps/reporting/tests/models/test_methodology.py
@@ -0,0 +1,43 @@
+from common.tests.utils.helpers import BaseTestCase
+from reporting.models import Methodology
+from django.test import TestCase
+from reporting.tests.utils.bakers import configuration_element_baker
+
+
+class TestInitialData(TestCase):
+  def test_methodology_initial_data(self):
+        expected_methodologies = sorted(
+            [
+            'Default HHV/Default EF',
+            'Default EF',
+            'Measured HHV/Default EF',
+            'Measured Steam/Default EF',
+            'Measured CC',
+            'Measured Steam/Measured EF',
+            'Alternative Parameter Measurement',
+            'Replacement Methodology',
+            'Anode Consumption',
+            'Slope method',
+            'Overvoltage method',
+            'C2F6 anode effects',
+            'Inventory',
+            'Input/output'
+            ]
+        )
+        existing_methodologies = sorted(list(Methodology.objects.values_list('name', flat=True)))
+
+        self.assertEqual(len(existing_methodologies), len(expected_methodologies))
+        self.assertEqual(existing_methodologies, expected_methodologies)
+
+
+class MethodologyModelTest(BaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_object = Methodology.objects.create(
+            name="testMethodology"
+        )
+        cls.field_data = [
+            ("id", "ID", None, None),
+            ("name", "name", 1000, None),
+            ("configuration_elements", "configuration element", None, None)
+        ]
--- bc_obps/reporting/tests/utils/bakers.py
+++ bc_obps/reporting/tests/utils/bakers.py
@@ -1,6 +1,22 @@
+from reporting.models.gas_type import GasType
+from registration.models import ReportingActivity
+from reporting.models.source_type import SourceType
 from model_bakery import baker
 from reporting.models.report import Report
+from reporting.models.configuration_element import ConfigurationElement
 
 
 def report_baker() -> Report:
     return baker.make(Report)
+
+def reporting_activity_baker() -> ReportingActivity:
+    return baker.make(ReportingActivity)
+
+def source_type_baker() -> SourceType:
+    return baker.make(SourceType)
+
+def gas_type_baker() -> GasType:
+    return baker.make(GasType)
+
+def configuration_element_baker() -> ConfigurationElement:
+    return baker.make(ConfigurationElement)
