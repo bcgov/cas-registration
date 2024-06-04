a30aae7433e56f110805f60506572db92047f07c
Dylan Leard <dylan@button.is>
2024-06-04 11:37:04 -0700

    chore: source_type model does not need UUID pk

--- bc_obps/reporting/models/source_type.py
+++ bc_obps/reporting/models/source_type.py
@@ -6,9 +6,6 @@ from django.db import models
 class SourceType(BaseModel):
     """Reporting source type model"""
 
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False, db_comment="Unique ID for the source type"
-    )
     name = models.CharField(max_length=1000, db_comment="The name of a source type")
 
     class Meta:
