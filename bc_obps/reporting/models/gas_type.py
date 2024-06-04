dc197255965d5dca510b72112b4de34ad8f55602
Dylan Leard <dylan@button.is>
2024-06-04 11:36:08 -0700

    chore: add chemical_formula column to gas type table & seed data

--- bc_obps/reporting/models/gas_type.py
+++ bc_obps/reporting/models/gas_type.py
@@ -5,7 +5,8 @@ from django.db import models
 class GasType(BaseModel):
     """Reporting gas type"""
 
-    name = models.CharField(max_length=1000, db_comment="The name of a greenhouse gas type")
+    name = models.CharField(max_length=1000, db_comment="The name of a greenhouse gas type (example: Carbon Dioxide)")
+    chemical_formula = models.CharField(max_length=100, db_comment ="The chemical formula representation of a greenhouse gast type (example: CO2)")
     def __str__(self) -> str:
         return self.name
 
