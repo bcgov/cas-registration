504f96bd352562e0071f200f05c181beb14933a8
marcellmueller <marcel@button.is>
2024-06-06 14:22:54 -0700

    refactor: remove dynamic javascript from notes component

--- bciers/libs/shared/components/src/lib/layout/Note.tsx
+++ bciers/libs/shared/components/src/lib/layout/Note.tsx
@@ -8,43 +8,17 @@ interface NoteProps {
 }
 
 const Note = ({ children, variant = "info" }: NoteProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState(0);
-
   const bgColour = variant === "info" ? "bc-bg-grey" : "bc-yellow";
 
-  useEffect(() => {
-    // When the component gets mounted, get the height of the children div
-    // This is so we can add padding to the bottom of the parent div to prevent content from being hidden
-    // due to the absolute positioning of the children which is necessary for the extended background colour to work
-
-    if (ref?.current) setHeight(ref.current.offsetHeight);
-
-    // to handle page resize
-    const getHeight = () => {
-      if (ref?.current) setHeight(ref.current.offsetHeight);
-    };
-    window.addEventListener("resize", getHeight);
-
-    // remove the event listener before the component gets unmounted
-    return () => window.removeEventListener("resize", getHeight);
-  }, []);
-
   return (
-    <div
-      className="relative w-full"
-      style={{
-        height: `${height}px`,
-      }}
-    >
+    <div className="relative w-full">
       <div
-        // absolute positioning combined with left: 50% and transform: translateX(-50%)
+        // relative positioning combined with left: 50% and transform: translateX(-50%)
         // to extend background colour to full width of the screen
-        className={`bg-${bgColour} absolute left-1/2 transform -translate-x-1/2 w-screen max-w-none`}
+        className={`bg-${bgColour} relative left-1/2 transform -translate-x-1/2 w-screen max-w-none`}
       >
         <div
-          className={`max-w-[1536px] mx-auto px-7 bg-${bgColour} py-5 h-[${height}px] text-lg`}
-          ref={ref}
+          className={`max-w-[1536px] mx-auto px-7 bg-${bgColour} py-5 h-fit text-lg`}
         >
           {children}
         </div>
