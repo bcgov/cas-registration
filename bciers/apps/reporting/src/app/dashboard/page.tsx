8ef9f75350262d22aecd77f734a92524ff87f46e
shon-button <shon@button.is>
2024-06-06 17:18:00 -0400

    fix: builds

--- bciers/apps/reporting/src/app/dashboard/page.tsx
+++ bciers/apps/reporting/src/app/dashboard/page.tsx
@@ -1,19 +1,4 @@
-import { Tiles } from "@bciers/components/server";
-import { buildTileContent, getJSONFiles } from "@bciers/actions/server";
-import { auth } from "@/dashboard/auth";
-
 export default async function Page() {
-  // Get the user's identity provider
-  const session = await auth();
-  const folderName = session?.identity_provider as string;
-  // Get the JSON files for this user type
-  const jsonFiles = await getJSONFiles(`apps/reporting/src/data/${folderName}`);
-  // Get the JSON objects from the files
-  const contents = await buildTileContent(jsonFiles);
   // Build the navigation tiles
-  return (
-    <>
-      <Tiles tiles={contents} />
-    </>
-  );
+  return <>Test</>;
 }
