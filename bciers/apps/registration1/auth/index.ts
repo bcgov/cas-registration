3b3c658e2ab8fbaefb7387511ded9f659df1ed77
shon-button <shon@button.is>
2024-06-07 10:30:17 -0400

    debug: import next-auth/jwt and debug flag

--- bciers/apps/registration1/auth/index.ts
+++ bciers/apps/registration1/auth/index.ts
@@ -1,10 +1,12 @@
 import NextAuth from "next-auth";
+import "next-auth/jwt";
 import authConfig from "./auth.config";
 
 /*
 📌 Make one call to NextAuth and export all the returned objects
 */
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true,
   session: {
     strategy: "jwt",
     maxAge: 30 * 60, // 30 minutes matching Keycloak token expiration time
