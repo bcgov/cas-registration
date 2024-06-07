518fdf90c31d8edef03ae084401dc91fe30a20a1
shon-button <shon@button.is>
2024-06-07 06:19:57 -0400

    fix: e2e ARIA tests

--- bciers/libs/shared/components/src/lib/navigation/Tiles.tsx
+++ bciers/libs/shared/components/src/lib/navigation/Tiles.tsx
@@ -14,30 +14,46 @@ export default async function Tiles({ tiles }: { tiles: ContentItem[] }) {
           };
 
           return (
-            <div
-              key={title}
-              className="dashboard-tile-container"
-              onClick={handleClick}
-            >
-              <h2 className="flex items-center m-0">
-                {icon}
-                <div className="ml-2">{tile.title}</div>
-              </h2>
-              <p className="mt-6 mb-0">{tile.content}</p>
-              {typeof links === "object" &&
-                links.map((link, i) => {
+            <section className="flex flex-wrap gap-x-16 lg:gap-x-24 gap-y-16 mt-4">
+              {tiles &&
+                tiles.map((tile) => {
+                  const { icon, links, title } = tile;
+                  const handleClick = () => {
+                    if (tile.href) {
+                      window.location.href = tile.href; // Navigate to href if exists
+                    }
+                  };
+
                   return (
-                    <a
-                      key={i}
-                      href={link.href}
-                      id={`${title.split(" ").join("-")}-link`}
-                      className={`flex items-center mt-6 no-underline font-bold`}
+                    <div
+                      key={title}
+                      className="dashboard-tile-container"
+                      onClick={handleClick}
+                      role="button" // Added ARIA role for button functionality
+                      aria-label={title} // Added ARIA label for screen reader accessibility
                     >
-                      {link.title}
-                    </a>
+                      <h2 className="flex items-center m-0">
+                        {icon}
+                        <div className="ml-2">{title}</div>
+                      </h2>
+                      <p className="mt-6 mb-0">{tile.content}</p>
+                      {typeof links === "object" &&
+                        links.map((link, i) => {
+                          return (
+                            <a
+                              key={i}
+                              href={link.href}
+                              id={`${title.split(" ").join("-")}-link`}
+                              className={`flex items-center mt-6 no-underline font-bold`}
+                            >
+                              {link.title}
+                            </a>
+                          );
+                        })}
+                    </div>
                   );
                 })}
-            </div>
+            </section>
           );
         })}
     </section>
