1b12016c1b52aea9d9f316cde1dc5cdfbd3c1b47
Dylan Leard <dylan@button.is>
2024-06-11 13:27:50 -0700

    chore: add exceptions / error handling to form builder service

--- bc_obps/service/form_builder_service.py
+++ bc_obps/service/form_builder_service.py
@@ -8,7 +8,9 @@ def camelCase(st):
 
 def build_schema(activity: int, source_type: int, gas_type: int, methodology: int, report_date: str):
     source_type_schema = ActivitySourceTypeBaseSchema.objects.select_related('base_schema').filter(reporting_activity_id=activity, source_type_id=source_type, valid_from__valid_from__lte=report_date, valid_to__valid_to__gte=report_date).first()
-    ## Fetch valid gas_type values for activity-sourceType pair
+    if source_type_schema is None:
+        raise Exception(f'No base schema found for activity_id {activity} & source_type_id {source_type}')
+    # Fetch valid gas_type values for activity-sourceType pair
     gas_types=ConfigurationElement.objects.select_related('gas_type').filter(reporting_activity_id=activity, source_type_id=source_type, valid_from__valid_from__lte=report_date,).distinct('gas_type__name')
     gas_type_enum = []
     gas_type_map = {}
@@ -19,19 +21,29 @@ def build_schema(activity: int, source_type: int, gas_type: int, methodology: in
     rjsf_schema = source_type_schema.base_schema.schema
     ## Append valid gas types to schema as an enum on the gasType property
     rjsf_schema['properties']['gasType']['enum'] = gas_type_enum
+
+
+
     if gas_type:
         rjsf_schema['properties']['emissions'] = {"type": "number", "title": "Emissions"}
-        rjsf_schema['properties']['equivalentEmisisons'] = {"type": "number", "title": "Equivalent Emissions"}
+        rjsf_schema['properties']['equivalentEmissions'] = {"type": "number", "title": "Equivalent Emissions"}
         ## Fetch valid methodology values for activity-sourceType pair + gasType selection
         methodologies=ConfigurationElement.objects.select_related('methodology').filter(reporting_activity_id=activity, source_type_id=source_type, gas_type_id=gas_type, valid_from__valid_from__lte=report_date, valid_to__valid_to__gte=report_date,)
-        methdology_enum = []
+        if not methodologies.exists():
+            raise Exception(f'No configuration found for activity_id {activity} & source_type_id {source_type} & gas_type_id {gas_type}')
+        methodology_enum = []
         for t in methodologies:
-            methdology_enum.append(t.methodology.name)
+            methodology_enum.append(t.methodology.name)
             methodology_map[t.methodology.id] = t.methodology.name
         ## Append methodology field to schema with valid methodologies as an enum
-        rjsf_schema['properties']['methodology'] = {"type": "string", "title": "Methodology", "enum": methdology_enum}
+        rjsf_schema['properties']['methodology'] = {"type": "string", "title": "Methodology", "enum": methodology_enum}
+
+
     if methodology:
-        methodology_fields=ConfigurationElement.objects.prefetch_related('reporting_fields').get(reporting_activity_id=activity, source_type_id=source_type, gas_type_id=gas_type, methodology_id=methodology, valid_from__valid_from__lte=report_date, valid_to__valid_to__gte=report_date).reporting_fields.all()
+        try:
+            methodology_fields=ConfigurationElement.objects.prefetch_related('reporting_fields').get(reporting_activity_id=activity, source_type_id=source_type, gas_type_id=gas_type, methodology_id=methodology, valid_from__valid_from__lte=report_date, valid_to__valid_to__gte=report_date).reporting_fields.all()
+        except:
+          raise Exception(f'No configuration found for activity_id {activity} & source_type_id {source_type} & gas_type_id {gas_type} & methodology_id {methodology}')
         for f in methodology_fields:
             property_field = camelCase(f.field_name)
             rjsf_schema['properties'][property_field] = {"type": f.field_type, "title": f.field_name}
@@ -49,8 +61,8 @@ class FormBuilderService:
     @classmethod
     def build_form_schema(request, activity=None, source_type=None, gas_type=None, methodology=None, report_date='2024-04-01'):
       if report_date is None:
-          return 'ERROR: Cannot build a schema without a valid report date'
+          raise Exception('Cannot build a schema without a valid report date')
       if activity is None or source_type is None:
-          return 'ERROR: Cannot build a schema without both Activity & Source Type data'
+          raise Exception('ERROR: Cannot build a schema without both Activity & Source Type data')
       schema = build_schema(activity, source_type, gas_type, methodology, report_date)
       return schema
