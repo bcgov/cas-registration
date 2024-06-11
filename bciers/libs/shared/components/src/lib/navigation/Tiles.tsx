508fb3cf16eeb52b49a1bc70093d21a9d2a5c54b
shon-button <shon@button.is>
2024-06-11 09:39:17 -0400

    fix: tiles

--- bciers/libs/shared/components/src/lib/navigation/Tiles.tsx
+++ bciers/libs/shared/components/src/lib/navigation/Tiles.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { ContentItem } from "@bciers/types";
 
-export default async function Tiles({ tiles }: { tiles: ContentItem[] }) {
+export default function Tiles({ tiles }: { tiles: ContentItem[] }) {
   return (
     <section className="flex flex-wrap gap-x-16 lg:gap-x-24 gap-y-16 mt-4">
-      {tiles &&
+      {tiles.length > 0 &&
         tiles.map((tile) => {
           const { icon, links, title } = tile;
           const handleClick = () => {
@@ -14,46 +14,32 @@ export default async function Tiles({ tiles }: { tiles: ContentItem[] }) {
           };
 
           return (
-            <section className="flex flex-wrap gap-x-16 lg:gap-x-24 gap-y-16 mt-4">
-              {tiles &&
-                tiles.map((tile) => {
-                  const { icon, links, title } = tile;
-                  const handleClick = () => {
-                    if (tile.href) {
-                      window.location.href = tile.href; // Navigate to href if exists
-                    }
-                  };
-
+            <div
+              key={title}
+              className="dashboard-tile-container"
+              onClick={handleClick}
+              role="button" // Added ARIA role for button functionality
+              aria-label={title} // Added ARIA label for screen reader accessibility
+            >
+              <h2 className="flex items-center m-0">
+                {icon}
+                <div className="ml-2">{title}</div>
+              </h2>
+              <p className="mt-6 mb-0">{tile.content}</p>
+              {typeof links === "object" &&
+                links.map((link, i) => {
                   return (
-                    <div
-                      key={title}
-                      className="dashboard-tile-container"
-                      onClick={handleClick}
-                      role="button" // Added ARIA role for button functionality
-                      aria-label={title} // Added ARIA label for screen reader accessibility
+                    <a
+                      key={i}
+                      href={link.href}
+                      id={`${title.split(" ").join("-")}-link`}
+                      className={`flex items-center mt-6 no-underline font-bold`}
                     >
-                      <h2 className="flex items-center m-0">
-                        {icon}
-                        <div className="ml-2">{title}</div>
-                      </h2>
-                      <p className="mt-6 mb-0">{tile.content}</p>
-                      {typeof links === "object" &&
-                        links.map((link, i) => {
-                          return (
-                            <a
-                              key={i}
-                              href={link.href}
-                              id={`${title.split(" ").join("-")}-link`}
-                              className={`flex items-center mt-6 no-underline font-bold`}
-                            >
-                              {link.title}
-                            </a>
-                          );
-                        })}
-                    </div>
+                      {link.title}
+                    </a>
                   );
                 })}
-            </section>
+            </div>
           );
         })}
     </section>
