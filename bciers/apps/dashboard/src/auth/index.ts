fbaa8b32a0138e9760645840d891cb9d9291ee9e
shon-button <shon@button.is>
2024-06-11 05:34:57 -0400

    debug: login tech debt

--- bciers/apps/dashboard/src/auth/index.ts
+++ bciers/apps/dashboard/src/auth/index.ts
@@ -5,7 +5,6 @@ import authConfig from "./auth.config";
 📌 Make one call to NextAuth and export all the returned objects
 */
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  debug: true,
   session: {
     strategy: "jwt",
     maxAge: 30 * 60, // 30 minutes matching Keycloak token expiration time
