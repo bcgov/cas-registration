99f626f9a4efcf40e71130f3d646a4ea5d02a6cc
Ballard <ballard@button.is>
2024-06-11 13:20:10 -0400

    chore: removed change to reporting to trigger CI changes

--- bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/page.tsx
+++ bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/page.tsx
@@ -6,7 +6,7 @@ export default async function OperatorsPage() {
   return (
     <Suspense fallback={<Loading />}>
       <div>
-        <h1>Facilities and operators page. Adding text to trigger CI.</h1>
+        <h1>Facilities and operators page</h1>
         <h2>Operators:</h2>
         <Operators />
       </div>
