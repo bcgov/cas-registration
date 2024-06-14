2dc64f66b795390beafc68784d06338924c860c6
Dylan Leard <dylan@button.is>
2024-06-14 15:29:43 -0700

    chore: add range constraints & test

--- bc_obps/reporting/models/configuration.py
+++ bc_obps/reporting/models/configuration.py
@@ -1,6 +1,15 @@
-from registration.models import BaseModel
+from common.models import BaseModel
 from django.db import models
+from django.contrib.postgres.constraints import ExclusionConstraint
+from django.contrib.postgres.fields import (
+    DateTimeRangeField,
+    RangeBoundary,
+    RangeOperators,
+)
 
+class TsTzRange(models.Func):
+    function = "TSTZRANGE"
+    output_field = DateTimeRangeField()
 
 class Configuration(BaseModel):
     """Reporting configuration for a year/program state"""
@@ -12,3 +21,14 @@ class Configuration(BaseModel):
     class Meta:
         db_table_comment = "Table containing program configurations for a date range. Each record will define a time period for when configuration elements are valid. When a change to the configuration is made a new configuration record will be created. This enables historical accuracy when applying configurations from previous years."
         db_table = 'erc"."configuration'
+        constraints = [
+            ExclusionConstraint(
+                name="exclude_overlapping_asb_records_by_date_range",
+                expressions=[
+                    (
+                        TsTzRange("valid_from", "valid_to", RangeBoundary()),
+                        RangeOperators.OVERLAPS
+                    )
+                ]
+            ),
+        ]
--- bc_obps/reporting/tests/models/test_activity_source_type_base_schema.py
+++ bc_obps/reporting/tests/models/test_activity_source_type_base_schema.py
@@ -1,23 +1,28 @@
 from common.tests.utils.helpers import BaseTestCase
-from reporting.models import ActivitySourceTypeBaseSchema
+from registration.models import ReportingActivity
+from reporting.models import ActivitySourceTypeBaseSchema, SourceType, Configuration
 from reporting.tests.utils.bakers import (
     configuration_baker,
     reporting_activity_baker,
     source_type_baker,
     base_schema_baker,
 )
+from django.core.exceptions import ValidationError
+from django.db import models
+import pytest
 
+from django.db.models import Q
 
 class ActivitySourceTypeBaseSchemaTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-
+        config = configuration_baker({'slug':'2024', 'valid_from': '2024-01-01', 'valid_to': '2024-12-31'})
         cls.test_object = ActivitySourceTypeBaseSchema.objects.create(
-            reporting_activity=reporting_activity_baker(),
-            source_type=source_type_baker(),
-            base_schema=base_schema_baker(),
-            valid_from=configuration_baker(),
-            valid_to=configuration_baker(),
+          reporting_activity=ReportingActivity.objects.get(pk=1),
+          source_type=SourceType.objects.get(pk=1),
+          base_schema=base_schema_baker(),
+          valid_from=config,
+          valid_to=config
         )
         cls.field_data = [
             ("id", "ID", None, None),
@@ -27,3 +32,26 @@ class ActivitySourceTypeBaseSchemaTest(BaseTestCase):
             ("valid_from", "valid from", None, None),
             ("valid_to", "valid to", None, None),
         ]
+    # Throws when a matching activity, source_type, base_schema has an overlapping date range
+    def testDuplicateBaseSchemaForDateRange(self):
+        invalid_record = ActivitySourceTypeBaseSchema(
+            reporting_activity=self.test_object.reporting_activity,
+            source_type=self.test_object.source_type,
+            base_schema=base_schema_baker(),
+            valid_from=self.test_object.valid_from,
+            valid_to=self.test_object.valid_from,
+        )
+
+        with pytest.raises(Exception) as exc:
+            invalid_record.save()
+            assert exc.match(r"^This record will result in duplicate base schemas")
+    def testValidInsert(self):
+        config = configuration_baker({'slug':'2026', 'valid_from': '2026-01-01', 'valid_to': '2026-12-31'})
+        invalid_record = ActivitySourceTypeBaseSchema(
+            reporting_activity=self.test_object.reporting_activity,
+            source_type=self.test_object.source_type,
+            base_schema=base_schema_baker(),
+            valid_from=config,
+            valid_to=config,
+        )
+        invalid_record.save()
