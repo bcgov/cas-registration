5656838f347eed610533b86623faf362a8035b62
Ballard <ballard@button.is>
2024-06-14 12:15:51 -0400

    chore: fix messed up file

--- bciers/apps/registration1/app/components/operations/OperationDataGrid.tsx
+++ bciers/apps/registration1/app/components/operations/OperationDataGrid.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useMemo, useState } from "react";
-<<<<<<<< HEAD:bciers/apps/registration1/app/components/operations/OperationDataGrid.tsx
 import DataGrid from "@bciers/components/datagrid/DataGrid";
 import { GridColumnGroupHeaderParams } from "@mui/x-data-grid";
 import { useSession } from "next-auth/react";
@@ -10,16 +9,6 @@ import HeaderSearchCell from "@bciers/components/datagrid/cells/HeaderSearchCell
 import operationColumns from "@/app/components/datagrid/models/operationColumns";
 import operationGroupColumns from "@/app/components/datagrid/models/operationGroupColumns";
 import { OperationRow } from "./types";
-========
-import DataGrid from "@shared/components/src/lib/layout/DataGrid";
-import { GridColumnGroupHeaderParams } from "@mui/x-data-grid";
-import { useSession } from "next-auth/react";
-import OperationsActionCell from "@shared/components/src/lib/datagrid/cells/OperationActionCell";
-import HeaderSearchCell from "@shared/components/src/lib/datagrid/cells/HeaderSearchCell";
-import operationColumns from "@reporting/src/app/components/datagrid/models/operationColumns";
-import operationGroupColumns from "@shared/components/src/lib/datagrid/models/operationGroupColumns";
-import { OperationRow } from "@shared/components/src/operations/type";
->>>>>>>> 25097e07 (chore: move components to shared library and fix references, rename OperationDataGrid -> ReportingOperationDataGrid for clarity):bciers/apps/reporting/src/app/components/operations/ReportingOperationDataGrid.tsx
 import { fetchOperationsPageData } from "./Operations";
 
 const OperationSearchCell = ({
