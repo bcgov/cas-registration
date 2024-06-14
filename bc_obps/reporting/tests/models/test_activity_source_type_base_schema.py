d156c4abe353c7bba323b45f60ddaee00b182841
Dylan Leard <dylan@button.is>
2024-06-14 15:50:43 -0700

    test: fix sonarcloud reliability rating issue

--- bc_obps/reporting/tests/models/test_activity_source_type_base_schema.py
+++ bc_obps/reporting/tests/models/test_activity_source_type_base_schema.py
@@ -44,7 +44,7 @@ class ActivitySourceTypeBaseSchemaTest(BaseTestCase):
 
         with pytest.raises(Exception) as exc:
             invalid_record.save()
-            assert exc.match(r"^This record will result in duplicate base schemas")
+        assert exc.match(r"^This record will result in duplicate base schemas")
     def testValidInsert(self):
         config = configuration_baker({'slug':'2026', 'valid_from': '2026-01-01', 'valid_to': '2026-12-31'})
         valid_record = ActivitySourceTypeBaseSchema(
