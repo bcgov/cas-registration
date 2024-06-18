3a83eac357bab95381563a5f4bc1d81225e4626b
Dylan Leard <dylan@button.is>
2024-06-18 15:36:20 -0700

    test: fix reporting_activity test to include related fields

--- bc_obps/registration/tests/models/test_reporting_activity.py
+++ bc_obps/registration/tests/models/test_reporting_activity.py
@@ -10,6 +10,8 @@ class ReportingActivityModelTest(BaseTestCase):
             ("name", "name", 1000, None),
             ("applicable_to", "applicable to", None, None),
             ("operations", "operation", None, None),
+            ("configuration_elements", "configuration element", None, None),
+            ("activity_source_type_base_schemas", "activity source type base schema", None, None),
         ]
         cls.test_object = ReportingActivity.objects.create(
             name="test activity",
