b5b227d44af01ea659956effd983ec123e03fef9
Ballard <ballard@button.is>
2024-06-18 12:19:28 -0400

    chore: add missing operators file

---
+++ bciers/apps/reporting/src/app/(authenticated)/authenticated/facilities/Operators.tsx
@@ -0,0 +1,16 @@
+import { actionHandler } from "@/app/utils/actions";
+
+async function getOperators() {
+  try {
+    return await actionHandler("registration/operations", "GET");
+  } catch (error) {
+    // Handle the error here or rethrow it to handle it at a higher level
+    throw error;
+  }
+}
+
+export default async function Operators() {
+  const operators = await getOperators();
+
+  return <p>{JSON.stringify(operators.data, null, 2)}</p>;
+}
