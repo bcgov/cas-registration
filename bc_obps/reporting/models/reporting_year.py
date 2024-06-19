43609c6c00c69f7e2ac57c9b633b35162a913640
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-06-19 08:57:10 -0700

    chore: reporting year model

---
+++ bc_obps/reporting/models/reporting_year.py
@@ -0,0 +1,22 @@
+from django.db import models
+from common.models.base_model import BaseModel
+
+
+class ReportingYear(BaseModel):
+    """Base Model for the reporting year"""
+
+    reporting_year = models.IntegerField(
+        primary_key=True, editable=False, db_comment="Year for the reporting year, unique and serves as primary key"
+    )
+    reporting_period_start = models.DateTimeField(
+        blank=False, null=False, db_comment="Start of the reporting period for that reporting year, UTC-based"
+    )
+    reporting_period_end = models.DateTimeField(
+        blank=False, null=False, db_comment="End of the reporting period for that reporting year, UTC-based"
+    )
+
+    description = models.CharField()
+
+    class Meta:
+        db_table_comment = "Reporting year"
+        db_table = 'erc"."reporting_year'
