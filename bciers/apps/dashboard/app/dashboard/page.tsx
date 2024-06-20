1df949d33c08d8c7598f6f591019204245b25c3a
marcellmueller <marcel@button.is>
2024-06-20 14:24:37 -0700

    chore: conditionally show bciers dashboard note

--- bciers/apps/dashboard/app/dashboard/page.tsx
+++ bciers/apps/dashboard/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
+"use server";
+
 import Tiles from "@bciers/components/navigation/Tiles";
 import Note from "@bciers/components/layout/Note";
 import { fetchDashboardData } from "@bciers/actions/server";
 import { ContentItem } from "@bciers/types/tiles";
+import { auth } from "@/dashboard/auth";
 
 export default async function Page() {
+  const session = await auth();
+
+  const isIndustryUser = session?.user?.app_role?.includes("industry");
+
   // 🚀 API fetch dashboard tiles
   // 🚩 Source: bc_obps/common/fixtures/dashboard/bciers/[IdProviderType]
   const data = (await fetchDashboardData(
@@ -13,11 +20,13 @@ export default async function Page() {
   // Build the navigation tiles
   return (
     <>
-      <Note variant="important">
-        <b>Important:</b> Please always ensure that the information in{" "}
-        <b>Registration</b> is complete and accurate before submit or amend
-        reports in <b>Reporting.</b>
-      </Note>
+      {isIndustryUser && (
+        <Note variant="important">
+          <b>Important:</b> Please always ensure that the information in{" "}
+          <b>Registration</b> is complete and accurate before submit or amend
+          reports in <b>Reporting.</b>
+        </Note>
+      )}
       <Tiles tiles={data} />
     </>
   );
