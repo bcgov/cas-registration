f00705e30d29b6b2bc7f40e0e34de3e5d43bdd6c
marcellmueller <marcel@button.is>
2024-06-20 08:04:36 -0700

    chore: fix linting errors from rebase

--- bciers/apps/dashboard/app/layout.tsx
+++ bciers/apps/dashboard/app/layout.tsx
@@ -5,6 +5,7 @@ The root layout must define <html> and <body> tags.
 You should not manually add <head> tags such as <title> and <meta> to root layouts. Instead, you should use the Metadata API which automatically handles advanced requirements such as streaming and de-duplicating <head> elements.
 */
 
+// eslint-disable-next-line import/extensions
 import "@bciers/styles/globals.css";
 import { RootLayout } from "@bciers/components/server";
 
