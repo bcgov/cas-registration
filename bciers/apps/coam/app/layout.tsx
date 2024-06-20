d7690ee12a15cb9cceac2ea2ce66619b4639e338
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2024-06-20 16:19:20 -0700

    ci: Edit coam config to fix eslint errors

--- bciers/apps/coam/app/layout.tsx
+++ bciers/apps/coam/app/layout.tsx
@@ -4,7 +4,7 @@ The app directory must include a root app/layout.js.
 The root layout must define <html> and <body> tags.
 You should not manually add <head> tags such as <title> and <meta> to root layouts. Instead, you should use the Metadata API which automatically handles advanced requirements such as streaming and de-duplicating <head> elements.
 */
-
+// eslint-disable-next-line import/extensions
 import "@bciers/styles/globals.css";
 import { RootLayout } from "@bciers/components/server";
 
