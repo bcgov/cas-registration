b3ffa465824bdd4dceb5f30b148a5afc4b0c786d
shon-button <shon@button.is>
2024-06-20 17:00:20 -0400

    chore: updated reporting middleware

--- bciers/apps/reporting/src/middlewares/withAuthorizationReporting.tsx
+++ bciers/apps/reporting/src/middlewares/withAuthorizationReporting.tsx
@@ -6,20 +6,18 @@ import {
 } from "next/server";
 
 import { MiddlewareFactory } from "@bciers/middlewares/server";
+
+import { IDP } from "@/app/utils/enums";
 import { getToken } from "@bciers/actions/server";
+import path from "path";
+
 /*
 Access control logic is managed using Next.js middleware and NextAuth.js authentication JWT token.
 The middleware intercepts requests, and for restricted areas...
-Checks for a valid user session, and extracts user information from the JWT token.
+Checks for a valid user token, and extracts user information from the JWT token.
 Based on JWT properties of identity_provider and role, the middleware dynamically rewrites the request URL
 to the appropriate folder structure.
  */
-
-// Function to check if the path is in the unauthenticated allow list
-const isUnauthenticatedAllowListedPath = (pathname: string): boolean => {
-  const authList = ["auth", "unauth"];
-  return authList.some((path) => pathname.includes(path));
-};
 
 // Function to check if the path is in the authenticated allow list
 const isAuthenticatedAllowListedPath = (pathname: string): boolean => {
@@ -28,73 +26,29 @@ const isAuthenticatedAllowListedPath = (pathname: string): boolean => {
   return allowList.includes(lastSegment || "");
 };
 
-// Function to check if the path requires authorization
-const isAuthorizationRequiredPath = (
-  pathname: string,
-  token: { identity_provider?: string; app_role?: string },
-): boolean => {
-  if (!token) {
-    return false;
-  }
-
-  const idp = token.identity_provider;
-  const appRole = token.app_role;
-  const authRoute = `${idp}/${appRole}`;
-
-  return (
-    !pathname.includes(authRoute) &&
-    !isUnauthenticatedAllowListedPath(pathname) &&
-    !isAuthenticatedAllowListedPath(pathname)
-  );
-};
-
 // Middleware for authorization
-export const withAuthorization: MiddlewareFactory = (next: NextMiddleware) => {
+export const withAuthorizationReporting: MiddlewareFactory = (
+  next: NextMiddleware,
+) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
     const { pathname } = request.nextUrl;
-    // Check if the path is in the unauthenticated allow list
-    if (isUnauthenticatedAllowListedPath(pathname)) {
-      return next(request, _next);
-    }
     // Check if the user is authenticated via the jwt encoded in server side cookie
     const token = await getToken();
-    if (token) {
-      // Redirect root requests to the dashboard
-      if (pathname.endsWith("/reporting")) {
-        return NextResponse.redirect(
-          new URL(`/reporting/dashboard`, request.url),
-        );
-      }
 
-      // Check if the path is in the authenticated allow list
+    if (token) {
+      // Check if the path is in the authenticated allow list- does not build authorization folders
       if (isAuthenticatedAllowListedPath(pathname)) {
         return next(request, _next);
       }
 
-      // Check if the path requires authorization
-      if (isAuthorizationRequiredPath(pathname, token)) {
-        //rewrite the request to reflected the token permissions
-        request.nextUrl.pathname = `${token.identity_provider}/${
-          token.app_role
-        }${pathname.replace("reporting/", "")}`;
-
-        return NextResponse.rewrite(request.nextUrl);
-      }
-
-      // Routes with the folder structure break the breadcrumbs
-      const pageSegment = pathname.replace(
-        `/${token.identity_provider}/${token.app_role}`,
-        "",
-      );
-
-      return NextResponse.redirect(new URL(`${pageSegment}`, request.url));
+      // build rewrite to physcial folder path which enforces authorization by IdP and role
+      request.nextUrl.pathname = `${token.identity_provider}/${
+        token.app_role
+      }${pathname.replace("reporting/", "")}`;
+      return NextResponse.rewrite(request.nextUrl);
     } else {
       // Handle unauthenticated requests
-      if (pathname.endsWith("/home")) {
-        return next(request, _next);
-      } else {
-        return NextResponse.redirect(new URL(`/home`, request.url));
-      }
+      return NextResponse.redirect(new URL(`/dashboard`, request.url));
     }
   };
 };
