55f43b65c85d5853ddb9d813cac828e63bcaada3
SeSo <sobhani.sepehr@gmail.com>
2024-06-20 15:02:28 -0700

    chore: remove hardcoded user to bypass authorization

--- bc_obps/registration/api/v1/_operations/_operation_id/facilities.py
+++ bc_obps/registration/api/v1/_operations/_operation_id/facilities.py
@@ -3,12 +3,14 @@ from registration.api.router import router
 from typing import List, Literal, Optional
 from django.db.models import QuerySet
 from django.http import HttpRequest
+from registration.api.utils.current_user_utils import get_current_user_guid
 from registration.constants import FACILITY_TAGS
+from registration.models.app_role import AppRole
 from registration.models.facility import Facility
-from registration.models.user import User
+from registration.models.user_operator import UserOperator
 from registration.schema.v1.facility import FacilityFilterSchema, FacilityListOut
 from service.facility_service import FacilityService
-from registration.decorators import handle_http_errors
+from registration.decorators import authorize, handle_http_errors
 from ninja.pagination import paginate, PageNumberPagination
 from registration.schema.generic import Message
 from ninja.responses import codes_4xx
@@ -22,7 +24,7 @@ from ninja import Query
     description="""Retrieves a paginated list of facilities based on the provided filters.
     The endpoint allows authorized users to view and sort facilities associated to an operation filtered by various criteria such as facility name, type, and bcghg_id.""",
 )
-# @authorize(AppRole.get_all_authorized_app_roles(), UserOperator.get_all_industry_user_operator_roles())
+@authorize(AppRole.get_all_authorized_app_roles(), UserOperator.get_all_industry_user_operator_roles())
 @handle_http_errors()
 @paginate(PageNumberPagination)
 def list_facilities(
@@ -32,6 +34,7 @@ def list_facilities(
     sort_field: Optional[str] = "created_at",
     sort_order: Optional[Literal["desc", "asc"]] = "desc",
 ) -> QuerySet[Facility]:
-    user = User.objects.get(pk="4da70f32-65fd-4137-87c1-111f2daba3dd")  # TODO: TEMP
     # NOTE: PageNumberPagination raises an error if we pass the response as a tuple (like 200, ...)
-    return FacilityService.list_facilities(user.user_guid, operation_id, sort_field, sort_order, filters)
+    return FacilityService.list_facilities(
+        get_current_user_guid(request), operation_id, sort_field, sort_order, filters
+    )
