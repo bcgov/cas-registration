0ae94300902a4330255bb38064d46dc02fbd7a94
shon-button <shon@button.is>
2024-06-22 09:31:03 -0400

    chore: nx tests reporting

--- bciers/apps/reporting/src/app/page.tsx
+++ bciers/apps/reporting/src/app/page.tsx
@@ -1,11 +1,8 @@
 import Tiles from "@bciers/components/navigation/Tiles";
 import { fetchDashboardData } from "@bciers/actions/server";
 import { ContentItem } from "@bciers/types";
-import { auth } from "@/dashboard/auth";
 
 export default async function Page() {
-  const session = await auth();
-  const role = session?.user?.app_role || "";
   // 🚀 API fetch dashboard tiles
   // 🚩 Source: bc_obps/common/fixtures/dashboard/reporting/[IdProviderType]/role?
   const data = (await fetchDashboardData(
