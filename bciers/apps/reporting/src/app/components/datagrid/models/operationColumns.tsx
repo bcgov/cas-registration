18b2abb27646bcd669914fc6171a6027719b506f
Ballard <ballard@button.is>
2024-06-26 13:03:38 -0400

    chore: add more column menu

--- bciers/apps/reporting/src/app/components/datagrid/models/operationColumns.tsx
+++ bciers/apps/reporting/src/app/components/datagrid/models/operationColumns.tsx
@@ -1,8 +1,52 @@
 import { GridColDef } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import * as React from "react";
 
 export const OPERATOR_COLUMN_INDEX = 1;
 
-const operationColumns = () => {
+const ActionCell: React.FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        ...
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Placeholder 1</MenuItem>
+        <MenuItem onClick={handleClose}>Placeholder 2</MenuItem>
+        <MenuItem onClick={handleClose}>Placeholder 3</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+const operationColumns = (): GridColDef[] => {
   const columns: GridColDef[] = [
     { field: "bcghg_id", headerName: "BC GHG ID", width: 160 },
     {
@@ -13,10 +57,9 @@ const operationColumns = () => {
     {
       field: "action",
       headerName: "Action",
-      renderCell: () => <div>...</div>,
+      renderCell: () => <ActionCell />,
       sortable: false,
       width: 120,
-      // Set flex to 1 to make the column take up all the remaining width if user zooms out
       flex: 1,
     },
   ];
