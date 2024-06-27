2920cf7fd45692176829e43b72d6de56801ae3ef
Brianna Cerkiewicz <brianna@button.is>
2024-06-27 12:05:53 -0700

    test: vitests

---
+++ bciers/apps/registration/app/components/operations/getOperation.tsx
@@ -0,0 +1,14 @@
+import { actionHandler } from "@bciers/utils/actions";
+
+export default async function getOperation(id: string) {
+  try {
+    return await actionHandler(
+      `registration/operations/${id}`,
+      "GET",
+      `/operations/${id}`,
+    );
+  } catch (error) {
+    // Handle the error here or rethrow it to handle it at a higher level
+    throw error;
+  }
+}
--- bciers/libs/shared/testConfig/src/mocks.ts
+++ bciers/libs/shared/testConfig/src/mocks.ts
@@ -34,6 +34,8 @@ const notFound = vi.fn();
 const useSession = vi.fn();
 const auth = vi.fn();
 const fetchOperationsPageData = vi.fn();
+const getFacility = vi.fn();
+const getOperation = vi.fn();
 
 export {
   actionHandler,
@@ -47,4 +49,6 @@ export {
   useSession,
   fetchOperationsPageData,
   notFound,
+  getFacility,
+  getOperation,
 };
