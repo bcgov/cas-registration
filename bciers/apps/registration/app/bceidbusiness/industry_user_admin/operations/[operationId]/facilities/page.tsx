188c2591af0af027c1bb90ec79ae9363e545aeb4
SeSo <sobhani.sepehr@gmail.com>
2024-06-27 13:55:48 -0700

    chore: rename operation route to operationId

--- bciers/apps/registration/app/bceidbusiness/industry_user_admin/operations/[operationId]/facilities/page.tsx
+++ bciers/apps/registration/app/bceidbusiness/industry_user_admin/operations/[operationId]/facilities/page.tsx
@@ -1,13 +1,16 @@
 // 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
 import { FacilitiesSearchParams } from "apps/registration/app/components/facilities/types";
 import FacilitiesPage from "apps/registration/app/components/routes/facilities/Page";
+import { UUID } from "crypto";
 
 export default async function Page({
-  params: { operation },
+  params: { operationId },
   searchParams,
 }: Readonly<{
-  params: { operation: string };
+  params: { operationId: UUID };
   searchParams: FacilitiesSearchParams;
 }>) {
-  return <FacilitiesPage operationId={operation} searchParams={searchParams} />;
+  return (
+    <FacilitiesPage operationId={operationId} searchParams={searchParams} />
+  );
 }
