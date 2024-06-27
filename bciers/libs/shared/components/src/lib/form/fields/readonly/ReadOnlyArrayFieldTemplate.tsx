3e255781d4414409624d9e5097ef27512bdc88da
Brianna Cerkiewicz <brianna@button.is>
2024-06-27 08:16:02 -0700

    feat: add facility form

---
+++ bciers/libs/shared/components/src/lib/form/fields/readonly/ReadOnlyArrayFieldTemplate.tsx
@@ -0,0 +1,45 @@
+import { ArrayFieldTemplateProps, FieldTemplateProps } from "@rjsf/utils";
+
+function BasicFieldTemplate({ children }: FieldTemplateProps) {
+  return <>{children}</>;
+}
+
+const ReadOnlyArrayFieldTemplate = ({
+  items,
+  uiSchema,
+}: ArrayFieldTemplateProps) => {
+  const customTitleName = uiSchema?.["ui:options"]?.title as string;
+
+  return (
+    <div className="flex min-w-full flex-col">
+      {items?.map((item, i: number) => {
+        return (
+          <div key={item.key} className="min-w-full">
+            <div className="form-heading flex justify-between m-w-full my-10">
+              {customTitleName && (
+                <span>
+                  {customTitleName} {i + 1}
+                </span>
+              )}
+            </div>
+            {{
+              ...item.children,
+              props: {
+                ...item.children.props,
+                uiSchema: {
+                  ...item.children.props.uiSchema,
+                  "ui:FieldTemplate": BasicFieldTemplate,
+                  "ui:options": {
+                    label: false,
+                  },
+                },
+              },
+            }}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ReadOnlyArrayFieldTemplate;
