378729832ef01bbfcc54f2cdad4b948ff272cfe2
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-06-28 11:28:41 -0700

    test: reporting year model

--- bc_obps/reporting/tests/models/test_reporting_year.py
+++ bc_obps/reporting/tests/models/test_reporting_year.py
@@ -1,6 +1,20 @@
 from common.tests.utils.helpers import BaseTestCase
+from reporting.models.reporting_year import ReportingYear
 
 
 class ReportingYearTest(BaseTestCase):
-    def fail(self):
-        raise Exception('aaa')
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_object = ReportingYear.objects.create(
+            reporting_year=1234,
+            reporting_window_start="2025-01-01T00:00:00.000Z",
+            reporting_window_end="2025-01-01T01:00:00.000Z",
+            description="test description",
+        )
+        cls.field_data = [
+            ("reporting_year", "reporting year", None, None),
+            ("reporting_window_start", "reporting window start", None, None),
+            ("reporting_window_end", "reporting window end", None, None),
+            ("description", "description", None, None),
+            ("report", "report", None, None),
+        ]
