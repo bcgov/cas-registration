166b094cf8f85718363a10cf1959b99d5eddefa2
Josh Gamache <joshua@button.is>
2024-07-02 13:21:53 -0600

    fix: build fails due to global vi

--- bciers/libs/shared/testConfig/src/mocks.ts
+++ bciers/libs/shared/testConfig/src/mocks.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { vi } from "vitest";
 import { fetchMocker } from "./global";
 
 // To reduce code duplication and simplify mocking you can import these common mocks from this file
