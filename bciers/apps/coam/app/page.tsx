f847da8183d178b1518612957439a76a50caa5bf
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2024-07-02 12:00:28 -0700

    feat: [rebase 2] Configure COAM app for auth sharing + fix css and package json for coam

--- bciers/apps/coam/app/page.tsx
+++ bciers/apps/coam/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Index() {
+import Tiles from "@bciers/components/navigation/Tiles";
+import { fetchDashboardData } from "@bciers/actions/server";
+import { ContentItem } from "@bciers/types";
+
+export default async function Page() {
+  // 🚀 API fetch dashboard tiles
+  // 🚩 Source: bc_obps/common/fixtures/dashboard/coam/[IdProviderType]/role?
+  const data = (await fetchDashboardData(
+    "common/dashboard-data?dashboard=reporting",
+  )) as ContentItem[];
+
   return (
-    <div>
-      <h1>Compliance Obligation Administration Module - (COAM)</h1>
-    </div>
+    <>
+      <Tiles tiles={data} />
+    </>
   );
 }
