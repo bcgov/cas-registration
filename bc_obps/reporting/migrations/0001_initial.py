d07b0b284915c6807aeaf6c035db0658a4acfbca
Dylan Leard <dylan@button.is>
2024-07-02 14:17:16 -0700

    chore: add prod & schema data migrations after rebase

--- bc_obps/reporting/migrations/0001_initial.py
+++ bc_obps/reporting/migrations/0001_initial.py
@@ -12,3 +12,18 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='Report',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(db_comment='The title of the report', max_length=100)),
+                ('description', models.TextField(db_comment='The description of the report')),
+                (
+                    'created_at',
+                    models.DateTimeField(auto_now_add=True, db_comment='The timestamp when the report was created'),
+                ),
+            ],
+            options={
+                'db_table': 'erc"."report',
+                'db_table_comment': 'A table to store reports',
+            },
+        ),
+    ]
