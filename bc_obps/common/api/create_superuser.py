5337981552253bc9f19f276a2237a27fbf6ed3e1
SeSo <sobhani.sepehr@gmail.com>
2024-07-03 08:34:15 -0700

    chore: add error logging to make codeql happy

--- bc_obps/common/api/create_superuser.py
+++ bc_obps/common/api/create_superuser.py
@@ -1,8 +1,11 @@
+import logging
 from .router import router
 from registration.constants import MISC_TAGS
 from django.core.management import call_command
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse
+
+logger = logging.getLogger(__name__)
 
 
 @router.get(
@@ -16,6 +19,7 @@ def setup(request: HttpRequest) -> HttpResponse:
             call_command('create_superuser')
             return HttpResponse("Superuser created successfully.", status=200)
         except Exception as e:
-            return HttpResponse(f"An error occurred while creating the superuser: {str(e)}", status=500)
+            logger.error(f"An error occurred while creating the superuser: {str(e)}")
+            return HttpResponse("An error occurred while creating the superuser.")
     else:
         return HttpResponse("This endpoint only exists in the development environment.", status=404)
