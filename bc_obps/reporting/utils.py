182761fc882ad396969f386321b3e3621ddc74d4
Dylan Leard <dylan@button.is>
2024-07-03 08:54:05 -0700

    chore: fix util function to handle objects without a source_type

--- bc_obps/reporting/utils.py
+++ bc_obps/reporting/utils.py
@@ -10,9 +10,14 @@ def validate_overlapping_records(
     save_self: Union[ActivityJsonSchema, ActivitySourceTypeJsonSchema, ConfigurationElement],
     exception_message: str,
 ) -> None:
-    all_ranges = object_class.objects.select_related('valid_from', 'valid_to').filter(
-        reporting_activity=save_self.reporting_activity, source_type=save_self.source_type
-    )
+    if hasattr(object_class, "source_type"):
+      all_ranges = object_class.objects.select_related('valid_from', 'valid_to').filter(
+          reporting_activity=save_self.reporting_activity, source_type=save_self.source_type
+      )
+    else:
+      all_ranges = object_class.objects.select_related('valid_from', 'valid_to').filter(
+          reporting_activity=save_self.reporting_activity
+      )
     for y in all_ranges:
         if (
             (save_self.valid_from.valid_from >= y.valid_from.valid_from)
