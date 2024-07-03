0c2944c8f8b061ba6373e114c175eaf8759ac434
SeSo <sobhani.sepehr@gmail.com>
2024-07-03 13:00:58 -0700

    chore: handle existing superuser in create_superuser command

--- bc_obps/registration/management/commands/create_superuser.py
+++ bc_obps/registration/management/commands/create_superuser.py
@@ -2,6 +2,7 @@ import os
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
@@ -12,9 +13,13 @@ class Command(BaseCommand):
         superuser_username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
         superuser_password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
         if superuser_username and superuser_password:
-            User.objects.create_superuser(username=superuser_username, password=superuser_password)
-            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
-            return None
+            try:
+                User.objects.create_superuser(username=superuser_username, password=superuser_password)
+                self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+                return None
+            except IntegrityError:
+                self.stdout.write(self.style.ERROR('Superuser already exists.'))
+                return None
 
         self.stdout.write(self.style.ERROR('Superuser not created.'))
         raise Exception('Superuser not created.')
