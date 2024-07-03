1ab571312efeb20bc98da2aa344e9dc9eced8ce9
marcellmueller <marcel@button.is>
2024-07-03 08:03:47 -0700

    chore: remove duplicate types

--- bciers/apps/registration/app/components/profile/ProfileForm.tsx
+++ bciers/apps/registration/app/components/profile/ProfileForm.tsx
@@ -8,24 +8,11 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { RJSFSchema } from "@rjsf/utils";
 import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
+import {
+  UserProfileFormData,
+  UserProfilePartialFormData,
+} from "@bciers/types/form/formData";
 
-export interface UserProfileFormData {
-  first_name: string;
-  last_name: string;
-  position_title: string;
-  email: string;
-  phone_number: string;
-  app_role?: { role_name: string };
-}
-
-export interface UserProfilePartialFormData {
-  first_name?: string;
-  last_name?: string;
-  position_title?: string;
-  email?: string;
-  phone_number?: string;
-  app_role?: { role_name: string };
-}
 export const userSchema: RJSFSchema = {
   type: "object",
   required: ["first_name", "last_name", "phone_number", "position_title"],
