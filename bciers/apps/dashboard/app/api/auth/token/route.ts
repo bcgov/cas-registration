d67a9bc7e6a6e2c440f1b33365529f30a32c48a0
Josh Gamache <joshua@button.is>
2024-07-04 08:46:20 -0600

    chore: cookie-based salt variable

--- bciers/apps/dashboard/app/api/auth/token/route.ts
+++ bciers/apps/dashboard/app/api/auth/token/route.ts
@@ -1,14 +1,17 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
+const cookieName =
+  process.env.NODE_ENV === "production"
+    ? "__Secure-authjs.session-token"
+    : "authjs.session-token";
+
 export async function GET(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: `${process.env.NEXTAUTH_SECRET}`,
-    salt:
-      process.env.NODE_ENV === "production"
-        ? "__Secure-authjs.session-token"
-        : "authjs.session-token",
+    salt: cookieName,
+    cookieName: cookieName,
   });
   return NextResponse.json(token, { status: 200 });
 }
