92a6119a81b43f1991aa04cbd0da3806bb29f643
marcellmueller <marcel@button.is>
2024-07-04 15:54:38 -0700

    chore: remove date widget and libraries

--- bciers/libs/components/src/form/widgets/index.ts
+++ bciers/libs/components/src/form/widgets/index.ts
@@ -2,7 +2,6 @@
 
 export { default as CheckboxWidget } from "./CheckboxWidget";
 export { default as ComboBox } from "./ComboBox";
-export { default as DateWidget } from "./DateWidget";
 export { default as EmailWidget } from "./EmailWidget";
 export { default as FileWidget } from "./FileWidget";
 export { default as MultiSelectWidget } from "./MultiSelectWidget";
