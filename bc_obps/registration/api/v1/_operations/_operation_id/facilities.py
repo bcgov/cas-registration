4df0f399adce779f0bb395cc453be3e4b2def4e6
SeSo <sobhani.sepehr@gmail.com>
2024-07-04 21:42:41 -0700

    chore: use custom error code

--- bc_obps/registration/api/v1/_operations/_operation_id/facilities.py
+++ bc_obps/registration/api/v1/_operations/_operation_id/facilities.py
@@ -13,13 +13,13 @@ from service.facility_service import FacilityService
 from registration.decorators import authorize, handle_http_errors
 from ninja.pagination import paginate, PageNumberPagination
 from registration.schema.generic import Message
-from ninja.responses import codes_4xx
+from service.error_service.custom_codes_4xx import custom_codes_4xx
 from ninja import Query
 
 
 @router.get(
     "/operations/{operation_id}/facilities",
-    response={200: List[FacilityListOut], codes_4xx: Message},
+    response={200: List[FacilityListOut], custom_codes_4xx: Message},
     tags=FACILITY_TAGS,
     description="""Retrieves a paginated list of facilities based on the provided filters.
     The endpoint allows authorized users to view and sort facilities associated to an operation filtered by various criteria such as facility name, type, and bcghg_id.""",
