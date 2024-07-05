aab82f1309fe73ae751eac78a1bc1ee19dc82a10
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-07-05 14:07:07 -0700

    chore: adding filter schema

--- bc_obps/reporting/schema/operation.py
+++ bc_obps/reporting/schema/operation.py
@@ -1,10 +1,12 @@
-from ninja import ModelSchema
+from typing import Optional, Union
+from ninja import FilterSchema, ModelSchema
 from registration.models.operation import Operation
 
 
 class ReportingDashboardOperationOut(ModelSchema):
     report_id: int
     report_version_id: int
+    report_status: str
 
     class Meta:
         model = Operation
@@ -17,3 +19,16 @@ class ReportingDashboardOperationOut(ModelSchema):
     @staticmethod
     def resolve_report_version_id(obj: Operation) -> int:
         return 0
+
+    @staticmethod
+    def resolve_report_status(obj: Operation) -> int:
+        return "test"
+
+
+class ReportingDashboardOperationFilterSchema(FilterSchema):
+    bcghg_id: Optional[str] = None
+    name: Optional[str] = None
+    report_status: Optional[str] = None
+    page: Union[int, float, str] = 1
+    sort_field: Optional[str] = "created_at"
+    sort_order: Optional[str] = "desc"
