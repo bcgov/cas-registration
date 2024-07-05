e5e3b9bdbc1ddc95841f2966b268c7eaeb5ef1b5
SeSo <sobhani.sepehr@gmail.com>
2024-07-05 10:09:29 -0700

    chore: replicate contacts page for other roles

---
+++ bciers/apps/registration/app/bceidbusiness/industry_user_admin/contacts/page.tsx
@@ -0,0 +1,11 @@
+// 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
+import { ContactsSearchParams } from "@/registration/app/components/contacts/types";
+import ContactsPage from "@/registration/app/components/routes/contacts/Page";
+
+export default async function Page({
+  searchParams,
+}: Readonly<{
+  searchParams: ContactsSearchParams;
+}>) {
+  return <ContactsPage searchParams={searchParams} />;
+}
