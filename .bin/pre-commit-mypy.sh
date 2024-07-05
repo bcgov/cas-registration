cd8265e5ae8b55464abf8433d95955765c3c3482
SeSo <sobhani.sepehr@gmail.com>
2024-07-05 09:31:36 -0700

    chore: not using mypy cache in pre-commit

--- .bin/pre-commit-mypy.sh
+++ .bin/pre-commit-mypy.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 
 # Navigate to the backend directory
 pushd bc_obps || exit 1
-
-make mypy
+# Not using the cache in pre-commit as it was causing issues when switching branches
+# If you are troubleshooting mypy issues, just run `make mypy` in the `bc_obps` directory to benefit from the mypy incremental mode and cache
+make mypy ARGS="--cache-dir=/dev/null"
