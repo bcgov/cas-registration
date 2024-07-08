0673ed58e61c4f94a126380d47171804467e3975
Dylan Leard <dylan@button.is>
2024-07-08 10:32:49 -0700

    chore: add reporting_field data

--- bc_obps/reporting/migrations/0007_prod_data.py
+++ bc_obps/reporting/migrations/0007_prod_data.py
@@ -1077,6 +1077,81 @@ def reverse_init_configuration_element_data(apps, schema_monitor):
         valid_to_id=Configuration.objects.get(valid_to='2099-12-31').id,
     ).delete()
 
+def init_reporting_field_data(apps, schema_monitor):
+    '''
+    Add initial data to erc.reporting_field
+    '''
+
+    ReportingField = apps.get_model('reporting', 'ReportingField')
+    ReportingField.objects.bulk_create(
+        [
+            ReportingField(field_name='Fuel Default High Heating Value', field_type='number', field_units=None),
+            ReportingField(
+                field_name='Unit-Fuel-CO2 Default Emission Factor', field_type='number', field_units='kg/GJ'
+            ),
+            ReportingField(
+                field_name='Unit-Fuel-CO2 Default Emission Factor', field_type='number', field_units='kg/fuel units'
+            ),
+            ReportingField(
+                field_name='Fuel Annual Weighted Average High Heating Value', field_type='number', field_units=None
+            ),
+            ReportingField(field_name='Unit-Fuel Annual Steam Generated', field_type='number', field_units=None),
+            ReportingField(field_name='Boiler Ratio', field_type='number', field_units=None),
+            ReportingField(field_name='Unit-Fuel-CO2 Emission Factor', field_type='number', field_units='kg/GJ'),
+            ReportingField(
+                field_name='Fuel Annual Weighted Average Carbon Content (weight fraction)',
+                field_type='number',
+                field_units=None,
+            ),
+            ReportingField(field_name='Unit-Fuel-CO2 Measured Emission Factor', field_type='number', field_units=None),
+            ReportingField(field_name='Description', field_type='string', field_units=None),
+            ReportingField(
+                field_name='Unit-Fuel-CH4 Default Emission Factor', field_type='number', field_units='kg/GJ'
+            ),
+            ReportingField(
+                field_name='Unit-Fuel-CH4 Default Emission Factor', field_type='number', field_units='kg/fuel units'
+            ),
+            ReportingField(
+                field_name='Unit-Fuel-CH4 Measured Emission Factor', field_type='number', field_units='kg/fuel units'
+            ),
+            ReportingField(field_name='Unit-Fuel Heat Input', field_type='number', field_units=None),
+            ReportingField(
+                field_name='Unit-Fuel-N2O Default Emission Factor', field_type='number', field_units='kg/GJ'
+            ),
+            ReportingField(
+                field_name='Unit-Fuel-N2O Default Emission Factor', field_type='number', field_units='kg/fuel units'
+            ),
+            ReportingField(
+                field_name='Unit-Fuel-N2O Measured Emission Factor', field_type='number', field_units='kg/fuel units'
+            ),
+        ]
+    )
+
+
+def reverse_init_reporting_field_data(apps, schema_monitor):
+    '''
+    Remove initial data from erc.reporting_field
+    '''
+    ReportingField = apps.get_model('reporting', 'ReportingField')
+    ReportingField.objects.filter(
+        field_name__in=[
+            'Unit-Fuel-CO2 Measured Emission Factor'
+            'Unit-Fuel Heat Input'
+            'Unit-Fuel-CO2 Default Emission Factor'
+            'Unit-Fuel-CH4 Measured Emission Factor'
+            'Unit-Fuel Annual Steam Generated'
+            'Description'
+            'Unit-Fuel-CO2 Emission Factor'
+            'Boiler Ratio'
+            'Unit-Fuel-N2O Default Emission Factor'
+            'Unit-Fuel-CH4 Default Emission Factor'
+            'Fuel Annual Weighted Average High Heating Value'
+            'Unit-Fuel-N2O Measured Emission Factor'
+            'Fuel Default High Heating Value'
+            'Fuel Annual Weighted Average Carbon Content (weight fraction)'
+        ]
+    ).delete()
+
 
 class Migration(migrations.Migration):
 
@@ -1091,4 +1166,5 @@ class Migration(migrations.Migration):
         migrations.RunPython(init_methodology_data, reverse_init_methodology_data),
         migrations.RunPython(init_configuration_data, reverse_init_configuration_data),
         migrations.RunPython(init_configuration_element_data, reverse_init_configuration_element_data),
+        migrations.RunPython(init_reporting_field_data, reverse_init_reporting_field_data),
     ]
