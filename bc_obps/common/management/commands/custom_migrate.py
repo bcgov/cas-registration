b46a9288595d04bceb9fa917c47c6a6c31f3b664
SeSo <sobhani.sepehr@gmail.com>
2024-07-11 16:57:07 -0700

    feat: add custom migrate command

---
+++ bc_obps/common/management/commands/custom_migrate.py
@@ -0,0 +1,78 @@
+import os
+import re
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    help = 'Run migrations up to the latest migration file for predefined apps dynamically'
+
+    def handle(self, *args, **options):
+        # List of predefined apps
+        apps_to_migrate = ['common', 'registration', 'reporting']
+
+        for app_label in apps_to_migrate:
+            self.migrate_app_to_latest(app_label)
+
+    def migrate_app_to_latest(self, app_label):
+        """
+        Run migrations for a specific app up to the latest migration file.
+
+        Args:
+            app_label (str): The label of the app to migrate.
+        """
+        migration_dir = self.get_migration_directory(app_label)
+        if not migration_dir:
+            self.stdout.write(self.style.WARNING(f'No migrations directory found for {app_label}'))
+            return
+
+        migration_files = self.get_migration_files(migration_dir)
+        if not migration_files:
+            self.stdout.write(self.style.WARNING(f'No migration files found for {app_label}'))
+            return
+
+        latest_migration = self.get_latest_migration_file(migration_files)
+        latest_migration_name = os.path.splitext(latest_migration)[0]
+
+        self.stdout.write(f'Running migrations for {app_label} up to {latest_migration_name}')
+        call_command('migrate', app_label, latest_migration_name)
+        self.stdout.write(self.style.SUCCESS(f'Successfully migrated {app_label} to {latest_migration_name}'))
+
+    def get_migration_directory(self, app_label):
+        """
+        Get the path to the migrations directory for a given app.
+
+        Args:
+            app_label (str): The label of the app.
+
+        Returns:
+            str: The path to the migrations directory.
+        """
+        app_config = apps.get_app_config(app_label)
+        return os.path.join(app_config.path, 'migrations')
+
+    def get_migration_files(self, migration_dir):
+        """
+        Get a list of migration files in the specified directory that match the required format.
+
+        Args:
+            migration_dir (str): The path to the migrations directory.
+
+        Returns:
+            list: A list of migration file names.
+        """
+        return [f for f in os.listdir(migration_dir) if re.match(r'^\d{4}_V\d+_\d+_\d+\.py$', f) and f != '__init__.py']
+
+    def get_latest_migration_file(self, migration_files):
+        """
+        Determine the latest migration file based on the numeric parts in the filename.
+
+        Args:
+            migration_files (list): A list of migration file names.
+
+        Returns:
+            str: The name of the latest migration file.
+        """
+        migration_files.sort(key=lambda x: tuple(map(int, re.findall(r'\d+', x))))
+        return migration_files[-1]
