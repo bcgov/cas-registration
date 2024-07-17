973b0fb99203dc198d0d386a52b5e10678667270
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-07-17 13:51:10 -0700

    chore: reverting root page

--- bciers/apps/reporting/src/app/page.tsx
+++ bciers/apps/reporting/src/app/page.tsx
@@ -7,8 +7,6 @@ import Main from "@bciers/components/layout/Main";
 import Logo from "@bciers/img/src/BCID_CleanBC_rev_tagline_colour.svg";
 import Image from "next/image";
 
-import TaskList from "@bciers/components/form/components/TaskList";
-
 export default function Index() {
   const handleIdirLogin = () => {
     signIn("keycloak", undefined, { kc_idp_hint: "idir" });
@@ -18,26 +16,33 @@ export default function Index() {
     signIn("keycloak", undefined, { kc_idp_hint: "bceidbusiness" });
   };
 
-  const items = [
-    {
-      section: "test",
-      title: "object 1",
-    },
-    {
-      section: "test",
-      title: "object 2",
-    },
-    { section: "test2", title: "aaa" },
-  ];
-  const status = {
-    test: true,
-    test2: false,
-  };
-
   return (
     <Main>
       <Image src={Logo} alt="testing" width="200" height="43" />
-      <TaskList taskListItems={items} taskListItemStatus={status}></TaskList>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          marginTop: "24px",
+          marginBottom: "48px",
+        }}
+      >
+        <Button
+          variant="outlined"
+          className="w-full md:max-w-[70%]"
+          onClick={handleIdirLogin}
+        >
+          Log in with IDIR
+        </Button>
+        <div></div>
+        <Button
+          variant="outlined"
+          className="w-full md:max-w-[70%]"
+          onClick={handleBceidLogin}
+        >
+          Log in with Business BCeID
+        </Button>
+      </Grid>
     </Main>
   );
 }
