6cbaf92b02631a6d46c9833f00ada76d517aab83
SeSo <sobhani.sepehr@gmail.com>
2024-07-17 14:48:16 -0700

    chore: add contact related endpoint, schema and service

---
+++ bc_obps/registration/api/v1/_user_operators/_current/users.py
@@ -0,0 +1,29 @@
+from django.http import HttpRequest
+from registration.api.utils.current_user_utils import get_current_user_guid
+from registration.constants import USER_OPERATOR_TAGS
+from registration.models.user_operator import UserOperator
+from service.data_access_service.user_operator_service import UserOperatorDataAccessService
+from django.db.models import QuerySet
+from registration.decorators import authorize, handle_http_errors
+from registration.schema.v1 import UserOperatorUsersOut
+from registration.schema.generic import Message
+from typing import List, Literal, Tuple
+from registration.api.router import router
+from service.error_service.custom_codes_4xx import custom_codes_4xx
+
+
+# "current" refers to the current user-operator (we look up the current user-operator via the current user)
+@router.get(
+    "/user-operators/current/users",
+    response={200: List[UserOperatorUsersOut], custom_codes_4xx: Message},
+    tags=USER_OPERATOR_TAGS,
+    description="""Retrieves the list of users associated with the operator of the current user. The current user is an industry user admin.
+    The endpoint returns the users that have Pending or Approved status.
+    The list is filtered based on the business GUID of the current user and the associated operator.""",
+)
+@authorize(["industry_user"], ["admin"])
+@handle_http_errors()
+def get_current_user_operator_users(request: HttpRequest) -> Tuple[Literal[200], QuerySet[UserOperator]]:
+    return 200, UserOperatorDataAccessService.get_an_operators_user_operators_by_user_guid(
+        get_current_user_guid(request)
+    )
--- bc_obps/service/contact_service.py
+++ bc_obps/service/contact_service.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from django.db.models import QuerySet
 from uuid import UUID
+from registration.constants import UNAUTHORIZED_MESSAGE
 from registration.models.contact import Contact
 from registration.schema.v1.contact import ContactFilterSchema
 from service.data_access_service.contact_service import ContactDataAccessService
@@ -22,3 +23,12 @@ class ContactService:
         sort_by = f"{sort_direction}{sort_field}"
         base_qs = ContactDataAccessService.get_all_contacts_for_user(user)
         return filters.filter(base_qs).order_by(sort_by)
+
+    @classmethod
+    def get_if_authorized(cls, user_guid: UUID, contact_id: int) -> Optional[Contact]:
+        user = UserDataAccessService.get_by_guid(user_guid)
+        user_contacts = ContactDataAccessService.get_all_contacts_for_user(user)
+        contact = user_contacts.filter(id=contact_id).first()
+        if user.is_industry_user() and not contact:
+            raise Exception(UNAUTHORIZED_MESSAGE)
+        return contact
