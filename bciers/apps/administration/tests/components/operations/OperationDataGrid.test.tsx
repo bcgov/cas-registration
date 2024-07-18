4346f2aaf66647dd6cdba94ed5b405356a036d89
shon-button <shon@button.is>
2024-07-18 11:16:49 -0400

    chore: fix obsolete tests

--- bciers/apps/administration/tests/components/operations/OperationDataGrid.test.tsx
+++ bciers/apps/administration/tests/components/operations/OperationDataGrid.test.tsx
@@ -123,11 +123,11 @@ describe("OperationsDataGrid component", () => {
 
     expect(facilitiesLinks[0]).toHaveAttribute(
       "href",
-      "operations/1/facilities",
+      "operations/1/facilities?operationsTitle=Operation 1",
     );
     expect(facilitiesLinks[1]).toHaveAttribute(
       "href",
-      "operations/2/facilities",
+      "operations/2/facilities?operationsTitle=Operation 2",
     );
   });
 
