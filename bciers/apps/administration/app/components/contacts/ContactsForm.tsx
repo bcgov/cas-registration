64d2a4e0b17dd73754345d1b391425bdd35ef3c1
marcellmueller <marcel@button.is>
2024-07-18 08:23:20 -0700

    chore: refresh contact form data on user selection

--- bciers/apps/administration/app/components/contacts/ContactsForm.tsx
+++ bciers/apps/administration/app/components/contacts/ContactsForm.tsx
@@ -7,6 +7,7 @@ import SingleStepTaskListForm from "@bciers/components/form/SingleStepTaskListFo
 import { actionHandler } from "@bciers/actions";
 import { ContactFormData } from "./types";
 import getUserData from "./getUserData";
+import { IChangeEvent } from "@rjsf/core";
 
 interface Props {
   schema: any;
@@ -23,37 +24,44 @@ export default function ContactsForm({
 }: Readonly<Props>) {
   const router = useRouter();
   const [error, setError] = useState(undefined);
-  const [formDataState, setFormDataState] = useState(formData);
+  const [formState, setFormState] = useState(formData ?? {});
+  const [key, setKey] = useState(Math.random());
 
   // Populate form data using the selected user data
   const handleSelectUserChange = async (userId: UUID) => {
     try {
       const userData: ContactFormData = await getUserData(userId);
-      setFormDataState(userData);
+      console.log("userData", userData);
+      setFormState(userData);
+      // Trigger a re-render to update the form data
+      setKey(Math.random());
     } catch (err) {
       setError("Failed to fetch user data!" as any);
     }
   };
+
   /*
-  {
-    "street_address": null,
-    "municipality": null,
-    "province": null,
-    "postal_code": null,
-    "first_name": "bc-cas-dev",
-    "last_name": "Industry User",
-    "email": "email@email.com",
-    "phone_number": "+16044015432",
-    "position_title": "Code Monkey"
-  }
+{
+  "street_address": null,
+  "municipality": null,
+  "province": null,
+  "postal_code": null,
+  "first_name": "bc-cas-dev",
+  "last_name": "Industry User",
+  "email": "email@email.com",
+  "phone_number": "+16044015432",
+  "position_title": "Code Monkey"
+}
 */
+
   return (
     <SingleStepTaskListForm
+      key={key}
       error={error}
       disabled={!isCreating}
       schema={schema}
       uiSchema={uiSchema}
-      formData={formDataState}
+      formData={formState}
       onSubmit={async (data: { formData?: any }) => {
         const method = isCreating ? "POST" : "PUT";
         const endpoint = isCreating ? "registration/contacts" : `tbd`;
@@ -75,16 +83,19 @@ export default function ContactsForm({
           return { error: response.error };
         }
         if (isCreating) {
+          // Not sure if we need shallow routing here but if so use something like:
+          // window.history.replaceState({}, "", `/contacts/${response.id}?title=${response.name}`);
+          // https://github.com/vercel/next.js/discussions/48110
           router.replace(
             `/contacts/${response.id}?title=${response.name}`,
-            // @ts-ignore
-            { shallow: true },
+            // // @ts-ignore
+            // { shallow: true },
           );
         }
       }}
-      customChangeHandler={(data) => {
-        if (data.formData?.section1?.selected_user) {
-          handleSelectUserChange(data.formData.section1.selected_user);
+      onChange={(e: IChangeEvent) => {
+        if (e.formData?.section1?.selected_user) {
+          handleSelectUserChange(e.formData.section1.selected_user);
         }
       }}
       onCancel={() => router.replace("/contacts")}
