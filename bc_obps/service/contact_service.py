124904d596595d53ce15a05dced47a7e649d7e3b
SeSo <sobhani.sepehr@gmail.com>
2024-07-19 10:25:21 -0700

    chore: add endpoint and service to create a contact

--- bc_obps/service/contact_service.py
+++ bc_obps/service/contact_service.py
@@ -1,9 +1,13 @@
+from django.db import transaction
 from typing import Optional
 from django.db.models import QuerySet
 from uuid import UUID
 from registration.constants import UNAUTHORIZED_MESSAGE
+from registration.models.business_role import BusinessRole
 from registration.models.contact import Contact
-from registration.schema.v1.contact import ContactFilterSchema
+from registration.models.operator import Operator
+from registration.schema.v1.contact import ContactFilterSchema, ContactIn
+from service.data_access_service.address_service import AddressDataAccessService
 from service.data_access_service.contact_service import ContactDataAccessService
 from service.data_access_service.user_service import UserDataAccessService
 from ninja import Query
@@ -31,4 +35,27 @@ class ContactService:
         contact = user_contacts.filter(id=contact_id).first()
         if user.is_industry_user() and not contact:
             raise Exception(UNAUTHORIZED_MESSAGE)
+        return contact
+
+    @classmethod
+    @transaction.atomic()
+    def create_contact(cls, user_guid: UUID, payload: ContactIn) -> Contact:
+        contact_data: dict = payload.dict(include={*ContactIn.Meta.fields})
+        # `business_role` is a mandatory field in the DB but we don't collect it from the user
+        # so we set it to a default value here and we can change it later if needed
+        contact_data['business_role'] = BusinessRole.objects.get(role_name="Operation Representative")
+        contact = ContactDataAccessService.update_or_create(None, contact_data, user_guid)
+
+        # Create address
+        address_data = payload.dict(
+            include={'street_address', 'municipality', 'province', 'postal_code'}, exclude_none=True
+        )
+        if address_data:
+            address = AddressDataAccessService.create_address(address_data)
+            contact.address = address
+            # not calling `set_create_or_update` because we are updating the contact in the same transaction
+            contact.save(update_fields=['address_id'])
+        operator: Operator = UserDataAccessService.get_user_operator_by_user(user_guid).operator
+        operator.contacts.add(contact)
+        operator.set_create_or_update(user_guid)
         return contact
