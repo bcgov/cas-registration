1038d1ec96b1b7a57692817bc1f7e92a25a002bb
Josh Gamache <joshua@button.is>
2024-07-19 14:24:41 -0600

    chore: add context for current report year

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user/operations/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user/operations/page.tsx
@@ -1,11 +1,16 @@
 // 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
 import { OperationsSearchParams } from "../../../components/operations/types";
 import OperationsPage from "../../../components/routes/operations/Page";
+import CurrentReportingYearContext from "@reporting/src/app/components/context/CurrentReportingYearContext";
 
 export default async function Page({
   searchParams,
 }: {
   searchParams: OperationsSearchParams;
 }) {
-  return <OperationsPage searchParams={searchParams} />;
+  return (
+    <CurrentReportingYearContext.Provider value={2024}>
+      <OperationsPage searchParams={searchParams} />
+    </CurrentReportingYearContext.Provider>
+  );
 }
