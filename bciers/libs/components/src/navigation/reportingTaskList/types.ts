3d1262efdb2c2435172dad2a787282068d09df3a
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-07-22 12:42:25 -0700

    chore: adding a key to a list of react components

--- bciers/libs/components/src/navigation/reportingTaskList/types.ts
+++ bciers/libs/components/src/navigation/reportingTaskList/types.ts
@@ -1,6 +1,7 @@
 export interface TaskListElement {
   type: "Section" | "Subsection" | "Page";
   title: string;
+  key?: string; // Overrides the key used for React when building the list of children elements. Defaults to the title.
   link?: string;
   elements?: TaskListElement[];
   isChecked?: boolean;
