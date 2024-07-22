cb719c05ec637dee85b3f549f7ee7973813ce1a0
Dylan Leard <dylan@button.is>
2024-07-22 13:55:32 -0700

    chore: page cleanup

--- bciers/apps/reporting/src/app/components/activities/gsc.tsx
+++ bciers/apps/reporting/src/app/components/activities/gsc.tsx
@@ -1,12 +1,10 @@
 "use client";
 import FormBase from "@bciers/components/form/FormBase";
-import defaultTheme from "@bciers/components/form/theme/defaultTheme";
 import { RJSFSchema } from "@rjsf/utils";
 import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
-import ArrayFieldTemplate from "@bciers/components/form/fields/ArrayFieldTemplate";
+import NestedArrayFieldTemplate from "@bciers/components/form/fields/NestedArrayFieldTemplate";
 import SourceTypeBoxTemplate from "@bciers/components/form/fields/SourceTypeBoxTemplate";
 import CheckboxWidgetLeft from "@bciers/components/form/widgets/CheckboxWidgetLeft";
-import Form from "@rjsf/mui"
 import { customizeValidator } from "@rjsf/validator-ajv8";
 import InlineFieldTemplate from "@bciers/components/form/fields/InlineFieldTemplate";
 
@@ -39,7 +37,12 @@ const uiSchema = {
     gcsWithProductionOfUsefulEnergy: {
       "ui:FieldTemplate": SourceTypeBoxTemplate,
       units: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
         "ui:FieldTemplate": FieldTemplate,
+        "ui:title": "Unit Data",
+        "ui:options": {
+          arrayAddLabel: 'Add Unit'
+        },
         items:{
           "ui:order": [
             "gscUnitName",
@@ -57,8 +60,12 @@ const uiSchema = {
             "ui:FieldTemplate": InlineFieldTemplate
           },
           fuels: {
-            "ui:title": "Fuel Information",
+            "ui:title": "Fuel Data",
+            "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
             "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              arrayAddLabel: 'Add Fuel'
+            },
             items:{
               "ui:order": [
                 "fuelName",
@@ -76,7 +83,11 @@ const uiSchema = {
                 "ui:FieldTemplate": InlineFieldTemplate
               },
               emissions: {
+                "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
                 "ui:FieldTemplate": FieldTemplate,
+                "ui:options": {
+                  arrayAddLabel: 'Add Emission'
+                },
               }
             },
           }
@@ -85,49 +96,66 @@ const uiSchema = {
     },
     gcsWithoutProductionOfUsefulEnergy: {
       "ui:FieldTemplate": SourceTypeBoxTemplate,
+      units: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+        "ui:FieldTemplate": FieldTemplate,
+        "ui:title": "Unit Data",
+        "ui:options": {
+          arrayAddLabel: 'Add Unit'
+        },
+        items:{
+          "ui:order": [
+            "gscUnitName",
+            "gscUnitType",
+            "description",
+            "fuels"
+          ],
+          gscUnitName: {
+            "ui:FieldTemplate": InlineFieldTemplate
+          },
+          gscUnitType: {
+            "ui:FieldTemplate": InlineFieldTemplate
+          },
+          description: {
+            "ui:FieldTemplate": InlineFieldTemplate
+          },
+          fuels: {
+            "ui:title": "Fuel Data",
+            "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+            "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              arrayAddLabel: 'Add Fuel'
+            },
+            items:{
+              "ui:order": [
+                "fuelName",
+                "fuelUnit",
+                "annualFuelAmount",
+                "emissions"
+              ],
+              fuelName: {
+                "ui:FieldTemplate": InlineFieldTemplate
+              },
+              fuelType: {
+                "ui:FieldTemplate": InlineFieldTemplate
+              },
+              annualFuelAmount: {
+                "ui:FieldTemplate": InlineFieldTemplate
+              },
+              emissions: {
+                "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+                "ui:FieldTemplate": FieldTemplate,
+                "ui:options": {
+                  arrayAddLabel: 'Add Emission'
+                },
+              }
+            },
+          }
+        },
+      },
     }
   },
 };
-
-
-const testSchema = {
-  "title": "A registration form",
-  "description": "A simple form example.",
-  "type": "object",
-  "required": [
-    "firstName",
-    "lastName"
-  ],
-  "properties": {
-    "firstName": {
-      "type": "string",
-      "title": "First name",
-      "default": "Chuck"
-    },
-    "lastName": {
-      "type": "string",
-      "title": "Last name"
-    },
-    "age": {
-      "type": "integer",
-      "title": "Age"
-    },
-    "bio": {
-      "type": "string",
-      "title": "Bio"
-    },
-    "password": {
-      "type": "string",
-      "title": "Password",
-      "minLength": 3
-    },
-    "telephone": {
-      "type": "string",
-      "title": "Telephone",
-      "minLength": 10
-    }
-  }
-}
 
 // 🧩 Main component
 export default function Gsc({schema}: Readonly<Props>) {
@@ -142,18 +170,9 @@ export default function Gsc({schema}: Readonly<Props>) {
 
   const validator = customizeValidator({ customFormats });
 
-  console.log("TEST SCHEMA: ",testSchema)
   // Render the DataGrid component
   return (
     <>
-    {/* <Form
-      schema={schema}
-      uiSchema={uiSchema}
-      validator={validator}
-      onChange={(c: any) => console.log('CHANGE: ', c)}
-      onError={(e: any) => console.log('ERROR: ', e)}
-      onSubmit={() => console.log('submit')}
-    /> */}
       <FormBase
         schema={schema}
         uiSchema={uiSchema}
