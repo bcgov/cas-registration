2a44db641945bb57780306fd60a361c97818320c
marcellmueller <marcel@button.is>
2024-07-23 12:28:02 -0700

    chore: remove readonly widget changes

--- bciers/libs/components/src/form/widgets/readOnly/ReadOnlyWidget.tsx
+++ bciers/libs/components/src/form/widgets/readOnly/ReadOnlyWidget.tsx
@@ -1,11 +1,9 @@
 import { WidgetProps } from "@rjsf/utils/lib/types";
 
 const ReadOnlyWidget: React.FC<WidgetProps> = ({ id, value }) => {
-  // If the value is an array, join it into a comma-separated string
-  const formattedValue = Array.isArray(value) ? value.join(", ") : value;
   return (
     <div id={id} className="read-only-widget">
-      {formattedValue}
+      {value}
     </div>
   );
 };
