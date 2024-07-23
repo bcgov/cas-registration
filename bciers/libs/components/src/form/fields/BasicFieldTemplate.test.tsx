0998b107b020b002287b464d9b40b9d2031bab94
marcellmueller <marcel@button.is>
2024-07-23 12:15:32 -0700

    chore: add basic field template test

---
+++ bciers/libs/components/src/form/fields/BasicFieldTemplate.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { RJSFSchema } from "@rjsf/utils";
+import FormBase from "@bciers/components/form/FormBase";
+import BasicFieldTemplate from "@bciers/components/form/fields/BasicFieldTemplate";
+
+const mockSchema: RJSFSchema = {
+  type: "object",
+  properties: {
+    testField: {
+      type: "string",
+      title: "Test Field",
+    },
+  },
+};
+
+const mockUiSchema = {
+  testField: {
+    "ui:FieldTemplate": BasicFieldTemplate,
+    "ui:widget": "CheckboxWidget",
+  },
+};
+
+describe("RJSF BasicFieldTemplate", () => {
+  it("should render a field", async () => {
+    render(<FormBase schema={mockSchema} uiSchema={mockUiSchema} />);
+    expect(screen.getByLabelText("Test Field")).toBeVisible();
+  });
+});
