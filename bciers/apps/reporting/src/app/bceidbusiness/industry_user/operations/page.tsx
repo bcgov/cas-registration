328af4b93aed3403ae67c813ce3b70b063e90ecd
Josh Gamache <joshua@button.is>
2024-07-24 15:26:03 -0600

    chore: drop context for provision of reporting year

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user/operations/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user/operations/page.tsx
@@ -1,7 +1,6 @@
 // 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
 import { OperationsSearchParams } from "../../../components/operations/types";
 import OperationsPage from "../../../components/routes/operations/Page";
-import CurrentReportingYearContext from "@reporting/src/app/components/context/CurrentReportingYearContext";
 
 export default async function Page({
   searchParams,
@@ -9,8 +8,6 @@ export default async function Page({
   searchParams: OperationsSearchParams;
 }) {
   return (
-    <CurrentReportingYearContext.Provider value={2024}>
-      <OperationsPage searchParams={searchParams} />
-    </CurrentReportingYearContext.Provider>
+    <OperationsPage searchParams={searchParams} />
   );
 }
