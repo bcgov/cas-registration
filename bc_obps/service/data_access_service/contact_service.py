b3b00e3c9e05b500ae27a1cb6072228562b4e11d
SeSo <sobhani.sepehr@gmail.com>
2024-07-24 13:22:32 -0700

    chore: refactor `ContactDataAccessService` to be able to reuse it

--- bc_obps/service/data_access_service/contact_service.py
+++ bc_obps/service/data_access_service/contact_service.py
@@ -13,17 +13,19 @@ class ContactDataAccessService:
 
     @classmethod
     def update_or_create(
-        cls, point_of_contact_id: Optional[int], updated_data: Dict[str, Optional[str]], user_guid: UUID
+        cls, existing_contact_id: Optional[int], updated_data: Dict[str, Optional[str]], user_guid: UUID
     ) -> Contact:
         contact, _ = Contact.objects.update_or_create(
-            id=point_of_contact_id,
+            id=existing_contact_id,
             defaults={
                 "first_name": updated_data["first_name"],
                 "last_name": updated_data["last_name"],
                 "position_title": updated_data["position_title"],
                 "email": updated_data["email"],
                 "phone_number": updated_data["phone_number"],
-                "business_role": BusinessRole.objects.get(role_name="Operation Registration Lead"),
+                "business_role": updated_data.get(
+                    "business_role", BusinessRole.objects.get(role_name="Operation Registration Lead")
+                ),
             },
         )
         contact.set_create_or_update(user_guid)
