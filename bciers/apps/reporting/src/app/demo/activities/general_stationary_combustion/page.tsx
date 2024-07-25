312ef885b93e8c1a04a3da7ffdbc6785b7a1a3ed
Dylan Leard <dylan@button.is>
2024-07-25 16:21:07 -0700

    feat: add ActivityForm component & gsc page

--- bciers/apps/reporting/src/app/demo/activities/general_stationary_combustion/page.tsx
+++ bciers/apps/reporting/src/app/demo/activities/general_stationary_combustion/page.tsx
@@ -1,10 +1,14 @@
-import ActivityPage from "../../../components/activities/Activity";
+import ActivityForm from "../../../components/activities/ActivityForm";
 import { Suspense } from "react";
+import { actionHandler } from "@bciers/actions";
 
 export default async function Page() {
+  const reportDate = '2024-04-01' // This should be passed in once we have a path to this page from starting a report
+  const activityData = await actionHandler(`reporting/get_activity_data?activity_name=General stationary combustion&report_date=${reportDate}`, "GET", "");
+  const activityDataObject = JSON.parse(activityData);
   return (
   <Suspense fallback="Loading Schema">
-    <ActivityPage />
+    <ActivityForm activityData={activityDataObject} reportDate={reportDate}/>
   </Suspense>
   )
 }
