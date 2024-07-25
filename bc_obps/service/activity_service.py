daea155b14aeaa72cb6ff02c4c61682e5c08a519
Dylan Leard <dylan@button.is>
2024-07-25 16:20:06 -0700

    chore: add endpoint to fetch initial activity data

---
+++ bc_obps/service/activity_service.py
@@ -0,0 +1,31 @@
+import json
+from reporting.models import Configuration, ConfigurationElement
+from registration.models import ReportingActivity
+from typing import List, Any
+
+class ActivityService:
+    @classmethod
+    def get_activity_data(request, activity_name: str, report_date: str) -> str:
+        print('start')
+        if report_date is None:
+            raise Exception('Cannot fetch activity data without a valid report date')
+        if activity_name is None:
+            raise Exception('Cannot fetch activity data without activity name')
+        # Get
+        source_type_map: dict[int, str] = {}
+        print('next')
+        activity_id = ReportingActivity.objects.get(name=activity_name).id
+        print('here')
+        config = Configuration.objects.get(valid_from__lte=report_date, valid_to__gte=report_date)
+        print('here 2')
+        source_type_data = (
+            ConfigurationElement.objects.select_related('source_type')
+            .filter(reporting_activity_id=activity_id, valid_from__lte=config, valid_to__gte=config)
+            .order_by('source_type__id')
+            .distinct('source_type__id')
+            .only('source_type__id', 'source_type__json_key')
+        )
+        print('here 3')
+        for s in source_type_data:
+            source_type_map[s.source_type.id] = s.source_type.json_key
+        return json.dumps({"activityId": activity_id, "sourceTypeMap": source_type_map})
