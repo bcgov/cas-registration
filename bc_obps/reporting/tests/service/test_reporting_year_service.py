b02084a23eed431aa9dbf25bbdc9537c67e6605c
Josh Gamache <joshua@button.is>
2024-07-26 15:04:47 -0600

    chore: add tests for reporting year service

---
+++ bc_obps/reporting/tests/service/test_reporting_year_service.py
@@ -0,0 +1,28 @@
+import pytest
+from datetime import datetime, timezone
+from unittest.mock import patch
+from service.reporting_year_service import ReportingYearService
+from reporting.models import ReportingYear
+
+pytestmark = pytest.mark.django_db
+
+
+@patch('service.reporting_year_service.datetime')
+class TestReportingYearService:
+
+    def setup_method(self):
+        self.reporting_year = ReportingYear.objects.create(
+            reporting_year=2000,
+            reporting_window_start='2001-01-01T00:00:00.000Z',
+            reporting_window_end='2001-12-31T23:59:59.999Z',
+            description='Test reporting year',
+        )
+
+    def test_get_current_reporting_year(self, mock_datetime):
+        expected_reporting_year = 2000
+        fake_now = datetime(2001, 1, 1, tzinfo=timezone.utc)
+
+        mock_datetime.now.return_value = fake_now
+        current_reporting_year = ReportingYearService.get_current_reporting_year()
+
+        assert current_reporting_year.reporting_year == expected_reporting_year
