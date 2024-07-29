2d999bef0ebe2c6d21a0d8d3fb8b143f877be8d6
Dylan Leard <dylan@button.is>
2024-07-29 16:30:23 -0700

    chore: do not overwrite formdata when booleans change
    chore: linting
    
    chore: linting

--- bciers/apps/reporting/src/app/demo/activities/general_stationary_combustion/page.tsx
+++ bciers/apps/reporting/src/app/demo/activities/general_stationary_combustion/page.tsx
@@ -3,13 +3,20 @@ import { Suspense } from "react";
 import { actionHandler } from "@bciers/actions";
 
 export default async function Page() {
-  const reportDate = '2024-04-01' // This should be passed in once we have a path to this page from starting a report
-  const activityData = await actionHandler(`reporting/get-initial-activity-data?activity_name=General stationary combustion&report_date=${reportDate}`, "GET", "");
+  const reportDate = "2024-04-01"; // This should be passed in once we have a path to this page from starting a report
+  const activityData = await actionHandler(
+    `reporting/get-initial-activity-data?activity_name=General stationary combustion&report_date=${reportDate}`,
+    "GET",
+    "",
+  );
   const activityDataObject = JSON.parse(activityData);
 
   return (
     <Suspense fallback="Loading Schema">
-      <GeneralStationaryCombustion activityData={activityDataObject} reportDate={reportDate}/>
+      <GeneralStationaryCombustion
+        activityData={activityDataObject}
+        reportDate={reportDate}
+      />
     </Suspense>
-  )
+  );
 }
