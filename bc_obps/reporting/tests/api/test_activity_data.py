9d3074ad1b19946c9046b05b8740fa89271a0e1a
Dylan Leard <dylan@button.is>
2024-07-30 16:13:14 -0700

    test: activity data endpoint test
    chore: update test
    
    test: remove debug

---
+++ bc_obps/reporting/tests/api/test_activity_data.py
@@ -0,0 +1,26 @@
+from django.test import Client
+import json
+import pytest
+
+pytestmark = pytest.mark.django_db
+client = Client()
+pytest.endpoint = "/api/reporting/get-initial-activity-data"
+
+
+class TestBuildFormSchema:
+    def test_invalid_without_report_date(self):
+        response = client.get(f'{pytest.endpoint}?activity_name=General stationary combustion')
+        assert response.status_code == 422
+
+    def test_invalid_without_activity(self):
+        response = client.get(f'{pytest.endpoint}?report_date=2024-05-01')
+        assert response.status_code == 422
+
+    def test_returns_activity_schema(self):
+        response = client.get(f'{pytest.endpoint}?activity_name=General stationary combustion&report_date=2024-05-01')
+        assert response.status_code == 200
+        response_object = json.loads(response.json())
+        print(response_object)
+        assert response_object['activityId'] == 1
+        # There are 2 source types in the map
+        assert len(response_object['sourceTypeMap'].keys()) == 2
