657a1964cec92d4e6400428d1c75e4b24e3162ee
Dylan Leard <dylan@button.is>
2024-08-09 15:46:52 -0700

    chore: update activity name with migration

--- bc_obps/reporting/migrations/0008_general_stationary_combustion_data.py
+++ bc_obps/reporting/migrations/0008_general_stationary_combustion_data.py
@@ -1709,7 +1709,7 @@ def reverse_init_activity_source_type_schema_data(apps, schema_monitor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reporting', '0007_prod_data'),
+        ('reporting', '0007_prod_data')
     ]
 
     operations = [
