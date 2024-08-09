a0e5a98bb8a3b76d1766f1b2afa9854ec3e0ccc4
marcellmueller <marcel@button.is>
2024-08-09 11:57:22 -0700

    chore: fetch sfo facility form data

--- bciers/apps/registration/app/components/operations/registration/FacilityInformationPage.tsx
+++ bciers/apps/registration/app/components/operations/registration/FacilityInformationPage.tsx
@@ -3,6 +3,9 @@ import FacilityInformationForm from "apps/registration/app/components/operations
 import fetchFacilitiesPageData from "@/administration/app/components/facilities/fetchFacilitiesPageData";
 import { FacilitiesSearchParams } from "apps/administration/app/components/facilities/types";
 import { FacilityTypes, OperationTypes } from "@bciers/utils/enums";
+import getFacility from "apps/administration/app/components/facilities/getFacility";
+import { createNestedFormData } from "@bciers/components/form/formDataUtils";
+import { facilitiesSfoSchema } from "apps/administration/app/data/jsonSchema/facilitiesSfo";
 
 const FacilityInformationPage = async ({
   operation,
@@ -20,20 +23,35 @@ const FacilityInformationPage = async ({
   steps: string[];
 }) => {
   const isOperationSfo = operationType === OperationTypes.SFO;
-  const isOperationLfo = operationType === OperationTypes.LFO;
+  const initialGridData = await fetchFacilitiesPageData(
+    operation,
+    searchParams,
+  );
 
-  // Fetch grid data for LFO operations
-  const initialGridData = isOperationLfo
-    ? await fetchFacilitiesPageData(operation, searchParams)
-    : undefined;
+  // Is there a cleaner way to get the facilityId for a single facility operation? Using this for now
+  const facilityId =
+    isOperationSfo && initialGridData.rows.length === 1
+      ? initialGridData.rows[0].id
+      : null;
 
-  // Single facility operations get read-only name and type fields pre-populated
-  const formData = isOperationSfo
-    ? { section1: { name: operationName, type: FacilityTypes.SFO } }
-    : {};
+  // Get facility form data for SFO operations if facilityId is available
+  const sfoFacilityData =
+    isOperationSfo && facilityId ? await getFacility(facilityId) : null;
+
+  const sfoFormData = sfoFacilityData
+    ? // Create nested form data for SFO operations
+      createNestedFormData(sfoFacilityData, facilitiesSfoSchema)
+    : {
+        // Pre-populate facility name and type for SFO Operations
+        section1: { name: operationName, type: FacilityTypes.SFO },
+      };
+
+  // LFO operations will always have empty form data since they can create multiple facilities
+  const formData = isOperationSfo ? sfoFormData : {};
 
   return (
     <FacilityInformationForm
+      isCreating={!isOperationSfo && !facilityId}
       initialGridData={initialGridData}
       formData={formData}
       operation={operation}
