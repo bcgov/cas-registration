0a78c06600a7e5bec0597a55ba015db32922755d
marcellmueller <marcel@button.is>
2024-08-12 14:36:22 -0700

    chore: add new entrant form schema

---
+++ bciers/apps/registration/app/components/operations/registration/form/titles.tsx
@@ -0,0 +1,24 @@
+const GenerateNewEntrantFormMessage = (
+  date: string,
+  applicationUrl: string,
+) => {
+  return (
+    <div className="max-w-[900px]">
+      <p>
+        For a definition of the Date of First Shipment, please refer to the{" "}
+        <a href="/tbd">GGERR</a>.
+      </p>
+      <p>
+        Please download and complete the following application form template to
+        receive designation as a New Entrant in the B.C. OBPS. This application
+        form is for operations with a date of First Shipment <b>{date}</b>.
+      </p>
+      <p>
+        Download <a href={applicationUrl}>application form template</a>
+      </p>
+      <p>Please upload the completed form below.</p>
+    </div>
+  );
+};
+
+export { GenerateNewEntrantFormMessage };
