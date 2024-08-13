f4c1ea9258cdf38e9e8c3f3d73dc25d5a15b9d4b
Ballard <ballard@button.is>
2024-08-13 12:09:20 -0400

    feat: adding activity page

---
+++ bciers/apps/reporting/src/app/components/activities/generalStationaryCombustionLineTracing.tsx
@@ -0,0 +1,31 @@
+"use client";
+import ActivityForm from "./ActivityForm";
+import uiSchema from "./uiSchemas/gscUiSchema";
+
+interface Props {
+  activityData: {
+    activityId: number;
+    sourceTypeMap: { [key: number]: string };
+  };
+  reportDate: string;
+}
+
+// 🧩 Main component
+export default function GeneralStationaryCombustionLineTracing({
+  activityData,
+  reportDate,
+}: Readonly<Props>) {
+  // Shape of an empty sourceType to create a set of fields on select
+  const defaultEmptySourceTypeState = {
+    units: [{ fuels: [{ emissions: [{}] }] }],
+  };
+
+  return (
+    <ActivityForm
+      activityData={activityData}
+      reportDate={reportDate}
+      uiSchema={uiSchema}
+      defaultEmptySourceTypeState={defaultEmptySourceTypeState}
+    />
+  );
+}
