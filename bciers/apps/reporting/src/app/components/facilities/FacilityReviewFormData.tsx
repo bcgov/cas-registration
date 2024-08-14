a300b9b052cfc62366bc0a6cd150cd48421272ee
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-08-13 17:11:10 -0700

    Fixed errors

--- bciers/apps/reporting/src/app/components/facilities/FacilityReviewFormData.tsx
+++ bciers/apps/reporting/src/app/components/facilities/FacilityReviewFormData.tsx
@@ -1,11 +1,13 @@
 import FacilityReview from "./FacilityReview";
 
+type FacilityReviewFormDataProps = Readonly<{
+  version_id: number;
+  facility_id: number;
+}>;
+
 export default function FacilityReviewFormData({
   version_id,
   facility_id,
-}: {
-  version_id: number;
-  facility_id: number;
-}) {
+}: FacilityReviewFormDataProps) {
   return <FacilityReview version_id={version_id} facility_id={facility_id} />;
 }
