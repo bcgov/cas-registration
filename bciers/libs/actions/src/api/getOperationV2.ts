0e7f0b0238d99fae9063e514b6a49e9dec9b3a88
marcellmueller <marcel@button.is>
2024-08-16 13:34:49 -0700

    chore: fix operations get api error

--- bciers/libs/actions/src/api/getOperationV2.ts
+++ bciers/libs/actions/src/api/getOperationV2.ts
@@ -2,7 +2,8 @@ import { actionHandler } from "@bciers/actions";
 
 async function getOperationV2(id: string) {
   try {
-    return await actionHandler(`registration/v2/operations/${id}`, "GET", "");
+    // /v2/operations/{id} was clashing with /v2/operations/current, need to fix/rename
+    return await actionHandler(`registration/v2/operation/${id}`, "GET", "");
   } catch (error) {
     throw error;
   }
