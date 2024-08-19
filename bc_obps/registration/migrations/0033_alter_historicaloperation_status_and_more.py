edbec66aae7a1814df07f5f351b3bb7fed174865
Dylan Leard <dylan@button.is>
2024-08-19 14:40:08 -0700

    chore: activity updates from BA / migration reworks

--- bc_obps/registration/migrations/0033_alter_historicaloperation_status_and_more.py
+++ bc_obps/registration/migrations/0033_alter_historicaloperation_status_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0033_historicalregistrationpurpose_registrationpurpose'),
+        ('registration', '0032_historicalregistrationpurpose_registrationpurpose'),
     ]
 
     operations = [
