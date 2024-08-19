9b504fb7c51c932e8a2889ec16a73746215d57c8
marcellmueller <marcel@button.is>
2024-08-19 13:56:16 -0700

    chore: resolve sfo facility id in operations list

--- bc_obps/registration/enums/enums.py
+++ bc_obps/registration/enums/enums.py
@@ -23,3 +23,8 @@ class AccessRequestTypes(Enum):
     ADMIN = "Admin"
     OPERATOR_WITH_ADMIN = "Operator With Admin"
     NEW_OPERATOR_AND_ADMIN = "New Operator And Admin"
+
+
+class OperationTypes(Enum):
+    LFO = "Linear Facility Operation"
+    SFO = "Single Facility Operation"
