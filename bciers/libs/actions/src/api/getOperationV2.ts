d685683c91d22aaf6f87f475381940373364c91f
marcellmueller <marcel@button.is>
2024-08-20 10:29:58 -0700

    chore: annotate dynamic api route parameter

--- bciers/libs/actions/src/api/getOperationV2.ts
+++ bciers/libs/actions/src/api/getOperationV2.ts
@@ -2,8 +2,7 @@ import { actionHandler } from "@bciers/actions";
 
 async function getOperationV2(id: string) {
   try {
-    // /v2/operations/{id} was clashing with /v2/operations/current, need to fix/rename
-    return await actionHandler(`registration/v2/operation/${id}`, "GET", "");
+    return await actionHandler(`registration/v2/operations/${id}`, "GET", "");
   } catch (error) {
     throw error;
   }
