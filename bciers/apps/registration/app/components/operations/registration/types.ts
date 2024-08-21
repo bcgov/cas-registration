e9644a102d71907dd2874693176caae485a5dcee
SeSo <sobhani.sepehr@gmail.com>
2024-08-21 11:23:33 -0700

    chore: frontend tweaks - outcome of tests

--- bciers/apps/registration/app/components/operations/registration/types.ts
+++ bciers/apps/registration/app/components/operations/registration/types.ts
@@ -6,14 +6,14 @@ export interface FacilityInformationFormData {}
 export interface NewEntrantOperationFormData {}
 
 export interface OptedInOperationFormData {
-  meets_section_3_emissions_requirements: boolean;
-  meets_electricity_import_criteria: boolean;
-  meets_entire_operation_requirements: boolean;
-  meets_section_6_emissions_requirements: boolean;
-  meets_naics_code_11_22_562_classification_requirements: boolean;
-  meets_producing_gger_schedule_a1_regulated_product: boolean;
-  meets_reporting_and_regulated_obligations: boolean;
-  meets_notification_to_director_on_criteria_change: boolean;
+  meets_section_3_emissions_requirements: boolean | null;
+  meets_electricity_import_operation_criteria: boolean | null;
+  meets_entire_operation_requirements: boolean | null;
+  meets_section_6_emissions_requirements: boolean | null;
+  meets_naics_code_11_22_562_classification_requirements: boolean | null;
+  meets_producing_gger_schedule_a1_regulated_product: boolean | null;
+  meets_reporting_and_regulated_obligations: boolean | null;
+  meets_notification_to_director_on_criteria_change: boolean | null;
 }
 
 export interface OperationInformationFormData {}
