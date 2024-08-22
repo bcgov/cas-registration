b68e5f6666b059d37b2a57b3f27e00ce1764f763
SeSo <sobhani.sepehr@gmail.com>
2024-08-22 10:01:55 -0700

    chore: resolve pre-commit and refactor issues

--- bc_obps/registration/api/v2/__init__.py
+++ bc_obps/registration/api/v2/__init__.py
@@ -1,5 +1,5 @@
 # ruff: noqa: F401
-from ._operations._operation_id._registration import operation
+from ._operations._operation_id._registration import operation, opted_in_operation
 from . import operations
 from ._user_operators._current import operator
 from ._operations import current
