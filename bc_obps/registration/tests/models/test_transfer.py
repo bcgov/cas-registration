ffce6dd05b70d0c2871a71e461378e9236482a78
Andrea Williams <andrea.williams@gov.bc.ca>
2024-08-21 17:50:11 -0700

    Chore: implemented str() fn for Transfer; added test_transfer.py

---
+++ bc_obps/registration/tests/models/test_transfer.py
@@ -0,0 +1,42 @@
+from common.tests.utils.helpers import BaseTestCase
+from registration.models import Transfer
+from registration.tests.constants import (
+    TIMESTAMP_COMMON_FIELDS,
+    ADDRESS_FIXTURE,
+    BC_OBPS_REGULATED_OPERATION_FIXTURE,
+    OPERATOR_FIXTURE,
+    USER_FIXTURE,
+    OPERATION_FIXTURE,
+    CONTACT_FIXTURE,
+    FACILITY_FIXTURE,
+    TRANSFER_FIXTURE,
+)
+
+
+class TemporaryShutdownModelTest(BaseTestCase):
+    fixtures = [
+        USER_FIXTURE,
+        ADDRESS_FIXTURE,
+        CONTACT_FIXTURE,
+        OPERATOR_FIXTURE,
+        BC_OBPS_REGULATED_OPERATION_FIXTURE,
+        OPERATION_FIXTURE,
+        FACILITY_FIXTURE,
+        TRANSFER_FIXTURE,
+    ]
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_object = Transfer.objects.first()
+        cls.field_data = [
+            *TIMESTAMP_COMMON_FIELDS,
+            ("id", "id", None, None),
+            ("effective_date", "effective date", None, None),
+            ("status", "status", 100, None),
+            ("description", "description", None, None),
+            ("operation", "operation", None, None),
+            ("facilities", "facilities", None, None),
+            ("other_operator", "other operator", None, None),
+            ("other_operator_contact", "other operator contact", None, None),
+            ("future_designated_operator", "future designated operator", None, None),
+        ]
