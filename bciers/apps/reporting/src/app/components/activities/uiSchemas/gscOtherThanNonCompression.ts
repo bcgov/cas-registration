6f28c1b469ed49ab9206c8337faf1a784271521f
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-08-22 12:22:25 -0700

    feat:302-Activity Page-General Stationary Combustion (other than non-compression and non-processing)

---
+++ bciers/apps/reporting/src/app/components/activities/uiSchemas/gscOtherThanNonCompression.ts
@@ -0,0 +1,229 @@
+import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
+import NestedArrayFieldTemplate from "@bciers/components/form/fields/NestedArrayFieldTemplate";
+import SourceTypeBoxTemplate from "@bciers/components/form/fields/SourceTypeBoxTemplate";
+import CheckboxWidgetLeft from "@bciers/components/form/widgets/CheckboxWidgetLeft";
+import InlineFieldTemplate from "@bciers/components/form/fields/InlineFieldTemplate";
+
+const uiSchema = {
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:classNames": "form-heading-label",
+  gscFuelOrWasteLinearFacilitiesUsefulEnergy: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:widget": CheckboxWidgetLeft,
+    "ui:options": {
+      label: false,
+    },
+  },
+  gscFuelOrWasteLinearFacilitiesWithoutUsefulEnergy: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:widget": CheckboxWidgetLeft,
+    "ui:options": {
+      label: false,
+    },
+  },
+  fieldProcessVentGasLinearFacilities: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:widget": CheckboxWidgetLeft,
+    "ui:options": {
+      label: false,
+    },
+  },
+  sourceTypes: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:options": {
+      label: false,
+    },
+    gscFuelOrWasteLinearFacilitiesUsefulEnergy: {
+      "ui:FieldTemplate": SourceTypeBoxTemplate,
+      units: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+        "ui:FieldTemplate": FieldTemplate,
+        "ui:title": "Unit Data",
+        "ui:options": {
+          arrayAddLabel: "Add Unit",
+          label: false,
+          title: "Unit",
+          padding: "p-2",
+        },
+        items: {
+          "ui:order": ["gscUnitName", "gscUnitType", "description", "fuels"],
+          gscUnitName: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          gscUnitType: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          description: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          fuels: {
+            "ui:title": "Fuel Data",
+            "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+            "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              arrayAddLabel: "Add Fuel",
+              label: false,
+              title: "Fuel",
+            },
+            items: {
+              "ui:order": [
+                "fuelName",
+                "fuelUnit",
+                "annualFuelAmount",
+                "emissions",
+              ],
+              fuelName: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              fuelType: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              annualFuelAmount: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              emissions: {
+                "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+                "ui:FieldTemplate": FieldTemplate,
+                "ui:options": {
+                  arrayAddLabel: "Add Emission",
+                  title: "Emission",
+                  label: false,
+                  verticalBorder: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    gscFuelOrWasteLinearFacilitiesWithoutUsefulEnergy: {
+      "ui:FieldTemplate": SourceTypeBoxTemplate,
+      units: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+        "ui:FieldTemplate": FieldTemplate,
+        "ui:title": "Unit Data",
+        "ui:options": {
+          arrayAddLabel: "Add Unit",
+          label: false,
+          title: "Unit",
+          padding: "p-2",
+        },
+        items: {
+          "ui:order": ["gscUnitName", "gscUnitType", "description", "fuels"],
+          gscUnitName: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          gscUnitType: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          description: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          fuels: {
+            "ui:title": "Fuel Data",
+            "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+            "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              arrayAddLabel: "Add Fuel",
+              label: false,
+              title: "Fuel",
+            },
+            items: {
+              "ui:order": [
+                "fuelName",
+                "fuelUnit",
+                "annualFuelAmount",
+                "emissions",
+              ],
+              fuelName: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              fuelType: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              annualFuelAmount: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              emissions: {
+                "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+                "ui:FieldTemplate": FieldTemplate,
+                "ui:options": {
+                  arrayAddLabel: "Add Emission",
+                  title: "Emission",
+                  label: false,
+                  verticalBorder: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    fieldProcessVentGasLinearFacilities: {
+      "ui:FieldTemplate": SourceTypeBoxTemplate,
+      units: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+        "ui:FieldTemplate": FieldTemplate,
+        "ui:title": "Unit Data",
+        "ui:options": {
+          arrayAddLabel: "Add Unit",
+          label: false,
+          title: "Unit",
+          padding: "p-2",
+        },
+        items: {
+          "ui:order": ["gscUnitName", "gscUnitType", "description", "fuels"],
+          gscUnitName: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          gscUnitType: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          description: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          fuels: {
+            "ui:title": "Fuel Data",
+            "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+            "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              arrayAddLabel: "Add Fuel",
+              label: false,
+              title: "Fuel",
+            },
+            items: {
+              "ui:order": [
+                "fuelName",
+                "fuelUnit",
+                "annualFuelAmount",
+                "emissions",
+              ],
+              fuelName: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              fuelType: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              annualFuelAmount: {
+                "ui:FieldTemplate": InlineFieldTemplate,
+              },
+              emissions: {
+                "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+                "ui:FieldTemplate": FieldTemplate,
+                "ui:options": {
+                  arrayAddLabel: "Add Emission",
+                  title: "Emission",
+                  label: false,
+                  verticalBorder: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default uiSchema;
