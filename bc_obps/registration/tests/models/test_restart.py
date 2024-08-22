a2678badc3fc42564efed4e928aa951c3497fb3d
Andrea Williams <andrea.williams@gov.bc.ca>
2024-08-21 17:32:40 -0700

    Chore: implemented str() fn for Restart; finished test_restart.py

--- bc_obps/registration/tests/models/test_restart.py
+++ bc_obps/registration/tests/models/test_restart.py
@@ -2,27 +2,37 @@ from common.tests.utils.helpers import BaseTestCase
 from registration.models import Restart
 from registration.tests.constants import (
     TIMESTAMP_COMMON_FIELDS,
+    ADDRESS_FIXTURE,
+    BC_OBPS_REGULATED_OPERATION_FIXTURE,
     OPERATOR_FIXTURE,
     USER_FIXTURE,
     OPERATION_FIXTURE,
     CONTACT_FIXTURE,
+    FACILITY_FIXTURE,
     RESTART_FIXTURE,
 )
 
 
 class RestartModelTest(BaseTestCase):
-    fixtures = [USER_FIXTURE, OPERATOR_FIXTURE, OPERATION_FIXTURE, CONTACT_FIXTURE, RESTART_FIXTURE]
+    fixtures = [
+        USER_FIXTURE,
+        ADDRESS_FIXTURE,
+        CONTACT_FIXTURE,
+        OPERATOR_FIXTURE,
+        BC_OBPS_REGULATED_OPERATION_FIXTURE,
+        OPERATION_FIXTURE,
+        FACILITY_FIXTURE,
+        RESTART_FIXTURE,
+    ]
 
     @classmethod
     def setUpTestData(cls):
-        # operator = Operator.objects.get(legal_name="Existing Operator 2 Legal Name")
-        # operation = Operation.objects.get(operator=operator)
-        # cls.test_object = Restart.objects.create(operation=operation)
         cls.test_object = Restart.objects.first()
         cls.field_data = [
             *TIMESTAMP_COMMON_FIELDS,
+            ("id", "id", None, None),
             ("effective_date", "effective date", None, None),
             ("status", "status", 100, None),
-            ("operation", "operation", None, 1),
-            ("facilities", "facility", None, None),
+            ("operation", "operation", None, None),
+            ("facilities", "facilities", None, None),
         ]
