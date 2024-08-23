9bf6bcb4a54a4fd15231a78b4543f722a57d5f74
marcellmueller <marcel@button.is>
2024-08-23 13:33:20 -0700

    chore: update playwright config base urls
    chore: uncomment firefox and webkit in playwright base config
    
    chore: add e2e nx library

--- bciers/apps/registration-e2e/playwright.config.ts
+++ bciers/apps/registration-e2e/playwright.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "@playwright/test";
 import playwrightBaseConfig from "../../playwright.config.base";
 
+// For CI, you may want to set BASE_URL to the deployed application.
+const baseURL = process.env.BASE_URL || "http://127.0.0.1:3000/registration";
+
 export default defineConfig({
   ...playwrightBaseConfig,
+  use: {
+    baseURL,
+  },
 });
