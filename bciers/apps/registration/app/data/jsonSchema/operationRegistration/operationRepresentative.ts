9d2092194ef79c8bd445dac5dbd2473ae55dfe75
SeSo <sobhani.sepehr@gmail.com>
2024-08-27 15:16:03 -0700

    chore: temp solution for contact schema in op rep form

--- bciers/apps/registration/app/data/jsonSchema/operationRegistration/operationRepresentative.ts
+++ bciers/apps/registration/app/data/jsonSchema/operationRegistration/operationRepresentative.ts
@@ -9,6 +9,90 @@ import {
   operationRepresentativePreface,
 } from "./titles";
 import { contactsUiSchema } from "@/administration/app/data/jsonSchema/contact";
+import provinceOptions from "@bciers/data/provinces.json";
+
+// Temp contact schema
+const section1: RJSFSchema = {
+  type: "object",
+  title: "Personal Information",
+  required: ["first_name", "last_name"],
+  properties: {
+    first_name: {
+      type: "string",
+      title: "First Name",
+    },
+    last_name: {
+      type: "string",
+      title: "Last Name",
+    },
+  },
+};
+
+const section2: RJSFSchema = {
+  type: "object",
+  title: "Work Information",
+  required: ["position_title"],
+  properties: {
+    position_title: {
+      type: "string",
+      title: "Job Title / Position",
+    },
+  },
+};
+
+const section3: RJSFSchema = {
+  type: "object",
+  title: "Contact Information",
+  required: ["email", "phone_number"],
+  properties: {
+    email: {
+      type: "string",
+      title: "Business Email Address",
+      format: "email",
+    },
+    phone_number: {
+      type: "string",
+      title: "Business Telephone Number",
+      format: "phone",
+    },
+  },
+};
+
+const section4: RJSFSchema = {
+  type: "object",
+  title: "Address Information",
+  properties: {
+    street_address: {
+      type: "string",
+      title: "Business Mailing Address",
+    },
+    municipality: {
+      type: "string",
+      title: "Municipality",
+    },
+    province: {
+      type: "string",
+      title: "Province",
+      anyOf: provinceOptions,
+    },
+    postal_code: {
+      type: "string",
+      title: "Postal Code",
+      format: "postal-code",
+    },
+  },
+};
+
+export const tempContactsSchema: RJSFSchema = {
+  type: "object",
+  required: ["section1", "section2", "section3", "section4"],
+  properties: {
+    section1,
+    section2,
+    section3,
+    section4,
+  },
+};
 
 export const operationRepresentativeSchema: RJSFSchema = {
   title: "Operation Representative",
@@ -57,7 +141,7 @@ export const operationRepresentativeUiSchema: UiSchema = {
     "ui:ArrayFieldTemplate": ArrayFieldTemplate,
     "ui:options": {
       label: false,
-      arrayAddLabel: "Add another representative",
+      arrayAddLabel: "Add Another Operation Representative",
       title: "Operation Representative",
     },
   },
