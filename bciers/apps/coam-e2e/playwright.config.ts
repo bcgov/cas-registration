469e3703b6673cb4202d8aa8f71c36986ca48136
marcellmueller <marcel@button.is>
2024-08-30 09:47:52 -0400

    chore: simplify playwright shared configs

--- bciers/apps/coam-e2e/playwright.config.ts
+++ bciers/apps/coam-e2e/playwright.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@playwright/test";
 import playwrightBaseConfig from "../../playwright.config.base";
 
-// For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env.BASE_URL || "http://127.0.0.1:3000/coam";
-
 export default defineConfig({
   ...playwrightBaseConfig,
-  use: {
-    baseURL,
-  },
 });
