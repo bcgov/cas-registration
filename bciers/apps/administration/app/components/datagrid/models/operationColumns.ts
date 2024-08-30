c33ff531ac958fd5af95171594a3adde6c33b165
SeSo <sobhani.sepehr@gmail.com>
2024-08-30 14:47:29 -0700

    feat: add status and boro id columns to operation data grid

--- bciers/apps/administration/app/components/datagrid/models/operationColumns.ts
+++ bciers/apps/administration/app/components/datagrid/models/operationColumns.ts
@@ -14,8 +14,15 @@ const operationColumns = (
       // Set flex to 1 to make the column take up all the remaining width if user zooms out
       flex: 1,
     },
-    { field: "bcghg_id", headerName: "BC GHG ID", width: 200 },
     { field: "type", headerName: "Operation Type", width: 200 },
+    {
+      field: "bc_obps_regulated_operation",
+      headerName: "BORO ID",
+      width: 120,
+      valueGetter: (params) => params.row?.bc_obps_regulated_operation ?? "N/A",
+    },
+    { field: "bcghg_id", headerName: "BC GHG ID", width: 120 },
+    { field: "status", headerName: "Status", width: 100 },
     {
       field: "facilities",
       headerName: "Facilities",
@@ -36,7 +43,7 @@ const operationColumns = (
     columns.splice(OPERATOR_COLUMN_INDEX, 0, {
       field: "operator",
       headerName: "Operator Legal Name",
-      width: 400,
+      width: 250,
     });
   }
 
