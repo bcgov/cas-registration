4deb4ed6fef4385e5397d5d46449b7b9d2af62a6
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-08-30 00:34:36 -0700

    Modified Operation review
    Modified Operation review
    
    chore:modified schema
    
    Added changes for save and continue
    
    Resolved errors
    
    chore:fixed person responsiblec component
    
    chore:fixed errors
    
    Fixed variable name issue

---
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/[version_id]/person-responsible/page.tsx
@@ -0,0 +1,9 @@
+import PersonResponsible from "@reporting/src/app/components/operations/PersonResponsible";
+
+export default async function Page({
+  params,
+}: {
+  params: { version_id: number };
+}) {
+  return <PersonResponsible />;
+}
