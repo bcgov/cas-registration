1e0db8136c397fa73193002af32c781df7c6e593
marcellmueller <marcel@button.is>
2024-09-10 11:30:49 -0700

    chore: remove default db values

--- bciers/libs/e2e/src/utils/pool.ts
+++ bciers/libs/e2e/src/utils/pool.ts
@@ -11,9 +11,9 @@ dotenv.config({ path: envFilePath });
 const { Pool } = require("pg");
 
 export const pool = new Pool({
-  user: process.env.DB_USER ?? "postgres",
-  password: process.env.DB_PASSWORD ?? "postgres",
-  host: process.env.DB_HOST ?? "localhost",
-  database: process.env.DB_NAME ?? "registration",
-  port: process.env.DB_PORT ?? 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
