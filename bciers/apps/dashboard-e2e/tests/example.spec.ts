0fb18d118878f7c13efeae82b368768b01d42243
marcellmueller <marcel@button.is>
2024-09-10 16:03:04 -0700

    chore: fix reporting landing route

--- bciers/apps/dashboard-e2e/tests/example.spec.ts
+++ bciers/apps/dashboard-e2e/tests/example.spec.ts
@@ -1,4 +1,3 @@
-import { UserRole } from "@/e2e/utils/enums";
 import testNxProjectLandingPage from "@bciers/e2e/utils/test-nx-app-landing-page";
 
-testNxProjectLandingPage("dashboard", UserRole.INDUSTRY_USER_ADMIN);
+testNxProjectLandingPage("dashboard");
--- bciers/apps/reporting-e2e/tests/example.spec.ts
+++ bciers/apps/reporting-e2e/tests/example.spec.ts
@@ -1,5 +1,3 @@
-import { UserRole } from "@/e2e/utils/enums";
 import testNxProjectLandingPage from "@bciers/e2e/utils/test-nx-app-landing-page";
 
-// External users can't access the dashboard yet
-testNxProjectLandingPage("reporting", UserRole.CAS_ANALYST);
+testNxProjectLandingPage("reporting/reports");
