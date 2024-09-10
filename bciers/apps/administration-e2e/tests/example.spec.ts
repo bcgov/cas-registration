769319d739be9247b9a7388200f6a9ea61a57124
marcellmueller <marcel@button.is>
2024-09-10 13:23:13 -0700

    chore: specify role for basic e2e tests
    chore: test

--- bciers/apps/administration-e2e/tests/example.spec.ts
+++ bciers/apps/administration-e2e/tests/example.spec.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@/e2e/utils/enums";
 import testNxProjectLandingPage from "@bciers/e2e/utils/test-nx-app-landing-page";
 
-testNxProjectLandingPage("administration");
+testNxProjectLandingPage("administration", UserRole.INDUSTRY_USER_ADMIN);
--- bciers/apps/registration-e2e/tests/example.spec.ts
+++ bciers/apps/registration-e2e/tests/example.spec.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@/e2e/utils/enums";
 import testNxProjectLandingPage from "@bciers/e2e/utils/test-nx-app-landing-page";
 
-testNxProjectLandingPage("registration");
+testNxProjectLandingPage("registration", UserRole.INDUSTRY_USER_ADMIN);
