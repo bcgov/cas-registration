3065d1ad9b9a931c03ae4091bbf1b27930d6959c
Brianna Cerkiewicz <brianna@button.is>
2024-09-11 13:42:25 -0700

    feat: step 1 of reg workflow

--- bciers/apps/registration/app/components/operations/registration/OperationInformationForm.tsx
+++ bciers/apps/registration/app/components/operations/registration/OperationInformationForm.tsx
@@ -1,46 +1,117 @@
 "use client";
 
 import MultiStepBase from "@bciers/components/form/MultiStepBase";
-import { operationInformationUiSchema } from "apps/registration/app/data/jsonSchema/operationRegistration/operationInformation";
 import { OperationInformationFormData } from "apps/registration/app/components/operations/registration/types";
 import { actionHandler } from "@bciers/actions";
 import { useRouter } from "next/navigation";
 import { RJSFSchema } from "@rjsf/utils";
+import { useState } from "react";
+import { IChangeEvent } from "@rjsf/core";
+import { getOperation } from "@bciers/actions/api";
+import {
+  createNestedFormData,
+  createUnnestedFormData,
+} from "@bciers/components/form/formDataUtils";
+import { registrationOperationInformationUiSchema } from "@/registration/app/data/jsonSchema/operationInformation/registrationOperationInformation";
 
 interface OperationInformationFormProps {
-  formData: OperationInformationFormData;
+  rawFormData: OperationInformationFormData;
   schema: RJSFSchema;
   step: number;
   steps: string[];
 }
 
 const OperationInformationForm = ({
-  formData,
+  rawFormData,
   schema,
   step,
   steps,
 }: OperationInformationFormProps) => {
+  const [selectedOperation, setSelectedOperation] = useState("");
+  const [, setError] = useState(undefined);
+  const nestedFormData = rawFormData
+    ? createNestedFormData(rawFormData, schema)
+    : {};
+  const [formState, setFormState] = useState(nestedFormData);
+  const [key, setKey] = useState(Math.random());
   const router = useRouter();
+
+  function customValidate(
+    formData: { [key: string]: any },
+    errors: { [key: string]: any },
+  ) {
+    // @ts-ignore
+    const requiredOperationProperties = schema?.properties?.section2.required;
+
+    const isOperationInformationComplete = requiredOperationProperties.every(
+      (el: any) => formData.section2[el],
+    );
+
+    if (!formData.section1.operation && !isOperationInformationComplete) {
+      errors.section1.operation.addError("You must select or add an operation");
+    }
+    return errors;
+  }
+
   const handleSubmit = async (data: { formData?: any }) => {
-    const endpoint = `registration/v2/operations/${data.formData?.operation}/registration/operation`;
-    const response = await actionHandler(endpoint, "PUT", "", {
-      body: JSON.stringify(data.formData),
-    });
+    const isCreating = !data.formData?.section1?.operation;
+    const postEndpoint = `registration/v2/operations`;
+    const putEndpoint = `registration/v2/operations/${data.formData?.section1?.operation}/registration/operation`;
+    const body = JSON.stringify(
+      createUnnestedFormData(data.formData, [
+        "section1",
+        "section2",
+        "section3",
+      ]),
+    );
+    const response = await actionHandler(
+      isCreating ? postEndpoint : putEndpoint,
+      isCreating ? "POST" : "PUT",
+      "",
+      {
+        body,
+      },
+    );
     // handling the redirect here instead of in the MultiStepBase because we don't have the operation information until we receive the response
     const nextStepUrl = `/register-an-operation/${response.id}/${
       step + 1
     }${`?title=${response.name}`}`;
     router.push(nextStepUrl);
   };
+  const handleSelectOperationChange = async (data: any) => {
+    const operationId = data.section1.operation;
+    try {
+      setSelectedOperation(operationId);
+      const operationData = await getOperation(operationId);
+
+      // combine the entered data with the fetched data
+      const combinedData = { ...data, section2: operationData };
+      setFormState(combinedData);
+      setKey(Math.random());
+    } catch (err) {
+      setError("Failed to fetch operation data!" as any);
+    }
+  };
   return (
     <MultiStepBase
+      key={key}
       cancelUrl="/"
-      formData={formData}
+      formData={formState}
       onSubmit={handleSubmit}
       schema={schema}
       step={step}
       steps={steps}
-      uiSchema={operationInformationUiSchema}
+      onChange={(e: IChangeEvent) => {
+        let newSelectedOperation = e.formData?.section1?.operation;
+        if (
+          newSelectedOperation &&
+          newSelectedOperation !== selectedOperation
+        ) {
+          handleSelectOperationChange(e.formData);
+        }
+      }}
+      uiSchema={registrationOperationInformationUiSchema}
+      customValidate={customValidate}
     />
   );
 };
--- bciers/apps/registration/app/components/operations/registration/OperationInformationPage.tsx
+++ bciers/apps/registration/app/components/operations/registration/OperationInformationPage.tsx
@@ -1,76 +1,5 @@
-import { UUID } from "crypto";
+import { createRegistrationOperationInformationSchema } from "@/registration/app/data/jsonSchema/operationInformation/registrationOperationInformation";
 import OperationInformationForm from "apps/registration/app/components/operations/registration/OperationInformationForm";
-import { operationInformationSchema } from "apps/registration/app/data/jsonSchema/operationRegistration/operationInformation";
-import { RJSFSchema } from "@rjsf/utils";
-import {
-  getRegulatedProducts,
-  getCurrentUsersOperations,
-  getRegistrationPurposes,
-} from "@bciers/actions/api";
-import safeJsonParse from "libs/utils/safeJsonParse";
-import { RegistrationPurposes } from "./enums";
-
-export const createOperationInformationSchema = (
-  schema: RJSFSchema,
-  purposes: string[],
-  regulatedProducts: {
-    id: number;
-    name: string;
-  }[],
-  operations: { id: UUID; name: string }[],
-) => {
-  const localSchema = safeJsonParse(JSON.stringify(schema));
-
-  // add purposes from db to schema
-  localSchema.properties.registration_purpose.anyOf = purposes.map(
-    (purpose) => ({
-      const: purpose,
-      title: purpose,
-    }),
-  );
-
-  // add operations from db to schema
-  localSchema.properties.operation.anyOf = operations.map((operation) => ({
-    const: operation.id,
-    title: operation.name,
-  }));
-
-  // add regulated products
-  const regulatedProductsEnum = regulatedProducts.map((product) => product.id);
-  const regulatedProductsEnumNames = regulatedProducts.map(
-    (product) => product.name,
-  );
-
-  const oneOfOptions = purposes.map((purpose) => {
-    const isRegulatedProducts =
-      purpose !== RegistrationPurposes.ELECTRICITY_IMPORT_OPERATION &&
-      purpose !== RegistrationPurposes.POTENTIAL_REPORTING_OPERATION;
-
-    return {
-      required: [isRegulatedProducts && "regulated_products"],
-      properties: {
-        registration_purpose: {
-          type: "string",
-          const: purpose,
-        },
-        ...(isRegulatedProducts && {
-          regulated_products: {
-            title: "Regulated Product Name(s)",
-            type: "array",
-            minItems: 1,
-            items: {
-              enum: regulatedProductsEnum,
-              enumNames: regulatedProductsEnumNames,
-            },
-          },
-        }),
-      },
-    };
-  });
-
-  localSchema.dependencies.registration_purpose.oneOf = oneOfOptions;
-  return localSchema;
-};
 
 const OperationInformationPage = async ({
   step,
@@ -79,22 +8,10 @@ const OperationInformationPage = async ({
   step: number;
   steps: string[];
 }) => {
-  const regulatedProducts: { id: number; name: string }[] =
-    await getRegulatedProducts();
-  const operations = await getCurrentUsersOperations();
-  const registrationPurposes = await getRegistrationPurposes();
-
-  const formSchema = createOperationInformationSchema(
-    operationInformationSchema,
-    registrationPurposes,
-    regulatedProducts,
-    operations,
-  );
-
   return (
     <OperationInformationForm
-      formData={{}}
-      schema={formSchema}
+      rawFormData={{}}
+      schema={await createRegistrationOperationInformationSchema()}
       step={step}
       steps={steps}
     />
