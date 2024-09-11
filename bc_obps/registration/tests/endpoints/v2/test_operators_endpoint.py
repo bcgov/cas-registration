2330f2209502c2459e7432cb5fcc198a7f58a2fb
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2024-09-10 17:24:31 -0700

    chore: update operators v2 endpoint to match searching strategy with operations

--- bc_obps/registration/tests/endpoints/v2/test_operators_endpoint.py
+++ bc_obps/registration/tests/endpoints/v2/test_operators_endpoint.py
@@ -21,7 +21,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
         # Get the default page 1 response
         response = TestUtils.mock_get_with_auth_role(self, "cas_admin")
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         # save the id of the first paginated response item
         page_1_response_id = response_data[0].get('id')
         assert len(response_data) == PAGE_SIZE
@@ -32,7 +32,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
             custom_reverse_lazy('list_operators_v2') + "?page=2&sort_field=created_at&sort_order=desc",
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         # save the id of the first paginated response item
         page_2_response_id = response_data[0].get('id')
         assert len(response_data) == PAGE_SIZE
@@ -46,7 +46,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
             custom_reverse_lazy('list_operators_v2') + "?page=2&sort_field=created_at&sort_order=asc",
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         # save the id of the first paginated response item
         page_2_response_id_reverse = response_data[0].get('id')
         assert len(response_data) == PAGE_SIZE
@@ -64,7 +64,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
             self, "cas_admin", custom_reverse_lazy('list_operators_v2') + "?legal_name=gouda"
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         assert len(response_data) == 1
         for item in response_data:
             assert item.get('legal_name') == 'gouda'
@@ -74,7 +74,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
             self, "cas_admin", custom_reverse_lazy('list_operators_v2') + "?legal_name=havarti"
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         assert len(response_data) == 0
 
         # Test with a cra_business_number filter
@@ -82,7 +82,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
             self, "cas_admin", custom_reverse_lazy('list_operators_v2') + "?cra_business_number=118675309"
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         assert len(response_data) == 1
 
         # Test with a cra_business_number filter that doesn't exist
@@ -90,7 +90,7 @@ class TestOperatorsEndpoint(CommonTestSetup):
             self, "cas_admin", custom_reverse_lazy('list_operators_v2') + "?cra_business_number=backrolls"
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         assert len(response_data) == 0
 
         # Test with multiple filters (with business structure obj.first (BC Corp))
@@ -100,5 +100,5 @@ class TestOperatorsEndpoint(CommonTestSetup):
             custom_reverse_lazy('list_operators_v2') + "?cra_business_number=118675309&business_structure=BC",
         )
         assert response.status_code == 200
-        response_data = response.json().get('data')
+        response_data = response.json().get('items')
         assert len(response_data) == 1
