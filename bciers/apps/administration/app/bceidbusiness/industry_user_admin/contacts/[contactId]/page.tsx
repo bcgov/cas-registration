0a2e82808aadddc23d2da89d7b0b160705e31546
Brianna Cerkiewicz <brianna@button.is>
2024-09-12 08:48:21 -0700

    fix: page is stuck on add mode after creating a contact

--- bciers/apps/administration/app/bceidbusiness/industry_user_admin/contacts/[contactId]/page.tsx
+++ bciers/apps/administration/app/bceidbusiness/industry_user_admin/contacts/[contactId]/page.tsx
@@ -3,7 +3,5 @@ import ContactPage from "@/administration/app/components/contacts/ContactPage";
 export default function Page({
   params: { contactId },
 }: Readonly<{ params: { contactId: string } }>) {
-  console.log("!!!!!!!!!");
-  console.log("contactId in [contactid] page.tsx", contactId);
   return <ContactPage contactId={contactId} />;
 }
