ea7535c12490b388d18d916eba7c9896cc20b10d
Brianna Cerkiewicz <brianna@button.is>
2024-09-12 08:46:46 -0700

    chore: comments and notes

--- bciers/apps/administration/app/bceidbusiness/industry_user_admin/contacts/add-contact/page.tsx
+++ bciers/apps/administration/app/bceidbusiness/industry_user_admin/contacts/add-contact/page.tsx
@@ -1,5 +1,6 @@
 import ContactPage from "@/administration/app/components/contacts/ContactPage";
 
 export default function Page() {
+  console.log("in add-contact page");
   return <ContactPage />;
 }
