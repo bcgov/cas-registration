03520e4c2a09e7e0d1736fc1254c1b732c8caf6b
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-09-11 23:47:27 -0700

    added changes

--- bc_obps/reporting/models/custom_methodology_schema.py
+++ bc_obps/reporting/models/custom_methodology_schema.py
@@ -1,12 +1,16 @@
-from django.db import models
 from common.models import BaseModel
+from django.db import models
+from registration.models import Activity
+from reporting.models import Configuration
 
 
 class CustomMethodologySchema(BaseModel):
     """Custom schema for a methodology."""
 
-    name = models.CharField(max_length=255)
     json_schema = models.JSONField()
+    activity = models.ForeignKey(Activity, on_delete=models.DO_NOTHING, related_name="+")
+    valid_from = models.ForeignKey(Configuration, on_delete=models.DO_NOTHING, related_name="+")
+    valid_to = models.ForeignKey(Configuration, on_delete=models.DO_NOTHING, related_name="+")
 
     class Meta:
         db_table = 'erc"."custom_methodology_schema'
