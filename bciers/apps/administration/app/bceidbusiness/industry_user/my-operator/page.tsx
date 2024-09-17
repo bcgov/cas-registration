d20a5671dd2931fa567631c98293339b8b71d85e
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2024-09-17 08:57:13 -0700

    chore: Wrap routes in suspense wrapper that haven't done so in child data components

--- bciers/apps/administration/app/bceidbusiness/industry_user/my-operator/page.tsx
+++ bciers/apps/administration/app/bceidbusiness/industry_user/my-operator/page.tsx
@@ -1,7 +1,13 @@
 // 🚩 flagging that for shared routes between roles, `Operator` code is a component for code maintainability
+import { Suspense } from "react";
+import Loading from "@bciers/components/loading/SkeletonForm";
 
 import Operator from "@/administration/app/components/operators/Operator";
 
 export default async function Page() {
-  return <Operator />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <Operator />
+    </Suspense>
+  );
 }
