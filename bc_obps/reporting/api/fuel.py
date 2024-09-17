9ffd32426936c480b3eaf06ebaec7c72cf608803
Dylan Leard <dylan@button.is>
2024-09-17 14:37:38 -0700

    chore: add endpoint for getting a single fuel

---
+++ bc_obps/reporting/api/fuel.py
@@ -0,0 +1,25 @@
+from service.data_access_service.fuel_service import FuelTypeDataAccessService
+from .router import router
+from registration.decorators import handle_http_errors
+from django.http import HttpRequest
+from typing import Tuple
+from reporting.models import FuelType
+
+from registration.schema.generic import Message
+from ninja.responses import codes_4xx, codes_5xx
+
+
+##### GET #####
+
+
+@router.get(
+    "/fuel",
+    response={200: str, codes_4xx: Message, codes_5xx: Message},
+    url_name="get_fuel",
+)
+@handle_http_errors()
+def get_fuel_data(
+    request: HttpRequest,
+    fuel_name: str,
+) -> Tuple[int, FuelType]:
+    return 200, FuelTypeDataAccessService.get_fuel(fuel_name)
