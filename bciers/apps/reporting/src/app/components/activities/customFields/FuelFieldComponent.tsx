b8334bb7dfb5a2bf5f11f0ce090532cb6dcb4638
Dylan Leard <dylan@button.is>
2024-09-18 08:40:13 -0700

    chore: fix build error with extended type

--- bciers/apps/reporting/src/app/components/activities/customFields/FuelFieldComponent.tsx
+++ bciers/apps/reporting/src/app/components/activities/customFields/FuelFieldComponent.tsx
@@ -5,17 +5,19 @@ import { actionHandler } from "@bciers/actions";
 import { IChangeEvent } from "@rjsf/core";
 const registry = getDefaultRegistry();
 
+interface FuelDataChangeEvent extends IChangeEvent {
+  fuelName: string;
+}
+
 const ObjectField = registry.fields.ObjectField;
 
 export const FuelFields: React.FunctionComponent<FieldProps> = (props) => {
-  const handleChange = async (c: IChangeEvent) => {
+  const handleChange = async (c: FuelDataChangeEvent) => {
     const fuelData = await actionHandler(
       `reporting/fuel?fuel_name=${c.fuelName}`,
       "GET",
       "",
     );
-    console.log(c);
-    console.log(fuelData);
     props.onChange({
       ...props.formData,
       fuelName: fuelData.name,
