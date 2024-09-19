f1db72441f2d0cc519f81c29871e018ffc57936a
Dylan Leard <dylan@button.is>
2024-09-19 10:46:10 -0700

    test: fix service tests

--- bc_obps/service/tests/test_facility_report_service.py
+++ bc_obps/service/tests/test_facility_report_service.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.exceptions import ObjectDoesNotExist
 from reporting.tests.utils.bakers import facility_report_baker, activity_baker
 from service.facility_report_service import FacilityReportService
+from reporting.schema.facility_report import FacilityReportIn
 
 
 class TestFacilityReportService(TestCase):
@@ -14,10 +15,10 @@ class TestFacilityReportService(TestCase):
         self.assertEqual(str(exception_context.exception), "FacilityReport matching query does not exist.")
 
     @staticmethod
-    def returns_facility_report():
+    def test_returns_facility_report():
         facility_report = facility_report_baker()
         assert facility_report == FacilityReportService.get_facility_report_by_version_and_id(
-            report_version_id=facility_report.report_version_id, facility_id=facility_report.faclity_id
+            report_version_id=facility_report.report_version_id, facility_id=facility_report.facility_id
         )
 
     def test_get_activities_throws_if_facility_report_does_not_exist(self):
@@ -29,14 +30,14 @@ class TestFacilityReportService(TestCase):
         self.assertEqual(str(exception_context.exception), "FacilityReport matching query does not exist.")
 
     @staticmethod
-    def returns_activity_id_list():
+    def test_returns_activity_id_list():
         facility_report = facility_report_baker()
         a1 = activity_baker()
         a2 = activity_baker()
         assert (
             len(
                 FacilityReportService.get_activity_ids_for_facility(
-                    version_id=facility_report.report_version_id, facility_id=facility_report.faclity_id
+                    version_id=facility_report.report_version_id, facility_id=facility_report.facility_id
                 )
             )
             == 0
@@ -46,37 +47,40 @@ class TestFacilityReportService(TestCase):
         assert (
             len(
                 FacilityReportService.get_activity_ids_for_facility(
-                    version_id=facility_report.report_version_id, facility_id=facility_report.faclity_id
+                    version_id=facility_report.report_version_id, facility_id=facility_report.facility_id
                 )
             )
             == 2
         )
 
     @staticmethod
-    def form_data_returns_none_if_facility_report_does_not_exist(self):
-        assert (
-            FacilityReportService.get_facility_report_form_data(
-                report_version_id=999, facility_id="00000000-00000000-00000000-00000000"
-            )
-            is None
-        )
+    def test_form_data_returns_none_if_facility_report_does_not_exist():
+        assert FacilityReportService.get_facility_report_form_data(None, []) is None
 
     @staticmethod
-    def form_data_returns_facility_report_form_data():
+    def test_form_data_returns_facility_report_form_data():
         facility_report = facility_report_baker()
-        returned_data = FacilityReportService.get_facility_report_by_version_and_id(
-            report_version_id=facility_report.report_version_id, facility_id=facility_report.faclity_id
+        returned_data = FacilityReportService.get_facility_report_form_data(
+            facility_report=facility_report, activity_ids=[1, 2]
         )
         assert facility_report.facility_name == returned_data.facility_name
         assert facility_report.facility_type == returned_data.facility_type
         assert facility_report.facility_bcghgid == returned_data.facility_bcghgid
 
+        print(returned_data)
+
     @staticmethod
-    def saves_facility_report_form_data():
+    def test_saves_facility_report_form_data():
         facility_report = facility_report_baker()
-        data = {"facility_name": "CHANGED"}
+        data = FacilityReportIn(
+            facility_name="CHANGED",
+            facility_type=facility_report.facility_type,
+            facility_bcghgid=facility_report.facility_bcghgid,
+            activities=[],
+            products=[],
+        )
         returned_data = FacilityReportService.save_facility_report(
-            report_version_id=facility_report.report_version_id, facility_id=facility_report.faclity_id, data=data
+            report_version_id=facility_report.report_version_id, facility_id=facility_report.facility_id, data=data
         )
         assert facility_report.facility_name == "CHANGEDs"
         assert facility_report.facility_type == returned_data.facility_type
