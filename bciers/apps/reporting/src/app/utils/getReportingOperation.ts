5d327f2a1b906839ad6d5089234112c12a6e593b
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-09-19 18:06:42 -0700

    chore:fixed minor issues

--- bciers/apps/reporting/src/app/utils/getReportingOperation.ts
+++ bciers/apps/reporting/src/app/utils/getReportingOperation.ts
@@ -1,10 +1,9 @@
 import { actionHandler } from "@bciers/actions";
-
 export async function getReportingOperation(version_id: number) {
   let response = await actionHandler(
     `reporting/report-version/${version_id}/report-operation`,
     "GET",
     `reporting/report-version/${version_id}/report-operation`,
   );
-  if (response.ok) return response;
+  if (!response.error) return response;
 }
