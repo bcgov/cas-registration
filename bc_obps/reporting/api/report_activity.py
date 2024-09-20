1b0150c3b995a8e054b0b01fd214ed52d62b0d3d
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-09-20 13:32:46 -0700

    chore: api route

--- bc_obps/reporting/api/report_activity.py
+++ bc_obps/reporting/api/report_activity.py
@@ -1,4 +1,4 @@
-import pprint
+import json
 from django.http import HttpRequest
 from django.db import transaction
 from registration.decorators import handle_http_errors
@@ -6,11 +6,17 @@ from reporting.schema.report_activity_data import ReportActivityDataIn
 from .router import router
 
 
-@router.post("report/{report_version}/facility/{facility_id}/report-activity")
+@router.post("reports/{report_version_id}/facilities/{facility_id}/activity/{activity_id}/report-activity")
 @handle_http_errors()
 @transaction.atomic()
-def save_activity_data(request: HttpRequest, payload: ReportActivityDataIn):
-
-    pprint.pprint(payload, None, 2)
+def save_activity_data(
+    request: HttpRequest,
+    report_version_id: int,
+    facility_id: int,
+    activity_id: int,
+    payload: ReportActivityDataIn,
+):
+    print(report_version_id, facility_id, activity_id)
+    print(payload)
 
     return 200
