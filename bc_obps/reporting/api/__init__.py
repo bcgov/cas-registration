aef8695c20149eff57f560aafa4d7aa68e6ef4f1
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-09-24 13:26:57 -0700

    Fixed some rebasing errors

--- bc_obps/reporting/api/__init__.py
+++ bc_obps/reporting/api/__init__.py
@@ -5,4 +5,3 @@ from .build_form_schema import build_form_schema
 from .operations import get_dashboard_operations_list
 from .activity_data import get_initial_activity_data
 from .facility_report import get_facility_report_form_data
-from .product_data import get_products
