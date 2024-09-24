64aab44b41e38b58d64465a0cf731eb754c681d1
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-09-24 16:34:23 -0700

    feat:added signoff page, function for formatting date

---
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user/reports/[version_id]/sign-off/page.tsx
@@ -0,0 +1,5 @@
+import SignOffPage from "@reporting/src/app/components/signOff/signOffPage";
+
+export default async function Page() {
+  return <SignOffPage />;
+}
---
+++ bciers/apps/reporting/src/app/components/signOff/Success.tsx
@@ -0,0 +1,31 @@
+import Check from "@bciers/components/icons/Check";
+import Link from "next/link";
+import { getTodaysDateWithTime } from "@reporting/src/app/utils/formatDate";
+
+const ReportSubmission = () => {
+  const currentDate = getTodaysDateWithTime();
+
+  return (
+    <div className="flex items-center justify-center">
+      <section className="flex flex-col items-center justify-center max-w-[600px] mx-auto h-[20vh] mt-40">
+        {" "}
+        {/* Adjusted height and added margin-top */}
+        <div className="flex flex-col items-center justify-center">
+          {Check}
+          <div className="flex flex-col items-center justify-center mt-5">
+            <h3 className="mb-2 mt-4">Successful Submission</h3>
+            <p className="m-0">You successfully submitted your report.</p>
+            <p className="m-0">Submission time: {currentDate}</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center mt-20">
+          <Link className="link-button-outlined" href="/reports">
+            Return to Report Table
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ReportSubmission;
