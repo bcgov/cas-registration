6682c4ff9d9c1278373664895faf570d25fa168f
Dylan Leard <dylan@button.is>
2024-09-26 16:35:18 -0700

    chore: update schemaMaps with all currently completed activities

--- bciers/apps/reporting/src/app/components/activities/uiSchemas/schemaMaps.tsx
+++ bciers/apps/reporting/src/app/components/activities/uiSchemas/schemaMaps.tsx
@@ -10,18 +10,32 @@ type DefaultEmptySourceTypeMap = {
   [key: string]: EmptyWithUnits | EmptyWithFuels | EmptyOnlyEmissions;
 };
 
-// Activity ID & matching uiSchema
+// Activity slug & matching uiSchema
 export const uiSchemaMap: UiSchemaMap = {
   gsc_excluding_line_tracing: "gscUiSchema",
+  gsc_solely_for_line_tracing: "gscUiSchema",
+  gsc_other_than_non_compression: "gscOthenThanNonCompression",
+  gsc_non_compression: "gscNonCompressionNonProcessingUiSchema",
+  fuel_combustion_by_mobile: "fuelCombustionMobileUiSchema",
+  hydrogen_production: "hydrogenProduction",
+  pulp_and_paper: "pulpAndPaperUiSchema",
+  refinery_fuel_gas: "refineryFuelGasUiSchema",
   carbonate_use: "carbonatesUseUiSchema",
 };
 
 const withUnits: EmptyWithUnits = { units: [{ fuels: [{ emissions: [{}] }] }] };
-// const withFuels: EmptyWithFuels = { fuels: [{ emissions: [{}] }] };
+const withFuels: EmptyWithFuels = { fuels: [{ emissions: [{}] }] };
 const onlyEmissions: EmptyOnlyEmissions = { emissions: [{}] };
 
-// Activity ID & matching shape of an empty Source Type
+// Activity slug & matching shape of an empty Source Type
 export const defaultEmtpySourceTypeMap: DefaultEmptySourceTypeMap = {
   gsc_excluding_line_tracing: withUnits,
+  gsc_solely_for_line_tracing: withUnits,
+  gsc_other_than_non_compression: withUnits,
+  gsc_non_compression: withUnits,
+  fuel_combustion_by_mobile: withFuels,
+  hydrogen_production: onlyEmissions,
+  pulp_and_paper: onlyEmissions,
+  refinery_fuel_gas: withFuels,
   carbonate_use: onlyEmissions,
 };
