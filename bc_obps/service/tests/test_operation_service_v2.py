3c178d527d9c75c4d4d75cf3510c5ac5b9e06956
SeSo <sobhani.sepehr@gmail.com>
2024-09-26 11:45:58 -0700

    test: add new tests and update existing ones
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/service/tests/test_operation_service_v2.py
+++ bc_obps/service/tests/test_operation_service_v2.py
@@ -3,8 +3,6 @@ from registration.models.registration_purpose import RegistrationPurpose
 from registration.models.opted_in_operation_detail import OptedInOperationDetail
 from registration.constants import UNAUTHORIZED_MESSAGE
 from registration.models.address import Address
-from registration.schema.v1.contact import ContactIn
-from registration.models.contact import Contact
 from registration.schema.v2.operation import (
     OperationRepresentativeIn,
     OperationStatutoryDeclarationIn,
@@ -176,105 +174,80 @@ class TestOperationServiceV2:
         approved_user_operator = baker.make_recipe('utils.approved_user_operator')
         operation = baker.make_recipe('utils.operation', operator=approved_user_operator.operator)
         payload = OperationRepresentativeIn(
-            new_operation_representatives=[
-                ContactIn(
-                    first_name="John",
-                    last_name="Doe",
-                    email="john.doe@example.com",
-                    phone_number="+16044011234",
-                    position_title="Mr.Tester",
-                    street_address='13 Street',
-                    municipality='municipality',
-                    province='AB',
-                    postal_code='H0H0H0',
-                ),
-                ContactIn(
-                    first_name="Fruit",
-                    last_name="Loops",
-                    email="froot.loops@example.com",
-                    phone_number="+16044011234",
-                    position_title="Mx.Tester",
-                ),
-            ]
+            first_name="John",
+            last_name="Doe",
+            email="john.doe@example.com",
+            phone_number="+16044011234",
+            position_title="Mr.Tester",
+            street_address='13 Street',
+            municipality='municipality',
+            province='AB',
+            postal_code='H0H0H0',
         )
 
-        OperationServiceV2.register_operation_operation_representative(
-            approved_user_operator.user.user_guid, operation.id, payload
-        )
+        OperationServiceV2.create_operation_representative(approved_user_operator.user.user_guid, operation.id, payload)
         operation.refresh_from_db()
 
-        assert approved_user_operator.operator.contacts.count() == 2
-        assert operation.contacts.count() == 2
+        assert approved_user_operator.operator.contacts.count() == 1
+        assert operation.contacts.count() == 1
         assert Address.objects.count() == 2  # 1 is the contact's, 1 is from the operation baker recipe
-        first_operation_contact = operation.contacts.first()
-        assert first_operation_contact.first_name == 'John'
-        assert first_operation_contact.address.street_address == '13 Street'
-        assert first_operation_contact.created_at is not None
-        assert first_operation_contact.created_by == approved_user_operator.user
-        assert operation.contacts.last().first_name == 'Fruit'
-        assert operation.contacts.last().address is None
-
+        operation_contact = operation.contacts.first()
+        assert operation_contact.first_name == 'John'
+        assert operation_contact.address.street_address == '13 Street'
+        assert operation_contact.created_at is not None
+        assert operation_contact.created_by == approved_user_operator.user
         assert operation.updated_at is not None
         assert operation.updated_by == approved_user_operator.user
 
     @staticmethod
     def test_assign_existing_contacts_to_operation():
+        approved_user_operator = baker.make_recipe('utils.approved_user_operator')
+        users_operator = approved_user_operator.operator
         contacts = baker.make_recipe('utils.contact', _quantity=5)
-        operator = baker.make_recipe('utils.operator', contacts=contacts)
-        approved_user_operator = baker.make_recipe('utils.approved_user_operator', operator=operator)
-        operation = baker.make_recipe('utils.operation', operator=approved_user_operator.operator)
+        contact_to_update = contacts[0]
+        users_operator.contacts.set(list(map(lambda c: c.id, contacts)))
+        operation = baker.make_recipe('utils.operation', operator=users_operator)
 
-        payload = OperationRepresentativeIn(operation_representatives=Contact.objects.values_list('id', flat=True))
+        # bad payload, should not update first_name, last_name and email when existing_contact_id is provided
+        bad_payload = OperationRepresentativeIn(
+            existing_contact_id=contact_to_update.id,
+            first_name="John",
+            last_name="Doe",
+            email="john.doe@example.com",
+            phone_number="+16044011234",
+            position_title="Mr.Tester",
+            street_address='13 Street',
+            municipality='municipality',
+            province='AB',
+            postal_code='H0H0H0',
+        )
 
-        OperationServiceV2.register_operation_operation_representative(
-            approved_user_operator.user.user_guid, operation.id, payload
+        with pytest.raises(Exception):
+            OperationServiceV2.create_operation_representative(
+                approved_user_operator.user.user_guid, operation.id, bad_payload
+            )
+        # good payload, using the same first_name, last_name and email when existing_contact_id is provided
+        good_payload = OperationRepresentativeIn(
+            existing_contact_id=contact_to_update.id,
+            first_name=contact_to_update.first_name,
+            last_name=contact_to_update.last_name,
+            email=contact_to_update.email,
+            phone_number="+16044011234",
+            position_title="Mr.Tester",
+            street_address='13 Street',
+            municipality='municipality',
+            province='AB',
+            postal_code='H0H0H0',
+        )
+
+        OperationServiceV2.create_operation_representative(
+            approved_user_operator.user.user_guid, operation.id, good_payload
         )
         operation.refresh_from_db()
-        assert approved_user_operator.operator.contacts.count() == 5  # existing contacts should be unaffected
-        assert operation.contacts.count() == 5
+        assert users_operator.contacts.count() == 5
+        assert operation.contacts.count() == 1
         assert operation.updated_at is not None
         assert operation.updated_by == approved_user_operator.user
-
-    @staticmethod
-    def test_assign_existing_and_new_contacts_to_operation():
-        contacts = baker.make_recipe('utils.contact', _quantity=5)
-        operator = baker.make_recipe('utils.operator', contacts=contacts)
-        approved_user_operator = baker.make_recipe('utils.approved_user_operator', operator=operator)
-        operation = baker.make_recipe('utils.operation', operator=approved_user_operator.operator)
-
-        payload = OperationRepresentativeIn(
-            operation_representatives=Contact.objects.values_list('id', flat=True),
-            new_operation_representatives=[
-                ContactIn(
-                    first_name="John",
-                    last_name="Doe",
-                    email="john.doe@example.com",
-                    phone_number="+16044011234",
-                    position_title="Mr.Tester",
-                    street_address='13 Street',
-                    municipality='municipality',
-                    province='AB',
-                    postal_code='H0H0H0',
-                ),
-                ContactIn(
-                    first_name="Fruit",
-                    last_name="Loops",
-                    email="froot.loops@example.com",
-                    phone_number="+16044011234",
-                    position_title="Mx.Tester",
-                ),
-            ],
-        )
-
-        OperationServiceV2.register_operation_operation_representative(
-            approved_user_operator.user.user_guid, operation.id, payload
-        )
-        operation.refresh_from_db()
-        assert approved_user_operator.operator.contacts.count() == 7
-        assert operation.updated_at is not None
-        assert operation.updated_by == approved_user_operator.user
-        assert operation.contacts.count() == 7
-        assert Address.objects.count() == 2  # 1 is the contact's, 1 is from the operation baker recipe
 
     @staticmethod
     def test_raises_error_if_operation_does_not_belong_to_user_when_updating_opted_in_operation_detail():
