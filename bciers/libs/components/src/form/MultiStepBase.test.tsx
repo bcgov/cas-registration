38751b9da0ff2ab0f25f05e5f17d7cbe0007978b
Brianna Cerkiewicz <brianna@button.is>
2024-10-01 13:54:33 -0700

    test: vitests for MultiStepBase error handling

--- bciers/libs/components/src/form/MultiStepBase.test.tsx
+++ bciers/libs/components/src/form/MultiStepBase.test.tsx
@@ -314,4 +314,38 @@ describe("The MultiStepBase component", () => {
       expect(screen.getByText("whoopsie")).toBeVisible();
     });
   });
+
+  it("calls the onChange prop when the form changes", () => {
+    useParams.mockReturnValue({
+      formSection: "1",
+      operation: "create",
+    } as QueryParams);
+
+    const changeHandler = vi.fn();
+    render(
+      <MultiStepBase
+        {...defaultProps}
+        disabled={false}
+        onChange={changeHandler}
+      />,
+    );
+    const input = screen.getByLabelText(/field1*/i);
+    fireEvent.change(input, { target: { value: "new value" } });
+
+    expect(changeHandler).toHaveBeenCalled();
+  });
+
+  it("renders children", () => {
+    useParams.mockReturnValue({
+      formSection: "1",
+      operation: "create",
+    } as QueryParams);
+
+    render(
+      <MultiStepBase {...defaultProps}>
+        <div data-testid="test-child">Test child</div>
+      </MultiStepBase>,
+    );
+    expect(screen.getByTestId("test-child")).toBeVisible();
+  });
 });
