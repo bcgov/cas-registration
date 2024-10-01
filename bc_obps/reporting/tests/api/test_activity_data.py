6bf0979ec01926fa1fcdd426473c336cfcac4d82
Dylan Leard <dylan@button.is>
2024-10-01 16:53:26 -0700

    test: update activity api test

--- bc_obps/reporting/tests/api/test_activity_data.py
+++ bc_obps/reporting/tests/api/test_activity_data.py
@@ -1,25 +1,31 @@
 import json
 from registration.tests.utils.helpers import CommonTestSetup, TestUtils
 from registration.tests.utils.bakers import operator_baker
-from registration.utils import custom_reverse_lazy
+from model_bakery import baker
 
 
 class TestActivityData(CommonTestSetup):
-    endpoint = custom_reverse_lazy("get_initial_activity_data")
+    # endpoint = custom_reverse_lazy("get_initial_activity_data")
 
     # AUTHORIZATION
     def test_unauthorized_users_cannot_get_activity_data(self):
-        response = TestUtils.mock_get_with_auth_role(self, "cas_pending")
+        facility_report = baker.make_recipe('reporting.tests.utils.facility_report')
+        response = TestUtils.mock_get_with_auth_role(
+            self,
+            'cas_pending',
+            f'/api/reporting/report-version/{facility_report.report_version_id}/facility-report/{facility_report.facility_id}/initial-activity-data?activity_id=1',
+        )
         assert response.status_code == 401
 
     def test_authorized_users_can_get_activity_data(self):
         operator = operator_baker()
+        facility_report = baker.make_recipe('reporting.tests.utils.facility_report')
         TestUtils.authorize_current_user_as_operator_user(self, operator=operator)
 
         response = TestUtils.mock_get_with_auth_role(
             self,
             "industry_user",
-            f'{self.endpoint}?activity_name=General stationary combustion excluding line tracing&report_date=2024-05-01',
+            f'/api/reporting/report-version/{facility_report.report_version_id}/facility-report/{facility_report.facility_id}/initial-activity-data?activity_id=1',
         )
 
         assert response.status_code == 200
@@ -28,26 +34,15 @@ class TestActivityData(CommonTestSetup):
         # There are 2 source types in the map
         assert len(response_object['sourceTypeMap'].keys()) == 2
 
-    def test_invalid_without_report_date(self):
-        operator = operator_baker()
-        TestUtils.authorize_current_user_as_operator_user(self, operator=operator)
-
-        response = TestUtils.mock_get_with_auth_role(
-            self,
-            "industry_user",
-            f'{self.endpoint}?activity_name=General stationary combustion excluding line tracing',
-        )
-
-        assert response.status_code == 422
-
     def test_invalid_without_activity(self):
+        facility_report = baker.make_recipe('reporting.tests.utils.facility_report')
         operator = operator_baker()
         TestUtils.authorize_current_user_as_operator_user(self, operator=operator)
 
         response = TestUtils.mock_get_with_auth_role(
             self,
             "industry_user",
-            f'{self.endpoint}?report_date=2024-05-01',
+            f'/api/reporting/report-version/{facility_report.report_version_id}/facility-report/{facility_report.facility_id}/initial-activity-data',
         )
 
         assert response.status_code == 422
