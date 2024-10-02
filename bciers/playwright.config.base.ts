16ef7172d67b7e6f25ba1156ddf27dd08f65c36f
shon-button <shon@button.is>
2024-10-02 16:45:39 -0400

    test: happy paths

--- bciers/playwright.config.base.ts
+++ bciers/playwright.config.base.ts
@@ -16,8 +16,8 @@ const playwrightBaseConfig = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? "blob" : "html",
   /* Configure projects for major browsers */
