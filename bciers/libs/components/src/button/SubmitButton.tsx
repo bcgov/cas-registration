96cf6973d8edb799bdd716258e790fcf2b37fe34
marcellmueller <marcel@button.is>
2024-10-03 10:19:00 -0700

    chore: change submit button bg colour while submitting

--- bciers/libs/components/src/button/SubmitButton.tsx
+++ bciers/libs/components/src/button/SubmitButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@mui/material";
 import LoadingSpinner from "@bciers/components/loading/LoadingSpinner";
-
+import { BC_GOV_PRIMARY_BRAND_COLOR_BLUE } from "@bciers/styles/colors";
 interface SubmitButtonProps {
   children?: React.ReactNode | string;
   disabled?: boolean;
@@ -20,19 +20,29 @@ const SubmitButton = ({
 }: SubmitButtonProps) => {
   const isDisabled = disabled || isSubmitting;
 
-  const spinnerClass =
-    "absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full min-w-10 h-10";
+  const spinnerContainerClass =
+    "absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full min-w-10 h-10 transition-opacity duration-500";
 
   return (
     <Button
       type={type}
       variant={variant}
       disabled={isDisabled}
+      style={
+        isSubmitting
+          ? {
+              opacity: 0.5,
+              backgroundColor: BC_GOV_PRIMARY_BRAND_COLOR_BLUE,
+            }
+          : {
+              opacity: 1,
+            }
+      }
       onClick={onClick}
     >
       <div
         data-testid="spinner"
-        className={spinnerClass}
+        className={spinnerContainerClass}
         style={{
           visibility: isSubmitting ? "visible" : "hidden",
         }}
@@ -42,6 +52,7 @@ const SubmitButton = ({
       <div
         className="w-full"
         style={{
+          // Using visibility instead of display to prevent the button width from shrinking when the text is hidden
           visibility: isSubmitting ? "hidden" : "visible",
         }}
       >
