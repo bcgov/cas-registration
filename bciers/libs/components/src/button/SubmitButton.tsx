98bfb1ba754b2b34103aaee68806c1caa61c86ca
marcellmueller <marcel@button.is>
2024-10-03 08:07:17 -0700

    chore: add loading spinner and submit button

---
+++ bciers/libs/components/src/button/SubmitButton.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@mui/material";
+import LoadingSpinner from "@bciers/components/loading/LoadingSpinner";
+
+interface SubmitButtonProps {
+  disabled?: boolean;
+  isSubmitting: boolean;
+  onClick?: () => void;
+  submitButtonText?: string;
+  type?: "submit" | "button";
+  variant?: "contained" | "outlined";
+}
+
+const SubmitButton = ({
+  disabled,
+  isSubmitting,
+  onClick,
+  submitButtonText = "Submit",
+  type = "submit",
+  variant = "contained",
+}: SubmitButtonProps) => {
+  const isDisabled = disabled || isSubmitting;
+
+  const spinnerClass =
+    "absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full min-w-10 h-10";
+
+  return (
+    <Button
+      type={type}
+      variant={variant}
+      disabled={isDisabled}
+      onClick={onClick}
+    >
+      <div
+        className={spinnerClass}
+        style={{
+          visibility: isSubmitting ? "visible" : "hidden",
+        }}
+      >
+        <LoadingSpinner />
+      </div>
+      <div
+        className="w-full"
+        style={{
+          visibility: isSubmitting ? "hidden" : "visible",
+        }}
+      >
+        {submitButtonText}
+      </div>
+    </Button>
+  );
+};
+
+export default SubmitButton;
