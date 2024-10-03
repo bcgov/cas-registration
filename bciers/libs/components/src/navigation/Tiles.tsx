3f36c1a0414d6b7ad627ba8ee4f32b11f0fdb753
marcellmueller <marcel@button.is>
2024-10-03 12:34:14 -0700

    chore: more diff testing

--- bciers/libs/components/src/navigation/Tiles.tsx
+++ bciers/libs/components/src/navigation/Tiles.tsx
@@ -3,13 +3,16 @@ import Tile from "./Tile";
 
 const Tiles = ({ tiles }: { tiles: ContentItem[] }) => {
   return (
-    <section className="w-full flex flex-wrap gap-x-8 lg:gap-x-24 gap-y-16 my-8">
-      {tiles &&
-        tiles.map((tile) => {
-          const { title } = tile;
-          return <Tile key={title} {...tile} />;
-        })}
-    </section>
+    <>
+      <h2>Testing diffs!!!!</h2>
+      <section className="w-full flex flex-wrap gap-x-8 lg:gap-x-24 gap-y-16 my-8">
+        {tiles &&
+          tiles.map((tile) => {
+            const { title } = tile;
+            return <Tile key={title} {...tile} />;
+          })}
+      </section>
+    </>
   );
 };
 
