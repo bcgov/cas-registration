f52ec5de8292c00af816d4ef7d57acfcb39b8d54
SeSo <sobhani.sepehr@gmail.com>
2024-10-04 10:29:48 -0700

    chore: excluding declined user operators and add regression test
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/registration/tests/endpoints/v1/_user_operators/_current/test_operator_users.py
+++ bc_obps/registration/tests/endpoints/v1/_user_operators/_current/test_operator_users.py
@@ -34,11 +34,17 @@ class TestOperatorUsersEndpoint(CommonTestSetup):
 
     def test_industry_users_can_get_users_associated_with_their_operator(self):
         operator = operator_baker()
-        TestUtils.authorize_current_user_as_operator_user(self, operator)
+        TestUtils.authorize_current_user_as_operator_user(self, operator)  # User Operator Admin
         # Adding multiple users to the operator(using the same business_guid)
-        users = user_baker(custom_properties={'business_guid': self.user.business_guid, '_quantity': 10})
-        for user in users:
+        approved_users = user_baker(custom_properties={'business_guid': self.user.business_guid, '_quantity': 5})
+        declined_users = user_baker(custom_properties={'business_guid': self.user.business_guid, '_quantity': 5})
+
+        # Marking some of the users as declined to test the filtering
+        for user in declined_users:
+            baker.make(UserOperator, user=user, operator=operator, status=UserOperator.Statuses.DECLINED)
+        for user in approved_users:
             baker.make(UserOperator, user=user, operator=operator)
+
         response = TestUtils.mock_get_with_auth_role(
             self,
             endpoint=custom_reverse_lazy("get_operator_users"),
@@ -46,7 +52,7 @@ class TestOperatorUsersEndpoint(CommonTestSetup):
         )
         assert response.status_code == 200
         response_json = response.json()
-        assert len(response_json) == 10
+        assert len(response_json) == 5
         for user in response_json:
             assert user.keys() == {'id', 'full_name'}
             assert user['id'] is not None
@@ -54,7 +60,7 @@ class TestOperatorUsersEndpoint(CommonTestSetup):
 
     def test_industry_users_cannot_get_users_not_associated_with_their_operator(self):
         operator = operator_baker()
-        TestUtils.authorize_current_user_as_operator_user(self, operator)
+        TestUtils.authorize_current_user_as_operator_user(self, operator)  # User Operator Admin
         # Adding multiple users to a different operator
         users = user_baker(custom_properties={'business_guid': '00000000-0000-0000-0000-000000000000', '_quantity': 10})
         random_operator = operator_baker()
