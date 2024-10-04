8b52f162618a0dc21c091ef2d53824d718c636ed
SeSo <sobhani.sepehr@gmail.com>
2024-10-04 16:52:23 -0700

    chore: update imports
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/administration/app/bceidbusiness/industry_user_admin/users/page.tsx
+++ bciers/apps/administration/app/bceidbusiness/industry_user_admin/users/page.tsx
@@ -1,7 +1,6 @@
 // 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
 import { Suspense } from "react";
 import Loading from "@bciers/components/loading/SkeletonGrid";
-import { ContactsSearchParams } from "@/administration/app/components/contacts/types";
 import AccessRequestsPage from "@/administration/app/components/userOperators/AccessRequestsPage";
 
 export default async function Page() {
