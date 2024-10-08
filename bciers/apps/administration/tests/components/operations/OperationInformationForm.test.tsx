7d052be89efd20a501b5da8dd40e4dba23b5fb2d
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2024-10-08 10:23:54 -0700

    test: [rebase] Add test for updating opt-in operation details

--- bciers/apps/administration/tests/components/operations/OperationInformationForm.test.tsx
+++ bciers/apps/administration/tests/components/operations/OperationInformationForm.test.tsx
@@ -39,9 +39,88 @@ const testSchema: RJSFSchema = {
   },
 };
 
+const testSchemaWithOpt: RJSFSchema = {
+  type: "object",
+  properties: {
+    section1: {
+      title: "Section 1",
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          title: "Operation Name",
+        },
+      },
+    },
+    section2: {
+      title: "Section 2",
+      type: "object",
+      properties: {
+        type: {
+          type: "string",
+          title: "Operation Type",
+        },
+      },
+    },
+    section3: {
+      title: "Section 3",
+      type: "object",
+      properties: {
+        opted_in_operation: {
+          type: "object",
+          properties: {
+            meets_section_3_emissions_requirements: {
+              type: "boolean",
+            },
+            meets_electricity_import_operation_criteria: {
+              type: "boolean",
+            },
+            meets_entire_operation_requirements: {
+              type: "boolean",
+            },
+            meets_section_6_emissions_requirements: {
+              type: "boolean",
+            },
+            meets_naics_code_11_22_562_classification_requirements: {
+              type: "boolean",
+            },
+            meets_producing_gger_schedule_a1_regulated_product: {
+              type: "boolean",
+            },
+            meets_reporting_and_regulated_obligations: {
+              type: "boolean",
+            },
+            meets_notification_to_director_on_criteria_change: {
+              type: "boolean",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
 const formData = {
   name: "Operation 3",
   type: "Single Facility Operation",
+};
+
+const optInFormData = {
+  name: "Operation 5",
+  type: "Single Facility Operation",
+  registration_purposes: ["Opted-in Operation"],
+  registration_purpose: "Opted-in Operation",
+  opted_in_operation: {
+    meets_section_3_emissions_requirements: true,
+    meets_electricity_import_operation_criteria: true,
+    meets_entire_operation_requirements: true,
+    meets_section_6_emissions_requirements: true,
+    meets_naics_code_11_22_562_classification_requirements: true,
+    meets_producing_gger_schedule_a1_regulated_product: true,
+    meets_reporting_and_regulated_obligations: true,
+    meets_notification_to_director_on_criteria_change: true,
+  },
+  opt_in: true,
 };
 
 const operationId = "8be4c7aa-6ab3-4aad-9206-0ef914fea063";
@@ -169,5 +248,68 @@ describe("the OperationInformationForm component", () => {
     ).not.toBeInTheDocument();
     // still show the cancel button
     expect(screen.getByRole("button", { name: "Cancel" })).toBeVisible();
+  });
+
+  it("should edit and submit opt-in operation details in the form", async () => {
+    render(
+      <OperationInformationForm
+        formData={optInFormData}
+        schema={testSchemaWithOpt}
+        operationId={operationId}
+      />,
+    );
+
+    await act(async () => {
+      // Click the Edit button
+      screen.getByRole("button", { name: "Edit" }).click();
+    });
+    await act(async () => {
+      // Grab all radio buttons
+      const allNoRadioButtons: HTMLInputElement[] = screen.getAllByRole(
+        "radio",
+        {
+          name: /no/i,
+        },
+      );
+
+      // Change all to no
+      allNoRadioButtons.forEach((radioBtn) => {
+        radioBtn.click();
+      });
+      allNoRadioButtons.forEach((radioBtn) => {
+        expect(radioBtn).toBeChecked();
+      });
+
+      // Click the Submit button
+      screen.getByRole("button", { name: "Submit" }).click();
+    });
+
+    expect(actionHandler).toHaveBeenCalledTimes(3);
+    expect(actionHandler).toHaveBeenCalledWith(
+      `registration/v2/operations/${operationId}/registration/opted-in-operation-detail`,
+      "PUT",
+      "",
+      {
+        body: JSON.stringify({
+          meets_section_3_emissions_requirements: false,
+          meets_electricity_import_operation_criteria: false,
+          meets_entire_operation_requirements: false,
+          meets_section_6_emissions_requirements: false,
+          meets_naics_code_11_22_562_classification_requirements: false,
+          meets_producing_gger_schedule_a1_regulated_product: false,
+          meets_reporting_and_regulated_obligations: false,
+          meets_notification_to_director_on_criteria_change: false,
+        }),
+      },
+    );
+
+    // Expect the form to contain no for each opt in criteria
+    await act(async () => {
+      const allNoText = screen.getAllByText(/no/i);
+
+      allNoText.forEach((radioValue) => {
+        expect(radioValue).toHaveTextContent(/no/i);
+      });
+    });
   });
 });
