6d0bc320a0b6d0222faf33f30fd3f47c7ca80617
SeSo <sobhani.sepehr@gmail.com>
2024-10-09 11:03:44 -0700

    chore: add separate dashboard data for external admins
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/service/data_access_service/dashboard_service.py
+++ bc_obps/service/data_access_service/dashboard_service.py
@@ -1,4 +1,6 @@
 from typing import Optional
+from uuid import UUID
+
 from django.db.models import QuerySet
 from django.db.models import Q
 from common.models import DashboardData
@@ -6,17 +8,22 @@ from common.schema.v1 import DashboardDataSchemaOut
 
 import logging
 
+from service.data_access_service.user_service import UserDataAccessService
+
 logger = logging.getLogger(__name__)
 
 
 class DashboardDataService:
     @classmethod
-    def get_dashboard_data_by_name_for_role(cls, dashboard: str, role: Optional[str] = None) -> QuerySet[DashboardData]:
+    def get_dashboard_data_by_name_for_role(
+        cls, dashboard: str, user_guid: UUID, role: Optional[str] = None
+    ) -> QuerySet[DashboardData]:
         """
         Fetches dashboard data filtered by dashboard name and user role.
 
         Args:
             dashboard (str): The name of the dashboard to filter by. Use "all" to fetch all dashboards.
+            user_guid (UUID): The user guid to filter by.
             role (Optional[str]): The user role to filter by. The role should be contained within the access_roles.
 
         Returns:
@@ -28,6 +35,12 @@ class DashboardDataService:
             if dashboard == "all":
                 return query
             else:
+                # Check if the user is an approved admin user operator
+                if role == "industry_user":
+                    approved_admin: bool = UserDataAccessService.is_user_an_approved_admin_user_operator(user_guid)[
+                        "approved"
+                    ]
+                    role = "industry_user_admin" if approved_admin else "industry_user"
                 query = query.filter(Q(data__dashboard=dashboard) & Q(data__access_roles__contains=role))
 
             return query
