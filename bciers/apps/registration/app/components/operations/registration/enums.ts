c0690743bc3893b0782379237d6eaef017c992a1
SeSo <sobhani.sepehr@gmail.com>
2024-10-11 12:52:53 -0700

    feat: show registration purpose help text
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/registration/app/components/operations/registration/enums.ts
+++ bciers/apps/registration/app/components/operations/registration/enums.ts
@@ -26,3 +26,20 @@ export enum RegistrationPurposes {
   ELECTRICITY_IMPORT_OPERATION = "Electricity Import Operation",
   POTENTIAL_REPORTING_OPERATION = "Potential Reporting Operation",
 }
+
+export const RegistrationPurposeHelpText: {
+  [key in RegistrationPurposes]: string;
+} = {
+  [RegistrationPurposes.REPORTING_OPERATION]:
+    "A Reporting Operation is an industrial operation that emits greater than or equal to 10 000 tCO2e per year and must submit an emission report, but does not produce a regulated product.",
+  [RegistrationPurposes.OBPS_REGULATED_OPERATION]:
+    "An OBPS Regulated Operation is a type of reporting operation that produces a regulated product and emits greater than or equal to 10 000 tCO2e per year. This option should be selected if you are a registering as a mandatory participant in the B.C. OBPS.",
+  [RegistrationPurposes.OPTED_IN_OPERATION]:
+    "An Opted-in Operation is an industrial operation than produces a regulated product and emits less than 10 000 tCO2e per year. These operations may apply to voluntarily participate in the B.C. OBPS. This option should be selected if you are registering and applying to be an Opted-In Operation.",
+  [RegistrationPurposes.NEW_ENTRANT_OPERATION]:
+    "This option should be selected if you are registering and applying to be a New Entrant.",
+  [RegistrationPurposes.ELECTRICITY_IMPORT_OPERATION]:
+    "An Electricity Import Operation is an industrial operation that imports electricity from an electricity generating facility located outside British Columbia and is the first point of delivery in British Columbia.",
+  [RegistrationPurposes.POTENTIAL_REPORTING_OPERATION]:
+    "A Potential Reporting Operation is an industrial operation that did not submit an emission report for the previous year, but has forecast it's emissions to be greater than or equal to 10 000 tCO2e for the current reporting period. Potential Reporting Operations must register, but do not need to submit an emission report unless they actually emit greater than or equal to 10 000 tCO2e in the current reporting period.",
+};
