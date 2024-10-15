7fe420cc2f9f4a7226e2bbb0a6cee08b6c05c8ec
acatchpole <adamantiumite@gmail.com>
2024-10-15 14:48:28 -0700

    chore - refactored form builder to have methodology data nested in its own obj

--- bc_obps/service/form_builder_service.py
+++ bc_obps/service/form_builder_service.py
@@ -60,7 +60,7 @@ def handle_methodologies(
         reporting_fields = fetched_config_map[key]
 
         # Create methodology object
-        methodology_object: Dict[str, Dict] = {"properties": {"methodology": {"enum": [methodology_name]}}}
+        methodology_object: Dict[str, Dict] =  {"properties": {"methodology": {"enum": [methodology_name]}}}
 
         # Check for custom schema
         if config_element_for_methodology.custom_methodology_schema_id:
@@ -87,8 +87,8 @@ def handle_methodologies(
         methodology_one_of['methodology']['oneOf'].append(methodology_object)
 
     # Update gas_type_one_of with computed values
-    gas_type_one_of['gasType']['oneOf'][index]['properties']['methodology']['enum'] = methodology_enum
-    gas_type_one_of['gasType']['oneOf'][index]['dependencies'] = methodology_one_of
+    gas_type_one_of['gasType']['oneOf'][index]['properties']['methodology']['properties']['methodology']['enum'] = methodology_enum
+    gas_type_one_of['gasType']['oneOf'][index]['properties']['methodology']['dependencies'] = methodology_one_of
 
 
 def handle_gas_types(
@@ -152,7 +152,10 @@ def handle_gas_types(
             {
                 "properties": {
                     "gasType": {"enum": [gas_type_chemical_formula]},
-                    "methodology": {"title": "Methodology", "type": "string", "enum": []},
+                    "methodology": { "type": "object", "properties": { 
+                        "methodology": {"title": "Methodology", "type": "string", "enum": [] }
+                        }
+                    },
                 }
             }
         )
