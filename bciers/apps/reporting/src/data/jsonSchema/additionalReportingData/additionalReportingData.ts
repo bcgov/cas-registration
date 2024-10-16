b76e1a0f9a760403a65bbd0e6402282e15573178
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-10-16 08:11:38 -0700

    chore:added more fields

--- bciers/apps/reporting/src/data/jsonSchema/additionalReportingData/additionalReportingData.ts
+++ bciers/apps/reporting/src/data/jsonSchema/additionalReportingData/additionalReportingData.ts
@@ -5,22 +5,42 @@ import SectionFieldTemplate from "@bciers/components/form/fields/SectionFieldTem
 import { CapturedEmmissionsInfo } from "@reporting/src/data/jsonSchema/additionalReportingData/additionalMessage";
 import { RadioWidget } from "@bciers/components/form/widgets";
 
-export const additionalReportingDataSchema: RJSFSchema = {
+export const baseSchema: RJSFSchema = {
   type: "object",
   title: "Additional Reporting Data",
   properties: {
-    capture_emissions_heading: {
-      type: "object",
-      readOnly: true,
-      title: "Captured emissions (Optional)",
-    },
-    purpose_note: {
-      type: "object",
-      readOnly: true,
-    },
-    capture_emissions: {
-      type: "boolean",
-    },
+    purpose_note: { type: "object", readOnly: true },
+    capture_emissions: { type: "boolean" },
+  },
+};
+
+// Conditional fields to be added when `capture_emissions` is true
+export const conditionalFields: RJSFSchema["properties"] = {
+  capture_type: {
+    type: "string",
+    title: "Capture type",
+    enum: ["On-site use", "On-site sequestration", "Off-site transfer"],
+  },
+  emissions_on_site_use: {
+    type: "string",
+    title: "Emissions (t) captured for on-site use",
+  },
+  emissions_on_site_sequestration: {
+    type: "string",
+    title: "Emissions (t) captured for on-site sequestration",
+  },
+  emissions_off_site_transfer: {
+    type: "string",
+    title: "Emissions (t) captured for off-site transfer",
+  },
+  additional_data: {
+    type: "object",
+    readOnly: true,
+    title: "Additional data",
+  },
+  electricity_generated: {
+    type: "string",
+    title: "Electricity generated (Optional for reporting only operations)",
   },
 };
 const customClassNamesForRadioWidget =
@@ -29,19 +49,19 @@ const customClassNamesForRadioWidget =
 export const additionalReportingDataUiSchema = {
   "ui:FieldTemplate": FieldTemplate,
   "ui:classNames": "form-heading-label",
-
-  capture_emissions_heading: {
-    "ui:FieldTemplate": SectionFieldTemplate,
-  },
+  additional_data: { "ui:FieldTemplate": SectionFieldTemplate },
   purpose_note: {
     "ui:FieldTemplate": TitleOnlyFieldTemplate,
-    "ui:style": { margin: 0, fontStyle: "italic", fontSize: 16 },
-
     "ui:title": CapturedEmmissionsInfo,
   },
   capture_emissions: {
     "ui:title": "Did you capture emissions?",
     "ui:widget": RadioWidget,
     "ui:classNames": customClassNamesForRadioWidget,
+  },
+  capture_type: {
+    "ui:widget": "select",
+    "ui:options": { style: { width: "100%", textAlign: "justify" } },
+    "ui:placeholder": "Capture type",
   },
 };
