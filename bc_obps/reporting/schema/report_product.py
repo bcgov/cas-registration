6e09e422f18e70415111dc00712fafd9a0b269bb
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-10-17 14:05:20 -0700

    chore: get production data endpoint

--- bc_obps/reporting/schema/report_product.py
+++ bc_obps/reporting/schema/report_product.py
@@ -1,9 +1,34 @@
-from ninja import ModelSchema
+from typing import List
+from ninja import Field, ModelSchema, Schema
+from registration.schema.v1.regulated_products import RegulatedProductSchema
 from reporting.models.report_product import ReportProduct
 
 
-class ReportProductIn(ModelSchema):
+class ReportProductSchema(ModelSchema):
     class Meta:
         model = ReportProduct
-        fields = "__all__"
-        fields_optional = "__all__"
+        fields = [
+            "unit",
+            "annual_production",
+            "production_data_apr_dec",
+            "production_methodology",
+            "storage_quantity_start_of_period",
+            "storage_quantity_end_of_period",
+            "quantity_sold_during_period",
+            "quantity_throughput_during_period",
+        ]
+        fields_optional = ['unit']
+
+
+class ReportProductSchemaIn(ReportProductSchema):
+    product_id: int
+
+
+class ReportProductSchemaOut(ReportProductSchema):
+    product_id: int = Field(..., alias="product.id")
+    product_name: str = Field(..., alias="product.name")
+
+
+class ProductionDataOut(Schema):
+    report_products: List[ReportProductSchemaOut]
+    allowed_products: List[RegulatedProductSchema]
