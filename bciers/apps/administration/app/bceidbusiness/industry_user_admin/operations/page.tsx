ef84308b01a797adb0d5cb5716fcc0f6ca6c2761
SeSo <sobhani.sepehr@gmail.com>
2024-10-18 08:36:15 -0700

    feat: internal user can view an operation
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/administration/app/bceidbusiness/industry_user_admin/operations/page.tsx
+++ bciers/apps/administration/app/bceidbusiness/industry_user_admin/operations/page.tsx
@@ -1,6 +1,6 @@
 // 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
 import { OperationsSearchParams } from "@/administration/app/components/operations/types";
-import OperationDataGridPage from "@/administration/app/components/operations/OperationDataGridPage";
+import OperationDataGridPage from "@/administration/app/components/operations/Operations";
 import { ExternalUserOperationDataGridLayout } from "@/administration/app/components/operations/OperationLayouts";
 
 export default async function Page({
--- bciers/apps/administration/app/idir/cas_analyst/operations/[operationId]/page.tsx
+++ bciers/apps/administration/app/idir/cas_analyst/operations/[operationId]/page.tsx
@@ -1,5 +1,16 @@
-// 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
+import OperationInformationPage from "@/administration/app/components/operations/OperationInformationPage";
+import { UUID } from "crypto";
+import { Suspense } from "react";
+import Loading from "@bciers/components/loading/SkeletonForm";
 
-export default async function Page() {
-  return <div>TBD</div>;
+export default async function Page({
+  params: { operationId },
+}: Readonly<{
+  params: { operationId: UUID };
+}>) {
+  return (
+    <Suspense fallback={<Loading />}>
+      <OperationInformationPage operationId={operationId} />
+    </Suspense>
+  );
 }
