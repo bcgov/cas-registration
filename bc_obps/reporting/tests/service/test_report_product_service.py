4552da8070eaeac8b6bbf8b6c9c7c799f17a8f6b
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-10-18 15:43:31 -0700

    chore: django schemas

---
+++ bc_obps/reporting/tests/service/test_report_product_service.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+pytestmark = pytest.mark.django_db
+
+
+class TestReportingYearService:
+    def test_get_current_reporting_year(self, mock_datetime):
+        raise
+
+    # Test the created_at and created_by
