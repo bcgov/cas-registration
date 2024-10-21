60c9c1a08caf907279a29d9fb73e4ce51fcee6cc
shon-button <shon@button.is>
2024-10-21 11:12:16 -0400

    test: endpoint and service

--- bc_obps/service/operation_service_v2.py
+++ bc_obps/service/operation_service_v2.py
@@ -63,12 +63,12 @@ class OperationServiceV2:
         )
 
     @classmethod
-    def check_current_users_registered_operation(cls, operator_guid: UUID) -> bool:
+    def check_current_users_registered_operation(cls, operator_id: UUID) -> bool:
         """
         Returns True if the userOperator's operator has at least one operation with status 'Registered', False otherwise.
         """
         return Operation.objects.filter(
-            operator_id=operator_guid, 
+            operator_id=operator_id, 
             status="Registered" 
         ).exists()
     
