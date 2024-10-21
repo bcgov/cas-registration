ebe772676b6f0a2ec761dd73df71d12d93005422
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-10-21 14:20:57 -0700

    chore: renaming migrations

--- bc_obps/reporting/migrations/0021_report_additional_data.py
+++ bc_obps/reporting/migrations/0021_report_additional_data.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('registration', '0045_V1_11_0'),
-        ('reporting', '0021_reportproduct_and_more'),
+        ('reporting', '0020_report_person_responsible'),
     ]
 
     operations = [
