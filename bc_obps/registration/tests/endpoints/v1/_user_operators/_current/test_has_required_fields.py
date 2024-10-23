5df6bf7afd5c953efe20a2c94976a166ffd3f8ae
shon-button <shon@button.is>
2024-10-23 14:23:50 -0400

    chore: cleanup

--- bc_obps/registration/tests/endpoints/v1/_user_operators/_current/test_has_required_fields.py
+++ bc_obps/registration/tests/endpoints/v1/_user_operators/_current/test_has_required_fields.py
@@ -1,5 +1,6 @@
 from registration.tests.utils.helpers import CommonTestSetup, TestUtils
 
+
 class TestGetCurrentUserOperatorHasRequiredFields(CommonTestSetup):
     endpoint = CommonTestSetup.base_endpoint + "user-operators/current/has-required-fields"
 
