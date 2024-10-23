b14351c5a7a65a2fc2ae68982da9fcc80e8811db
Brianna Cerkiewicz <brianna@button.is>
2024-10-23 08:10:38 -0700

    chore: fix typing for operator id

--- bciers/apps/administration/app/bceidbusiness/industry_user/select-operator/(request-access)/confirm/[id]/page.tsx
+++ bciers/apps/administration/app/bceidbusiness/industry_user/select-operator/(request-access)/confirm/[id]/page.tsx
@@ -1,9 +1,10 @@
 import SelectOperatorConfirmPage from "@/administration/app/components/userOperators/SelectOperatorConfirmPage";
 import { Suspense } from "react";
 import Loading from "@bciers/components/loading/SkeletonForm";
+import { UUID } from "crypto";
 export default function Page({
   params: { id },
-}: Readonly<{ params: { id: string } }>) {
+}: Readonly<{ params: { id: UUID } }>) {
   return (
     <Suspense fallback={<Loading />}>
       <SelectOperatorConfirmPage id={id} />
