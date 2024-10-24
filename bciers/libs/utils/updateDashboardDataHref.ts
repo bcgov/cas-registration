8615b4f1bdbe284fcbbfddfb52a92754efd76843
SeSo <sobhani.sepehr@gmail.com>
2024-10-24 09:46:44 -0700

    chore: tweak `updateDashboardDataHref` function to accept functions
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/libs/utils/updateDashboardDataHref.ts
+++ bciers/libs/utils/updateDashboardDataHref.ts
@@ -1,22 +1,24 @@
-// 🛠️ Function to update dashboard JSON data by replacing a static placeholder in href with a dynamic value, and optionally appending a title.
 import { ContentItem } from "@bciers/types/tiles";
 
 /**
- * Replaces a static href value in a list of dashboard content items with a dynamic value and optionally appends a title.
+ * Replaces a static href value in a list of dashboard content items with a dynamic value based on conditions
+ * and optionally appends a title based on conditions.
  *
  * @param items - Array of ContentItem objects containing dashboard data.
  * @param replaceValue - The static part of the href to be replaced.
- * @param newValue - The dynamic value to replace the static part of the href.
- * @param title - An optional string to append to the href after replacement (default is an empty string).
+ * @param getNewValue - A function that returns the dynamic value based on the current item.
+ * @param getTitle - A function that returns the title based on the current item.
  * @returns A new array of ContentItems with updated href values.
  */
 const updateDashboardDataHref = (
   items: ContentItem[],
   replaceValue: string,
-  newValue: string,
-  title: string = "",
+  getNewValue: (item: ContentItem) => string,
+  getTitle: (item: ContentItem) => string,
 ): ContentItem[] => {
   return items.map((item) => {
+    const newValue = getNewValue(item); // Determine the new value based on the item's properties
+    const title = getTitle(item); // Determine the title based on the item's properties
     return {
       ...item,
       href: item.href.replace(replaceValue, newValue) + title,
