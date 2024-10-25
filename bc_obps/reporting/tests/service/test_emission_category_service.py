21064d9516ecf55bbf983305678a262d528ae4b0
Dylan Leard <dylan@button.is>
2024-10-25 14:59:12 -0700

    test: add more tests for emission_category_service

--- bc_obps/reporting/tests/service/test_emission_category_service.py
+++ bc_obps/reporting/tests/service/test_emission_category_service.py
@@ -4,6 +4,7 @@ from reporting.models.report_emission import ReportEmission
 from reporting.models.report_source_type import ReportSourceType
 from reporting.service.emission_category_service import EmissionCategoryService
 from reporting.tests.service.test_report_activity_save_service.infrastructure import TestInfrastructure
+from decimal import Decimal, getcontext
 from model_bakery.baker import make
 
 
@@ -58,12 +59,13 @@ class TestEmissionCategoryService(TestCase):
             ]
         )
 
-        flaring_return_value = EmissionCategoryService.get_flaring_emission_category_total(
-            report_activity.facility_report.id
+        flaring_return_value = EmissionCategoryService.get_total_emissions_by_emission_category(
+            report_activity.facility_report.id, 1
         )
-        assert flaring_return_value == 200.41510
+        assert flaring_return_value == Decimal('200.41510')
 
-    def test_ignores_other_categ(self):
+    def test_returns_correct_value_all_categories(self):
+        getcontext().prec = 5  # Set precision to 5 to match our precision set in the report_emission model manager
         test_infrastructure = TestInfrastructure.build()
         act_st_1 = test_infrastructure.make_activity_source_type(
             source_type__json_key="sourceTypeWithUnitAndFuel",
@@ -92,7 +94,7 @@ class TestEmissionCategoryService(TestCase):
             report_fuel=report_fuel,
             report_source_type=report_source_type,
             report_version=test_infrastructure.report_version,
-            json_data={"emission": 101},
+            json_data={"emission": 100.00001},
         )
 
         report_emission_2 = make(
@@ -100,11 +102,60 @@ class TestEmissionCategoryService(TestCase):
             report_fuel=report_fuel,
             report_source_type=report_source_type,
             report_version=test_infrastructure.report_version,
-            json_data={"emission": 99.4151},
+            json_data={"emission": 200.00002},
         )
 
-        report_emission.emission_categories.set([1])
-        report_emission_2.emission_categories.set([1])
+        report_emission_3 = make(
+            ReportEmission,
+            report_fuel=report_fuel,
+            report_source_type=report_source_type,
+            report_version=test_infrastructure.report_version,
+            json_data={"emission": 300.00003},
+        )
 
-        return_value = EmissionCategoryService.get_flaring_emission_category_total(report_activity.facility_report.id)
-        assert return_value == 200.41510
+        report_emission_4 = make(
+            ReportEmission,
+            report_fuel=report_fuel,
+            report_source_type=report_source_type,
+            report_version=test_infrastructure.report_version,
+            json_data={"emission": 400.00004},
+        )
+
+        report_emission.emission_categories.set([1, 10])
+        report_emission_2.emission_categories.set([1, 12])
+        flaring_value = EmissionCategoryService.get_total_emissions_by_emission_category(
+            report_activity.facility_report.id, 1
+        )
+        woody_biomass_value = EmissionCategoryService.get_total_emissions_by_emission_category(
+            report_activity.facility_report.id, 10
+        )
+        excluded_non_biomass_value = EmissionCategoryService.get_total_emissions_by_emission_category(
+            report_activity.facility_report.id, 12
+        )
+        assert flaring_value == Decimal('300.00003')
+        assert woody_biomass_value == Decimal('100.00001')
+        assert excluded_non_biomass_value == Decimal('200.00002')
+
+        report_emission_3.emission_categories.set([2])
+        report_emission_4.emission_categories.set([2])
+        fugitive_value = EmissionCategoryService.get_total_emissions_by_emission_category(
+            report_activity.facility_report.id, 2
+        )
+        assert fugitive_value == Decimal('700.00007')
+
+        all_categories = EmissionCategoryService.get_all_category_totals(report_activity.facility_report.id)
+        assert all_categories == {
+            'flaring': Decimal('300.00003'),
+            'fugitive': Decimal('700.00007'),
+            'industrial_process': 0,
+            'onsite': 0,
+            'stationary': 0,
+            'venting_useful': 0,
+            'venting_non_useful': 0,
+            'waste': 0,
+            'wastewater': 0,
+            'woody_biomass': Decimal('100.00001'),
+            'excluded_biomass': 0,
+            'excluded_non_biomass': Decimal('200.00002'),
+            'lfo_excluded': 0,
+        }
