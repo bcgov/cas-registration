6f688b77a936a3f48050583f20d25a62ef4d03cc
Dylan Leard <dylan@button.is>
2024-10-25 16:50:51 -0700

    feat: add facility emission summary page

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/[version_id]/facilities/[facility_id]/summary/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/[version_id]/facilities/[facility_id]/summary/page.tsx
@@ -1,3 +1,15 @@
-export default async function Page() {
-  return <>Facility Emission Summary Page TBD</>;
+import { Suspense } from "react";
+import FacilityEmissionSummary from "../../../../../../../components/facility/FacilityEmissionSummary";
+
+export default async function Page(router: any) {
+  return (
+    <>
+      <Suspense fallback="Loading Schema">
+        <FacilityEmissionSummary
+          versionId={parseInt(router.params?.version_id)}
+          facilityId={router.params?.facility_id}
+        />
+      </Suspense>
+    </>
+  );
 }
