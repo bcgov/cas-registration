84ad07bf91c54905d4cb3582982254b9abe818e9
Dylan Leard <dylan@button.is>
2024-10-28 16:17:27 -0700

    chore: add calculation function to service for reporting only emissions

--- bc_obps/reporting/service/emission_category_service.py
+++ bc_obps/reporting/service/emission_category_service.py
@@ -9,7 +9,7 @@ class EmissionCategoryService:
 from reporting.models.report_emission import ReportEmission
 from decimal import Decimal
 from django.db.models import Sum
-from typing import Dict
+from typing import Dict, List
 
 
 class EmissionCategoryService:
@@ -26,6 +26,26 @@ class EmissionCategoryService:
         category_sum = records.aggregate(emission_sum=Sum('emission'))
 
         return 0 if category_sum['emission_sum'] is None else category_sum['emission_sum']
+
+    @staticmethod
+    def get_reporting_only_emissions(facility_report_id: int) -> Decimal:
+        # Fugitive, fuel_excluded, other_excluded
+        category_ids = [2, 10, 11, 12, 13, 14]
+        # We will ignore a report_emission record with the .exclude() below if we have already its emissions to the total (don't count an emission more than once)
+        traversed_report_emission_ids: List[int] = []
+        reporting_only_total = Decimal('0')
+        for category_id in category_ids:
+            records = ReportEmission.objects_with_decimal_emissions.filter(
+                report_source_type__report_activity__facility_report_id=facility_report_id,
+                emission_categories__id=category_id,
+            ).exclude(pk__in=traversed_report_emission_ids)
+            category_sum = records.aggregate(emission_sum=Sum('emission'))
+            if category_sum['emission_sum'] is not None:
+                reporting_only_total += Decimal(category_sum['emission_sum'])
+            # Add all IDs of the report_emission records that contributed to the sum to the ignore list
+            traversed_report_emission_ids = traversed_report_emission_ids + list(records.values_list('id', flat=True))
+
+        return reporting_only_total
 
     @classmethod
     def get_all_category_totals(cls, facility_report_id: int) -> Dict[str, Decimal | int]:
@@ -58,6 +78,21 @@ class EmissionCategoryService:
         # OTHER EXCLUDED
         lfo_excluded_total = EmissionCategoryService.get_total_emissions_by_emission_category(facility_report_id, 13)
 
+        # ATTRIBUTABLE EMISSIONS
+        attributable_for_reporting = (
+            flaring_total
+            + fugitive_total
+            + industrial_process_total
+            + onsite_transportation_total
+            + stationary_combustion_total
+            + venting_useful_total
+            + venting_non_useful_total
+            + waste_total
+            + wastewater_total
+        )
+        attributable_for_threshold = attributable_for_reporting - woody_biomass_total
+        reporting_only = EmissionCategoryService.get_reporting_only_emissions(facility_report_id)
+
         return {
             'flaring': flaring_total,
             'fugitive': fugitive_total,
@@ -72,4 +107,7 @@ class EmissionCategoryService:
             'excluded_biomass': excluded_biomass_total,
             'excluded_non_biomass': excluded_non_biomass_total,
             'lfo_excluded': lfo_excluded_total,
+            'attributable_for_reporting': attributable_for_reporting,
+            'attributable_for_threshold': attributable_for_threshold,
+            'reporting_only': reporting_only,
         }
