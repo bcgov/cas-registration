6938e6437d72a12461788e82891d685fe928b4a6
Dylan Leard <dylan@button.is>
2024-10-29 11:33:59 -0700

    chore: use server component to fetch data instead of useEffect

--- bciers/apps/reporting/src/app/components/facility/FacilityEmissionSummary.tsx
+++ bciers/apps/reporting/src/app/components/facility/FacilityEmissionSummary.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Box, Button } from "@mui/material";
 import MultiStepHeader from "@bciers/components/form/components/MultiStepHeader";
 import FormBase from "@bciers/components/form/FormBase";
@@ -9,63 +9,45 @@ import {
   facilityEmissionSummaryUiSchema,
   facilityEmissionSummarySchema,
 } from "@reporting/src/data/jsonSchema/facilityEmissionSummary";
-import { actionHandler } from "@bciers/actions";
 import { UUID } from "crypto";
 import Link from "next/link";
 
 interface Props {
   versionId: number;
   facilityId: UUID;
+  summaryFormData: {
+    attributableForReporting: string;
+    attributableForReportingThreshold: string;
+    reportingOnlyEmission: string;
+    emissionCategories: {
+      flaring: string;
+      fugitive: string;
+      industrialProcess: string;
+      onSiteTransportation: string;
+      stationaryCombustion: string;
+      ventingUseful: string;
+      ventingNonUseful: string;
+      waste: string;
+      wastewater: string;
+    };
+    fuelExcluded: {
+      woodyBiomass: string;
+      excludedBiomass: string;
+      excludedNonBiomass: string;
+    };
+    otherExcluded: {
+      lfoExcluded: string;
+      fogExcluded: string; // To be handled once we implement a way to capture FOG emissions
+    };
+  };
 }
-
-const getsummaryData = async (versionId: number, facilityId: UUID) => {
-  return actionHandler(
-    `reporting/report-version/${versionId}/facility-report/${facilityId}/emission-summary`,
-    "GET",
-    `reporting/report-version/${versionId}/facility-report/${facilityId}/emission-summary`,
-  );
-};
 
 const FacilityEmissionSummary: React.FC<Props> = ({
   versionId,
   facilityId,
+  summaryFormData,
 }) => {
-  const [summaryData, setSummaryData] = useState({} as any);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedSummaryData = await getsummaryData(versionId, facilityId);
-      setSummaryData(fetchedSummaryData);
-    };
-
-    fetchData();
-  }, [versionId, facilityId]);
-
-  const formData = {
-    attributableForReporting: summaryData.attributable_for_reporting,
-    attributableForReportingThreshold: summaryData.attributable_for_threshold,
-    reportingOnlyEmission: summaryData.reporting_only,
-    emissionCategories: {
-      flaring: Number(summaryData.flaring),
-      fugitive: Number(summaryData.fugitive),
-      industrialProcess: Number(summaryData.industrial_process),
-      onSiteTransportation: Number(summaryData.onsite),
-      stationaryCombustion: Number(summaryData.stationary),
-      ventingUseful: Number(summaryData.venting_useful),
-      ventingNonUseful: Number(summaryData.venting_non_useful),
-      waste: Number(summaryData.waste),
-      wastewater: Number(summaryData.wastewater),
-    },
-    fuelExcluded: {
-      woodyBiomass: Number(summaryData.woody_biomass),
-      excludedBiomass: Number(summaryData.excluded_biomass),
-      excludedNonBiomass: Number(summaryData.excluded_non_biomass),
-    },
-    otherExcluded: {
-      lfoExcluded: Number(summaryData.lfo_excluded),
-      fogExcluded: 0, // To be handled once we implement a way to capture FOG emissions
-    },
-  };
+  console.log(summaryFormData);
 
   const customStepNames = [
     "Operation Information",
@@ -92,7 +74,7 @@ const FacilityEmissionSummary: React.FC<Props> = ({
           <FormBase
             schema={facilityEmissionSummarySchema}
             uiSchema={facilityEmissionSummaryUiSchema}
-            formData={formData}
+            formData={summaryFormData}
           >
             <Box display="flex" justifyContent="space-between" mt={3}>
               <Link href={backRef} passHref>
