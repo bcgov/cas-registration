90a45241bcbf4045f42c8bd58ea3991498b58b8a
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-10-30 12:44:22 -0700

    chore: added Add activity button

--- bciers/apps/reporting/src/data/jsonSchema/nonAttributableEmissions/nonAttributableEmissions.ts
+++ bciers/apps/reporting/src/data/jsonSchema/nonAttributableEmissions/nonAttributableEmissions.ts
@@ -3,6 +3,7 @@ import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
 import { TitleOnlyFieldTemplate } from "@bciers/components/form/fields";
 import { RadioWidget, SelectWidget } from "@bciers/components/form/widgets";
 import { NonAttributableEmmissionsInfo } from "@reporting/src/data/jsonSchema/nonAttributableEmissions/additionalMessage";
+import NestedArrayFieldTemplate from "@bciers/components/form/fields/NestedArrayFieldTemplate";
 
 export const nonAttributableEmissionSchema: RJSFSchema = {
   type: "object",
@@ -19,28 +20,33 @@ export const nonAttributableEmissionSchema: RJSFSchema = {
 export const nonAttributableEmissionUiSchema = {
   "ui:FieldTemplate": FieldTemplate,
   "ui:classNames": "form-heading-label",
-  "ui:order": [
-    "info_note",
-    "emissions_exceeded",
-    "activity",
-    "source_type",
-    "emission_category",
-    "gas_type",
-  ],
+  "ui:order": ["info_note", "emissions_exceeded", "activities"],
   info_note: {
     "ui:FieldTemplate": TitleOnlyFieldTemplate,
     "ui:title": NonAttributableEmmissionsInfo,
   },
   emissions_exceeded: { "ui:widget": RadioWidget },
-  emission_category: {
-    "ui:widget": SelectWidget,
-    "ui:placeholder": "Select Emissions Category",
-    "ui:options": { style: { width: "100%", textAlign: "justify" } },
-  },
-  gas_type: {
-    "ui:widget": "MultiSelectWidget",
-    "ui:placeholder": "Select Gas Type",
-    uniqueItems: true,
+  activities: {
+    "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:options": {
+      arrayAddLabel: "Add Activity",
+      padding: "p-2",
+      label: false,
+    },
+    items: {
+      "ui:order": ["activity", "source_type", "emission_category", "gas_type"],
+      emission_category: {
+        "ui:widget": SelectWidget,
+        "ui:placeholder": "Select Emissions Category",
+        "ui:options": { style: { width: "100%", textAlign: "justify" } },
+      },
+      gas_type: {
+        "ui:widget": SelectWidget,
+        "ui:placeholder": "Select Gas Type",
+      },
+    },
   },
 };
 
@@ -53,28 +59,35 @@ export const generateUpdatedSchema = (
     dependencies: {
       emissions_exceeded: {
         oneOf: [
-          { properties: { emissions_exceeded: { enum: [false] } } },
+          {
+            properties: { emissions_exceeded: { enum: [false] } },
+          },
           {
             properties: {
               emissions_exceeded: { enum: [true] },
-              activity: { type: "string", title: "Activity Name" },
-              source_type: { type: "string", title: "Source Type" },
-              gas_type: {
+              activities: {
                 type: "array",
-                title: "Gas Type (Optional)",
                 items: {
-                  type: "number",
-                  enum: gasTypes.map((gas) => gas.id),
-                  enumNames: gasTypes.map((gas) => gas.chemical_formula),
+                  type: "object",
+                  properties: {
+                    activity: { type: "string", title: "Activity Name" },
+                    source_type: { type: "string", title: "Source Type" },
+                    gas_type: {
+                      type: "string",
+                      title: "Gas Type (Optional)",
+
+                      enum: gasTypes.map((gas) => gas.chemical_formula),
+                      // enumNames: gasTypes.map((gas) => gas.chemical_formula),
+                    },
+                    emission_category: {
+                      type: "string",
+                      title: "Emission Category (Optional)",
+                      enum: emissionCategories.map(
+                        (category) => category.category_name,
+                      ),
+                    },
+                  },
                 },
-                uniqueItems: true,
-              },
-              emission_category: {
-                type: "string",
-                title: "Emission Category (Optional)",
-                enum: emissionCategories.map(
-                  (category) => category.category_name,
-                ),
               },
             },
           },
