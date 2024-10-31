0a01187013cfab9dccb29fb50c84ac3f2dabf5d8
acatchpole <adamantiumite@gmail.com>
2024-10-31 10:38:36 -0700

    feature: storage of petroleum products activity page

---
+++ bciers/apps/reporting/src/app/components/activities/uiSchemas/storageOfPetroleumProductsUiSchema.ts
@@ -0,0 +1,67 @@
+import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
+import NestedArrayFieldTemplate from "@bciers/components/form/fields/NestedArrayFieldTemplate";
+import SourceTypeBoxTemplate from "@bciers/components/form/fields/SourceTypeBoxTemplate";
+import CheckboxWidgetLeft from "@bciers/components/form/widgets/CheckboxWidgetLeft";
+import InlineFieldTemplate from "@bciers/components/form/fields/InlineFieldTemplate";
+
+const uiSchema = {
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:classNames": "form-heading-label",
+  aboveGroundStorageTanks: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:widget": CheckboxWidgetLeft,
+    "ui:options": {
+      label: false,
+    },
+  },
+  sourceTypes: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:options": {
+      label: false,
+    },
+    aboveGroundStorageTanks: {
+      "ui:FieldTemplate": SourceTypeBoxTemplate,
+      emissions: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+        "ui:FieldTemplate": FieldTemplate,
+        "ui:options": {
+          arrayAddLabel: "Add Emission",
+          label: false,
+          title: "Emission",
+          padding: "p-2",
+        },
+        items: {
+          "ui:order": [
+            "gasType",
+            "emission",
+            "equivalentEmission",
+            "methodology",
+          ],
+          methodology: {
+            "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              label: false,
+            },
+            methodology: {
+              "ui:FieldTemplate": InlineFieldTemplate,
+            },
+            description: {
+              "ui:FieldTemplate": InlineFieldTemplate,
+            },
+          },
+          emission: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          gasType: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          equivalentEmission: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+        },
+      },
+    },
+  },
+};
+
+export default uiSchema;
