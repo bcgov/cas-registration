c628fe69394f0010bbf6c3b42a66a21215abe95c
Brianna Cerkiewicz <brianna@button.is>
2024-11-01 16:00:26 -0700

    chore: update BCGHG ID in v1 schemas and endpoints

--- bc_obps/registration/schema/v1/facility.py
+++ bc_obps/registration/schema/v1/facility.py
@@ -5,16 +5,18 @@ from typing import List, Optional
 
 
 class FacilityListOut(ModelSchema):
+    bcghg_id: Optional[str] = Field(None, alias="bcghg_id.id")
+
     class Meta:
         model = Facility
-        fields = ['id', 'name', 'is_current_year', 'starting_date', 'type', 'bcghg_id']
+        fields = ['id', 'name', 'is_current_year', 'starting_date', 'type']
 
 
 class FacilityFilterSchema(FilterSchema):
     # NOTE: we could simply use the `q` parameter to filter by name and ... but,
     # due to this issue: https://github.com/vitalik/django-ninja/issues/1037 mypy is unhappy so I'm using the `json_schema_extra` parameter
     # If we want to achieve more by using the `q` parameter, we should use it and ignore the mypy error
-    bcghg_id: Optional[str] = Field(None, json_schema_extra={'q': 'bcghg_id__icontains'})
+    bcghg_id: Optional[str] = Field(None, json_schema_extra={'q': 'bcghg_id__id__icontains'})
     name: Optional[str] = Field(None, json_schema_extra={'q': 'name__icontains'})
     type: Optional[str] = Field(None, json_schema_extra={'q': 'type__icontains'})
 
