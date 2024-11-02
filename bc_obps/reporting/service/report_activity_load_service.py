8595d87fb480c52eba9528c32d7f019709e26cf3
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-11-01 21:23:08 -0700

    test: testing the load data api endpoint

---
+++ bc_obps/reporting/service/report_activity_load_service.py
@@ -0,0 +1,14 @@
+from uuid import UUID
+from reporting.models.report_activity import ReportActivity
+from reporting.service.report_activity_serializers import ReportActivitySerializer
+
+
+class ReportActivityLoadService:
+    @classmethod
+    def load(cls, report_version_id: int, facility_id: UUID, activity_id: int) -> dict:
+
+        r = ReportActivity.objects.get(
+            report_version_id=report_version_id, facility_report__facility_id=facility_id, activity_id=activity_id
+        )
+
+        return ReportActivitySerializer.serialize(r)
