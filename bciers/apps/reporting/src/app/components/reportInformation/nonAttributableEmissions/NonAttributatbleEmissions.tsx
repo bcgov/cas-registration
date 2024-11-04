6585fbeefe5b2debfc3af0a857db99ae921271c2
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-11-04 13:41:34 -0800

    chore:fixed loading formData

--- bciers/apps/reporting/src/app/components/reportInformation/nonAttributableEmissions/NonAttributatbleEmissions.tsx
+++ bciers/apps/reporting/src/app/components/reportInformation/nonAttributableEmissions/NonAttributatbleEmissions.tsx
@@ -4,6 +4,7 @@ import NonAttributableEmissionsForm from "@reporting/src/app/components/reportIn
 import { UUID } from "crypto";
 import { getAllGasTypes } from "@reporting/src/app/utils/getAllGasTypes";
 import { getAllEmissionCategories } from "@reporting/src/app/utils/getAllEmissionCategories";
+import { getNonAttributableEmissionsData } from "@reporting/src/app/utils/getNonAttributableEmissionsData";
 
 interface NonAttributableEmissionsProps {
   versionId: number;
@@ -16,13 +17,44 @@ export default async function NonAttributableEmissions({
 }: NonAttributableEmissionsProps) {
   const gasTypes = await getAllGasTypes();
   const emissionCategories = await getAllEmissionCategories();
+  const emissionFormData = await getNonAttributableEmissionsData(
+    versionId,
+    facilityId,
+  );
+
+  // Create the mappings here
+  const gasTypeMap = gasTypes.reduce(
+    (
+      acc: { [x: string]: any },
+      gas: { id: string | number; chemical_formula: any },
+    ) => {
+      acc[gas.id] = gas.chemical_formula;
+      return acc;
+    },
+    {} as Record<number, string>,
+  );
+
+  const emissionCategoryMap = emissionCategories.reduce(
+    (
+      acc: { [x: string]: any },
+      category: { id: string | number; category_name: any },
+    ) => {
+      acc[category.id] = category.category_name;
+      return acc;
+    },
+    {} as Record<number, string>,
+  );
+
   return (
     <Suspense fallback={<Loading />}>
       <NonAttributableEmissionsForm
         versionId={versionId}
         facilityId={facilityId}
+        emissionFormData={emissionFormData}
         gasTypes={gasTypes}
         emissionCategories={emissionCategories}
+        gasTypeMap={gasTypeMap} // Pass the map to the form component
+        emissionCategoryMap={emissionCategoryMap} // Pass the map to the form component
       />
     </Suspense>
   );
