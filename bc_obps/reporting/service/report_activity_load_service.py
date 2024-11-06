97f1e90bbf30ace7556a3fef6919ced5366e83ec
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-11-06 15:15:07 -0800

    docs: load service heredocs

--- bc_obps/reporting/service/report_activity_load_service.py
+++ bc_obps/reporting/service/report_activity_load_service.py
@@ -6,7 +6,11 @@ from reporting.service.report_activity_serializers import ReportActivitySerializ
 class ReportActivityLoadService:
     @classmethod
     def load(cls, report_version_id: int, facility_id: UUID, activity_id: int) -> dict:
-
+        """
+        Loads the report activity data from the database structure into a json dictionary.
+        If the ReportActivity record is not found, it means this is the first load of the
+        form and we can safely return an empty object.
+        """
         try:
             r = ReportActivity.objects.get(
                 report_version_id=report_version_id, facility_report__facility_id=facility_id, activity_id=activity_id
