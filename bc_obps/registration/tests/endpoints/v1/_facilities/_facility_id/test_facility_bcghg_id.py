4bf54eb99f63240c9e5cd10d59e037d009b4ee24
Brianna Cerkiewicz <brianna@button.is>
2024-11-06 15:55:59 -0800

    chore: apply suggestions from code review

--- bc_obps/registration/tests/endpoints/v1/_facilities/_facility_id/test_facility_bcghg_id.py
+++ bc_obps/registration/tests/endpoints/v1/_facilities/_facility_id/test_facility_bcghg_id.py
@@ -1,8 +1,10 @@
 from registration.tests.utils.helpers import CommonTestSetup
+from registration.tests.utils.helpers import TestUtils
+from registration.utils import custom_reverse_lazy
+from model_bakery import baker
 
 
 class TestFacilityBcghgIdEndpoint(CommonTestSetup):
-   
     def test_authorized_role_can_issue_id(self):
         timeline = baker.make_recipe('utils.facility_designated_operation_timeline', end_date=None)
         roles = ["cas_admin", "cas_analyst"]
@@ -18,4 +20,4 @@ class TestFacilityBcghgIdEndpoint(CommonTestSetup):
                 ),
             )
             assert response.status_code == 200
-            assert response.json()['id'] == '14862100001'  # '1' and '486210' come from the recipe's mock data
+            assert response.json() == {'id': '14862100001'}  # '1' and '486210' come from the recipe's mock data
--- bc_obps/registration/tests/endpoints/v2/_operations/_operation_id/test_bcghg_id.py
+++ bc_obps/registration/tests/endpoints/v2/_operations/_operation_id/test_bcghg_id.py
@@ -1,8 +1,10 @@
 from registration.tests.utils.helpers import CommonTestSetup
+from registration.tests.utils.helpers import TestUtils
+from registration.utils import custom_reverse_lazy
+from model_bakery import baker
 
 
 class TestOperationBcghgIdEndpoint(CommonTestSetup):
- 
     def test_authorized_role_can_issue_id(self):
         operation = baker.make_recipe('utils.operation')
         roles = ["cas_admin", "cas_analyst"]
@@ -18,4 +20,4 @@ class TestOperationBcghgIdEndpoint(CommonTestSetup):
                 ),
             )
             assert response.status_code == 200
-            assert response.json()['id'] == '14862100001'  # '1' and '486210' come from the recipe's mock data
+            assert response.json() == {'id': '14862100001'}  # '1' and '486210' come from the recipe's mock data
