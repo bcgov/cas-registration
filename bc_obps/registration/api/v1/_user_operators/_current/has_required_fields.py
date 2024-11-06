733e711cdd4fedb2147c0ebceabf88a58732fb0a
SeSo <sobhani.sepehr@gmail.com>
2024-11-06 10:27:00 -0800

    chore: minor tweak to handle the endpoint error
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/registration/api/v1/_user_operators/_current/has_required_fields.py
+++ bc_obps/registration/api/v1/_user_operators/_current/has_required_fields.py
@@ -2,16 +2,19 @@ from django.http import HttpRequest
 from typing import Dict, Literal, Tuple
 from registration.api.router import router
 from registration.constants import USER_OPERATOR_TAGS
+from registration.decorators import handle_http_errors
 from service.operator_service_v2 import OperatorServiceV2
 from common.api.utils import get_current_user_guid
 from service.data_access_service.user_service import UserDataAccessService
 from common.permissions import authorize
 from django.core.exceptions import ObjectDoesNotExist
+from service.error_service.custom_codes_4xx import custom_codes_4xx
+from registration.schema.generic import Message
 
 
 @router.get(
     "/user-operators/current/has-required-fields",
-    response={200: Dict[str, bool]},
+    response={200: Dict[str, bool], custom_codes_4xx: Message},
     tags=USER_OPERATOR_TAGS,
     description="""
         Checks if a current user's operator has all required fields completed.
@@ -19,12 +22,11 @@ from django.core.exceptions import ObjectDoesNotExist
     """,
     auth=authorize("industry_user"),
 )
+@handle_http_errors()
 def get_current_user_operator_has_required_fields(request: HttpRequest) -> Tuple[Literal[200], Dict[str, bool]]:
-    # Get the current user GUID from the request
-    user_guid = get_current_user_guid(request)
     try:
         # Retrieve the operator associated with the current user
-        operator = UserDataAccessService.get_operator_by_user(user_guid)
+        operator = UserDataAccessService.get_operator_by_user(get_current_user_guid(request))
         # Use the service to check if the operator has all required fields filled
         has_required_fields = OperatorServiceV2.has_required_fields(operator)
         return 200, {"has_required_fields": has_required_fields}
