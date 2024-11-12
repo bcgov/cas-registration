ce993f23b0097235959c4ed9bd7357466d054e6d
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-11-12 13:05:59 -0800

    chore: nextjs page factory

---
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user/reports/[version_id]/attachments/page.tsx
@@ -0,0 +1,4 @@
+import Attachments from "@reporting/src/app/components/attachments/Attachments";
+import defaultPageFactory from "@reporting/src/app/utils/defaultPageFactory";
+
+export default defaultPageFactory(Attachments);
