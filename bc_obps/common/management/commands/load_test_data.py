a3919da0d7e14b4cb8ad6b346496bbb9f4f8aab9
Dylan Leard <dylan@button.is>
2024-11-14 13:14:09 -0800

    chore: add more test fixtures

--- bc_obps/common/management/commands/load_test_data.py
+++ bc_obps/common/management/commands/load_test_data.py
@@ -16,6 +16,10 @@ class Command(BaseCommand):
             f'{fixture_base_dir}/operator.json',
             f'{fixture_base_dir}/operation.json',
             f'{fixture_base_dir}/operation_designated_operator_timeline.json',
+            f'{fixture_base_dir}/user_operator.json',
+            f'{fixture_base_dir}/registration_purpose.json',
+            f'{fixture_base_dir}/facility.json',
+            f'{fixture_base_dir}/facility_designated_operation_timeline.json',
         ]
 
         for fixture in fixtures:
