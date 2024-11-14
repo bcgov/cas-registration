d6c84a9a46a72774b833a65be9a9c463c326e909
Brianna Cerkiewicz <brianna@button.is>
2024-11-14 14:49:10 -0800

    chore: fix filtering for N/A

--- bc_obps/registration/schema/v1/transfer_event.py
+++ bc_obps/registration/schema/v1/transfer_event.py
@@ -4,6 +4,8 @@ from uuid import UUID
 from registration.models.facility import Facility
 from registration.models.event.transfer_event import TransferEvent
 from ninja import ModelSchema, Field, FilterSchema
+from django.db.models import Q
+import re
 
 
 class FacilityForTransferEventGrid(ModelSchema):
@@ -39,6 +41,17 @@ class TransferEventFilterSchema(FilterSchema):
     # due to this issue: https://github.com/vitalik/django-ninja/issues/1037 mypy is unhappy so I'm using the `json_schema_extra` parameter
     # If we want to achieve more by using the `q` parameter, we should use it and ignore the mypy error
     effective_date: Optional[str] = Field(None, json_schema_extra={'q': 'effective_date__icontains'})
-    operation__name: Optional[str] = Field(None, json_schema_extra={'q': 'operation__name__icontains'})
+    operation__name: Optional[str] = None
     facilities__name: Optional[str] = Field(None, json_schema_extra={'q': 'facilities__name__icontains'})
     status: Optional[str] = Field(None, json_schema_extra={'q': 'status__icontains'})
+
+    def filtering_including_not_applicable(self, field: str, value: str) -> Q:
+        if value and re.search(value, 'n/a', re.IGNORECASE):
+            return Q(**{f"{field}__icontains": value}) | Q(**{f"{field}__isnull": True})
+        return Q(**{f"{field}__icontains": value}) if value else Q()
+
+    def filter_operation__name(self, value: str) -> Q:
+        return self.filtering_including_not_applicable('operation__name', value)
+
+    def filter_facilities__name(self, value: str) -> Q:
+        return self.filtering_including_not_applicable('facilities__name', value)
