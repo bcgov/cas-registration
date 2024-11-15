bae67c1874f0de81f0188d9f043864734c514a3c
Dylan Leard <dylan@button.is>
2024-11-15 11:18:34 -0800

    chore: add LFO operations & facilities

--- bc_obps/common/management/commands/load_test_data.py
+++ bc_obps/common/management/commands/load_test_data.py
@@ -15,10 +15,12 @@ class Command(BaseCommand):
             f'{fixture_base_dir}/bc_greenhouse_gas_id.json',
             f'{fixture_base_dir}/operator.json',
             f'{fixture_base_dir}/operation_sfo.json',
+            f'{fixture_base_dir}/operation_lfo.json',
             f'{fixture_base_dir}/operation_designated_operator_timeline.json',
             f'{fixture_base_dir}/user_operator.json',
             f'{fixture_base_dir}/registration_purpose.json',
             f'{fixture_base_dir}/facility_sfo.json',
+            f'{fixture_base_dir}/facility_lfo.json',
             f'{fixture_base_dir}/facility_designated_operation_timeline.json',
         ]
 
