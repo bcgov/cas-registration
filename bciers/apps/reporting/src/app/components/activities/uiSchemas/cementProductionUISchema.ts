e6d4da51a545577fb34149a84f58cbd8e6670026
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2024-11-18 17:43:25 -0800

    chore: Tweak previous migration and add UISchema

---
+++ bciers/apps/reporting/src/app/components/activities/uiSchemas/cementProductionUISchema.ts
@@ -0,0 +1,56 @@
+"use client";
+
+import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
+import NestedArrayFieldTemplate from "@bciers/components/form/fields/NestedArrayFieldTemplate";
+import SourceTypeBoxTemplate from "@bciers/components/form/fields/SourceTypeBoxTemplate";
+import InlineFieldTemplate from "@bciers/components/form/fields/InlineFieldTemplate";
+
+const uiSchema = {
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:classNames": "form-heading-label",
+  "ui:order": ["coverGasFromElectrolysisCells", "sourceTypes"],
+  sourceTypes: {
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:options": {
+      label: false,
+    },
+    calcinationUsedToProductClinker: {
+      "ui:FieldTemplate": SourceTypeBoxTemplate,
+      emissions: {
+        "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+        "ui:FieldTemplate": FieldTemplate,
+        "ui:options": {
+          arrayAddLabel: "Add Emission",
+          title: "Emission",
+          label: false,
+          verticalBorder: true,
+        },
+        items: {
+          gasType: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          emissions: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          equivalentEmissions: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+          methodology: {
+            "ui:FieldTemplate": FieldTemplate,
+            "ui:options": {
+              label: false,
+            },
+            methodology: {
+              "ui:FieldTemplate": InlineFieldTemplate,
+            },
+          },
+          timesMissingDataProceduresWereFollowed: {
+            "ui:FieldTemplate": InlineFieldTemplate,
+          },
+        },
+      },
+    },
+  },
+};
+
+export default uiSchema;
