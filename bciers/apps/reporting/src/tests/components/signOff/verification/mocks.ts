b899bb3ee32ae68bfdca31053cf701525ed2ecbc
shon-button <shon@button.is>
2024-11-20 06:49:43 -0500

    tests: vitests

--- bciers/apps/reporting/src/tests/components/signOff/verification/mocks.ts
+++ bciers/apps/reporting/src/tests/components/signOff/verification/mocks.ts
@@ -1,12 +1,18 @@
 import { vi } from "vitest";
 
-// Create the mock function
-export const createVerificationSchema = vi.fn();
+// Create the mock functions
+const createVerificationSchema = vi.fn();
+const getReportFacilityList = vi.fn();
 
-// Mock the module to use the exported mock function
+// Link the module to the import path in the component so to override the real function.
 vi.mock(
-  "@reporting/src/app/components/signOff/verification/createVerificationSchema",
+  "apps/reporting/src/app/components/signOff/verification/createVerificationSchema",
   () => ({
     default: createVerificationSchema,
   }),
 );
+vi.mock("apps/reporting/src/app/utils/getReportFacilityList", () => ({
+  default: getReportFacilityList,
+}));
+
+export { createVerificationSchema, getReportFacilityList };
