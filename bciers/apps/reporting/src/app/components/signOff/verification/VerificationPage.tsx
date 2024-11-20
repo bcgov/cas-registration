26e7adeff999daf9eb4246e7d2952981f0a58f02
shon-button <shon@button.is>
2024-11-20 06:49:43 -0500

    tests: vitests

--- bciers/apps/reporting/src/app/components/signOff/verification/VerificationPage.tsx
+++ bciers/apps/reporting/src/app/components/signOff/verification/VerificationPage.tsx
@@ -1,14 +1,19 @@
+import { getReportFacilityList } from "@reporting/src/app/utils/getReportFacilityList";
+
+import { createVerificationSchema } from "@reporting/src/app/components/signOff/verification/createVerificationSchema";
 import { verificationUiSchema } from "@reporting/src/data/jsonSchema/signOff/verification/verification";
-import { createVerificationSchema } from "./createVerificationSchema";
-import VerificationForm from "./VerificationForm";
+import VerificationForm from "@reporting/src/app/components/signOff/verification/VerificationForm";
 
 export default async function VerificationPage({
   versionId,
 }: {
   versionId: number;
 }) {
+  // Fetch the list of facilities associated with the specified version ID
+  const facilityList = await getReportFacilityList(versionId);
+
   // Create schema with dynamic facility list
-  const verificationSchema = await createVerificationSchema(versionId);
+  const verificationSchema = createVerificationSchema(facilityList.facilities);
   // Render the verification form
   return (
     <>
--- bciers/apps/reporting/src/app/utils/getReportFacilityList.ts
+++ bciers/apps/reporting/src/app/utils/getReportFacilityList.ts
@@ -1,5 +1,5 @@
 import { actionHandler } from "@bciers/actions";
-
+// Fetches the facility list for the operation associated with the given report version ID
 export async function getReportFacilityList(version_id: number) {
   const response = await actionHandler(
     `reporting/report-version/${version_id}/facility-list`,
