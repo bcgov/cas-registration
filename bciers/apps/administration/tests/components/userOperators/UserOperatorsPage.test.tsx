b392a24ec47e3ae281b1fae6174385444b718db4
SeSo <sobhani.sepehr@gmail.com>
2024-11-21 13:56:19 -0800

    chore: implement PR review suggestions
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/administration/tests/components/userOperators/UserOperatorsPage.test.tsx
+++ bciers/apps/administration/tests/components/userOperators/UserOperatorsPage.test.tsx
@@ -5,6 +5,7 @@ import {
 } from "@bciers/testConfig/mocks";
 import UserOperatorsPage from "@/administration/app/components/userOperators/UserOperatorsPage";
 import { expect } from "vitest";
+import { UserOperatorStatus } from "@bciers/utils/src/enums";
 
 useSearchParams.mockReturnValue({
   get: vi.fn(),
@@ -16,6 +17,32 @@ vi.mock(
     default: getUserOperatorsPageData,
   }),
 );
+
+const mockResponse = {
+  rows: [
+    {
+      id: 1,
+      user_friendly_id: "1",
+      status: UserOperatorStatus.APPROVED,
+      user__first_name: "John",
+      user__last_name: "Doe",
+      user__email: "john.doe@example.com",
+      user__bceid_business_name: "John Doe Inc.",
+      operator__legal_name: "FakeOperator 1",
+    },
+    {
+      id: 2,
+      user_friendly_id: "2",
+      status: UserOperatorStatus.PENDING,
+      user__first_name: "Jane",
+      user__last_name: "Smith",
+      user__email: "jane.smith@example.com",
+      user__bceid_business_name: "Jane Smith Inc.",
+      operator__legal_name: "FakeOperator 2",
+    },
+  ],
+  row_count: 2,
+};
 
 describe("User Operators (External Access Requests) Page", () => {
   beforeEach(() => {
@@ -36,6 +63,13 @@ describe("User Operators (External Access Requests) Page", () => {
     ).toBeVisible();
     expect(screen.queryByRole("grid")).toBeInTheDocument();
     expect(screen.getByText(/No records found/i)).toBeVisible();
+  });
+  it("renders UserOperatorsPage that correctly handle a non-empty data array", async () => {
+    getUserOperatorsPageData.mockReturnValueOnce(mockResponse);
+    render(await UserOperatorsPage({ searchParams: {} }));
+    expect(screen.queryByRole("grid")).toBeInTheDocument();
+    const allRows = screen.getAllByRole("row");
+    expect(allRows).toHaveLength(4); // 2 rows + 1 header + 1 filter row
   });
   it("renders the appropriate error component when getUserOperatorsPageData fails", async () => {
     getUserOperatorsPageData.mockReturnValueOnce(undefined);
