e4f2ca2534bb70157e4c6666092835697f97a1f4
acatchpole <adamantiumite@gmail.com>
2024-11-22 14:55:57 -0800

    chore: fix migration name after rebase

--- bc_obps/registration/migrations/0058_regulatedproduct_add_isregulated_and_unit.py
+++ bc_obps/registration/migrations/0058_regulatedproduct_add_isregulated_and_unit.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0056_remove_historicaltransferevent_future_designated_operator_and_more'),
+        ('registration', '0057_remove_registrationpurpose_archived_by_and_more'),
     ]
 
     def update_regulated_product_units_and_is_regulated(apps, schema_editor):
