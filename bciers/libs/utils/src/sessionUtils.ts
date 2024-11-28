d3f1e5dc258042ab013f20dea069881aa9d309ff
Brianna Cerkiewicz <brianna@button.is>
2024-11-28 14:51:58 -0800

    chore: add comments

--- bciers/libs/utils/src/sessionUtils.ts
+++ bciers/libs/utils/src/sessionUtils.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/dashboard/auth";
 import { useSession } from "next-auth/react";
 
+// use getSessionRole in server components
 const getSessionRole = async () => {
   const session = await auth();
   if (!session?.user?.app_role) {
@@ -10,6 +11,7 @@ const getSessionRole = async () => {
   return session.user.app_role;
 };
 
+// useSessionFole is for client components
 const useSessionRole = () => {
   const session = useSession();
   if (!session?.data?.user?.app_role) {
