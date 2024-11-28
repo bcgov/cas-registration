ed78464df858ae9f85f6c8941bf60f37b56505b8
Brianna Cerkiewicz <brianna@button.is>
2024-11-28 15:51:52 -0800

    chore: add to init

--- bc_obps/registration/api/v2/__init__.py
+++ bc_obps/registration/api/v2/__init__.py
@@ -7,10 +7,18 @@ from ._operations._operation_id._registration import (
     new_entrant_application,
 )
 from ._operations import operation_id
-from . import operations
-from . import operators
-from . import user_operators
-from . import facilities
+from . import (
+    business_structures,
+    e2e_test_setup,
+    naics_codes,
+    operators,
+    regulated_products,
+    operations,
+    user_operators,
+    reporting_activities,
+    users,
+    contacts,
+)
 from ._user_operators._current import operator
 from ._operations import current
 from ._operations._operation_id import boro_id, bcghg_id as operation_bcghg_id
@@ -19,5 +27,5 @@ from ._facilities._facility_id import bcghg_id as facility_bcghg_id
 from ._facilities import facility_id
 from ._operators import search
 from .transfer_events import list_transfer_events
-from ._operators._operator_id import has_admin,request_access,request_admin_access,access_declined
+from ._operators._operator_id import has_admin, request_access, request_admin_access, access_declined
 from ._operators import operator_id
