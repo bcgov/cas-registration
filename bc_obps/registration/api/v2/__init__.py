61075e3877358dd584fd2f0a5e82640510dc0f46
Brianna Cerkiewicz <brianna@button.is>
2024-11-28 14:42:24 -0800

    chore: spec work

--- bc_obps/registration/api/v2/__init__.py
+++ bc_obps/registration/api/v2/__init__.py
@@ -14,5 +14,8 @@ from . import facilities
 from ._user_operators._current import operator
 from ._operations import current
 from ._operations._operation_id import boro_id, bcghg_id as operation_bcghg_id
+from ._operations._operation_id.facilities import list_facilities_by_operation_id
 from ._facilities._facility_id import bcghg_id as facility_bcghg_id
 from ._facilities import facility_id
+from ._operators import search
+from .transfer_events import list_transfer_events
