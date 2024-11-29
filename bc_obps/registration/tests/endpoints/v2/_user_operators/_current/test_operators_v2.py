42fb959dcbb70ccee201ad040c6c5169fb790b78
Brianna Cerkiewicz <brianna@button.is>
2024-11-29 15:31:31 -0800

    chore: fixin stuff

--- bc_obps/registration/tests/endpoints/v2/_user_operators/_current/test_operators_v2.py
+++ bc_obps/registration/tests/endpoints/v2/_user_operators/_current/test_operators_v2.py
@@ -4,36 +4,6 @@ from registration.utils import custom_reverse_lazy
 
 
 class TestCurrentUserOperatorV2Endpoint(CommonTestSetup):
-    # GET USER OPERATOR OPERATOR ID 200
-    def test_get_current_operator_and_user_operator(self):
-        # Act
-        operator = operator_baker()
-        TestUtils.authorize_current_user_as_operator_user(self, operator=operator)
-        response = TestUtils.mock_get_with_auth_role(
-            self, "industry_user", custom_reverse_lazy("get_current_operator_and_user_operator")
-        )
-
-        response_json = response.json()
-
-        # Assert
-        assert response.status_code == 200
-
-        # Additional Assertions
-        assert response_json['id'] == str(operator.id)
-        assert "street_address" in response_json
-        assert "municipality" in response_json
-        assert "province" in response_json
-        assert "postal_code" in response_json
-        assert "operator_has_parent_operators" in response_json
-
-    # GET USER OPERATOR OPERATOR ID 401
-    def test_get_current_operator_and_user_operator_with_invalid_user(self):
-        # Act
-        response = TestUtils.mock_get_with_auth_role(
-            self, "industry_user", custom_reverse_lazy("get_current_operator_and_user_operator")
-        )
-        # User_operator must be approved to see their operator info
-        assert response.status_code == 401
 
     # PUT
     def test_put_current_user_operator(self):
