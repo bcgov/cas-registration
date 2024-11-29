0b3d142c18dbabcb37dd6fffd988bff550b14fdb
Brianna Cerkiewicz <brianna@button.is>
2024-11-29 08:38:37 -0800

    chore: more v2 endpoints

---
+++ bc_obps/registration/api/v2/_user_operators/pending.py
@@ -0,0 +1,28 @@
+from typing import Literal, Tuple
+from common.permissions import authorize
+from django.http import HttpRequest
+from registration.constants import USER_OPERATOR_TAGS
+from registration.schema.v1.user_operator import PendingUserOperatorOut
+from service.data_access_service.user_service import UserDataAccessService
+from common.api.utils import get_current_user_guid
+from registration.decorators import handle_http_errors
+from registration.schema.generic import Message
+from registration.api.router import router
+from registration.models import UserOperator
+from service.error_service.custom_codes_4xx import custom_codes_4xx
+
+# We don't need a user_operator_id parameter (using "pending" instead) because we can look up the user_operator via the user data we receive in the middleware
+
+
+# This endpoint retrieves data about both the user-operator and the operator.
+@router.get(
+    "/v2/user-operators/pending",
+    response={200: PendingUserOperatorOut, custom_codes_4xx: Message},
+    tags=USER_OPERATOR_TAGS,
+    description="""Retrieves data about the pending user-operator and their associated operator.
+    Declined user-operators are excluded from the results.""",
+    auth=authorize("industry_user"),
+)
+@handle_http_errors()
+def get_pending_operator_and_user_operator(request: HttpRequest) -> Tuple[Literal[200], UserOperator]:
+    return 200, UserDataAccessService.get_user_operator_by_user(get_current_user_guid(request))
---
+++ bc_obps/registration/api/v2/_user_operators/user_operator_id.py
@@ -0,0 +1,60 @@
+from typing import Dict, Literal, Tuple
+from common.permissions import authorize
+from django.http import HttpRequest
+from registration.constants import USER_OPERATOR_TAGS
+from service.user_operator_service import UserOperatorService
+from common.api.utils import get_current_user_guid
+from registration.decorators import handle_http_errors
+from registration.schema.v1 import (
+    UserOperatorOperatorIn,
+    RequestAccessOut,
+)
+from registration.schema.generic import Message
+from registration.api.router import router
+from registration.models import UserOperator
+from uuid import UUID
+from service.error_service.custom_codes_4xx import custom_codes_4xx
+from service.data_access_service.user_operator_service import UserOperatorDataAccessService
+from registration.schema.v1 import UserOperatorOut
+
+
+# GET
+@router.get(
+    "/v2/user-operators/{uuid:user_operator_id}",
+    response={200: UserOperatorOut, custom_codes_4xx: Message},
+    tags=USER_OPERATOR_TAGS,
+    description="""Retrieves data about a specific user-operator by its ID.
+    It checks if a user is eligible to access a user_operator (i.e., they're allowed to access their own information (user_operator, operations, etc.) but not other people's).
+    Internal users are always allowed to access user operators.""",
+    auth=authorize("approved_authorized_roles"),
+)
+@handle_http_errors()
+def get_user_operator_by_id(request: HttpRequest, user_operator_id: UUID) -> Tuple[Literal[200], UserOperator]:
+    UserOperatorService.check_if_user_eligible_to_access_user_operator(
+        get_current_user_guid(request), user_operator_id
+    )  # industry users can only access their own user_operators
+    return 200, UserOperatorDataAccessService.get_user_operator_by_id(user_operator_id)
+
+
+## PUT
+@router.put(
+    "/user-operators/{uuid:user_operator_id}",
+    response={200: RequestAccessOut, custom_codes_4xx: Message},
+    tags=USER_OPERATOR_TAGS,
+    description="""Updates both the operator and the user-operator by their ID.
+    The endpoint ensures that industry users can only update their own user-operators.
+    It checks for the uniqueness of the CRA Business Number.
+    If all permissions and checks are in order, it updates the operator's status to 'Pending' if it is currently 'Draft'.
+    The updated data is saved, and a new user-operator instance is created if one does not already exist.""",
+    auth=authorize("approved_industry_user"),
+)
+@handle_http_errors()
+def update_operator_and_user_operator(
+    request: HttpRequest, payload: UserOperatorOperatorIn, user_operator_id: UUID
+) -> Tuple[Literal[200], Dict[str, UUID]]:
+    UserOperatorService.check_if_user_eligible_to_access_user_operator(
+        get_current_user_guid(request), user_operator_id
+    )  # industry users can only update their own user_operators
+    return 200, UserOperatorService.update_operator_and_user_operator(
+        user_operator_id, payload, get_current_user_guid(request)
+    )
