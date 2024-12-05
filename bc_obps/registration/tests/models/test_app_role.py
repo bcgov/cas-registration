4c42fd9b42e23b1ea8889f2867d9e8812b40bbed
Brianna Cerkiewicz <brianna@button.is>
2024-12-05 10:49:54 -0800

    chore: migration for cas_view_only role

--- bc_obps/registration/tests/models/test_app_role.py
+++ bc_obps/registration/tests/models/test_app_role.py
@@ -16,7 +16,9 @@ class AppRoleModelTest(BaseTestCase):
         ]
 
     def test_initial_data(self):
-        expected_roles = sorted(['cas_admin', 'cas_analyst', 'cas_director', 'cas_pending', 'industry_user'])
+        expected_roles = sorted(
+            ['cas_admin', 'cas_analyst', 'cas_director', 'cas_pending', 'cas_view_only', 'industry_user']
+        )
         existing_roles = sorted(list(AppRole.objects.values_list('role_name', flat=True)))
 
         self.assertEqual(len(existing_roles), len(expected_roles))
@@ -24,30 +26,16 @@ class AppRoleModelTest(BaseTestCase):
 
     def test_static_methods(self):
         self.assertEqual(
-            sorted(AppRole.get_authorized_irc_roles()), sorted(['cas_admin', 'cas_analyst', 'cas_director'])
+            sorted(AppRole.get_authorized_irc_roles()),
+            sorted(['cas_admin', 'cas_analyst', 'cas_director', 'cas_view_only']),
         )
         self.assertEqual(
             sorted(AppRole.get_all_authorized_app_roles()),
-            sorted(
-                [
-                    'cas_admin',
-                    'cas_analyst',
-                    'industry_user',
-                    'cas_director',
-                ]
-            ),
+            sorted(['cas_admin', 'cas_analyst', 'industry_user', 'cas_director', 'cas_view_only']),
         )
         self.assertEqual(
             sorted(AppRole.get_all_app_roles()),
-            sorted(
-                [
-                    'cas_admin',
-                    'cas_analyst',
-                    'cas_pending',
-                    'industry_user',
-                    'cas_director',
-                ]
-            ),
+            sorted(['cas_admin', 'cas_analyst', 'cas_pending', 'industry_user', 'cas_director', 'cas_view_only']),
         )
         self.assertEqual(
             sorted(UserOperator.get_all_industry_user_operator_roles()), sorted(['admin', 'reporter', 'pending'])
