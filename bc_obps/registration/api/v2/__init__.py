afd622180bc47bc18ea00f4a2126875f62a4ae7e
SeSo <sobhani.sepehr@gmail.com>
2024-12-06 15:16:43 -0800

    chore: add transfer event endpoint and schema
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/registration/api/v2/__init__.py
+++ bc_obps/registration/api/v2/__init__.py
@@ -21,11 +21,11 @@ from . import (
     facilities,
 )
 from ._operations import current as operations_current
-from ._operations._operation_id import boro_id, bcghg_id as operation_bcghg_id
+from ._operations._operation_id import boro_id, bcghg_id as operation_bcghg_id, operation_representatives
 from ._operations._operation_id.facilities import list_facilities_by_operation_id
 from ._facilities._facility_id import bcghg_id as facility_bcghg_id
 from ._facilities import facility_id
-from ._operators import search
+from ._operators import search, operator_id
 from .transfer_events import list_transfer_events
 from ._operators._operator_id import has_admin, request_access, request_admin_access, access_declined, confirm
 from ._operators import operator_id
@@ -38,9 +38,8 @@ from ._user_operators._current import (
     operator_users,
 )
 from ._user_operators._user_operator_id import update_status
-
 from .user import user_profile, user_app_role
 from ._user_operators import user_operator_id, pending, current
 from ._contacts import contact_id
 from ._users import user_id
-from ._operations._operation_id import operation_representatives
+from . import transfer_events
