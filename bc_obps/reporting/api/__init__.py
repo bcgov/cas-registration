9e5a3859d63264032a2b26cb758d8e4c65fe6e73
shon-button <shon@button.is>
2024-12-06 10:59:53 -0500

    chore: add emission allocations service

--- bc_obps/reporting/api/__init__.py
+++ bc_obps/reporting/api/__init__.py
@@ -19,3 +19,5 @@ from .report_activity import save_report_activity_data, load_report_activity_dat
 from .report_facilities import get_report_facility_list_by_version_id
 from .report_verification import get_report_verification_by_version_id, save_report_verification
 from .report_attachments import save_report_attachments, get_report_attachments
+from .report_emission_allocations import get_emission_allocations, save_emission_allocation_data
+
