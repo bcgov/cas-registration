00627365cf8e3a8710a01e8ecf6e97416aade2ae
Andrea Williams <andrea.williams@gov.bc.ca>
2024-12-10 17:16:16 -0800

    chore: changing RP in progress. All tests pass except for wretched EIO as original RP bc of doc count that's haunting me

--- bc_obps/service/tests/test_document_service.py
+++ bc_obps/service/tests/test_document_service.py
@@ -27,7 +27,6 @@ class TestDocumentService:
 
     @staticmethod
     def test_do_not_update_duplicate_operation_document():
-
         approved_user_operator = baker.make_recipe('utils.approved_user_operator')
         operation = baker.make_recipe('utils.operation', operator=approved_user_operator.operator)
         existing_document = DocumentDataAccessService.create_document(
@@ -50,7 +49,6 @@ class TestDocumentService:
 
     @staticmethod
     def test_update_operation_document():
-
         approved_user_operator = baker.make_recipe('utils.approved_user_operator')
         operation = baker.make_recipe('utils.operation', operator=approved_user_operator.operator)
         existing_document = DocumentDataAccessService.create_document(
@@ -68,3 +66,27 @@ class TestDocumentService:
         # MOCK_DATA_URL's filename is test.pdf
         assert document.file.name.find("test") != -1
         assert created is True
+
+    @staticmethod
+    def test_get_active_documents():
+        approved_user_operator = baker.make_recipe('utils.approved_user_operator')
+        operation = baker.make_recipe('utils.operation', operator=approved_user_operator.operator)
+        b_map = DocumentDataAccessService.create_document(
+            approved_user_operator.user_id, data_url_to_file(MOCK_DATA_URL), 'boundary_map'
+        )
+        pfd = DocumentDataAccessService.create_document(
+            approved_user_operator.user_id, data_url_to_file(MOCK_DATA_URL), 'process_flow_diagram'
+        )
+        nea = DocumentDataAccessService.create_document(
+            approved_user_operator.user_id, data_url_to_file(MOCK_DATA_URL), 'new_entrant_application'
+        )
+        operation.documents.set([b_map.id, pfd.id, nea.id])
+
+        assert Document.objects.count() == 3
+
+        DocumentService.archive_operation_document(
+            approved_user_operator.user_id, operation.id, 'new_entrant_application'
+        )
+
+        assert Document.objects.count() == 2
+        assert DocumentDataAccessService.get_active_operation_documents(operation.id).count() == 2
