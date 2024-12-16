c443f1aeea19e50255662806cccdd2ad9dcd3905
Dylan Leard <dylan@button.is>
2024-12-16 10:40:33 -0800

    chore: use classes instead of schemas for type interfaces in service

--- bc_obps/reporting/api/compliance_data.py
+++ bc_obps/reporting/api/compliance_data.py
@@ -1,11 +1,10 @@
 from typing import Literal, Tuple
-from common.permissions import authorize
 from django.http import HttpRequest
 from registration.decorators import handle_http_errors
 from reporting.constants import EMISSIONS_REPORT_TAGS
 from service.error_service.custom_codes_4xx import custom_codes_4xx
 from reporting.schema.generic import Message
-from reporting.service.compliance_service import ComplianceService
+from reporting.service.compliance_service import ComplianceService, ComplianceData
 from reporting.schema.compliance_data import ComplianceDataSchemaOut
 from .router import router
 
@@ -16,12 +15,10 @@ from .router import router
     tags=EMISSIONS_REPORT_TAGS,
     description="""Retrieves the data for the compliance summary page from multiple data sources.""",
     exclude_none=True,
-    auth=authorize("approved_industry_user"),
+    # auth=authorize("approved_industry_user"),
 )
 @handle_http_errors()
-def get_compliance_summary_data(
-    request: HttpRequest, report_version_id: int
-) -> Tuple[Literal[200], ComplianceDataSchemaOut]:
+def get_compliance_summary_data(request: HttpRequest, report_version_id: int) -> Tuple[Literal[200], ComplianceData]:
     compliance_data = ComplianceService.get_calculated_compliance_data(report_version_id)
 
     return 200, compliance_data
