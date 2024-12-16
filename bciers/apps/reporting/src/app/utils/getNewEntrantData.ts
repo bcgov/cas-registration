bee1956568f7bbe7e86866ad7c985c36c9aeb07c
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-12-16 15:52:31 -0800

    chore: fixed eslint error

--- bciers/apps/reporting/src/app/utils/getNewEntrantData.ts
+++ bciers/apps/reporting/src/app/utils/getNewEntrantData.ts
@@ -1,7 +1,7 @@
 import { actionHandler } from "@bciers/actions";
 
 export async function getNewEntrantData(version_id: number) {
-  return await actionHandler(
+  return actionHandler(
     `reporting/report-version/${version_id}/new-entrant-data`,
     "GET",
     `reporting/report-version/${version_id}/new-entrant-data`,
