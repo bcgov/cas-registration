ae7866db038c1501cddd18c25cbefd64df5b2a8f
Brianna Cerkiewicz <brianna@button.is>
2024-12-17 12:11:35 -0800

    chore: update statuses on operation and timeline models

---
+++ bc_obps/registration/tests/models/test_operation_designated_operator_timeline.py
@@ -0,0 +1,20 @@
+from common.tests.utils.helpers import BaseTestCase
+from model_bakery import baker
+from registration.tests.constants import (
+    TIMESTAMP_COMMON_FIELDS,
+)
+
+
+class OperationDesignatedOperatorTimelineModelTest(BaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_object = baker.make_recipe('utils.operation_designated_operator_timeline')
+        cls.field_data = [
+            *TIMESTAMP_COMMON_FIELDS,
+            ("id", "ID", None, None),
+            ("operation", "operation", None, None),
+            ("operator", "operator", None, None),
+            ("start_date", "start date", None, None),
+            ("end_date", "end date", None, None),
+            ("status", "status", 1000, None),
+        ]
