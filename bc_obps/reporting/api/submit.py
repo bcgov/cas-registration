574c3e1ba0f33175a931d52578a9e9eafc5865f4
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-12-17 08:06:42 -0800

    chore: moving service file to the right place

--- bc_obps/reporting/api/submit.py
+++ bc_obps/reporting/api/submit.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 from registration.decorators import handle_http_errors
 from reporting.constants import EMISSIONS_REPORT_TAGS
 from reporting.schema.generic import Message
-from service.report_submission_service import ReportSubmissionService
+from reporting.service.report_submission_service import ReportSubmissionService
 
 from .router import router
 
@@ -15,7 +15,7 @@ from .router import router
     response={200: int, 400: Message},
     tags=EMISSIONS_REPORT_TAGS,
     description="""Submits a report version""",
-    auth=authorize("approved_authorized_roles"),
+    auth=authorize("approved_industry_user"),
 )
 @handle_http_errors()
 def submit_report_version(request: HttpRequest, version_id: int) -> int:
