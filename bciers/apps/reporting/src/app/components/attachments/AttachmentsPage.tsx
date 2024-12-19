0c5881459522c67ec7776d90e066f2e9a65d6b0e
shon-button <shon@button.is>
2024-12-19 13:35:44 -0500

    chore: add needs verification call to attachment page

--- bciers/apps/reporting/src/app/components/attachments/AttachmentsPage.tsx
+++ bciers/apps/reporting/src/app/components/attachments/AttachmentsPage.tsx
@@ -3,6 +3,7 @@ import AttachmentsForm from "./AttachmentsForm";
 import getAttachments from "@reporting/src/app/utils/getAttachments";
 import { UploadedAttachment } from "./types";
 import { getSignOffAndSubmitSteps } from "../taskList/5_signOffSubmit";
+import { getReportNeedsVerification } from "@reporting/src/app/utils/getReportNeedsVerification";
 
 export const getDictFromAttachmentArray = (array: UploadedAttachment[]) =>
   Object.fromEntries(array.map((a) => [a.attachment_type, a]));
@@ -16,12 +17,15 @@ const AttachmentsPage: React.FC<HasReportVersion> = async ({ version_id }) => {
 
   const taskListElements = getSignOffAndSubmitSteps(version_id, 1);
 
+  //🔍 Check if reports need verification
+  const needsVerification = await getReportNeedsVerification(version_id);
+
   return (
     <AttachmentsForm
       version_id={version_id}
       initialUploadedAttachments={uploadedAttachmentsDict}
       taskListElements={taskListElements}
-      isVerificationStatementMandatory={true}
+      isVerificationStatementMandatory={needsVerification}
     />
   );
 };
