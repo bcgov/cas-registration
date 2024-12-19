8090df593d96949cdc386c89ca42877f67a07157
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2024-12-18 17:45:01 -0800

    chore: fixed alert box content

--- bciers/apps/reporting/src/data/jsonSchema/reviewOperationInformationText.tsx
+++ bciers/apps/reporting/src/data/jsonSchema/reviewOperationInformationText.tsx
@@ -1,14 +1,24 @@
 import InfoIcon from "@mui/icons-material/Info";
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Link, Paper, Typography } from "@mui/material";
 import { BC_GOV_TEXT, LIGHT_BLUE_BG_COLOR } from "@bciers/styles";
+import React from "react";
 export const purposeNote = (
   <Paper sx={{ p: 2, mb: 3, bgcolor: LIGHT_BLUE_BG_COLOR, color: BC_GOV_TEXT }}>
     <Box sx={{ display: "flex", alignItems: "center" }}>
       <InfoIcon sx={{ mr: 1 }} />
       <Typography variant="body2">
-        The information shown on this page is data entered in registration. You
-        can edit it here but it will only apply to this report. To make edits
-        for all of your reports, please edit this information in registration.
+        The information shown on this page is data entered in Administration.
+        You can edit Operation Information here but it will only apply to this
+        report. To apply Operation information edits to all of your reports,
+        please edit this information in{" "}
+        <Link
+          href="/administration"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{ color: "inherit", textDecoration: "none" }}
+        >
+          Administration.
+        </Link>
       </Typography>
     </Box>
   </Paper>
