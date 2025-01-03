65b15ac3c6cf003e51f34811f628818ab23289b0
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-01-03 15:54:46 -0800

    refactor: breaking out the report service into a report version service

--- bc_obps/service/report_service.py
+++ bc_obps/service/report_service.py
@@ -9,29 +9,25 @@ from reporting.models.facility_report import FacilityReport
 from reporting.models.report_operation import ReportOperation
 from reporting.models.report_version import ReportVersion
 from reporting.schema.report_operation import ReportOperationIn
-from service.data_access_service.facility_service import FacilityDataAccessService
 from service.data_access_service.report_service import ReportDataAccessService
 from service.data_access_service.reporting_year import ReportingYearDataAccessService
+from service.report_version_service import ReportVersionService
 
 
 class ReportService:
     @classmethod
     @transaction.atomic()
     def create_report(cls, operation_id: UUID, reporting_year: int) -> int:
-
         if ReportDataAccessService.report_exists(operation_id, reporting_year):
             raise Exception("A report already exists for this operation and year, unable to create a new one.")
 
         # Fetching report context
-
         operation = (
-            Operation.objects.select_related('operator')
-            .prefetch_related('activities', 'regulated_products')
+            Operation.objects.select_related("operator")
+            .prefetch_related("activities", "regulated_products")
             .get(id=operation_id)
         )
-
         operator = operation.operator
-        facilities = FacilityDataAccessService.get_current_facilities_by_operation(operation)
 
         # Creating report object
 
@@ -41,39 +37,7 @@ class ReportService:
             reporting_year=ReportingYearDataAccessService.get_by_year(reporting_year),
         )
 
-        # Creating first version
-
-        report_version = ReportVersion.objects.create(report=report)
-
-        # Pre-populating data to the first version
-
-        report_operation = ReportOperation.objects.create(
-            operator_legal_name=operator.legal_name,
-            operator_trade_name=operator.trade_name,
-            operation_name=operation.name,
-            operation_type=operation.type,
-            operation_bcghgid=operation.bcghg_id.id if operation.bcghg_id else None,
-            bc_obps_regulated_operation_id=(
-                operation.bc_obps_regulated_operation.id if operation.bc_obps_regulated_operation else ""
-            ),
-            operation_representative_name=(
-                operation.point_of_contact.get_full_name() if operation.point_of_contact else ""
-            ),
-            report_version=report_version,
-        )
-        report_operation.activities.add(*list(operation.activities.all()))
-        report_operation.regulated_products.add(*list(operation.regulated_products.all()))
-
-        for f in facilities:
-            facility_report = FacilityReport.objects.create(
-                facility=f,
-                facility_name=f.name,
-                facility_type=f.type,
-                facility_bcghgid=f.bcghg_id.id if f.bcghg_id else None,
-                report_version=report_version,
-            )
-            facility_report.activities.add(*list(operation.activities.all()))
-
+        report_version = ReportVersionService.create_report_version(report)
         return report_version.id
 
     @classmethod
@@ -108,10 +72,6 @@ class ReportService:
             for f in facility_reports:
                 f.activities.set(activities)
                 f.save()
-
-        report_version = ReportVersion.objects.get(id=report_version_id)
-        report_version.report_type = data.operation_report_type
-        report_version.save()
 
         return report_operation
 
