03932f8a5eaff194c56566eb013540d07d91f689
SeSo <sobhani.sepehr@gmail.com>
2025-01-07 14:49:36 -0800

    chore: django command to process transfer events
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

---
+++ bc_obps/registration/management/commands/process_due_transfer_events.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from service.transfer_event_service import TransferEventService
+
+class Command(BaseCommand):
+    help = 'Process all due transfer events'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Processing due transfer events...')
+        try:
+            TransferEventService.process_due_transfer_events()
+            self.stdout.write(self.style.SUCCESS('Successfully processed due transfer events'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error processing due transfer events: {e}'))
