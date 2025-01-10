6d85488ba745d902a48271142ff401c9fac023c4
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-01-10 10:54:54 -0800

    test: endpoint permissions

--- bc_obps/reporting/tests/api/test_report_version_endpoint.py
+++ bc_obps/reporting/tests/api/test_report_version_endpoint.py
@@ -11,7 +11,10 @@ class TestReportVersionEndpoint(CommonTestSetup):
         response = TestUtils.mock_get_with_auth_role(
             self,
             "industry_user",
-            custom_reverse_lazy("get_report_operation_by_version_id", kwargs={"version_id": report_version.id}),
+            custom_reverse_lazy(
+                "get_report_operation_by_version_id",
+                kwargs={"version_id": report_version.id},
+            ),
         )
         assert response.status_code == 200
         # Test that the endpoint returns the correct data
@@ -22,11 +25,11 @@ class TestReportVersionEndpoint(CommonTestSetup):
 
     # POST report-operation
     def test_authorized_users_can_post_updates_to_report_version(self):
-        report_version = report_version_baker(report_type="Initial Report")
+        report_version = report_version_baker(report_type="Simple Report")
 
         TestUtils.authorize_current_user_as_operator_user(self, operator=report_version.report.operator)
 
-        endpoint_under_test = f'/api/reporting/report-version/{report_version.id}/report-operation'
+        endpoint_under_test = f"/api/reporting/report-version/{report_version.id}/report-operation"
 
         data = {
             "operator_legal_name": "new legal name",
@@ -40,21 +43,23 @@ class TestReportVersionEndpoint(CommonTestSetup):
             "operation_report_type": "Annual Report",
             "operation_representative_name": [1, 2],
         }
-        assert report_version.report_operation.operator_legal_name != data['operator_legal_name']
-        assert report_version.report_operation.operator_trade_name != data['operator_trade_name']
-        assert report_version.report_operation.operation_name != data['operation_name']
-        assert report_version.report_operation.operation_bcghgid != data['operation_bcghgid']
-        assert report_version.report_operation.bc_obps_regulated_operation_id != data['bc_obps_regulated_operation_id']
-        assert report_version.report_type != data['operation_report_type']
+        assert report_version.report_operation.operator_legal_name != data["operator_legal_name"]
+        assert report_version.report_operation.operator_trade_name != data["operator_trade_name"]
+        assert report_version.report_operation.operation_name != data["operation_name"]
+        assert report_version.report_operation.operation_bcghgid != data["operation_bcghgid"]
+        assert report_version.report_operation.bc_obps_regulated_operation_id != data["bc_obps_regulated_operation_id"]
+        assert report_version.report_operation.operation_representative_name != data["operation_representative_name"]
+        assert report_version.report_type != data["operation_report_type"]
 
         response = TestUtils.mock_post_with_auth_role(
-            self, 'industry_user', self.content_type, data, endpoint_under_test
+            self, "industry_user", self.content_type, data, endpoint_under_test
         )
 
         assert response.status_code == 201
         response_json = response.json()
-        assert response_json['operator_legal_name'] == data['operator_legal_name']
-        assert response_json['operator_trade_name'] == data['operator_trade_name']
-        assert response_json['operation_name'] == data['operation_name']
-        assert response_json['operation_bcghgid'] == data['operation_bcghgid']
-        assert response_json['bc_obps_regulated_operation_id'] == data['bc_obps_regulated_operation_id']
+        assert response_json["operator_legal_name"] == data["operator_legal_name"]
+        assert response_json["operator_trade_name"] == data["operator_trade_name"]
+        assert response_json["operation_name"] == data["operation_name"]
+        assert response_json["operation_bcghgid"] == data["operation_bcghgid"]
+        assert response_json["bc_obps_regulated_operation_id"] == data["bc_obps_regulated_operation_id"]
+        assert response_json["operation_representative_name"] == data["operation_representative_name"]
