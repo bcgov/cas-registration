ee6443d7af2c83b82b4be12cb3b9529ecabe00ba
shon-button <shon@button.is>
2025-01-15 17:07:09 -0500

    chore: update verification schema for LFO
    chore: cleanup
    
    chore: cleanup

--- bciers/apps/reporting/src/app/components/verification/createVerificationSchema.ts
+++ bciers/apps/reporting/src/app/components/verification/createVerificationSchema.ts
@@ -1,16 +1,47 @@
 import { RJSFSchema } from "@rjsf/utils";
 import { verificationSchema } from "@reporting/src/data/jsonSchema/verification/verification";
 
-export const createVerificationSchema = (facilities: string[]): RJSFSchema => {
+export const createVerificationSchema = (
+  facilities: string[],
+  schemaType: "SFO" | "LFO",
+): RJSFSchema => {
   // Retrieve a local copy of the base verification schema based
   const localSchema = { ...verificationSchema };
 
-  // Dynamically populate the "visited_facilities" field's enum with the facilities
-  (localSchema.properties?.visit_name as any).enum = [
-    ...facilities,
-    "Other",
-    "None",
-  ];
+  switch (schemaType) {
+    case "SFO":
+      // Dynamically populate the "visited_facilities" field's enum with the facilities
+      (localSchema.properties?.visit_names as any).enum = [
+        ...facilities,
+        "Other",
+        "None",
+      ];
+      break;
+    case "LFO":
+      // Ensure properties exist before modifying
+      localSchema.properties = localSchema.properties ?? {};
+
+      // Remove old visit_names if they exist
+      delete localSchema.properties.visit_names;
+      if (localSchema.dependencies) {
+        delete localSchema.dependencies.visit_names;
+      }
+
+      // Add the new visit_names property
+      localSchema.properties.visit_names = {
+        type: "array",
+        title: "Sites visited",
+        items: {
+          type: "string",
+          enum: Array.isArray(facilities)
+            ? [...facilities, "Other", "None"]
+            : ["Other", "None"],
+        },
+        uniqueItems: true,
+      };
+
+      break;
+  }
 
   // Return the customized schema.
   return localSchema;
