23663695c38c44e3438f5aedad4d1a75292f7e6d
Brianna Cerkiewicz <brianna@button.is>
2025-01-16 13:14:56 -0800

    feat: replace operation rep

--- bciers/apps/administration/app/components/operations/OperationInformationForm.tsx
+++ bciers/apps/administration/app/components/operations/OperationInformationForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import { UUID } from "crypto";
 import SingleStepTaskListForm from "@bciers/components/form/SingleStepTaskListForm";
@@ -18,6 +18,7 @@ import {
 } from "apps/registration/app/components/operations/registration/enums";
 import { FormMode, FrontEndRoles } from "@bciers/utils/src/enums";
 import { useSessionRole } from "@bciers/utils/src/sessionUtils";
+import Note from "@bciers/components/layout/Note";
 
 const OperationInformationForm = ({
   formData,
@@ -29,11 +30,11 @@ const OperationInformationForm = ({
   schema: RJSFSchema;
 }) => {
   const [error, setError] = useState(undefined);
-
   const router = useRouter();
-
   // To get the user's role from the session
   const role = useSessionRole();
+  const searchParams = useSearchParams();
+  const isRedirectedFromContacts = searchParams.get("from_contacts") as string;
 
   const handleSubmit = async (data: {
     formData?: OperationInformationFormData;
@@ -67,29 +68,36 @@ const OperationInformationForm = ({
       return { error: response2.error };
     }
   };
-
   return (
-    <SingleStepTaskListForm
-      allowEdit={!role.includes("cas_")}
-      mode={FormMode.READ_ONLY}
-      error={error}
-      schema={schema}
-      uiSchema={administrationOperationInformationUiSchema}
-      formData={formData ?? {}}
-      onSubmit={handleSubmit}
-      onCancel={() => router.push("/operations")}
-      formContext={{
-        operationId,
-        isRegulatedOperation: regulatedOperationPurposes.includes(
-          formData.registration_purpose as RegistrationPurposes,
-        ),
-        isCasDirector: role === FrontEndRoles.CAS_DIRECTOR,
-        isEio: formData.registration_purpose?.match(
-          RegistrationPurposes.ELECTRICITY_IMPORT_OPERATION.valueOf(),
-        ),
-        status: formData.status,
-      }}
-    />
+    <>
+      {isRedirectedFromContacts && (
+        <Note variant="important">
+          To remove the current operation representative, please select a new
+          contact to replace them.
+        </Note>
+      )}
+      <SingleStepTaskListForm
+        allowEdit={!role.includes("cas_")}
+        mode={FormMode.READ_ONLY}
+        error={error}
+        schema={schema}
+        uiSchema={administrationOperationInformationUiSchema}
+        formData={formData ?? {}}
+        onSubmit={handleSubmit}
+        onCancel={() => router.push("/operations")}
+        formContext={{
+          operationId,
+          isRegulatedOperation: regulatedOperationPurposes.includes(
+            formData.registration_purpose as RegistrationPurposes,
+          ),
+          isCasDirector: role === FrontEndRoles.CAS_DIRECTOR,
+          isEio: formData.registration_purpose?.match(
+            RegistrationPurposes.ELECTRICITY_IMPORT_OPERATION.valueOf(),
+          ),
+          status: formData.status,
+        }}
+      />
+    </>
   );
 };
 
