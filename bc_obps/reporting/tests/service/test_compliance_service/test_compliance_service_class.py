9d450187d517b324a7dbf6102d7873cc27386a14
Dylan Leard <dylan@button.is>
2025-01-15 16:56:13 -0800

    test: update test

--- bc_obps/reporting/tests/service/test_compliance_service/test_compliance_service_class.py
+++ bc_obps/reporting/tests/service/test_compliance_service/test_compliance_service_class.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from reporting.models.report_emission import ReportEmission
 from reporting.service.compliance_service import ComplianceService
 from infrastructure import ComplianceTestInfrastructure
+from decimal import Decimal
 
 
 class TestComplianceSummaryServiceClass(TestCase):
@@ -9,24 +9,7 @@ class TestComplianceSummaryServiceClass(TestCase):
         build_data = ComplianceTestInfrastructure.build()
         ComplianceTestInfrastructure.pare_data_single_product_flaring()
         result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
-        emissions = ReportEmission.objects.all()
-        print(emissions)
-        print('### RESULT ###')
-        print(result.__dict__)
 
-    def test_with_industrial_process_emissions(self):
-        build_data = ComplianceTestInfrastructure.build()
-        ComplianceTestInfrastructure.pare_data_remove_reporting_only()
-        result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
-        emissions = ReportEmission.objects.all()
-        print(emissions)
-        print('### RESULT ###')
-        print(result.__dict__)
-
-    def test_compliance_summary_with_all_data(self):
-        build_data = ComplianceTestInfrastructure.build()
-        ## TESTS ##
-        result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
         print('### RESULT ###')
         print(result.__dict__)
         print('### REG VALUES ###')
@@ -34,3 +17,29 @@ class TestComplianceSummaryServiceClass(TestCase):
         print('### PRODUCTS ###')
         for p in result.products:
             print(p.__dict__)
+        assert result.emissions_attributable_for_reporting == Decimal('10000.0001')
+        # assert result.emissions_attributable_for_compliance == Decimal('5000.0001')
+        assert result.emissions_limit == Decimal('159.25')
+        assert result.excess_emissions == Decimal('4840.75')
+        assert result.credited_emissions == 0
+
+    # def test_with_industrial_process_emissions(self):
+    #     build_data = ComplianceTestInfrastructure.build()
+    #     ComplianceTestInfrastructure.pare_data_remove_reporting_only()
+    #     result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
+    #     emissions = ReportEmission.objects.all()
+    #     print(emissions)
+    #     print('### RESULT ###')
+    #     print(result.__dict__)
+
+    # def test_compliance_summary_with_all_data(self):
+    #     build_data = ComplianceTestInfrastructure.build()
+    #     ## TESTS ##
+    #     result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
+    #     print('### RESULT ###')
+    #     print(result.__dict__)
+    #     print('### REG VALUES ###')
+    #     print(result.regulatory_values.__dict__)
+    #     print('### PRODUCTS ###')
+    #     for p in result.products:
+    #         print(p.__dict__)
