fdab5dd4b0a3d336dc2376da1106f18786f607c4
Dylan Leard <dylan@button.is>
2025-01-17 16:50:44 -0800

    test: remove potential side effects of test infrastructure

--- bc_obps/reporting/tests/service/test_compliance_service/test_compliance_service_class.py
+++ bc_obps/reporting/tests/service/test_compliance_service/test_compliance_service_class.py
@@ -7,8 +7,7 @@ from decimal import Decimal
 class TestComplianceSummaryServiceClass(TestCase):
     def test_compliance_summary_only_flaring_single_product(self):
         # Assertion values from compliance_class_manual_calcs.xlsx sheet 1
-        build_data = ComplianceTestInfrastructure.build()
-        ComplianceTestInfrastructure.pare_data_single_product_flaring()
+        build_data = ComplianceTestInfrastructure.pare_data_single_product_flaring()
 
         result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
 
@@ -19,8 +18,7 @@ class TestComplianceSummaryServiceClass(TestCase):
 
     def test_with_industrial_process_emissions(self):
         # Assertion values from compliance_class_manual_calcs.xlsx sheet 2
-        build_data = ComplianceTestInfrastructure.build()
-        ComplianceTestInfrastructure.pare_data_remove_reporting_only()
+        build_data = ComplianceTestInfrastructure.pare_data_remove_reporting_only()
 
         result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
 
@@ -46,8 +44,7 @@ class TestComplianceSummaryServiceClass(TestCase):
 
     def test_compliance_summary_2025_period(self):
         # Assertion values from compliance_class_manual_calcs.xlsx sheet 4
-        build_data = ComplianceTestInfrastructure.build()
-        ComplianceTestInfrastructure.reporting_year_2025()
+        build_data = ComplianceTestInfrastructure.reporting_year_2025()
 
         result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
 
@@ -59,8 +56,7 @@ class TestComplianceSummaryServiceClass(TestCase):
         assert result.credited_emissions == 0
 
     def test_new_entrant(self):
-        build_data = ComplianceTestInfrastructure.build()
-        ComplianceTestInfrastructure.new_entrant()
+        build_data = ComplianceTestInfrastructure.new_entrant()
         ## TESTS ##
         result = ComplianceService.get_calculated_compliance_data(build_data.report_version_1.id)
 
