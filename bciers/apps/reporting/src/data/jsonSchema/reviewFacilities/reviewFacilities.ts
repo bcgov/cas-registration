f5a9f96631e68fc175fc24915b0d880bb6689745
acatchpole <adamantiumite@gmail.com>
2025-01-20 16:51:38 -0800

    chore: continued WIP RJSF schema

--- bciers/apps/reporting/src/data/jsonSchema/reviewFacilities/reviewFacilities.ts
+++ bciers/apps/reporting/src/data/jsonSchema/reviewFacilities/reviewFacilities.ts
@@ -1,9 +1,21 @@
 import { RJSFSchema } from "@rjsf/utils";
 import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
 import { infoNote, instructionNote, SyncFacilitiesButton } from "./reviewFacilitiesInfoText"
-import { TitleOnlyFieldTemplate, SectionFieldTemplate } from "@bciers/components/form/fields";
+import { TitleOnlyFieldTemplate, SectionFieldTemplate, ArrayFieldTemplate } from "@bciers/components/form/fields";
+import { CheckboxGroupWidget } from "@bciers/components/form/widgets";
 
-export const reviewFacilitiesSchema: RJSFSchema = {
+export const reviewFacilitiesSchema = (
+  current_facilities: {
+    facility_id: string;
+    facility_name: string;
+    is_selected: boolean;
+  }[],
+  past_facilities: {
+    facility_id: string;
+    facility_name: string;
+    is_selected: boolean;
+  }[],
+): RJSFSchema => ({
   title: "Review Facilities",
   type: "object",
   properties: {
@@ -15,11 +27,25 @@ export const reviewFacilitiesSchema: RJSFSchema = {
       type: "object",
       readOnly: true,
     },
+    current_facilities: {
+      type: "array",
+      items: {
+        type: "string",
+        enum: current_facilities.map((facility) => facility.facility_name),
+      },
+    },
+    past_facilities: {
+      type: "array",
+      items: {
+        type: "string",
+        enum: past_facilities.map((facility) => facility.facility_name),
+      },
+    },
     sync_button: {
       type: "object",
     },
   },
-};
+});
 
 export const reviewFacilitiesUiSchema = {
   "ui:FieldTemplate": FieldTemplate,
@@ -40,12 +66,18 @@ export const reviewFacilitiesUiSchema = {
     "ui:title": instructionNote,
   },
   current_facilities: {
-    "ui:FieldTemplate": SectionFieldTemplate,
     "ui:title": "List of facilities currently assigned to this operation",
+    "ui:widget": CheckboxGroupWidget,
+    "ui:options": {
+      inline: true,
+    },
   },
   past_facilities: {
-    "ui:FieldTemplate": SectionFieldTemplate,
     "ui:title": "Past facilities that belonged to this operation",
+    "ui:widget": CheckboxGroupWidget,
+    "ui:options": {
+      inline: true,
+    },
   },
   sync_button: {
     "ui:FieldTemplate": SyncFacilitiesButton,
