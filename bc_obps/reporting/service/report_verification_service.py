63580afa11eb96e87faaa9fcd2f5a2975bcaf5b9
shon-button <shon@button.is>
2025-01-20 19:15:43 -0500

    feat: report verification for LFO

--- bc_obps/reporting/service/report_verification_service.py
+++ bc_obps/reporting/service/report_verification_service.py
@@ -3,6 +3,7 @@ from django.db import transaction
 from reporting.models.report_verification import ReportVerification
 from reporting.models import ReportVersion
 from reporting.schema.report_verification import ReportVerificationIn
+from reporting.schema.report_verification import ReportVerificationOut
 
 from registration.models import Operation
 from reporting.service.report_additional_data import ReportAdditionalDataService
@@ -13,7 +14,7 @@ class ReportVerificationService:
     @staticmethod
     def get_report_verification_by_version_id(
         report_version_id: int,
-    ) -> ReportVerification:
+    ) -> ReportVerificationOut:
         """
         Retrieve a ReportVerification instance for a given report version ID.
 
@@ -21,9 +22,11 @@ class ReportVerificationService:
             version_id: The report version ID
 
         Returns:
-            ReportVerification instance
+            ReportVerificationOut schema
         """
-        return ReportVerification.objects.get(report_version__id=report_version_id)
+        report_verification = ReportVerification.objects.get(report_version__id=report_version_id)
+        report_verification.report_verification_visits = report_verification.reportverificationvisit_records.all() 
+        return report_verification
 
     @staticmethod
     @transaction.atomic
@@ -45,10 +48,6 @@ class ReportVerificationService:
             "scope_of_verification": data.scope_of_verification,
             "threats_to_independence": data.threats_to_independence,
             "verification_conclusion": data.verification_conclusion,
-            "visit_name": data.visit_name,
-            "visit_type": data.visit_type,
-            "other_facility_name": data.other_facility_name,
-            "other_facility_coordinates": data.other_facility_coordinates,
         }
         report_version = ReportVersion.objects.get(pk=version_id)
         # Update or create ReportVerification record
