03ca1e8bd20c2434d112fb1304d7bcdd77f4852d
acatchpole <adamantiumite@gmail.com>
2025-01-21 16:59:23 -0800

    chore: WIP confirmation modal for facility deselection in ReviewFacilitiesForm

--- bciers/apps/reporting/src/app/components/operations/reviewFacilities/ReviewFacilitiesForm.tsx
+++ bciers/apps/reporting/src/app/components/operations/reviewFacilities/ReviewFacilitiesForm.tsx
@@ -13,6 +13,7 @@ import {
   ActivePage,
 } from "@reporting/src/app/components/taskList/1_operationInformation";
 import { multiStepHeaderSteps } from "@reporting/src/app/components/taskList/multiStepHeaderConfig";
+import SimpleModal from "@bciers/components/modal/SimpleModal";
 
 interface Props {
   initialData: any;
@@ -22,6 +23,8 @@ interface Props {
 export default function LFOFacilitiesForm({ initialData, version_id }: Props) {
   const [formData, setFormData] = useState(() => ({ ...initialData }));
   const [errors, setErrors] = useState<string[] | undefined>(undefined);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [confirmedToChange, setConfirmedToChange] = useState(true);
   const saveAndContinueUrl = `/reports/${version_id}/report-information`;
   const backUrl = `/reports/${version_id}/person-responsible`;
 
@@ -36,9 +39,54 @@ export default function LFOFacilitiesForm({ initialData, version_id }: Props) {
     "Linear Facility Operation",
   );
 
+    const checkIfFacilityReportWillBeDeleted = () => {
+      if (
+        !formData ||
+        !formData.current_facilities_section ||
+        !formData.current_facilities_section.current_facilities
+      ){
+        console.log("NO FORM DATA");
+        return null;
+      }
+        
+      const presentFacilities =
+        formData.current_facilities_section.current_facilities.concat(
+          formData.past_facilities_section.past_facilities,
+        );
+      const previousFacilities = formData.current_facilities.concat(
+        initialData.past_facilities,
+      );
+      // first check if a facility has been deselected
+      if (presentFacilities.length >= previousFacilities.length) {
+        console.log("NO FACILITY DESELECTED");
+        return null;
+      }
+
+      const deselectedFacilities = previousFacilities.filter(
+        (facility) => !presentFacilities.includes(facility),
+      );
+      console.log("***********deselectedFacilities", deselectedFacilities);
+      return deselectedFacilities;
+    };
+
   const handleChange = (e: any) => {
+    console.log("*******handleChange event", e);
+    checkIfFacilityReportWillBeDeleted();
+    setModalOpen(true);
+    
     setFormData({ ...e.formData });
   };
+
+  const handleModalCancel = () => {
+    setConfirmedToChange(false);
+    setModalOpen(false);
+  }
+
+  const handleModalConfirm = () => {
+    setConfirmedToChange(true);
+    setModalOpen(false);
+    //setFormData({ ...formData });
+  }
 
   const handleSubmit = async (data: any) => {
     const endpoint = `reporting/report-version/${version_id}/review-facilities`;
@@ -63,18 +111,31 @@ export default function LFOFacilitiesForm({ initialData, version_id }: Props) {
   };
 
   return (
-    <MultiStepFormWithTaskList
-      formData={formData}
-      schema={schema}
-      uiSchema={reviewFacilitiesUiSchema}
-      taskListElements={taskListElements}
-      steps={multiStepHeaderSteps}
-      errors={errors}
-      continueUrl={saveAndContinueUrl}
-      initialStep={1}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-      backUrl={backUrl}
-    />
+    <>
+      <SimpleModal
+        title="Confirmation"
+        open={modalOpen}
+        onCancel={ handleModalCancel }
+        onConfirm={ handleModalConfirm }
+        confirmText="Deselect Facility"
+      >
+        <p>
+          This facility has an existing report. Deselecting this facility will delete this facility&apos;s report on Save. Are you sure you want to deselect?
+        </p>
+      </SimpleModal>
+      <MultiStepFormWithTaskList
+        formData={formData}
+        schema={schema}
+        uiSchema={reviewFacilitiesUiSchema}
+        taskListElements={taskListElements}
+        steps={multiStepHeaderSteps}
+        errors={errors}
+        continueUrl={saveAndContinueUrl}
+        initialStep={1}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        backUrl={backUrl}
+      />
+    </>
   );
 }
