735c4175ccb2c95875528d7f36ffb60227f808b5
SeSo <sobhani.sepehr@gmail.com>
2025-01-23 14:55:24 -0800

    chore: add comment and make pre-commit happy
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/rls/middleware/rls.py
+++ bc_obps/rls/middleware/rls.py
@@ -8,11 +8,13 @@ from registration.models import User
 
 logger = logging.getLogger(__name__)
 
+
 class RlsMiddleware:
     """
     Middleware to set the user context for Row Level Security (RLS) in PostgreSQL.
     NOTE: We need to use this middleware after CurrentUserMiddleware to be able to access the current_user attribute
     """
+
     def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]):
         self.get_response = get_response
 
@@ -22,12 +24,12 @@ class RlsMiddleware:
             rls_index = settings.MIDDLEWARE.index("rls.middleware.rls.RlsMiddleware")
             current_user_index = settings.MIDDLEWARE.index(current_user_middleware)
             if current_user_index > rls_index:
-                raise RuntimeError(
-                    "CurrentUserMiddleware must be placed before RlsMiddleware in MIDDLEWARE settings."
-                )
+                raise RuntimeError("CurrentUserMiddleware must be placed before RlsMiddleware in MIDDLEWARE settings.")
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
         user: Optional[User] = getattr(request, 'current_user', None)
+        # Note from Dylan for later:
+        # when we get to actually setting roles, we'll want to still set a role here. We might need an "Unauthenticated" role to set in this case.
         if user:
             self._set_user_context(user.user_guid)
         else:
