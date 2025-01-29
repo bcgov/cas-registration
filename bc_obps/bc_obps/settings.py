92b4c3044b90f8a95de55a4a916525acc001feb5
Dylan Leard <dylan@button.is>
2025-01-28 16:11:48 -0800

    chore: add django-pgtrigger package

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'simple_history',
     "corsheaders",
     "localflavor",
+    "pgtrigger",
     # Local apps
     "registration.apps.RegistrationConfig",
     "reporting.apps.ReportingConfig",
