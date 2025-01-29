5f5eb5e0d983546289520b6cf610cc5fdc349294
Andrea Williams <andrea.williams@gov.bc.ca>
2025-01-29 13:23:10 -0800

    chore: in progress. Confused

--- bciers/apps/administration/app/components/operations/OperationInformationForm.tsx
+++ bciers/apps/administration/app/components/operations/OperationInformationForm.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import { UUID } from "crypto";
 import SingleStepTaskListForm from "@bciers/components/form/SingleStepTaskListForm";
 import { RJSFSchema } from "@rjsf/utils";
+import { IChangeEvent } from "@rjsf/core";
 import { administrationOperationInformationUiSchema } from "../../data/jsonSchema/operationInformation/administrationOperationInformation";
 import {
   OperationInformationFormData,
@@ -20,6 +21,7 @@ import { FormMode, FrontEndRoles } from "@bciers/utils/src/enums";
 import { useSessionRole } from "@bciers/utils/src/sessionUtils";
 import Note from "@bciers/components/layout/Note";
 import Link from "next/link";
+import ConfirmChangeOfRegistrationPurposeModal from "apps/registration/app/components/operations/registration/ConfirmChangeOfRegistrationPurposeModal";
 
 const OperationInformationForm = ({
   formData,
@@ -31,6 +33,13 @@ const OperationInformationForm = ({
   schema: RJSFSchema;
 }) => {
   const [error, setError] = useState(undefined);
+  const [selectedPurpose, setSelectedPurpose] = useState(
+    formData.registration_purpose || "",
+  );
+  const [
+    pendingChangeRegistrationPurpose,
+    setPendingChangeRegistrationPurpose,
+  ] = useState("");
   const router = useRouter();
   // To get the user's role from the session
   const role = useSessionRole();
@@ -80,6 +89,32 @@ const OperationInformationForm = ({
       return { error: response2.error };
     }
   };
+
+  const cancelRegistrationPurposeChange = () => {
+    setPendingChangeRegistrationPurpose("");
+  };
+
+  const confirmRegistrationPurposeChange = () => {
+    if (pendingChangeRegistrationPurpose !== "") {
+      setSelectedPurpose(pendingChangeRegistrationPurpose);
+    }
+    setPendingChangeRegistrationPurpose("");
+  };
+
+  const handleSelectedPurposeChange = (newSelectedPurpose: string) => {
+    consoleLogRPs();
+    if (newSelectedPurpose && selectedPurpose) {
+      setPendingChangeRegistrationPurpose(newSelectedPurpose);
+    }
+    consoleLogRPs();
+  };
+
+  const consoleLogRPs = () => {
+    console.log("form data RP ", formData.registration_purpose);
+    console.log("selectedPurpose ", selectedPurpose);
+    console.log("pendingChangeRP ", pendingChangeRegistrationPurpose);
+  };
+
   return (
     <>
       {isRedirectedFromContacts && !role.includes("cas_") && (
@@ -88,6 +123,12 @@ const OperationInformationForm = ({
           contact to replace them.
         </Note>
       )}
+      {console.log(formData)}
+      <ConfirmChangeOfRegistrationPurposeModal
+        open={pendingChangeRegistrationPurpose !== ""}
+        cancelRegistrationPurposeChange={cancelRegistrationPurposeChange}
+        confirmRegistrationPurposeChange={confirmRegistrationPurposeChange}
+      />
       <SingleStepTaskListForm
         allowEdit={!role.includes("cas_")}
         mode={FormMode.READ_ONLY}
@@ -96,6 +137,16 @@ const OperationInformationForm = ({
         uiSchema={administrationOperationInformationUiSchema}
         formData={formData ?? {}}
         onSubmit={handleSubmit}
+        onChange={(e: IChangeEvent) => {
+          console.log(e);
+          console.log(formData);
+          let newSelectedPurpose = e.formData?.section3?.registration_purpose;
+          consoleLogRPs();
+          console.log("new selected purpose ", newSelectedPurpose);
+          if (newSelectedPurpose !== selectedPurpose) {
+            handleSelectedPurposeChange(newSelectedPurpose);
+          }
+        }}
         onCancel={() => router.push("/operations")}
         formContext={{
           operationId,
