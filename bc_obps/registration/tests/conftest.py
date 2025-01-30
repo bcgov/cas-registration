004ac4e3420c6c2d3f868264e7a3689025f752c7
Dylan Leard <dylan@button.is>
2025-01-30 15:37:15 -0800

    test: change conftest scope to 'class'

--- bc_obps/registration/tests/conftest.py
+++ bc_obps/registration/tests/conftest.py
@@ -3,8 +3,8 @@ from django.db import connection
 from common.tests.utils.helpers import set_db_user_guid_for_tests
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="class", autouse=True)
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        with connection.cursor() as cursor:
-            set_db_user_guid_for_tests(cursor)
+        set_db_user_guid_for_tests(connection.cursor())
+        yield
