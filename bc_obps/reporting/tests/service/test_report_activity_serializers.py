5f82e937eaecf6b211767b8472aed091bb552109
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-01-29 16:02:35 -0800

    chore: load and save units without fuel

--- bc_obps/reporting/tests/service/test_report_activity_serializers.py
+++ bc_obps/reporting/tests/service/test_report_activity_serializers.py
@@ -26,28 +26,36 @@ class TestReportActivityDataSerializers(SimpleTestCase):
         serialized = ReportMethodologySerializer.serialize(report_methodology)
 
         assert serialized == {
-            'id': 1983898,
-            'methodology': 'Test Methodology',
-            'field': 'field',
-            'a': True,
+            "id": 1983898,
+            "methodology": "Test Methodology",
+            "field": "field",
+            "a": True,
         }
 
     @patch("reporting.models.report_emission.ReportEmission.report_methodology")
     @patch("reporting.service.report_activity_serializers.ReportMethodologySerializer.serialize")
     def test_report_emissions_serializer(self, mock_methodology_serializer: MagicMock, mock_reverse_manager: MagicMock):
-        mock_methodology_serializer.return_value = 'serialized!'
+        mock_methodology_serializer.return_value = "serialized!"
         report_emissions = [
             prepare(
                 ReportEmission,
                 id=92871,
                 gas_type__chemical_formula="CaS",
-                json_data={"testprop1": "test", "emission": 1234, "methodology": {"methodology": "data"}},
+                json_data={
+                    "testprop1": "test",
+                    "emission": 1234,
+                    "methodology": {"methodology": "data"},
+                },
             ),
             prepare(
                 ReportEmission,
                 id=987134,
                 gas_type__chemical_formula="GaS",
-                json_data={"testprop2": "test", "emission": 0, "methodology": {"methodology2": "data2"}},
+                json_data={
+                    "testprop2": "test",
+                    "emission": 0,
+                    "methodology": {"methodology2": "data2"},
+                },
             ),
         ]
 
@@ -60,18 +68,18 @@ class TestReportActivityDataSerializers(SimpleTestCase):
 
         assert serialized == [
             {
-                'emission': 1234,
-                'gasType': 'CaS',
-                'id': 92871,
-                'testprop1': 'test',
-                'methodology': 'serialized!',
+                "emission": 1234,
+                "gasType": "CaS",
+                "id": 92871,
+                "testprop1": "test",
+                "methodology": "serialized!",
             },
             {
-                'emission': 0,
-                'gasType': 'GaS',
-                'id': 987134,
-                'testprop2': 'test',
-                'methodology': 'serialized!',
+                "emission": 0,
+                "gasType": "GaS",
+                "id": 987134,
+                "testprop2": "test",
+                "methodology": "serialized!",
             },
         ]
 
@@ -83,17 +91,17 @@ class TestReportActivityDataSerializers(SimpleTestCase):
             prepare(
                 ReportFuel,
                 id=2,
-                fuel_type__name='testFuelName',
-                fuel_type__unit='testFuelUnit',
-                fuel_type__classification='testFuelClass',
+                fuel_type__name="testFuelName",
+                fuel_type__unit="testFuelUnit",
+                fuel_type__classification="testFuelClass",
                 json_data={"aaa": "bbb", "ccc": 123},
             ),
             prepare(
                 ReportFuel,
                 id=9876,
-                fuel_type__name='fuel2',
-                fuel_type__unit='unit2',
-                fuel_type__classification='class2',
+                fuel_type__name="fuel2",
+                fuel_type__unit="unit2",
+                fuel_type__classification="class2",
                 json_data={"x": "y", "z": 1},
             ),
         ]
@@ -104,53 +112,71 @@ class TestReportActivityDataSerializers(SimpleTestCase):
         assert mock_reverse_manager.all.call_count == 2
         assert serialized == [
             {
-                'aaa': 'bbb',
-                'ccc': 123,
-                'emissions': 'mock serialized!!',
-                'fuelType': {
-                    'fuelClassification': 'testFuelClass',
-                    'fuelName': 'testFuelName',
-                    'fuelUnit': 'testFuelUnit',
+                "aaa": "bbb",
+                "ccc": 123,
+                "emissions": "mock serialized!!",
+                "fuelType": {
+                    "fuelClassification": "testFuelClass",
+                    "fuelName": "testFuelName",
+                    "fuelUnit": "testFuelUnit",
                 },
-                'id': 2,
+                "id": 2,
             },
             {
-                'emissions': 'mock serialized!!',
-                'fuelType': {
-                    'fuelClassification': 'class2',
-                    'fuelName': 'fuel2',
-                    'fuelUnit': 'unit2',
+                "emissions": "mock serialized!!",
+                "fuelType": {
+                    "fuelClassification": "class2",
+                    "fuelName": "fuel2",
+                    "fuelUnit": "unit2",
                 },
-                'id': 9876,
-                'x': 'y',
-                'z': 1,
+                "id": 9876,
+                "x": "y",
+                "z": 1,
             },
         ]
 
+    @patch("reporting.models.report_unit.ReportUnit.reportemission_records")
     @patch("reporting.models.report_unit.ReportUnit.reportfuel_records")
     @patch("reporting.service.report_activity_serializers.ReportFuelIterableSerializer.serialize")
-    def test_report_unit_serializer(self, mock_fuels_serializer: MagicMock, mock_reverse_manager: MagicMock):
-        mock_fuels_serializer.return_value = 'serialized!'
+    @patch("reporting.service.report_activity_serializers.ReportEmissionIterableSerializer.serialize")
+    def test_report_unit_serializer(
+        self,
+        mock_emissions_serializer: MagicMock,
+        mock_fuels_serializer: MagicMock,
+        mock_fuels_reverse_manager: MagicMock,
+        mock_emissions_reverse_manager: MagicMock,
+    ):
+        mock_fuels_serializer.return_value = "serialized!"
         mock_fuels_serializer.ass
         report_units = [
-            prepare(ReportUnit, id=8971, json_data={"mock_json_prop": True}),
-            prepare(ReportUnit, id=999999, json_data={"real_json_prop": False}),
+            prepare(
+                ReportUnit,
+                id=8971,
+                json_data={"mock_json_prop": True},
+                report_source_type__activity_source_type_base_schema__has_fuel=True,
+            ),
+            prepare(
+                ReportUnit,
+                id=999999,
+                json_data={"real_json_prop": False},
+                report_source_type__activity_source_type_base_schema__has_fuel=False,
+            ),
         ]
 
         serialized = ReportUnitIterableSerializer.serialize(report_units)
 
         assert mock_fuels_serializer.call_count == 2
-        assert mock_reverse_manager.all.call_count == 2
+        assert mock_fuels_reverse_manager.all.call_count == 2
         assert serialized == [
             {
-                'fuels': 'serialized!',
-                'id': 8971,
-                'mock_json_prop': True,
+                "fuels": "serialized!",
+                "id": 8971,
+                "mock_json_prop": True,
             },
             {
-                'fuels': 'serialized!',
-                'id': 999999,
-                'real_json_prop': False,
+                "fuels": "serialized!",
+                "id": 999999,
+                "real_json_prop": False,
             },
         ]
 
@@ -218,24 +244,24 @@ class TestReportActivityDataSerializers(SimpleTestCase):
         assert mock_fuel_reverse_manager.all.call_count == 1
         assert mock_emission_reverse_manager.all.call_count == 1
         assert serialized == {
-            'stWithUnit': {
-                'id': 1,
-                'units': 'units serialized!',
-                'with_unit_1': 1,
+            "stWithUnit": {
+                "id": 1,
+                "units": "units serialized!",
+                "with_unit_1": 1,
             },
-            'anotherStWithUnit': {
-                'id': 2,
-                'units': 'units serialized!',
-                'with_unit_2': 1,
+            "anotherStWithUnit": {
+                "id": 2,
+                "units": "units serialized!",
+                "with_unit_2": 1,
             },
-            'stWithFuelOnly': {
-                'fuels': 'fuels serialized!',
-                'id': 3,
-                'with_fuel_only': 1,
+            "stWithFuelOnly": {
+                "fuels": "fuels serialized!",
+                "id": 3,
+                "with_fuel_only": 1,
             },
-            'stWithEmissionOnly': {
-                'emissions': 'emissions serialized!',
-                'id': 4,
-                'with_emission_only': 1,
+            "stWithEmissionOnly": {
+                "emissions": "emissions serialized!",
+                "id": 4,
+                "with_emission_only": 1,
             },
         }
