4f16ed7a22fece4fd5178cf1f1798571f58e91c1
Dylan Leard <dylan@button.is>
2025-01-29 16:42:00 -0800

    chore: fix typo

--- bc_obps/registration/tests/models/test_timestamped_model.py
+++ bc_obps/registration/tests/models/test_timestamped_model.py
@@ -5,8 +5,7 @@ from registration.models import Operator
 
 pytestmark = pytest.mark.django_db
 
-
-class TestTimeStamedModel:
+class TestTimeStampedModel:
     @staticmethod
     def test_create_update_audit_fields():
         # Create a timestamped table (operator)
