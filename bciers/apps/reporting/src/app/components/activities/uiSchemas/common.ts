f5ca90bfd724213461bbf633cd5f1eba2e2f4b9a
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-01-31 21:50:02 -0800

    chore: finished data entry for all source types

---
+++ bciers/apps/reporting/src/app/components/activities/uiSchemas/common.ts
@@ -0,0 +1,121 @@
+import {
+  FieldTemplate,
+  InlineFieldTemplate,
+} from "@bciers/components/form/fields";
+import NestedArrayFieldTemplate from "@bciers/components/form/fields/NestedArrayFieldTemplate";
+import SourceTypeBoxTemplate from "@bciers/components/form/fields/SourceTypeBoxTemplate";
+import CheckboxWidgetLeft from "@bciers/components/form/widgets/CheckboxWidgetLeft";
+
+export const emissionsFieldsUiSchema = {
+  "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:options": {
+    arrayAddLabel: "Add Emission",
+    title: "Emission",
+    label: false,
+    verticalBorder: true,
+    padding: "p-2",
+  },
+  items: {
+    methodology: {
+      "ui:FieldTemplate": FieldTemplate,
+      "ui:options": {
+        label: false,
+      },
+      methodology: {
+        "ui:FieldTemplate": InlineFieldTemplate,
+      },
+    },
+  },
+};
+
+export const fuelsFieldsUiSchema = {
+  "ui:title": "Fuel Data",
+  "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:options": {
+    arrayAddLabel: "Add Fuel",
+    label: false,
+    title: "Fuel",
+  },
+  items: {
+    "ui:order": ["fuelType", "annualFuelAmount", "emissions"],
+    fuelType: {
+      "ui:field": "fuelType",
+      "ui:FieldTemplate": FieldTemplate,
+      "ui:options": {
+        label: false,
+      },
+      fuelName: {
+        "ui:FieldTemplate": InlineFieldTemplate,
+      },
+      fuelUnit: {
+        "ui:FieldTemplate": InlineFieldTemplate,
+      },
+      fuelClassification: {
+        "ui:FieldTemplate": InlineFieldTemplate,
+      },
+    },
+    annualFuelAmount: {
+      "ui:FieldTemplate": InlineFieldTemplate,
+    },
+    emissions: emissionsFieldsUiSchema,
+  },
+};
+
+export const sourceTypeBoxUiSchema = {
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:widget": CheckboxWidgetLeft,
+  "ui:options": {
+    label: false,
+  },
+};
+
+export const sourceSubTypeWithFuelUiSchema = {
+  "ui:FieldTemplate": SourceTypeBoxTemplate,
+  units: {
+    "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:title": "Source sub-type data",
+    "ui:options": {
+      arrayAddLabel: "Add source sub-type",
+      label: false,
+      title: "Source sub-type",
+      padding: "p-2",
+    },
+    items: {
+      "ui:order": ["sourceSubType", "fuels"],
+      sourceSubType: {
+        "ui:FieldTemplate": InlineFieldTemplate,
+      },
+      fuels: fuelsFieldsUiSchema,
+    },
+  },
+};
+
+export const sourceSubTypeWithoutFuelUiSchema = {
+  "ui:FieldTemplate": SourceTypeBoxTemplate,
+  units: {
+    "ui:ArrayFieldTemplate": NestedArrayFieldTemplate,
+    "ui:FieldTemplate": FieldTemplate,
+    "ui:title": "Source sub-type data",
+    "ui:options": {
+      arrayAddLabel: "Add source sub-type",
+      label: false,
+      title: "Source sub-type",
+      padding: "p-2",
+    },
+    items: {
+      "ui:order": ["sourceSubType", "emissions"],
+      sourceSubType: {
+        "ui:FieldTemplate": InlineFieldTemplate,
+      },
+      emissions: emissionsFieldsUiSchema,
+    },
+  },
+};
+
+export const emissionsOnlyUiSchema = {
+  "ui:FieldTemplate": SourceTypeBoxTemplate,
+  emissions: emissionsFieldsUiSchema,
+};
