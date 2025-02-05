0a9d80ac7d92d6221241d1588f24faabd290c151
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-02-05 10:16:51 -0800

    chore: fixed migrations after rebase

--- bc_obps/reporting/migrations/0053_facilityreport_is_completed.py
+++ bc_obps/reporting/migrations/0053_facilityreport_is_completed.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reporting', '0051_natural_gas_non_compression_configuration'),
+        ('reporting', '0052_add_audit_triggers_to_timestamped_models'),
     ]
 
     operations = [
