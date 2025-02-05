5c3373e2907837848e165f709e912d88994c9f50
SeSo <sobhani.sepehr@gmail.com>
2025-02-05 14:42:20 -0800

    chore: update conftest to fix tests
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/reporting/tests/conftest.py
+++ bc_obps/reporting/tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
-from django.db import connection
 from common.tests.utils.helpers import set_db_user_guid_for_tests
 from rls.utils.manager import RlsManager
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='class', autouse=True)
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         RlsManager.re_apply_rls()
-        set_db_user_guid_for_tests(connection.cursor())
+        set_db_user_guid_for_tests()
