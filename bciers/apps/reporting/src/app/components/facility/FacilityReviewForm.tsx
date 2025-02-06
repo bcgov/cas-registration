4fc63a1c264d68b8ed5df17840122327e84915ed
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-02-05 17:25:36 -0800

    chore:added test =

--- bciers/apps/reporting/src/app/components/facility/FacilityReviewForm.tsx
+++ bciers/apps/reporting/src/app/components/facility/FacilityReviewForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import {
-  buildFacilitySchema,
+  ActivityData,
   facilityReviewUiSchema,
 } from "@reporting/src/data/jsonSchema/facilities";
 import { actionHandler } from "@bciers/actions";
@@ -10,15 +10,15 @@ import serializeSearchParams from "@bciers/utils/src/serializeSearchParams";
 import { TaskListElement } from "@bciers/components/navigation/reportingTaskList/types";
 import MultiStepFormWithTaskList from "@bciers/components/form/MultiStepFormWithTaskList";
 import { multiStepHeaderSteps } from "@reporting/src/app/components/taskList/multiStepHeaderConfig";
+import { RJSFSchema } from "@rjsf/utils";
 
 interface Props {
   version_id: number;
   facility_id: string;
-  operationType: string;
-  activitiesData: [];
-  selectedActivities: [];
+  activitiesData: ActivityData[];
   taskListElements: TaskListElement[];
   formsData: object;
+  schema: RJSFSchema;
 }
 
 interface Activity {
@@ -32,6 +32,7 @@ const FacilityReview: React.FC<Props> = ({
   activitiesData,
   taskListElements,
   formsData,
+  schema,
 }) => {
   const [formData, setFormData] = useState<any>(formsData);
   const queryString = serializeSearchParams(useSearchParams());
@@ -51,9 +52,11 @@ const FacilityReview: React.FC<Props> = ({
     const updatedFormData = {
       ...formData,
       activities: formData.activities
-        .map((activityName: string) => activityNameToIdMap.get(activityName))
-        .filter((id: number) => id !== undefined)
-        .map(String), // 🔥 Ensure all IDs are strings
+        .map((activityName: string) => {
+          return activityNameToIdMap.get(activityName); // No console.error() here, just return the ID (or undefined)
+        })
+        .filter((id: number | undefined) => id !== undefined) // Filter out undefined IDs
+        .map(String), // Ensure all IDs are strings
     };
     const response = await actionHandler(endpoint, method, pathToRevalidate, {
       body: JSON.stringify(updatedFormData),
@@ -69,7 +72,7 @@ const FacilityReview: React.FC<Props> = ({
 
   return (
     <MultiStepFormWithTaskList
-      schema={buildFacilitySchema(activitiesData)}
+      schema={schema}
       uiSchema={facilityReviewUiSchema}
       formData={formData}
       onSubmit={handleSubmit}
