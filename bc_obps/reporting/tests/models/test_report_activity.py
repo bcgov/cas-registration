3d2643db541dcdd29fb0bd02c4f390500609b4c2
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-02-06 17:16:12 -0800

    chore: test for report activity

--- bc_obps/reporting/tests/models/test_report_activity.py
+++ bc_obps/reporting/tests/models/test_report_activity.py
@@ -1,4 +1,6 @@
 from common.tests.utils.helpers import BaseTestCase
+from django.db import ProgrammingError
+import pytest
 from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
 from reporting.models.activity_json_schema import ActivityJsonSchema
 from reporting.models.report_activity import ReportActivity
@@ -17,7 +19,7 @@ class ReportActivityModelTest(BaseTestCase):
             json_data="{'test': 1}",
             activity_base_schema=ActivityJsonSchema.objects.first(),
             activity=ActivityJsonSchema.objects.first().activity,
-            facility_report=baker.make_recipe('reporting.tests.utils.facility_report'),
+            facility_report=baker.make_recipe("reporting.tests.utils.facility_report"),
         )
         cls.field_data = [
             *TIMESTAMP_COMMON_FIELDS,
@@ -27,3 +29,24 @@ class ReportActivityModelTest(BaseTestCase):
             ("reportsourcetype_records", "report source type", None, 0),
             ("facility_report", "facility report", None, None),
         ]
+
+    def test_report_activity_immutable_after_report_version_submitted(self):
+        report_version = baker.make_recipe("reporting.tests.utils.report_version")
+        report_activity = baker.make_recipe(
+            "reporting.tests.utils.report_activity",
+            report_version=report_version,
+            facility_report__report_version=report_version,
+        )
+
+        report_activity.json_data = "{'test': 'allow change'}"
+        report_activity.save()
+
+        report_version.status = "Submitted"
+        report_version.save()
+
+        with pytest.raises(
+            ProgrammingError,
+            match="reportactivity record is immutable after a report version has been submitted",
+        ):
+            report_activity.json_data = "{'test': 'forbid change'}"
+            report_activity.save()
