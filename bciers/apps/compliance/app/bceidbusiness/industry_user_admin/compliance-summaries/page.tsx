db10053940cb3a747967c1593f34d043d3f24bdd
Nataliia Piguliak <nataly.pigulyak@gmail.com>
2025-02-12 16:23:49 +0000

    feat: add compliance summary layout
    Signed-off-by: Nataliia Piguliak <nataly.pigulyak@gmail.com>

---
+++ bciers/apps/compliance/app/bceidbusiness/industry_user_admin/compliance-summaries/page.tsx
@@ -0,0 +1,12 @@
+import { generateMetadata } from "@bciers/components/layout/RootLayout";
+
+const title = "Compliance Summaries";
+export const metadata = generateMetadata(title);
+
+export default function ComplianceSummariesPage() {
+  return (
+    <div>
+      <h2>My Compliance</h2>
+    </div>
+  );
+}
