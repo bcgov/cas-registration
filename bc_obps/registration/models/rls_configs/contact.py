6b49681b596940ac8d4c1938d02a2175b749f396
SeSo <sobhani.sepehr@gmail.com>
2025-02-13 11:59:40 -0800

    chore: update contact data model grants
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/registration/models/rls_configs/contact.py
+++ bc_obps/registration/models/rls_configs/contact.py
@@ -6,9 +6,11 @@ from rls.utils.helpers import generate_rls_grants
 class Rls:
     role_grants_mapping = {
         RlsRoles.INDUSTRY_USER: [RlsOperations.SELECT, RlsOperations.INSERT, RlsOperations.UPDATE],
-        RlsRoles.CAS_DIRECTOR: [RlsOperations.SELECT],
+        # CAS_DIRECTOR can create new contacts when approving a user_operator
+        RlsRoles.CAS_DIRECTOR: [RlsOperations.SELECT, RlsOperations.INSERT],
         RlsRoles.CAS_ADMIN: [RlsOperations.SELECT],
-        RlsRoles.CAS_ANALYST: [RlsOperations.SELECT],
+        # CAS_ANALYST can create new contacts when approving a user_operator
+        RlsRoles.CAS_ANALYST: [RlsOperations.SELECT, RlsOperations.INSERT],
         RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
     }
     grants = generate_rls_grants(role_grants_mapping, RegistrationTableNames.CONTACT)
