df375ab18364f5ecead7a0df5385b29eec87c2e4
Slava <slava.ivanov@gov.bc.ca>
2025-02-18 22:57:00 -0800

    chore: add base compliance summaries tabel

--- bciers/apps/compliance/src/app/bceidbusiness/industry_user/compliance-summaries/page.tsx
+++ bciers/apps/compliance/src/app/bceidbusiness/industry_user/compliance-summaries/page.tsx
@@ -1,12 +1,20 @@
 import { generateMetadata } from "@bciers/components/layout/RootLayout";
+import { ComplianceSummariesSearchParams } from "@/compliance/src/app/components/compliance-summaries/types";
+import ComplianceSummariesPage from "@/compliance/src/app/components/compliance-summaries/ComplianceSummariesPage";
+import { Suspense } from "react";
+import Loading from "@bciers/components/loading/SkeletonForm";
 
 const title = "Compliance Summaries";
 export const metadata = generateMetadata(title);
 
-export default function ComplianceSummariesPage() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: ComplianceSummariesSearchParams;
+}) {
   return (
-    <div>
-      <h2>My Compliance</h2>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <ComplianceSummariesPage searchParams={searchParams} />
+    </Suspense>
   );
 }
