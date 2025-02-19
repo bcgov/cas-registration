ed727f2a6da001e27742e2a51a48d4ad75a87154
Dylan Leard <dylan@button.is>
2025-02-19 13:25:54 -0800

    chore: fix facility_report m2m rls

--- bc_obps/reporting/models/rls_configs/facility_report.py
+++ bc_obps/reporting/models/rls_configs/facility_report.py
@@ -1,6 +1,6 @@
 from reporting.enums.enums import ReportingTableNames
 from rls.enums import RlsRoles, RlsOperations
-from rls.utils.helpers import generate_rls_grants
+from rls.utils.helpers import generate_rls_grants, generate_m2m_rls
 
 
 class Rls:
@@ -17,3 +17,17 @@ class Rls:
         RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
     }
     grants = generate_rls_grants(role_grants_mapping, ReportingTableNames.FACILITY_REPORT)
+    m2m_models_grants_mapping = {
+        ReportingTableNames.FACILITY_REPORT_ACTIVITIES: {
+            RlsRoles.CAS_DIRECTOR: [
+                RlsOperations.SELECT,
+                RlsOperations.INSERT,
+                RlsOperations.UPDATE,
+                RlsOperations.DELETE,
+            ],
+            RlsRoles.CAS_ADMIN: [RlsOperations.SELECT],
+            RlsRoles.CAS_ANALYST: [RlsOperations.SELECT],
+            RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
+        }
+    }
+    m2m_rls_list = generate_m2m_rls(m2m_models_grants_mapping)
