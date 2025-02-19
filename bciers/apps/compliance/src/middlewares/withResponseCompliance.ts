7d88e081c31c8ea97128c5a2be1249ae2d704bef
Slava <slava.ivanov@gov.bc.ca>
2025-02-18 22:40:47 -0800

    chore: add base compliance app and routes

---
+++ bciers/apps/compliance/src/middlewares/withResponseCompliance.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { MiddlewareFactory } from "@bciers/middlewares";
+import { getToken } from "@bciers/actions";
+import { appName } from "../middleware";
+
+export const withResponseCompliance: MiddlewareFactory = () => {
+  return async (request: NextRequest) => {
+    const { pathname } = request.nextUrl;
+    if (pathname.endsWith(`/`)) {
+      return NextResponse.next();
+    }
+
+    const token = await getToken();
+    // Build rewrite to physical folder path which enforces authorization by IdP and role
+    request.nextUrl.pathname = `${token.identity_provider}/${
+      token.app_role
+    }${pathname.replace(`${appName}/`, "")}`;
+
+    return NextResponse.rewrite(request.nextUrl);
+  };
+}; 
