db8a0e700363ea89b4c442f36612dbddc60b97b5
Slava <slava.ivanov@gov.bc.ca>
2025-02-18 23:25:43 -0800

    chore: add laerts

--- bciers/apps/compliance/src/app/components/compliance-summaries/ComplianceSummaries.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summaries/ComplianceSummaries.tsx
@@ -8,6 +8,8 @@ import Link from "next/link";
 import HeaderSearchCell from "@bciers/components/datagrid/cells/HeaderSearchCell";
 import ComplianceSummariesDataGrid from "./ComplianceSummariesDataGrid";
 import { fetchComplianceSummariesPageData } from "./fetchComplianceSummariesPageData";
+import { Alert } from "@mui/material";
+import AlertIcon from "@bciers/components/icons/AlertIcon";
 
 export default async function ComplianceSummaries({
   searchParams,
@@ -21,7 +23,22 @@ export default async function ComplianceSummaries({
   }
 
   return (
-    <div className="mt-4">
+    <div className="flex flex-col">
+      <div className="mb-5">
+      <div className="mb-2">
+        <Alert severity="info" icon={<AlertIcon />}>
+          Your compliance obligation for the 2024 reporting year is{" "}
+          <strong>due on November 30, 2025</strong>. Please pay five business days in
+          advance to account for the processing time.
+        </Alert>
+        </div>
+        <Alert severity="info" icon={<AlertIcon />}>
+          An automatic overdue penalty has been incurred and{" "}
+          <strong>accrues at 0.38% daily</strong> since the compliance obligation
+          was not paid by its due date. You may pay the penalty after the compliance
+          obligation is paid.
+        </Alert>
+      </div>
       <ComplianceSummariesDataGrid initialData={complianceSummaries} />
     </div>
   );
