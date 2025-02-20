ff67ab7468ff34e3222204c9f124f7d01c41eba9
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-02-20 10:09:56 -0800

    chore: fixed test and url

--- bciers/apps/reporting/src/tests/components/dataGrid/model/operations/operationColumns.test.tsx
+++ bciers/apps/reporting/src/tests/components/dataGrid/model/operations/operationColumns.test.tsx
@@ -1,7 +1,7 @@
-import { describe } from "vitest";
+import { describe, vi } from "vitest";
 import operationColumns from "@reporting/src/app/components/datagrid/models/operations/operationColumns";
 import { GridColDef } from "@mui/x-data-grid";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { useRouter } from "@bciers/testConfig/mocks";
 
@@ -18,6 +18,7 @@ describe("operationColumns function", () => {
   vi.mock("@reporting/src/app/utils/getReportingYear", () => ({
     getReportingYear: vi.fn().mockResolvedValue({ reporting_year: 2023 }),
   }));
+
   vi.mock("@reporting/src/app/utils/createReport", () => ({
     createReport: vi.fn().mockResolvedValue(1),
   }));
@@ -45,32 +46,33 @@ describe("operationColumns function", () => {
     assert(columns[1].width === 560, "Column 2 width should be 560");
 
     assert(
-      columns[2].field === "report_id",
-      'Column 3 field should be "report_id"',
+      columns[2].field === "report_status",
+      'Column 3 field should be "report_status"',
     );
-    assert(
-      columns[2].headerName === "Actions",
-      'Column 3 headerName should be "Actions"',
-    );
-    assert(columns[2].width === 120, "Column 3 width should be 120");
+    assert(columns[2].width === 200, "Column 3 width should be 200");
 
     assert(
-      columns[3].field === "report_status",
-      'Column 4 field should be "report_status"',
+      columns[3].field === "report_id",
+      'Column 4 field should be "report_id"',
     );
-    assert(columns[3].width === 160, "Column 4 width should be 160");
-
-    assert(columns[4].field === "more", 'Column 5 field should be "action"');
     assert(
-      columns[4].headerName === "More",
-      'Column 5 headerName should be "More"',
+      columns[3].headerName === "Actions",
+      'Column 4 headerName should be "Actions"',
     );
-    assert(columns[4].sortable === false, "Column 4 sortable should be false");
-    assert(columns[4].width === 120, "Column 4 width should be 120");
-    assert(columns[4].flex === 1, "Column 4 flex should be 1");
+    assert(columns[3].sortable === false, "Column 4 sortable should be false");
+    assert(columns[3].width === 200, "Column 4 width should be 200");
+
+    assert(columns[4].field === "more", 'Column 5 field should be "more"');
+    assert(
+      columns[4].headerName === "More Actions",
+      'Column 5 headerName should be "More Actions"',
+    );
+    assert(columns[4].sortable === false, "Column 5 sortable should be false");
+    assert(columns[4].width === 120, "Column 5 width should be 120");
+    assert(columns[4].flex === 1, "Column 5 flex should be 1");
   });
 
-  it("has a 'start' button in the 'Actions' column when report_id is null", () => {
+  it("has a 'start' button in the 'Actions' column when report_version_id is null", () => {
     const columns: GridColDef[] = operationColumns();
 
     const row = {
@@ -84,53 +86,18 @@ describe("operationColumns function", () => {
 
     const params = {
       row,
-      value: row.report_id,
+      value: row.report_version_id,
     };
 
     function WrapperComponent() {
-      // in order for hook to work
-      const cell = columns[2].renderCell;
+      const cell = columns[3].renderCell;
 
       return <div>{cell(params)}</div>;
     }
 
     render(<WrapperComponent />);
-    expect(screen.getByText("Start")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Start" })).toBeInTheDocument();
   });
-
-  it("navigates to the new report when clicking the 'Start' button", async () => {
-    const user = userEvent.setup();
-
-    const columns = operationColumns();
-
-    const row = {
-      id: "1",
-      bcghg_id: "12111130002",
-      name: "Operation with report",
-      report_id: null,
-      report_version_id: null,
-      report_status: null,
-    };
-
-    const params = {
-      row,
-      value: row.report_id,
-    };
-
-    function WrapperComponent() {
-      const cell = columns[2].renderCell;
-
-      return <div>{cell(params)}</div>;
-    }
-
-    render(<WrapperComponent />);
-    await user.click(screen.getByText("Start"));
-
-    await waitFor(() => {
-      expect(useRouter().push).toHaveBeenCalled();
-    });
-  });
-
   it("has a 'continue' button in the 'Actions' column when report_id exists", () => {
     const columns: GridColDef[] = operationColumns();
 
@@ -149,7 +116,7 @@ describe("operationColumns function", () => {
     };
 
     function WrapperComponent() {
-      const cell = columns[2].renderCell;
+      const cell = columns[3].renderCell;
 
       return <div>{cell(params)}</div>;
     }
@@ -178,7 +145,7 @@ describe("operationColumns function", () => {
     };
 
     function WrapperComponent() {
-      const cell = columns[2].renderCell;
+      const cell = columns[3].renderCell;
 
       return <div>{cell(params)}</div>;
     }
@@ -186,8 +153,6 @@ describe("operationColumns function", () => {
     render(<WrapperComponent />);
     await user.click(screen.getByText("Continue"));
 
-    expect(useRouter().push).toHaveBeenCalledWith(
-      `reports/15/review-operator-data`,
-    );
+    expect(mockPush).toHaveBeenCalledWith(`reports/15/review-operator-data`);
   });
 });
