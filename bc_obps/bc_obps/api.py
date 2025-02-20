803abe90a9d846208fac55fbcfe7944eb02af260
SeSo <sobhani.sepehr@gmail.com>
2025-02-20 15:54:36 -0800

    chore: add custom exception handler for Ninja ValidationError
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/bc_obps/api.py
+++ bc_obps/bc_obps/api.py
@@ -1,4 +1,7 @@
+from typing import Type
+from django.http import HttpRequest, HttpResponse
 from ninja import NinjaAPI, Swagger
+from ninja.errors import ValidationError
 from common.api import router as common_router
 from registration.api import router as registration_router
 from reporting.api import router as reporting_router
@@ -10,9 +13,13 @@ api = NinjaAPI(
     title="BCIERS API", docs=Swagger(settings={"filter": True, "operationsSorter": "method", "tagsSorter": "alpha"})
 )
 
+# This is a custom exception handler for Ninja ValidationError, This helps to return a more detailed error message for Unprocessable Entity (422) responses
+@api.exception_handler(ValidationError)
+def custom_validation_errors(request: HttpRequest, exc: Type[ValidationError]) -> HttpResponse:
+    print(exc.errors)  # <--------------------- !!!!
+    return api.create_response(request, {"detail": exc.errors}, status=422)
 
-# Global exception handler
-api.add_exception_handler(Exception, handle_exception)
+api.add_exception_handler(Exception, handle_exception) # Global exception handler
 
 api.add_router("/common/", common_router, tags=["V1"])
 api.add_router("/registration/", registration_router, tags=["V1"])
