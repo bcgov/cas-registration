737d1e9433cfec495fa877c6f9fd9eeffdcfb2d7
Nataliia Piguliak <nataly.pigulyak@gmail.com>
2025-02-20 22:24:16 +0000

    feat: fix to use defaultPageFactory
    Signed-off-by: Nataliia Piguliak <nataly.pigulyak@gmail.com>

--- bciers/apps/compliance/app/bceidbusiness/industry_user_admin/payment-summaries/page.tsx
+++ bciers/apps/compliance/app/bceidbusiness/industry_user_admin/payment-summaries/page.tsx
@@ -1,15 +1,10 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@/compliance/app/components/compliance-navigation/ComplianceNavigationPage";
 import { generateMetadata } from "@bciers/components/layout/RootLayout";
-import ComplianceNavigationPage from "@/compliance/app/components/compliance-navigation/ComplianceNavigationPage";
-import { Suspense } from "react";
-import Loading from "@bciers/components/loading/SkeletonForm";
 
 const title = "Payment Summaries";
 export const metadata = generateMetadata(title);
 
-export default function Page() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <ComplianceNavigationPage activeTab={2} />
-    </Suspense>
-  );
-}
+export default defaultPageFactory(() => {
+  return <Page activeTab={1} />;
+});
