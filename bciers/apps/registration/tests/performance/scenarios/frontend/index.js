844cbd3170233dccb875168a67caf450205e755c
SeSo <sobhani.sepehr@gmail.com>
2025-02-24 14:10:41 -0800

    chore: refactor frontend load test imports for better organization
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

---
+++ bciers/apps/registration/tests/performance/scenarios/frontend/index.js
@@ -0,0 +1,4 @@
+export { default as landingPage } from "./landing-page.js";
+export { default as operation } from "./operation.js";
+export { default as user } from "./user.js";
+export { default as userOperator } from "./user-operator.js";
