e4b364aa25fe0f7f8e036f7b83a2d092cc05d0d9
SeSo <sobhani.sepehr@gmail.com>
2025-02-24 14:36:48 -0800

    chore: semantic response code and using valid schema

--- bc_obps/registration/api/v2/_operations/_operation_id/bcghg_id.py
+++ bc_obps/registration/api/v2/_operations/_operation_id/bcghg_id.py
@@ -2,7 +2,7 @@ from typing import Literal, Tuple
 from uuid import UUID
 from django.http import HttpRequest
 from registration.models.bc_greenhouse_gas_id import BcGreenhouseGasId
-from registration.schema.v2.operation import OperationBoroIdOut
+from registration.schema.v2.operation import OperationBcghgIdOut
 from service.operation_service_v2 import OperationServiceV2
 from registration.constants import V2
 from common.permissions import authorize
@@ -14,7 +14,7 @@ from registration.schema.generic import Message
 
 @router.patch(
     "/operations/{uuid:operation_id}/bcghg-id",
-    response={200: OperationBoroIdOut, custom_codes_4xx: Message},
+    response={200: OperationBcghgIdOut, custom_codes_4xx: Message},
     tags=V2,
     description="""Generates and returns a BCGHG ID for the operation if the operation doesn't already have a BCGHG ID.""",
     auth=authorize('cas_director'),
