0a7ea66d7f389a8b70569e59b1c052d8312a5690
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-02-26 09:30:45 -0800

    chore: types

---
+++ bciers/apps/reporting/src/app/components/taskList/pageList.ts
@@ -0,0 +1,23 @@
+export enum ReportingPage {
+  // 1
+  ReviewOperatorInfo,
+  PersonResponsible,
+  ReviewFacilities,
+  // 2
+  ReviewInformation,
+  NonAttributableEmission,
+  EmissionSummary,
+  ProductionData,
+  AllocationOfEmissions,
+  // 3
+  AdditionalReportingData,
+  NewEntrantInformation,
+  OperationEmissionSummary,
+  // 4
+  ComplianceSummary,
+  // 5
+  Verification,
+  Attachments,
+  FinalReview,
+  SignOff,
+}
