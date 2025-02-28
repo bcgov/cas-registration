d6107fe059dd4a4825cbf5f761f3934f0d46d5ab
SeSo <sobhani.sepehr@gmail.com>
2025-02-28 11:13:28 -0800

    chore: update submission messages and improve layout in success component
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/registration/app/components/operations/registration/Success.tsx
+++ bciers/apps/registration/app/components/operations/registration/Success.tsx
@@ -1,7 +1,8 @@
 import MultiStepHeader from "@bciers/components/form/components/MultiStepHeader";
 import Check from "@bciers/components/icons/Check";
 import Link from "next/link";
-import { allOperationRegistrationSteps } from "./enums";
+import { allOperationRegistrationSteps } from "@/registration/app/components/operations/registration/enums";
+import {ghgRegulatorEmail, reportAnEventLink} from "@bciers/utils/src/urls";
 
 interface SubmissionProps {
   step: number;
@@ -16,15 +17,16 @@ const Submission = ({
     <>
       <MultiStepHeader stepIndex={step - 1} steps={steps} />
       <h2 className="form-heading mt-0">Submission</h2>
-      <section className="flex flex-col items-center justify-center max-w-[600px] mx-auto mt-4">
+      <section className="flex flex-col items-center justify-center max-w-[600px] mx-auto mt-10">
         <div className="flex flex-col items-center justify-center">
           {Check}
           <h3 className="mb-2 mt-4">Registration complete</h3>
-          <p className="m-0">This operation has been registered</p>
+          <p className="m-0">This operation has been registered.</p>
         </div>
         <section>
-          <p className="mt-6 mb-0" data-testid="submission-date-message">
-           The Greenhouse Gas Emission Reporting Regulation requires an operator to report the following events:
+          <p className="mt-6 mb-0" data-testid="submission-message">
+            The Greenhouse Gas Emission Reporting Regulation requires an
+            operator to report the following events:
           </p>
           <ul className="pl-6 my-0">
             <li>A closure or temporary shutdown;</li>
@@ -33,13 +35,18 @@ const Submission = ({
             <li>A transfer of control and direction to the operator</li>
           </ul>
           <p className="mb-0">
-            To report any of these events, please use the following link: Link to form
+            To report any of these events, please use the following link:{" "}
+            <Link href={reportAnEventLink} target="_blank">Link to form</Link>
           </p>
           <p className="mt-0">
-            Staff will review the information provided and administer changes in BCIERS as needed. For questions reach out to GHGRegulator@gov.bc.ca
+            Staff will review the information provided and administer changes in
+            BCIERS as needed. For questions reach out to{" "}
+            <Link href={ghgRegulatorEmail}>GHGRegulator@gov.bc.ca</Link>.
           </p>
           <p>
-            If you did not have any of these events, no further action is required and this registration is complete, and you may return back to the dashboard.
+            If you did not have any of these events, no further action is
+            required and this registration is complete, and you may return back
+            to the dashboard.
           </p>
         </section>
         <Link className="link-button-outlined" href="/">
