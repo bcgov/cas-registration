41549544ba93461354f0c721111c7a534bc512ec
SeSo <sobhani.sepehr@gmail.com>
2025-03-01 09:42:00 -0800

    chore: remove unused services and schemas
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/registration/schema/v1/operation.py
+++ bc_obps/registration/schema/v1/operation.py
@@ -1,50 +1,9 @@
-from typing import List, Optional, Union
+from typing import Optional, Union
 from registration.schema.v1.operator import OperatorForOperationOut
-from registration.utils import file_to_data_url, data_url_to_file
-from ninja import Field, FilterSchema, ModelSchema, Schema
+from registration.utils import file_to_data_url
+from ninja import Field, FilterSchema, ModelSchema
 from registration.models import Operation, Operator, User
-from pydantic import field_validator
 from ninja.types import DictStrAny
-from django.core.files.base import ContentFile
-
-
-class OperationUpdateIn(ModelSchema):
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    position_title: Optional[str] = None
-    email: Optional[str] = None
-    phone_number: Optional[str] = None
-    # external point of contact details
-    external_point_of_contact_first_name: Optional[str] = None
-    external_point_of_contact_last_name: Optional[str] = None
-    external_point_of_contact_position_title: Optional[str] = None
-    external_point_of_contact_email: Optional[str] = None
-    external_point_of_contact_phone_number: Optional[str] = None
-    is_external_point_of_contact: Optional[bool] = None
-    statutory_declaration: Optional[str] = None
-
-    @field_validator("statutory_declaration")
-    @classmethod
-    def validate_statutory_declaration(cls, value: str) -> Optional[ContentFile]:
-        if value:
-            return data_url_to_file(value)
-        return None
-
-    class Meta:
-        model = Operation
-        fields = ['name', 'type', 'naics_code', 'opt_in', 'point_of_contact', 'regulated_products']
-        populate_by_name = True
-
-
-class OperationListOut(ModelSchema):
-    operator: str = Field(..., alias="operator.legal_name")
-    bc_obps_regulated_operation: Optional[str] = Field(None, alias="bc_obps_regulated_operation.id")
-    bcghg_id: Optional[str] = Field(None, alias="bcghg_id.id")
-
-    class Meta:
-        model = Operation
-        fields = ['id', 'name', 'submission_date', 'status']
-        from_attributes = True
 
 
 class OperationOut(ModelSchema):
--- bc_obps/service/operation_service.py
+++ bc_obps/service/operation_service.py
@@ -1,32 +1,20 @@
-from typing import Dict, Optional, Union
+from typing import Dict, Union
 from uuid import UUID
-from django.db import transaction
 from registration.emails import send_boro_id_application_email
 from registration.enums.enums import BoroIdApplicationStates
-from service.user_operator_service import UserOperatorService
-from service.data_access_service.document_service import DocumentDataAccessService
-from service.document_service import DocumentService
 from service.data_access_service.user_service import UserDataAccessService
-from service.data_access_service.contact_service import ContactDataAccessService
-from registration.schema.v1.operation import OperationFilterSchema, OperationUpdateIn
+from registration.schema.v1.operation import OperationFilterSchema
 from service.data_access_service.operation_service import OperationDataAccessService
 from django.db.models import Q
 from django.core.paginator import Paginator
 from ninja import Query
-from registration.utils import (
-    files_have_same_hash,
-)
 from datetime import datetime
 from registration.models import (
-    Contact,
-    Document,
     Operation,
     Operator,
     User,
-    UserOperator,
 )
 from zoneinfo import ZoneInfo
-from django.core.files.base import ContentFile
 from registration.constants import PAGE_SIZE, UNAUTHORIZED_MESSAGE
 
 
@@ -80,124 +68,7 @@ class OperationService:
             return operation
         return operation
 
-    @classmethod
-    @transaction.atomic()
-    def create_or_update_point_of_contact(
-        cls, user_guid: UUID, existing_point_of_contact_id: Optional[int], payload: OperationUpdateIn
-    ) -> Optional[Contact]:
-        is_external_point_of_contact = payload.is_external_point_of_contact
-        if is_external_point_of_contact is False:  # the point of contact is the user
 
-            user_contact_data = {
-                "first_name": payload.first_name,
-                "last_name": payload.last_name,
-                "position_title": payload.position_title,
-                "email": payload.email,
-                "phone_number": payload.phone_number,
-            }
-            poc = ContactDataAccessService.update_or_create(existing_point_of_contact_id, user_contact_data)
-            return poc
-
-        elif is_external_point_of_contact is True:  # the point of contact is an external user
-            external_contact_data = {
-                "first_name": payload.external_point_of_contact_first_name,
-                "last_name": payload.external_point_of_contact_last_name,
-                "position_title": payload.external_point_of_contact_position_title,
-                "email": payload.external_point_of_contact_email,
-                "phone_number": payload.external_point_of_contact_phone_number,
-            }
-
-            external_poc = ContactDataAccessService.update_or_create(
-                existing_point_of_contact_id, external_contact_data
-            )
-            return external_poc
-        return None
-
-    @classmethod
-    @transaction.atomic()
-    def create_or_replace_statutory_declaration(
-        cls, user_guid: UUID, payload: OperationUpdateIn, existing_statutory_document: Optional[Document]
-    ) -> Optional[Document]:
-
-        # if there is an existing statutory declaration document, check if the new one is different
-        if existing_statutory_document:
-            # We need to check if the file has changed, if it has, we need to delete the old one and create a new one
-            if not files_have_same_hash(payload.statutory_declaration, existing_statutory_document.file):  # type: ignore[arg-type] # mypy is not aware of the schema validator
-                existing_statutory_document.delete()
-                return DocumentDataAccessService.create_document(
-                    user_guid, payload.statutory_declaration, "signed_statutory_declaration"  # type: ignore[arg-type] # mypy is not aware of the schema validator
-                )
-            return None
-        # if there is no existing statutory declaration document, create a new one
-        return DocumentDataAccessService.create_document(
-            user_guid, payload.statutory_declaration, "signed_statutory_declaration"  # type: ignore[arg-type] # mypy is not aware of the schema validator
-        )
-
-    @classmethod
-    @transaction.atomic()
-    def update_operation(
-        cls, user_guid: UUID, operation_id: UUID, submit: str, form_section: int, payload: OperationUpdateIn
-    ) -> Operation:
-        user: User = UserDataAccessService.get_by_guid(user_guid)
-        user_operator: UserOperator = UserOperatorService.get_current_user_approved_user_operator_or_raise(user)
-        operation: Operation = OperationDataAccessService.get_by_id_for_update(operation_id)
-        payload_statutory_declaration: Optional[ContentFile] = payload.statutory_declaration  # type: ignore[assignment] # mypy is not aware of the schema validator
-
-        # industry users can only edit operations that belong to their operator
-        if not operation.user_has_access(user_guid) or operation.operator_id != user_operator.operator_id:
-            raise Exception(UNAUTHORIZED_MESSAGE)
-
-        # the frontend includes default values, which are being sent in the payload to the backend. We need to know
-        # whether the data being received in the payload is what the user has actually viewed, so we separate this
-        # by form_section (the paginated form in the UI)
-        if form_section == 1:
-            if operation.status == Operation.Statuses.NOT_STARTED:
-                operation.status = Operation.Statuses.DRAFT
-            payload_dict: dict = payload.dict(include={'name', 'type', 'bcghg_id', 'opt_in'})
-            for attr, value in payload_dict.items():
-                setattr(operation, attr, value)
-            operation.naics_code_id = payload.naics_code  # type: ignore[attr-defined]
-            operation.save(update_fields=[*payload_dict.keys(), 'naics_code_id', 'status'])
-            operation.regulated_products.set(payload.regulated_products)  # type: ignore[attr-defined]
-        elif form_section == 2:
-            existing_point_of_contact_id: Optional[int] = operation.point_of_contact_id or None
-            poc = cls.create_or_update_point_of_contact(user_guid, existing_point_of_contact_id, payload)
-            operation.point_of_contact = poc
-            operation.save(update_fields=['point_of_contact'])
-
-        elif form_section == 3 and payload_statutory_declaration:
-            existing_statutory_document = DocumentService.get_existing_statutory_declaration_by_operation_id(
-                operation_id
-            )
-            # If document is new or has changed, we need to set it to the operation
-            document = cls.create_or_replace_statutory_declaration(user_guid, payload, existing_statutory_document)
-            if document:
-                operation.documents.set([document])
-
-        if submit == "true":
-            """
-            if the PUT request has submit == "true" (i.e., user has clicked Submit button in UI form), the desired behaviour depends on
-            the Operation's status:
-                - if operation.status was already "Approved", it should remain Approved and the submission date should not be altered
-                - if operation.status was "Changes Requested", it should switch to Pending
-                - if operation.status was "Declined", it should switch to Pending
-                - if operation.status was "Not Started", it should switch to Pending
-                - if operation.status was "Pending", it should remain as Pending
-            """
-            if operation.status != Operation.Statuses.APPROVED:
-                operation.status = Operation.Statuses.PENDING
-                operation.submission_date = datetime.now(ZoneInfo("UTC"))
-                operation.save(update_fields=['status', 'submission_date'])
-                send_boro_id_application_email(
-                    application_state=BoroIdApplicationStates.CONFIRMATION,
-                    operator_legal_name=operation.operator.legal_name,
-                    operation_name=operation.name,
-                    opted_in=operation.opt_in,
-                    operation_creator=operation.created_by,
-                    point_of_contact=operation.point_of_contact,
-                )
-
-        return operation
 
     @classmethod
     def list_operations(
