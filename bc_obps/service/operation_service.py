3d1918595f79733be248c6e82f87def20b4ce8da
SeSo <sobhani.sepehr@gmail.com>
2025-03-01 09:58:36 -0800

    chore: remove unused list_operations method and related imports from operation_service.py
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/service/operation_service.py
+++ bc_obps/service/operation_service.py
@@ -1,13 +1,8 @@
-from typing import Dict, Union
 from uuid import UUID
 from registration.emails import send_boro_id_application_email
 from registration.enums.enums import BoroIdApplicationStates
 from service.data_access_service.user_service import UserDataAccessService
-from registration.schema.v1.operation import OperationFilterSchema
 from service.data_access_service.operation_service import OperationDataAccessService
-from django.db.models import Q
-from django.core.paginator import Paginator
-from ninja import Query
 from datetime import datetime
 from registration.models import (
     Operation,
@@ -15,7 +10,7 @@ from registration.models import (
     User,
 )
 from zoneinfo import ZoneInfo
-from registration.constants import PAGE_SIZE, UNAUTHORIZED_MESSAGE
+from registration.constants import UNAUTHORIZED_MESSAGE
 
 
 class OperationService:
@@ -67,47 +62,3 @@ class OperationService:
                 raise Exception(UNAUTHORIZED_MESSAGE)
             return operation
         return operation
-
-
-
-    @classmethod
-    def list_operations(
-        cls, user_guid: UUID, filters: OperationFilterSchema = Query(...)
-    ) -> Dict[str, Union[list[Operation], int]]:
-        user = UserDataAccessService.get_by_guid(user_guid)
-        page = filters.page
-        bcghg_id = filters.bcghg_id
-        bc_obps_regulated_operation = filters.bc_obps_regulated_operation
-        name = filters.name
-        operator = filters.operator
-        sort_field = filters.sort_field
-        sort_order = filters.sort_order
-        status = filters.status
-        sort_direction = "-" if sort_order == "desc" else ""
-
-        base_qs = OperationDataAccessService.get_all_operations_for_user(user)
-        list_of_filters = [
-            Q(bcghg_id__id__icontains=bcghg_id) if bcghg_id else Q(),
-            (
-                Q(bc_obps_regulated_operation__id__icontains=bc_obps_regulated_operation)
-                if bc_obps_regulated_operation
-                else Q()
-            ),
-            Q(name__icontains=name) if name else Q(),
-            Q(operator__legal_name__icontains=operator) if operator else Q(),
-            Q(status__icontains=status) if status else Q(),
-        ]
-
-        qs = base_qs.filter(*list_of_filters).order_by(f"{sort_direction}{sort_field}")
-
-        paginator = Paginator(qs, PAGE_SIZE)
-
-        try:
-            page = paginator.validate_number(page)
-        except Exception:
-            page = 1
-
-        return {
-            "data": [(operation) for operation in paginator.page(page).object_list],
-            "row_count": paginator.count,
-        }
