3b98a2e00ee4b995d70ba05ebac301bea9c5086b
SeSo <sobhani.sepehr@gmail.com>
2025-03-01 09:23:04 -0800

    chore: remove unused schemas from facility.py
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/registration/schema/v1/facility.py
+++ bc_obps/registration/schema/v1/facility.py
@@ -1,26 +1,8 @@
-from ninja import FilterSchema, ModelSchema, Field
+from ninja import ModelSchema, Field
 from registration.models import Facility
 from uuid import UUID
 from typing import List, Optional
 import decimal
-
-
-class FacilityListOut(ModelSchema):
-    bcghg_id: Optional[str] = Field(None, alias="bcghg_id.id")
-
-    class Meta:
-        model = Facility
-        fields = ['id', 'name', 'is_current_year', 'starting_date', 'type']
-
-
-class FacilityFilterSchema(FilterSchema):
-    # NOTE: we could simply use the `q` parameter to filter by name and ... but,
-    # due to this issue: https://github.com/vitalik/django-ninja/issues/1037 mypy is unhappy so I'm using the `json_schema_extra` parameter
-    # If we want to achieve more by using the `q` parameter, we should use it and ignore the mypy error
-    bcghg_id: Optional[str] = Field(None, json_schema_extra={'q': 'bcghg_id__id__icontains'})
-    name: Optional[str] = Field(None, json_schema_extra={'q': 'name__icontains'})
-    type: Optional[str] = Field(None, json_schema_extra={'q': 'type__icontains'})
-
 
 class FacilityIn(ModelSchema):
     street_address: Optional[str] = None
