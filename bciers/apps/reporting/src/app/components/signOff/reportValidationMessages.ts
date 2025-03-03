bbad37fc83221e7a5c8365bbf6a7aebc9c9064b0
shon-button <shon@button.is>
2025-03-03 14:24:25 -0500

    chore: validate report verification on submit
    chore: cleanup
    
    chore: cleanup
    
    chore: cleanup
    
    chore: cleanup

--- bciers/apps/reporting/src/app/components/signOff/reportValidationMessages.ts
+++ bciers/apps/reporting/src/app/components/signOff/reportValidationMessages.ts
@@ -1,4 +1,6 @@
 const reportValidationMessagesMap: { [key: string]: string } = {
+  missing_report_verification:
+    "Verification information must be completed with this report.  Please complete the Verification page.",
   verification_statement:
     "A verification statement must be uploaded with this report. Please upload a verification statement on the Attachments page.",
 };
