4baa1bbc01eb0d8eee57e0505dc4dad26f909492
SeSo <sobhani.sepehr@gmail.com>
2025-03-07 10:29:38 -0800

    chore: update load test scenarios and adjust VU targets
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/registration/tests/performance/backend-load-test.js
+++ bciers/apps/registration/tests/performance/backend-load-test.js
@@ -11,19 +11,22 @@ import {
   contact,
 } from "./scenarios/backend/index.js";
 
+// Calculate the number of VUs by multiplying the number of users by the number of scenarios
+// Example: 5 users and 5 scenarios result in 25 VUs
 const stages = [
-  { duration: "5m", target: 20 }, // simulate ramp-up of traffic from 1 to 20 users over 5 minutes.
-  { duration: "10m", target: 20 }, // stay at 20 users for 10 minutes
-  { duration: "3m", target: 40 }, // ramp-up to 40 users over 3 minutes (peak hour starts)
-  { duration: "2m", target: 40 }, // stay at 40 users for short amount of time (peak hour)
-  { duration: "3m", target: 15 }, // ramp-down to 20 users over 3 minutes (peak hour ends)
-  { duration: "10m", target: 15 }, // continue at 20 for additional 10 minutes
-  { duration: "2m", target: 0 }, // ramp-down to 0 users
+  { duration: "2m", target: 10 }, // 80 VUs
+  { duration: "3m", target: 20 }, // 160 VUs
+  { duration: "3m", target: 30 }, // 240 VUs
+  { duration: "3m", target: 25 }, // 200 VUs
+  { duration: "3m", target: 15 }, // 120 VUs
+  { duration: "2m", target: 15 }, // 120 VUs
+  { duration: "2m", target: 0 }, // 0 VUs
 ];
 
+
 export const options = {
-  abortOnFail: true,
-  delayAbortEval: "20s",
+  // abortOnFail: true, // Abort the test if a check fails
+  // delayAbortEval: "20s", // Wait 20s before aborting the test - Gives the test time to generate some metrics before aborting
   scenarios: Object.fromEntries(
     [
       "operation",
@@ -43,6 +46,7 @@ export const options = {
     ]),
   ),
   thresholds: {
+    // http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s // REG1 threshold
     "http_req_duration{status:200}": ["p(90)<500", "p(95)<700", "p(99)<1000"], // 90% of requests must complete below 500ms, 95% below 700ms, and 99% below 1s
     "http_req_duration{status:201}": ["p(90)<500", "p(95)<800", "p(99)<1200"], // 90% of requests must complete below 500ms, 95% below 700ms, and 99% below 1s
     errors: ["rate<0.05"], // Fail test if error rate exceeds 5%
