59f4aba5bfa06a45cf69c4fdac0bcc159ea007c7
Slava <slava.ivanov@gov.bc.ca>
2025-03-06 23:03:41 -0800

    chore: disable compliance mocks

--- bc_obps/reporting/management/commands/load_reporting_fixtures.py
+++ bc_obps/reporting/management/commands/load_reporting_fixtures.py
@@ -1,5 +1,7 @@
+import json
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from service.report_service import ReportService
 
 
 class Command(BaseCommand):
@@ -7,13 +9,24 @@ class Command(BaseCommand):
     fixture_base_dir = 'reporting/fixtures/mock'
 
     def handle(self, *args, **options):
+
         fixtures = [
             f'{self.fixture_base_dir}/reporting_year.json',
-            f'{self.fixture_base_dir}/report.json',
-            f'{self.fixture_base_dir}/facility_report.json',
-            f'{self.fixture_base_dir}/report_product.json',
         ]
 
         for fixture in fixtures:
             self.stdout.write(self.style.SUCCESS(f"Loading: {fixture}"))
             call_command('loaddata', fixture)
+
+        self.stdout.write(self.style.SUCCESS(f"Creating Reports from {self.fixture_base_dir}/report.json"))
+        self.load_reports()
+
+    def load_reports(self):
+        reports_fixture = f'{self.fixture_base_dir}/report.json'
+        with open(reports_fixture) as f:
+            reports = json.load(f)
+            for report in reports:
+                ReportService.create_report(
+                    operation_id=report['fields']['operation_id'],
+                    reporting_year=report['fields']['reporting_year_id'],
+                )
