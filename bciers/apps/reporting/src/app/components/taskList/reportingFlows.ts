014f37d6820c5f7eca92518b5593403b55c371db
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-03-07 10:47:27 -0800

    chore: a

--- bciers/apps/reporting/src/app/components/taskList/reportingFlows.ts
+++ bciers/apps/reporting/src/app/components/taskList/reportingFlows.ts
@@ -1,3 +1,4 @@
+import { getRegistrationPurpose } from "../../utils/getRegistrationPurpose";
 import { getReportingOperation } from "../../utils/getReportingOperation";
 import {
   HeaderStep,
@@ -28,19 +29,68 @@ const sfoFlow: ReportingFlowDescription = {
   ],
 };
 
+const lfoFlow: ReportingFlowDescription = {
+  [HeaderStep.OperationInformation]: [
+    ReportingPage.ReviewOperatorInfo,
+    ReportingPage.PersonResponsible,
+    ReportingPage.ReviewFacilities,
+  ],
+  [HeaderStep.ReportInformation]: [
+    ReportingPage.FacilitiesTable,
+    ReportingPage.ReviewInformation,
+    ReportingPage.Activities,
+    ReportingPage.NonAttributableEmission,
+    ReportingPage.EmissionSummary,
+    ReportingPage.ProductionData,
+    ReportingPage.AllocationOfEmissions,
+  ],
+  [HeaderStep.AdditionalInformation]: [ReportingPage.AdditionalReportingData],
+  [HeaderStep.ComplianceSummary]: [ReportingPage.ComplianceSummary],
+  [HeaderStep.SignOffSubmit]: [
+    ReportingPage.FinalReview,
+    ReportingPage.Verification,
+    ReportingPage.Attachments,
+    ReportingPage.SignOff,
+  ],
+};
+
 export const reportingFlows: {
   [Flow in ReportingFlow]?: ReportingFlowDescription;
 } = {
   [ReportingFlow.SFO]: sfoFlow,
-  // More to come
+  [ReportingFlow.LFO]: lfoFlow,
 };
 
 export async function getFlow(reportVersionId: number): Promise<ReportingFlow> {
   const reportOperationData = await getReportingOperation(reportVersionId);
-  //const factilityReport = await getFacilityReport(reportVersionId);
+  const registrationPurpose = (await getRegistrationPurpose(reportVersionId))
+    .registration_purpose;
 
   const operationType = reportOperationData.operation_type;
+  const reportType = reportOperationData.operation_report_type;
+
+  if (reportType === "Simple Report") return ReportingFlow.SimpleReport;
+
+  if (registrationPurpose === "Electricity Import Operation")
+    return ReportingFlow.EIO;
+
+  if (
+    registrationPurpose === "Reporting Operation" &&
+    operationType === "Single Facility Operation"
+  )
+    return ReportingFlow.ReportingOnlySFO;
+
+  if (
+    registrationPurpose === "Reporting Operation" &&
+    operationType === "Linear Facilities Operation"
+  )
+    return ReportingFlow.ReportingOnlyLFO;
 
   if (operationType === "Single Facility Operation") return ReportingFlow.SFO;
-  return ReportingFlow.SFO;
+
+  if (operationType === "Linear Facilities Operation") return ReportingFlow.LFO;
+
+  throw new Error(
+    `Unable to resolve reporting flow for registration purpose ${registrationPurpose.registration_purpose} and operation type ${operationType}`,
+  );
 }
