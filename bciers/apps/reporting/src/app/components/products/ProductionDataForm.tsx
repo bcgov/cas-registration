1900440501b8810bce06f665664de88d27a5f682
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2025-03-10 11:58:40 -0700

    chore: make product a requirement for production page & set default to allocation of emissions page when no product saved

--- bciers/apps/reporting/src/app/components/products/ProductionDataForm.tsx
+++ bciers/apps/reporting/src/app/components/products/ProductionDataForm.tsx
@@ -60,6 +60,13 @@ const ProductionDataForm: React.FC<Props> = ({
     });
   };
 
+  // User must select a product if facility type is not small or medium
+  useEffect(() => {
+    if (!["Small Aggregate", "Medium Facility"].includes(facilityType) && formData.product_selection.length < 1) {
+      setErrors(["A product must be selected"]);
+    } else setErrors(undefined);
+  }, [formData]);
+
   // If facility type is small or medium, add not applicable as an option to production quantity
   const modifiedSchema = schema;
   if (modifiedSchema.definitions) {
@@ -141,6 +148,8 @@ const ProductionDataForm: React.FC<Props> = ({
       onChange={(data) => onChange(data.formData)}
       continueUrl={navigationInformation.continueUrl}
       errors={errors}
+      saveButtonDisabled={errors && errors.length > 0}
+      submitButtonDisabled={errors && errors.length > 0}
     />
   );
 };
