eed9fbb243623c02579beea8032c22fd78dceef5
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-03-11 12:57:26 -0700

    chore: refactoring non-attr emissions page and facility emissions summary page

--- bciers/apps/reporting/src/app/components/facility/FacilityEmissionSummaryForm.tsx
+++ bciers/apps/reporting/src/app/components/facility/FacilityEmissionSummaryForm.tsx
@@ -4,48 +4,34 @@ import { Box } from "@mui/material";
 import MultiStepHeader from "@bciers/components/form/components/MultiStepHeader";
 import FormBase from "@bciers/components/form/FormBase";
 import ReportingTaskList from "@bciers/components/navigation/reportingTaskList/ReportingTaskList";
-import { TaskListElement } from "@bciers/components/navigation/reportingTaskList/types";
 import {
   emissionSummaryUiSchema,
   facilityEmissionSummarySchema,
 } from "@reporting/src/data/jsonSchema/emissionSummary";
 import ReportingStepButtons from "@bciers/components/form/components/ReportingStepButtons";
 import { EmissionSummaryFormData } from "@reporting/src/app/utils/emissionSummaryTypes";
+import { NavigationInformation } from "../taskList/types";
+import { multiStepHeaderSteps } from "../taskList/multiStepHeaderConfig";
 
 interface Props {
-  versionId: number;
-  facilityId: string;
   summaryFormData: EmissionSummaryFormData;
-  taskListElements: TaskListElement[];
+  navigationInformation: NavigationInformation;
 }
 
 const FacilityEmissionSummaryForm: React.FC<Props> = ({
-  versionId,
-  facilityId,
   summaryFormData,
-  taskListElements,
+  navigationInformation,
 }) => {
-  const customStepNames = [
-    "Operation Information",
-    "Report Information",
-    "Additional Information",
-    "Compliance Summary",
-    "Sign-off & Submit",
-  ];
-
-  const backRef = `/reports/${versionId}/facilities/${facilityId}/non-attributable`;
-  const continueRef = `/reports/${versionId}/facilities/${facilityId}/production-data`;
-
   return (
     <Box sx={{ p: 3 }}>
       <div
         className="container mx-auto p-4"
         data-testid="facility-emission-summary"
       >
-        <MultiStepHeader stepIndex={1} steps={customStepNames} />
+        <MultiStepHeader stepIndex={1} steps={multiStepHeaderSteps} />
       </div>
       <div className="w-full flex">
-        <ReportingTaskList elements={taskListElements} />
+        <ReportingTaskList elements={navigationInformation.taskList} />
         <div className="w-full md:max-w-[60%]">
           <FormBase
             schema={facilityEmissionSummarySchema}
@@ -53,8 +39,8 @@ const FacilityEmissionSummaryForm: React.FC<Props> = ({
             formData={summaryFormData}
           >
             <ReportingStepButtons
-              backUrl={backRef}
-              continueUrl={continueRef}
+              backUrl={navigationInformation.backUrl}
+              continueUrl={navigationInformation.continueUrl}
               saveButtonDisabled={true}
               submitButtonDisabled={false}
             />
