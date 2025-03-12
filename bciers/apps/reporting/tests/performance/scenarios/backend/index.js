4611c0d03273f24b999f2e77887a65d96111d63c
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-03-11 23:02:20 -0700

    chore: Reporting load testing

---
+++ bciers/apps/reporting/tests/performance/scenarios/backend/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+export { default as facilities } from "./facilities.js";
+export { default as facility } from "./facility.js";
+export { default as report } from "./report.js";
+export { default as activity } from "./activity.js";
