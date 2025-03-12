673df1b57089dce207bd13b783644ac0cee8d20e
Brianna Cerkiewicz <brianna@button.is>
2025-03-12 14:53:39 -0700

    chore: apply suggestions from code review

--- bc_obps/registration/migrations/0089_set_purpose_for_existing_operations.py
+++ bc_obps/registration/migrations/0089_set_purpose_for_existing_operations.py
@@ -12,13 +12,12 @@ def migrate_assign_regulated_purpose(apps, schema_monitor):
     # import the required Django model
     Operation = apps.get_model('registration', 'Operation')
 
-    for operation in Operation.objects.all():
-        if operation.bc_obps_regulated_operation is not None:
-            if operation.opt_in:
-                operation.registration_purpose = 'Opted-in Operation'
-            else:
-                operation.registration_purpose = 'OBPS Regulated Operation'
-            operation.save(update_fields=['registration_purpose'])
+    for operation in Operation.objects.filter(bc_obps_regulated_operation__isnull=False):
+        if operation.opt_in:
+            operation.registration_purpose = 'Opted-in Operation'
+        else:
+            operation.registration_purpose = 'OBPS Regulated Operation'
+        operation.save(update_fields=['registration_purpose'])
 
 
 class Migration(migrations.Migration):
