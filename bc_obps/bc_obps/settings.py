c82a2be91308a4b9bfc38e5e74a41ed3640cc067
SeSo <sobhani.sepehr@gmail.com>
2025-03-11 19:07:13 -0700

    refactor: enhance migration commands to exclude specified apps in production and improve documentation
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -67,9 +67,6 @@ CHES_API_URL = os.environ.get("CHES_API_URL")
 ELICENSING_API_URL = os.environ.get("ELICENSING_API_URL")
 ELICENSING_AUTH_TOKEN = os.environ.get("ELICENSING_AUTH_TOKEN")
 
-# Application definition
-
-
 LOCAL_APPS = [
     "registration",
     "reporting",
@@ -83,6 +80,9 @@ RLS_GRANT_APPS = [
     "registration",
     "reporting",
 ]
+
+# Apps that should not be included in production migrations
+APPS_TO_NOT_INCLUDE_IN_PROD = ["compliance", "events"]
 
 INSTALLED_APPS = [
     # Django apps
