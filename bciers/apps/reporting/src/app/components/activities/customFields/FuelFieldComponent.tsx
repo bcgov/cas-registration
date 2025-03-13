17d0ec1126fe5eb85ea8dc4f68029e4c4e3aecdd
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-03-13 10:43:31 -0700

    chore:fix fuel name query string

--- bciers/apps/reporting/src/app/components/activities/customFields/FuelFieldComponent.tsx
+++ bciers/apps/reporting/src/app/components/activities/customFields/FuelFieldComponent.tsx
@@ -13,8 +13,10 @@ const ObjectField = registry.fields.ObjectField;
 
 export const FuelFields: React.FunctionComponent<FieldProps> = (props) => {
   const handleChange = async (c: FuelDataChangeEvent) => {
+    //encode fuel name to handle `#` in URL as it was being treated as a  fragment identifier, causing data loss when sent in the query string
+    const fuelName = encodeURIComponent(c.fuelName);
     const fuelData = await actionHandler(
-      `reporting/fuel?fuel_name=${c.fuelName}`,
+      `reporting/fuel?fuel_name=${fuelName}`,
       "GET",
       "",
     );
