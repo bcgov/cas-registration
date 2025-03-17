596e8598dd29ebb30e427d7f9bd0a67331d52308
SeSo <sobhani.sepehr@gmail.com>
2025-03-17 12:43:33 -0700

    chore: make pre-commit happy
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/common/migrations/0039_update_dashboard_data.py
+++ bc_obps/common/migrations/0039_update_dashboard_data.py
@@ -3,8 +3,10 @@
 from django.db import migrations
 from common.utils import reset_dashboard_data
 
+
 def reset_dashboard_data_fn(_, schema_editor):
     reset_dashboard_data()
+
 
 class Migration(migrations.Migration):
 
