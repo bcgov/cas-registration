6cb52abf9170bc346b6d1b34442ab9dfd6e1c4be
SeSo <sobhani.sepehr@gmail.com>
2025-03-17 14:18:16 -0700

    refactor: update getOperatorConfirmationInfo to return Operator type
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/administration/app/components/operators/getOperatorConfirmationInfo.ts
+++ bciers/apps/administration/app/components/operators/getOperatorConfirmationInfo.ts
@@ -1,11 +1,12 @@
 import { actionHandler } from "@bciers/actions";
 import { UUID } from "crypto";
+import { Operator } from "@/administration/app/components/userOperators/types";
 
 // 🛠️ Function to get an operator by operator id
 export default async function getOperatorConfirmationInfo(
   id: UUID,
   pathToRevalidate: string = "",
-) {
+): Promise<Operator | { error: string }> {
   const response = await actionHandler(
     `registration/operators/${id}/confirm`,
     "GET",
