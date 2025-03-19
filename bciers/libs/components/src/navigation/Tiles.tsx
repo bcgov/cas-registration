1f34ae5b691d1e62374b3d895d173d736f08dba5
SeSo <sobhani.sepehr@gmail.com>
2025-03-19 16:28:46 -0700

    refactor: use optional chaining for links and tiles mapping
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/libs/components/src/navigation/Tiles.tsx
+++ bciers/libs/components/src/navigation/Tiles.tsx
@@ -4,8 +4,7 @@ import Tile from "./Tile";
 const Tiles = ({ tiles }: { tiles: ContentItem[] }) => {
   return (
     <section className="w-full flex flex-wrap gap-x-8 lg:gap-x-24 gap-y-16 my-8">
-      {tiles &&
-        tiles.map((tile) => {
+      {tiles?.map((tile) => {
           const { title } = tile;
           return <Tile key={title} {...tile} />;
         })}
