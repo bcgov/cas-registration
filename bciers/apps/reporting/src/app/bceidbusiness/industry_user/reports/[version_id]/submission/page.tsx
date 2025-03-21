d9cf85299e1119f0059acc1161d60954a2079d74
shon-button <shon@button.is>
2025-03-21 15:53:40 -0400

    fix: report submitted routing rule
    chore: cleanup
    
    chore: cleanup
    
    fix: report submitted routing rule
    
    chore: cleanup
    
    chore: cleanup
    
    chore: cleanup

---
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user/reports/[version_id]/submission/page.tsx
@@ -0,0 +1,4 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@reporting/src/app/components/signOff/SubmissionPage";
+
+export default defaultPageFactory(Page);
