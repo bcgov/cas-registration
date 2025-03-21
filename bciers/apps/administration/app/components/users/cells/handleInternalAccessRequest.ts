f6e5b08059d760850e001bbde314b403c98159e8
Brianna Cerkiewicz <brianna@button.is>
2025-03-21 15:09:02 -0700

    feat: create internal user access request grid

---
+++ bciers/apps/administration/app/components/users/cells/handleInternalAccessRequest.ts
@@ -0,0 +1,23 @@
+import { InternalFrontEndRoles } from "@bciers/utils/src/enums";
+import { actionHandler } from "@bciers/actions";
+import { UUID } from "crypto";
+
+export default async function handleInternalAccessRequest(
+  userId: UUID,
+  roleUpdate: InternalFrontEndRoles,
+  archive: boolean,
+) {
+  if (!roleUpdate) return;
+  const response = await actionHandler(
+    `registration/users/${userId}`,
+    "PATCH",
+    "",
+    {
+      body: JSON.stringify({
+        app_role: roleUpdate,
+        archive,
+      }),
+    },
+  );
+  return response;
+}
