9a2886dcc2af2b017732536458ed0302f5f144c7
Nataliia Piguliak <nataly.pigulyak@gmail.com>
2025-03-26 10:38:23 -0700

    fix migration
    Signed-off-by: Nataliia Piguliak <nataly.pigulyak@gmail.com>

--- bc_obps/compliance/migrations/0005_add_complianceobligation_obligation_id.py
+++ bc_obps/compliance/migrations/0005_add_complianceobligation_obligation_id.py
@@ -12,20 +12,16 @@ class Migration(migrations.Migration):
             model_name='complianceobligation',
             name='obligation_id',
             field=models.CharField(
-                blank=True,
                 db_comment='A human-readable identifier for the obligation in format YY-OOOO-R-V',
                 max_length=30,
-                null=True,
             ),
         ),
         migrations.AddField(
             model_name='historicalcomplianceobligation',
             name='obligation_id',
             field=models.CharField(
-                blank=True,
                 db_comment='A human-readable identifier for the obligation in format YY-OOOO-R-V',
                 max_length=30,
-                null=True,
             ),
         ),
     ]
