fb8d81dffaf61ab04d4db398cc813773e299e34f
SeSo <sobhani.sepehr@gmail.com>
2025-03-28 16:36:30 -0700

    chore: remove reg1 pages
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/registration1/app/(authenticated)/bceidbusiness/industry_user_admin/dashboard/select-operator/(request-access)/received/[step]/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SelectOperatorRequestAccessReceivedPage from "@/app/components/routes/select-operator/request-access/received/Page";
-
-export default async function Page({
-  params,
-}: {
-  readonly params: { id: string; step: string };
-}) {
-  return <SelectOperatorRequestAccessReceivedPage params={params} />;
-}
