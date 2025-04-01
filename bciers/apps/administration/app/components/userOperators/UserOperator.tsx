4597a1ce857af7cb812164a24585c21fcd0a4da6
SeSo <sobhani.sepehr@gmail.com>
2025-03-31 19:53:49 -0700

    chore: update import paths for UserOperator components
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/administration/app/components/userOperators/UserOperator.tsx
+++ bciers/apps/administration/app/components/userOperators/UserOperator.tsx
@@ -1,7 +1,7 @@
-import { UserOperatorFormData } from "./types";
-import UserOperatorReviewForm from "./UserOperatorReviewForm";
-import { createOperatorSchema } from "../../data/jsonSchema/operator";
-import getUserOperatorFormData from "./getUserOperatorFormData";
+import { UserOperatorFormData } from "@/administration/app/components/userOperators/types";
+import UserOperatorReviewForm from "@/administration/app/components/userOperators/UserOperatorReviewForm";
+import { createOperatorSchema } from "@/administration/app/data/jsonSchema/operator";
+import getUserOperatorFormData from "@/administration/app/components/userOperators/getUserOperatorFormData";
 import { UUID } from "crypto";
 
 export default async function UserOperator({
