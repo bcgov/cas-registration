5e3f513483fb58788fd9490d268406a50822c47b
Brianna Cerkiewicz <brianna@button.is>
2025-04-01 14:52:57 -0700

    chore: middleware to support archived users

--- bciers/apps/registration/middlewares/withAuthorizationReg.test.ts
+++ bciers/apps/registration/middlewares/withAuthorizationReg.test.ts
@@ -2,6 +2,8 @@ import { NextURL } from "next/dist/server/web/next-url";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { instance, mock, reset, when } from "ts-mockito";
 import middleware from "../middleware";
+import { actionHandler, getToken } from "@bciers/testConfig/mocks";
+import { mockCasPendingToken } from "@bciers/testConfig/data/tokens";
 
 const domain = "https://localhost:3000";
 const mockedRequest: NextRequest = mock(NextRequest);
@@ -11,12 +13,31 @@ const mockNextFetchEvent: NextFetchEvent = mock(NextFetchEvent);
 
 describe("withAuthorizationReg middleware", () => {
   beforeEach(() => {
-    vi.clearAllMocks();
+    vi.resetAllMocks();
   });
   afterEach(() => {
     reset(mockedRequest);
   });
+  it("redirects to the declined page if the user is archived", async () => {
+    getToken.mockResolvedValue(mockCasPendingToken);
+    actionHandler.mockResolvedValue(true); // this mocks hitting registration/user/user-is-archived
 
+    // The user tries to access the register an register an operation page
+    const nextUrl = new NextURL(`${domain}/registration/register-an-operation`);
+
+    when(mockedRequest.nextUrl).thenReturn(nextUrl);
+    when(mockedRequest.url).thenReturn(domain);
+
+    const result = await middleware(
+      instance(mockedRequest),
+      mockNextFetchEvent,
+    );
+    expect(NextResponse.redirect).toHaveBeenCalledOnce();
+    expect(NextResponse.redirect).toHaveBeenCalledWith(
+      new URL("/dashboard/declined", domain),
+    );
+    expect(result?.status).toBe(307);
+  });
   it("redirects to the onboarding page if the user is not authenticated", async () => {
     // The user tries to access the register an register an operation page
     const nextUrl = new NextURL(`${domain}/registration/register-an-operation`);
