b791ab045b3065608f62416e01094b4ec3bd4ff6
Brianna Cerkiewicz <brianna@button.is>
2025-04-02 08:53:52 -0700

    chore: remove pending as a dropdown option

--- bciers/apps/administration/app/components/users/cells/InternalUserRoleColumnCell.tsx
+++ bciers/apps/administration/app/components/users/cells/InternalUserRoleColumnCell.tsx
@@ -48,7 +48,6 @@ const InternalUserRoleColumnCell = (
         <MenuItem value="cas_analyst">Analyst</MenuItem>
         <MenuItem value="cas_director">Director</MenuItem>
         <MenuItem value="cas_view_only">View-only</MenuItem>
-        <MenuItem value="cas_pending">Pending</MenuItem>
       </Select>
     </FormControl>
   );
