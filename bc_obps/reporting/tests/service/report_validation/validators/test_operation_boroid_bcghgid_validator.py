079e845cd900f70a9c6ffb69741c62fbc73e0c9c
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-04-04 12:23:25 -0700

    test: testing the facility report validation

--- bc_obps/reporting/tests/service/report_validation/validators/test_operation_boroid_bcghgid_validator.py
+++ bc_obps/reporting/tests/service/report_validation/validators/test_operation_boroid_bcghgid_validator.py
@@ -3,5 +3,11 @@ import pytest
 
 @pytest.mark.django_db
 class TestOperationBoroBcghgidValidator:
-    def test_fails(self):
+    def test_error_if_no_boro_id(self):
+        raise
+
+    def test_error_if_no_bcghgid(self):
+        raise
+
+    def test_succeeds_if_boro_and_bcghgid(self):
         raise
