550c02d914dc04de4c28cb2fe62d76b0f60cd446
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-04-04 10:58:35 -0700

    feat: adding validators for operation and facility BORO ID and BCGHGID

---
+++ bc_obps/reporting/service/report_validation/validators/facility_bcghgid_presence.py
@@ -0,0 +1,22 @@
+from typing import List
+from reporting.models.facility_report import FacilityReport
+from reporting.models.report_version import ReportVersion
+from reporting.service.report_validation.report_validation_error import (
+    ReportValidationError,
+    Severity,
+)
+
+
+def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
+    facility_reports: List[FacilityReport] = report_version.facility_reports.all()
+
+    errors = {}
+
+    for facility_report in facility_reports:
+        if facility_report.facility_bcghgid is None:
+            errors[f"facility_bcghgid_{facility_report.facility_id}"] = ReportValidationError(
+                Severity.ERROR,
+                f"Report is missing a BCGHGID for the facility {facility_report.facility_name}, please make sure one has been assigned.",
+            )
+
+    return errors
