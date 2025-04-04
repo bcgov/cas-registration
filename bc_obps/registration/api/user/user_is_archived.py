652e547f9108c06d65df5be09600ae58babee0cd
Brianna Cerkiewicz <brianna@button.is>
2025-04-04 08:23:31 -0700

    chore: apply suggestions from code review

--- bc_obps/registration/api/user/user_is_archived.py
+++ bc_obps/registration/api/user/user_is_archived.py
@@ -6,10 +6,7 @@ from service.data_access_service.user_service import UserDataAccessService
 from registration.schema import Message
 from registration.api.router import router
 from service.error_service.custom_codes_4xx import custom_codes_4xx
-import logging
 from django.http import JsonResponse
-
-logger = logging.getLogger(__name__)
 
 
 @router.get(
@@ -20,13 +17,12 @@ logger = logging.getLogger(__name__)
 )
 def check_is_user_archived(request: HttpRequest) -> JsonResponse | Tuple[Literal[200], bool]:
     # we can't use request.current_user if the user is archived
-
     auth_header = request.headers.get("Authorization")
     if auth_header:
         try:
             user_data = json.loads(auth_header)
             user_guid = user_data.get("user_guid")
-        except ValueError as e:
-            logger.warning(f"Invalid Authorization header: {e}", exc_info=True)
+        except ValueError:
             return JsonResponse({"error": "Invalid Authorization header"}, status=400)
-    return 200, bool(UserDataAccessService.get_by_guid(user_guid=user_guid, include_archived=True).archived_at)
+        return 200, bool(UserDataAccessService.get_by_guid(user_guid=user_guid, include_archived=True).archived_at)
+    return JsonResponse({"error": "Invalid Authorization header"}, status=400)
--- bciers/apps/dashboard/app/dashboard/declined/page.tsx
+++ bciers/apps/dashboard/app/dashboard/declined/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { AlertIcon } from "@bciers/components/icons";
 
 export default function Page() {
