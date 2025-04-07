bb6c3f94e185434d7021ae0fd7a25ee06a879638
Andrea Williams <andrea.williams@gov.bc.ca>
2025-04-07 16:21:04 -0700

    chore: updated test_send_reg_and_boro_id_email working; refactored TestSendOperatorAccessRequestEmail for consistency

--- bc_obps/registration/tests/test_emails.py
+++ bc_obps/registration/tests/test_emails.py
@@ -18,9 +18,8 @@ external_user_email_address = "send_operator_access_request_email@email.test"
 
 
 class TestSendOperatorAccessRequestEmail:
-    def test_send_operator_access_request_email(self, mocker):
-        # Mock the send_email_by_template method to prevent sending real emails
-        mocked_send_email_by_template = mocker.patch.object(email_service, 'send_email_by_template')
+    def mock_email_service(self, mocker):
+        expected_email_notifications_after_sending = []
 
         expected_context = {
             "operator_legal_name": operator_legal_name,
@@ -28,43 +27,61 @@ class TestSendOperatorAccessRequestEmail:
             "external_user_email_address": external_user_email_address,
         }
 
-        for access_type in AccessRequestTypes:
-            for access_state in AccessRequestStates:
-                template_name = f"{access_type.value} Access Request {access_state.value}"
-                transaction_id = uuid.uuid4()
-                message_id = uuid.uuid4()
-                mocked_send_email_by_template.return_value = {
+        def generate_return_value(*args, **kwargs):
+            transaction_id = uuid.uuid4()
+            message_id = uuid.uuid4()
+            expected_email_notifications_after_sending.append(
+                {
                     'txId': transaction_id,
                     'messages': [{'msgId': message_id}],
+                    'template': args[0].id,
+                    'recipients': args[2][0],
                 }
-                # Get the template instance
-                template_instance = EmailNotificationTemplateService.get_template_by_name(template_name)
+            )
+            return {'txId': transaction_id, 'messages': [{'msgId': message_id}]}
 
-                # Make sure we don't have an email notification for the user before they request admin access
-                assert not EmailNotification.objects.filter(
-                    transaction_id=transaction_id,
-                    message_id=message_id,
-                    template=template_instance,
-                    recipients_email=[external_user_email_address],
-                ).exists()
+        # Mock the send_email_by_template method to prevent sending real emails
+        mocked_send_email_by_template = mocker.patch.object(
+            email_service, 'send_email_by_template', side_effect=generate_return_value
+        )
 
-                # Call the function
-                send_operator_access_request_email(
-                    access_state, access_type, operator_legal_name, external_user_full_name, external_user_email_address
-                )
+        return mocked_send_email_by_template, expected_email_notifications_after_sending, expected_context
 
-                # Assert that send_email_by_template is called with the correct data
-                mocked_send_email_by_template.assert_called_with(
-                    template_instance, expected_context, [external_user_email_address]
-                )
+    @pytest.mark.parametrize("access_type", list(AccessRequestTypes))
+    @pytest.mark.parametrize("access_state", list(AccessRequestStates))
+    def test_send_operator_access_request_email(self, access_type, access_state, mocker):
+        template_name = f"{access_type.value} Access Request {access_state.value}"
+        template_instance = EmailNotificationTemplateService.get_template_by_name(template_name)
+        (
+            mocked_send_email_by_template,
+            expected_email_notifications_after_sending,
+            expected_context,
+        ) = self.mock_email_service(mocker)
 
-                # Assert that an email notification record was created
-                assert EmailNotification.objects.filter(
-                    transaction_id=transaction_id,
-                    message_id=message_id,
-                    template=template_instance,
-                    recipients_email=[external_user_email_address],
-                ).exists()
+        # Make sure we don't have an email notification for the user before they request admin access
+        assert not EmailNotification.objects.filter(
+            template=template_instance,
+            recipients_email=[external_user_email_address],
+        ).exists()
+
+        # Call the function
+        send_operator_access_request_email(
+            access_state, access_type, operator_legal_name, external_user_full_name, external_user_email_address
+        )
+
+        # Assert that send_email_by_template is called with the correct data
+        mocked_send_email_by_template.assert_called_with(
+            template_instance, expected_context, [external_user_email_address]
+        )
+
+        # Assert that an email notification records were created
+        for expected_email_notification in expected_email_notifications_after_sending:
+            assert EmailNotification.objects.filter(
+                transaction_id=expected_email_notification['txId'],
+                message_id=expected_email_notification['messages'][0]['msgId'],
+                template=expected_email_notification['template'],
+                recipients_email=[expected_email_notification['recipients']],
+            ).exists()
 
 
 class TestSendBoroIdApplicationEmail:
@@ -95,21 +112,20 @@ class TestSendBoroIdApplicationEmail:
         )
         return mocked_send_email_by_template, expected_email_notifications_after_sending
 
-    @pytest.mark.parametrize("application_state", list(EmailTemplateNames))
-    def test_send_registration_and_boro_id_email_with_the_same_recipients(self, application_state, mocker):
+    @pytest.mark.parametrize("template_name", list(EmailTemplateNames))
+    def test_send_registration_and_boro_id_email_with_the_same_recipients(self, template_name, mocker):
         assert EmailNotification.objects.count() == 0
-        template_name = f"BORO ID Application {application_state.value}"
-        template_instance = EmailNotificationTemplateService.get_template_by_name(template_name)
+        template_instance = EmailNotificationTemplateService.get_template_by_name(template_name.value)
         mocked_send_email_by_template, expected_email_notifications_after_sending = self.mock_email_service(mocker)
 
         self.operation.created_by = user_baker({'email': 'email@email.com'})
         self.operation.save()
 
         send_registration_and_boro_id_email(
-            application_state,
+            template_name.value,
             self.operation.operator.legal_name,
-            self.operation.name,
-            operation_creator=self.operation.created_by,
+            self.operation,
+            registration_creator=self.operation.created_by,
         )
 
         assert mocked_send_email_by_template.call_count == 1
