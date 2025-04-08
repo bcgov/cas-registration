e6ce7bbebf7e655f53f8591dc6cb10aece19da93
Josh Gamache <jsgamache@gmail.com>
2025-04-08 13:07:36 -0600

    chore: account for processing time to run the check more efficiently

--- bc_obps/registration/management/commands/check_document_file_status.py
+++ bc_obps/registration/management/commands/check_document_file_status.py
@@ -3,22 +3,39 @@ from registration.models import Document
 from service.data_access_service.document_service import DocumentDataAccessService
 import time
 
-# Repeat the check every 45 seconds for 37 times, equalling a total of 27:45 minutes with 2:75 minutes for pod leeway
-REPETITIONS = 37  # Number of times to repeat
-REPEAT_DELAY = 45  # Delay between repeats in seconds
+# Repeat the check every 30 seconds for 59 times, equalling a total of 29:30 minutes with 30 seconds for pod leeway
+REPETITIONS = 59  # Number of times to repeat
+REPEAT_DELAY = 30  # Delay between scans in seconds
 
 
 class Command(BaseCommand):
-    help = (
-        f"Check the status of the malware scans unscanned documents every {REPEAT_DELAY} seconds, {REPETITIONS} times"
-    )
+    help = f"Check the status of the malware scans unscanned documents every {REPEAT_DELAY} seconds, {REPETITIONS} times. Accounts for processing time"
 
     def handle(self, *args, **kwargs):
         self.stdout.write(
             f"Starting check_document_file_status, checking every {REPEAT_DELAY} seconds for {REPETITIONS} times"
         )
-        for attempt in range(1, REPETITIONS + 1):
-            self.stdout.write("Checking status of malware scanning unscanned documents")
+        start_time = int(time.time())
+        iteration = 0
+        print(f"start_time: {start_time}")
+        while iteration <= REPETITIONS:
+            print(f"Iteration {iteration}")
+            # compute sleep duration (first iteration happens at start_time)
+            next_attempt_time = start_time + iteration * REPEAT_DELAY
+            sleep_duration = next_attempt_time - int(time.time())
+            print(
+                f"Current is {int(time.time())}, next attempt is {next_attempt_time}, sleep_duration is {sleep_duration}"
+            )
+
+            # move to next iteration
+            iteration = iteration + 1
+
+            # skip next iteration if we spent too much time on the previous iteration
+            if sleep_duration < 0:
+                continue
+
+            # wait the appropriate amount of time
+            time.sleep(sleep_duration)
 
             try:
                 unscanned_documents = Document.objects.filter(status=Document.FileStatus.UNSCANNED)
@@ -29,8 +46,5 @@ class Command(BaseCommand):
             except Exception as e:
                 self.stdout.write(self.style.NOTICE(f"Error checking status of documents: {e}"))
                 raise e
-
-            if attempt < REPETITIONS:
-                time.sleep(REPEAT_DELAY)  # Wait before retrying
 
         self.stdout.write("Completed check loop")
