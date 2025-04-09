b70714aecb0d993b67edfe0f6454fb3c52a6a72b
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2025-04-09 14:01:29 -0700

    fix: disallow 'e' in number fields

--- bciers/libs/components/src/form/widgets/TextWidget.tsx
+++ bciers/libs/components/src/form/widgets/TextWidget.tsx
@@ -33,6 +33,22 @@ const TextWidget: React.FC<WidgetProps> = ({
     onChange(val === "" ? undefined : val);
   };
 
+  // handleKeyDown and handlePaste are just for the number field's 'e
+  const handleKeyDown = (e: any) => {
+    // Necessary to check for 'e' since it does not appear in handleChange event
+    if (e.keyCode === 69) {
+      e.preventDefault();
+    }
+  };
+  const handlePaste = (e: any) => {
+    // Check to allow only optional -, .digit or digit.digit
+    const regExp = /^-?(\d+(\.\d*)?|\.\d+)$/g;
+    if (!regExp.test(e.clipboardData.getData("text"))) {
+      // Paste data is no good
+      e.preventDefault();
+    }
+  };
+
   const isError = rawErrors && rawErrors.length > 0;
   const borderColor = isError ? BC_GOV_SEMANTICS_RED : DARK_GREY_BG_COLOR;
 
@@ -55,6 +71,8 @@ const TextWidget: React.FC<WidgetProps> = ({
       sx={styles}
       type={type}
       placeholder={placeholder}
+      onKeyDown={type === "number" ? handleKeyDown : undefined}
+      onPaste={type === "number" ? handlePaste : undefined}
     />
   );
 };
