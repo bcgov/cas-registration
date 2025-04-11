36bf50bae88c6927570931f822891162b8b75edd
Brianna Cerkiewicz <brianna@button.is>
2025-04-11 09:23:06 -0700

    chore: put reg e2e job back into CI

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -14,7 +14,6 @@ import os
 from google.oauth2 import service_account
 from pathlib import Path
 import dj_database_url
-import urllib.parse
 from dotenv import load_dotenv
 
 # Sentry Exception Tracking
@@ -162,7 +161,12 @@ WSGI_APPLICATION = "bc_obps.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-default_db_url = f"postgres://{os.environ.get('DB_USER', 'postgres')}:{urllib.parse.quote(str(os.environ.get('DB_PASSWORD')))}@{os.environ.get('DB_HOST', '127.0.0.1')}:{os.environ.get('DB_PORT', '5432')}/{os.environ.get('DB_NAME', 'registration')}"
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST", "db")
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_NAME = os.environ.get("DB_NAME", "registration")
+default_db_url = f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 DATABASES = {'default': dj_database_url.config(default=default_db_url, conn_max_age=60, conn_health_checks=True)}
 
 # Password validation
