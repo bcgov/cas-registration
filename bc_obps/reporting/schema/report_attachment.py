60503f2a1615f66036f2aea31d947f91347dbb96
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-04-10 22:00:08 -0700

    chore: updating scanning command to process report attachments
    chore: adding url to ninja schema

--- bc_obps/reporting/schema/report_attachment.py
+++ bc_obps/reporting/schema/report_attachment.py
@@ -3,6 +3,12 @@ from reporting.models.report_attachment import ReportAttachment
 
 
 class ReportAttachmentOut(ModelSchema):
+    url: str
+
     class Meta:
         model = ReportAttachment
-        fields = ['id', 'attachment_type', 'attachment_name']
+        fields = ["id", "attachment_type", "attachment_name"]
+
+    @staticmethod
+    def resolve_url(obj: ReportAttachment) -> str:
+        return obj.get_file_url() or ""
--- bciers/apps/reporting/src/app/components/attachments/types.ts
+++ bciers/apps/reporting/src/app/components/attachments/types.ts
@@ -2,4 +2,5 @@ export interface UploadedAttachment {
   id: number;
   attachment_name: string;
   attachment_type: string;
+  url: string;
 }
