d212e5f26175489e3e04217e5fcf68250f5c2d32
SeSo <sobhani.sepehr@gmail.com>
2025-04-16 09:14:57 -0700

    chore: capture db level errors using sentry

--- bc_obps/service/error_service/handle_exception.py
+++ bc_obps/service/error_service/handle_exception.py
@@ -1,12 +1,18 @@
+import logging
 import traceback
+import sentry_sdk
 from typing import Union
+from django.conf import settings
 from django.http import HttpRequest, Http404
-from django.db.utils import InternalError, ProgrammingError
+from django.db.utils import InternalError, ProgrammingError, DatabaseError
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from bc_obps.settings import DEBUG
 from ninja.responses import Response
 from registration.utils import generate_useful_error
 from registration.constants import UNAUTHORIZED_MESSAGE
+
+
+logger = logging.getLogger(__name__)
 
 
 def handle_exception(request: HttpRequest, exc: Union[Exception, type[Exception]]) -> Response:
@@ -32,7 +38,13 @@ def handle_exception(request: HttpRequest, exc: Union[Exception, type[Exception]
     if isinstance(exc, PermissionError):
         return Response({"message": "Permission denied."}, status=403)
 
-    if isinstance(exc, (InternalError, ProgrammingError)):
-        return Response({"message": "Internal Server Error"}, status=500)
+    if isinstance(exc, (InternalError, ProgrammingError, DatabaseError)):
+        msg = "Internal Server Error."
+        if settings.SENTRY_ENVIRONMENT == 'prod':
+            event_id = sentry_sdk.capture_exception(exc)
+            sentry_sdk.set_tag("database_error", True)
+            msg = f"{msg} Reference ID: {event_id}"
+        logger.error(msg, exc_info=exc)
+        return Response({"message": msg}, status=500)
 
     return Response({"message": str(exc)}, status=400)
