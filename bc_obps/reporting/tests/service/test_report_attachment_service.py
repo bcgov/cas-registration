421c8df8054da02abe9275bdf79d62d7ef5562b2
shon-button <shon@button.is>
2025-04-17 06:59:54 -0400

    chore: update attachments api

--- bc_obps/reporting/tests/service/test_report_attachment_service.py
+++ bc_obps/reporting/tests/service/test_report_attachment_service.py
@@ -8,6 +8,7 @@ import pytest
 from registration.models.user import User
 from reporting.models.report_attachment import ReportAttachment
 from reporting.service.report_attachment_service import ReportAttachmentService
+from reporting.models.report_attachment_confirmation import ReportAttachmentConfirmation
 
 
 pytestmark = pytest.mark.django_db
@@ -101,3 +102,62 @@ class TestReportAttachmentService:
         response = ReportAttachmentService.get_attachment(self.report_version.id, r.id)
 
         assert response == r
+    
+
+    def test_save_attachment_confirmations_creates_record_both_flags(self):
+        # Act
+        ReportAttachmentService.save_attachment_confirmations(
+            self.report_version.id,
+            confirm_required_uploaded=True,
+            confirm_existing_relevant=False,
+        )
+        # Assert
+        obj = ReportAttachmentConfirmation.objects.get(
+            report_version_id=self.report_version.id
+        )
+        assert obj.confirm_supplementary_required_attachments_uploaded is True
+        assert obj.confirm_supplementary_existing_attachments_relevant is False
+
+    def test_save_attachment_confirmations_creates_record_one_flag(self):
+        ReportAttachmentService.save_attachment_confirmations(
+            self.report_version.id,
+            confirm_required_uploaded=None,
+            confirm_existing_relevant=True,
+        )
+        obj = ReportAttachmentConfirmation.objects.get(
+            report_version_id=self.report_version.id
+        )
+        assert obj.confirm_supplementary_required_attachments_uploaded is False or obj.confirm_supplementary_required_attachments_uploaded is None
+        assert obj.confirm_supplementary_existing_attachments_relevant is True
+
+    def test_save_attachment_confirmations_updates_existing(self):
+        # Pre-create confirmation
+        orig = ReportAttachmentConfirmation.objects.create(
+            report_version_id=self.report_version.id,
+            confirm_supplementary_required_attachments_uploaded=False,
+            confirm_supplementary_existing_attachments_relevant=False,
+        )
+        # Act: update only required_uploaded
+        ReportAttachmentService.save_attachment_confirmations(
+            self.report_version.id,
+            confirm_required_uploaded=True,
+            confirm_existing_relevant=None,
+        )
+        obj = ReportAttachmentConfirmation.objects.get(
+            report_version_id=self.report_version.id
+        )
+        assert obj.id == orig.id
+        assert obj.confirm_supplementary_required_attachments_uploaded is True
+        assert obj.confirm_supplementary_existing_attachments_relevant is False
+
+    def test_save_attachment_confirmations_no_op_when_no_flags(self):
+        # Act
+        ReportAttachmentService.save_attachment_confirmations(
+            self.report_version.id,
+            confirm_required_uploaded=None,
+            confirm_existing_relevant=None,
+        )
+        # Assert no record created
+        assert ReportAttachmentConfirmation.objects.filter(
+            report_version_id=self.report_version.id
+        ).count() == 0
