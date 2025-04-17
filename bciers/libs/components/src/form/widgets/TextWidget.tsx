99de0ba0b01ff1b0d02e17ecdb0821f1f37fa404
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2025-04-16 17:52:20 -0700

    fix: disallow 'e' in number fields by separating text and number fields

--- bciers/libs/components/src/form/widgets/TextWidget.tsx
+++ bciers/libs/components/src/form/widgets/TextWidget.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { TextField } from "@mui/material";
+import { NumberField } from "@base-ui-components/react/number-field";
 import { WidgetProps } from "@rjsf/utils/lib/types";
 import {
   DARK_GREY_BG_COLOR,
   BC_GOV_SEMANTICS_RED,
+  BC_GOV_LINKS_COLOR,
 } from "@bciers/styles/colors";
 
 const TextWidget: React.FC<WidgetProps> = ({
@@ -27,10 +29,13 @@ const TextWidget: React.FC<WidgetProps> = ({
 
   const handleChange = (e: { target: { value: string } }) => {
     const val = e.target.value;
-    if (type === "number" && !isNaN(Number(val)) && Number(val) > maxNum)
-      return;
-
     onChange(val === "" ? undefined : val);
+  };
+
+  const handleNumberChange = (val: number | null) => {
+    if (!isNaN(Number(val)) && Number(val) > maxNum) return;
+
+    onChange(val);
   };
 
   const isError = rawErrors && rawErrors.length > 0;
@@ -45,17 +50,42 @@ const TextWidget: React.FC<WidgetProps> = ({
     },
   };
 
-  return (
-    <TextField
-      id={id}
-      disabled={disabled || readonly}
-      name={id}
-      value={value}
-      onChange={handleChange}
-      sx={styles}
-      type={type}
-      placeholder={placeholder}
-    />
-  );
+  if (type === "number") {
+    return (
+      <NumberField.Root
+        id={id}
+        disabled={disabled || readonly}
+        value={value}
+        onValueChange={handleNumberChange}
+        style={styles}
+      >
+        <NumberField.Group style={styles}>
+          <NumberField.Input
+            className={`w-full px-[14px] py-4 rounded-sm border border-transparent hover:border-[${BC_GOV_LINKS_COLOR}]`}
+            style={styles}
+            // sx doesn't like numberInput and tailwind doesn't like dynamic colors
+            onMouseEnter={(e) => {
+              e.currentTarget.style.borderColor = BC_GOV_LINKS_COLOR;
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.style.borderColor = "transparent";
+            }}
+          />
+        </NumberField.Group>
+      </NumberField.Root>
+    );
+  } else {
+    return (
+      <TextField
+        id={id}
+        disabled={disabled || readonly}
+        name={id}
+        value={value}
+        onChange={handleChange}
+        sx={styles}
+        placeholder={placeholder}
+      />
+    );
+  }
 };
 export default TextWidget;
