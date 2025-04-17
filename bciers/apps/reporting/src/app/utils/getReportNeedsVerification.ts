91312623b996da3584190ce2a7122de3a692ebb4
shon-button <shon@button.is>
2025-04-17 11:04:57 -0400

    chore: add eio flow
    chore: cleanup

--- bciers/apps/reporting/src/app/utils/getReportNeedsVerification.ts
+++ bciers/apps/reporting/src/app/utils/getReportNeedsVerification.ts
@@ -1,6 +1,15 @@
 import { actionHandler } from "@bciers/actions";
+import { getReportingOperation } from "@reporting/src/app/utils/getReportingOperation";
+import { OperationTypes } from "@bciers/utils/src/enums";
 
 export async function getReportNeedsVerification(reportVersionId: number) {
+  // 🚀 Fetch the operation associated with the specified version ID
+  const reportOperation = await getReportingOperation(reportVersionId);
+  // Check for EIO bypass
+  const operationType = reportOperation?.operation_type;
+  if (operationType === OperationTypes.EIO) {
+    return true;
+  }
   const endpoint = `reporting/report-version/${reportVersionId}/report-needs-verification`;
   const response = await actionHandler(endpoint, "GET");
   if (response.error) {
