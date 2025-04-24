3d54ae811bbb3372d1884d6d1d75402af08af610
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-04-24 13:04:13 -0700

    chore: adding jsonschema lib

--- bc_obps/reporting/service/report_validation/validators/__init__.py
+++ bc_obps/reporting/service/report_validation/validators/__init__.py
@@ -5,6 +5,7 @@ from . import (
     mandatory_verification_statement,
     operation_boroid_presence,
     report_attachments_are_scanned,
+    report_activity_json_validation,
 )
 from . import supplementary_report_attachments_confirmation
 
@@ -16,4 +17,5 @@ __all__ = [
     "operation_boroid_presence",
     "report_attachments_are_scanned",
     "supplementary_report_attachments_confirmation",
+    "report_activity_json_validation",
 ]
