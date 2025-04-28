8f6e53bc05e98fa559328766404e2c996c153853
Slava <slava.ivanov@gov.bc.ca>
2025-04-28 01:42:47 -0700

    chore: refactor

--- bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/review-compliance-summary/MonetaryPaymentsAlertNote.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/review-compliance-summary/MonetaryPaymentsAlertNote.tsx
@@ -5,9 +5,7 @@ export const MonetaryPaymentsAlertNote = () => {
   return (
     <div className="mb-5">
       <Alert severity="info" icon={<AlertIcon />}>
-        Your compliance obligation for the 2024 reporting year is{" "}
-        <strong>due on November 30, 2025</strong>. Please pay five business days
-        in advance to account for the processing time.
+        You have not made any monetary payment yet.
       </Alert>
     </div>
   );
