f1eb69d67639e64af2916deabebed9002d298d27
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-04-29 09:03:10 -0700

    chore: test endpoint

--- bc_obps/reporting/service/report_validation/validators/report_activity_json_validation.py
+++ bc_obps/reporting/service/report_validation/validators/report_activity_json_validation.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 import jsonschema
 import jsonschema.protocols
 from reporting.models.report_version import ReportVersion
@@ -7,6 +8,21 @@ from reporting.service.report_validation.report_validation_error import (
     ReportValidationError,
 )
 from service.form_builder_service import FormBuilderService
+
+
+def insert_additional_properties_keys(schema: Any) -> None:
+    if isinstance(schema, list):
+        for item in schema:
+            insert_additional_properties_keys(item)
+
+    if not isinstance(schema, dict):
+        return
+
+    for key in schema:
+        insert_additional_properties_keys(schema[key])
+
+    if "properties" in schema and schema.get("type") == "object":
+        schema["unevaluatedProperties"] = False
 
 
 def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
@@ -24,10 +40,10 @@ def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
                 ).values_list("id", flat=True)
             )
 
-            print(report_raw_activity.activity_id)
-            print(report_version.id)
-            print(source_type_ids)
-            print(facility_report.facility_id)
+            # print(report_raw_activity.activity_id)
+            # print(report_version.id)
+            # print(source_type_ids)
+            # print(facility_report.facility_id)
 
             activity_schema = json.loads(
                 FormBuilderService.build_form_schema(
@@ -37,12 +53,18 @@ def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
                     facility_report.facility_id,
                 )
             )["schema"]
-            # activity_schema["additionalProperties"] = False
+            insert_additional_properties_keys(activity_schema)
 
-            print(data)
-            print(activity_schema)
+            # data["sourceTypes"]["gscWithProductionOfUsefulEnergy"]["units"][0][
+            #     "extraUnitProp"
+            # ] = "sheep"
 
-            result = jsonschema.Draft202012Validator.validate(data, activity_schema)
+            # print(json.dumps(data, indent=1))
+            print(json.dumps(activity_schema, indent=2))
+
+            validator = jsonschema.Draft202012Validator(activity_schema)
+            result = validator.validate(data)
+
             print(result)
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
