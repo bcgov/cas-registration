cdf9f14b01421f5093c6b3c871753b903511c1b7
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2025-04-30 16:14:58 -0700

    chore: Update return as suggested by Shon in pr review

--- bciers/libs/components/src/layout/NewTabBanner.tsx
+++ bciers/libs/components/src/layout/NewTabBanner.tsx
@@ -9,22 +9,20 @@ const NewTabBanner: React.FC = () => {
   const isNewTab = (searchParams?.get("isNewTab") as string) === "true";
   const fromRegistration =
     (searchParams?.get("from_registration") as string) === "true";
-  return (
-    isNewTab && (
-      <>
-        {fromRegistration ? (
-          <Note variant="important">
-            This link has opened in a new tab. If you make edits here, refresh
-            the previous tab to see the updates. To go back, close this tab.
-          </Note>
-        ) : (
-          <Note variant="important">
-            This link has opened in a new tab. To go back to the report, close
-            this tab. Then click sync latest changes.
-          </Note>
-        )}
-      </>
-    )
+
+  if (!isNewTab) {
+    return null;
+  }
+  return fromRegistration ? (
+    <Note variant="important">
+      This link has opened in a new tab. If you make edits here, refresh the
+      previous tab to see the updates. To go back, close this tab.
+    </Note>
+  ) : (
+    <Note variant="important">
+      This link has opened in a new tab. To go back to the report, close this
+      tab. Then click sync latest changes.
+    </Note>
   );
 };
 
