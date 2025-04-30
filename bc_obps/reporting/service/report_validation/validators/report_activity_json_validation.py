e84d5e4dd4d41f2c26247dfd2f4db698161e673f
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-04-30 08:55:34 -0700

    chore: enable schema validation with draft 2020

--- bc_obps/reporting/service/report_validation/validators/report_activity_json_validation.py
+++ bc_obps/reporting/service/report_validation/validators/report_activity_json_validation.py
@@ -1,28 +1,36 @@
 import json
 from typing import Any
 import jsonschema
-import jsonschema.protocols
 from reporting.models.report_version import ReportVersion
 from reporting.models.source_type import SourceType
 from reporting.service.report_validation.report_validation_error import (
     ReportValidationError,
+    Severity,
 )
 from service.form_builder_service import FormBuilderService
 
 
-def insert_additional_properties_keys(schema: Any) -> None:
+def enable_jsonschema_draft_2020_validation(schema: Any) -> None:
+    """
+    RJSF uses draft-07 and doesn't support the `unevaluatedProperties` keyword.
+    This method replces the `dependencies` keyword with `dependentSchemas` and adds
+    `unevaluatedProperties: False` to the schema where appropriate, to enable
+    validation with draft-2020-12.
+    """
+
     if isinstance(schema, list):
         for item in schema:
-            insert_additional_properties_keys(item)
+            enable_jsonschema_draft_2020_validation(item)
 
-    if not isinstance(schema, dict):
-        return
+    if isinstance(schema, dict):
+        for key in schema:
+            enable_jsonschema_draft_2020_validation(schema[key])
 
-    for key in schema:
-        insert_additional_properties_keys(schema[key])
+        if "properties" in schema and schema.get("type") == "object":
+            schema["unevaluatedProperties"] = False
 
-    if "properties" in schema and schema.get("type") == "object":
-        schema["unevaluatedProperties"] = False
+        if "dependencies" in schema:
+            schema["dependentSchemas"] = schema.pop("dependencies")
 
 
 def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
@@ -30,42 +38,34 @@ def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
 
     for facility_report in report_version.facility_reports.all():
         for report_raw_activity in facility_report.reportrawactivitydata_records.all():
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
             data = report_raw_activity.json_data
 
             source_type_json_keys = [source_type for source_type in data["sourceTypes"]]
             source_type_ids = list(
-                SourceType.objects.filter(
-                    json_key__in=source_type_json_keys
-                ).values_list("id", flat=True)
+                SourceType.objects.filter(json_key__in=source_type_json_keys).values_list("id", flat=True)
             )
-
-            # print(report_raw_activity.activity_id)
-            # print(report_version.id)
-            # print(source_type_ids)
-            # print(facility_report.facility_id)
 
             activity_schema = json.loads(
                 FormBuilderService.build_form_schema(
                     report_raw_activity.activity_id,
                     report_version.id,
                     source_type_ids,
-                    facility_report.facility_id,
+                    str(facility_report.facility_id),
                 )
             )["schema"]
-            insert_additional_properties_keys(activity_schema)
 
-            # data["sourceTypes"]["gscWithProductionOfUsefulEnergy"]["units"][0][
-            #     "extraUnitProp"
-            # ] = "sheep"
-
-            # print(json.dumps(data, indent=1))
-            print(json.dumps(activity_schema, indent=2))
+            enable_jsonschema_draft_2020_validation(activity_schema)
 
             validator = jsonschema.Draft202012Validator(activity_schema)
-            result = validator.validate(data)
 
-            print(result)
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            try:
+                validator.validate(data)
+            except jsonschema.ValidationError as e:
+                errors[
+                    f"facility_{facility_report.facility_id}_{report_raw_activity.activity.slug}"
+                ] = ReportValidationError(
+                    severity=Severity.ERROR,
+                    message=f"Validation error: {e.message} at: {' > '.join(str(elt) for elt in e.path)}",
+                )
 
     return errors
