0b6cc7c1ef08db7a12a2da9a5cd88b53864073bc
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-04-30 18:02:18 -0700

    chore: added requested changes

--- bc_obps/reporting/schema/electricity_import_data.py
+++ bc_obps/reporting/schema/electricity_import_data.py
@@ -1,6 +1,5 @@
 from ninja import ModelSchema
 from pydantic import alias_generators
-from decimal import Decimal
 from reporting.models import ElectricityImportData
 
 
@@ -28,24 +27,14 @@ class ElectricityImportDataOut(ModelSchema):
 
 class ElectricityImportDataIn(ModelSchema):
     """
-    Schema for the save report additional data endpoint request input.
+    Schema for the save electricity import data endpoint request input.
     """
-
-    import_specified_electricity: Decimal | int
-    import_specified_emissions: Decimal | int
-    import_unspecified_electricity: Decimal | int
-    import_unspecified_emissions: Decimal | int
-    export_specified_electricity: Decimal | int
-    export_specified_emissions: Decimal | int
-    export_unspecified_electricity: Decimal | int
-    export_unspecified_emissions: Decimal | int
-    canadian_entitlement_electricity: Decimal | int
-    canadian_entitlement_emissions: Decimal | int
 
     class Meta:
         alias_generator = to_snake
         model = ElectricityImportData
         fields = [
+            'import_specified_electricity',
             'import_specified_emissions',
             'import_unspecified_electricity',
             'import_unspecified_emissions',
