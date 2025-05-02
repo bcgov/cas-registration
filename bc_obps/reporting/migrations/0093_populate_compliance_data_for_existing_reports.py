76779a261fc10aab2b663fce6acde73991d0af5c
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-05-02 13:08:24 -0700

    chore: ignore immutability / audit column triggers in compliance data migration

--- bc_obps/reporting/migrations/0093_populate_compliance_data_for_existing_reports.py
+++ bc_obps/reporting/migrations/0093_populate_compliance_data_for_existing_reports.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations
 from reporting.service.compliance_service import ComplianceService
+import common.lib.pgtrigger as pgtrigger
 
 
 def populate_compliance_data_for_existing_reports(apps, schema_monitor):
@@ -11,8 +12,11 @@ def populate_compliance_data_for_existing_reports(apps, schema_monitor):
 
     # Retrieve submitted report versions & persist the compliance data for those versions
     ReportVersion = apps.get_model("reporting", "ReportVersion")
-    for r in ReportVersion.objects.filter(status='Submitted'):
-        ComplianceService.save_compliance_data(r.id)
+    with pgtrigger.ignore(
+        "reporting.ReportVersion:immutable_report_version", "reporting.ReportVersion.set_updated_audit_columns"
+    ):
+        for r in ReportVersion.objects.filter(status='Submitted'):
+            ComplianceService.save_compliance_data(r.id)
 
 
 class Migration(migrations.Migration):
