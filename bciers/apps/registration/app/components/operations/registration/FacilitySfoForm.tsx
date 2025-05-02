031232cb811ea770cee80f6953a33dae328b2f5d
Brianna Cerkiewicz <brianna@button.is>
2025-05-02 09:13:23 -0700

    feat: add save button to facility form Co-authored by: Shon shon@button.is

---
+++ bciers/apps/registration/app/components/operations/registration/FacilitySfoForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { UUID } from "crypto";
+import { IChangeEvent } from "@rjsf/core";
+import { RJSFSchema } from "@rjsf/utils";
+import { actionHandler } from "@bciers/actions";
+import MultiStepBase from "@bciers/components/form/MultiStepBase";
+import {
+  facilityInformationSfoSchema,
+  facilityInformationSfoUiSchema,
+} from "apps/registration/app/data/jsonSchema/operationRegistration/facilityInformation";
+import { FacilitySfoFormData } from "apps/registration/app/components/operations/registration/types";
+import { createUnnestedFormData } from "@bciers/components/form/formDataUtils";
+import { facilitiesSfoSchema } from "@/administration/app/data/jsonSchema/facilitiesSfo";
+
+interface FacilitySfoFormProps {
+  facilityId?: UUID;
+  formData: FacilitySfoFormData;
+  operationId: UUID | "create";
+  step: number;
+  steps: string[];
+}
+
+const FacilitySfoForm = ({
+  formData,
+  operationId,
+  step,
+  steps,
+}: FacilitySfoFormProps) => {
+  const [formState, setFormState] = useState(formData ?? {});
+  // Get the list of sections in the SFO schema - used to unnest the formData
+  const formSectionListSfo = Object.keys(
+    facilitiesSfoSchema.properties as RJSFSchema,
+  );
+
+  const handleFormChange = useCallback(
+    (e: IChangeEvent) => {
+      setFormState(e.formData);
+    },
+    [setFormState],
+  );
+
+  const handleSubmit = async (e: IChangeEvent) => {
+    const body = [
+      {
+        ...createUnnestedFormData(e.formData, formSectionListSfo),
+        operation_id: operationId,
+      },
+    ];
+    const response = await actionHandler(
+      "registration/facilities",
+      "POST",
+      "",
+      {
+        body: JSON.stringify(body),
+      },
+    );
+    if (!response || response?.error) {
+      return { error: response.error };
+    }
+    return response;
+  };
+
+  return (
+    <MultiStepBase
+      allowBackNavigation
+      baseUrl={`/register-an-operation/${operationId}`}
+      cancelUrl="/"
+      formData={formState}
+      onChange={handleFormChange}
+      onSubmit={handleSubmit}
+      schema={facilityInformationSfoSchema}
+      step={step}
+      steps={steps}
+      uiSchema={facilityInformationSfoUiSchema}
+    ></MultiStepBase>
+  );
+};
+
+export default FacilitySfoForm;
