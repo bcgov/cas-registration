03f20b3f79afbbf9fde4c5ef164767fa4261c6e3
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-05-02 17:15:45 -0700

    test: testing validator

---
+++ bc_obps/reporting/tests/service/report_validation/validators/test_report_activity_json_validator.py
@@ -0,0 +1,254 @@
+import json
+from unittest.mock import patch
+from model_bakery.baker import make_recipe
+import pytest
+from reporting.service.report_validation.report_validation_error import (
+    ReportValidationError,
+    Severity,
+)
+from reporting.service.report_validation.validators.report_activity_json_validation import (
+    enable_jsonschema_draft_2020_validation,
+    validate,
+)
+
+
+@pytest.mark.django_db
+class TestReportActivityJsonValidator:
+    def test_enable_jsonschema_draft_2020_validation(self):
+        schema = {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "test_prop": {
+                    "type": "object",
+                    "properties": {
+                        "test_prop_1": {"type": "string"},
+                        "test_prop_2": {"type": "string"},
+                    },
+                    "dependencies": {
+                        "test_prop_1": {
+                            "properties": {
+                                "test_prop_2": {
+                                    "type": "object",
+                                    "properties": {"test_prop_3": {}},
+                                },
+                            }
+                        }
+                    },
+                },
+            },
+            "dependencies": {"name": {"minLength": 3}},
+        }
+
+        enable_jsonschema_draft_2020_validation(schema)
+
+        assert not schema["unevaluatedProperties"]
+        assert not schema["properties"]["test_prop"]["unevaluatedProperties"]
+        assert not schema["properties"]["test_prop"]["dependentSchemas"]["test_prop_1"]["properties"]["test_prop_2"]
+
+        assert "dependentSchemas" in schema and "dependencies" not in schema
+        assert (
+            "dependentSchemas" in schema["properties"]["test_prop"]
+            and "dependencies" not in schema["properties"]["test_prop"]
+        )
+
+    @patch("service.form_builder_service.FormBuilderService.build_form_schema")
+    def test_validate_with_extra_field(self, mock_build_form_schema):
+        mock_build_form_schema.return_value = json.dumps(
+            {
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "testSourceType": {
+                            "type": "boolean",
+                        },
+                        "sourceTypes": {
+                            "type": "object",
+                            "properties": {
+                                "testSourceType": {
+                                    "type": "object",
+                                    "properties": {
+                                        "prop1": {"type": "string"},
+                                    },
+                                }
+                            },
+                        },
+                    },
+                }
+            }
+        )
+
+        raw_activity_data = make_recipe(
+            "reporting.tests.utils.report_raw_activity_data",
+            facility_report__facility__id="00000000-0000-0000-0000-000000000012",
+            activity__slug="test-activity",
+            json_data={
+                "testSourceType": True,
+                "sourceTypes": {
+                    "testSourceType": {
+                        "prop1": "value1",
+                        "prop3": "value3",
+                    }
+                },
+            },
+        )
+
+        errors = validate(raw_activity_data.facility_report.report_version)
+        assert errors == {
+            "facility_00000000-0000-0000-0000-000000000012_test-activity": ReportValidationError(
+                Severity.ERROR,
+                "Validation error: Unevaluated properties are not allowed ('prop3' was unexpected) at: sourceTypes > testSourceType",
+            )
+        }
+
+    @patch("service.form_builder_service.FormBuilderService.build_form_schema")
+    def test_validate_with_dependent_schema(self, mock_build_form_schema):
+        mock_build_form_schema.return_value = json.dumps(
+            {
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "testSourceType": {
+                            "type": "boolean",
+                        },
+                        "sourceTypes": {
+                            "type": "object",
+                            "properties": {
+                                "testSourceType": {
+                                    "type": "object",
+                                    "properties": {
+                                        "prop1": {
+                                            "type": "string",
+                                            "enum": ["value1", "value2"],
+                                        },
+                                    },
+                                    "dependencies": {
+                                        "prop1": {
+                                            "oneOf": [
+                                                {
+                                                    "properties": {
+                                                        "prop1": {"enum": ["value1"]},
+                                                        "prop2": {"type": "string"},
+                                                    }
+                                                },
+                                                {
+                                                    "properties": {
+                                                        "prop1": {"enum": ["value2"]},
+                                                        "prop3": {"type": "number"},
+                                                    }
+                                                },
+                                            ]
+                                        }
+                                    },
+                                }
+                            },
+                        },
+                    },
+                }
+            }
+        )
+
+        raw_activity_data = make_recipe(
+            "reporting.tests.utils.report_raw_activity_data",
+            facility_report__facility__id="00000000-0000-0000-0000-000000000012",
+            activity__slug="test-activity",
+            json_data={
+                "testSourceType": True,
+                "sourceTypes": {
+                    "testSourceType": {
+                        "prop1": "value2",
+                        "prop3": 1234,
+                    }
+                },
+            },
+        )
+
+        errors = validate(raw_activity_data.facility_report.report_version)
+        assert errors == {}
+
+        # Need to test the other branch of the dependent schema
+        # Need to test no matching branch of the dependent schema
+
+    def test_validate_with_field_type():
+        raise
+
+    def test_validate_integration():
+        test_data = {
+            "fuelCombustionForElectricityGeneration": True,
+            "sourceTypes": {
+                "fuelCombustionForElectricityGeneration": {
+                    "units": [
+                        {
+                            "fuels": [
+                                {
+                                    "fuelType": {
+                                        "fuelName": "Acid Gas",
+                                        "fuelUnit": "Sm^3",
+                                        "fuelClassification": "Non-biomass",
+                                    },
+                                    "emissions": [
+                                        {
+                                            "gasType": "CH4",
+                                            "emission": 777,
+                                            "equivalentEmission": "Value will be computed upon saving",
+                                            "methodology": {
+                                                "methodology": "Default EF",
+                                                "unitFuelCh4DefaultEf": 888,
+                                                "unitFuelCh4DefaultEfFieldUnits": "g/fuel units",
+                                            },
+                                        }
+                                    ],
+                                    "fuelDescription": "test description",
+                                    "annualFuelAmount": 666,
+                                }
+                            ],
+                            "netPower": 555,
+                            "unitName": "test unit",
+                            "unitType": "Kiln",
+                            "generationType": "Cogeneration",
+                            "nameplateCapacity": 444,
+                            "cycleType": "Topping",
+                            "thermalOutput": 111,
+                            "supplementalFiringPurpose": "Industrial output",
+                            "steamHeatAcquisitionAmount": 222,
+                            "steamHeatAcquisitionProvider": "333",
+                        },
+                        {
+                            "fuels": [
+                                {
+                                    "fuelType": {
+                                        "fuelName": "Diesel",
+                                        "fuelUnit": "kilolitres",
+                                        "fuelClassification": "Non-biomass",
+                                    },
+                                    "emissions": [
+                                        {
+                                            "gasType": "N2O",
+                                            "emission": 4444,
+                                            "equivalentEmission": "Value will be computed upon saving",
+                                            "methodology": {
+                                                "methodology": "Heat Input/Default EF",
+                                                "unitFuelHeatInput": 5555,
+                                                "unitFuelN2OHeatInputDefaultEf": 6666,
+                                                "unitFuelN2OHeatInputDefaultEfFieldUnits": "g/GJ",
+                                            },
+                                        }
+                                    ],
+                                    "fuelDescription": "2222",
+                                    "annualFuelAmount": 3333,
+                                }
+                            ],
+                            "netPower": 1111,
+                            "unitName": "test unit 2",
+                            "unitType": "Other",
+                            "generationType": "Non-Cogeneration",
+                            "nameplateCapacity": 999,
+                        },
+                    ]
+                }
+            },
+        }
+
+        assert test_data == {}
+
+        raise
