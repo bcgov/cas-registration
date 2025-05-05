8861f82c94c8685684af545d1854c080b3cb1974
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-05-05 14:29:51 -0700

    chore: style

--- bc_obps/reporting/api/submit.py
+++ bc_obps/reporting/api/submit.py
@@ -2,9 +2,7 @@ from uuid import UUID
 from reporting.schema.report_sign_off import ReportSignOffIn
 from common.api.utils.current_user_utils import get_current_user_guid
 from django.http import HttpRequest
-from reporting.api.permissions import (
-    approved_industry_user_report_version_composite_auth,
-)
+from reporting.api.permissions import approved_industry_user_report_version_composite_auth
 from reporting.constants import EMISSIONS_REPORT_TAGS
 from reporting.schema.generic import Message
 from reporting.service.report_submission_service import ReportSubmissionService
