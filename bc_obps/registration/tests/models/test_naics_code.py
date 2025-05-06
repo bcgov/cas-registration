3174781a940a244a89b19694eef51f19ca810390
Brianna Cerkiewicz <brianna@button.is>
2025-05-06 08:27:42 -0700

    test: pytests for unregulated NAICS codes

--- bc_obps/registration/tests/models/test_naics_code.py
+++ bc_obps/registration/tests/models/test_naics_code.py
@@ -8,12 +8,14 @@ class NaicsCodeModelTest(BaseTestCase):
         cls.test_object = NaicsCode.objects.create(
             naics_code="1",
             naics_description="test",
+            is_regulated=True,
         )
         cls.field_data = [
             ("id", "ID", None, None),
             ("naics_code", "naics code", 1000, None),
             ("naics_description", "naics description", 1000, None),
             ("operations", "operation", None, None),
+            ("is_regulated", "is regulated", None, None),
             ("operations_naics_secondary", "operation", None, None),
             ("operations_naics_tertiary", "operation", None, None),
             ("regulatory_values", "naics regulatory value", None, None),
