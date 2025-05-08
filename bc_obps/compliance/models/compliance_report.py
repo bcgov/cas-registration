f1ba60cd3d02a609bdae1aa8b03b9a50e0503ae2
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-05-08 11:52:06 -0700

    chore: add compliance_report anchor model

---
+++ bc_obps/compliance/models/compliance_report.py
@@ -0,0 +1,43 @@
+from django.db import models
+from registration.models.time_stamped_model import TimeStampedModel
+from registration.models import Operation
+from reporting.models.report import Report
+from .compliance_period import CompliancePeriod
+from .rls_configs.compliance_report import Rls as ComplianceReportRls
+
+
+class ComplianceReport(TimeStampedModel):
+    """Model to store compliance reports"""
+
+    report = models.OneToOneField(
+        Report,
+        on_delete=models.PROTECT,
+        related_name="compliance_report",
+        db_comment="The emissions report associated with this compliance report",
+    )
+
+    operation = models.ForeignKey(
+      Operation,
+      on_delete=models.PROTECT,
+      related_name="compliance_reports"
+    )
+
+    compliance_period = models.ForeignKey(
+        CompliancePeriod,
+        on_delete=models.PROTECT,
+        related_name="compliance_reports",
+        db_comment="The compliance period this compliance report is for",
+    )
+
+    class Meta(TimeStampedModel.Meta):
+        app_label = "compliance"
+        db_table_comment = "This table is the parent anchor table of all compliance data for an operation and compliance period"
+        db_table = 'erc"."compliance_report'
+        constraints = [
+            models.UniqueConstraint(
+                fields=["report", "compliance_period"],
+                name="unique_compliance_report_per_emission_report",
+            )
+        ]
+
+    Rls = ComplianceReportRls
