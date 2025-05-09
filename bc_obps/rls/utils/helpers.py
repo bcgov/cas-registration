583d220a3d71221b410d2fc3eab887c1edbec1b0
Brianna Cerkiewicz <brianna@button.is>
2025-05-09 14:05:16 -0700

    chore: spec work on operation policy

--- bc_obps/rls/utils/helpers.py
+++ bc_obps/rls/utils/helpers.py
@@ -3,6 +3,21 @@ from typing import Dict, List, Any
 from rls.enums import RlsRoles, RlsOperations
 from rls.utils.grant import RlsGrant
 from rls.utils.m2m import M2mRls
+from rls.utils.policy import RlsPolicy
+
+
+# brianna access to their own operator, e.g. statement only
+approved_industry_user_statement = """
+                    operator_id IN (
+                        SELECT o.id
+                        FROM erc.operator o
+                        JOIN erc.user_operator uo
+                        ON o.id = uo.operator_id
+                        AND uo.user_id = current_setting('my.guid', true)::uuid
+                        AND uo.status = 'Approved'
+                    )
+                    """
+
 
 
 def generate_rls_grants(
@@ -15,6 +30,30 @@ def generate_rls_grants(
     """
     return [
         RlsGrant(role=role, grants=grants, table=table, schema=schema) for role, grants in role_grants_mapping.items()
+    ]
+
+def generate_rls_policies(
+    role_grants_mapping: Dict[RlsRoles, List[RlsOperations]],
+    table: Any,
+    using_statement: str = "FALSE",
+    schema: Schemas = Schemas.ERC,
+) -> List[RlsGrant]:
+    """
+    Generate a list of RlsPolicy objects for a given table based on role-to-grants mapping.
+    Each policy is defined for a specific role and operation. Using statement defaults to FALSE so that if forgotten, policies won't allow access.
+    """
+    return [
+        RlsPolicy(
+            role=role,
+            policy_name=f"{table.value.lower()}_{role.name.lower()}_{operation.name.lower()}",
+            operation=operation,
+            # CAS roles are all or nothing, but industry users need more granularity. In addition to role they have an app_role, and they should only be able to access records belonging to their organization
+            using_statement=using_statement if role.name not in [RlsRoles.CAS_ADMIN, RlsRoles.CAS_ANALYST, RlsRoles.CAS_DIRECTOR, RlsRoles.CAS_VIEW_ONLY] else "TRUE",
+            table=table,
+            schema=schema,
+        )
+        for role, operations in role_grants_mapping.items()
+        for operation in operations
     ]
 
 
