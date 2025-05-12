4d53d5bbdc93d517458085be26dfa1ce3c49953e
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-05-12 08:46:18 -0700

    chore: fixed after migration

--- bc_obps/reporting/migrations/0100_update_gwp_perfluoroethane.py
+++ bc_obps/reporting/migrations/0100_update_gwp_perfluoroethane.py
@@ -9,7 +9,7 @@ def update_gwp(apps, _):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('reporting', '0097_reportsignoff_acknowledgement_of_errors'),
+        ('reporting', '0099_remove_reportelectricityimportdata_set_created_audit_columns_and_more'),
     ]
 
     operations = [
