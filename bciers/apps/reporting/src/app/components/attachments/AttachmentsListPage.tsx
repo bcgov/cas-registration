edb3f1071b70faf6c2f285f72a25adbd845fb83e
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-05-13 20:53:07 -0700

    feat: internal page to list all attachments

--- bciers/apps/reporting/src/app/components/attachments/AttachmentsListPage.tsx
+++ bciers/apps/reporting/src/app/components/attachments/AttachmentsListPage.tsx
@@ -1,7 +1,11 @@
-import { List, ListItem } from "@mui/material";
-import getAttachmentsList from "../../utils/getAttachmentsList";
+import getAttachmentsList, {
+  AttachmentsSearchParams,
+} from "../../utils/getAttachmentsList";
+import { GridColDef } from "@mui/x-data-grid";
+import AttachmentsListGrid from "./AttachmentsListGrid";
 
-type Attachment = {
+export type Attachment = {
+  id: number;
   operator: string;
   operation: string;
   report_version_id: number;
@@ -10,17 +14,31 @@ type Attachment = {
   attachment_name: string;
 };
 
-const AttachmentsListPage: React.FC = async () => {
-  const data: Attachment[] = await getAttachmentsList();
+const columns: GridColDef[] = [
+  {
+    field: "operator",
+    headerName: "Operator",
+    flex: 1,
+  },
+  { field: "operation", flex: 1, headerName: "Operation" },
+  { field: "report_version_id", flex: 0.5, headerName: "Version ID" },
+  { field: "attachment_type", flex: 1, headerName: "Type" },
+  { field: "attachment_name", flex: 1, headerName: "Download" },
+];
+
+interface Props {
+  searchParams: AttachmentsSearchParams;
+}
+
+const AttachmentsListPage: React.FC<Props> = async ({ searchParams }) => {
+  const data = await getAttachmentsList(searchParams);
 
   return (
     <div>
-      <h1>Attachments:</h1>
-      <List dense={true}>
-        {data.map((a, index) => (
-          <ListItem key={index}>{a.operation}</ListItem>
-        ))}
-      </List>
+      <h1 className="form-heading-label">
+        <label>Attachments:</label>
+      </h1>
+      <AttachmentsListGrid columns={columns} initialData={data} />
     </div>
   );
 };
