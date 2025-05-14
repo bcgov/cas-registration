66dcab8cd33a3ae2918a3eaeb1ea2ed1f55f3e05
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-05-14 16:14:01 -0700

    chore: download and open in new tab

---
+++ bciers/apps/reporting/src/app/components/attachments/download.ts
@@ -0,0 +1,22 @@
+import getAttachmentFileUrl from "../../utils/getAttachmentFileUrl";
+
+export default async function downloadAttachment(
+  versionId: number,
+  attachmentId: number | undefined,
+) {
+  // This should not happen in a regular scenario
+  if (!attachmentId)
+    throw new Error("Unable to download a file without an id.");
+
+  const response = await getAttachmentFileUrl(versionId, attachmentId);
+
+  // 'download' attribute is not available for an external URL like our storage API
+  const anchorTag = document.createElement("a");
+  Object.assign(anchorTag, {
+    target: "_blank",
+    rel: "noopener noreferrer",
+    href: response,
+    // Browsers will ignore the download attribute since the URL is not from the same origin
+  });
+  anchorTag.click();
+}
