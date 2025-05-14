02de1fc1affd788d22127cd2c2d296b5564a4824
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-05-14 14:24:48 -0700

    chore: filename rename

--- bc_obps/compliance/api/__init__.py
+++ bc_obps/compliance/api/__init__.py
@@ -2,7 +2,7 @@
 from .router import router
 
 # Import new API structure
-from .compliance_report_version import get_compliance_report_versions_list
+from .compliance_report_versions import get_compliance_report_versions_list
 from ._compliance_report_version.compliance_report_version_id import get_compliance_report_version
 
 # from ._summaries._summary_id.issuance import get_compliance_summary_issuance
