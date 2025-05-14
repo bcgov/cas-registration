c555545afaf3202508cb38d8e4c23b70b41e4d7c
fviduya <francesca.viduya@gov.bc.ca>
2025-05-13 18:03:56 -0600

    chore: fix failing CI

--- bciers/libs/e2e/src/utils/helpers.ts
+++ bciers/libs/e2e/src/utils/helpers.ts
@@ -288,7 +288,9 @@ export async function fillRequiredFormFields(
       // Special condition for province
       if (/province/i.test(labelText)) {
         await inputField.fill(values[labelText]);
-        await page.getByRole("option", { name: values[labelText] }).click();
+        const option = page.getByRole("option", { name: values[labelText] });
+        await expect(option).toBeVisible();
+        await option.click();
       } else {
         await inputField.fill(values[labelText]);
       }
