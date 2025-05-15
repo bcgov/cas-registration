fc1ef13b231c5f57b742ca7c84449dd1b60b2252
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-05-15 15:00:08 -0700

    test: testing api endpoint

--- bc_obps/reporting/tests/api/test_report_attachment_internal_endpoint.py
+++ bc_obps/reporting/tests/api/test_report_attachment_internal_endpoint.py
@@ -12,14 +12,38 @@ class TestReportAttachmentInternalEndpoints(CommonTestSetup):
         TestUtils.save_app_role(self, "cas_analyst")
 
     def test_gets_only_submitted_attachments(self):
-
         # Create a report version with attachments
-        report_version = make_recipe("reporting.tests.utils.report_version", status="Submitted")
-        make_recipe(
+        report_version = make_recipe(
+            "reporting.tests.utils.report_version",
+            status="Submitted",
+            report__operation__name="test operation",
+            report__operator__legal_name="test operator",
+        )
+
+        attachment = make_recipe(
             "reporting.tests.utils.report_attachment",
             report_version=report_version,
             attachment_type=ReportAttachment.ReportAttachmentType.VERIFICATION_STATEMENT,
             attachment_name="test_attachment.txt",
+        )
+        attachment2 = make_recipe(
+            "reporting.tests.utils.report_attachment",
+            report_version=report_version,
+            attachment_type=ReportAttachment.ReportAttachmentType.CONFIDENTIALITY_REQUEST,
+            attachment_name="test_conf_req.txt",
+        )
+
+        report_version2 = make_recipe(
+            "reporting.tests.utils.report_version",
+            status="Draft",
+            report__operation__name="test operation 2",
+            report__operator__legal_name="test operator 2",
+        )
+        make_recipe(
+            "reporting.tests.utils.report_attachment",
+            report_version=report_version2,
+            attachment_type=ReportAttachment.ReportAttachmentType.CONFIDENTIALITY_REQUEST,
+            attachment_name="test_conf_req2.txt",
         )
 
         # Call the endpoint
@@ -27,4 +51,24 @@ class TestReportAttachmentInternalEndpoints(CommonTestSetup):
 
         # Assert the response
         assert response.status_code == 200
-        assert response == "abc"
+        assert response.json() == {
+            "count": 2,
+            "items": [
+                {
+                    "attachment_name": "test_conf_req.txt",
+                    "attachment_type": "confidentiality_request",
+                    "id": attachment2.id,
+                    "operation": "test operation",
+                    "operator": "test operator",
+                    "report_version_id": report_version.id,
+                },
+                {
+                    "attachment_name": "test_attachment.txt",
+                    "attachment_type": "verification_statement",
+                    "id": attachment.id,
+                    "operation": "test operation",
+                    "operator": "test operator",
+                    "report_version_id": report_version.id,
+                },
+            ],
+        }
