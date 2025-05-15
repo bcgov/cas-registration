5c94f06a4272f3b304fd5e98fe174c77ff64d8c5
SeSo <sobhani.sepehr@gmail.com>
2025-05-14 20:23:08 -0700

    chore: add s to compliance report version endpoints
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/api/__init__.py
+++ bc_obps/compliance/api/__init__.py
@@ -3,8 +3,8 @@ from .router import router
 
 # Import new API structure
 from .compliance_report_versions import get_compliance_report_versions_list
-from ._compliance_report_version.compliance_report_version_id import get_compliance_report_version
-from ._compliance_report_version._compliance_report_version_id.payments import get_compliance_report_version_payments
+from ._compliance_report_versions._compliance_report_version_id.payments import get_compliance_report_version_payments
+from ._compliance_report_versions.compliance_report_version_id import get_compliance_report_version
 
 # from ._summaries._summary_id.issuance import get_compliance_summary_issuance
 
