77f21c8888ab0a5909f00e5464dfedc974105d83
SeSo <sobhani.sepehr@gmail.com>
2025-05-14 20:21:57 -0700

    chore: register ComplianceReportVersion and ReportComplianceSummary in django admin
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/admin.py
+++ bc_obps/compliance/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+from compliance.models import ComplianceReportVersion
+
+
+@admin.register(ComplianceReportVersion)
+class ComplianceReportVersionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'compliance_report',
+        'report_compliance_summary',
+        'excess_emissions_delta_from_previous',
+        'credited_emissions_delta_from_previous',
+        'status',
+    )
