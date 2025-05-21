c781474b25da7d4cf42bb4569bfdbc89750f4343
SeSo <sobhani.sepehr@gmail.com>
2025-05-21 13:50:25 -0700

    refactor: update EarnedCredits component to use camelCase for data properties and improve layout
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/compliance/src/app/components/compliance-summary/request-issuance/review-compliance-summary/EarnedCredits.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/request-issuance/review-compliance-summary/EarnedCredits.tsx
@@ -2,17 +2,21 @@ import { InfoRow } from "../../InfoRow";
 import { TitleRow } from "../../TitleRow";
 import { EarnedCreditsAlertNote } from "./EarnedCreditsAlertNote";
 
-export const EarnedCredits = ({ data }: any) => {
+interface Props {
+  data: any; // TODO: Define a proper type for the data
+}
+
+export const EarnedCredits = ({ data }: Props) => {
   return (
-    <div className={`w-full mb-2.5`}>
+    <div className={`w-full mt-16`}>
       <TitleRow label={`Earned Credits`} />
 
       <EarnedCreditsAlertNote />
 
-      <InfoRow label="Earned Credits:" value={data.earned_credits} />
+      <InfoRow label="Earned Credits:" value={data.earnedCredits} />
       <InfoRow
         label="Status of Issuance:"
-        value={data.issuance_status}
+        value={data.issuanceStatus}
         style={{ marginBottom: "50px" }}
       />
     </div>
