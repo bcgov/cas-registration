24f60584e659cc59453c2a2d282cad128cd1aa93
SeSo <sobhani.sepehr@gmail.com>
2025-05-21 15:23:55 -0700

    refactor: move compliance button to compliance related directory
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/review-compliance-summary/ComplianceSummaryReviewContent.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/review-compliance-summary/ComplianceSummaryReviewContent.tsx
@@ -6,7 +6,7 @@ import { ComplianceUnitsGrid } from "./ComplianceUnitsGrid";
 import { MonetaryPaymentsGrid } from "./MonetaryPaymentsGrid";
 import { OutstandingComplianceObligation } from "./OutstandingComplianceObligation";
 import { AutomaticOverduePenalty } from "./AutomaticOverduePenalty";
-import ComplianceStepButtons from "@bciers/components/form/components/ComplianceStepButtons";
+import ComplianceStepButtons from "@/compliance/src/app/components/ComplianceStepButtons";
 import { useState } from "react";
 import { PaymentsData } from "@/compliance/src/app/types/payments";
 
