5c6df9b99ea4b2959e6442e4a833dd50d99267d3
SeSo <sobhani.sepehr@gmail.com>
2025-05-21 13:28:01 -0700

    chore: add hidden field template
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

---
+++ bciers/libs/components/src/form/fields/HiddenFieldTemplate.tsx
@@ -0,0 +1,17 @@
+import { FieldTemplateProps } from '@rjsf/utils';
+import { InlineFieldTemplate } from "@bciers/components/form/fields/index";
+
+// Custom field to control visibility of both widget and label
+const HiddenFieldTemplate: React.FC<FieldTemplateProps> = (props) => {
+  const { formData } = props;
+
+  // Check if value is defined, not null, not empty string, and not empty array
+  const hasValue = formData !== undefined && formData !== null && formData !== '' && (!Array.isArray(formData) || formData.length > 0);
+
+  // If no value, return null to hide the entire field (including label)
+  if (!hasValue) return null;
+
+  return <InlineFieldTemplate {...props} />;
+};
+
+export default HiddenFieldTemplate;
