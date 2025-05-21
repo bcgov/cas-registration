a2573d3d276480367759e62338c333f65bf8277a
SeSo <sobhani.sepehr@gmail.com>
2025-05-21 13:42:43 -0700

    chore: update compliance api endpoint init
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/api/__init__.py
+++ bc_obps/compliance/api/__init__.py
@@ -4,8 +4,10 @@ from .router import router
 # Import new API structure
 from .compliance_report_versions import get_compliance_report_versions_list
 from ._compliance_report_versions.compliance_report_version_id import get_compliance_report_version
+from ._compliance_report_versions._compliance_report_version_id import operation
 
 # from ._summaries._summary_id.issuance import get_compliance_summary_issuance
 
 # Other imports
 from .invoice import generate_invoice
+from ._bccr._accounts import account_id
