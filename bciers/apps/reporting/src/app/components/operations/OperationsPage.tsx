2d3c2f8f61a4339d982c92c00d8f4a922e67d52d
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-05-22 14:47:16 -0700

    chore: fixed date in reports table page

--- bciers/apps/reporting/src/app/components/operations/OperationsPage.tsx
+++ bciers/apps/reporting/src/app/components/operations/OperationsPage.tsx
@@ -1,7 +1,7 @@
 import { getReportingYear } from "@reporting/src/app/utils/getReportingYear";
 import Operations from "apps/reporting/src/app/components/operations/Operations";
 import { OperationsSearchParams } from "apps/reporting/src/app/components/operations/types";
-import { formatDate } from "@reporting/src/app/utils/formatDate";
+import dayjs from "dayjs";
 
 export default async function OperationsPage({
   searchParams,
@@ -9,18 +9,18 @@ export default async function OperationsPage({
   searchParams: OperationsSearchParams;
 }) {
   const reportingYearObj = await getReportingYear();
+  const reportDueDate = reportingYearObj.report_due_date;
+  const yearOnly = dayjs(reportDueDate).year();
 
-  const reportDueDate = formatDate(
-    reportingYearObj.report_due_date,
-    "MMM DD,YYYY",
-  );
   return (
     <>
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-bold">
           Reporting year {reportingYearObj.reporting_year}
         </h2>
-        <h3 className="text-bc-text text-right">Reports due {reportDueDate}</h3>
+        <h3 className="text-bc-text text-right">
+          Reports due May 31,{yearOnly}
+        </h3>
       </div>
       <Operations searchParams={searchParams} />
     </>
