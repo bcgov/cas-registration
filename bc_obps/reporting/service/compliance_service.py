1499fbc1522397ea628feb772aef1163436be3ff
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-05-23 11:24:55 -0700

    chore: fixing funny category double count

--- bc_obps/reporting/service/compliance_service.py
+++ bc_obps/reporting/service/compliance_service.py
@@ -54,7 +54,9 @@ class ComplianceService:
 
     @staticmethod
     def get_regulatory_values_by_naics_code(report_version_id: int) -> RegulatoryValues:
-        data = ReportVersion.objects.select_related('report__operation').get(pk=report_version_id)
+        data = ReportVersion.objects.select_related("report__operation").get(
+            pk=report_version_id
+        )
         naics_code_id = data.report.operation.naics_code_id
         compliance_year = data.report.reporting_year.reporting_year
         regulatory_values = NaicsRegulatoryValue.objects.get(
@@ -74,23 +76,25 @@ class ComplianceService:
     def get_emissions_attributable_for_reporting(report_version_id: int) -> Decimal:
         records = ReportEmission.objects_with_decimal_emissions.filter(
             report_version_id=report_version_id,
-            emission_categories__category_type='basic',
+            emission_categories__category_type="basic",
         )
-        attributable_sum = records.aggregate(emission_sum=Sum('emission'))
+        attributable_sum = records.aggregate(emission_sum=Sum("emission"))
 
-        return attributable_sum['emission_sum'] or Decimal('0')
+        return attributable_sum["emission_sum"] or Decimal("0")
 
     @staticmethod
     def get_production_totals(report_version_id: int) -> Dict[str, Decimal]:
         records = ReportProduct.objects.filter(
             report_version_id=report_version_id,
         )
-        annual_production_sum = records.aggregate(production_sum=Sum('annual_production'))
-        apr_dec_sum = records.aggregate(production_sum=Sum('production_data_apr_dec'))
+        annual_production_sum = records.aggregate(
+            production_sum=Sum("annual_production")
+        )
+        apr_dec_sum = records.aggregate(production_sum=Sum("production_data_apr_dec"))
 
         return {
-            "annual_amount": annual_production_sum['production_sum'] or Decimal('0'),
-            "apr_dec": apr_dec_sum['production_sum'] or Decimal('0'),
+            "annual_amount": annual_production_sum["production_sum"] or Decimal("0"),
+            "apr_dec": apr_dec_sum["production_sum"] or Decimal("0"),
         }
 
     @staticmethod
@@ -102,26 +106,35 @@ class ComplianceService:
             report_product__product_id=product_id,
             emission_category_id__in=emission_category_ids,
         )
-        allocated_to_category_sum = records.aggregate(production_sum=Sum('allocated_quantity'))
+        allocated_to_category_sum = records.aggregate(
+            production_sum=Sum("allocated_quantity")
+        )
 
-        return allocated_to_category_sum['production_sum'] or Decimal('0')
+        return allocated_to_category_sum["production_sum"] or Decimal("0")
 
     @staticmethod
-    def get_report_product_aggregated_totals(report_version_id: int, product_id: int) -> Dict[str, Decimal]:
+    def get_report_product_aggregated_totals(
+        report_version_id: int, product_id: int
+    ) -> Dict[str, Decimal]:
         records = ReportProduct.objects.filter(
             report_version_id=report_version_id,
             product_id=product_id,
         )
-        product_annual_amount_sum = records.aggregate(production_sum=Sum('annual_production'))
-        apr_dec_sum = records.aggregate(production_sum=Sum('production_data_apr_dec'))
+        product_annual_amount_sum = records.aggregate(
+            production_sum=Sum("annual_production")
+        )
+        apr_dec_sum = records.aggregate(production_sum=Sum("production_data_apr_dec"))
 
         return {
-            "annual_amount": product_annual_amount_sum['production_sum'] or Decimal('0'),
-            "apr_dec": apr_dec_sum['production_sum'] or Decimal('0'),
+            "annual_amount": product_annual_amount_sum["production_sum"]
+            or Decimal("0"),
+            "apr_dec": apr_dec_sum["production_sum"] or Decimal("0"),
         }
 
     @staticmethod
-    def get_reporting_only_allocated(report_version_id: int, product_id: int) -> Decimal:
+    def get_reporting_only_allocated(
+        report_version_id: int, product_id: int
+    ) -> Decimal:
         # CO2 emissions from excluded woody biomass, Other emissions from excluded biomass, Emissions from excluded non-biomass, Fugitive emissions, Venting emissions — non-useful
         reporting_only_category_ids = [10, 11, 12, 2, 7]
         reporting_only_allocated = ComplianceService.get_allocated_emissions_by_report_product_emission_category(
@@ -130,14 +143,26 @@ class ComplianceService:
         return reporting_only_allocated
 
     @staticmethod
-    def get_unregulated_products_allocated(report_version_id: int) -> Decimal:
-        unregulated_product_allocation_records = ReportProductEmissionAllocation.objects.filter(
+    def get_emissions_from_only_funny_category_13(report_version_id: int) -> Decimal:
+        """
+        We need to total up the emissions associated with category 13 without double-counting
+        (i.e. the ones that are not already under 10,11,12 fuel_excluded)
+        and subtract them from the total attributable for compliance,
+        since they're not based on a single product and therefore are not allocated anywhere.
+        """
+        records = ReportEmission.objects_with_decimal_emissions.filter(
             report_version_id=report_version_id,
-            report_product__product__is_regulated=False,
+            emission_categories__id=13,
+        ).exclude(
+            emission_categories__id__in=[10, 11, 12],
         )
-        return unregulated_product_allocation_records.aggregate(allocated_sum=Sum("allocated_quantity"))[
-            "allocated_sum"
+        return records.aggregate(emission_total=Sum("emission"))[
+            "emission_total"
         ] or Decimal("0")
+
+    @staticmethod
+    def get_fog_emissions():
+        pass
 
     @staticmethod
     def calculate_product_emission_limit(
@@ -151,14 +176,16 @@ class ComplianceService:
         initial_compliance_period: int = 2024,
     ) -> Decimal:
         # Handle divide by 0 error if allocated_for_compliance is 0
-        industrial_process_compliance_allocated_division = Decimal('0')
+        industrial_process_compliance_allocated_division = Decimal("0")
         if allocated_for_compliance > 0:
-            industrial_process_compliance_allocated_division = allocated_industrial_process / allocated_for_compliance
+            industrial_process_compliance_allocated_division = (
+                allocated_industrial_process / allocated_for_compliance
+            )
 
         product_emission_limit = (apr_dec_production * pwaei) * (
             reduction_factor
             - (
-                (Decimal('1') - (industrial_process_compliance_allocated_division))
+                (Decimal("1") - (industrial_process_compliance_allocated_division))
                 * tightening_rate
                 * (compliance_period - initial_compliance_period)
             )
@@ -167,8 +194,12 @@ class ComplianceService:
 
     @classmethod
     def get_calculated_compliance_data(cls, report_version_id: int) -> ComplianceData:
-        naics_data = ComplianceService.get_regulatory_values_by_naics_code(report_version_id)
-        registration_purpose = ReportVersion.objects.get(pk=report_version_id).report.operation.registration_purpose
+        naics_data = ComplianceService.get_regulatory_values_by_naics_code(
+            report_version_id
+        )
+        registration_purpose = ReportVersion.objects.get(
+            pk=report_version_id
+        ).report.operation.registration_purpose
         ##### Don't use schemas, use classes or dicts
         compliance_product_list: List[ReportProductComplianceData] = []
         total_allocated_reporting_only = Decimal(0)
@@ -179,7 +210,7 @@ class ComplianceService:
         report_products = (
             ReportProduct.objects.order_by("product_id")
             .filter(report_version_id=report_version_id, product__is_regulated=True)
-            .distinct('product_id')
+            .distinct("product_id")
         )
         # Iterate on all products reported (by product ID)
         for rp in report_products:
@@ -196,23 +227,33 @@ class ComplianceService:
             # the industrial_process emission total attributed to the product "Pulp and paper: chemical pulp".
             if (
                 rp.report_version.report.operation.naics_code
-                and rp.report_version.report.operation.naics_code.naics_code.startswith('322')
-                and rp.product.name == 'Pulp and paper: chemical pulp'
-            ):
-                overlapping_industrial_process_emissions = (
-                    EmissionCategoryService.get_industrial_process_excluded_biomass_overlap_by_report_version(
-                        report_version_id
-                    )
+                and rp.report_version.report.operation.naics_code.naics_code.startswith(
+                    "322"
                 )
-                industrial_process = industrial_process - overlapping_industrial_process_emissions
+                and rp.product.name == "Pulp and paper: chemical pulp"
+            ):
+                overlapping_industrial_process_emissions = EmissionCategoryService.get_industrial_process_excluded_biomass_overlap_by_report_version(
+                    report_version_id
+                )
+                industrial_process = (
+                    industrial_process - overlapping_industrial_process_emissions
+                )
 
-            production_totals = ComplianceService.get_report_product_aggregated_totals(report_version_id, rp.product_id)
+            production_totals = ComplianceService.get_report_product_aggregated_totals(
+                report_version_id, rp.product_id
+            )
             allocated = ComplianceService.get_allocated_emissions_by_report_product_emission_category(
                 report_version_id,
                 rp.product_id,
-                list(EmissionCategory.objects.filter(category_type='basic').values_list('id', flat=True)),
+                list(
+                    EmissionCategory.objects.filter(category_type="basic").values_list(
+                        "id", flat=True
+                    )
+                ),
             )
-            allocated_reporting_only = ComplianceService.get_reporting_only_allocated(report_version_id, rp.product_id)
+            allocated_reporting_only = ComplianceService.get_reporting_only_allocated(
+                report_version_id, rp.product_id
+            )
             allocated_for_compliance = allocated - allocated_reporting_only
             allocated_for_compliance_2024 = (
                 allocated_for_compliance / Decimal(production_totals["annual_amount"])
@@ -242,25 +283,39 @@ class ComplianceService:
                     apr_dec_production=production_totals["apr_dec"],
                     emission_intensity=ei,
                     allocated_industrial_process_emissions=industrial_process,
-                    allocated_compliance_emissions=round(allocated_for_compliance_2024, 4),
+                    allocated_compliance_emissions=round(
+                        allocated_for_compliance_2024, 4
+                    ),
                 )
             )
 
-        # Emissions allocated to unregulated products
-        total_allocated_unregulated_products = ComplianceService.get_unregulated_products_allocated(report_version_id)
+        # Emissions allocated to
+        total_allocated_unregulated_products = (
+            ComplianceService.get_emissions_from_only_funny_category_13(
+                report_version_id
+            )
+        )
         total_allocated_reporting_only += total_allocated_unregulated_products
 
         # Get attributable emission total
-        attributable_for_reporting_total = ComplianceService.get_emissions_attributable_for_reporting(report_version_id)
+        attributable_for_reporting_total = (
+            ComplianceService.get_emissions_attributable_for_reporting(
+                report_version_id
+            )
+        )
         # Calculated Excess/credited emissions
         excess_emissions = Decimal(0)
         credited_emissions = Decimal(0)
         if total_allocated_for_compliance_2024 > emissions_limit_total:
-            excess_emissions = total_allocated_for_compliance_2024 - emissions_limit_total
+            excess_emissions = (
+                total_allocated_for_compliance_2024 - emissions_limit_total
+            )
         else:
-            credited_emissions = emissions_limit_total - total_allocated_for_compliance_2024
+            credited_emissions = (
+                emissions_limit_total - total_allocated_for_compliance_2024
+            )
 
-        if registration_purpose == 'New Entrant Operation':
+        if registration_purpose == "New Entrant Operation":
             total_allocated_for_compliance_2024 = Decimal(0)
             emissions_limit_total = Decimal(0)
             excess_emissions = Decimal(0)
@@ -269,7 +324,9 @@ class ComplianceService:
         return_object = ComplianceData(
             emissions_attributable_for_reporting=attributable_for_reporting_total,
             reporting_only_emissions=round(Decimal(total_allocated_reporting_only), 4),
-            emissions_attributable_for_compliance=round(total_allocated_for_compliance_2024, 4),
+            emissions_attributable_for_compliance=round(
+                total_allocated_for_compliance_2024, 4
+            ),
             emissions_limit=round(emissions_limit_total, 4),
             excess_emissions=round(excess_emissions, 4),
             credited_emissions=round(credited_emissions, 4),
@@ -282,11 +339,17 @@ class ComplianceService:
     @classmethod
     @transaction.atomic()
     def save_compliance_data(cls, report_version_id: int) -> None:
-        compliance_data_to_save = ComplianceService.get_calculated_compliance_data(report_version_id)
+        compliance_data_to_save = ComplianceService.get_calculated_compliance_data(
+            report_version_id
+        )
         compliance_summary_record_id = None
         report_version_record = ReportVersion.objects.get(id=report_version_id)
-        if ReportComplianceSummary.objects.filter(report_version_id=report_version_id).exists():
-            compliance_summary_record_id = ReportComplianceSummary.objects.get(report_version_id=report_version_id).id
+        if ReportComplianceSummary.objects.filter(
+            report_version_id=report_version_id
+        ).exists():
+            compliance_summary_record_id = ReportComplianceSummary.objects.get(
+                report_version_id=report_version_id
+            ).id
         compliance_summary_record, _ = ReportComplianceSummary.objects.update_or_create(
             id=compliance_summary_record_id,
             report_version=report_version_record,
@@ -306,16 +369,20 @@ class ComplianceService:
         for product_data_to_save in compliance_data_to_save.products:
             product_data_id = None
             if ReportComplianceSummaryProduct.objects.filter(
-                report_version_id=report_version_id, product_id=product_data_to_save.product_id
+                report_version_id=report_version_id,
+                product_id=product_data_to_save.product_id,
             ):
                 product_data_id = ReportComplianceSummaryProduct.objects.get(
-                    report_version_id=report_version_id, product_id=product_data_to_save.product_id
+                    report_version_id=report_version_id,
+                    product_id=product_data_to_save.product_id,
                 ).id
             ReportComplianceSummaryProduct.objects.update_or_create(
                 id=product_data_id,
                 report_version=report_version_record,
                 report_compliance_summary=compliance_summary_record,
-                product=RegulatedProduct.objects.get(id=product_data_to_save.product_id),
+                product=RegulatedProduct.objects.get(
+                    id=product_data_to_save.product_id
+                ),
                 defaults={
                     "annual_production": product_data_to_save.annual_production,
                     "apr_dec_production": product_data_to_save.apr_dec_production,
