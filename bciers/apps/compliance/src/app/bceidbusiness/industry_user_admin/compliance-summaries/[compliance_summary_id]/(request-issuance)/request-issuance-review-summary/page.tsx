eb49158ebc7efbc68a667c1878dc438f732fb208
SeSo <sobhani.sepehr@gmail.com>
2025-05-23 11:19:26 -0700

    refactor: implement rjsf approach in all request issuance workflow
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

---
+++ bciers/apps/compliance/src/app/bceidbusiness/industry_user_admin/compliance-summaries/[compliance_summary_id]/(request-issuance)/request-issuance-review-summary/page.tsx
@@ -0,0 +1,4 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@/compliance/src/app/components/compliance-summary/request-issuance/review-compliance-summary/ComplianceSummaryReviewPage";
+
+export default defaultPageFactory(Page);
