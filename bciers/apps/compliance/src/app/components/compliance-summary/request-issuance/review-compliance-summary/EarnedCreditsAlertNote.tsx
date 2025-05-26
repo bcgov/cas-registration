96cc64f3e89dad5f8ca71b1ddb2c670a69ca4d69
SeSo <sobhani.sepehr@gmail.com>
2025-05-26 14:40:56 -0700

    refactor: replace Paper component with AlertNote in EarnedCreditsAlertNote and update schema properties
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/compliance/src/app/components/compliance-summary/request-issuance/review-compliance-summary/EarnedCreditsAlertNote.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/request-issuance/review-compliance-summary/EarnedCreditsAlertNote.tsx
@@ -1,30 +1,21 @@
-import { AlertIcon } from "@bciers/components/icons";
+import AlertNote from "@bciers/components/form/components/AlertNote";
 import { bcCarbonRegistryLink } from "@bciers/utils/src/urls";
-import { Link, Paper } from "@mui/material";
+import { Link } from "@mui/material";
 
 export const EarnedCreditsAlertNote = () => {
   return (
-    <Paper
-      className="p-[16px] mb-[10px] bg-[#DCE9F6] text-bc-text"
-      data-testid="earned-credits-alert-note"
-    >
-      <div className="flex items-center">
-        <AlertIcon width="40" height="40" />
-        <p className="ml-[10px] my-0">
-          The earned credits have not been issued yet. You may request issuance
-          of them as long as you have an active trading account in the{" "}
-          <Link
-            href={bcCarbonRegistryLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            underline="hover"
-          >
-            B.C. Carbon Registry
-          </Link>{" "}
-          (BCCR). Once issued, you may trade or use them to meet your compliance
-          obligation.
-        </p>
-      </div>
-    </Paper>
+    <AlertNote>
+      The earned credits have not been issued yet. You may request issuance of
+      them as long as you have an active trading account in the{" "}
+      <Link
+        href={bcCarbonRegistryLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        B.C. Carbon Registry
+      </Link>{" "}
+      (BCCR). Once issued, you may trade or use them to meet your compliance
+      obligation.
+    </AlertNote>
   );
 };
