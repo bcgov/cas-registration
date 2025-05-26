8f0deac2fb49a71be26c2d5bd5b1647090d0ebf4
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-05-26 11:45:42 -0700

    test: add tests for earned credit record creation

--- bc_obps/compliance/tests/service/test_compliance_report_version_service.py
+++ bc_obps/compliance/tests/service/test_compliance_report_version_service.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from compliance.service.compliance_report_version_service import ComplianceReportVersionService
 import pytest
 from unittest.mock import patch, MagicMock
-from compliance.models import ComplianceReportVersion
+from compliance.models import ComplianceReportVersion, ComplianceEarnedCredits
 from model_bakery import baker
 
 pytestmark = pytest.mark.django_db  # This is used to mark a test function as requiring the database
@@ -59,3 +59,24 @@ class TestComplianceReportVersionService:
         # Test OBLIGATION_FULLY_MET
         status = ComplianceReportVersionService._determine_compliance_status(Decimal('0.0'), Decimal('0.0'))
         assert status == ComplianceReportVersion.ComplianceStatus.OBLIGATION_FULLY_MET
+
+    def test_create_compliance_report_version_with_credited_emissions(self):
+        # Arrange
+        report_compliance_summary = baker.make_recipe(
+            'reporting.tests.utils.report_compliance_summary', credited_emissions=Decimal('10.6'), excess_emissions=0
+        )
+        compliance_report = baker.make_recipe(
+            'compliance.tests.utils.compliance_report', report_id=report_compliance_summary.report_version.report_id
+        )
+
+        # Act
+        result = ComplianceReportVersionService.create_compliance_report_version(
+            compliance_report, report_compliance_summary.report_version.id
+        )
+        earned_credits_record = ComplianceEarnedCredits.objects.get(compliance_report_version_id=result.id)
+
+        # Assert
+        assert result.status == ComplianceReportVersion.ComplianceStatus.EARNED_CREDITS
+        assert result.report_compliance_summary_id == report_compliance_summary.id
+        assert result.compliance_report_id == compliance_report.id
+        assert earned_credits_record.earned_credits_amount == 10
