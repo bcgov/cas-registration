c35e761acf400160f009835e4b65116e62caac70
SeSo <sobhani.sepehr@gmail.com>
2025-05-26 14:40:21 -0700

    chore: enhance ReadOnlyWidget to support prefix and suffix from uiSchema
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/libs/components/src/form/widgets/readOnly/ReadOnlyWidget.tsx
+++ bciers/libs/components/src/form/widgets/readOnly/ReadOnlyWidget.tsx
@@ -1,11 +1,15 @@
 import { WidgetProps } from "@rjsf/utils/lib/types";
 
-const ReadOnlyWidget: React.FC<WidgetProps> = ({ id, value }) => {
+const ReadOnlyWidget: React.FC<WidgetProps> = ({ id, value, uiSchema }) => {
   const formatValue = Array.isArray(value) ? value.join(",\n") : value;
+  const prefix = uiSchema?.["ui:options"]?.prefix as string;
+  const suffix = uiSchema?.["ui:options"]?.suffix as string;
 
   return (
     <div id={id} className="read-only-widget whitespace-pre-line">
+      {prefix && <span className="prefix">{prefix}</span>}
       {formatValue}
+      {suffix && <span className="suffix">{suffix}</span>}
     </div>
   );
 };
