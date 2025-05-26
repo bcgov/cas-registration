bea5d1a43edb821ce490ea0dcecd0764e392dfbf
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-05-26 11:45:42 -0700

    test: add tests for earned credit record creation

--- bc_obps/compliance/tests/service/test_compliance_earned_credits_service.py
+++ bc_obps/compliance/tests/service/test_compliance_earned_credits_service.py
@@ -2,6 +2,7 @@ from compliance.service.earned_credits_service import ComplianceEarnedCreditsSer
 import pytest
 from compliance.models import ComplianceEarnedCredits
 from model_bakery.baker import make_recipe
+from decimal import Decimal
 
 pytestmark = pytest.mark.django_db  # This is used to mark a test function as requiring the database
 
@@ -20,3 +21,16 @@ class TestComplianceEarnedCreditsService:
         )
         assert result.earned_credits_amount == 100
         assert result.issuance_status == ComplianceEarnedCredits.IssuanceStatus.CREDITS_NOT_ISSUED
+
+    def test_create_earned_credits_record(self):
+        report_compliance_summary = make_recipe(
+            'reporting.tests.utils.report_compliance_summary',
+            credited_emissions=Decimal('100.9898'),
+            excess_emissions=0,
+        )
+        compliance_report_version = make_recipe(
+            'compliance.tests.utils.compliance_report_version', report_compliance_summary=report_compliance_summary
+        )
+        result = ComplianceEarnedCreditsService.create_earned_credits_record(compliance_report_version)
+
+        assert result.earned_credits_amount == 100
