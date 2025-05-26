6d6f9e634b1cc446bc6cbbfa16c9f016c6f8d81b
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-05-26 11:39:15 -0700

    chore: earned credits record is created when a compliance summary has credited emissions

--- bc_obps/compliance/service/earned_credits_service.py
+++ bc_obps/compliance/service/earned_credits_service.py
@@ -1,4 +1,4 @@
-from compliance.models import ComplianceEarnedCredits
+from compliance.models import ComplianceEarnedCredits, ComplianceReportVersion
 from typing import Optional
 
 
@@ -23,4 +23,23 @@ class ComplianceEarnedCreditsService:
         earned_credits_record = ComplianceEarnedCredits.objects.filter(
             compliance_report_version_id=compliance_report_version_id
         ).first()
+        return earned_credits_record
+
+    @classmethod
+    def create_earned_credits_record(
+        cls, compliance_report_version: ComplianceReportVersion
+    ) -> ComplianceEarnedCredits:
+        """
+        Creates an earned credits record for the specific compliance compliance_report_version
+
+        Args:
+            compliance_report_version_id: The ID of the compliance compliance_report_version to retrieve earned credits data for
+
+        Returns:
+            The EarnedCredits object if it exists
+        """
+        earned_credits_record = ComplianceEarnedCredits.objects.create(
+            compliance_report_version=compliance_report_version,
+            earned_credits_amount=int(compliance_report_version.report_compliance_summary.credited_emissions),
+        )
         return earned_credits_record
