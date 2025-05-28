bc5265bd89a456a27f00ce717fd8b72e2cd2f81f
SeSo <sobhani.sepehr@gmail.com>
2025-05-28 11:37:14 -0700

    refactor: replace Paper in AlertNote for improved styling

--- bciers/apps/compliance/src/app/components/compliance-summaries/ComplianceSummariesPage.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summaries/ComplianceSummariesPage.tsx
@@ -1,10 +1,9 @@
 import { ComplianceSummariesSearchParams } from "@/compliance/src/app/types";
 import { fetchComplianceSummariesPageData } from "@/compliance/src/app/utils/fetchComplianceSummariesPageData";
-import { Alert } from "@mui/material";
-import AlertIcon from "@bciers/components/icons/AlertIcon";
 import ComplianceSummariesDataGrid from "@/compliance/src/app/components/compliance-summaries/ComplianceSummariesDataGrid";
 import Loading from "@bciers/components/loading/SkeletonForm";
 import { Suspense } from "react";
+import AlertNote from "@bciers/components/form/components/AlertNote";
 
 export default async function ComplianceSummariesPage({
   searchParams,
@@ -17,18 +16,18 @@ export default async function ComplianceSummariesPage({
     <div className="flex flex-col">
       <div className="mb-5">
         <div className="mb-2">
-          <Alert severity="info" icon={<AlertIcon />}>
+          <AlertNote>
             Your compliance obligation for the 2024 reporting year is{" "}
             <strong>due on November 30, 2025</strong>. Please pay five business
             days in advance to account for the processing time.
-          </Alert>
+          </AlertNote>
         </div>
-        <Alert severity="info" icon={<AlertIcon />}>
+        <AlertNote>
           An automatic overdue penalty has been incurred and{" "}
           <strong>accrues at 0.38% daily</strong> since the compliance
           obligation was not paid by its due date. You may pay the penalty after
           the compliance obligation is paid.
-        </Alert>
+        </AlertNote>
       </div>
       <Suspense fallback={<Loading />}>
         <ComplianceSummariesDataGrid initialData={initialData} />
