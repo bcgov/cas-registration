f5c6bf9232c901350ce93d702f4ae41812647ffe
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2025-05-29 15:36:53 -0700

    chore: replace download payment instruction page with RJSF form and rebase

--- bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/download-payment-instructions/PaymentInstructionsDownloadComponent.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/download-payment-instructions/PaymentInstructionsDownloadComponent.tsx
@@ -1,106 +1,36 @@
 "use client";
 
-import ComplianceHeading from "../../ComplianceHeading";
-import ComplianceSummaryTaskList from "@bciers/components/navigation/complianceSummaryTaskList/ComplianceSummaryTaskList";
-import ComplianceStepButtons from "@bciers/components/form/components/ComplianceStepButtons";
-import { TaskListElement } from "@bciers/components/navigation/reportingTaskList/types";
-import { TitleRow } from "../../TitleRow";
-import { InfoRow } from "../../InfoRow";
 import { useState } from "react";
+import { FormBase } from "@bciers/components/form";
+import ComplianceStepButtons from "../../../ComplianceStepButtons";
+import {
+  createDownloadPaymentInstructionsSchema,
+  downloadPaymentInstructionsUiSchema,
+} from "@/compliance/src/app/data/jsonSchema/manageObligation/downloadPaymentInstructionsSchema";
 
 interface Props {
-  readonly taskListElements: TaskListElement[];
-}
-
-function PayeeInformation() {
-  return (
-    <div className="w-full mb-8">
-      <InfoRow label={"Invoice #:"} value={"invoiceNumber"} classNames="mb-8" />
-      <TitleRow label={"Payee Information"} />
-      <InfoRow label={"Name of Bank:"} value={"bankName"} />
-      <InfoRow label={"Bank Transit Number:"} value={"transitNumber"} />
-      <InfoRow label={"Institution Number:"} value={"institutionNumber"} />
-      <InfoRow label={"Swift Code:"} value={"swiftCode"} />
-      <InfoRow label={"Account Number:"} value={"accountNumber"} />
-      <InfoRow label={"Account Name:"} value={"accountName"} />
-      <InfoRow label={"Bank Address:"} value={"bankAddress"} />
-    </div>
-  );
-}
-
-function PaymentInstructionsDetails() {
-  return (
-    <div className="w-full mb-8">
-      <TitleRow label={"Payment Instructions"} />
-      <h3>Before making a payment</h3>
-      <p>
-        Send an email notification to{" "}
-        <a href="mailto:OBPSPayments@gov.bc.ca">OBPSPayments@gov.bc.ca</a>{" "}
-        before sending payment
-      </p>
-      <p>Include:</p>
-      <ul>
-        <li>Operator Name</li>
-        <li>Exact payment date</li>
-        <li>Payment amount</li>
-        <li>Invoice number</li>
-      </ul>
-      <h3>Pay by electronic fund transfer (EFT)</h3>
-      <ul>
-        <li>Include the invoice number as a reference</li>
-        <li>
-          Pay at least five business days before the due date to allow for
-          processing time
-        </li>
-      </ul>
-      <h3>Pay by wire transfer</h3>
-      <p>Include the invoice number as a reference</p>
-      <h3>Provide correct information for timely processing</h3>
-      <p>
-        Failure to provide accurate payment information will result in delays in
-        payment processing and possible interest and/or penalty charges.
-      </p>
-      <p>
-        Contact{" "}
-        <a href="mailto:OBPSPayments@gov.bc.ca">OBPSPayments@gov.bc.ca</a> for
-        any questions.
-      </p>
-    </div>
-  );
-}
-
-function PaymentRemarks() {
-  return (
-    <div className="w-full">
-      <TitleRow label={"Remarks"} />
-      <ul>
-        <li>
-          Pay by the due date to avoid <a>penalties</a> and/or <a>interests</a>
-        </li>
-        <li>Do not include other charges with your payment for this invoice</li>
-        <li>Do not mail cash</li>
-      </ul>
-    </div>
-  );
-}
-
-function PaymentInstructions() {
-  return (
-    <div className="w-full">
-      <ComplianceHeading title="Download Payment Instructions" />
-      <PayeeInformation />
-      <PaymentInstructionsDetails />
-      <PaymentRemarks />
-    </div>
-  );
+  readonly complianceSummaryId: any;
 }
 
 export default function PaymentInstructionsDownloadComponent({
-  taskListElements,
+  complianceSummaryId,
 }: Props) {
+  const backUrl = `/compliance-summaries/${complianceSummaryId}/manage-obligation/review-compliance-summary`;
+  const saveAndContinueUrl = `/compliance-summaries/${complianceSummaryId}/manage-obligation/pay-obligation-track-payments`;
   const [isGeneratingDownload, setIsGeneratingDownload] = useState(false);
+  const instructionFormData = {
+    complianceSummaryId,
+    invoiceNumber: "TBD",
+    bankName: "Canadian Imperial Bank of Commerce",
+    bankTransitNumber: "00090",
+    institutionNumber: "010",
+    swiftCode: "CIBCCATT",
+    accountNumber: "09-70301",
+    accountName: "Province of British Columbia-OBPS-BCIERS",
+    bankAddress: "1175 DOUGLAS STREET, VICTORIA, BC V8W2E1",
+  };
+
   const handleDownloadInstructions = async () => {
-    const complianceSummaryId = "OBI000004";
     if (!complianceSummaryId) {
       return;
     }
@@ -119,28 +49,22 @@ export default function PaymentInstructionsDownloadComponent({
     }
   };
   return (
-    <div>
-      <div className="container mx-auto p-4" data-testid="compliance-review">
-        <h2 className="text-2xl font-bold mb-4 text-bc-bg-blue">
-          Pink Operation
-        </h2>
-      </div>
-      <div className="w-full flex">
-        <div className="hidden md:block">
-          <ComplianceSummaryTaskList elements={taskListElements} />
-        </div>
-        <PaymentInstructions />
-      </div>
+    <FormBase
+      schema={createDownloadPaymentInstructionsSchema()}
+      uiSchema={downloadPaymentInstructionsUiSchema}
+      formData={instructionFormData}
+      className="w-full"
+    >
       <ComplianceStepButtons
         key="form-buttons"
-        backUrl={"test"}
-        continueUrl={"test"}
+        backUrl={backUrl}
+        continueUrl={saveAndContinueUrl}
         backButtonDisabled={false}
         middleButtonDisabled={isGeneratingDownload}
         submitButtonDisabled={false}
         middleButtonText={"Download Payment Information"}
         onMiddleButtonClick={handleDownloadInstructions}
       />
-    </div>
+    </FormBase>
   );
 }
--- bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/download-payment-instructions/PaymentInstructionsDownloadPage.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/download-payment-instructions/PaymentInstructionsDownloadPage.tsx
@@ -3,7 +3,7 @@ import {
   generateManageObligationTaskList,
 } from "@/compliance/src/app/components/taskLists/1_manageObligationTaskList";
 import CompliancePageLayout from "@/compliance/src/app/components/layout/CompliancePageLayout";
-import ComplianceStepButtons from "@/compliance/src/app/components/ComplianceStepButtons";
+import PaymentInstructionsDownloadComponent from "./PaymentInstructionsDownloadComponent";
 
 interface Props {
   readonly compliance_summary_id: string;
@@ -12,7 +12,6 @@ interface Props {
 export default async function PaymentInstructionsDownloadPage({
   compliance_summary_id: complianceSummaryId,
 }: Props) {
-  // const complianceSummary = await getComplianceSummary(complianceSummaryId);
   const complianceSummary = {
     reportingYear: "2025",
   };
@@ -23,18 +22,13 @@ export default async function PaymentInstructionsDownloadPage({
     ActivePage.DownloadPaymentInstructions,
   );
 
-  const backUrl = `/compliance-summaries/${complianceSummaryId}/manage-obligation-review-summary`;
-  const saveAndContinueUrl = `/compliance-summaries/${complianceSummaryId}/pay-obligation-track-payments`;
-
   return (
     <CompliancePageLayout
       taskListElements={taskListElements}
       complianceSummaryId={complianceSummaryId}
     >
-      <p>PaymentInstructionsDownload ...TBD</p>
-      <ComplianceStepButtons
-        backUrl={backUrl}
-        continueUrl={saveAndContinueUrl}
+      <PaymentInstructionsDownloadComponent
+        complianceSummaryId={complianceSummaryId}
       />
     </CompliancePageLayout>
   );
