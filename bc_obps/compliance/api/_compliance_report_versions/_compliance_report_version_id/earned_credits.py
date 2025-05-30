d36a28c8e60d1cfd38abd6a1b32477451cdcada7
SeSo <sobhani.sepehr@gmail.com>
2025-05-30 15:09:47 -0700

    chore: add compliance report version earned credits endpoint and schema
    - Introduced a new endpoint to retrieve earned credits data for compliance report versions.
    - Updated response model to use ComplianceEarnedCreditsOut schema for better data representation.
    - Renamed existing endpoint for generating invoices to reflect its purpose more accurately.
    
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/api/_compliance_report_versions/_compliance_report_version_id/earned_credits.py
+++ bc_obps/compliance/api/_compliance_report_versions/_compliance_report_version_id/earned_credits.py
@@ -6,21 +6,18 @@ from compliance.models import ComplianceEarnedCredits
 from service.error_service.custom_codes_4xx import custom_codes_4xx
 from compliance.service.earned_credits_service import ComplianceEarnedCreditsService
 from registration.schema.generic import Message
-from ...router import router
+from compliance.api.router import router
 from typing import Optional
+from compliance.schema.compliance_earned_credits import ComplianceEarnedCreditsOut
 
 
 @router.get(
     "/compliance-report-versions/{compliance_report_version_id}/earned-credits",
-    response={200: ComplianceEarnedCredits, 404: Message, custom_codes_4xx: Message},
+    response={200: ComplianceEarnedCreditsOut, 404: Message, custom_codes_4xx: Message},
     tags=COMPLIANCE,
     description="Get earned credits data for a compliance report version",
     auth=authorize("approved_authorized_roles"),
 )
-def get_compliance_report_version_earned_credits(
-    request: HttpRequest, compliance_report_version_id: int
-) -> Tuple[int, Optional[ComplianceEarnedCredits]]:
-    compliance_earned_credits = ComplianceEarnedCreditsService.get_earned_credits_data_by_report_version(
-        compliance_report_version_id
-    )
+def get_compliance_report_version_earned_credits(request: HttpRequest, compliance_report_version_id: int) -> Tuple[int, Optional[ComplianceEarnedCredits]]:
+    compliance_earned_credits = ComplianceEarnedCreditsService.get_earned_credits_data_by_report_version(compliance_report_version_id)
     return 200, compliance_earned_credits
