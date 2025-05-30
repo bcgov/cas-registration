cb377bdfc4e08382bf04899ee043ab89e80411ba
Andrea Williams <andrea.williams@gov.bc.ca>
2025-05-29 17:34:48 -0700

    chore: count how many selected current_facilities there are, render page conditionally

--- bciers/apps/reporting/src/app/components/finalReview/FinalReviewPage.tsx
+++ bciers/apps/reporting/src/app/components/finalReview/FinalReviewPage.tsx
@@ -5,6 +5,7 @@ import reviewDataFactory, {
   ReviewData,
 } from "@reporting/src/app/components/finalReview/reviewDataFactory/factory";
 import { getNavigationInformation } from "@reporting/src/app/components/taskList/navigationInformation";
+import { getOperationFacilitiesList } from "@reporting/src/app/utils/getOperationFacilitiesList";
 import {
   HeaderStep,
   ReportingPage,
@@ -15,29 +16,19 @@ import { getIsSupplementaryReport } from "@reporting/src/app/utils/getIsSuppleme
 export default async function FinalReviewPage({
   version_id,
 }: HasReportVersion) {
-  // Get the report version flow data definitions
-  const flowData = await getFlowData(version_id);
+  const facilities = await getOperationFacilitiesList(version_id);
+  // count the number of current_facilities that are selected
+  const selectedFacilitiesCount = facilities.current_facilities.filter(
+    (facility) => facility.is_selected,
+  ).length;
 
-  const startTime = performance.now();
-
-  // Build final review data based on the report version flow type
-  const finalReviewData: ReviewData[] = await reviewDataFactory(
-    version_id,
-    flowData,
-  );
-
-  const endTime = performance.now();
-  console.log(
-    `Final review data factory took ${endTime - startTime} milliseconds`,
-  );
+  let finalReviewData: ReviewData[] = [];
 
   //🔍 Check if is a supplementary report
   const isSupplementaryReport = await getIsSupplementaryReport(version_id);
-
   //🔍 Check if reports need verification
   const { show_verification_page: showVerificationPage } =
     await getReportVerificationStatus(version_id);
-
   const navInfo = await getNavigationInformation(
     HeaderStep.SignOffSubmit,
     ReportingPage.FinalReview,
@@ -48,8 +39,30 @@ export default async function FinalReviewPage({
       skipChangeReview: !isSupplementaryReport,
     },
   );
+  // if number of facilities is >20
+  if (selectedFacilitiesCount > 20) {
+    console.warn(
+      "You have " +
+        selectedFacilitiesCount +
+        " facilities selected. That's too many facilities.",
+    );
+    // instead of using reviewDataFactory, we will fill finalReviewData with a component that
+    // displays a button that opens a new tab where the data will be displayed ugly-style
+  } else {
+    // carrry on with rest of page
+    // Get the report version flow data definitions
+    const flowData = await getFlowData(version_id);
 
-  console.log("finalReviewData", finalReviewData);
+    const startTime = performance.now();
+
+    // // Build final review data based on the report version flow type
+    finalReviewData = await reviewDataFactory(version_id, flowData);
+
+    const endTime = performance.now();
+    console.log(
+      `Final review data factory took ${endTime - startTime} milliseconds`,
+    );
+  }
 
   return (
     <FinalReviewForm navigationInformation={navInfo} data={finalReviewData} />
