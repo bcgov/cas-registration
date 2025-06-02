fc87351076142b795d29d327137eefd00de2790d
Brianna Cerkiewicz <brianna@button.is>
2025-06-02 09:24:21 -0700

    chore: seems to work?

--- bc_obps/rls/utils/helpers.py
+++ bc_obps/rls/utils/helpers.py
@@ -25,35 +25,48 @@ def generate_rls_policies(
     role_grants_mapping: Dict[RlsRoles, List[RlsOperations]],
     table: Any,
     using_statement: str = "FALSE",
-    check_statement: str = "",
+    check_statement: str = "FALSE",
     schema: Schemas = Schemas.ERC,
 ) -> List[RlsGrant]:
     """
     Generate a list of RlsPolicy objects for a given table based on role-to-grants mapping.
-    Each policy is defined for a specific role and operation. Using statement defaults to FALSE so that if forgotten, policies won't allow access.
+    - For SELECT or UPDATE operations: set using_statement
+    - For INSERT or UPDATE operations: set check_statement
+    - CAS roles always use "TRUE" for those fields; others use the provided arguments
     """
-    return [
-        RlsPolicy(
-            role=role,
-            policy_name=f"{table.value.lower()}_{role.name.lower()}_{operation.name.lower()}",
-            operation=operation,
-            # CAS roles are all or nothing, but industry users need more granularity. In addition to role they have an app_role, and they should only be able to access records belonging to their organization
-            **(
-                {"using_statement": using_statement if role.name in [RlsRoles.CAS_ADMIN, RlsRoles.CAS_ANALYST, RlsRoles.CAS_DIRECTOR, RlsRoles.CAS_VIEW_ONLY] else "TRUE"}
-                if operation in [RlsOperations.SELECT, RlsOperations.UPDATE]
-                else {}
-            ),
-            **(
-                {"check_statement": check_statement}
-                if operation in [RlsOperations.INSERT, RlsOperations.UPDATE]
-                else {}
-            ),
-            table=table,
-            schema=schema,
-        )
-        for role, operations in role_grants_mapping.items()
-        for operation in operations
-    ]
+    """
+    Generate a list of RlsPolicy objects for a given table based on role-to-grants mapping.
+    - For SELECT or UPDATE operations: set using_statement
+    - For INSERT or UPDATE operations: set check_statement
+    - CAS roles always use "TRUE" for those fields; others use the provided arguments
+    """
+    cas_roles = {
+        RlsRoles.CAS_ADMIN,
+        RlsRoles.CAS_ANALYST,
+        RlsRoles.CAS_DIRECTOR,
+        RlsRoles.CAS_VIEW_ONLY,
+    }
+
+    policies = []
+    for role, operations in role_grants_mapping.items():
+        for operation in operations:
+            policy_kwargs = {
+                "role": role,
+                "policy_name": f"{table.value.lower()}_{role.name.lower()}_{operation.name.lower()}",
+                "operation": operation,
+                "table": table,
+                "schema": schema,
+            }
+
+            if operation in [RlsOperations.SELECT, RlsOperations.UPDATE]:
+                policy_kwargs["using_statement"] = "TRUE" if role in cas_roles else using_statement
+
+            if operation in [RlsOperations.INSERT, RlsOperations.UPDATE]:
+                policy_kwargs["check_statement"] = "TRUE" if role in cas_roles else check_statement
+
+            policies.append(RlsPolicy(**policy_kwargs))
+
+    return policies
 
 
 def generate_m2m_rls(
