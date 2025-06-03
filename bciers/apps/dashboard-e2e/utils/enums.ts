3e2854895b5c2300601b6f2221674e15b8eee9aa
fviduya <francesca.viduya@gov.bc.ca>
2025-06-03 11:43:36 -0600

    chore: dashboard e2e test

---
+++ bciers/apps/dashboard-e2e/utils/enums.ts
@@ -0,0 +1,57 @@
+export enum DashboardTiles {
+  ADMINISTRATION = "Administration",
+  REGISTRATION = "Registration",
+  REPORTING = "Reporting",
+  COMPLIANCE = "Compliance",
+  REPORT_A_PROBLEM = "Report a Problem",
+  ACCESS_REQUEST = "Clean Growth User Access Management",
+  TRANSFERS = "Transfers",
+}
+
+export enum AdministrationTileText {
+  MY_OPERATOR = "My Operator",
+  OPERATIONS = "Operations",
+  TRANSFER = "Report transfer of operation or facility",
+  CONTACTS = "Contacts",
+  ACCESS_REQUEST = "Users and Access Requests",
+}
+
+export enum RegistrationTileText {
+  REGISTER = "Register an Operation",
+}
+
+export enum ReportingTileText {
+  SUBMIT_REPORT = "Submit Annual Report(s)",
+}
+
+export enum ComplianceTileText {
+  MY_COMPLIANCE = "My Compliance",
+  BCCR = "B.C. Carbon Registry",
+}
+
+export enum AccessRequestText {
+  ACCESS_REQUEST = "Access Requests",
+}
+
+export enum InternalAdministrationTileText {
+  OPERATORS = "Operators",
+  OPERATIONS = "Operations",
+  OPERATOR_ADMIN_ACCESS_REQUEST = "Operator Administrators and Access Requests",
+  CONTACTS = "Contacts",
+}
+
+export enum InternalReportingTileText {
+  ANNUAL_REPORTS = "Annual Reports",
+  PREVIOUS_REPORTS = "Previous Years Reports",
+  REPORT_ATTACHMENTS = "Report Attachments",
+}
+
+export enum InternalComplianceTileText {
+  COMPLIANCE_SUMMARIES = "Compliance Summaries",
+  PAYMENT_SUMMARIES = "Payment Summaries",
+}
+
+export enum InternalTransfersTileText {
+  TRANSFER = "Transfers",
+  TRANSFER_OPERATION_OR_FACILITY = "Transfer an operation or facility",
+}
