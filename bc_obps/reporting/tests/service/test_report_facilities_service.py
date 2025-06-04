0449621e0932cc2e024099751945a7b4d577ab3f
acatchpole <adamantiumite@gmail.com>
2025-06-04 14:17:39 -0700

    test: ensure activities are attached to facility reports

--- bc_obps/reporting/tests/service/test_report_facilities_service.py
+++ bc_obps/reporting/tests/service/test_report_facilities_service.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from reporting.models import FacilityReport
+from reporting.models.report_operation import ReportOperation
 from reporting.tests.utils.bakers import report_version_baker
 from reporting.service.report_facilities_service import ReportFacilitiesService
 from model_bakery import baker
@@ -42,9 +43,40 @@ class TestReportFacilitiesService(TestCase):
         ReportFacilitiesService.save_selected_facilities(self.report_version.id, facility_uuids)
 
         saved_facilities = FacilityReport.objects.filter(report_version=self.report_version)
+        # Assert that the facilities were saved correctly
         self.assertEqual(saved_facilities.count(), 2)
         self.assertTrue(saved_facilities.filter(facility_id=facility1.id).exists())
         self.assertTrue(saved_facilities.filter(facility_id=facility2.id).exists())
 
         ReportFacilitiesService.save_selected_facilities(self.report_version.id, [facility1.id])
+        # Assert that only facility1 remains after saving again
         self.assertEqual(FacilityReport.objects.filter(report_version=self.report_version).count(), 1)
+
+    def test_saved_selected_facilities_have_activities(self):
+        # Arrange: add activities to the report operation
+        self.report_version.report_operation.activities.set(
+            baker.make_recipe(
+                'reporting.tests.utils.activity',
+                _quantity=3,
+            )
+        )
+        facility = baker.make_recipe(
+            'registration.tests.utils.facility',
+            operation=self.operation,
+        )
+
+        facility_uuids = [facility.id]
+        # Act: save the selected facility
+        ReportFacilitiesService.save_selected_facilities(self.report_version.id, facility_uuids)
+
+        saved_facilities = FacilityReport.objects.filter(report_version=self.report_version)
+
+        ro = ReportOperation.objects.get(report_version=self.report_version)
+        expected_activities = ro.activities.all()
+
+        # Assert that the activities from the report operation were attached to the facility reports
+        for facility in saved_facilities:
+            self.assertSetEqual(
+                set(facility.activities.values_list("id", flat=True)),
+                set(expected_activities.values_list("id", flat=True)),
+            )
