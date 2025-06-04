1bf9452629157deaf3937b888b3518a343475eea
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-06-04 15:39:18 -0700

    chore: add payments service for summary dashboard

--- bc_obps/compliance/service/compliance_dashboard_service.py
+++ bc_obps/compliance/service/compliance_dashboard_service.py
@@ -1,14 +1,21 @@
 from uuid import UUID
+from compliance.models.elicensing_link import ELicensingLink
+from compliance.service.elicensing.elicensing_api_client import ELicensingAPIClient
 from compliance.service.compliance_report_version_service import ComplianceReportVersionService
 from django.db.models import QuerySet
 from compliance.models.compliance_report_version import ComplianceReportVersion
 from service.data_access_service.user_service import UserDataAccessService
 from service.data_access_service.operation_service import OperationDataAccessService
 from registration.models.operation import Operation
-from typing import Optional, List
+from typing import Optional, List, cast, Any, Dict
 from compliance.service.elicensing.obligation_elicensing_service import ObligationELicensingService
+from compliance.service.elicensing.elicensing_link_service import ELicensingLinkService
+from registration.models import Operator
+from compliance.models import ComplianceObligation
 from dataclasses import dataclass
 from decimal import Decimal
+from django.core.exceptions import ValidationError
+from django.contrib.contenttypes.models import ContentType
 
 
 @dataclass
@@ -24,6 +31,22 @@ class Payment:
 @dataclass
 class PaymentsList:
     rows: List[Payment]
+    row_count: int
+
+
+@dataclass
+class PaymentDashboardRow:
+    compliance_period: int
+    operation_name: str
+    payment_towards: str
+    invoice_number: str
+    payment_amount: Decimal
+    outstanding_balance: Decimal
+
+
+@dataclass
+class PaymentsDashboardList:
+    rows: List[PaymentDashboardRow]
     row_count: int
 
 
@@ -128,3 +151,49 @@ class ComplianceDashboardService:
         ]
 
         return PaymentsList(rows=payment_objects, row_count=len(payment_objects))
+
+    @classmethod
+    def get_payments_for_dashboard(cls, operator_id: UUID) -> PaymentsDashboardList:
+        """
+        Get payments for the payments summaries dashboard
+
+        Args:
+            operator_id: The ID of the operator for which payments are being requested
+
+        Returns:
+            PaymentsList object containing the payment records
+        """
+
+        elicensing_api_client = ELicensingAPIClient()
+
+        client_link = ELicensingLinkService.get_link_for_model(
+            model_class=Operator, object_id=operator_id, elicensing_object_kind=ELicensingLink.ObjectKind.CLIENT
+        )
+        if client_link and client_link.elicensing_object_id:
+            client_id = client_link.elicensing_object_id
+            fee_response = elicensing_api_client.query_fees(client_object_id=client_id)
+            payments = []
+            for fee in fee_response['fees']:
+                invoice = elicensing_api_client.query_invoice(client_id=client_id, invoice_number=fee['invoiceNumber'])
+                print("INVOICE: ", invoice)
+                linked_fee = ELicensingLink.objects.filter(elicensing_object_id=fee['feeObjectId']).first()
+                if linked_fee:
+                    if linked_fee.content_type == ContentType.objects.get_for_model(ComplianceObligation):
+                        compliance_report = ComplianceObligation.objects.get(
+                            id=int(linked_fee.object_id)
+                        ).compliance_report_version.compliance_report
+                        payment_towards = 'Compliance obligation'
+                        for payment in fee['payments']:
+                            payment_row = PaymentDashboardRow(
+                                compliance_period=compliance_report.report.reporting_year.reporting_year,
+                                operation_name=compliance_report.report.operation.name,
+                                payment_towards=payment_towards,
+                                invoice_number=fee['invoiceNumber'],
+                                payment_amount=payment['amount'],
+                                outstanding_balance=cast(Dict[str, Any], invoice)['invoiceOutstandingBalance'],
+                            )
+                            payments.append(payment_row)
+        else:
+            raise ValidationError("No link for client found")
+
+        return PaymentsDashboardList(rows=payments, row_count=len(payments))
