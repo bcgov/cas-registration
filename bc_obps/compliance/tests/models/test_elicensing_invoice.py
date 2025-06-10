e10bbb69291e601b6e28858fcf25efa4619fd9a3
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-06-10 16:39:10 -0700

    test: add line item model test

--- bc_obps/compliance/tests/models/test_elicensing_invoice.py
+++ bc_obps/compliance/tests/models/test_elicensing_invoice.py
@@ -1,19 +1,12 @@
 from common.tests.utils.helpers import BaseTestCase
 from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
 from model_bakery.baker import make_recipe
-from decimal import Decimal
 
 
 class ElicensingInvoiceTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_object = make_recipe(
-            'compliance.tests.utils.elicensing_invoice',
-            due_date='2024-11-30',
-            outstanding_balance=Decimal('100.01'),
-            invoice_fee_balance=Decimal('100.01'),
-            invoice_interest_balance=Decimal('0.00'),
-        )
+        cls.test_object = make_recipe('compliance.tests.utils.elicensing_invoice')
         cls.field_data = [
             *TIMESTAMP_COMMON_FIELDS,
             ("id", "ID", None, None),
