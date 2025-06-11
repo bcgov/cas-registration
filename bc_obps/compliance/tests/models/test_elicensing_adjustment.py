fb4a9ae49b6cbc796de906edbd76bbde3b10317b
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-06-11 09:40:59 -0700

    test: elicensing_adjustment model test

--- bc_obps/compliance/tests/models/test_elicensing_adjustment.py
+++ bc_obps/compliance/tests/models/test_elicensing_adjustment.py
@@ -3,18 +3,21 @@ from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
 from model_bakery.baker import make_recipe
 
 
-class ElicensingPaymentTest(BaseTestCase):
+class ElicensingAdjustmentTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.test_object = make_recipe(
-            'compliance.tests.utils.elicensing_payment',
+            'compliance.tests.utils.elicensing_adjustment',
         )
         cls.field_data = [
             *TIMESTAMP_COMMON_FIELDS,
             ("id", "ID", None, None),
             ("elicensing_line_item", "elicensing line item", None, None),
-            ("payment_object_id", "payment object id", None, None),
-            ("payment_guid", "payment guid", None, None),
+            ("adjustment_object_id", "adjustment object id", None, None),
+            ("adjustment_guid", "adjustment guid", None, None),
             ("amount", "amount", None, None),
-            ("received_date", "received date", None, None),
+            ("adjustment_date", "adjustment date", None, None),
+            ("reason", "reason", None, None),
+            ("type", "type", None, None),
+            ("comment", "comment", None, None),
         ]
