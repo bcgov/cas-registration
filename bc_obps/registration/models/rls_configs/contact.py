ae727fcc64642cb0d8d6641ecf33c0fa72c3b8b2
Brianna Cerkiewicz <brianna@button.is>
2025-06-11 09:22:30 -0700

    chore: add RLS to Contact model

--- bc_obps/registration/models/rls_configs/contact.py
+++ bc_obps/registration/models/rls_configs/contact.py
@@ -4,6 +4,9 @@ from rls.utils.helpers import generate_rls_grants
 
 
 class Rls:
+    enable_rls = True
+    schema = "erc"
+    table = RegistrationTableNames.CONTACT
     role_grants_mapping = {
         RlsRoles.INDUSTRY_USER: [RlsOperations.SELECT, RlsOperations.INSERT, RlsOperations.UPDATE],
         # CAS_DIRECTOR can create new contacts when approving a user_operator
@@ -14,3 +17,23 @@ class Rls:
         RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
     }
     grants = generate_rls_grants(role_grants_mapping, RegistrationTableNames.CONTACT)
+    policies = generate_rls_policies(
+        role_grants_mapping=role_grants_mapping,
+        table=RegistrationTableNames.CONTACT,
+        using_statement="""
+                    operator_id in (
+        select uo.operator_id
+        from erc.user_operator uo
+        where uo.user_id = current_setting('my.guid', true)::uuid
+          and uo.status = 'approved'
+    )
+                    """,
+        check_statement="""
+                    operator_id in (
+        select uo.operator_id
+        from erc.user_operator uo
+        where uo.user_id = current_setting('my.guid', true)::uuid
+          and uo.status = 'approved'
+    )
+                    """,
+    )
