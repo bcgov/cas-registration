9edf7d3dd9b48ac7026a9dbb1335d6fae41cc54d
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-06-12 12:06:39 -0700

    chore: remove manager from model

--- bc_obps/compliance/models/elicensing_client_operator.py
+++ bc_obps/compliance/models/elicensing_client_operator.py
@@ -1,20 +1,8 @@
+from __future__ import annotations
 from django.db import models
 from registration.models.time_stamped_model import TimeStampedModel
 from registration.models import Operator
 from .rls_configs.elicensing_client_operator import Rls as ElicensingClientOperatorRls
-from django.core.exceptions import MultipleObjectsReturned
-
-
-class GetOneOrNoneManager(models.Manager):
-    """Return one record, None if no record exists, raise if multiple records returned"""
-
-    def get_one_or_none(self, *args, **kwargs):  # type: ignore[no-untyped-def]
-        try:
-            return self.get(*args, **kwargs)
-        except (self.model.DoesNotExist):  # type: ignore[attr-defined]
-            return None
-        except (self.model.MultipleObjectsReturned):  # type: ignore[attr-defined]
-            raise MultipleObjectsReturned
 
 
 class ElicensingClientOperator(TimeStampedModel):
@@ -22,8 +10,6 @@ class ElicensingClientOperator(TimeStampedModel):
     Model the relationship between an Operator record in BCIERS and the related client in elicensing.
 
     """
-
-    managed_objects = GetOneOrNoneManager()
 
     client_object_id = models.IntegerField(
         db_comment="The clientObjectId identifier from elicensing for the related client"
