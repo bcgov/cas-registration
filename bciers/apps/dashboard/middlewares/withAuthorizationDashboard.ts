0bd6f700bae6495c0592c45e7b763ff56ca10a34
Andrea Williams <andrea.williams@gov.bc.ca>
2025-06-12 08:54:07 -0700

    chore: refactored withAuthorizationDashboard for less nested if-else statements

--- bciers/apps/dashboard/middlewares/withAuthorizationDashboard.ts
+++ bciers/apps/dashboard/middlewares/withAuthorizationDashboard.ts
@@ -8,6 +8,7 @@ import {
 import { MiddlewareFactory } from "@bciers/middlewares";
 import { getToken } from "@bciers/actions";
 import isInAllowedPath from "@bciers/utils/src/isInAllowedList";
+import { FrontEndRoles } from "@bciers/utils/src/enums";
 /*
 Access control logic is managed using Next.js middleware and NextAuth.js authentication JWT session.
 The middleware intercepts requests, and for restricted areas...
@@ -22,6 +23,7 @@ const paths = {
   dashboard: "dashboard",
   profile: "profile",
   declined: "declined",
+  administration: "administration",
 };
 export const authAllowedPaths = [paths.dashboard, paths.profile];
 const unauthAllowedPaths = [paths.auth, paths.unauth, paths.declined];
@@ -32,25 +34,8 @@ export const withAuthorizationDashboard: MiddlewareFactory = (
 ) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
     const { pathname } = request.nextUrl;
-    // Check if the path is in the unauthenticated allow list
-    if (isInAllowedPath(pathname, unauthAllowedPaths)) {
-      // 🛸 Route to next middleware
-      return next(request, _next);
-    }
 
-    // Check if the user is authenticated via the jwt encoded in server side cookie
-    const token = await getToken();
-    if (token) {
-      if (pathname === "/" || pathname === `/${paths.onboarding}`) {
-        // 🛸 Redirect to dashboard
-        return NextResponse.redirect(
-          new URL(`/${paths.dashboard}`, request.url),
-        );
-      } else {
-        // 🛸 Route to next middleware
-        return next(request, _next);
-      }
-    } else {
+    const routeWithNoToken = () => {
       if (pathname.endsWith(`/${paths.onboarding}`)) {
         // 🛸 Route to next middleware
         return next(request, _next);
@@ -60,6 +45,64 @@ export const withAuthorizationDashboard: MiddlewareFactory = (
           new URL(`/${paths.onboarding}`, request.url),
         );
       }
+    };
+
+    const routeWithNoAppRole = () => {
+      if (pathname.endsWith(`/${paths.profile}`)) {
+        // route to next middleware
+        return next(request, _next);
+      } else {
+        return NextResponse.redirect(
+          new URL(`/${paths.administration}/${paths.profile}`, request.url),
+        );
+      }
+    };
+
+    const routeForRoleCasPending = () => {
+      if (isInAllowedPath(pathname, authAllowedPaths)) {
+        // 🛸 Route to next middleware
+        return next(request, _next);
+      } else {
+        // 🛸 Redirect to dashboard
+        return NextResponse.redirect(
+          new URL(`/${paths.dashboard}`, request.url),
+        );
+      }
+    };
+
+    const routeForOtherAppRoles = () => {
+      if (pathname === "/" || pathname === `/${paths.onboarding}`) {
+        // 🛸 Redirect to dashboard
+        return NextResponse.redirect(
+          new URL(`/${paths.dashboard}`, request.url),
+        );
+      } else {
+        // 🛸 Route to next middleware
+        return next(request, _next);
+      }
+    };
+
+    // Check if the path is in the unauthenticated allow list
+    if (isInAllowedPath(pathname, unauthAllowedPaths)) {
+      // 🛸 Route to next middleware
+      return next(request, _next);
+    }
+
+    // Check if the user is authenticated via the jwt encoded in server side cookie
+    const token = await getToken();
+    if (token) {
+      // Handle user without token.user.app_role
+      if (!token.app_role || token.app_role === "") {
+        return routeWithNoAppRole();
+      }
+      // Handle user with token.user.app_role = cas_pending
+      if (token.app_role === FrontEndRoles.CAS_PENDING) {
+        return routeForRoleCasPending();
+      }
+      // else Handle user with token.user.app_role that isn't cas_pending
+      return routeForOtherAppRoles();
+    } else {
+      return routeWithNoToken();
     }
   };
 };
