7c5e79513380f49c74cb355fd6d8778090fd888f
SeSo <sobhani.sepehr@gmail.com>
2025-06-13 09:01:03 -0700

    chore: update RequestIssuanceOfEarnedCreditsComponent for account ID validation and rename fields for consistency
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/compliance/src/app/data/jsonSchema/requestIssuance/requestIssuanceOfEarnedCreditsSchema.tsx
+++ bciers/apps/compliance/src/app/data/jsonSchema/requestIssuance/requestIssuanceOfEarnedCreditsSchema.tsx
@@ -4,7 +4,7 @@ import { Link } from "@mui/material";
 import { bcCarbonRegistryLink } from "@bciers/utils/src/urls";
 import HiddenFieldTemplate from "@bciers/components/form/fields/HiddenFieldTemplate";
 import { ReadOnlyWidget } from "@bciers/components/form/widgets/readOnly";
-import BCCRHoldingAccountWidget from "@/compliance/src/app/widgets/BccrHoldingAcountWidget";
+import BccrHoldingAccountWidget from "@/compliance/src/app/widgets/BccrHoldingAcountWidget";
 import {
   headerUiConfig,
   readOnlyObjectField,
@@ -16,47 +16,29 @@ export const requestIssuanceOfEarnedCreditsSchema: RJSFSchema = {
   title: "Request Issuance of Earned Credits",
   required: ["bccrHoldingAccountId"],
   properties: {
-    bccrAccountHeader: readOnlyObjectField(
+    bccr_account_header: readOnlyObjectField(
       "B.C. Carbon Registry (BCCR) Account Information",
     ),
-    bccrHoldingAccountId: {
+    bccr_holding_account_id: {
       type: "string",
       title: "BCCR Holding Account ID:",
-      pattern: "^\\d{15}$",
-      maxLength: 15,
-      minLength: 15,
     },
-    bccrTradingName: readOnlyStringField("BCCR Trading Name:"),
+    bccr_trading_name: readOnlyStringField("BCCR Trading Name:"),
   },
 };
 
 export const requestIssuanceOfEarnedCreditsUiSchema: UiSchema = {
   "ui:FieldTemplate": FieldTemplate,
   "ui:classNames": "form-heading-label",
-  bccrAccountHeader: headerUiConfig,
-  bccrHoldingAccountId: {
-    "ui:widget": BCCRHoldingAccountWidget,
+  bccr_account_header: headerUiConfig,
+  bccr_holding_account_id: {
+    "ui:widget": "BccrHoldingAccountWidget",
     "ui:classNames": "[&>div:first-child]:w-1/3", // modify the width of the label
-    "ui:help": (
-      <small>
-        No account?{" "}
-        <Link
-          href={bcCarbonRegistryLink}
-          underline="hover"
-          className="text-bc-link-blue font-medium"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Create account
-        </Link>{" "}
-        in BCCR.
-      </small>
-    ),
     "ui:options": {
       inline: true,
     },
   },
-  bccrTradingName: {
+  bccr_trading_name: {
     "ui:FieldTemplate": HiddenFieldTemplate,
     "ui:widget": ReadOnlyWidget,
   },
