ca4f01d03055a81d85f38c55819ab97cbf28b6f2
acatchpole <adamantiumite@gmail.com>
2025-06-16 13:17:46 -0700

    wip

--- bc_obps/reporting/models/rls_configs/report_emission.py
+++ bc_obps/reporting/models/rls_configs/report_emission.py
@@ -1,9 +1,18 @@
 from reporting.enums.enums import ReportingTableNames
 from rls.enums import RlsRoles, RlsOperations
-from rls.utils.helpers import generate_rls_grants, generate_m2m_rls
-
+from rls.utils.helpers import (
+    generate_report_policy_mapping_from_grants,
+    generate_rls_grants,
+    generate_m2m_rls,
+    generate_rls_policies,
+)
+from rls.utils.m2m import M2MPolicyStatements
+from rls.utils.policy import RlsPolicy
 
 class Rls:
+    enable_rls = True
+    schema = 'erc'
+    table = ReportingTableNames.REPORT_EMISSION
     role_grants_mapping = {
         RlsRoles.INDUSTRY_USER: [
             RlsOperations.SELECT,
@@ -16,8 +25,26 @@ class Rls:
         RlsRoles.CAS_ANALYST: [RlsOperations.SELECT],
         RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
     }
-    grants = generate_rls_grants(role_grants_mapping, ReportingTableNames.REPORT_EMISSION)
+    grants = generate_rls_grants(role_grants_mapping, table)
     # M2M relationships
+    report_emission_emission_categories_using_statement = """
+            reportemission_id IN (
+                SELECT re.id
+                FROM erc.report_emission re
+                JOIN erc.report_version rv ON re.report_version_id = rv.id
+                JOIN erc.report r ON rv.report_id = r.id
+                WHERE r.operator_id IN (
+                    SELECT uo.operator_id
+                    FROM erc.user_operator uo
+                    WHERE uo.user_id = current_setting('my.guid', true)::uuid
+                    AND uo.status = 'Approved'
+                )
+            )
+        """
+    report_emission_emission_categories_delete_using_statement = RlsPolicy.add_draft_check_to_report_using_statement(
+        report_emission_emission_categories_using_statement
+    )
+
     m2m_models_grants_mapping = {
         ReportingTableNames.REPORT_EMISSION_EMISSION_CATEGORIES: {
             RlsRoles.INDUSTRY_USER: [
@@ -32,4 +59,22 @@ class Rls:
             RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
         }
     }
-    m2m_rls_list = generate_m2m_rls(m2m_models_grants_mapping)
+    m2m_models_policy_mapping = {
+        ReportingTableNames.REPORT_EMISSION_EMISSION_CATEGORIES: M2MPolicyStatements(
+            using_statement=report_emission_emission_categories_using_statement,
+            delete_using_statement=report_emission_emission_categories_delete_using_statement,
+        )
+    }
+    m2m_rls_list = generate_m2m_rls(
+        m2m_models_grants_mapping,
+        m2m_models_policy_mapping,
+        enable_rls,
+    )
+
+    role_policy_mapping = generate_report_policy_mapping_from_grants(
+        role_grants_mapping,
+        RlsPolicy.REPORT_USING_STATEMENT,
+        RlsPolicy.REPORT_DELETE_USING_STATEMENT,
+    )
+
+    policies = generate_rls_policies(role_policy_mapping, table)
--- bc_obps/reporting/tests/models/test_report.py
+++ bc_obps/reporting/tests/models/test_report.py
@@ -1,7 +1,11 @@
 from common.tests.utils.helpers import BaseTestCase
+from django.db import ProgrammingError
+import pytest
 from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
-from reporting.tests.utils.bakers import report_baker
-
+from reporting.models.report import Report
+from reporting.tests.utils.bakers import report_baker, reporting_year_baker
+from model_bakery.baker import make_recipe
+from rls.tests.helpers import test_policies_for_cas_roles, test_policies_for_industry_user
 
 class ReportTest(BaseTestCase):
     @classmethod
@@ -16,3 +20,90 @@ class ReportTest(BaseTestCase):
             ("report_versions", "report version", None, 0),
             ("compliance_report", "compliance report", None, None),
         ]
+
+class ReportRlsTest(BaseTestCase):
+
+    def test_report_rls_industry_user(self):
+        approved_user_operator = make_recipe('registration.tests.utils.approved_user_operator')
+        operation = make_recipe('registration.tests.utils.operation', operator=approved_user_operator.operator)
+        reporting_year = reporting_year_baker(reporting_year=2012)
+        report = report_baker( operation=operation, operator=approved_user_operator.operator, reporting_year=reporting_year)
+        number_of_accesible_records = Report.objects.filter(operation=operation).count()
+
+        random_operation = make_recipe('registration.tests.utils.operation')
+        random_report = report_baker(operation=random_operation, reporting_year=reporting_year)
+        number_of_total_records = Report.objects.count()
+
+        new_operation = make_recipe(
+                'registration.tests.utils.operation', operator=approved_user_operator.operator
+            )
+        new_reporting_year = reporting_year_baker(reporting_year=2042)
+
+        def select_function(cursor):
+            # Select the report for the approved user operator and not the random report
+            assert Report.objects.count() < number_of_total_records
+            assert Report.objects.count() == number_of_accesible_records
+            assert Report.objects.filter(operation=random_operation).count() == 0
+        def insert_function(cursor):
+            # Insert a new report for the approved user operator
+            new_report = Report.objects.create(
+                operation=new_operation,
+                operator=approved_user_operator.operator,
+                reporting_year=report.reporting_year,
+            )
+            number_of_accesible_records_after_insert = number_of_accesible_records + 1
+            assert Report.objects.count() == number_of_accesible_records_after_insert
+            assert new_report.id is not None
+            # Attempt to insert a report that the user is not an operator for
+            with pytest.raises(
+                ProgrammingError, match='new row violates row-level security policy for table "report'
+            ):
+                Report.objects.create(
+                    operation=random_operation,
+                    operator=random_report.operator,
+                    reporting_year=random_report.reporting_year,
+                )
+
+        def update_function(cursor):
+            # Update the report for the approved user operator
+            report.reporting_year = new_reporting_year
+            report.save()
+            assert Report.objects.get(id=report.id).reporting_year.reporting_year == new_reporting_year.reporting_year
+            # Attempt to update a report that the user should not have access to
+            with pytest.raises(
+                ProgrammingError, match='new row violates row-level security policy for table "report'
+            ):
+                random_report.reporting_year = new_reporting_year
+                random_report.save()
+
+        def delete_function(cursor):
+            # Delete the report for the approved user operator
+            number_of_deleted_reports, _ = report.delete()
+            number_of_accesible_records_after_delete = number_of_accesible_records - number_of_deleted_reports
+            assert number_of_deleted_reports > 0
+            assert Report.objects.count() == number_of_accesible_records_after_delete
+
+            # Attempt to delete a report that the user should not have access to
+            number_of_deleted_reports, _ = random_report.delete()
+            assert number_of_deleted_reports == 0
+            assert Report.objects.count() == number_of_accesible_records_after_delete
+
+        test_policies_for_industry_user(
+            Report,
+            approved_user_operator.user,
+            select_function,
+            insert_function,
+            update_function,
+            delete_function,
+        )
+
+    def test_report_rls_cas_user(self):
+        report_baker(_quantity=5)
+
+        def select_function(cursor, i):
+            assert Report.objects.count() == 5
+
+        test_policies_for_cas_roles(
+            Report,
+            select_function=select_function,
+        )
