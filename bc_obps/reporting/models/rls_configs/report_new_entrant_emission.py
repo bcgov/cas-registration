1f34f463fbf86b4b6962b75bfe03b9625982bac1
acatchpole <adamantiumite@gmail.com>
2025-06-16 13:17:46 -0700

    remove report_verification_visit in rebase

--- bc_obps/reporting/models/rls_configs/report_new_entrant_emission.py
+++ bc_obps/reporting/models/rls_configs/report_new_entrant_emission.py
@@ -1,9 +1,13 @@
 from reporting.enums.enums import ReportingTableNames
 from rls.enums import RlsRoles, RlsOperations
-from rls.utils.helpers import generate_rls_grants
+from rls.utils.helpers import generate_report_policy_mapping_from_grants, generate_rls_grants, generate_rls_policies
+from rls.utils.policy import RlsPolicy
 
 
 class Rls:
+    enable_rls = True
+    schema = 'erc'
+    table = ReportingTableNames.REPORT_NEW_ENTRANT_EMISSION
     role_grants_mapping = {
         RlsRoles.INDUSTRY_USER: [
             RlsOperations.SELECT,
@@ -16,4 +20,23 @@ class Rls:
         RlsRoles.CAS_ANALYST: [RlsOperations.SELECT],
         RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
     }
-    grants = generate_rls_grants(role_grants_mapping, ReportingTableNames.REPORT_NEW_ENTRANT_EMISSION)
+    using_statement = """
+        report_new_entrant_id IN (
+            SELECT rne.id
+            FROM erc.report_new_entrant rne
+            JOIN erc.report_version rv ON rne.report_version_id = rv.id
+            JOIN erc.report r ON rv.report_id = r.id
+            WHERE r.operator_id IN (
+                SELECT uo.operator_id
+                FROM erc.user_operator uo
+                WHERE uo.user_id = current_setting('my.guid', true)::uuid
+                AND uo.status = 'Approved'
+            )
+        )
+        """
+    delete_using_statement = RlsPolicy.add_draft_check_to_report_using_statement(using_statement)
+    grants = generate_rls_grants(role_grants_mapping, table)
+    role_policy_mapping = generate_report_policy_mapping_from_grants(
+        role_grants_mapping, using_statement, delete_using_statement    
+    )
+    policies = generate_rls_policies(role_policy_mapping, table)
--- bc_obps/reporting/models/rls_configs/report_version.py
+++ bc_obps/reporting/models/rls_configs/report_version.py
@@ -1,9 +1,15 @@
+import re
+from common.enums import Schemas
+from rls.utils.policy import RlsPolicy
 from reporting.enums.enums import ReportingTableNames
 from rls.enums import RlsRoles, RlsOperations
-from rls.utils.helpers import generate_rls_grants
+from rls.utils.helpers import generate_report_policy_mapping_from_grants, generate_rls_grants, generate_rls_policies
 
 
 class Rls:
+    enable_rls = True
+    schema = 'erc'
+    table = ReportingTableNames.REPORT_VERSION
     role_grants_mapping = {
         RlsRoles.INDUSTRY_USER: [
             RlsOperations.SELECT,
@@ -16,4 +22,29 @@ class Rls:
         RlsRoles.CAS_ANALYST: [RlsOperations.SELECT],
         RlsRoles.CAS_VIEW_ONLY: [RlsOperations.SELECT],
     }
-    grants = generate_rls_grants(role_grants_mapping, ReportingTableNames.REPORT_VERSION)
+    grants = generate_rls_grants(role_grants_mapping, table)
+
+    using_statement = """
+        report_id IN (
+            SELECT r.id
+            FROM erc.report r
+            WHERE r.operator_id IN (
+                SELECT uo.operator_id
+                FROM erc.user_operator uo
+                WHERE uo.user_id = current_setting('my.guid', true)::uuid
+                AND uo.status = 'Approved'
+            )
+        )"""
+    delete_using_statement = re.sub(
+        r"(AND uo\.status = 'Approved'\s*\)\s*\))",
+        r"\1 AND status = 'Draft'",
+        using_statement,
+        count=1,
+    )
+    role_policy_mapping = generate_report_policy_mapping_from_grants(
+        role_grants_mapping=role_grants_mapping,
+        using_statement=using_statement,
+        delete_using_statement=delete_using_statement,
+    )
+
+    policies = generate_rls_policies(role_policy_mapping, table)
