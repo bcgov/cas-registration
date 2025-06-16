2e3fc1b8873d00880994df79efc0f133c5ebd3ac
Brianna Cerkiewicz <brianna@button.is>
2025-06-16 08:18:08 -0700

    chore: make operation readonly when navigating from grid

--- bciers/libs/components/src/datagrid/cells/operations/OperationsActionCell.tsx
+++ bciers/libs/components/src/datagrid/cells/operations/OperationsActionCell.tsx
@@ -13,7 +13,7 @@ const OperationsActionCell = (isInternalUser: boolean) => {
           break;
         case OperationStatus.DRAFT:
           actionText = "Continue Registration";
-          url = `/registration/register-an-operation/${params.row.operation__id}/1`;
+          url = `/registration/register-an-operation/${params.row.operation__id}/1?continue=true`;
           break;
       }
     }
