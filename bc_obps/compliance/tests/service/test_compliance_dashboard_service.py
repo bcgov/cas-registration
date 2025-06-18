bd3b545835a0aec81e6b1fc3eb55d715fa6b4ec5
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-06-18 16:24:45 -0700

    test: add test for get payments service

--- bc_obps/compliance/tests/service/test_compliance_dashboard_service.py
+++ bc_obps/compliance/tests/service/test_compliance_dashboard_service.py
@@ -1,12 +1,33 @@
 from django.test import TestCase
-from decimal import Decimal
-from dataclasses import dataclass
-
-
-@dataclass
-class TestInvoiceQueryResponse:
-    invoiceOutstandingBalance: Decimal
+from unittest.mock import patch
+import pytest
+from model_bakery.baker import make_recipe
+from compliance.service.elicensing.elicensing_data_refresh_service import RefreshWrapperReturn
+from compliance.service.compliance_dashboard_service import ComplianceDashboardService
 
 
 class TestComplianceDashboardService(TestCase):
     """Tests for the ComplianceDashboardService class"""
+
+    @pytest.mark.django_db
+    @patch(
+        'compliance.service.elicensing.elicensing_data_refresh_service.ElicensingDataRefreshService.refresh_data_wrapper_by_compliance_report_version_id'
+    )
+    def test_get_compliance_obligation_payments_by_compliance_report_version_id(self, mock_refresh):
+        payment_1 = make_recipe('compliance.tests.utils.elicensing_payment')
+        payment_2 = make_recipe(
+            'compliance.tests.utils.elicensing_payment', elicensing_line_item=payment_1.elicensing_line_item
+        )
+        obligation = make_recipe(
+            'compliance.tests.utils.compliance_obligation',
+            elicensing_invoice=payment_1.elicensing_line_item.elicensing_invoice,
+        )
+        mock_refresh.return_value = RefreshWrapperReturn(
+            data_is_fresh=True, invoice=payment_1.elicensing_line_item.elicensing_invoice
+        )
+        returned_data = ComplianceDashboardService.get_compliance_obligation_payments_by_compliance_report_version_id(
+            compliance_report_version_id=obligation.compliance_report_version_id
+        )
+        assert returned_data.data_is_fresh == True  # noqa: E712
+        assert returned_data.data.first() == payment_1
+        assert returned_data.data.last() == payment_2
