98a49d84ae44308ea0e16385ae6c59eec46ba440
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-06-20 15:55:04 -0700

    chore: migrations after rebase

--- bc_obps/reporting/migrations/0116_reportversion_test_field.py
+++ bc_obps/reporting/migrations/0116_reportversion_test_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reporting', '0113_V3_7_0'),
+        ('reporting', '0115_V3_8_1'),
     ]
 
     operations = [
