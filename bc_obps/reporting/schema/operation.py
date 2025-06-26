cb3657f61f8af72fcdb34330f9bca10b9a429e58
Brianna Cerkiewicz <brianna@button.is>
2025-06-26 14:41:21 -0700

    chore: working

--- bc_obps/reporting/schema/operation.py
+++ bc_obps/reporting/schema/operation.py
@@ -28,28 +28,7 @@ class ReportingDashboardOperationFilterSchema(FilterSchema):
     bcghg_id: Optional[str] = Field(None, json_schema_extra={'q': 'bcghg_id__id__icontains'})
     operation_name: Optional[str] = Field(None, json_schema_extra={'q': 'operation_name__icontains'})
     report_status: Optional[str] = Field(None, json_schema_extra={'q': 'report_status__icontains'})
-    report_version: Optional[str] = Field(None, json_schema_extra={'q': 'report_version__icontains'})
     
-
-    # @staticmethod
-    # def filtering_including_custom_statuses(field: str, value: str) -> Q:
-    #     # re.search("c", "abcdef")
-    #     if value and re.search(value, 'not started', re.IGNORECASE):
-    #         return Q(**{f"{field}__isnull": True})
-    #     if value and re.search(value, 'draft supplementary report', re.IGNORECASE):
-    #         # breakpoint()
-    #         return Q(**{f"{field}__icontains": value}) | Q(**{f"{field}__isnull": True})
-    #     return Q(**{f"{field}__icontains": value}) if value else Q()
-
-    #     # Fallback: basic icontains match
-    #     return Q(**{f"{field}__icontains": value})
-
-    # def filter_bcghg_id(self, value: str) -> Q:
-    #     return self.filtering_including_custom_statuses('bcghg_id', value)
-    
-    # def filter_operation__name(self, value: str) -> Q:
-    #     return self.filtering_including_custom_statuses('operation__name', value)
-
     def filter_report_status(self, value: str) -> Q:
         """
         Sometimes on the front-end, we tweak the status display to give the user more information. This function allows us to filter by the front-end status Not Started (db status is null) and Draft Supplementary Report (db status is Draft and report_version_id > 1).
