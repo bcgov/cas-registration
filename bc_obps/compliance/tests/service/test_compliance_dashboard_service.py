1861ab518e3264c63912f0f99415e5322032e5bb
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-06-26 15:58:35 -0700

    test: add test for get dashboard compliance report versions

--- bc_obps/compliance/tests/service/test_compliance_dashboard_service.py
+++ bc_obps/compliance/tests/service/test_compliance_dashboard_service.py
@@ -4,6 +4,8 @@ import pytest
 from model_bakery.baker import make_recipe
 from compliance.service.elicensing.elicensing_data_refresh_service import RefreshWrapperReturn
 from compliance.service.compliance_dashboard_service import ComplianceDashboardService
+from compliance.models import ComplianceReportVersion
+from registration.models import Operation
 
 
 class TestComplianceDashboardService(TestCase):
@@ -31,3 +33,33 @@ class TestComplianceDashboardService(TestCase):
         assert returned_data.data_is_fresh == True  # noqa: E712
         assert returned_data.data.first() == payment_1
         assert returned_data.data.last() == payment_2
+
+    @pytest.mark.django_db
+    @patch(
+        'compliance.service.compliance_report_version_service.ComplianceReportVersionService.get_compliance_report_versions_for_previously_owned_operations'
+    )
+    def test_get_compliance_report_versions_for_dashboard(self, mock_previously_owned):
+        user_operator = make_recipe('registration.tests.utils.approved_user_operator')
+
+        operation = make_recipe(
+            'registration.tests.utils.operation', operator=user_operator.operator, status=Operation.Statuses.REGISTERED
+        )
+
+        report = make_recipe('reporting.tests.utils.report', operator=user_operator.operator, operation=operation)
+        compliance_report = make_recipe('compliance.tests.utils.compliance_report', report=report)
+        compliance_report_version = make_recipe(
+            'compliance.tests.utils.compliance_report_version', compliance_report=compliance_report
+        )
+        previous_compliance_report_version = make_recipe('compliance.tests.utils.compliance_report_version')
+        mock_previously_owned.return_value = ComplianceReportVersion.objects.filter(
+            id=previous_compliance_report_version.id
+        )
+
+        result = ComplianceDashboardService.get_compliance_report_versions_for_dashboard(
+            user_guid=user_operator.user.user_guid
+        )
+
+        # Returns the union of reports for currently owned operations & reports for previously owned operations
+        assert result.count() == 2
+        assert result.first() == compliance_report_version
+        assert result.last() == previous_compliance_report_version
