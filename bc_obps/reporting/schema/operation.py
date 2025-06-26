58779c2ac5f8b080e226ed3b1e81b9884e693fcc
Brianna Cerkiewicz <brianna@button.is>
2025-06-26 14:25:26 -0700

    chore: mostly working

--- bc_obps/reporting/schema/operation.py
+++ bc_obps/reporting/schema/operation.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+import re
 from typing import Optional
 
 from ninja import FilterSchema, ModelSchema
 
+from pgtrigger import Q
 from registration.models.operation import Operation
 from ninja import Field
+from reporting.models.report_version import ReportVersion
 
 
 class ReportingDashboardOperationOut(ModelSchema):
@@ -25,3 +28,32 @@ class ReportingDashboardOperationFilterSchema(FilterSchema):
     bcghg_id: Optional[str] = Field(None, json_schema_extra={'q': 'bcghg_id__id__icontains'})
     operation_name: Optional[str] = Field(None, json_schema_extra={'q': 'operation_name__icontains'})
     report_status: Optional[str] = Field(None, json_schema_extra={'q': 'report_status__icontains'})
+    report_version: Optional[str] = Field(None, json_schema_extra={'q': 'report_version__icontains'})
+    
+
+    # @staticmethod
+    # def filtering_including_custom_statuses(field: str, value: str) -> Q:
+    #     # re.search("c", "abcdef")
+    #     if value and re.search(value, 'not started', re.IGNORECASE):
+    #         return Q(**{f"{field}__isnull": True})
+    #     if value and re.search(value, 'draft supplementary report', re.IGNORECASE):
+    #         # breakpoint()
+    #         return Q(**{f"{field}__icontains": value}) | Q(**{f"{field}__isnull": True})
+    #     return Q(**{f"{field}__icontains": value}) if value else Q()
+
+    #     # Fallback: basic icontains match
+    #     return Q(**{f"{field}__icontains": value})
+
+    # def filter_bcghg_id(self, value: str) -> Q:
+    #     return self.filtering_including_custom_statuses('bcghg_id', value)
+    
+    # def filter_operation__name(self, value: str) -> Q:
+    #     return self.filtering_including_custom_statuses('operation__name', value)
+
+    def filter_report_status(self, value: str) -> Q:
+        # re.search("c", "abcdef")
+        if value and re.search(value, 'not started', re.IGNORECASE):
+            return Q(report_status__isnull=True)
+        if value and re.search(value, 'draft supplementary report', re.IGNORECASE):
+            return Q(report_status=ReportVersion.ReportVersionStatus.Draft) & Q(report_version_id__gt=1)
+        return Q(report_status__icontains=value)
