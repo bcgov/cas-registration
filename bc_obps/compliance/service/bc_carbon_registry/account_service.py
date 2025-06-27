b5f4e238354be9ee9f6abc06a219ef8aa81084af
SeSo <sobhani.sepehr@gmail.com>
2025-06-27 09:18:50 -0700

    chore: remove outdated docstring
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/service/bc_carbon_registry/account_service.py
+++ bc_obps/compliance/service/bc_carbon_registry/account_service.py
@@ -7,7 +7,6 @@ T = TypeVar('T', bound=Dict[str, Any])
 
 
 class BCCarbonRegistryAccountService:
-    """Service layer for BC Carbon Registry operations."""
 
     def __init__(self) -> None:
         self.client = BCCarbonRegistryAPIClient()
