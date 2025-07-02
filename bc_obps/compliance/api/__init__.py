60fc3991f5a62600b7e684e044c7ce91fa7047cf
SeSo <sobhani.sepehr@gmail.com>
2025-07-02 12:34:01 -0700

    feat: implement BCCR project creation endpoint
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/api/__init__.py
+++ bc_obps/compliance/api/__init__.py
@@ -12,4 +12,7 @@ from ._compliance_report_versions._compliance_report_version_id import (
 )
 from ._bccr._accounts import account_id
 from ._bccr._compliance_report_versions._compliance_report_version_id import applied_compliance_units
-from ._bccr._accounts._account_id._compliance_report_versions._compliance_report_version_id import compliance_units
+from ._bccr._accounts._account_id._compliance_report_versions._compliance_report_version_id import (
+    compliance_units,
+    projects,
+)
