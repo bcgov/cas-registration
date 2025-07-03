c7a61055b93febff0dea20183f2822634319d554
Brianna Cerkiewicz <brianna@button.is>
2025-07-03 13:23:20 -0700

    feat: add Draft Supplementary Report status on FE

--- bciers/apps/reporting/src/app/components/operations/cells/StatusCell.tsx
+++ bciers/apps/reporting/src/app/components/operations/cells/StatusCell.tsx
@@ -11,9 +11,16 @@ export default function StatusCell(params: GridRenderCellParams) {
   const colorMap = new Map<string, string>([
     [ReportOperationStatus.NOT_STARTED, BC_GOV_PRIMARY_BRAND_COLOR_BLUE],
     [ReportOperationStatus.DRAFT, BC_GOV_PRIMARY_BRAND_COLOR_BLUE],
+    [
+      ReportOperationStatus.DRAFT_SUPPLEMENTARY,
+      BC_GOV_PRIMARY_BRAND_COLOR_BLUE,
+    ],
     [ReportOperationStatus.SUBMITTED, BC_GOV_SEMANTICS_GREEN],
   ]);
-  const status = params.value || ReportOperationStatus.NOT_STARTED;
+  const status =
+    params.row.report_version_id > params.row.first_report_version_id
+      ? `${params.value} Supplementary Report`
+      : params.value || ReportOperationStatus.NOT_STARTED;
   const statusColor = colorMap.get(status) || "primary";
   return (
     <div
