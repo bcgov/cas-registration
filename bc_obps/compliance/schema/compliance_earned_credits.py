660ead054f3c01bd6c99d84d8acdde2e9caa2dff
SeSo <sobhani.sepehr@gmail.com>
2025-07-09 18:46:44 -0700

    chore: populate analyst_submitted_by using the full name
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bc_obps/compliance/schema/compliance_earned_credits.py
+++ bc_obps/compliance/schema/compliance_earned_credits.py
@@ -20,6 +20,7 @@ class ComplianceEarnedCreditsOut(ModelSchema):
     emissions_attributable_for_compliance: Optional[Decimal] = Field(None, alias=EMISSIONS_ATTRIBUTABLE_ALIAS)
     emissions_limit: Optional[Decimal] = Field(None, alias=EMISSIONS_LIMIT_ALIAS)
     excess_emissions: Optional[Decimal] = Field(None, alias=EXCESS_EMISSIONS_ALIAS)
+    analyst_submitted_by: Optional[str] = Field(None, alias="analyst_submitted_by.get_full_name")
 
     class Meta:
         model = ComplianceEarnedCredit
@@ -31,7 +32,6 @@ class ComplianceEarnedCreditsOut(ModelSchema):
             "analyst_comment",
             "director_comment",
             "analyst_submitted_date",
-            "analyst_submitted_by",
             "analyst_suggestion",
         ]
 
