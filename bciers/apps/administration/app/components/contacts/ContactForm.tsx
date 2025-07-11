d188d6c54d1273b55668ac0a827a9cde4a96bccb
Brianna Cerkiewicz <brianna@button.is>
2025-07-11 11:48:08 -0700

    feat: user can archive contacts

--- bciers/apps/administration/app/components/contacts/ContactForm.tsx
+++ bciers/apps/administration/app/components/contacts/ContactForm.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import SingleStepTaskListForm from "@bciers/components/form/SingleStepTaskListForm";
-import { actionHandler } from "@bciers/actions";
 import { ContactFormData } from "./types";
 import { FormMode } from "@bciers/utils/src/enums";
 import { contactsUiSchema } from "@/administration/app/data/jsonSchema/contact";
-import { useSessionRole } from "@bciers/utils/src/sessionUtils";
 import Link from "next/link";
+import SimpleModal from "@bciers/components/modal/SimpleModal";
+import { archiveContact } from "@bciers/actions/api";
+import { useParams, useRouter } from "next/navigation";
+import { useSessionRole } from "@bciers/utils/src/sessionUtils";
+import { actionHandler } from "@bciers/actions";
 
 interface Props {
   schema: any;
@@ -37,65 +39,109 @@ export default function ContactForm({
   const [isCreatingState, setIsCreatingState] = useState(isCreating);
   const [key, setKey] = useState(Math.random());
   const role = useSessionRole();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const params = useParams();
+
+  const handleClickDelete = () => {
+    setModalOpen(true);
+  };
+
+  const handleArchiveContact = async () => {
+    setIsSubmitting(true);
+    const response = await archiveContact(
+      params.contactId as string,
+      "/contacts",
+    );
+    if (response?.error) {
+      setError(response.error as any);
+      setModalOpen(false);
+      setIsSubmitting(false);
+      return;
+    }
+    router.push("/contacts?from_deletion=true");
+  };
+
+  const hasPlacesAssigned =
+    formData.places_assigned && formData.places_assigned.length > 0;
 
   return (
-    <SingleStepTaskListForm
-      key={key}
-      error={error}
-      schema={schema}
-      uiSchema={contactsUiSchema}
-      formData={formState}
-      formContext={{ userRole: role }}
-      mode={isCreatingState ? FormMode.CREATE : FormMode.READ_ONLY}
-      allowEdit={allowEdit}
-      inlineMessage={
-        isCreatingState && !role.includes("cas") && <NewOperationMessage />
-      }
-      onSubmit={async (data: { formData?: any }) => {
-        setError(undefined);
-        const updatedFormData = { ...formState, ...data.formData };
-        setFormState(updatedFormData);
-
-        const method = isCreatingState ? "POST" : "PUT";
-        const endpoint = isCreatingState
-          ? "registration/contacts"
-          : `registration/contacts/${formState.id}`;
-        const pathToRevalidate = isCreatingState
-          ? "/contacts"
-          : `/contacts/${formState.id}`;
-        const body = {
-          ...data.formData,
-        };
-
-        const response = await actionHandler(
-          endpoint,
-          method,
-          pathToRevalidate,
-          {
-            body: JSON.stringify(body),
-          },
-        );
-
-        if (response.error) {
-          setError(response.error);
-          return { error: response.error };
+    <>
+      <SimpleModal
+        title="Confirmation"
+        open={modalOpen}
+        onCancel={() => setModalOpen(false)}
+        onConfirm={handleArchiveContact}
+        confirmText="Delete Contact"
+        cancelText={hasPlacesAssigned ? "Cancel" : "Back"}
+        showConfirmButton={!hasPlacesAssigned}
+        isSubmitting={isSubmitting}
+      >
+        {hasPlacesAssigned
+          ? "Before you can delete this contact, please replace them in the places they are assigned with another contact first."
+          : "Please confirm that you would like to delete this contact."}
+      </SimpleModal>
+      <SingleStepTaskListForm
+        key={key}
+        error={error}
+        schema={schema}
+        uiSchema={contactsUiSchema}
+        formData={formState}
+        formContext={{ userRole: role }}
+        mode={isCreatingState ? FormMode.CREATE : FormMode.READ_ONLY}
+        allowEdit={allowEdit}
+        inlineMessage={
+          isCreatingState && !role.includes("cas") && <NewOperationMessage />
         }
+        showDeleteButton={!isCreatingState && !role.includes("cas")}
+        handleDelete={handleClickDelete}
+        deleteButtonText="Delete Contact"
+        onSubmit={async (data: { formData?: any }) => {
+          setError(undefined);
+          const updatedFormData = { ...formState, ...data.formData };
+          setFormState(updatedFormData);
 
-        if (isCreatingState) {
-          setIsCreatingState(false);
-          setFormState((prevState) => ({
-            ...prevState,
-            id: response.id,
-          }));
-        } else {
-          setKey(Math.random());
-        }
-        const replaceUrl = `/contacts/${
-          method === "POST" ? response.id : formState.id
-        }?contacts_title=${response.first_name} ${response.last_name}`;
-        router.replace(replaceUrl);
-      }}
-      onCancel={() => router.replace("/contacts")}
-    />
+          const method = isCreatingState ? "POST" : "PUT";
+          const endpoint = isCreatingState
+            ? "registration/contacts"
+            : `registration/contacts/${formState.id}`;
+          const pathToRevalidate = isCreatingState
+            ? "/contacts"
+            : `/contacts/${formState.id}`;
+          const body = {
+            ...data.formData,
+          };
+
+          const response = await actionHandler(
+            endpoint,
+            method,
+            pathToRevalidate,
+            {
+              body: JSON.stringify(body),
+            },
+          );
+
+          if (response.error) {
+            setError(response.error);
+            return { error: response.error };
+          }
+
+          if (isCreatingState) {
+            setIsCreatingState(false);
+            setFormState((prevState) => ({
+              ...prevState,
+              id: response.id,
+            }));
+          } else {
+            setKey(Math.random());
+          }
+          const replaceUrl = `/contacts/${
+            method === "POST" ? response.id : formState.id
+          }?contacts_title=${response.first_name} ${response.last_name}`;
+          router.replace(replaceUrl);
+        }}
+        onCancel={() => router.replace("/contacts")}
+      />
+    </>
   );
 }
