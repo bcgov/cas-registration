e5a512d92da538765af9c648d241ba7c3d760287
Brianna Cerkiewicz <brianna@button.is>
2025-07-14 10:08:26 -0700

    chore: conditionally use a longer timeout for local e2e tests

--- bciers/apps/compliance-e2e/playwright.config.ts
+++ bciers/apps/compliance-e2e/playwright.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "@playwright/test";
 import playwrightBaseConfig from "../../playwright.config.base";
 
-export default defineConfig({
+const config = {
   ...playwrightBaseConfig,
-});
+};
+
+if (process.env.E2E_TIMEOUT) {
+  config.expect = {
+    timeout: Number(process.env.E2E_TIMEOUT),
+  };
+}
+
+export default defineConfig(config);
