5a960a496c99a9b2f4212f417ddca2c054dc25cc
shon-button <shon@button.is>
2025-07-16 09:49:04 -0400

    chore: create app routes for role industry_user

---
+++ bciers/apps/compliance/src/app/bceidbusiness/industry_user/compliance-summaries/[compliance_summary_id]/(manage-obligation)/apply-compliance-units/page.tsx
@@ -0,0 +1,4 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@/compliance/src/app/components/compliance-summary/manage-obligation/apply-compliance-units/ApplyComplianceUnitsPage";
+
+export default defaultPageFactory(Page);
---
+++ bciers/apps/compliance/src/app/bceidbusiness/industry_user/compliance-summaries/[compliance_summary_id]/(request-issuance)/track-status-of-issuance/page.tsx
@@ -0,0 +1,4 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@/compliance/src/app/components/compliance-summary/request-issuance/track-status-of-issuance/TrackStatusOfIssuancePage";
+
+export default defaultPageFactory(Page);
