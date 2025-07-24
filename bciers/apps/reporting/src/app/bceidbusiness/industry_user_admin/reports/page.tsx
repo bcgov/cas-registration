23c9368e309c5b8f6e12e4c420ca34df9a9ead9c
acatchpole <adamantiumite@gmail.com>
2025-07-24 16:53:36 -0700

    chore: create pages for past and current reports

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/page.tsx
@@ -1,4 +1,23 @@
+import { generateMetadata } from "@bciers/components/layout/RootLayout";
 import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import Page from "apps/reporting/src/app/components/operations/OperationsPage";
+import OperationsPage from "@reporting/src/app/components/operations/OperationsPage";
+import ReportsBasePage from "@reporting/src/app/components/operations/ReportsBasePage";
+import { OperationsSearchParams } from "@reporting/src/app/components/operations/types";
 
-export default defaultPageFactory(Page);
+const title = "Reports";
+export const metadata = generateMetadata(title);
+
+function ReportsPage({
+  searchParams,
+}: {
+  searchParams: OperationsSearchParams;
+}) {
+  return (
+    <ReportsBasePage activeTab={0}>
+      <div className="flex flex-col">
+        <OperationsPage searchParams={searchParams || {}} />
+      </div>
+    </ReportsBasePage>
+  );
+}
+export default defaultPageFactory(ReportsPage);
