8e366d2bb7e4098bf0a07070796bef685e9cd515
SeSo <sobhani.sepehr@gmail.com>
2025-07-25 14:15:24 -0700

    chore: replace america/vancouver timezone with utc

--- bc_obps/service/reporting_year_service.py
+++ bc_obps/service/reporting_year_service.py
@@ -1,11 +1,10 @@
+from django.utils import timezone
 from reporting.models.reporting_year import ReportingYear
-from datetime import datetime
-from zoneinfo import ZoneInfo
 
 
 class ReportingYearService:
     @classmethod
     def get_current_reporting_year(cls) -> ReportingYear:
-        now = datetime.now(ZoneInfo("America/Vancouver"))
+        now = timezone.now()
 
         return ReportingYear.objects.get(reporting_window_start__lte=now, reporting_window_end__gte=now)
