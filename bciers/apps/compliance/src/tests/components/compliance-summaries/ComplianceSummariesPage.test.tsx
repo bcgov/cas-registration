282ec778e1d9225e71ba04ea34511ad67deafa12
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2025-07-31 09:17:38 -0700

    test: tests for the conditional alert messages on compliance summary grid page

--- bciers/apps/compliance/src/tests/components/compliance-summaries/ComplianceSummariesPage.test.tsx
+++ bciers/apps/compliance/src/tests/components/compliance-summaries/ComplianceSummariesPage.test.tsx
@@ -3,7 +3,10 @@ import ComplianceSummariesPage from "@/compliance/src/app/components/compliance-
 import * as fetchModule from "@/compliance/src/app/utils/fetchComplianceSummariesPageData";
 import * as sessionUtils from "@bciers/utils/src/sessionUtils";
 import { DataGridSearchParams } from "@/compliance/src/app/types";
-import { FrontEndRoles } from "@bciers/utils/src/enums";
+import {
+  ComplianceSummaryStatus,
+  FrontEndRoles,
+} from "@bciers/utils/src/enums";
 import * as reportingYearModule from "@reporting/src/app/utils/getReportingYear";
 
 // --- Component Mocks ---
@@ -32,8 +35,16 @@ const mockFetchResponse = {
       reporting_year: 2024,
       excess_emissions: 0,
     },
+    {
+      id: 2,
+      operation_name: "Another Test Operation",
+      reporting_year: 2024,
+      excess_emissions: 0,
+      obligation_id: "obligation-123",
+      status: ComplianceSummaryStatus.OBLIGATION_NOT_MET,
+    },
   ],
-  row_count: 1,
+  row_count: 2,
 };
 
 // --- Spies ---
@@ -81,5 +92,39 @@ describe("ComplianceSummariesPage", () => {
       ),
     ).toBeVisible();
     expect(screen.getByText(/due on November 30, 2025/i)).toBeVisible();
+  });
+
+  it("renders alert message with 'was' if current date is past due", async () => {
+    const currentDate = new Date("2025-12-01");
+    vi.setSystemTime(currentDate);
+    await renderPage();
+    expect(
+      screen.getByText(
+        /Your compliance obligation for the 2024 reporting year was/i,
+      ),
+    ).toBeVisible();
+    expect(screen.getByText(/due on November 30, 2025/i)).toBeVisible();
+  });
+
+  it("doesn't render alert message for overdue penalty if date is before due", async () => {
+    const currentDate = new Date("2025-10-01");
+    vi.setSystemTime(currentDate);
+
+    await renderPage();
+
+    expect(
+      screen.queryByText(/An automatic overdue penalty has been incurred/i),
+    ).toBeNull();
+  });
+
+  it("renders alert message for overdue penalty if date is past due and initial data has obligation not met", async () => {
+    const currentDate = new Date("2025-12-01");
+    vi.setSystemTime(currentDate);
+
+    await renderPage();
+
+    expect(
+      screen.getByText(/An automatic overdue penalty has been incurred/i),
+    ).toBeVisible();
   });
 });
