85162572dc7b44733f68d60d1035ad41408ae4db
Brianna Cerkiewicz <brianna@button.is>
2025-08-08 13:52:35 -0700

    chore: cleanup

--- bc_obps/compliance/service/compliance_invoice_service.py
+++ bc_obps/compliance/service/compliance_invoice_service.py
@@ -274,7 +274,7 @@ class ComplianceInvoiceService:
 
             # Generate and return the PDF generator, filename, and size
             return PDFGeneratorService.generate_pdf(
-                template_name="aop_invoice.html",
+                template_name="automatic_overdue_penalty_invoice.html",
                 context=context,
                 filename=filename,
             )
