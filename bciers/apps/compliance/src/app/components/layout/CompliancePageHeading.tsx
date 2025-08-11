e0affcf1b0dcd69bc29ef3d28865572c60795902
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2025-08-11 10:20:42 -0700

    chore: make operation name derived from reporting in obligation review page and compliance invoice

--- bciers/apps/compliance/src/app/components/layout/CompliancePageHeading.tsx
+++ bciers/apps/compliance/src/app/components/layout/CompliancePageHeading.tsx
@@ -1,16 +1,14 @@
 import { HasComplianceReportVersion } from "../../types";
-import getOperationByComplianceReportVersionId from "../../utils/getOperationByComplianceReportVersionId";
+import { getComplianceSummary } from "../../utils/getComplianceSummary";
 
 export const CompliancePageHeading = async ({
   compliance_report_version_id: complianceReportVersionId,
 }: Readonly<HasComplianceReportVersion>) => {
-  const operation = await getOperationByComplianceReportVersionId(
-    complianceReportVersionId,
-  );
+  const operation = await getComplianceSummary(complianceReportVersionId);
   return (
     <div className="container mx-auto pb-4">
       <h2 className="text-2xl font-bold mb-4 text-bc-bg-blue">
-        {operation.name}
+        {operation.operation_name}
       </h2>
     </div>
   );
