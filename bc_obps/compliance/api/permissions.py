88027c13fa069c373426ed0d7646b39272cc2ba8
Brianna Cerkiewicz <brianna@button.is>
2025-08-14 13:28:02 -0700

    chore: create permissions class for compliance

--- bc_obps/compliance/api/permissions.py
+++ bc_obps/compliance/api/permissions.py
@@ -34,33 +34,6 @@ def _validate_version_ownership_in_url(request: HttpRequest, version_id_param: s
     # this is the operator at the time the report was submitted, which may not be the same as the current operator if the operation was transferred
     operator = compliance_report_version.compliance_report.report.operator
 
-    # operation: Operation = ComplianceReportVersionService.get_operation_by_compliance_report_version(
-    #     compliance_report_version_id
-    # )
-
-    # created_at = compliance_report_version.compliance_report.created_at
-
-    # if not created_at:
-    #     logger.warning("Compliance report is missing created_at date.")
-    #     return False
-
-    # timeline = (
-    #     OperationDesignatedOperatorTimeline.objects.filter(
-    #         operation=operation,
-    #         start_date__lte=created_at.date(),  # ownership started on or before the report was created
-    #     )
-    #     .filter(
-    #         Q(end_date__gte=created_at.date())
-    #         | Q(end_date__isnull=True)  # ownership is ongoing or didn't end until after the report was created
-    #     )
-    #     .first()
-    # )
-
-    # if not timeline:
-    #     logger.warning("No valid operation timeline found for the compliance report version.")
-    #     return False
-    # operator = timeline.operator
-
     user_operator = UserOperator.objects.filter(
         user=request.current_user,  # type: ignore
         operator=operator,
@@ -76,21 +49,20 @@ def check_compliance_version_ownership_in_url(
         # Internal users can access all compliance report versions
         if request.current_user.is_irc_user():  # type: ignore
             return True
-
         return _validate_version_ownership_in_url(request, version_id_param)
 
     return validate_func
 
 
 approved_industry_user_compliance_report_version_composite_auth: Callable[[HttpRequest], bool] = compose_auth(
-    authorize("approved_industry_user"), check_compliance_version_ownership_in_url("version_id")
+    authorize("approved_industry_user"), check_compliance_version_ownership_in_url("compliance_report_version_id")
 )
 approved_authorized_roles_compliance_report_version_composite_auth: Callable[[HttpRequest], bool] = compose_auth(
-    authorize("approved_authorized_roles"), check_compliance_version_ownership_in_url("version_id")
+    authorize("approved_authorized_roles"), check_compliance_version_ownership_in_url("compliance_report_version_id")
 )
 approved_industry_user_cas_director_cas_analyst_compliance_report_version_composite_auth: Callable[
     [HttpRequest], bool
 ] = compose_auth(
     authorize("cas_director_analyst_and_approved_industry_user"),
-    check_compliance_version_ownership_in_url("version_id"),
+    check_compliance_version_ownership_in_url("compliance_report_version_id"),
 )
