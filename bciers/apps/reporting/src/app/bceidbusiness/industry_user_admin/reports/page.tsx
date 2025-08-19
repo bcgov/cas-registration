b1a83fbd999b581120d55ad7b2bb8d3735c65267
acatchpole <adamantiumite@gmail.com>
2025-08-19 08:50:21 -0700

    chore: redirect from /reports to current-reports

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/page.tsx
@@ -1,23 +1,5 @@
-import { generateMetadata } from "@bciers/components/layout/RootLayout";
-import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import OperationsPage from "@reporting/src/app/components/operations/OperationsPage";
-import ReportsBasePage from "@reporting/src/app/components/operations/ReportsBasePage";
-import { OperationsSearchParams } from "@reporting/src/app/components/operations/types";
+import { redirect } from "next/navigation";
 
-const title = "Reports";
-export const metadata = generateMetadata(title);
-
-function ReportsPage({
-  searchParams,
-}: {
-  searchParams: OperationsSearchParams;
-}) {
-  return (
-    <ReportsBasePage activeTab={0}>
-      <div className="flex flex-col">
-        <OperationsPage searchParams={searchParams || {}} />
-      </div>
-    </ReportsBasePage>
-  );
+export default function ReportsPage() {
+  redirect("/reports/current-reports");
 }
-export default defaultPageFactory(ReportsPage);
