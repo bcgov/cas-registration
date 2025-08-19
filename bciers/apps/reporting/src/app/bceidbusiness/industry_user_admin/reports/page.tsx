019e3e1e27b57847f7252797635c83dad578e572
acatchpole <adamantiumite@gmail.com>
2025-08-19 08:54:43 -0700

    chore: changed redirect to permanent

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function ReportsPage() {
-  redirect("/reports/current-reports");
+  permanentRedirect("/reports/current-reports");
 }
