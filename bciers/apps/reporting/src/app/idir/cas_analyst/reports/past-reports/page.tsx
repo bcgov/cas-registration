850197a841c698ef7ae04b75b5c3648da66b7f76
acatchpole <adamantiumite@gmail.com>
2025-08-28 15:28:06 -0700

    chore: add internal report-history placeholder

---
+++ bciers/apps/reporting/src/app/idir/cas_analyst/reports/past-reports/page.tsx
@@ -0,0 +1,5 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+
+export default defaultPageFactory(() => (
+  <div>Past Reports page is under development</div>
+));
