2763a5b4a41db885c38eb9fc652bd91742fadefc
Brianna Cerkiewicz <brianna@button.is>
2025-08-29 10:48:07 -0700

    chore: update wording in InternalIssuanceStatusDeclinedNote

--- bciers/apps/compliance/src/app/components/compliance-summary/request-issuance/internal/track-status-of-issuance/InternalIssuanceStatusDeclinedNote.tsx
+++ bciers/apps/compliance/src/app/components/compliance-summary/request-issuance/internal/track-status-of-issuance/InternalIssuanceStatusDeclinedNote.tsx
@@ -4,8 +4,9 @@ import { AlertIcon } from "@bciers/components/icons";
 export const InternalIssuanceStatusDeclinedNote = () => {
   return (
     <AlertNote icon={<AlertIcon width="20" height="20" />}>
-      The issuance request is declined. The earned credits will not be issued to
-      the holding account as identified below in B.C. Carbon Registry (BCCR).
+      Please contact the operator to clarify the supplementary report
+      requirement in the previous step. This request has been declined
+      automatically.
     </AlertNote>
   );
 };
