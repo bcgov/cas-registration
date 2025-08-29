9a3061b4892abb3b469884f5f6040dda2c7723c1
Brianna Cerkiewicz <brianna@button.is>
2025-08-29 13:17:44 -0700

    chore: remove reporting data from get_earned_credit_data_by_report_version

--- bc_obps/compliance/schema/compliance_earned_credits.py
+++ bc_obps/compliance/schema/compliance_earned_credits.py
@@ -1,25 +1,16 @@
 from typing import Literal, Optional
-from decimal import Decimal
 from compliance.service.bc_carbon_registry.schema import FifteenDigitString
 from ninja import ModelSchema, Field, Schema
 from compliance.models.compliance_earned_credit import ComplianceEarnedCredit
 
 OPERATION_NAME_ALIAS = "compliance_report_version.compliance_report.report.operation.name"
 REPORTING_YEAR_ALIAS = "compliance_report_version.compliance_report.compliance_period.end_date.year"
-EMISSIONS_ATTRIBUTABLE_ALIAS = (
-    "compliance_report_version.report_compliance_summary.emissions_attributable_for_compliance"
-)
-EMISSIONS_LIMIT_ALIAS = "compliance_report_version.report_compliance_summary.emissions_limit"
-EXCESS_EMISSIONS_ALIAS = "compliance_report_version.report_compliance_summary.excess_emissions"
 
 
 class ComplianceEarnedCreditsOut(ModelSchema):
     """Schema for compliance earned credits data"""
 
     reporting_year: int = Field(..., alias=REPORTING_YEAR_ALIAS)
-    emissions_attributable_for_compliance: Optional[Decimal] = Field(None, alias=EMISSIONS_ATTRIBUTABLE_ALIAS)
-    emissions_limit: Optional[Decimal] = Field(None, alias=EMISSIONS_LIMIT_ALIAS)
-    excess_emissions: Optional[Decimal] = Field(None, alias=EXCESS_EMISSIONS_ALIAS)
     analyst_submitted_by: Optional[str] = Field(None, alias="analyst_submitted_by.get_full_name")
 
     class Meta:
