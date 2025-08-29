70caef06fc17bea5cf9ec1bbadcb7059865f6399
acatchpole <adamantiumite@gmail.com>
2025-08-29 09:11:48 -0700

    chore: refactor reports pages to use ReportsBasePage

--- bciers/apps/reporting/src/app/idir/cas_analyst/reports/past-reports/page.tsx
+++
@@ -1,5 +0,0 @@
-import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-
-export default defaultPageFactory(() => (
-  <div>Past Reports page is under development</div>
-));
--- bciers/apps/reporting/src/app/idir/cas_director/reports/report-history/[report_id]/page.tsx
+++ bciers/apps/reporting/src/app/idir/cas_director/reports/report-history/[report_id]/page.tsx
@@ -1,4 +1,5 @@
 import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import Page from "@reporting/src/app/components/reportHistory/ReportHistoryPage";
 
-export default defaultPageFactory(Page);
+export default defaultPageFactory(() => (
+  <div>Internal Report History page is under development</div>
+));
