18246022617ad182132f72eb73b7eb41dda68aed
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-09-04 12:28:47 -0700

    chore: fixed issues with the naming

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/[version_id]/final-review/lfo/page.tsx
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user_admin/reports/[version_id]/final-review/lfo/page.tsx
@@ -1,3 +1,3 @@
 import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import Page from "@reporting/src/app/components/finalReview/LfoFinalReviewForm";
+import Page from "@reporting/src/app/components/finalReview/FacilityReportFinalReview";
 export default defaultPageFactory(Page);
