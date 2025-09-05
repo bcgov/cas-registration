3537d93e7353586ec3c33a14cc948a9a2e6250c1
Josh Gamache <jsgamache@gmail.com>
2025-09-05 13:11:46 -0600

    chore: ignore Bandit issue for Docker temp directory

--- dags/bc_obps_test_migrations.py
+++ dags/bc_obps_test_migrations.py
@@ -64,7 +64,7 @@ postgres_helm_install = KubernetesJobOperator(
     namespace="{{ params.destination_namespace }}",
     service_account_name=SERVICE_ACCOUNT_NAME,
     image=K8S_IMAGE,
-    env_vars={"HOME": "/tmp"},
+    env_vars={"HOME": "/tmp"},  # nosec B108
     cmds=["bash", "-c"],
     arguments=[
         "helm repo add cas-registration https://bcgov.github.io/cas-registration/ && "
@@ -109,7 +109,7 @@ backend_helm_install = KubernetesJobOperator(
     namespace="{{ params.destination_namespace }}",
     service_account_name=SERVICE_ACCOUNT_NAME,
     image=K8S_IMAGE,
-    env_vars={"HOME": "/tmp"},
+    env_vars={"HOME": "/tmp"},  # nosec B108
     cmds=["bash", "-c"],
     arguments=[
         "helm repo add cas-registration https://bcgov.github.io/cas-registration/ && "
