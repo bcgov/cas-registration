f2db4580462a9aa4d4d8166554d2de957f1328d1
Nataliia Piguliak <nataly.pigulyak@gmail.com>
2025-09-11 14:58:49 -0700

    chore: change default Alert icon color on Director Review page
    Signed-off-by: Nataliia Piguliak <nataly.pigulyak@gmail.com>

--- bciers/apps/compliance/src/app/data/jsonSchema/requestIssuance/internal/InternalDirectorReviewAwaitingNote.tsx
+++ bciers/apps/compliance/src/app/data/jsonSchema/requestIssuance/internal/InternalDirectorReviewAwaitingNote.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import AlertNote from "@bciers/components/form/components/AlertNote";
+import { AlertIcon } from "@bciers/components/icons";
 
 export const InternalDirectorReviewAwaitingNote = () => {
   return (
-    <AlertNote>
+    <AlertNote icon={<AlertIcon fill="#635231" />}>
       Once the issuance request is approved, the earned credits will be issued
       to the holding account as identified above in B.C. Carbon Registry.
     </AlertNote>
