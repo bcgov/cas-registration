ce0bc55e7952ed3d417e44d84231b22a29b01cf0
Brianna Cerkiewicz <brianna@button.is>
2025-09-11 14:54:48 -0700

    chore: BE work

---
+++ bc_obps/compliance/api/elicensing_invoices.py
@@ -0,0 +1,32 @@
+from typing import List
+from compliance.models.compliance_obligation import ComplianceObligation
+from compliance.models.elicensing_invoice import ElicensingInvoice
+from compliance.schema.elicensing_invoice import ElicensingInvoiceListOut
+from compliance.service.compliance_invoice_service import ComplianceInvoiceService
+from django.http import HttpRequest
+from django.db.models import QuerySet
+from common.permissions import authorize
+from common.api.utils import get_current_user_guid
+from compliance.models import ComplianceReportVersion
+from compliance.schema.compliance_report_version import ComplianceReportVersionListOut
+from ninja import ModelSchema
+from service.error_service.custom_codes_4xx import custom_codes_4xx
+from ninja.pagination import paginate, PageNumberPagination
+from compliance.service.compliance_dashboard_service import ComplianceDashboardService
+from registration.schema.generic import Message
+from .router import router
+from compliance.constants import COMPLIANCE
+
+
+
+@router.get(
+    "/elicensing-invoices",
+    response={200: List[ElicensingInvoiceListOut], custom_codes_4xx: Message},
+    tags=COMPLIANCE,
+    description="Get all compliance report versions for the current user's operations",
+    auth=authorize("approved_authorized_roles"),
+)
+@paginate(PageNumberPagination)
+def get_elicensing_invoice_list(request: HttpRequest) -> QuerySet[ComplianceReportVersion]:
+    user_guid = get_current_user_guid(request)
+    return ComplianceInvoiceService.get_elicensing_invoice_for_dashboard(user_guid)
--- bc_obps/compliance/schema/elicensing_invoice.py
+++ bc_obps/compliance/schema/elicensing_invoice.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
+from typing import Optional
 from compliance.models.elicensing_invoice import ElicensingInvoice
-from ninja import ModelSchema, Schema
+from ninja import Field, ModelSchema, Schema
 
 
 class ElicensingInvoiceOut(ModelSchema):
@@ -22,3 +24,17 @@ class ElicensingLastRefreshOut(Schema):
 
     last_refreshed_display: str | None
     data_is_fresh: bool
+
+class ElicensingInvoiceListOut(ModelSchema):
+    compliance_period: int = Field(..., alias="compliance_obligation.compliance_report_version.compliance_report.report.reporting_year.reporting_year")
+    operator_legal_name: str = Field(...,alias='compliance_obligation.compliance_report_version.compliance_report.report.operator.legal_name')
+    operation_name: str = Field(...,alias='compliance_obligation.compliance_report_version.compliance_report.report.operation.name')
+    invoice_total: Optional[Decimal] = None
+    total_adjustments: Optional[Decimal] = None
+    total_payments: Optional[Decimal] = None
+    invoice_type: Optional[str] = None
+    
+
+    class Meta:
+        model = ElicensingInvoice
+        fields = ['id', 'invoice_number','outstanding_balance']
