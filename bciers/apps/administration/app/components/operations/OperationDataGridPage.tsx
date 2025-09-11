fdfb495b0dc686254a5e89ec8b388545792dbba1
acatchpole <adamantiumite@gmail.com>
2025-09-11 10:15:09 -0700

    feat: user warning when operations are missing contact

--- bciers/apps/administration/app/components/operations/OperationDataGridPage.tsx
+++ bciers/apps/administration/app/components/operations/OperationDataGridPage.tsx
@@ -2,37 +2,26 @@ import OperationDataGrid from "./OperationDataGrid";
 import { OperationRow, OperationsSearchParams } from "./types";
 import { Suspense } from "react";
 import Loading from "@bciers/components/loading/SkeletonGrid";
-import { getSessionRole } from "@bciers/utils/src/sessionUtils";
-import { fetchOperationsPageData } from "@bciers/actions/api";
 
 // 🧩 Main component
 export default async function OperationDataGridPage({
-  searchParams,
+  isInternalUser,
+  initialData,
+  filteredSearchParams,
 }: {
-  searchParams: OperationsSearchParams;
-}) {
-  const role = await getSessionRole();
-  const isInternalUser = role.includes("cas_");
-
-  // IRC users should only see Registered operations
-  const filteredSearchParams = isInternalUser
-    ? { ...searchParams, operation__status: "Registered" }
-    : searchParams;
-
-  // Fetch operations data
-  const operations: {
+  isInternalUser: boolean;
+  initialData: {
     rows: OperationRow[];
     row_count: number;
-  } = await fetchOperationsPageData(filteredSearchParams);
-  if (!operations || "error" in operations)
-    throw new Error("Failed to retrieve operations");
-
+  };
+  filteredSearchParams: OperationsSearchParams;
+}) {
   // Render the DataGrid component
   return (
     <Suspense fallback={<Loading />}>
       <div className="mt-5">
         <OperationDataGrid
-          initialData={operations}
+          initialData={initialData}
           isInternalUser={isInternalUser}
           filteredSearchParams={filteredSearchParams}
         />
