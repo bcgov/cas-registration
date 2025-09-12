bd1f1b7fde9181ce9c6c0d03141d4b64c2cc7f5f
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-09-12 10:51:27 -0700

    feat: common alert field template factory function

--- bciers/libs/components/src/form/components/AlertNote.tsx
+++ bciers/libs/components/src/form/components/AlertNote.tsx
@@ -2,18 +2,29 @@ import { ReactNode, FC } from "react";
 import { AlertIcon } from "@bciers/components/icons";
 import { Alert } from "@mui/material";
 
-interface AlertNoteProps {
+export interface AlertNoteProps {
+  id: string;
   children: ReactNode;
   icon?: ReactNode;
+  alertType: "INFO" | "ALERT" | "ERROR";
 }
 
+const bgClassMap = {
+  INFO: "bg-bc-light-blue",
+  ALERT: "bg-bc-yellow",
+  ERROR: "bg-bc-error-red",
+};
+
 const AlertNote: FC<AlertNoteProps> = ({
+  id,
   children,
+  alertType = "INFO",
   icon = <AlertIcon width="25" height="25" />,
 }) => {
   return (
     <Alert
-      className="bg-bc-light-blue text-bc-text mb-2 items-center"
+      id={id}
+      className={`${bgClassMap[alertType]} text-bc-text mb-2 items-center`}
       icon={icon}
     >
       {children}
