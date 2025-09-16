dbc72ebec04c48a523c4f294015f683050d07553
acatchpole <adamantiumite@gmail.com>
2025-09-16 15:03:05 -0700

    chore: missing rep alert on op info form

--- bciers/apps/administration/app/data/jsonSchema/operationInformation/MissingRepresentativeAlertFieldTemplate.tsx
+++ bciers/apps/administration/app/data/jsonSchema/operationInformation/MissingRepresentativeAlertFieldTemplate.tsx
@@ -1,8 +1,16 @@
+"use client";
 import AlertFieldTemplateFactory from "@bciers/components/form/fields/AlertFieldTemplateFactory";
+import { BC_GOV_TEXT } from "@bciers/styles";
+import { InfoRounded } from "@mui/icons-material";
 
+const MissingRepresentativeAlertContent: React.FC = () => {
+  return <div>Please select an operation representative</div>;
+};
 
-const MissingRepresentativeAlertContent: React.FC = () => (
-  <div>Please select an operation representative</div>
+const component = AlertFieldTemplateFactory(
+  MissingRepresentativeAlertContent,
+  "ALERT",
+  <InfoRounded fontSize="inherit" sx={{ color: BC_GOV_TEXT }} />,
 );
 
-export default AlertFieldTemplateFactory(MissingRepresentativeAlertContent, "ALERT");
+export default component;
