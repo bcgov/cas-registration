f9b84df3d47a4597fcfc3859c9bd990a2b050be3
acatchpole <adamantiumite@gmail.com>
2025-09-17 15:56:28 -0700

    test: move missing rep test from page to form
    Signed-off-by: Nataliia Piguliak <nataly.pigulyak@gmail.com>

--- bciers/apps/administration/app/components/operations/types.ts
+++ bciers/apps/administration/app/components/operations/types.ts
@@ -67,7 +67,7 @@ export interface OperationInformationPartialFormData {
     mo_province?: string;
     mo_postal_code?: string;
   }[];
-  operation_representatives?: string[];
+  operation_representatives?: number[];
   registration_purpose?: string;
   regulated_operation?: string;
   new_entrant_operation?: string;
