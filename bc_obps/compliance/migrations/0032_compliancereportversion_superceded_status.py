d49e976ec2397d8ddf612f9c8482833f8c9bc062
Dylan Leard <dylan.1.leard@gov.bc.ca>
2025-09-17 16:03:40 -0700

    chore: fix migrations after rebase

--- bc_obps/compliance/migrations/0032_compliancereportversion_superceded_status.py
+++ bc_obps/compliance/migrations/0032_compliancereportversion_superceded_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('compliance', '0030_generate_compliance_reports'),
+        ('compliance', '0031_alter_elicensingadjustment_comment_and_more'),
     ]
 
     operations = [
