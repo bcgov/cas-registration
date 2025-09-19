cc6612b6243ea8ef6684082495b43db2e66f874d
Nataliia Piguliak <nataly.pigulyak@gmail.com>
2025-09-19 11:07:35 -0700

    feat: add iconColor prop to AlertNote component for direct color control
    Signed-off-by: Nataliia Piguliak <nataly.pigulyak@gmail.com>

--- bciers/apps/compliance/src/app/data/jsonSchema/requestIssuance/internal/InternalDirectorReviewAwaitingNote.tsx
+++ bciers/apps/compliance/src/app/data/jsonSchema/requestIssuance/internal/InternalDirectorReviewAwaitingNote.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import AlertNote from "@bciers/components/form/components/AlertNote";
-import { AlertIcon } from "@bciers/components/icons";
 
 export const InternalDirectorReviewAwaitingNote = () => {
   return (
-    <AlertNote icon={<AlertIcon fill="#635231" />}>
+    <AlertNote iconColor="#635231">
       Once the issuance request is approved, the earned credits will be issued
       to the holding account as identified above in B.C. Carbon Registry.
     </AlertNote>
