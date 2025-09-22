673f77c7ea2934d040ae15c78f7289e76f7b0820
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-09-22 16:36:41 -0700

    chore: fix file structure

--- bciers/apps/reporting/src/app/bceidbusiness/industry_user/reports/[version_id]/final-review/final-review/page.tsx
+++
@@ -1,4 +0,0 @@
-import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import Page from "@reporting/src/app/components/finalReview/FinalReviewPage";
-
-export default defaultPageFactory(Page);
--- bciers/apps/reporting/src/app/bceidbusiness/industry_user/reports/[version_id]/submitted/submitted/page.tsx
+++
@@ -1,4 +0,0 @@
-import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import Page from "@reporting/src/app/components/submitted/SubmittedPage";
-
-export default defaultPageFactory(Page);
