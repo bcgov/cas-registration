3c1bf9758b94991f9ba54a7bfeac4f91b10c2fd6
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2025-09-22 09:30:28 -0700

    chore: fixed directory and files

---
+++ bciers/apps/reporting/src/app/bceidbusiness/industry_user/reports/[version_id]/submitted/submitted/page.tsx
@@ -0,0 +1,4 @@
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@reporting/src/app/components/submitted/SubmittedPage";
+
+export default defaultPageFactory(Page);
