c0386638f308beefb3eb64be09cde731976a09e6
fviduya <Francesca.Viduya@gov.bc.ca>
2025-10-21 10:15:43 -0600

    chore: make sonarcloud happy

--- bciers/apps/compliance-e2e/tests/example.spec.ts
+++ bciers/apps/compliance-e2e/tests/example.spec.ts
@@ -1,6 +1,3 @@
-// import testNxProjectUrl from "@bciers/e2e/utils/test-nx-app-url";
-
-// testNxProjectUrl(["compliance"]);
 import testNxProjectLandingPage from "@bciers/e2e/utils/test-nx-app-landing-page";
 
 testNxProjectLandingPage(["compliance-summaries"]);
