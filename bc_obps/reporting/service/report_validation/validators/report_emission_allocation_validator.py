d28274309913359dd0e3a473c8a69f187c552624
acatchpole <adamantiumite@gmail.com>
2025-10-28 16:36:44 -0700

    test: emission allocation validation tests

--- bc_obps/reporting/service/report_validation/validators/report_emission_allocation_validator.py
+++ bc_obps/reporting/service/report_validation/validators/report_emission_allocation_validator.py
@@ -1,4 +1,5 @@
 from reporting.models.facility_report import FacilityReport
+from reporting.models.report_emission_allocation import ReportEmissionAllocation
 from reporting.models.report_version import ReportVersion
 from reporting.service.report_emission_allocation_service import ReportEmissionAllocationService
 from reporting.service.report_validation.report_validation_error import (
@@ -21,14 +22,13 @@ def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
     Returns:
         dict[str, ReportValidationError]: A dictionary of validation errors, keyed by facility and category.
     """
-
     facility_reports = FacilityReport.objects.filter(report_version=report_version)
 
-    errors = {}
+    errors: dict[str, ReportValidationError] = {}
 
     for fr in facility_reports:
         emission_data = ReportEmissionAllocationService.get_emission_allocation_data(report_version.id, fr.facility_id)
-        if emission_data.allocation_methodology != "Not applicable":
+        if emission_data.allocation_methodology != ReportEmissionAllocation.AllocationMethodologyChoices.NOT_APPLICABLE:
             for category in emission_data.report_product_emission_allocations:
                 total_allocated = sum(product.allocated_quantity for product in category.products)
                 if total_allocated != category.emission_total:
@@ -36,6 +36,6 @@ def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
                         f"facility_{fr.facility_id}_category_{category.emission_category_id}_allocation_mismatch"
                     ] = ReportValidationError(
                         Severity.ERROR,
-                        f"Allocated emissions for {fr.facility_name} in '{category.emission_category_name}' category do not match reported emissions.",
+                        f"Emissions allocated for {fr.facility_name} in '{category.emission_category_name}' category do not match reported emissions.",
                     )
     return errors
