50f8be669bfd3bd434546263871458466071d286
acatchpole <adamantiumite@gmail.com>
2025-10-30 14:41:49 -0700

    chore: transfer button and page for cas_director

---
+++ bc_obps/common/migrations/0093_update_dashboard_data.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+from common.utils import reset_dashboard_data
+
+
+def reset_dashboard_data_fn(_, schema_editor):
+    reset_dashboard_data()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0092_notice_of_obligation_met_email_template'),
+    ]
+
+    operations = [
+        migrations.RunPython(reset_dashboard_data_fn, elidable=True),
+    ]
