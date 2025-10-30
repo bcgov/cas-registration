7503f921655cd83bd007dea2d6d46099b2d1cb47
Josh Gamache <jsgamache@gmail.com>
2025-10-30 15:09:16 -0600

    chore: remove unused variable

--- .bin/check-immutable-migration-files.sh
+++ .bin/check-immutable-migration-files.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 # Get the latest release tag from the main branch, as we consider migrations immutable from that point
 git fetch --tags origin main
 latest_main_tag=$(git tag --merged origin/main | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n1)
-
-latest_tag=$(git describe --tags --abbrev=0)
 files_diff=$(git diff --name-only origin/main -- bc_obps/**/migrations)
 
 poetry --directory ./bc_obps run python manage.py check_immutable_migration_files --tag "$latest_main_tag" --diff "$files_diff"
