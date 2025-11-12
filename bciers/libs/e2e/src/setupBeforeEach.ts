c1a39c753e60bf78d2c1f24dbecba18b2461c41c
SeSo <sobhani.sepehr@gmail.com>
2025-11-12 10:22:50 -0800

    chore: refactor e2e test setup to centralize logic in `setupTest`
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/libs/e2e/src/setupBeforeEach.ts
+++ bciers/libs/e2e/src/setupBeforeEach.ts
@@ -1,19 +1,5 @@
-import { test as baseTest } from "@playwright/test";
-import { test as happoTest } from "happo-playwright";
-import { mergeTests } from "@playwright/test";
-import { AppName } from "@/administration-e2e/utils/constants";
-import { setupTestEnvironment } from "@bciers/e2e/utils/helpers";
-import { getStorageStateForRole } from "@bciers/e2e/utils/helpers";
-
-const test = mergeTests(baseTest, happoTest);
+import { setupTest } from "./setupTest";
 
 export function setupBeforeEachTest(role: string) {
-  // 👤 run test using the storageState for this role
-  test.use({ storageState: getStorageStateForRole(role) });
-
-  test.beforeEach(async () => {
-    await setupTestEnvironment(`${AppName}-${role}`);
-  });
-
-  return test;
+  return setupTest(role, "beforeEach");
 }
