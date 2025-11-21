518a0ec4d281572c94637d1cabf2b0bc9523adc0
SeSo <sobhani.sepehr@gmail.com>
2025-11-20 17:26:43 -0800

    chore: remove unused function
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/libs/actions/src/api/hasRegisteredRegulatedOperationForCurrentUser.ts
+++
@@ -1,8 +0,0 @@
-import { actionHandler } from "@bciers/actions";
-
-export default async function hasRegisteredRegulatedOperationForCurrentUser() {
-  return actionHandler(
-    "registration/user-operators/current/has_registered_regulated_operation",
-    "GET",
-  );
-}
