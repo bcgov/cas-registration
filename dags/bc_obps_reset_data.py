8d01940bf81b38acaf1b14757f97460feb90f5ba
Josh Gamache <jsgamache@gmail.com>
2025-11-21 15:09:05 -0700

    refactor: update reset data DAG to use TaskFlow patterns

--- dags/bc_obps_reset_data.py
+++ dags/bc_obps_reset_data.py
@@ -1,10 +1,9 @@
 from dag_configuration import default_dag_args
 from trigger_k8s_cronjob import trigger_k8s_cronjob
-from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
 from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime, timedelta, timezone
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 import os
 import sys
 
@@ -19,23 +18,22 @@ BCIERS_NAMESPACE = os.getenv("BCIERS_NAMESPACE")
 
 default_args = {**default_dag_args}
 
+RESET_DAG_DOC = """
+DAG to reset the data in the BCIERS database to a freshly deployed state.
+"""
+
 
 @dag(
     dag_id=RESET_DATA_DAG_NAME,
     schedule=None,  # This dag is intended to be run manually
     default_args=default_args,
     is_paused_upon_creation=False,
+    doc_md=RESET_DAG_DOC,
 )
 def reset_data():
-    """
-    DAG to reset the data in the BCIERS database to a freshly deployed state.
-    """
-
-    reset_data_task = PythonOperator(
-        task_id="reset_data_task",
-        python_callable=trigger_k8s_cronjob,
-        op_args=['reset-database', BCIERS_NAMESPACE],
-    )
+    @task
+    def reset_data_task():
+        trigger_k8s_cronjob('reset-database', BCIERS_NAMESPACE)
 
     cycle_backend_pod_task = KubernetesJobOperator(
         task_id="cycle_backend_pod",
@@ -57,16 +55,19 @@ def reset_data():
     reset_data_task >> cycle_backend_pod_task >> trigger_wait_for_backend_rollout
 
 
+ROLLOUT_DAG_DOC = """
+DAG to wait for the backend rollout to complete. Intended to be triggered after the DB reset.
+"""
+
+
 @dag(
     dag_id=WAIT_FOR_BACKEND_ROLLOUT_DAG_NAME,
     schedule=None,  # This dag is intended to be run manually
     default_args=default_args,
     is_paused_upon_creation=False,
+    doc_md=ROLLOUT_DAG_DOC,
 )
 def wait_for_backend_rollout():
-    """
-    DAG to wait for the backend rollout to complete. Intended to be triggered after the DB reset.
-    """
     wait_for_backend_rollout_task = KubernetesJobOperator(
         task_id="wait_for_backend_rollout",
         name="wait-for-backend-rollout",
