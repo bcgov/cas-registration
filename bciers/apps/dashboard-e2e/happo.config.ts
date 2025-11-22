7d9a8f5846c956ebb0ad8886308a5fc3191fbb81
SeSo <sobhani.sepehr@gmail.com>
2025-11-21 16:06:42 -0800

    chore: migrate happo from legacy packages

---
+++ bciers/apps/dashboard-e2e/happo.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig, type Config } from "happo";
+// @ts-ignore - CommonJS module
+import { baseConfig } from "../../happo-base.config.js";
+
+export default defineConfig({
+  ...baseConfig,
+  project: "cas-dashboard",
+} as Config);
