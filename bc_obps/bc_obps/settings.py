913b8026c1e6d8efb2ba4e4d6e9ed72e610c1fd4
SeSo <sobhani.sepehr@gmail.com>
2025-12-03 12:40:00 -0800

    chore: intentional change to make sure CI flags working

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+CI test: verifying backend change detection
 """
 
 import os
