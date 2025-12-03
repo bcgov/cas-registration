3478dd6cd67378e37f028c029c18971b08816e09
SeSo <sobhani.sepehr@gmail.com>
2025-12-03 08:28:33 -0800

    chore: add a change to make sure flag works

--- bc_obps/bc_obps/settings.py
+++ bc_obps/bc_obps/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+Test change to verify backend changes detection.
 """
 
 import os
