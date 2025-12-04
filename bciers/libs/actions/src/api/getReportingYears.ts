60b4ea46bb54fab48546bc4986a1266737299156
Andrea Williams <andrea.williams@gov.bc.ca>
2025-12-03 17:36:04 -0800

    chore: added new endpoint for getting all reporting years

---
+++ bciers/libs/actions/src/api/getReportingYears.ts
@@ -0,0 +1,8 @@
+import { actionHandler } from "@bciers/actions";
+
+async function getReportingYears(exclude_past?: boolean) {
+    const query = exclude_past !== undefined ? `?exclude_past=${exclude_past}` : "";
+    return actionHandler(`reporting/reporting-years${query}`, "GET", "");
+}
+
+export default getReportingYears;
