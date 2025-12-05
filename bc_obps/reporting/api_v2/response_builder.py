873d95459fbe866f4fbc73b58721a4a875220b93
acatchpole <adamantiumite@gmail.com>
2025-12-05 10:38:17 -0800

    chore: wip

--- bc_obps/reporting/api_v2/response_builder.py
+++ bc_obps/reporting/api_v2/response_builder.py
@@ -1,5 +1,7 @@
+from django.db.models import QuerySet
 from typing import Self
 
+from ninja.pagination import PageNumberPagination
 
 class ResponseBuilder:
 
@@ -10,6 +12,25 @@ class ResponseBuilder:
 
     def payload(self, payload: dict) -> Self:
         self.response["payload"] = payload
+        return self
+
+    def build(self) -> dict:
+        return self.response
+
+
+class PaginatedResponseBuilder:
+    def __init__(self, page_size:int, **kwargs,):
+        self.response = {}
+        self.input = PageNumberPagination.Input(page_size=page_size, **kwargs)
+
+    def payload(self, payload: QuerySet) -> Self:
+        self.response["payload"] = payload
+        paginator = PageNumberPagination(page_size=self.page_size)
+        self.response['payload'] = paginator.paginate_queryset(
+            payload, self.input
+        )
+        self.response['payload']['items'] = list(self.response['payload']['items'])
+        print('*********** payload:', self.response['payload'])
         return self
 
     def build(self) -> dict:
