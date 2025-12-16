d6a08df3e595b6a9c93219830cfe4bfcf334b86c
Andrea Williams <andrea.williams@gov.bc.ca>
2025-12-15 17:29:45 -0800

    chore: moved reporting endpoints tests to new /endpoints dir to match behaviour described in docs & registration; added tests for reporting-years endpoints

---
+++ bc_obps/reporting/tests/endpoints/test_reporting_years_endpoints.py
@@ -0,0 +1,32 @@
+from registration.tests.utils.helpers import CommonTestSetup, TestUtils
+from datetime import datetime
+
+
+class TestReportingYearsEndpoint(CommonTestSetup):
+
+    def test_get_current_reporting_year(self):
+        endpoint_to_test = "/api/reporting/reporting-year"
+
+        # arbitrarily chose role cas_analyst
+        response = TestUtils.mock_get_with_auth_role(self, 'cas_analyst', endpoint_to_test)
+        assert response.status_code == 200
+
+    def test_get_all_reporting_years_no_filter(self):
+        endpoint_to_test = "/api/reporting/reporting-years"
+
+        response = TestUtils.mock_get_with_auth_role(self, 'cas_director', endpoint_to_test)
+        assert response.status_code == 200
+        response_json = response.json()
+        returned_years = [item["reporting_year"] for item in response_json]
+        assert [year in returned_years for year in [2023, 2024, 2025, 2026]]
+
+    def test_get_all_reporting_years_exclude_past(self):
+        endpoint_to_test = "/api/reporting/reporting-years?exclude_past=true"
+
+        response = TestUtils.mock_get_with_auth_role(self, 'cas_director', endpoint_to_test)
+        assert response.status_code == 200
+        response_json = response.json()
+        returned_years = [item["reporting_year"] for item in response_json]
+        assert 2023 not in returned_years
+        current_year = datetime.now().year
+        assert current_year in returned_years
