34a83ebd964d86be1fbea057e55a33202d755fd7
SeSo <sobhani.sepehr@gmail.com>
2025-12-19 14:52:11 -0800

    chore: migrate `@bciers/styles` to ECMAScript modules and clean up TypeScript references
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

---
+++ bciers/libs/styles/src/colors.js
@@ -0,0 +1,18 @@
+// BC GOV colors
+export const BC_GOV_LINKS_COLOR = "#1A5A96";
+export const BC_GOV_PRIMARY_BRAND_COLOR_BLUE = "#003366";
+export const BC_GOV_BACKGROUND_COLOR_BLUE = "#38598a";
+export const BC_GOV_TEXT = "#313132";
+export const BC_GOV_BACKGROUND_COLOR_GREY = "#f2f2f2";
+export const BC_GOV_COMPONENTS_GREY = "#606060";
+export const BC_GOV_YELLOW = "#FCBA19";
+export const BC_GOV_BG_YELLOW = "#F8F1CB";
+export const BC_GOV_SEMANTICS_RED = "#D8292F";
+export const BC_GOV_SEMANTICS_GREEN = "#2E8540";
+// App specific colors
+export const DARK_GREY_BG_COLOR = "#E5E5E5";
+export const LIGHT_GREY_BG_COLOR = "#fafafc";
+export const LIGHT_GREY_COLOR_200 = "#f5f5f5";
+export const LIGHT_GREY_COLOR_300 = "#f0f0f0";
+export const LIGHT_BLUE_BG_COLOR = "#DCE9F6";
+export const WHITE = "#ffffff";
