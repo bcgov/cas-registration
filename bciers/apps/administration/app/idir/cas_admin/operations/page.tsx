2206cacafaf269eba1db3b2c273ca136037bec83
shon-button <shon@button.is>
2025-12-30 11:21:41 -0500

    refactor: use defaultPageFactory for routes - Admin Operations

--- bciers/apps/administration/app/idir/cas_admin/operations/page.tsx
+++ bciers/apps/administration/app/idir/cas_admin/operations/page.tsx
@@ -1,10 +1,4 @@
-// 🚩 flagging that for shared routes between roles, "Page" code is a component for code maintainability
-import { OperationsSearchParams } from "@/administration/app/components/operations/types";
-import OperationPage from "@/administration/app/components/operations/OperationPage";
+import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
+import Page from "@/administration/app/components/operations/OperationPage";
 
-export default async function Page(props: {
-  searchParams: Promise<OperationsSearchParams>;
-}) {
-  const searchParams = await props.searchParams;
-  return <OperationPage searchParams={searchParams} />;
-}
+export default defaultPageFactory(Page);
