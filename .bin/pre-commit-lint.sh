c57fc65dc35da64f90340fed01957fefdc4f85a6
SeSo <sobhani.sepehr@gmail.com>
2026-01-08 07:55:07 -0800

    chore: ensure pre-commit lint script fetches `origin/develop` if missing
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- .bin/pre-commit-lint.sh
+++ .bin/pre-commit-lint.sh
@@ -4,4 +4,10 @@ set -euxo pipefail
 
 pushd bciers || exit 1
 
-NODE_OPTIONS="--max-old-space-size=5120" yarn nx affected --target=lint --base=develop --parallel
+# NX requires origin/develop to exist when using --base=origin/develop to determine affected files.
+# Fetch it explicitly as a remote tracking branch if it doesn't exist.
+if ! git rev-parse --verify origin/develop >/dev/null 2>&1; then
+  git fetch origin develop:refs/remotes/origin/develop --depth=1
+fi
+
+NODE_OPTIONS="--max-old-space-size=5120" yarn nx affected --base=origin/develop --target=lint --parallel
