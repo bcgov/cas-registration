2ca775d9465ca1f28ec21a7619c95fc776e2d81a
SeSo <sobhani.sepehr@gmail.com>
2026-01-08 07:28:11 -0800

    chore: fix pre-commit lint script to correctly use 'develop' as the base branch
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- .bin/pre-commit-lint.sh
+++ .bin/pre-commit-lint.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 pushd bciers || exit 1
 
-NODE_OPTIONS="--max-old-space-size=5120" yarn nx affected --target=lint --base=origin/develop --parallel
+NODE_OPTIONS="--max-old-space-size=5120" yarn nx affected --target=lint --base=develop --parallel
