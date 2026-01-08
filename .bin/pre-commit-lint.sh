818b3f73afe51570eb64dfdc017029d1eb79dc24
SeSo <sobhani.sepehr@gmail.com>
2026-01-08 08:28:55 -0800

    chore: prevent git lock conflicts in pre-commit lint script by disabling parallel file handling
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- .bin/pre-commit-lint.sh
+++ .bin/pre-commit-lint.sh
@@ -7,7 +7,7 @@ pushd bciers || exit 1
 # NX requires origin/develop to exist when using --base=origin/develop to determine affected files.
 # Fetch it explicitly as a remote tracking branch if it doesn't exist.
 if ! git rev-parse --verify origin/develop >/dev/null 2>&1; then
-  git fetch origin develop:refs/remotes/origin/develop --depth=1
+  git fetch origin develop --depth=1
 fi
 
 NODE_OPTIONS="--max-old-space-size=5120" yarn nx affected --base=origin/develop --target=lint --parallel
