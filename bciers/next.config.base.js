3b35dff84f5a7e7eac0c1e6002a6b04010fb515c
Dylan Leard <dylan.1.leard@gov.bc.ca>
2026-01-27 15:26:40 -0800

    chore: remove header that clashes with rjsf ajv validation

--- bciers/next.config.base.js
+++ bciers/next.config.base.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const { withSentryConfig } = require("@sentry/nextjs");
 const path = require("node:path");
-const isLocal = process.env.ENVIRONMENT === "local";
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' ${isLocal ? " 'unsafe-eval'" : ""};
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
 const baseConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
@@ -76,10 +63,6 @@ const baseConfig = {
       {
         source: "/(.*)",
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
