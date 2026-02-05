e34f6178ab5db4184b66c6b02699ab4a9cdb6668
acatchpole <adamantiumite@gmail.com>
2026-02-04 16:04:19 -0800

    chore: revert changes to save override

--- bc_obps/reporting/models/activity_source_type_json_schema.py
+++ bc_obps/reporting/models/activity_source_type_json_schema.py
@@ -2,6 +2,8 @@ from common.models import BaseModel
 from django.db import models
 from registration.models import Activity
 from reporting.models import SourceType, Configuration
+import typing
+from reporting.utils import validate_overlapping_records
 from reporting.models.rls_configs.activity_source_type_json_schema import Rls as ActivitySourceTypeJsonSchemaRls
 
 
@@ -30,3 +32,13 @@ class ActivitySourceTypeJsonSchema(BaseModel):
         db_table = 'erc"."activity_source_type_json_schema'
 
     Rls = ActivitySourceTypeJsonSchemaRls
+
+    @typing.no_type_check
+    def save(self, *args, **kwargs) -> None:
+        """
+        Override the save method to validate if there are overlapping records.
+        """
+        exception_message = f"This record will result in duplicate json schemas being returned for the date range {self.valid_from.valid_from} - {self.valid_to.valid_to} as it overlaps with a current record or records"
+
+        validate_overlapping_records(ActivitySourceTypeJsonSchema, self, exception_message)
+        super().save(*args, **kwargs)
