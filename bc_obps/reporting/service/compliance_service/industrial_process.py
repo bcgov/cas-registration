b93411ba38e7f855200f95c9416561b85abeb0b0
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-02-06 16:18:40 -0800

    chore: refactor compliance service to resolve the proper things
    chore: stub for biogenic emissions split

---
+++ bc_obps/reporting/service/compliance_service/industrial_process.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from decimal import Decimal
+import logging
+from reporting.models.report_product import ReportProduct
+from reporting.service.compliance_service import ComplianceService
+from reporting.service.emission_category_service import EmissionCategoryService
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class BiogenicEmissionsSplit:
+    chemical_pulp_ratio: Decimal
+    lime_recovery_kilm_ratio: Decimal
+
+    def __post_init__(self) -> None:
+        if not self.chemical_pulp_ratio + self.lime_recovery_kilm_ratio == Decimal('1'):
+            raise ValueError("The biogenic emissions split reported must total to 1.")
+
+
+def retrieve_pulp_and_paper_biogenic_emissions_split(report_version_id: int) -> BiogenicEmissionsSplit:
+    logger.error("Biogenic emissions split should be retrieved from the database instead.")
+    return BiogenicEmissionsSplit(chemical_pulp_ratio=Decimal('0.4'), lime_recovery_kilm_ratio=Decimal('0.6'))
+
+
+def compute_industrial_process_emissions(rp: ReportProduct) -> Decimal:
+
+    industrial_process = ComplianceService.get_allocated_emissions_by_report_product_emission_category(
+        rp.report_version.id, rp.product_id, [3]
+    )  # ID=3 is Industrial Emissions category
+
+    # Handle Pulp & Paper specific edge case:
+    # Subtract the sum of emissions that were categorized as industrial_process & (woody_biomass or other_excluded_biomass) from
+    # the industrial_process emission total attributed to the product "Pulp and paper: chemical pulp".
+
+    """
+    TODO
+    This is where we need to:
+    - for chemical pulp, take the (industrial process for chem pulp + lime recovery kikn) overlapping * percentage associated and remove from industrial process
+    - for lime recovery kiln, take the overlapping * percentage associated and remove from industrial process
+    """
+
+    if (
+        rp.report_version.report.operation.naics_code
+        and rp.report_version.report.operation.naics_code.naics_code.startswith("322")
+        and rp.product.name in ["Pulp and paper: chemical pulp", "Pulp and paper: lime recovery kiln"]
+    ):
+        biogenic_emissions_split = retrieve_pulp_and_paper_biogenic_emissions_split(rp.report_version.id)
+        overlapping_industrial_process_emissions = (
+            EmissionCategoryService.get_industrial_process_excluded_biomass_overlap_by_report_version(
+                rp.report_version.id
+            )
+        )
+
+        if rp.product.name == "Pulp and paper: chemical pulp":
+            return (
+                industrial_process
+                - overlapping_industrial_process_emissions * biogenic_emissions_split.chemical_pulp_ratio
+            )
+
+        if rp.product.name == "Pulp and paper: lime recovery kiln":
+            return (
+                industrial_process
+                - overlapping_industrial_process_emissions * biogenic_emissions_split.lime_recovery_kilm_ratio
+            )
+
+    return industrial_process
