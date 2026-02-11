3bec19198e9fc6c81af0bf428d1991d7e9e6a4ca
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-02-11 14:56:39 -0800

    chore: updating help text formatting

--- bciers/libs/components/src/form/fields/FieldHelpTemplate.tsx
+++ bciers/libs/components/src/form/fields/FieldHelpTemplate.tsx
@@ -1,22 +1,14 @@
 "use client";
 
-import { BC_GOV_BACKGROUND_COLOR_BLUE } from "@bciers/styles/colors";
-import { Typography } from "@mui/material";
 import { FieldHelpProps } from "@rjsf/utils";
 
 function FieldHelpTemplate(props: FieldHelpProps) {
   const { help } = props;
   return (
-    <span>
-      <Typography
-        variant="body2"
-        color={BC_GOV_BACKGROUND_COLOR_BLUE}
-        fontStyle="italic"
-        fontSize={16}
-      >
-        {help}
-      </Typography>
-    </span>
+    <small>
+      <b>Note: </b>
+      {help}
+    </small>
   );
 }
 
