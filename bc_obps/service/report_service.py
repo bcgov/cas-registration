a2cfca9341124fd83cbae6d8061a86e15f19f6c5
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2026-02-18 11:01:44 -0800

    chore: reverted code

--- bc_obps/service/report_service.py
+++ bc_obps/service/report_service.py
@@ -130,7 +130,12 @@ class ReportService:
                 report_version__id=report_version_id
             )
             for f in facility_reports:
-                FacilityReportService.set_activities_for_facility_report(facility_report=f, activities=data.activities)
+                # For LFOs, only set activities if the facility doesn't have any yet (initial setup)
+                # This prevents overwriting user-selected activities at the facility level
+                if not f.activities.exists():
+                    FacilityReportService.add_activities_to_facility_report(
+                        facility_report=f, activities=data.activities
+                    )
                 # Always update regulated products
                 FacilityReportService.prune_report_product_data_for_facility_report(
                     facility_report=f, regulated_products=data.regulated_products
