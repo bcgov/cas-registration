fc6d3b93f08b029e71ed22775b7126581f0e2123
acatchpole <adamantiumite@gmail.com>
2026-03-16 15:55:37 -0700

    chore: refactor with constants and indexOf

--- bciers/libs/components/src/form/widgets/RegulatedProductMultiSelectWidget.tsx
+++ bciers/libs/components/src/form/widgets/RegulatedProductMultiSelectWidget.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import MultiSelectWidgetWithTooltip from "@bciers/components/form/widgets/MultiSelectWidgetWithTooltip";
+import MultiSelectWidgetWithTooltip, {
+  FieldSchemaWithTooltip,
+} from "@bciers/components/form/widgets/MultiSelectWidgetWithTooltip";
 import { WidgetProps } from "@rjsf/utils";
-import { FieldSchemaWithTooltip } from "@bciers/components/form/widgets/MultiSelectWidgetWithTooltip";
 
-const CHEMICAL_PULP_NAME = "Pulp and paper: chemical pulp";
-const LIME_RECOVERED_NAME = "Pulp and paper: lime recovered by kiln";
+export const CHEMICAL_PULP_NAME = "Pulp and paper: chemical pulp";
+export const LIME_RECOVERED_NAME = "Pulp and paper: lime recovered by kiln";
 
 const RegulatedProductMultiSelectWidget = (props: WidgetProps) => {
   const value = Array.isArray(props.value) ? props.value : [];
@@ -15,11 +16,10 @@ const RegulatedProductMultiSelectWidget = (props: WidgetProps) => {
   const enumIds = fieldSchema?.enum ?? [];
   const enumNames = fieldSchema?.enumNames ?? [];
 
-  const chemicalPulpProductId =
-    enumIds[enumNames.findIndex((name) => name === CHEMICAL_PULP_NAME)];
+  const chemicalPulpProductId = enumIds[enumNames.indexOf(CHEMICAL_PULP_NAME)];
 
   const limeRecoveredByKilnProductId =
-    enumIds[enumNames.findIndex((name) => name === LIME_RECOVERED_NAME)];
+    enumIds[enumNames.indexOf(LIME_RECOVERED_NAME)];
 
   const hasChemicalPulp =
     chemicalPulpProductId !== undefined &&
@@ -36,8 +36,7 @@ const RegulatedProductMultiSelectWidget = (props: WidgetProps) => {
   const helpText = showPulpPaperHelp ? (
     <>
       <strong>Note: </strong>If this is a chemical pulp mill that recovered lime
-      by kiln, select both 'Pulp and paper: chemical pulp' and 'Pulp and paper:
-      lime recovered by kiln'
+      by kiln, select both '{CHEMICAL_PULP_NAME}' and '{LIME_RECOVERED_NAME}'
     </>
   ) : (
     ""
