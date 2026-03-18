1e7127478ac82849a577e88cb2f6fe7fdc7b4de5
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-03-18 12:32:02 -0700

    chore: hide new product at the service level
    test: test
    
    chore: depend on flag

--- bc_obps/service/data_access_service/regulated_product_service.py
+++ bc_obps/service/data_access_service/regulated_product_service.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from django.conf import settings
 from registration.models import RegulatedProduct
 from registration.schema import RegulatedProductSchema
 from django.core.cache import cache
@@ -12,6 +13,11 @@ class RegulatedProductDataAccessService:
         if cached_data:
             return cached_data
         else:
-            regulated_products = RegulatedProduct.objects.only(*RegulatedProductSchema.Meta.fields)
+            if settings.RESTRICTED_NAICS_CODES_FOR_REPORTING:
+                regulated_products = RegulatedProduct.objects.exclude(
+                    name="Pulp and paper: lime recovered by kiln"
+                ).only(*RegulatedProductSchema.Meta.fields)
+            else:
+                regulated_products = RegulatedProduct.objects.only(*RegulatedProductSchema.Meta.fields)
             cache.set("regulated_products", regulated_products, 60 * 60 * 24 * 1)  # 1 day
             return regulated_products
