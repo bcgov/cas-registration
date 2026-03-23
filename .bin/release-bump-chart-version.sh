0e7af4893ca9b9cbace2669f14dd43db8ff886a4
nhm500043 <gabriele.dalcengio@gov.bc.ca>
2026-03-23 11:30:25 -0700

    chore: add security context, ignore flag, and changed image tag pull from latest to appVersion to fix trivy issues in helm folder

--- .bin/release-bump-chart-version.sh
+++ .bin/release-bump-chart-version.sh
@@ -5,4 +5,6 @@ set -euxo pipefail
 VERSION=$1
 
 sed -i '' "s/^appVersion:.*/appVersion: \"${VERSION}\"/" helm/cas-bciers/Chart.yaml
+sed -i '' "s/^appVersion:.*/appVersion: \"${VERSION}\"/" helm/migration-test/cas-obps-backend-migration-test/Chart.yaml
 git add helm/cas-bciers/Chart.yaml
+git add helm/migration-test/cas-obps-backend-migration-test/Chart.yaml
