dd97922ecf162fb08b0c49cee7802feab4ae83bb
Dylan Leard <dylan.1.leard@gov.bc.ca>
2026-04-16 14:50:29 -0700

    chore: update new range bound validator to use validator tags

--- bc_obps/reporting/service/report_validation/validators/__init__.py
+++ bc_obps/reporting/service/report_validation/validators/__init__.py
@@ -25,17 +25,3 @@ __all__ = [
     "report_emission_allocation_validator",
     "report_data_by_fuel_type_validator",
 ]
-
-__errors__ = [
-    "operation_boroid_presence",
-    "mandatory_verification_statement",
-    "report_attachments_are_scanned",
-    "supplementary_report_version_change",
-    "supplementary_report_attachments_confirmation",
-    "report_activity_json_validation",
-    "report_emission_allocation_validator",
-]
-
-__warnings__ = [
-    "report_data_by_fuel_type_validator",
-]
