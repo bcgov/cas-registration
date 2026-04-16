07a7bb23740d8ce9af4cbeaf962c0a4ad5dea884
Dylan Leard <dylan.1.leard@gov.bc.ca>
2026-04-16 11:57:39 -0700

    chore: make tag parameter optional and run all validators if it is None

--- bc_obps/reporting/service/report_validation/report_validation_service.py
+++ bc_obps/reporting/service/report_validation/report_validation_service.py
@@ -36,14 +36,14 @@ class ReportValidationService:
     validation_plugins = collect_validation_plugins()
 
     @staticmethod
-    def validate_report_version(version_id: int, tag: ValidationTags) -> dict[str, ReportValidationError]:
+    def validate_report_version(version_id: int, tag: ValidationTags | None = None) -> dict[str, ReportValidationError]:
 
         report_version = ReportVersion.objects.get(id=version_id)
 
         results: List[dict[str, ReportValidationError]] = [
             validation_plugin.validate(report_version)
             for validation_plugin in ReportValidationService.validation_plugins
-            if tag in validation_plugin.TAGS
+            if tag in validation_plugin.TAGS or tag is None
         ]
 
         # Aggregate the results in one dictionary, by key
