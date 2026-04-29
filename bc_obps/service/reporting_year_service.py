1352c4fd26eaf8efd70d722ce7210524d8a3680b
SeSo <sobhani.sepehr@gmail.com>
2026-04-29 14:58:57 -0700

    chore: order reporting year by the year

--- bc_obps/service/reporting_year_service.py
+++ bc_obps/service/reporting_year_service.py
@@ -1,8 +1,8 @@
+from django.db.models import QuerySet
 from django.utils import timezone
 from reporting.models.report import Report
 from reporting.models.report_version import ReportVersion
 from reporting.models.reporting_year import ReportingYear
-from typing import List
 
 
 class ReportingYearService:
@@ -13,16 +13,17 @@ class ReportingYearService:
         return ReportingYear.objects.get(reporting_window_start__lte=now, reporting_window_end__gte=now)
 
     @classmethod
-    def get_all_reporting_years(cls, exclude_past: bool = False) -> List[ReportingYear]:
+    def get_all_reporting_years(cls, exclude_past: bool = False) -> QuerySet[ReportingYear]:
         """
-        Returns list of all ReportingYear objects in database, or only the ReportingYears that haven't already closed.
+        Returns all ReportingYear objects in the database, or only the ReportingYears that haven't already closed.
         @param exclude_past (optional, boolean) - will exclude from the returned list ReportingYears that have already ended.
         """
+        qs = ReportingYear.objects.all()
         if exclude_past:
             current_year = cls.get_current_reporting_year()
-            resp = list(ReportingYear.objects.filter(reporting_year__gte=current_year.reporting_year))
+            resp = qs.filter(reporting_year__gte=current_year.reporting_year)
             return resp
-        return list(ReportingYear.objects.all())
+        return qs
 
     @classmethod
     def is_reporting_open(cls, reporting_year: ReportingYear) -> bool:
