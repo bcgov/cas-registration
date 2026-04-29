d51a0aaea513bc4c9874cfc20c42f992e1ed6a15
SeSo <sobhani.sepehr@gmail.com>
2026-04-29 16:39:47 -0700

    test(e2e): update reporting landing page test to include current reports path
    Signed-off-by: SeSo <sobhani.sepehr@gmail.com>

--- bciers/apps/reporting-e2e/tests/landing-page.spec.ts
+++ bciers/apps/reporting-e2e/tests/landing-page.spec.ts
@@ -1,3 +1,3 @@
 import testNxProjectLandingPage from "@bciers/e2e/utils/test-nx-app-landing-page";
 
-testNxProjectLandingPage(["reporting"]);
+testNxProjectLandingPage(["reporting/reports/current-reports"]);
