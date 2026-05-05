b4ca7015e89fc34138d82c6b1f34718fdfaeb0fe
SeSo <sobhani.sepehr@gmail.com>
2026-05-05 14:06:45 -0700

    chore: happo spec work

--- bciers/happo-base.config.js
+++ bciers/happo-base.config.js
@@ -3,31 +3,37 @@ require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const VIEWPORT = "1366x768";
 const MAXHEIGHT = 20000;
+const CHUNKS = 4;
 
 const baseConfig = {
   integration: {
     type: "playwright",
+    downloadAllAssets: true,
   },
   targets: {
     chrome: {
       type: "chrome",
       viewport: VIEWPORT,
       maxHeight: MAXHEIGHT,
+      chunks: CHUNKS,
     },
     firefox: {
       type: "firefox",
       viewport: VIEWPORT,
       maxHeight: MAXHEIGHT,
+      chunks: CHUNKS,
     },
     safari: {
       type: "safari",
       viewport: VIEWPORT,
       maxHeight: MAXHEIGHT,
+      chunks: CHUNKS,
     },
     edge: {
       type: "edge",
       viewport: VIEWPORT,
       maxHeight: MAXHEIGHT,
+      chunks: CHUNKS,
     },
   },
 };
