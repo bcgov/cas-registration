d4a0ac00965bea0647f9d9f0cbe72b0453af87cc
Andrea Williams <andrea.williams@gov.bc.ca>
2026-05-12 17:44:54 -0700

    chore: removed stray console logger statement

--- bciers/apps/reporting/src/app/components/complianceSummary/ComplianceSummaryForm.tsx
+++ bciers/apps/reporting/src/app/components/complianceSummary/ComplianceSummaryForm.tsx
@@ -30,8 +30,6 @@ const ComplianceSummaryForm: React.FC<Props> = ({
     displayJanMarProduction,
   );
 
-  console.log(summaryFormData);
-
   const complianceSummaryUiSchema = createComplianceSummaryUiSchema(
     summaryFormData.reporting_year,
   );
