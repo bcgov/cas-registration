43243eb802242dec922ec273e82a9cdbe208dc4a
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-05-26 14:18:23 -0700

    chore: working validator

--- bc_obps/reporting/service/report_validation/validators/reporting_configuration_validator.py
+++ bc_obps/reporting/service/report_validation/validators/reporting_configuration_validator.py
@@ -1,3 +1,6 @@
+
+from django.db.models import Exists, OuterRef
+from reporting.models.configuration_element import ConfigurationElement
 from reporting.models.report_methodology import ReportMethodology
 from reporting.models.report_version import ReportVersion
 from reporting.service.report_validation.report_validation_error import ReportValidationError
@@ -17,8 +20,26 @@ def validate(report_version: ReportVersion) -> dict[str, ReportValidationError]:
     - Validate that the extra reporting fields reported are in that configuration element
     """
 
-    report_methodology_records = ReportMethodology.objects.select_related(
-        "report_emission",
-        "report_emission__report_source_type",
-        "report_emission__report_source_type__report_activity",
-    ).filter(report_version_id=report_version.id)
+    config_element = ConfigurationElement.objects.filter(
+        activity_id=OuterRef("report_emission__report_source_type__report_activity__activity_id"),
+        source_type_id=OuterRef("report_emission__report_source_type__source_type_id"),
+        gas_type_id=OuterRef("report_emission__gas_type_id"),
+        methodology_id=OuterRef("methodology_id"),
+        valid_from__valid_from__year__lte=OuterRef("report_version__report__reporting_year_id"),
+        valid_to__valid_to__year__gte=OuterRef("report_version__report__reporting_year_id"),
+    ).values("id")[:1]
+
+    missing_config = (
+        ReportMethodology.objects.select_related(
+            "report_emission",
+            "report_emission__report_source_type",
+            "report_emission__report_source_type__report_activity",
+            "report_version__report",
+        )
+        .filter(report_version_id=report_version.id)
+        .annotate(
+            matching_config_exists=Exists(config_element),
+        )
+        .filter(matching_config_exists=False)
+        .values("id")
+    )
