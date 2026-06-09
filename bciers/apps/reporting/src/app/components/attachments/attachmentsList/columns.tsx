81e7f408c741d778b81c847cd86e376f335525c9
SeSo <sobhani.sepehr@gmail.com>
2026-06-09 10:51:52 -0700

    test: add report attachments e2e tests

--- bciers/apps/reporting/src/app/components/attachments/attachmentsList/columns.tsx
+++ bciers/apps/reporting/src/app/components/attachments/attachmentsList/columns.tsx
@@ -1,5 +1,5 @@
 import { CloudDownload } from "@mui/icons-material";
-import { Stack } from "@mui/material";
+import Button from "@mui/material/Button";
 import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import downloadAttachment from "../download";
 
@@ -7,17 +7,18 @@ const AttachmentDownloadCell = (params: GridRenderCellParams) => {
   const handleDownload = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     e.stopPropagation();
-
     await downloadAttachment(params.row.report_version_id, params.row.id);
   };
 
   return (
-    <button onClick={handleDownload} style={{ cursor: "pointer" }}>
-      <Stack alignItems="center" direction="row" spacing={1}>
-        <span>{params.row.attachment_name}</span>
-        <CloudDownload />
-      </Stack>
-    </button>
+    <Button
+      variant="outlined"
+      endIcon={<CloudDownload />}
+      onClick={handleDownload}
+      className="h-10 rounded border border-bc-link-blue"
+    >
+      {params.row.attachment_name}
+    </Button>
   );
 };
 
