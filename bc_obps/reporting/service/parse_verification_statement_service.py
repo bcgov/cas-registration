e62ce019eefb3beb53069d67a450eff164ea14c4
Andrea Williams <andrea.williams@gov.bc.ca>
2026-06-22 11:57:20 -0700

    Add parse_verification_statement_service.py

---
+++ bc_obps/reporting/service/parse_verification_statement_service.py
@@ -0,0 +1,23 @@
+import camelot
+import fitz
+from typing import List
+import pandas as pd
+
+class ParseVerificationStatementService:
+    @staticmethod
+    def read_pdf(file_path: str) -> str:
+        # Use fitz to extract text from the PDF
+        doc = fitz.open(file_path)
+        text = ""
+        for page in doc:
+            text += page.get_text()
+        return text
+
+    @staticmethod
+    def read_tables(file_path: str) -> List[pd.DataFrame]:
+        # Use camelot to extract tables from the PDF
+        tables = camelot.read_pdf(file_path, pages='all')
+        table_dataframes = []
+        for table in tables:
+            table_dataframes.append(table.df)
+        return table_dataframes
