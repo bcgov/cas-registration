76701756aaaba01a2ad953033de5985aebbd1b47
acatchpole <adamantiumite@gmail.com>
2026-06-23 13:43:04 -0700

    chore: update get api to fetch comments by report_id

--- bc_obps/reporting/api/comments.py
+++ bc_obps/reporting/api/comments.py
@@ -3,7 +3,9 @@ from venv import logger
 from common.permissions import authorize
 
 
+from django.db.models import Subquery
 from django.http import HttpRequest
+from reporting.models.report_version import ReportVersion
 from service.error_service.custom_codes_4xx import custom_codes_4xx
 from reporting.schema.generic import Message
 from ..models import (
@@ -16,18 +18,19 @@ from .router import router
 @router.get(
     "/comments/version_id/{version_id}",
     response={200: list[CommentSchema], custom_codes_4xx: Message},
-    description="Fetch only the facility_report data for the given report version and facility id (final review format).",
-    auth=authorize("approved_authorized_roles"),
+    description="Fetch comments for a given report version.",
+    auth=authorize("authorized_irc_user"),
 )
 def pickupPassengers(
     request: HttpRequest, version_id: int, facility_id: str | None = None
 ) -> list[ReportCommentHeadHonchoOfTheCongaLineBoii]:
     """
-    Fetch the head comment threads for a given report version and facility id.
+    Fetch the comment threads for a given report version and facility id.
     """
-    query = ReportCommentHeadHonchoOfTheCongaLineBoii.objects.prefetch_related(
-        "report_comments_bodyofthesnake",
-    ).filter(report_version_id=version_id)
+    report_id = ReportVersion.objects.filter(pk=version_id).values("report_id")[:1]
+    query = ReportCommentHeadHonchoOfTheCongaLineBoii.objects.filter(
+        report_version__report_id=Subquery(report_id)
+    ).prefetch_related("report_comments_bodyofthesnake")
 
     if facility_id is not None:
         facility_uuid = UUID(facility_id)
