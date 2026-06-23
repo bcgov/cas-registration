773f036a85845afe59a571acee01d908a69e5dc5
Andrea Williams <andrea.williams@gov.bc.ca>
2026-06-23 15:15:07 -0700

    In progress. Table extraction works on 1 PDF

--- bc_obps/reporting/service/parse_verification_statement_service.py
+++ bc_obps/reporting/service/parse_verification_statement_service.py
@@ -1,24 +1,31 @@
-import camelot
-# import fitz
+import tabula
 from typing import List
 import pandas as pd
 
 
 class ParseVerificationStatementService:
-    # @staticmethod
-    # def read_pdf(file_path: str) -> str:
-    #     # Use fitz to extract text from the PDF
-    #     doc = fitz.open(file_path)
-    #     text = ""
-    #     for page in doc:
-    #         text += page.get_text()
-    #     return text
-
     @staticmethod
     def read_tables(file_path: str) -> List[pd.DataFrame]:
-        # Use camelot to extract tables from the PDF
-        tables = camelot.read_pdf(file_path, pages='all')
-        table_dataframes = []
+        # Use tabula to extract tables from the PDF
+        return tabula.read_pdf(file_path, stream=True, pages="all")
+    
+    @staticmethod
+    def search_table_headers_for_keywords(tables: list[pd.DataFrame]):
+        """
+        Best-guess attempt at determining which tables are the most relevant. 
+        This is done by searching for keywords in the table headers, such as "Error" or "Opinion".
+
+        Tables with "Opinion" in the column headers are assumed to relate to verifier's determination
+        of Unmodified/Modified/Adverse (1 for emissions sections, 1 for compliance section if the operation is Regulated.)
+
+        Tables with "Error" in the column headers are assumed to list the errors the verifier found,
+        including whether those errors were Material or Immaterial.
+
+        If a table contains any of these keywords in its headers, it is considered a key table and is returned in the list of key tables.
+        """
+        key_tables = []
         for table in tables:
-            table_dataframes.append(table.df)
-        return table_dataframes
+            headers = table.columns.astype(str)
+            if headers.str.contains("Error|Opinion", regex=True, na=False).any():
+                key_tables.append(table)
+        return key_tables
