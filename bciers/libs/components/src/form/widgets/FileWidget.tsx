94a0ea7c007381cde6549bdb260e7a63e1d545b4
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-06-29 15:35:35 -0700

    chore: almost feature complete

--- bciers/libs/components/src/form/widgets/FileWidget.tsx
+++ bciers/libs/components/src/form/widgets/FileWidget.tsx
@@ -2,19 +2,23 @@ import { actionHandler } from "@bciers/actions";
 import { AlertIcon } from "@bciers/components/icons";
 import { useSessionRole } from "@bciers/utils/src/sessionUtils";
 import { TranslatableString, WidgetProps } from "@rjsf/utils";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const MAX_FILE_SIZE = 20000000;
 type FileScanStatus = "Unscanned" | "Clean" | "Quarantined";
 
 // Show a different message depending on the fileScanStatus
-const showScanStatus = (status: FileScanStatus | undefined | null) => {
+const showScanStatus = (
+  status: FileScanStatus | undefined | null,
+  filename: string,
+) => {
   if (status === "Quarantined") {
     return (
       <div className="flex items-center justify-between text-red-500 text-sm">
         <AlertIcon />
         <span className="ml-2">
-          Security risk found. Check for malware or upload a different file.
+          Security risk found in "{filename}". Check for malware or upload a
+          different file.
         </span>
       </div>
     );
@@ -24,8 +28,8 @@ const showScanStatus = (status: FileScanStatus | undefined | null) => {
       <div className="flex items-center justify-between">
         <div className="flex flex-col">
           <span>
-            Uploading. You may continue to the next page while the file is being
-            scanned for security.
+            Uploading "{filename}"". You may continue to the next page while the
+            file is being scanned for security.
           </span>
         </div>
       </div>
@@ -72,23 +76,41 @@ const useFileUploadWidget = (): [
 export function FileElement({
   value,
   preview,
+  localFile,
 }: {
   readonly value: {
-    id: number;
+    id?: number;
     name: string;
     status?: FileScanStatus;
   };
   readonly preview?: boolean;
+  readonly localFile?: File;
 }) {
-  const handlePreview = () => {
+  const handlePreview = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (localFile) {
+      const anchorTag = document.createElement("a");
+      Object.assign(anchorTag, {
+        target: "_blank",
+        rel: "noopener noreferrer",
+        href: URL.createObjectURL(localFile),
+        download: localFile.name,
+      });
+      anchorTag.click();
+      anchorTag.remove();
+      return;
+    }
+
+    alert("file preview not available");
     // Similarly to the report attachments, we'll call the backend
-    // to fetch a temporary URL for the file in GCS, and open it in a new tab
+    // to fetch a temporary URL for the file in GCS, and open it in a new tab.
   };
 
   return (
     <ul className="m-0 py-0 flex flex-col justify-start list-none">
       <li data-name={value.name}>
-        {JSON.stringify(value)} {showScanStatus(value.status) || value.name}{" "}
+        {showScanStatus(value.status, value.name) || value.name}{" "}
         {preview && value.status !== "Quarantined" && (
           <button className="button-link file-download" onClick={handlePreview}>
             {TranslatableString.PreviewLabel}
@@ -118,21 +140,32 @@ const FileWidget: React.FC<WidgetProps> = (props) => {
     );
   }
 
+  const [localFile, setLocalFile] = useState<File | undefined>(undefined);
+
   const role = useSessionRole();
   const isCasInternal = role?.includes("cas") && !role?.includes("pending");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-    if (file) {
-      if (file.size > MAX_FILE_SIZE) {
-        alert("File size must be less than 20MB");
-        return;
-      }
-      registry.formContext.onFileSelected(file, name);
+    if (!file) {
+      setLocalFile(undefined);
+      onChange(undefined);
+      return;
     }
 
+    if (file.size > MAX_FILE_SIZE) {
+      alert("File size must be less than 20MB");
+      return;
+    }
+    setLocalFile(file);
+    registry.formContext.onFileSelected(file, name);
+
     // From the RJSF perspective this will just be a string with the file name.
-    onChange(file?.name);
+
+    onChange({
+      name: file.name,
+      status: "Unscanned",
+    });
   };
 
   const hiddenFileInput = useRef<HTMLInputElement>(null);
@@ -162,6 +195,7 @@ const FileWidget: React.FC<WidgetProps> = (props) => {
           {value ? reuploadLabel : uploadLabel}
         </button>
       )}
+
       <input
         id={id}
         name={id}
@@ -177,7 +211,11 @@ const FileWidget: React.FC<WidgetProps> = (props) => {
         accept={options.accept ? String(options.accept) : undefined}
       />
       {value ? (
-        <FileElement value={value} preview={options.filePreview} />
+        <FileElement
+          value={value}
+          preview={options.filePreview}
+          localFile={localFile}
+        />
       ) : (
         <span className="text-base md:text-lg flex-shrink-0">
           No attachment was uploaded.
