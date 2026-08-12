d4cdf19e55f9efbf4325c8a1a05877ff4cd0e2d0
Andrea <andrea.williams@gov.bc.ca>
2026-08-12 14:43:05 -0700

    chore: in progress - building internal Penalty Calculator UI

---
+++ bciers/apps/compliance/src/app/components/compliance-summary/manage-obligation/internal/review-compliance-summary/PenaltyCalculatorPage.tsx
@@ -0,0 +1,3 @@
+import PenaltyCalculatorPage from "@/compliance/src/app/components/compliance-summary/manage-obligation/internal/review-penalty-summary/PenaltyCalculatorPage";
+
+export default PenaltyCalculatorPage;
---
+++ bciers/apps/compliance/src/app/data/jsonSchema/manageObligation/internal/PenaltyCalculatorSchema.tsx
@@ -0,0 +1,76 @@
+import { RJSFSchema, UiSchema } from "@rjsf/utils";
+import FieldTemplate from "@bciers/components/form/fields/FieldTemplate";
+import FieldTemplateFullWidth from "@bciers/components/form/fields/FieldTemplateFullWidth";
+import {
+  readOnlyObjectField,
+  readOnlyStringField,
+} from "@/compliance/src/app/data/jsonSchema/helpers";
+import { PenaltyTypeButtonGroupWidget } from "../../../../components/compliance-summary/manage-obligation/internal/review-penalty-summary/PenaltyTypeButtonGroupWidget";
+import { PenaltySummaryField } from "../../../../components/compliance-summary/manage-obligation/internal/review-penalty-summary/PenaltySummaryWidget";
+
+export const createPenaltyCalculatorSchema = (): RJSFSchema => ({
+  type: "object",
+  title: "Penalty Calculator",
+  properties: {
+    automatic_overdue_penalty: readOnlyStringField(
+      "Automatic overdue penalty:",
+    ),
+    ggeapar_penalty: readOnlyStringField("GGEAPAR Penalty:"),
+    penalty_type: {
+      type: "string",
+      title: "1. Select penalty type",
+      enum: ["automatic_overdue", "ggeapar"],
+      default: "automatic_overdue",
+    },
+    final_day_of_penalty_accrual: {
+      type: "string",
+      format: "date",
+      title: "2. Select final day of penalty accrual",
+    },
+    penalty_summary: {
+      type: "object",
+      title: "Penalty summary",
+      properties: {
+        total_penalty_amount: {
+          type: ["string", "number", "null"],
+        },
+        days_late: {
+          type: ["string", "number", "null"],
+        },
+      },
+      additionalProperties: false,
+    },
+    accrual_data: readOnlyObjectField("Accrual data"),
+  },
+});
+
+export const penaltyCalculatorUiSchema: UiSchema = {
+  "ui:FieldTemplate": FieldTemplate,
+  "ui:classNames": "form-heading-label",
+
+  automatic_overdue_penalty: {},
+  ggeapar_penalty: {},
+  penalty_type: {
+    "ui:widget": PenaltyTypeButtonGroupWidget,
+    "ui:options": {
+      label: false,
+      classNames: "text-bc-bg-blue",
+    },
+  },
+  final_day_of_penalty_accrual: {
+    "ui:widget": "DateWidget",
+    "ui:options": {
+      classNames: "text-bc-bg-blue",
+    },
+  },
+
+  penalty_summary: {
+    "ui:field": PenaltySummaryField,
+    "ui:FieldTemplate": FieldTemplateFullWidth,
+    "ui:options": {
+      label: false,
+      inline: true,
+    },
+    "ui:classNames": "!block [&>div]:!w-full [&>div]:!max-w-none",
+  },
+};
