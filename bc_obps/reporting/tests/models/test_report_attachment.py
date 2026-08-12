8f5d51b6fd9d8c0f7b1cdf202df795f7f2d8da8a
Dylan <dylan.1.leard@gov.bc.ca>
2026-08-12 14:28:16 -0700

    test: more rls tests

--- bc_obps/reporting/tests/models/test_report_attachment.py
+++ bc_obps/reporting/tests/models/test_report_attachment.py
@@ -1,130 +1,160 @@
-# from common.tests.utils.helpers import BaseTestCase
-# from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
-# from reporting.models.report_attachment import ReportAttachment
-# from reporting.tests.utils.immutable_report_version import (
-#     assert_immutable_report_version,
-# )
-# from model_bakery.baker import make_recipe, make
-# from reporting.tests.utils.report_rls_test_infrastructure import ReportRlsSetup
-# from rls.tests.helpers import assert_policies_for_cas_roles, assert_policies_for_industry_user
+from common.tests.utils.helpers import BaseTestCase
+from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
+from reporting.models.report_attachment import ReportAttachment
+from reporting.tests.utils.immutable_report_version import (
+    assert_immutable_report_version,
+)
+from model_bakery.baker import make_recipe
+from rls.tests.helpers import assert_policies_for_cas_roles, assert_policies_for_industry_user
+from reporting.tests.utils.report_rls_test_infrastructure import ReportRlsTestSetup
+from common.lib import pgtrigger
 
 
-# class ReportAttachmentTest(BaseTestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.test_object = ReportAttachment()
-#         cls.field_data = [
-#             ("id", "ID", None, None),
-#             *TIMESTAMP_COMMON_FIELDS,
-#             ("report_version", "report version", None, None),
-#             ("attachment", "attachment", None, None),
-#             ("attachment_type", "attachment type", None, None),
-#             ("attachment_name", "attachment name", None, None),
-#             ("status", "status", None, None),
-#         ]
+class ReportAttachmentTest(BaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_object = ReportAttachment()
+        cls.field_data = [
+            ("id", "ID", None, None),
+            *TIMESTAMP_COMMON_FIELDS,
+            ("report_version", "report version", None, None),
+            ("attachment", "attachment", None, None),
+            ("attachment_type", "attachment type", None, None),
+            ("attachment_name", "attachment name", None, None),
+            ("status", "status", None, None),
+        ]
 
-#     def test_supports_over_100_chars_in_file_name(self):
-#         long_file_name = "ten_chars " * 90 + ".longextension"
-#         report_attachment = make_recipe("reporting.tests.utils.report_attachment", attachment=long_file_name)
-#         self.assertEqual(report_attachment.attachment.name, long_file_name)
+    def test_supports_over_100_chars_in_file_name(self):
+        long_file_name = "ten_chars " * 90 + ".longextension"
+        report_attachment = make_recipe("reporting.tests.utils.report_attachment", attachment=long_file_name)
+        self.assertEqual(report_attachment.attachment.name, long_file_name)
 
-#     def test_immutable_after_report_version_submitted(self):
-#         assert_immutable_report_version("reporting.tests.utils.report_attachment")
+    def test_immutable_after_report_version_submitted(self):
+        assert_immutable_report_version("reporting.tests.utils.report_attachment")
 
 
-# class ReportAttachmentRlsTest(BaseTestCase):
-#     def test_report_attachment_rls_industry_user(self):
-#         # Create a report attachment for an approved user operator
-#         test = ReportRlsSetup()
-#         test_report_attachment = make(
-#             ReportAttachment,
-#             report_version=test.report_version,
-#             attachment='test_attachment.pdf',
-#             attachment_type=ReportAttachment.ReportAttachmentType.VERIFICATION_STATEMENT,
-#         )
+class ReportAttachmentRlsTest(BaseTestCase):
+    def test_report_attachment_rls_industry_user(self):
+        # Common Test Setup
+        t = ReportRlsTestSetup()
 
-#         # Create a random report attachment which the user should not have access to
-#         random = ReportRlsSetup()
-#         random_report_attachment = make(
-#             ReportAttachment,
-#             report_version=random.report_version,
-#             attachment='random_attachment.pdf',
-#             attachment_type=ReportAttachment.ReportAttachmentType.VERIFICATION_STATEMENT,
-#         )
+        # ReportAttachment Setup
+        # Inside access bounds
+        with pgtrigger.ignore("reporting.ReportAttachment:immutable_report_version"):
+            report_attachment_2010_submitted = make_recipe(
+                'reporting.tests.utils.report_attachment', report_version=t.report_version_2010_submitted
+            )
+        report_attachment_2010_draft = make_recipe(
+            'reporting.tests.utils.report_attachment', report_version=t.report_version_2010_draft
+        )
+        # Outside access bounds
+        report_attachment_2013_draft = make_recipe(
+            'reporting.tests.utils.report_attachment', report_version=t.report_version_2013_draft
+        )
 
-#         def select_function(cursor):
-#             ReportAttachment.objects.get(id=test_report_attachment.id)
+        # Additionl report_version needed for insert test: 2012 - Within access bounds
+        # reporting_year_2012 = make_recipe('reporting.tests.utils.reporting_year', reporting_year=2012)
+        # report_2012 = make_recipe(
+        #     'reporting.tests.utils.report',
+        #     operation=t.report_version_2010_submitted.report.operation,
+        #     operator=t.report_version_2010_submitted.report.operator,
+        #     reporting_year=reporting_year_2012,
+        # )
+        # report_version_2012_draft = make_recipe(
+        #     'reporting.tests.utils.report_version', report=report_2012, status='Draft'
+        # )
 
-#         def forbidden_select_function(cursor):
-#             ReportAttachment.objects.get(id=random_report_attachment.id)
+        def select_function(cursor):
+            ReportAttachment.objects.get(id=report_attachment_2010_submitted.id)
 
-#         def insert_function(cursor):
-#             ReportAttachment.objects.create(
-#                 report_version=test.report_version,
-#                 attachment_name='new attachment',
-#                 attachment='new_attachment.pdf',
-#                 attachment_type=ReportAttachment.ReportAttachmentType.WCI_352_362,
-#             )
+        def forbidden_select_function(cursor):
+            ReportAttachment.objects.get(id=report_attachment_2013_draft.id)
 
-#         def forbidden_insert_function(cursor):
-#             cursor.execute(
-#                 """
-#                     INSERT INTO "erc"."report_attachment" (
-#                         "report_version_id", "attachment", "attachment_type", "attachment_name"
-#                     )
-#                     VALUES (%s, %s, %s, %s)
-#                 """,
-#                 (
-#                     random.report_version.id,
-#                     "failed_attachment.pdf",
-#                     ReportAttachment.ReportAttachmentType.WCI_352_362,
-#                     "Failed_Attachment.pdf",
-#                 ),
-#             )
+        def insert_function(cursor):
+            ReportAttachment.objects.create(
+                report_version=t.report_version_2010_draft,
+                attachment='test.pdf',
+                attachment_type='wci_352_362',
+                attachment_name='test',
+            )
 
-#         def update_function(cursor):
-#             return ReportAttachment.objects.filter(id=test_report_attachment.id).update(
-#                 attachment_name="Updated_Attachment.pdf"
-#             )
+        def forbidden_insert_function(cursor):
+            cursor.execute(
+                """
+                    INSERT into "erc"."report_attachment"(report_version_id)
+                    values(%s)
+                """,
+                (t.report_version_2013_draft.id,),
+            )
 
-#         def forbidden_update_function(cursor):
-#             return ReportAttachment.objects.filter(id=random_report_attachment.id).update(
-#                 attachment_name="Updated_Attachment.pdf"
-#             )
+        def update_function(cursor):
+            cursor.execute(
+                """
+                    UPDATE "erc"."report_attachment"
+                    SET attachment_name = %s
+                    WHERE id = %s
+                """,
+                ('changed', report_attachment_2010_draft.id),
+            )
+            return cursor.rowcount
 
-#         def delete_function(cursor):
-#             test_report_attachment.delete()
+        def forbidden_update_function(cursor):
+            cursor.execute(
+                """
+                    UPDATE "erc"."report_attachment"
+                    SET attachment_name = %s
+                    WHERE id = %s
+                """,
+                ('changed', report_attachment_2013_draft.id),
+            )
+            return cursor.rowcount
 
-#         def forbidden_delete_function(cursor):
-#             random_report_attachment.delete()
+        def delete_function(cursor):
+            # Delete the report for the approved user operator
+            cursor.execute(
+                """
+                    DELETE from "erc"."report_attachment"
+                    WHERE id = %s
+                """,
+                (report_attachment_2010_draft.id,),
+            )
+            return cursor.rowcount
 
-#         assert_policies_for_industry_user(
-#             ReportAttachment,
-#             test.approved_user_operator.user,
-#             select_function,
-#             insert_function,
-#             update_function,
-#             delete_function,
-#             forbidden_select_function=forbidden_select_function,
-#             forbidden_insert_function=forbidden_insert_function,
-#             forbidden_update_function=forbidden_update_function,
-#             forbidden_delete_function=forbidden_delete_function,
-#         )
+        def forbidden_delete_function(cursor):
+            # Delete the report for the approved user operator
+            cursor.execute(
+                """
+                    DELETE from "erc"."report_attachment"
+                    WHERE id in (%s,%s)
+                """,
+                (report_attachment_2010_submitted.id, report_attachment_2013_draft.id),
+            )
+            return cursor.rowcount
 
-#     def test_report_attachment_rls_cas_user(self):
-#         test_quantity = 5
-#         for i in range(test_quantity):
-#             facility_report = make_recipe("reporting.tests.utils.facility_report")
-#             make(
-#                 ReportAttachment,
-#                 report_version=facility_report.report_version,
-#                 attachment=f'test_attachment_{i}.pdf',
-#             )
+        assert_policies_for_industry_user(
+            ReportAttachment,
+            t.approved_user_operator.user,
+            select_function=select_function,
+            insert_function=insert_function,
+            update_function=update_function,
+            delete_function=delete_function,
+            forbidden_select_function=forbidden_select_function,
+            forbidden_insert_function=forbidden_insert_function,
+            forbidden_update_function=forbidden_update_function,
+            forbidden_delete_function=forbidden_delete_function,
+        )
 
-#         def select_function(cursor):
-#             assert ReportAttachment.objects.count() == test_quantity
+    def test_report_attachment_rls_cas_user(self):
+        test_quantity = 5
+        make_recipe(
+            "reporting.tests.utils.report_attachment",
+            _quantity=test_quantity,
+        )
 
-#         assert_policies_for_cas_roles(
-#             ReportAttachment,
-#             select_function=select_function,
-#         )
+        def select_function(cursor):
+            assert ReportAttachment.objects.count() == test_quantity
+
+        assert_policies_for_cas_roles(
+            ReportAttachment,
+            select_function=select_function,
+        )
--- bc_obps/reporting/tests/models/test_report_electricity_import_data.py
+++ bc_obps/reporting/tests/models/test_report_electricity_import_data.py
@@ -5,6 +5,10 @@ from model_bakery.baker import make_recipe
 from reporting.tests.utils.immutable_report_version import (
     assert_immutable_report_version,
 )
+from reporting.models import ReportElectricityImportData
+from reporting.tests.utils.report_rls_test_infrastructure import ReportRlsTestSetup
+from rls.tests.helpers import assert_policies_for_cas_roles, assert_policies_for_industry_user
+from common.lib import pgtrigger
 
 
 class ReportElectricityImportDataTest(BaseTestCase):
@@ -29,3 +33,128 @@ class ReportElectricityImportDataTest(BaseTestCase):
 
     def test_immutable_after_report_version_submitted(self):
         assert_immutable_report_version("reporting.tests.utils.electricity_import_data")
+
+
+class ReportElectricityImportDataRlsTest(BaseTestCase):
+
+    def test_report_electricity_import_data_rls_industry_user(self):
+        # Common Test Setup
+        t = ReportRlsTestSetup()
+
+        # ReportElectricityImportData Setup
+        # Inside access bounds
+        with pgtrigger.ignore("reporting.ReportElectricityImportData:immutable_report_version"):
+            report_electricity_import_data_2010_submitted = make_recipe(
+                'reporting.tests.utils.electricity_import_data', report_version=t.report_version_2010_submitted
+            )
+        report_electricity_import_data_2010_draft = make_recipe(
+            'reporting.tests.utils.electricity_import_data', report_version=t.report_version_2010_draft
+        )
+        # Outside access bounds
+        report_electricity_import_data_2013_draft = make_recipe(
+            'reporting.tests.utils.electricity_import_data', report_version=t.report_version_2013_draft
+        )
+
+        # Additional report_version needed for insert test: 2012 - Within access bounds
+        reporting_year_2012 = make_recipe('reporting.tests.utils.reporting_year', reporting_year=2012)
+        report_2012 = make_recipe(
+            'reporting.tests.utils.report',
+            operation=t.report_version_2010_submitted.report.operation,
+            operator=t.report_version_2010_submitted.report.operator,
+            reporting_year=reporting_year_2012,
+        )
+        report_version_2012_draft = make_recipe(
+            'reporting.tests.utils.report_version', report=report_2012, status='Draft'
+        )
+
+        def select_function(cursor):
+            ReportElectricityImportData.objects.get(id=report_electricity_import_data_2010_submitted.id)
+
+        def forbidden_select_function(cursor):
+            ReportElectricityImportData.objects.get(id=report_electricity_import_data_2013_draft.id)
+
+        def insert_function(cursor):
+            ReportElectricityImportData.objects.create(
+                report_version=report_version_2012_draft,
+            )
+
+        def forbidden_insert_function(cursor):
+            cursor.execute(
+                """
+                    INSERT into "erc"."report_electricity_import_data"(report_version_id)
+                    values(%s)
+                """,
+                (t.report_version_2013_draft.id,),
+            )
+
+        def update_function(cursor):
+            cursor.execute(
+                """
+                    UPDATE "erc"."report_electricity_import_data"
+                    SET canadian_entitlement_emissions = %s
+                    WHERE id = %s
+                """,
+                (20.00, report_electricity_import_data_2010_draft.id),
+            )
+            return cursor.rowcount
+
+        def forbidden_update_function(cursor):
+            cursor.execute(
+                """
+                    UPDATE "erc"."report_electricity_import_data"
+                    SET canadian_entitlement_emissions = %s
+                    WHERE id = %s
+                """,
+                (20.00, report_electricity_import_data_2013_draft.id),
+            )
+            return cursor.rowcount
+
+        def delete_function(cursor):
+            # Delete the report for the approved user operator
+            cursor.execute(
+                """
+                    DELETE from "erc"."report_electricity_import_data"
+                    WHERE id = %s
+                """,
+                (report_electricity_import_data_2010_draft.id,),
+            )
+            return cursor.rowcount
+
+        def forbidden_delete_function(cursor):
+            # Delete the report for the approved user operator
+            cursor.execute(
+                """
+                    DELETE from "erc"."report_electricity_import_data"
+                    WHERE id in (%s,%s)
+                """,
+                (report_electricity_import_data_2010_submitted.id, report_electricity_import_data_2013_draft.id),
+            )
+            return cursor.rowcount
+
+        assert_policies_for_industry_user(
+            ReportElectricityImportData,
+            t.approved_user_operator.user,
+            select_function=select_function,
+            insert_function=insert_function,
+            update_function=update_function,
+            delete_function=delete_function,
+            forbidden_select_function=forbidden_select_function,
+            forbidden_insert_function=forbidden_insert_function,
+            forbidden_update_function=forbidden_update_function,
+            forbidden_delete_function=forbidden_delete_function,
+        )
+
+    def test_report_electricity_import_data_rls_cas_user(self):
+        test_quantity = 5
+        make_recipe(
+            "reporting.tests.utils.electricity_import_data",
+            _quantity=test_quantity,
+        )
+
+        def select_function(cursor):
+            assert ReportElectricityImportData.objects.count() == test_quantity
+
+        assert_policies_for_cas_roles(
+            ReportElectricityImportData,
+            select_function=select_function,
+        )
