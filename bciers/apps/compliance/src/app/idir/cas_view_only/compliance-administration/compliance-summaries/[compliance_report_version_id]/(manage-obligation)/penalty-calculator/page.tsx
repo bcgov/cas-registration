4f02f987fdcfbd17056cf6505dafd72f0188b691
Andrea <andrea.williams@gov.bc.ca>
2026-08-17 17:57:11 -0700

    chore: rearranged files

--- bciers/apps/compliance/src/app/idir/cas_view_only/compliance-administration/compliance-summaries/[compliance_report_version_id]/(manage-obligation)/penalty-calculator/page.tsx
+++ bciers/apps/compliance/src/app/idir/cas_view_only/compliance-administration/compliance-summaries/[compliance_report_version_id]/(manage-obligation)/penalty-calculator/page.tsx
@@ -1,4 +1,4 @@
 import defaultPageFactory from "@bciers/components/nextPageFactory/defaultPageFactory";
-import Page from "@/compliance/src/app/components/compliance-summary/manage-obligation/internal/review-penalty-summary/PenaltyCalculatorPage";
+import Page from "@/compliance/src/app/components/compliance-summary/manage-obligation/internal/review-compliance-summary/PenaltyCalculatorPage";
 
 export default defaultPageFactory(Page);
