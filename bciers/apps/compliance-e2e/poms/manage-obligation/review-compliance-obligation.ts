1b0f14e9dc14856f8f950cd54b60fbcebfe90c47
SeSo <sobhani.sepehr@gmail.com>
2026-08-21 14:12:07 -0700

    chore: retry invoice button click until request fires to avoid flake

--- bciers/apps/compliance-e2e/poms/manage-obligation/review-compliance-obligation.ts
+++ bciers/apps/compliance-e2e/poms/manage-obligation/review-compliance-obligation.ts
@@ -3,7 +3,6 @@ import {
   COMPLIANCE_INVOICE_API_BASE,
   ComplianceInvoiceType,
   GENERATE_INVOICE_BUTTON_TEXT,
-  INVOICE_GENERATION_TIMEOUT_MS,
 } from "@/compliance-e2e/utils/constants";
 
 export class ReviewComplianceObligationPOM {
@@ -31,12 +30,21 @@ export class ReviewComplianceObligationPOM {
     const invoiceUrl = new RegExp(
       `${COMPLIANCE_INVOICE_API_BASE}/${id}/${type}(\\?|$)`,
     );
-    const [response] = await Promise.all([
-      this.page.waitForResponse((r) => invoiceUrl.test(r.url()), {
-        timeout: INVOICE_GENERATION_TIMEOUT_MS,
-      }),
-      this.clickGenerateInvoice(),
-    ]);
+    const responsePromise = this.page.waitForResponse(
+      (r) => invoiceUrl.test(r.url()),
+      { timeout: 30_000 },
+    );
+
+    await expect(async () => {
+      await Promise.all([
+        this.page.waitForRequest((r) => invoiceUrl.test(r.url()), {
+          timeout: 5_000,
+        }),
+        this.clickGenerateInvoice(),
+      ]);
+    }).toPass({ timeout: 30_000 });
+
+    const response = await responsePromise;
 
     const contentType = response.headers()["content-type"] ?? "";
 
