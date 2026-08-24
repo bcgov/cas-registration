d942159b0d854df8a12dde2b09120510dbb0a21b
Gabriele Dal Cengio <gabriele.dalcengio@gov.bc.ca>
2026-08-24 12:15:37 -0700

    chore: update schema in bccr account_id get endpoint

--- bc_obps/compliance/api/_bccr/_accounts/_account_id/_compliance_report_versions/_compliance_report_version_id/account_id.py
+++ bc_obps/compliance/api/_bccr/_accounts/_account_id/_compliance_report_versions/_compliance_report_version_id/account_id.py
@@ -1,5 +1,5 @@
 from compliance.api.router import router
-from typing import Literal, Tuple, Optional, Dict
+from typing import Literal, Tuple
 from django.http import HttpRequest
 from compliance.service.bc_carbon_registry.schema import FifteenDigitString
 from service.error_service.custom_codes_4xx import custom_codes_4xx
@@ -7,6 +7,7 @@ from registration.schema.generic import Message
 from compliance.constants import BCCR
 from compliance.service.bc_carbon_registry.account_service import BCCarbonRegistryAccountService
 from compliance.service.bc_carbon_registry.exceptions import BCCarbonRegistryError
+from compliance.schema.bccr_account import BCCRAccountDetailsSchema
 from compliance.api.permissions import approved_industry_user_compliance_report_version_composite_auth
 
 bccr_service = BCCarbonRegistryAccountService()
@@ -14,19 +15,19 @@ bccr_service = BCCarbonRegistryAccountService()
 
 @router.get(
     "/bccr/accounts/{account_id}/compliance-report-versions/{compliance_report_version_id}",
-    response={200: dict, custom_codes_4xx: Message},
+    response={200: BCCRAccountDetailsSchema, custom_codes_4xx: Message},
     tags=BCCR,
     description="Get BCCR account details",
     auth=approved_industry_user_compliance_report_version_composite_auth,
 )
 def get_bccr_account_details(
     request: HttpRequest, account_id: FifteenDigitString, compliance_report_version_id: int
-) -> Tuple[Literal[200], Dict[str, Optional[str | bool]]]:
+) -> Tuple[Literal[200], BCCRAccountDetailsSchema]:
     try:
         account_details = bccr_service.get_account_details(account_id=account_id)
         trading_name = getattr(account_details, "trading_name", None) if account_details else None
-        return 200, {"bccr_trading_name": trading_name, "has_remote_bccr_errors": False}
+        return 200, BCCRAccountDetailsSchema(bccr_trading_name=trading_name)
 
     except BCCarbonRegistryError:
         # Handle exceptions that come from BCCR API
-        return 200, {"bccr_trading_name": None, "has_remote_bccr_errors": True}
+        return 200, BCCRAccountDetailsSchema(bccr_trading_name=None, has_remote_bccr_errors=True)
