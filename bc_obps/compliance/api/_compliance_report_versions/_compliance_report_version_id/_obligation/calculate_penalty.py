a9774fabef22a0f5fea3599fe81776126c4b915e
Andrea <andrea.williams@gov.bc.ca>
2026-09-03 15:03:31 -0700

    chore: more refactoring

--- bc_obps/compliance/api/_compliance_report_versions/_compliance_report_version_id/_obligation/calculate_penalty.py
+++ bc_obps/compliance/api/_compliance_report_versions/_compliance_report_version_id/_obligation/calculate_penalty.py
@@ -4,7 +4,7 @@ from compliance.constants import COMPLIANCE
 from service.error_service.custom_codes_4xx import custom_codes_4xx
 from registration.schema.generic import Message
 from compliance.api.router import router
-from compliance.schema.calculated_penalty import CalculatedPenaltyOut
+from compliance.schema.calculated_penalty import CalculatedPenaltyOut, PenaltyAccrual
 from compliance.service.penalty_calculation_service import (
     PenaltyCalculationService,
 )
@@ -22,6 +22,27 @@ def get_calculated_penalty_for_obligation(
     request: HttpRequest, compliance_report_version_id: int, requested_penalty_type: str, end_date: str
 ) -> Tuple[Literal[200], CalculatedPenaltyOut] | Tuple[Literal[422], dict[str, str]]:
 
-    return 200, PenaltyCalculationService.calculate_penalty_for_obligation(
+    result = PenaltyCalculationService.calculate_penalty_for_obligation(
         compliance_report_version_id, requested_penalty_type, end_date
+    )
+
+    calculated_penalty = result.calculated_penalty
+
+    return 200, CalculatedPenaltyOut(
+        automatic_overdue_penalty_status=result.automatic_overdue_penalty_status,
+        ggeapar_interest_status=result.ggeapar_interest_status,
+        penalty_type=calculated_penalty.penalty_type,
+        days_late=calculated_penalty.days_late,
+        total_penalty=calculated_penalty.total_penalty,
+        daily_accumulated_list=[
+            PenaltyAccrual(
+                date=accrual.date,
+                interest_rate=accrual.interest_rate,
+                daily_penalty=accrual.daily_penalty,
+                daily_compounded=accrual.daily_compounded,
+                accumulated_penalty=accrual.accumulated_penalty,
+                accumulated_compounded=accrual.accumulated_compounded,
+            )
+            for accrual in calculated_penalty.daily_accumulated_list
+        ],
     )
