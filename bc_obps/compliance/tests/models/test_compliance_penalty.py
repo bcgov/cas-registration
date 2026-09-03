15bdd62822f758b88388a6a55cceef3430fc755d
Dylan <dylan.1.leard@gov.bc.ca>
2026-09-03 13:20:41 -0700

    chore: updated compliance_penalty rls + tests

--- bc_obps/compliance/tests/models/test_compliance_penalty.py
+++ bc_obps/compliance/tests/models/test_compliance_penalty.py
@@ -1,9 +1,9 @@
 from rls.tests.helpers import assert_policies_for_cas_roles, assert_policies_for_industry_user
-from compliance.models import CompliancePenalty, ComplianceReportVersion
+from compliance.models import CompliancePenalty
 from common.tests.utils.helpers import BaseTestCase
 from registration.tests.constants import TIMESTAMP_COMMON_FIELDS
 from model_bakery.baker import make_recipe
-from compliance.tests.utils.compliance_test_helper import ComplianceTestHelper
+from compliance.tests.utils.compliance_rls_test_infrastructure import ComplianceReportRlsTestSetup
 
 
 class CompliancePenaltyTest(BaseTestCase):
@@ -31,90 +31,46 @@ class CompliancePenaltyTest(BaseTestCase):
 #  RLS tests
 class TestCompliancePenaltyRls(BaseTestCase):
     def test_compliance_penalty_rls_industry_user(self):
-        test_data_new_operator = ComplianceTestHelper.build_test_data(
-            crv_status=ComplianceReportVersion.ComplianceStatus.OBLIGATION_NOT_MET
+        t = ComplianceReportRlsTestSetup()
+        # within access bounds
+        obligation_2010 = make_recipe(
+            'compliance.tests.utils.compliance_obligation', compliance_report_version=t.compliance_report_version_2010
         )
-
-        # create two user_operators to set up for transfers
-        new_user_operator = make_recipe(
-            'registration.tests.utils.approved_user_operator', operator=test_data_new_operator.operation.operator
+        penalty_2010 = make_recipe('compliance.tests.utils.compliance_penalty', compliance_obligation=obligation_2010)
+        # outside access bounds
+        obligation_2013 = make_recipe(
+            'compliance.tests.utils.compliance_obligation', compliance_report_version=t.compliance_report_version_2013
         )
-        old_user_operator = make_recipe('registration.tests.utils.approved_user_operator')
-
-        # timeline of current and historical ownership
-        make_recipe(
-            'registration.tests.utils.operation_designated_operator_timeline',
-            operation=test_data_new_operator.operation,
-            operator=old_user_operator.operator,
-        )
-        make_recipe(
-            'registration.tests.utils.operation_designated_operator_timeline',
-            operation=test_data_new_operator.operation,
-            operator=new_user_operator.operator,
-        )
-        # old operator's data
-        old_operator_report = make_recipe(
-            'reporting.tests.utils.report',
-            operation=test_data_new_operator.operation,
-            operator=old_user_operator.operator,
-        )
-        old_operator_compliance_report = make_recipe(
-            'compliance.tests.utils.compliance_report', report=old_operator_report
-        )
-
-        old_operator_compliance_report_version = make_recipe(
-            'compliance.tests.utils.compliance_report_version', compliance_report=old_operator_compliance_report
-        )
-
-        old_operator_compliance_obligation = make_recipe(
-            'compliance.tests.utils.compliance_obligation',
-            compliance_report_version=old_operator_compliance_report_version,
-        )
-        old_operator_compliance_penalty = make_recipe(
-            'compliance.tests.utils.compliance_penalty', compliance_obligation=old_operator_compliance_obligation
-        )
-
-        new_operator_compliance_penalty = make_recipe(
-            'compliance.tests.utils.compliance_penalty',
-            compliance_obligation=test_data_new_operator.compliance_obligation,
-        )
+        penalty_2013 = make_recipe('compliance.tests.utils.compliance_penalty', compliance_obligation=obligation_2013)
 
         def select_function(cursor):
-            CompliancePenalty.objects.get(id=new_operator_compliance_penalty.id)
+            CompliancePenalty.objects.get(id=penalty_2010.id)
 
         def forbidden_select_function(cursor):
-            CompliancePenalty.objects.get(id=old_operator_compliance_penalty.id)
+            CompliancePenalty.objects.get(id=penalty_2013.id)
 
         assert_policies_for_industry_user(
             CompliancePenalty,
-            new_user_operator.user,
-            select_function=select_function,
-            forbidden_select_function=forbidden_select_function,
-        )
-
-        def select_function(cursor):
-            CompliancePenalty.objects.get(id=old_operator_compliance_penalty.id)
-
-        def forbidden_select_function(cursor):
-            CompliancePenalty.objects.get(id=new_operator_compliance_penalty.id)
-
-        assert_policies_for_industry_user(
-            CompliancePenalty,
-            old_user_operator.user,
+            t.approved_user_operator.user,
             select_function=select_function,
             forbidden_select_function=forbidden_select_function,
         )
 
     def test_compliance_penalty_rls_cas_users(self):
-        test_data = ComplianceTestHelper.build_test_data(
-            crv_status=ComplianceReportVersion.ComplianceStatus.OBLIGATION_NOT_MET
+        t = ComplianceReportRlsTestSetup()
+        # within access bounds
+        obligation_2010 = make_recipe(
+            'compliance.tests.utils.compliance_obligation', compliance_report_version=t.compliance_report_version_2010
         )
-        make_recipe(
-            'compliance.tests.utils.compliance_penalty', id=888, compliance_obligation=test_data.compliance_obligation
+        make_recipe('compliance.tests.utils.compliance_penalty', compliance_obligation=obligation_2010)
+        # outside access bounds
+        obligation_2013 = make_recipe(
+            'compliance.tests.utils.compliance_obligation', compliance_report_version=t.compliance_report_version_2013
         )
+        make_recipe('compliance.tests.utils.compliance_penalty', compliance_obligation=obligation_2013)
 
         def select_function(cursor):
-            assert CompliancePenalty.objects.count() == 1
+            assert CompliancePenalty.objects.count() == 2
 
         assert_policies_for_cas_roles(
             CompliancePenalty,
