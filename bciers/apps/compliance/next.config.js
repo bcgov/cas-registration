9de59b468bad31d2dcb2763c5a49c305f1c85cae
SeSo <sobhani.sepehr@gmail.com>
2026-09-10 10:30:47 -0700

    chore(deps): migrate to nx 23 and remove @nx/next config wrappers

--- bciers/apps/compliance/next.config.js
+++ bciers/apps/compliance/next.config.js
@@ -1,23 +1,15 @@
-const { composePlugins, withNx } = require("@nx/next");
-
 // Next.js doesn't use TS's paths, so we need to use the relative path
 const { nextConfigBase, withSentry } = require("../../next.config.base");
 
 /**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   ...nextConfigBase,
   // To deploy a Next.js application under a sub-path of a domain you can use the basePath config option
   basePath: "/compliance",
   assetPrefix: "/compliance/",
-  nx: {},
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-  withSentry, // Use shared Sentry config without overrides
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+// Wrap the config in more Next.js plugins here if needed.
+module.exports = withSentry(nextConfig);
