f3889e404810ad3193d3cfe3774f8308192b7456
acatchpole <adamantiumite@gmail.com>
2026-09-14 15:14:11 -0700

    chore: add currency widget to currencyuiconfig

--- bciers/apps/compliance/src/app/data/jsonSchema/helpers.ts
+++ bciers/apps/compliance/src/app/data/jsonSchema/helpers.ts
@@ -63,6 +63,7 @@ export const tco2eUiConfig = {
 
 export const currencyUiConfig = {
   ...commonReadOnlyOptions,
+  "ui:widget": "ReadOnlyCurrencyWidget",
   "ui:options": {
     prefix: "$",
   },
