90bdd2a20ed1b9f3addecb734d5fc5e023a4e96d
Dylan <dylan.1.leard@gov.bc.ca>
2026-09-14 14:45:22 -0700

    fix: compliance summary page renders when a bccr connection error occurs

--- bciers/apps/compliance/src/app/utils/getComplianceAppliedUnits.ts
+++ bciers/apps/compliance/src/app/utils/getComplianceAppliedUnits.ts
@@ -3,19 +3,28 @@ import { ComplianceAppliedUnitsData } from "@/compliance/src/app/types";
 
 const getComplianceAppliedUnits = async (
   complianceReportVersionId: number,
-): Promise<ComplianceAppliedUnitsData> => {
-  const response = await actionHandler(
+): Promise<ComplianceAppliedUnitsData> =>
+  await actionHandler(
     `compliance/bccr/compliance-report-versions/${complianceReportVersionId}/applied-compliance-units`,
     "GET",
     "",
-  );
-  const data = response.applied_compliance_units;
-  const canApplyComplianceUnits = response.can_apply_compliance_units;
-  return {
-    rows: data,
-    row_count: data.length || 0,
-    can_apply_compliance_units: canApplyComplianceUnits,
-  };
-};
+  )
+    .then((response) => {
+      const data = response.applied_compliance_units;
+      const canApplyComplianceUnits = response.can_apply_compliance_units;
+      return {
+        rows: data,
+        row_count: data.length || 0,
+        can_apply_compliance_units: canApplyComplianceUnits,
+      };
+    })
+    .catch(() => {
+      return {
+        rows: [],
+        row_count: 0,
+        can_apply_compliance_units: false,
+        connection_error: true,
+      };
+    });
 
 export default getComplianceAppliedUnits;
