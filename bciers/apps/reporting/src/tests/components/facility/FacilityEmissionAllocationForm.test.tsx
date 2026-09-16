f5a327b31c3185bdc1cac2f9e4d9651c1efba8a9
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2026-09-16 16:24:11 -0700

    test: frontend

--- bciers/apps/reporting/src/tests/components/facility/FacilityEmissionAllocationForm.test.tsx
+++ bciers/apps/reporting/src/tests/components/facility/FacilityEmissionAllocationForm.test.tsx
@@ -380,6 +380,11 @@ describe("FacilityEmissionAllocationForm component", () => {
     expect(screen.getByRole("alert")).toHaveTextContent(
       /To allocate emissions to a product that isn't shown below, return to the production data page and select the product first./i,
     );
+    expect(
+      screen.queryByText(
+        /Note: unregulated products will not be shown below./i,
+      ),
+    ).not.toBeInTheDocument();
   });
 
   it("clears products when methodology is changed to 'Not Applicable' and restores them when changed back", async () => {
